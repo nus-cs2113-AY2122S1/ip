cc733de88b7ee4c41d9ed3f9890e8d073cd51356
Mohamed Irfan <irfancuber@gmail.com>
2021-08-14 00:57:11 +0800

    Add Increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        System.out.println(" Hello! I'm Duke\n" +
+                " What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
