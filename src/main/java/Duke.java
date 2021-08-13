232075a397d1e59886d8b318c08e3e65ff84d65b
theodorekwok <theodorekwok@gmail.com>
2021-08-13 18:53:02 +0800

    Add printSeparatingLine method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,18 @@
 public class Duke {
+    public static void printSeparatingLine() {
+        System.out.println("---------------------------------");
+    }
+
     public static void greet() {
+        printSeparatingLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
     }
 
     public static void goodbye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        printSeparatingLine();
+        System.out.println("Bye. Hope to see you again soon!\n");
+        printSeparatingLine();
     }
 
     public static void main(String[] args) {
