82983ec4815c3de10e5104aef2809a3ec29dd23d
Kureans <leonardtengys@gmail.com>
2021-08-15 02:35:41 +0800

    Implement increment: Level 0 in Duke.java Add .class & .md extension to .gitignore

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,23 @@
 public class Duke {
+    public static void greeting() {
+        line();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("");
+    }
+
+    public static void exit() {
+        line();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void line() {
+        for (int i = 0; i < 40; i++) {
+            System.out.print("_");
+        }
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +25,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greeting();
+        exit();
+        line();
+
+
     }
 }
