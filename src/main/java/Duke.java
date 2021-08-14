4d3b862c62b63269b52521b12494bd07d9bb006e
Woolicious98 <e527269@u.nus.edu>
2021-08-14 18:19:03 +0800

    Add Increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.sql.SQLOutput;
+
 public class Duke {
+    final static String line = "____________________________________________________________";
     public static void main(String[] args) {
+        System.out.println(line);
+        printWelcomeMesage();
+        printExitMessage();
+    }
+    public static void printExitMessage(){
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+    public static void printWelcomeMesage(){
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+    }
+    public static void printLogoMessage(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(line);
     }
 }
