5e296dd2ea42bb979868153b3f2976163d8581fc
Wei Yin, Joanne Wong <jwweiyin@gmail.com>
2021-08-14 14:04:59 +0800

    Add Greet Function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,18 @@
 public class Duke {
+
+    public static void printLine(){
+        String line = "__________________________________________________";
+        System.out.println(line);
+    }
+
+    public static void Greet(){
+        printLine();
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +20,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Greet();
     }
 }
