492d124fd0a747f841013600c8177eefd91a450c
KZQ1999 <e0543569@u.nus.edu>
2021-08-14 20:19:34 +0800

    Add increment level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,13 @@
 public class Duke {
+
+    public static void printLine(String[] args) {
+        for(int x =0;x <= 50;x++){
+            System.out.print("-");
+        }
+        System.out.println(" ");
+    }
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +15,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printLine(args);
+        System.out.println("Hello! I'm Duke'");
+        System.out.println("What can I do for you?");
+        printLine(args);
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
