5bbf4356b811975433fbaca46c67226bc146f308
NonRNP <69129667+NonRNP@users.noreply.github.com>
2021-08-14 15:50:50 +0800

    level 0 - Greet
    Duke greets the user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,17 @@
 public class Duke {
+    public static void addLine() {
+        System.out.println("----------------------");
+    }
+
+    public static void dukeGreet() {
+        System.out.println("Hello!, I'm Duke");
+        System.out.println("How can I help you?");
+    }
+
+    public static void dukeBye() {
+        System.out.println("Bye, see you again!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +19,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        addLine();
+
+        dukeGreet();
+        addLine();
+        
+        dukeBye();
+        addLine();
     }
 }
