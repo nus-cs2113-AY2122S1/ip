1084f710581797f585dca80307d91b014c8e1872
owensoh <owinsoh@gmail.com>
2021-08-14 20:14:13 +0800

    Add Level-0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(lines());
+        System.out.println(greeting());
+        System.out.println(lines());
+        System.out.println(bye());
+        System.out.println(lines());
+    }
+
+    public static String lines() {
+        return "____________________________________________________________";
+    }
+
+    public static String greeting() {
+        return "Hello! I'm Duke\nWhat can I do for you?";
+    }
+
+    public static String bye() {
+        return "Bye. Hope to see you again soon!";
     }
 }
