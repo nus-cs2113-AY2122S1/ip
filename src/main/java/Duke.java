3756a626f0ee34c0adb664c9187ef26f90cbfb8c
Tng Jun Wei <tngjunwei@gmail.com>
2021-08-15 11:58:04 +0800

    Add tasklist

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Duke {
 
+    static ArrayList<String> inputs = new ArrayList<String>();
+
     public static void main(String[] args) {
 
         String logo = " ____        _        \n"
@@ -19,12 +21,25 @@ public class Duke {
             String userInput = getUserInput();
             if(userInput.equals("bye"))
                 break;
-            else
-                System.out.println(userInput);
+            else if(userInput.equals("list"))
+                listInputs();
+            else {
+                inputs.add(userInput);
+                System.out.println("added: " + userInput);
+            }
             System.out.println();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void listInputs() {
+        Iterator<String> it = inputs.iterator();
+        int i = 1;
+
+        while(it.hasNext()) {
+            System.out.printf("%d. %s\n", i++, it.next());
+        }
     }
 
     public static String getUserInput() {
