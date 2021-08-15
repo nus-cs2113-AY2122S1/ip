91d98ece57b0e6276bb3a2e813ef8c80d59b9972
SeanRobertDH <seandh1998@gmail.com>
2021-08-15 20:50:25 +0800

    Add introductory and ending messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.sql.SQLOutput;
+
 public class Duke {
+    
+    private final String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___| inc.\n";
+    private final String spacer = "____________________________________________________________\n";
+    private final String introMsg = "Hello! I'm Duke\nWhat can I do for you?\n";
+    private final String endMsg = "Bye. Hope to see you again soon!\n";
+
+    void begin(){
+        System.out.print(logo+spacer+introMsg+spacer);
+    }
+    void end(){
+        System.out.print(endMsg+spacer);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.begin();
+        duke.end();
     }
 }
