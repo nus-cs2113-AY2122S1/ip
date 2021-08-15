d95cf1526c24d763c227e859a1dac8110713e4b2
Justin Wong <justinfidelis@gmail.com>
2021-08-15 17:31:55 +0800

    Add Increment: Level 0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
 public class Duke {
     public static void main(String[] args) {
+        printStartMessage();
+        printExitMessage();
+    }
+
+    public static void printAppLogo() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+
+    public static void printLine() {
+        System.out.println("______________________________________");
+    }
+
+    public static void printStartMessage() {
+        printLine();
+        System.out.println("Hello! I'm Duke\n How may I assist you");
+        printLine();
+    }
+
+    public static void printExitMessage() {
+        System.out.println("Goodbye! Hope to see you soon!");
+        printLine();
     }
 }
