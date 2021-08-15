487902aed55bd0c90de0ea26605f1825bec0059f
Kair0s3 <stianboon@gmail.com>
2021-08-15 13:41:17 +0800

    Level-0
    Level-0 increment for Week 2's iP task. Adding a simple greeting and an ASCII Art as the logo.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Generated ASCII Art - https://patorjk.com/software/taag/#p=display&f=Dancing%20Font&t=Duke
+        String logo = "  ____     _   _    _  __  U _____ u \n"
+                    + " |  _\"\\ U |\"|u| |  |\"|/ /  \\| ___\"|/ \n"
+                    + "/| | | | \\| |\\| |  | ' /    |  _|\"\n"
+                    + "U| |_| |\\ | |_| |U/| . \\u   | |___\n"
+                    + " |____/ u<<\\___/   |_|\\_\\   |_____|\n"
+                    + "  |||_  (__) )(  ,-,>> \\\\,-.<<   >>\n"
+                    + " (__)_)     (__)  \\.)   (_/(__) (__)\n";
+        
+        System.out.println(logo);
+
+        // Skeletal Barebone - Level-0
+        String greeting = "____________________________________________________________\n"
+                            + " Hello! I'm Duke\n"
+                            + " What can I do for you?\n"
+                            + "____________________________________________________________\n"
+                            + " Bye. Hope to see you again soon!\n"
+                            + "____________________________________________________________\n";
+
+        System.out.println(greeting);
     }
 }
