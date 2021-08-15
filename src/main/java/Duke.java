2fd78fdf107b434bb19341c5d468d9f2a8fccb3d
Jonathan Mui <jonathanmui4@gmail.com>
2021-08-15 10:33:47 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String line = "____________________________________________________________\n";
+
+        String helloGreeting = "Hello! I'm Duke \n" + "What can I do for you?\n";
+
+        String byeGreeting = "Bye. Hope to see you again soon!\n";
+
+        System.out.println(line + helloGreeting + line + byeGreeting + line);
+
+
     }
 }
