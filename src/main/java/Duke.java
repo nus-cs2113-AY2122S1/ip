cdd6b46efd6d8585b06680461c0ba34e1020dc80
powzx <69313768+powzx@users.noreply.github.com>
2021-08-15 22:24:02 +0800

    Added Level 0
    Greet and exit messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
 public class Duke {
-    public static void main(String[] args) {
+
+    public static void printHorizontal() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greetUser() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printHorizontal();
+        System.out.println("Welcome to\n" + logo);
+        System.out.println("Hello there! I'm Duke, your personal assistant chat bot.\n"
+                + "What can I do for you today?");
+        printHorizontal();
+    }
+
+    public static void exitUser() {
+        System.out.println("Bye! See you again soon. \uD83D\uDE04");
+        printHorizontal();
+    }
+
+    public static void main(String[] args) {
+        greetUser();
+        exitUser();
     }
 }
