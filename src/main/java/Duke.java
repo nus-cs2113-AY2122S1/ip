0920b56c306a64278e909b83598f0dbb5a424b52
arraysius <16810847+arraysius@users.noreply.github.com>
2021-08-15 16:23:13 +0800

    Add greeting and bye messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +7,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String line = "____________________________________________________________";
+
+        // Greet
+        System.out.println(line
+                + "\nHello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + line);
+
+        // Bye
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
     }
 }
