3a54ff6bdd71faad9b4b2620110f25c3b03b858f
rizemon <tan.jia.le.98@gmail.com>
2021-08-17 01:37:30 +0800

    Add increment Level-0 that prints hello and bye message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
+
+    private final static String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private final static String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.print("Hello from\n" + LOGO);
+        System.out.println(LINE);
+
+        System.out.println(" Hello! I'm Duke\n"
+                + " What can I do for you?");
+        System.out.println(LINE);
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
