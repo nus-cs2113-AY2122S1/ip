424a26e9a235ed658c743a7d2cc82548f3c70263
LAPTOP-61K9T4RL\Nai Jie <yeatnaijie@gmail.com>
2021-08-16 21:47:38 +0800

    Add Level 0 increment - Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,10 @@
 public class Duke {
+    public static void printHorizontalLine(int length) {
+        for(int i = 0; i < length; i++) {
+            System.out.print("-");
+        }
+        System.out.println("");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +12,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        printHorizontalLine(100);
+        System.out.println("Thank you for using our application. We hope to see you again soon");
+        printHorizontalLine(100);
     }
 }
