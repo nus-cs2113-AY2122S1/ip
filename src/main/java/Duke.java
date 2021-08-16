af54223c9c7f471684bae54c74eb1f797ac01318
Kelvin Neo <2332196+kelvneo@users.noreply.github.com>
2021-08-16 15:26:56 +0800

    Add increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,21 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printSection("Hello from", logo);
+        printSection("Hello! I'm Duke", "What can I do for you?");
+        printSection("Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Prints the given data provided, and a horizontal line denoting the end of a section.
+     * If no data is provided, it will print a horizontal line.
+     *
+     * @param data The string(s) to print to console.
+     */
+    private static void printSection(String ...data) {
+        for (String s: data) {
+            System.out.println(s);
+        }
+        System.out.println("____________________________________________________________");
     }
 }
