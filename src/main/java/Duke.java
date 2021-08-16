5f06155f38c4ec97db0190f1011159467be61972
APZH <amospzh@gmail.com>
2021-08-17 01:59:10 +0800

    Update UI design + Add display UI method to "Duke" class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,36 @@
+import java.sql.SQLOutput;
+
 public class Duke {
+
+    private final String logo = "-------------------------------------------\n"
+                            + "  __         __\n"
+                            + " /  \\.-\"\"\"-./  \\     ____        _        \n"
+                            + " \\    -   -    /    |  _ \\ _   _| | _____ \n"
+                            + "  |   o   o   |     | | | | | | | |/ / _ \\\n"
+                            + "  \\  .-'''-.  /     | |_| | |_| |   <  __/\n"
+                            + "   '-\\__Y__/-'      |____/ \\__,_|_|\\_\\___|\n"
+                            + "      `---`\n"
+                            + "-------------------------------------------";
+
+    private final String introMsg = "[Duke]:\n-> Hello! I'm Duke \uD83D\uDE00\n" + "-> What can I do for you?\n";
+
+    private final String exitMsg = "[Duke]:\n-> Bye. Hope to see you again soon!\n";
+
+    /* Default Constructor */
+    public Duke() {
+        displayUI();
+    }
+
+    /* Method to Display User Interface */
+    public void displayUI() {
+        System.out.println(logo);
+        System.out.println(introMsg);
+        System.out.println(exitMsg);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
 
-        String introMsg = "____________________________________________________________\n"
-                + " Hello! I'm Duke\n"
-                + " What can I do for you?\n";
-
-        String exitMsg = "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-
-        System.out.println(introMsg + exitMsg);
-
+        Duke myObject = new Duke();
 
     }
 }
