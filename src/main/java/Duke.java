52fd2251e02ed0ed437cd190bf9ede8fb7cd9aad
3m0W33D <sky.t4y@gmail.com>
2021-08-16 10:32:15 +0800

    Implement Level-0: Greet
    - Add greeting and bye messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String line = "____________________________________________________________";
+
+        String greetMsg = line
+                + "\nHello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + line;
+
+        String byeMsg = "Bye. Hope to see you again soon!\n" + line;
+
+        System.out.println(greetMsg + "\n" + byeMsg);
     }
 }
