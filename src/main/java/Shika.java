6568bebdd4d1c19e8e06f0bf23a42aceaa677fc2
ChewyChair <69298017+ChewyChair@users.noreply.github.com>
2021-08-17 12:33:30 +0800

    Update to reflect Java Coding Standard

--- src/main/java/Shika.java
+++ src/main/java/Shika.java
@@ -4,6 +4,10 @@ public class Shika {
 
     public static String line = "______________________________________________________\n";
 
+    /**
+     * Main function that displays Shika logo and greeting message, then calls scan() to run Shika.
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         String logo = "  _________.__    .__ __            \n"
                 + " /   _____/|  |__ |__|  | _______   \n"
@@ -17,6 +21,11 @@ public class Shika {
         scan(taskList);
     }
 
+    /**
+     * Function that input is an integer. It checks if value can be parsed by parseInt() without throwing an exception.
+     * @param input String to be parsed.
+     * @return Returns false if parseInt() throws an exception, returns true otherwise.
+     */
     public static boolean isInteger(String input) {
         try {
             Integer.parseInt(input);
@@ -26,6 +35,15 @@ public class Shika {
         }
     }
 
+    /**
+     * Function that waits for user input, then executes user command.
+     * "bye" will exit the program.
+     * "list" will list all current tasks.
+     * "done x" will mark task x as done, where x is the number of the task.
+     * Any other string will add it to the taskList as a task.
+     * Aside from "bye", scan() will call itself again after command is executed.
+     * @param taskList Array containing all recorded tasks.
+     */
     public static void scan(Task[] taskList) {
         Scanner in = new Scanner(System.in);
         String text;
@@ -41,22 +59,18 @@ public class Shika {
                 int index = Integer.parseInt(str);
                 if (index > Task.count) {
                     System.out.print(line + "> Oopsie! That task does not exist... yet!\n" + line);
-                }
-                else if (index < 1) {
+                } else if (index < 1) {
                     System.out.print(line + "> ...Stop trying to break me :<\n" + line);
-                }
-                else {
+                } else {
                     taskList[index - 1].markAsDone();
                     System.out.println(line + "> Otsukare! You've done:");
                     taskList[index - 1].printTask();
                     System.out.print(line);
                 }
-            }
-            else {
+            } else {
                 System.out.print(line + "> Uh-oh! Exception occurred! Please key in a number :p\n" + line);
             }
-        }
-        else {
+        } else {
             taskList[Task.count] = new Task(text, Task.count);
             System.out.print(line + "> Added: " + text + "!\n" + line);
             Task.count += 1;
@@ -64,6 +78,10 @@ public class Shika {
         scan(taskList);
     }
 
+    /**
+     * Function to print all tasks in taskList.
+     * @param taskList Array containing all recorded tasks.
+     */
     public static void printTasks(Task[] taskList) {
         System.out.println(line + "> Here is your list of tasks:") ;
         for (int i = 0; i < Task.count; i++) {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,21 +1,34 @@
 public class Task {
 
+    /** Number of tasks recorded. */
+    public static int count = 0;
+
     private String name;
     private int id;
     private boolean isDone;
 
-    public static int count = 0;
-
+    /**
+     * Constructor for Task. Initialises an instance of Task with the input values.
+     * @param name Name of the task.
+     * @param id Number of the task.
+     */
     public Task(String name, int id) {
         this.name = name;
         this.id = id;
         this.isDone = false;
     }
 
-    public void markAsDone() { this.isDone = true; }
+    public void markAsDone() {
+        this.isDone = true;
+    }
 
-    public String status() { return (isDone ? "X" : " "); }
+    public String status() {
+        return (isDone ? "X" : " ");
+    }
 
+    /**
+     * Prints out the task. Displays its id, status and name.
+     */
     public void printTask() {
         System.out.println("\t" + (id + 1) + ".[" + status() + "] " + name);
     }
