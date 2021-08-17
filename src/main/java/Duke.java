73ffe644d00e47f5b1dc7290f1ccb415abaa3fd9
jushg <e0550425@u.nus.edu>
2021-08-17 10:12:28 +0800

    Add Increment: Level 0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,20 @@
 public class Duke {
+    public static void sayHello() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void sayGoodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void drawHorizontalLine(int length) {
+        for (int i = 0; i < length; i++) {
+            System.out.print("\u2500");
+        }
+        System.out.println("");
+    }
+    
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +22,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        drawHorizontalLine(32);
+        sayHello();
+        drawHorizontalLine(32);
+        sayGoodbye();
+        drawHorizontalLine(32);
     }
 }
