2192f2045e7fdb630e58c47919f707a6f0796bc3
poppolette <seashanty13.9@gmail.com>
2021-08-17 17:14:18 +0800

    Add opening and ending message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String lineBar = "____________________________________________________________\n";
+        String helloMessage = "Hello! I'm Duke\nWhat can I do for you?\n";
+        String goodbyeMessage = "Bye. Hope to see you again soon!\n";
+        System.out.println(lineBar + helloMessage + lineBar + goodbyeMessage + lineBar);
     }
 }
