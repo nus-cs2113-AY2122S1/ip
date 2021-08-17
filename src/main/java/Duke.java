bc0824e16f4310fc9db2691a1aa067271066fbaa
richwill28 <richwill@LAPTOP-6AQ96QAR.localdomain>
2021-08-18 01:26:32 +0800

    Implement an initial skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Greet.greet();
+    }
+}
+
+class Greet {
+    public static void greet() {
+        String logo = "      ____        _        \n"
+                    + "     |  _ \\ _   _| | _____ \n"
+                    + "     | | | | | | | |/ / _ \\\n"
+                    + "     | |_| | |_| |   <  __/\n"
+                    + "     |____/ \\__,_|_|\\_\\___|\n";
+
+        String line = "    ____________________________________________________________\n";
+
+        System.out.print(line);
+        System.out.println(logo);
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.print(line);
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.print(line);
     }
 }
