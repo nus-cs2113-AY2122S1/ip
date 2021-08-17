333d02fab8aa69e9d4ab615793cca8bed7d144df
Vignesh Kumaravel <k_vignesh@hotmail.com>
2021-08-17 12:35:36 +0800

    Level-0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,13 @@
 public class Duke {
+    public static void greet() {
+        System.out.println("_".repeat(30));
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+        System.out.println("_".repeat(30));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_".repeat(30));
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +15,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
