1674a80d8aa4004ecc8065ef06bbd0c9f7528c3f
ianwang <ianwangeeen@gmail.com>
2021-08-17 18:15:19 +0800

    Added greeting() and goodbyeMessage()

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,15 @@
 public class Duke {
+    public static void greeting() {
+        System.out.println("_____________________________________________________");
+        System.out.println("Hello! I'm Duke.");
+        System.out.println("What can I do for you today, sir?");
+        System.out.println("_____________________________________________________");
+    }
+
+    public static void goodbyeMessage() {
+        System.out.println("Goodbye. Till we meet again!");
+        System.out.println("_____________________________________________________");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +17,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greeting();
+        goodbyeMessage();
     }
 }
