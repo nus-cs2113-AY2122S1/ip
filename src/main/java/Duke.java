538cb85fa0d5017364a02fc821145b383a049742
richwill28 <e0550368@u.nus.edu>
2021-08-18 01:45:19 +0800

    Restructure Greet class to static methods in the Duke class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,29 @@
 public class Duke {
-    public static void main(String[] args) {
-        Greet.greet();
-    }
-}
+    static String logo = "      ____        _        \n"
+            + "     |  _ \\ _   _| | _____ \n"
+            + "     | | | | | | | |/ / _ \\\n"
+            + "     | |_| | |_| |   <  __/\n"
+            + "     |____/ \\__,_|_|\\_\\___|\n";
 
-class Greet {
-    public static void greet() {
-        String logo = "      ____        _        \n"
-                    + "     |  _ \\ _   _| | _____ \n"
-                    + "     | | | | | | | |/ / _ \\\n"
-                    + "     | |_| | |_| |   <  __/\n"
-                    + "     |____/ \\__,_|_|\\_\\___|\n";
+    static String line = "    ____________________________________________________________\n";
 
-        String line = "    ____________________________________________________________\n";
-
+    // Duke greeting
+    static void greet() {
         System.out.print(line);
         System.out.println(logo);
         System.out.println("     Hello! I'm Duke");
         System.out.println("     What can I do for you?");
         System.out.print(line);
+    }
+
+    // Duke exits
+    static void exit() {
         System.out.println("     Bye. Hope to see you again soon!");
         System.out.print(line);
+    }
+
+    public static void main(String[] args) {
+        greet();
+        exit();
     }
 }
