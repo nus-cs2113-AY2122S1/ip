eff0c1f1f06fde51d5b65c0c1ca3be09722ae0fa
poppolette <seashanty13.9@gmail.com>
2021-08-17 19:09:11 +0800

    Reformat greetings and goodbye message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,17 @@
 public class Duke {
+
+    public static void greetingsMessage() {
+        String lineBar = "____________________________________________________________\n";
+        String helloMessage = "Hello! I'm Duke\nWhat can I do for you?\n";
+        System.out.println(lineBar + helloMessage + lineBar);
+    }
+
+    public static void goodbyeMessage() {
+        String lineBar = "____________________________________________________________\n";
+        String goodbyeMessage = "Bye. Hope to see you again soon!\n";
+        System.out.println(goodbyeMessage + lineBar);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,9 +20,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        String lineBar = "____________________________________________________________\n";
-        String helloMessage = "Hello! I'm Duke\nWhat can I do for you?\n";
-        String goodbyeMessage = "Bye. Hope to see you again soon!\n";
-        System.out.println(lineBar + helloMessage + lineBar + goodbyeMessage + lineBar);
+        greetingsMessage();
+        goodbyeMessage();
     }
 }
