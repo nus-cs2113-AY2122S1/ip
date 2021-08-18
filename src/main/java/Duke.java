4fcb5574ca6dcfa3e73d683aacf60414fa58ba85
Tan Teck Hwee <tan.brian98@gmail.com>
2021-08-18 16:19:30 +0800

    Add Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "_________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "_________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "_________________________________________________________\n";
+        System.out.println(logo);
     }
 }
