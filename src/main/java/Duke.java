8336dc72891b8052a2a2a0b25a10f04a3f578c02
Kelvin Neo <2332196+kelvneo@users.noreply.github.com>
2021-08-18 17:35:30 +0800

    Add echo function to the bot The bot will now repeat the same message as the user.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,21 +7,59 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        printSection("Hello from", logo);
+        System.out.println("Hello from\n" + logo);
         printSection("Hello! I'm Duke", "What can I do for you?");
+
+        // Initialize a scanner to read in user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Keep listening for new commands
+        while (true) {
+            // Read the input of the user
+            System.out.print("> ");
+            String cmd = scanner.nextLine();
+            System.out.println();
+
+            // Check if the user wishes to exit the program.
+            if (!parseCommands(cmd)) {
+                break;
+            }
+
+            // Otherwise, ask the user for more commands
+            System.out.println("--");
+            System.out.println("Anything else I can help you with?");
+        }
+
+        // Cleanup and print exit message
         printSection("Bye. Hope to see you again soon!");
     }
 
     /**
-     * Prints the given data provided, and a horizontal line denoting the end of a section.
-     * If no data is provided, it will print a horizontal line.
+     * Prints the bot name and given data provided.
+     * If no data is provided, it will print a horizontal line with the bot name.
      *
      * @param data The string(s) to print to console.
      */
     private static void printSection(String ...data) {
+        System.out.println("--// Duke //-----------------------------------------------");
         for (String s: data) {
             System.out.println(s);
         }
-        System.out.println("____________________________________________________________");
+    }
+
+    /**
+     * Interprets the commands sent by the user.
+     *
+     * @param cmd The command sent by the user.
+     * @return Returns true if the program should continue executing.
+     */
+    private static boolean parseCommands(String cmd) {
+        switch (cmd.toLowerCase()) {
+        case "bye":
+            return false;
+        default:
+            printSection(cmd);
+            return true;
+        }
     }
 }
