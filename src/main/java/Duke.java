c888af91913e1dc89fdd8916287db4d019ab319b
ngnigel99 <e0543444@u.nus.edu>
2021-08-18 16:56:23 +0800

    Add greetings, goodbye functionality Remove default DUKE greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+    public static void printLine(){
+        System.out.println("                 ...                 ");
+    }
+
+    public static void sayHi() {
+        printLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printLine();
+    }
+
+    public static void sayGoodbye() {
+        printLine();
+        System.out.println("Bye. Hope to see you  again soon!");
+        printLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        sayHi();
+        sayGoodbye();
     }
 }
