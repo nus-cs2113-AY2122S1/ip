9efbf7bd96281f08e2634c0736d8f8a479232f17
leowyy99 <leowyy99@hotmail.com>
2021-08-18 21:48:26 +0800

    Add greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String separator = "____________________________________________________________";
+        System.out.println(separator);
+        System.out.println("Hello I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
