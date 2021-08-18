70903e7ed188f33129bf8cbb17bb656d2bd231d6
Kenneth <e0543512@u.nus.edu>
2021-08-18 15:53:46 +0800

    Added level 0 - Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,16 @@
 public class Duke {
+
+    // prints a divider line and new line to command line output
+    public static void dividerLine(){
+        System.out.println("-----------------------------------------");
+    }
+
+    // prints message and new line to command line output
+    public static void printMessage(String message, boolean extra_line){
+        System.out.println(message);
+        if (extra_line) System.out.println();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +18,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        dividerLine();
+        printMessage(" Hello! I'm Duke", false);
+        printMessage(" What can I do for you?", true);
+        dividerLine();
+        printMessage(" Bye. Hope to see you again soon!", true);
+        dividerLine();
     }
 }
