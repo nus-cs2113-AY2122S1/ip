fcf4b7258b21c749ccf040763c50a833e5f3fa9d
Chris <e0544145@u.nus.edu>
2021-08-18 20:35:23 +0800

    Intro Statement

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
+
+    static String intro = "____________________________________________________________\n"
+            + "Hello! I'm Duke\n" + "What can I do for you?\n"
+            + "____________________________________________________________\n"
+            + "Bye. Hope to see you again soon!\n"
+            + "____________________________________________________________";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+        System.out.println(intro);
     }
 }
