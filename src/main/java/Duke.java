2ca856cbf4225f0d332b37cca253e9fef965b43e
swongts <70316271+swongts@users.noreply.github.com>
2021-08-18 20:46:24 +0800

    add level-0 greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = " Hello! I'm Duke\n" +
+                        " What can I do for you?";
+
+        String bye = " Bye. Hope to see you again soon!";
+
+        String divider = "\n____________________________________________________________";
+
+        System.out.println(divider);
+        System.out.println(greeting + divider);
+        System.out.println(bye + divider);
+
     }
 }
