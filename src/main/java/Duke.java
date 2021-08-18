f8eaff1516b82ab5c2710a61483e6625e29668b1
WU_LUOYU_SERENA <siriuswusss@gmail.com>
2021-08-18 18:31:32 +0800

    Add greeting, exit and drawSplitLine

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Karlett");
+        System.out.println("What can I do for you?");
+        drawSplitLine();
+        System.out.println("Bye bye! Hope to see you again soon.");
+        drawSplitLine();
+    }
+    public static void drawSplitLine() {
+        int n = 70;
+        while (n>0) {
+            System.out.print("_");
+            n--;
+        }
+        System.out.println();
     }
 }
