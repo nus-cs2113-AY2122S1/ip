253e6230af2b9844aa117e402db63177fae7e75f
Aayush <aayushmathur7@gmail.com>
2021-08-19 00:14:07 +0800

    Implemented Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontal = " __________________________________________________";
+        String introduction = "   Hello! I'm Duke\n"
+                + "   What can I do for you?";
+        String farewell = "   Bye. Hope to see you again soon!";
+
+        System.out.println(horizontal);
+        System.out.println(introduction);
+        System.out.println(horizontal);
+        System.out.println(farewell);
+        System.out.println(horizontal);
     }
 }
