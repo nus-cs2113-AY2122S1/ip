5d002c5c4112b72e5af3b86855303182cdbef022
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-08-19 23:21:47 +0800

    Add greeting and exit messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    private static void greet() {
+        String greeting = " Hello! I'm Duke\n"
+                + " What can I do for you?";
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(greeting);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private static void exit() {
+        String farewell = " Bye. Hope to see you again soon!";
+        System.out.println(farewell);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
     }
 }
