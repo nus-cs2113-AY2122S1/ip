9cddba2ff43049eca113ee4cbaae9790597e4759
maxwireddevs <williamwahyudiwlliam@gmail.com>
2021-08-20 00:28:36 +0800

    Week 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,12 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "________________________________________";
+        System.out.println(logo);
+        System.out.println(line);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
