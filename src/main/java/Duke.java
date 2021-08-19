c0887186deb9434256d6a6880ebf2c98dbb45c99
adricpjw <adricpjw@gmail.com>
2021-08-20 04:15:21 +0800

    increment level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,48 @@
 import java.util.Scanner;  // Import the Scanner class
+import java.util.ArrayList; // import the ArrayList class
 
 
 public class Duke {
-    public static void main(String[] args) {
-        level1();
+    
+    private static Scanner myScan= new Scanner(System.in);
 
+
+    public static void main(String[] args) {
+        greet();
+        // level1();
+        level2();
     }
 
-    public static void level1() {
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    public static void level2() {
+        ArrayList<String> storedInput = new ArrayList<String>();
+
         while (true) {
-            String input = myObj.nextLine();
+            String input = myScan.nextLine();
+            switch(input) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    myScan.close();
+                    return;
+                case "list":
+                    for (int i = 0; i < storedInput.size(); i++) 
+                        System.out.println(i+1 + ". " + storedInput.get(i));
+                    break;
+                default:
+                    System.out.println("added: " + input);
+                    storedInput.add(input);
+            }
+        }
+    }
+
+
+    public static void level1() {
+
+        while (true) {
+            String input = myScan.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                myObj.close();
+                myScan.close();
                 return;
             }
             System.out.println(input);  // Output user input
@@ -25,17 +51,16 @@ public class Duke {
     }
 
     public static void greet() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("_________________________");
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
+        // System.out.println("_________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("_________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
 
