52cf3c56927e1e6f06f4bbec910567f1c421407c
adricpjw <adricpjw@gmail.com>
2021-08-20 03:55:27 +0800

    increment 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;  // Import the Scanner class
+
+
 public class Duke {
     public static void main(String[] args) {
+        level1();
+
+    }
+
+    public static void level1() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        while (true) {
+            String input = myObj.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                myObj.close();
+                return;
+            }
+            System.out.println(input);  // Output user input
+        }
+        
+    }
+
+    public static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,7 +36,6 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("_________________________");
         System.out.println("Bye. Hope to see you again soon!");
-
     }
 }
 
