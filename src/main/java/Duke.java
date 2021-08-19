8c319402284c49c33a23e060c39e69e3e91b09e7
shoibloya <52075633+shoibloya@users.noreply.github.com>
2021-08-19 20:51:34 +0800

    Increment - Level 0 (update v2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,6 @@
 public class Duke {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";*/
-        //System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke\n" +
-                "What can I do for you?\n" + "Bye. Hope to see you again soon!");
+        //Implementing Increment Level-0: Initial skeleton of Duke
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!");
     }
 }
