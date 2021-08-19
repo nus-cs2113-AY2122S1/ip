e79018cf205644516f51f28694d8414ae7505780
LimJiaJing <jiajinglim06.study@gmail.com>
2021-08-19 19:59:24 +0800

    Complete Level0.

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,11 @@
+public class Greeting {
+    static String closingLine = "__________________________________________________________";
+    public static void openingGreet(){
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println(closingLine);
+    }
+    public static void closingGreet(){
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(closingLine);
+    }
+}
