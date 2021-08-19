f5ae1b10eab3f5c753a5622da28ecd91a7f10d96
Ashraf Jaffar <a.ashrafjfr@gmail.com>
2021-08-19 13:24:07 +0800

    Update Duke.java
    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("_____________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println();
-        System.out.println("_____________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println();
-        System.out.println("_____________________________________");
+        String logo = "____________________________________________________________\n"
+        + "Hello! I'm Duke\n"
+        + "What can I do for you?\n"
+        + "____________________________________________________________\n"
+        + "Bye. Hope to see you again soon!\n"
+        + "____________________________________________________________\n";
+        System.out.println(logo);
     }
 }
