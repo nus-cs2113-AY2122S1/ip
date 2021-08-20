00f862b2cf9761b3cd724a8176aa5bd0b28eeb0b
MaifengNg <maifeng26@hotmail.com>
2021-08-20 22:16:04 +0800

    Add chatbot echos input by user until user says bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String greetings = "Hello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + "\n"
-                + "Bye. Hope to see you again soon!";
+                + "What can I do for you?\n";
         System.out.println(greetings);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                break;
+            } else {
+                System.out.println(userInput + "\n");
+            }
+        }
+
+        String goodbyeMessage = "Bye. Hope to see you again soon!";
+        System.out.println(goodbyeMessage);
+
     }
 }
