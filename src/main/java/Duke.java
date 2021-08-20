236594d8238be01f109723c4803ab64761155be4
rondayvoo <69455258+rondayvoo@users.noreply.github.com>
2021-08-20 17:59:56 +0800

    Revert "Level-0"
    This reverts commit e031c3b2ed2f6bd5405b4bc2c9a6bf4e14ae81b5.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,10 @@
 public class Duke {
-    public static void printGreeting() {
-        printDividingLine();
-        System.out.println("Greetings, human! I'm Duke. \nWhat can I do for you?");
-        printDividingLine();
-    }
-
-    public static void printDividingLine() {
-        System.out.println("____________________________________");
-    }
-
-    public static void printFarewell() {
-        System.out.println("Closing Duke. Have a nice day!");
-        printDividingLine();
-    }
-
     public static void main(String[] args) {
-        printGreeting();
-        printFarewell();
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
     }
 }
