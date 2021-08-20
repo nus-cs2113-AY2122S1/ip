a60b1322c218132627052ff93f5e828930ea375a
MaifengNg <maifeng26@hotmail.com>
2021-08-21 02:33:10 +0800

    Add storing text from user and displaying list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,18 +8,34 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
 
+        int itemNumber = 0;
+        String[] listAllAddedItems = new String[100];
+
         while (true) {
             String userInput = scanner.nextLine();
-
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                if (listAllAddedItems.length == 0) {
+                    System.out.println("No items added yet!");
+                } else {
+                    for (int i = 0; i < listAllAddedItems.length; i++) {
+                        String currentItem = listAllAddedItems[i];
+                        if (currentItem == null) {
+                            break;
+                        } else {
+                            System.out.println(i + 1 + ". " + currentItem);
+                        }
+                    }
+                }
+                System.out.println();
             } else {
-                System.out.println(userInput + "\n");
+                listAllAddedItems[itemNumber] = userInput;
+                itemNumber ++;
+                System.out.println("added: " + userInput + "\n");
             }
         }
-
         String goodbyeMessage = "Bye. Hope to see you again soon!";
         System.out.println(goodbyeMessage);
-
     }
 }
