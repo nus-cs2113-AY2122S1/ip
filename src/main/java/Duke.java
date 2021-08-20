7403031343c2ab0c1aa85be5b5cba4467cdf6ebd
rondayvoo <69455258+rondayvoo@users.noreply.github.com>
2021-08-20 20:36:22 +0800

    Add user input and "bye" to exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void printGreeting() {
         printDividingLine();
@@ -10,12 +12,26 @@ public class Duke {
     }
 
     public static void printFarewell() {
+        printDividingLine();
         System.out.println("Closing Duke. Have a nice day!");
         printDividingLine();
     }
 
+    public static void echoInput() {
+        Scanner userInput = new Scanner(System.in);
+        String userInputString = userInput.nextLine();
+
+        while (!userInputString.equals("bye")) {
+            printDividingLine();
+            System.out.println(userInputString);
+            printDividingLine();
+            userInputString = userInput.nextLine();
+        }
+    }
+
     public static void main(String[] args) {
         printGreeting();
+        echoInput();
         printFarewell();
     }
 }
