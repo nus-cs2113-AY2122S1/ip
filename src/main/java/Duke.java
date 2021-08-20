29e641375eb1fc083c253423b4a81e81bed39129
arvejw <jiewen.tehh@gmail.com>
2021-08-20 15:41:24 +0800

    Change logo and name from Duke to Oberon Add indent to greeting and farewell messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,25 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "\n    ___   ______   ________  _______      ___   ____  _____   .--.\n" +
+                "  .'   `.|_   _ \\ |_   __  ||_   __ \\   .'   `.|_   \\|_   _|.'_\\/_'.\n" +
+                " /  .-.  \\ | |_) |  | |_ \\_|  | |__) | /  .-.  \\ |   \\ | |  '. /\\,.'\n" +
+                " | |   | | |  __'.  |  _| _   |  __ /  | |   | | | |\\ \\| |    \"||\"\n" +
+                " \\  `-'  /_| |__) |_| |__/ | _| |  \\ \\_\\  `-'  /_| |_\\   |_    || /\\`\n" +
+                "  `.___.'|_______/|________||____| |___|`.___.'|_____|\\____|/\\ ||//\\)\n" +
+                "                                                           (/\\\\||/\n" +
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\\||^^^^";
 
-        String horizontalBar = "_______________" +
-                               "_______________" +
-                               "_______________" +
+        String horizontalBar = "__________________" +
+                               "__________________" +
+                               "__________________" +
                                "_______________";
 
         System.out.println(horizontalBar);
         System.out.println(logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("  Hello! I'm Oberon");
+        System.out.println("  What can I do for you?");
         System.out.println(horizontalBar);
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("  Bye. Hope to see you again soon!");
         System.out.println(horizontalBar);
     }
 }
