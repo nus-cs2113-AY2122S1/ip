c3586f0e82dfc60b2e1f762e36de8c072dee05b1
Joshua Lee <joshualee99ky@gmail.com>
2021-08-20 16:54:08 +0800

    Revert "Add Increment: Level-3"
    This reverts commit d881fcd4412379c40e26aa4e1a22fe5bf267b430.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,20 +15,45 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println(line);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("I can do the following: Echo, List");
         System.out.println(line);
 
-        while (true) {
-            String userInput = scannerObj.nextLine();
-            if (userInput.equalsIgnoreCase("bye")) break;
-            echo(userInput);
+        //User choice of what to do
+        String userChoice = scannerObj.nextLine();
+
+        //Check if choice is valid
+        while (!userChoice.equalsIgnoreCase("echo") && !userChoice.equalsIgnoreCase("list")) {
+            System.out.println("Invalid input, please try again. You may choose from: List, Echo");
+            userChoice = scannerObj.nextLine();
         }
 
+        //Execute choice
+        switch (userChoice.toLowerCase()) {
+            case "echo": //Echo what is said to user
+                while (true) {
+                    System.out.println("Copy cat, copy cat");
+                    String userInput = scannerObj.nextLine();
+                    if (userInput.equalsIgnoreCase("bye")) break;
+                    else System.out.println(userInput);
+                }
+                break;
+
+            case "list": //Activate list actions
+                ArrayList<Tasks> tasksAL = new ArrayList<>();
+                while (true) {
+                    System.out.println("To view your list, enter 'list'. To add to your list just type what you would like to add.");
+                    String userInput = scannerObj.nextLine();
+                    if (userInput.equalsIgnoreCase("bye")) break;
+                    else Tasks.list(userInput, tasksAL);
+                }
+                break;
+
+            default:
+                break;
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
     }
-
-    public static void echo(String input) {
-        System.out.println(input);
-    }
-
 }
+
+
