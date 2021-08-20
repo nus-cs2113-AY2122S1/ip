679436b7c18f597138dd5170a2f0b51d359420b1
arvejw <jiewen.tehh@gmail.com>
2021-08-21 01:13:26 +0800

    Add skeleton of storing inputs in a task list
    ArrayList<String> list used to store the task input
    echo method revised and incorporated into editList method which echoes what task input was added to the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,24 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    static String logo = "\n    ___   ______   ________  _______      ___   ____  _____   .--.\n" +
-            "  .'   `.|_   _ \\ |_   __  ||_   __ \\   .'   `.|_   \\|_   _|.'_\\/_'.\n" +
-            " /  .-.  \\ | |_) |  | |_ \\_|  | |__) | /  .-.  \\ |   \\ | |  '. /\\,.'\n" +
-            " | |   | | |  __'.  |  _| _   |  __ /  | |   | | | |\\ \\| |    \"||\"\n" +
-            " \\  `-'  /_| |__) |_| |__/ | _| |  \\ \\_\\  `-'  /_| |_\\   |_    || /\\`\n" +
-            "  `.___.'|_______/|________||____| |___|`.___.'|_____|\\____|/\\ ||//\\)\n" +
-            "                                                           (/\\\\||/\n" +
+     private static String logo = System.lineSeparator() +
+            "    ___   ______   ________  _______      ___   ____  _____   .--." + System.lineSeparator() +
+            "  .'   `.|_   _ \\ |_   __  ||_   __ \\   .'   `.|_   \\|_   _|.'_\\/_'." + System.lineSeparator() +
+            " /  .-.  \\ | |_) |  | |_ \\_|  | |__) | /  .-.  \\ |   \\ | |  '. /\\,.'" + System.lineSeparator() +
+            " | |   | | |  __'.  |  _| _   |  __ /  | |   | | | |\\ \\| |    \"||\"" + System.lineSeparator() +
+            " \\  `-'  /_| |__) |_| |__/ | _| |  \\ \\_\\  `-'  /_| |_\\   |_    || /\\`" + System.lineSeparator() +
+            "  `.___.'|_______/|________||____| |___|`.___.'|_____|\\____|/\\ ||//\\)" + System.lineSeparator() +
+            "                                                           (/\\\\||/" + System.lineSeparator() +
             "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\\||^^^^";
 
-    static String horizontalBar = "__________________" +
-                                  "__________________" +
-                                  "__________________" +
-                                  "_______________";
+    private static String horizontalBar = "__________________" +
+                                          "__________________" +
+                                          "__________________" +
+                                          "_______________";
+
+    private static ArrayList<String> list = new ArrayList<String>();
 
     public static void greetingMessage() {
         System.out.println(horizontalBar);
@@ -30,20 +35,25 @@ public class Duke {
         System.out.println(horizontalBar);
     }
 
-    public static void echo() {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        while (!input.equals("bye")) {
+    public static void echo(String input) {
             System.out.println(horizontalBar);
             System.out.println("  " + input);
             System.out.println(horizontalBar);
-            input = scanner.nextLine();
-        }
+    }
+
+    public static void editList(String input) {
+        list.add(input);
+        echo("added: " + input);
     }
 
     public static void main(String[] args) {
         greetingMessage();
-        echo();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while(!input.equalsIgnoreCase("bye")) {
+            editList(input);
+            input = scanner.nextLine();
+        }
         farewellMessage();
     }
 }
