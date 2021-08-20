7d567bb993e044cee57782042dce24fa28c3a8c3
Joshua Lee <joshualee99ky@gmail.com>
2021-08-20 11:18:13 +0800

    Add Increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scannerObj = new Scanner(System.in);
         String line = "________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,9 +13,19 @@ public class Duke {
         System.out.println(line);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         System.out.println(line);
+
+        while (true) {
+            String userInput = scannerObj.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) break;
+            echo(userInput);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
-
-
     }
+
+    public static void echo(String input) {
+        System.out.println(input);
+    }
+
 }
