c593a1b7f4885fc7dbef64815e67cfd56be9b602
alvintan01 <35805635+alvintan01@users.noreply.github.com>
2021-08-20 20:06:10 +0800

    Add increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,12 +7,38 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("____________________________________________________________");
-        System.out.println(logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        String[] welcome = {"Hello! I'm Duke", "What can I do for you?"};
+        custom_print(welcome);
+
+        String command = "";
+
+        Scanner in = new Scanner(System.in);
+
+        while (!command.equals("bye")) {
+            command = in.nextLine();
+
+            // Handle the bye case
+            if (command.equals("bye")) {
+                custom_print("Bye. Hope to see you again soon!");
+                break;
+            }
+            custom_print(command);
+        }
+
+    }
+
+    public static void custom_print(String statement) {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     " + statement);
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void custom_print(String[] statements) {
+        System.out.println("    ____________________________________________________________");
+        for (String statement : statements) {
+            System.out.println("     " + statement);
+        }
+        System.out.println("    ____________________________________________________________");
     }
 }
