7564cb1acb3872ff80ec64f45ac602137031ce5c
Kair0s3 <stianboon@gmail.com>
2021-08-20 21:57:45 +0800

    Level-0 Increment 2.0
    Second round of Level-0 increment to make the program more casual/fun - referencing marvel films. Along with some formatting of the strings.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,23 +2,24 @@ public class Duke {
     public static void main(String[] args) {
         // Generated ASCII Art - https://patorjk.com/software/taag/#p=display&f=Dancing%20Font&t=Duke
         String logo = "  ____     _   _    _  __  U _____ u \n"
-                    + " |  _\"\\ U |\"|u| |  |\"|/ /  \\| ___\"|/ \n"
-                    + "/| | | | \\| |\\| |  | ' /    |  _|\"\n"
-                    + "U| |_| |\\ | |_| |U/| . \\u   | |___\n"
-                    + " |____/ u<<\\___/   |_|\\_\\   |_____|\n"
-                    + "  |||_  (__) )(  ,-,>> \\\\,-.<<   >>\n"
-                    + " (__)_)     (__)  \\.)   (_/(__) (__)\n";
-        
+                + " |  _\"\\ U |\"|u| |  |\"|/ /  \\| ___\"|/ \n"
+                + "/| | | | \\| |\\| |  | ' /    |  _|\"\n"
+                + "U| |_| |\\ | |_| |U/| . \\u   | |___\n"
+                + " |____/ u<<\\___/   |_|\\_\\   |_____|\n"
+                + "  |||_  (__) )(  ,-,>> \\\\,-.<<   >>\n"
+                + " (__)_)     (__)  \\.)   (_/(__) (__)";
+
         System.out.println(logo);
 
         // Skeletal Barebone - Level-0
         String greeting = "____________________________________________________________\n"
-                            + " Hello! I'm Duke\n"
-                            + " What can I do for you?\n"
-                            + "____________________________________________________________\n"
-                            + " Bye. Hope to see you again soon!\n"
-                            + "____________________________________________________________\n";
-
+                + " Hello! Iron Man has probably the best assistant called Jarvis and Spiderman has hmmm, his tingly spidey senses.\n"
+                + " But don't worry! You have me, Duke! I am your personal Sidekick that does \"something\"!\n"
+                + " What is \"something\" you want me to do?";
+        String farewell = "____________________________________________________________\n"
+                + " Bye, have a nice day! From your friendly neighbourhood assistant, Duke~ (it's time to DUDUDUKEEEE)\n"
+                + "____________________________________________________________";
         System.out.println(greeting);
+        System.out.println(farewell);
     }
 }
