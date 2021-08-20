1dd0829f48391ba28c2f69e06411e1c476594ff5
arvejw <jiewen.tehh@gmail.com>
2021-08-20 20:30:23 +0800

    Add echo method Edit farewell Message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     static String logo = "\n    ___   ______   ________  _______      ___   ____  _____   .--.\n" +
             "  .'   `.|_   _ \\ |_   __  ||_   __ \\   .'   `.|_   \\|_   _|.'_\\/_'.\n" +
@@ -22,12 +24,26 @@ public class Duke {
     }
 
     public static void farewellMessage() {
-        System.out.println("  Bye. Hope to see you again soon!");
         System.out.println(horizontalBar);
+        System.out.println("  Bye. Hope to see you again soon!");
+        System.out.println(logo);
+        System.out.println(horizontalBar);
+    }
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(horizontalBar);
+            System.out.println("  " + input);
+            System.out.println(horizontalBar);
+            input = scanner.nextLine();
+        }
     }
 
     public static void main(String[] args) {
         greetingMessage();
+        echo();
         farewellMessage();
     }
 }
