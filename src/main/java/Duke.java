b4b53f711758a902cae8427385248c54e65bf5ee
xuanlc113 <xuan.lc113@gmail.com>
2021-08-20 17:58:13 +0800

    add level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 public class Duke {
+
+    private static final String DIVIDER = "____________________________________________________________";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(DIVIDER);
+        greet();
+        bye();
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
     }
 }
