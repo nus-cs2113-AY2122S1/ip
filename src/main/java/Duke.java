61455f592ff7369adc41f8c04622b6693529d2ae
alvynben <alvynben@gmail.com>
2021-08-20 23:31:38 +0800

    Implement Level-1
    - Change name of Robot to Triss
    - Change terminology used for greeting or exiting
    - Insert echo for user input
    - Add exit once user types "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "████████ ██████  ██ ███████ ███████ \n" +
+                "   ██    ██   ██ ██ ██      ██      \n" +
+                "   ██    ██████  ██ ███████ ███████ \n" +
+                "   ██    ██   ██ ██      ██      ██ \n" +
+                "   ██    ██   ██ ██ ███████ ███████ \n";
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
+        System.out.println("Hello! I'm Triss :)");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        // Initialise boolean to track if user has said "bye"
+        boolean hasUserSaidBye = false;
+        Scanner in = new Scanner(System.in);
+
+        // While user has not said "bye", check for next line of input
+        while (!hasUserSaidBye) {
+
+            String userInput = in.nextLine();
+            System.out.println("____________________________________________________________");
+
+            // Check if user has said "bye"
+            if (userInput.equals("bye")) {
+                // If user said "bye", update hasUserSaidBye and print closing phrase
+                hasUserSaidBye = true;
+                System.out.println("Thanks for coming. Auf wiedersehen!");
+            } else {
+                // If user has not said "bye", echo user input
+                System.out.println(userInput);
+            }
+
+            System.out.println("____________________________________________________________");
+
+        }
     }
 }
