9309c9f83c39f3a4ae1c14677d9911402a7c8a1a
arvejw <jiewen.tehh@gmail.com>
2021-08-20 20:24:50 +0800

    Free up main from clutter
    Add greetingMessage and farewellMessage() methods
    Make logo and horizontalBar static variables

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,33 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = "\n    ___   ______   ________  _______      ___   ____  _____   .--.\n" +
-                "  .'   `.|_   _ \\ |_   __  ||_   __ \\   .'   `.|_   \\|_   _|.'_\\/_'.\n" +
-                " /  .-.  \\ | |_) |  | |_ \\_|  | |__) | /  .-.  \\ |   \\ | |  '. /\\,.'\n" +
-                " | |   | | |  __'.  |  _| _   |  __ /  | |   | | | |\\ \\| |    \"||\"\n" +
-                " \\  `-'  /_| |__) |_| |__/ | _| |  \\ \\_\\  `-'  /_| |_\\   |_    || /\\`\n" +
-                "  `.___.'|_______/|________||____| |___|`.___.'|_____|\\____|/\\ ||//\\)\n" +
-                "                                                           (/\\\\||/\n" +
-                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\\||^^^^";
+    static String logo = "\n    ___   ______   ________  _______      ___   ____  _____   .--.\n" +
+            "  .'   `.|_   _ \\ |_   __  ||_   __ \\   .'   `.|_   \\|_   _|.'_\\/_'.\n" +
+            " /  .-.  \\ | |_) |  | |_ \\_|  | |__) | /  .-.  \\ |   \\ | |  '. /\\,.'\n" +
+            " | |   | | |  __'.  |  _| _   |  __ /  | |   | | | |\\ \\| |    \"||\"\n" +
+            " \\  `-'  /_| |__) |_| |__/ | _| |  \\ \\_\\  `-'  /_| |_\\   |_    || /\\`\n" +
+            "  `.___.'|_______/|________||____| |___|`.___.'|_____|\\____|/\\ ||//\\)\n" +
+            "                                                           (/\\\\||/\n" +
+            "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\\||^^^^";
 
-        String horizontalBar = "__________________" +
-                               "__________________" +
-                               "__________________" +
-                               "_______________";
+    static String horizontalBar = "__________________" +
+                                  "__________________" +
+                                  "__________________" +
+                                  "_______________";
 
+    public static void greetingMessage() {
         System.out.println(horizontalBar);
         System.out.println(logo);
         System.out.println("  Hello! I'm Oberon");
         System.out.println("  What can I do for you?");
         System.out.println(horizontalBar);
+    }
+
+    public static void farewellMessage() {
         System.out.println("  Bye. Hope to see you again soon!");
         System.out.println(horizontalBar);
+    }
+
+    public static void main(String[] args) {
+        greetingMessage();
+        farewellMessage();
     }
 }
