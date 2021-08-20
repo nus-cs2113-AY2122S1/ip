694f8c71e2b64c3ecde7381dc296975731cd7786
Yitching <seetohyitching@gmail.com>
2021-08-20 13:36:22 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String horizontalLine = "------------------------------------------------------";
         String logo = " ____        _        \n"
@@ -6,10 +11,20 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String userInput = "";
         System.out.println("Hello from\n" + logo);
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Duke!\n" + "What can I do for you?");
         System.out.println(horizontalLine);
+
+        while(!userInput.equalsIgnoreCase("bye")) {
+            Scanner in = new Scanner(System.in);
+            userInput = in.nextLine();
+            System.out.println(horizontalLine);
+            System.out.println(userInput);
+            System.out.println(horizontalLine);
+
+        }
         System.out.println("Bye! Hope to see you again soon!");
         System.out.println(horizontalLine);
     }
