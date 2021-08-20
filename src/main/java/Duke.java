13b1b9ca16ac686af5f8b9324a5580bb647283b5
Roycius <roycius@gmail.com>
2021-08-20 13:54:59 +0800

    Level-2: add listing feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
@@ -10,19 +11,36 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        //Echo
+        //Initialize Variables
         String strInput = "";
+        int listLength = 0;
+        String[] list = new String[100];
+
+        //Main loop
         while(!strInput.equals("bye")) {
             strInput = in.nextLine();
-            if (strInput.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-            }
-            else {
-                System.out.println("____________________________________________________________");
-                System.out.println(strInput);
-                System.out.println("____________________________________________________________");
+            switch (strInput) {
+                case "bye":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("____________________________________________________________");
+                    break;
+
+                case "list":
+                    System.out.println("____________________________________________________________");
+                    for (int i = 0; i < listLength; i++) {
+                        System.out.println((i+1) + ". " + list[i]);
+                    }
+                    System.out.println("____________________________________________________________");
+                    break;
+
+                default:
+                    list[listLength] = strInput;
+                    listLength++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("added: " + strInput);
+                    System.out.println("____________________________________________________________");
+                    break;
             }
         }
     }
