ab5014c0538d13c9d9112c7587e0d459456214e4
Yitching <seetohyitching@gmail.com>
2021-08-20 13:44:18 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Duke {
 
     public static void main(String[] args) {
+        ArrayList<String> taskList = new ArrayList<>();
         String horizontalLine = "------------------------------------------------------";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,6 +13,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String userInput = "";
+
         System.out.println("Hello from\n" + logo);
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Duke!\n" + "What can I do for you?");
@@ -20,10 +22,20 @@ public class Duke {
         while(!userInput.equalsIgnoreCase("bye")) {
             Scanner in = new Scanner(System.in);
             userInput = in.nextLine();
-            System.out.println(horizontalLine);
-            System.out.println(userInput);
-            System.out.println(horizontalLine);
 
+            if (!userInput.contains("list") && !userInput.contains("bye")) {
+                System.out.println(horizontalLine);
+                System.out.println("Added: " + userInput);
+                System.out.println(horizontalLine);
+                taskList.add(userInput);
+            } else if (userInput.contains("list")) {
+                int i = 1;
+                for (String task : taskList) {
+                    System.out.println(i + ". " +task);
+                    i++;
+                }
+                System.out.println(horizontalLine);
+            }
         }
         System.out.println("Bye! Hope to see you again soon!");
         System.out.println(horizontalLine);
