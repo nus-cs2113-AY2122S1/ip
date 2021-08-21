165f96c2d810fd6567cda49af14426a298cc569c
AnShengLee <anshengl@yahoo.com.sg>
2021-08-21 17:42:10 +0800

    Add echo mechanism

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         final String HORIZONTAL_LINE = "___________________" +
@@ -12,6 +14,20 @@ public class Duke {
 
         System.out.println("Hello from\n" + LOGO);
         System.out.println(HORIZONTAL_LINE + "Hello! I'm BATMAN\n" + "What can I do for you?\n" +
-                HORIZONTAL_LINE + "Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE);
+                HORIZONTAL_LINE);
+
+        while (true) {
+            String line;
+            Scanner sc = new Scanner(System.in);
+            line = sc.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println(HORIZONTAL_LINE + line + "\n" +
+                    HORIZONTAL_LINE);
+        }
+
+        System.out.println(HORIZONTAL_LINE + "Bye. Hope to see you again soon!\n" +
+                HORIZONTAL_LINE);
     }
 }
