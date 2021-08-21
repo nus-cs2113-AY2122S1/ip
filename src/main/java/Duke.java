76b977a074f2d16e0c2776bbb85c96bcc45450ef
Cesare4869 <2538849728@qq.com>
2021-08-21 17:34:39 +0800

    add Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("-------------------------------------------------");
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
-        System.out.println("-------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("-------------------------------------------------");
+        String divider = "    ————————————————————————————————————————————————————————————";
+        System.out.println(divider);
+        System.out.println("    Hello! I'm Duke\n    What can I do for you?");
+        System.out.println(divider);
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!line.equals("bye")) {
+            System.out.println(divider);
+            System.out.print("    ");
+            System.out.println(line);
+            System.out.println(divider);
+            line = in.nextLine();
+        }
+        System.out.println(divider);
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
