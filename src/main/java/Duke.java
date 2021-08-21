7baca7b4aef23938434fc13cc6145c6004a5d02f
Kair0s3 <stianboon@gmail.com>
2021-08-21 16:12:33 +0800

    NOTE - NOT AN INCREMENT UPDATE
    Just a commit to move some functionalities into seperate method calls eg. the printing of greeting, farewell and logo display.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+
+    // Print logo
+    public static void printLogo(){
         // Generated ASCII Art - https://patorjk.com/software/taag/#p=display&f=Dancing%20Font&t=Duke
         String logo = "  ____     _   _    _  __  U _____ u \n"
                 + " |  _\"\\ U |\"|u| |  |\"|/ /  \\| ___\"|/ \n"
@@ -10,20 +12,29 @@ public class Duke {
                 + " |____/ u<<\\___/   |_|\\_\\   |_____|\n"
                 + "  |||_  (__) )(  ,-,>> \\\\,-.<<   >>\n"
                 + " (__)_)     (__)  \\.)   (_/(__) (__)";
-
         System.out.println(logo);
+    }
 
-        // Level-0. Skeletal Barebone
+    // Print Level-0. Skeletal Barebone, Greeting and Farewell
+    public static void printGreeting(){
         String greeting = "____________________________________________________________\n"
-                + " Hello! Iron Man has probably the best assistant called Jarvis and Spiderman has hmmm, his tingly spidey senses.\n"
-                + " But don't worry! You have me, Duke! I am your personal Sidekick that does \"something\"!\n"
+                + " Hello! You probably know that Iron Man has the best AI-assistant called Jarvis and Spiderman has hmmm, maybe his tingly spidey senses?\n"
+                + " But don't worry! You have me, Duke! I am your personal SIDEKICK that does \"something\"!\n"
                 + " What is \"something\" you want me to do?\n"
                 + "____________________________________________________________";
+        System.out.println(greeting);
+    }
+
+    public static void printFarewell(){
         String farewell = "____________________________________________________________\n"
                 + " Bye, have a nice day! From your friendly neighbourhood assistant, Duke~ (it's time to DUDUDUKEEEE)\n"
                 + "____________________________________________________________";
-        System.out.println(greeting);
+        System.out.println(farewell);
+    }
 
+    public static void main(String[] args) {
+        printLogo();
+        printGreeting();
         // Level-1. Greet, Echo, Exit
         Scanner sc = new Scanner(System.in); // initialize the user input "scanner"
         String userCommand = "";
@@ -41,7 +52,6 @@ public class Duke {
                     + "____________________________________________________________";
             System.out.println(returnOutput);
         }
-
-        System.out.println(farewell);
+        printFarewell();
     }
 }
