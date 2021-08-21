cc9deec3c457a8d1542a9eb1e9ec925d2c0d13c1
andrewtkh1 <andrewtkh1@gmail.com>
2021-08-21 19:53:36 +0800

    Add echo & end function.
    The bot will continiously echo any input till "end"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String intro = "Hello! I'm Duke \nWhat can I do for you?";
-        String farewell = "Bye. Hope to see you again soon!";
+        String intro = "Top of the morning my good sir, what can I do for you on this fine day?";
+        String farewell = "I bid you farewell my good man. Good Bye.";
+        Scanner sc = new Scanner(System.in);
         System.out.println(intro);
-        System.out.println(farewell);
+        String input;
+        do {
+            input = sc.nextLine();
+            input = input.stripTrailing();
+            input = input.stripLeading();
+            if (input.equals("end")) {
+                System.out.println(farewell);
+            } else {
+                System.out.println((input));
+            }
+        } while (!(input.equals("end")));
     }
 }
