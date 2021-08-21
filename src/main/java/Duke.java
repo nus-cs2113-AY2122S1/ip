1bcb0296307880de24fe5ec5b1323f6fea1ed730
Yue Junfeng <yuejunfeng0909@gmail.com>
2021-08-21 16:07:06 +0800

    Greet, Echo, Exit
    Implement a skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types "bye".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello from\n" + logo +"\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello from\n" + logo + "\n     What can I do for you?");
+        System.out.println("    ____________________________________________________________\n");
+        while (true) {
+            String command = in.nextLine();
+            if (command.equals("bye")) break;
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + command);
+            System.out.println("    ____________________________________________________________");
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
 
     }
 }
