9feffc7fc8686106dc57e89ebc26592669161842
alvynben <alvynben@gmail.com>
2021-08-21 12:11:58 +0800

    Handle exception if user types "done [task]" and [task] does not exist

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -35,18 +35,23 @@ public class Duke {
             } else if (userInput.equals("list")) {
                 // If user said "list", print a list of all saved tasks
                 for (int i = 0; i < noOfTasks; i++) {
-                    System.out.println(Integer.toString(i+1) + "." + tasks[i].getDoneStatusAsSymbol()
+                    System.out.println(Integer.toString(i + 1) + "." + tasks[i].getDoneStatusAsSymbol()
                             + " " + tasks[i].getName());
                 }
             } else if (userInput.contains("done")) {
                 // Get number of task after the term "done"
                 int indexOfCompletedTask = Integer.parseInt(userInput.split(" ")[1]) - 1;
-                // Set chosen task as done
-                Task chosenTask = tasks[indexOfCompletedTask];
-                chosenTask.setDone(true);
-                // Give user feedback regarding change in completion status
-                System.out.println("Wunderbar! This task has been marked as done:");
-                System.out.println("    " + chosenTask.getDoneStatusAsSymbol() + " " + chosenTask.getName());
+                if (indexOfCompletedTask > noOfTasks - 1 || indexOfCompletedTask < 1) {
+                    // If task does not exist, do not delete any task
+                    System.out.println("Apologies! That task does not exist.");
+                } else {
+                    // Set chosen task as done
+                    Task chosenTask = tasks[indexOfCompletedTask];
+                    chosenTask.setDone(true);
+                    // Give user feedback regarding change in completion status
+                    System.out.println("Wunderbar! This task has been marked as done:");
+                    System.out.println("    " + chosenTask.getDoneStatusAsSymbol() + " " + chosenTask.getName());
+                }
             } else {
                 // If user has not said "bye", store user input as task
                 tasks[noOfTasks] = new Task(userInput);
