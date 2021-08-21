64a5a9e6b26a6b548d0e1cc8d668329c630256b6
siyuancheng178 <e0556728@u.nus.edu>
2021-08-21 15:27:15 +0800

    Level 3 code include Done class

---
+++ src/main/java/Done.java
@@ -0,0 +1,13 @@
+public class Done extends UserInput{
+
+    public Done(String userInput) {
+        super(userInput);
+    }
+
+    @Override
+    public void execute() {
+        String[] inputSplit = userInput.split(" ");
+        int taskIndex = Integer.parseInt(inputSplit[1]) - 1;
+        System.out.println(UserInput.tasks[taskIndex].setFinished());
+    }
+}
