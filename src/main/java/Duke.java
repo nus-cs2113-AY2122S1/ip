a45627b77003c8364e850ac540f64867d5036414
siyuancheng178 <e0556728@u.nus.edu>
2021-08-21 13:43:00 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String divisionLine = "____________________________________________________________\n";
+    private static final String greetings = divisionLine + " Hello! I'm Duke\n" +
+            " What can I do for you?\n" + divisionLine;
+    private static final String bye = divisionLine + " Bye. Hope to see you again soon!\n" + divisionLine;
+
     public static void main(String[] args) {
-        String divisionLine = "____________________________________________________________\n";
-        String greetings = " Hello! I'm Duke\n" +
-                " What can I do for you?\n";
-        String bye = " Bye. Hope to see you again soon!\n";
-        System.out.println(divisionLine + greetings + divisionLine + bye + divisionLine);
+        System.out.println(greetings);
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(divisionLine + " " + userInput + "\n" + divisionLine);
+            userInput = sc.nextLine();
+        }
+        System.out.println(bye);
     }
 }
