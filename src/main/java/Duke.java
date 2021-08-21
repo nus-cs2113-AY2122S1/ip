569da74aa338539221f97b92e3a329a8b0934cc5
DESKTOP-TO5H2A5\Ishaa <ishaan.vyas@hotmail.com>
2021-08-21 19:41:09 +0800

    Level-1 Increment - Added echo function which ends once user types "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void printMessage(String message) {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine + System.lineSeparator() + message + System.lineSeparator()
+                            + horizontalLine);
+    }
+
     public static void main(String[] args) {
+        //Starting message
+        String horizontalLine = "____________________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontalLine + System.lineSeparator()
+                            + "Hello! I'm Duke" + System.lineSeparator()
+                            + "What can I do for you?" + System.lineSeparator()
+                            + horizontalLine);
+        //initialise Scanner
+        Scanner in = new Scanner(System.in);
+        boolean conversationIsOver = false;
+        while (!conversationIsOver) {
+            String command = in.nextLine();
+            if (command.equalsIgnoreCase("bye")) conversationIsOver = true;
+            printMessage(command);
+        }
+
+
+        //Ending message
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontalLine);
+        //
     }
 }
