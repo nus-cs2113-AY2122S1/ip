76274a28eb8ee0749b687ce42d8c0b64ba5d942f
Cesare4869 <2538849728@qq.com>
2021-08-21 21:23:40 +0800

    add lists

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,14 +7,27 @@ public class Duke {
         System.out.println("    Hello! I'm Duke\n    What can I do for you?");
         System.out.println(divider);
         String line;
+        String[] lists = new String[100];
+        int number = 0;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
-        while(!line.equals("bye")) {
-            System.out.println(divider);
-            System.out.print("    ");
-            System.out.println(line);
-            System.out.println(divider);
-            line = in.nextLine();
+        while (!line.equals("bye")) {
+            if (line.equals("list")) {
+                System.out.println(divider);
+                for(int i = 0; i < number; i++) {
+                    System.out.print("    " + (i+1) + ". ");
+                    System.out.println(lists[i]);
+                }
+                System.out.println(divider);
+                line = in.nextLine();
+            } else {
+                lists[number++] = line;
+                System.out.println(divider);
+                System.out.print("    ");
+                System.out.println("added: " + line);
+                System.out.println(divider);
+                line = in.nextLine();
+            }
         }
         System.out.println(divider);
         System.out.println("    Bye. Hope to see you again soon!");
