52f9a786ed33ad46746252bab70db7ad0a1d7352
Cesare4869 <2538849728@qq.com>
2021-08-21 23:32:10 +0800

    add Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,23 +9,40 @@ public class Duke {
         System.out.println(divider);
         String line;
         String[] lists = new String[100];
+        boolean[] isDones = new boolean[100];
+        for (boolean done : isDones) {
+            done = false;
+        }
         int number = 0;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while (!line.equals("bye")) {
             if (line.equals("list")) {
                 System.out.println(divider);
+                System.out.println("    " + "Here are the tasks in your list:");
                 for(int i = 0; i < number; i++) {
-                    System.out.print("    " + (i+1) + ". ");
+                    System.out.print("    " + (i + 1) + ". " + "[");
+                    if (isDones[i]) {
+                        System.out.print("X] ");
+                    } else {
+                        System.out.print(" ] ");
+                    }
                     System.out.println(lists[i]);
                 }
+                System.out.println(divider);
+                line = in.nextLine();
+            }  else if (line.contains("done")) {
+                int taskDone = Integer.parseInt(line.substring(5));
+                System.out.println(divider);
+                System.out.println("    Nice! I've marked this task as done:");
+                isDones[taskDone - 1] = true;
+                System.out.println("      [X] " + lists[taskDone - 1]);
                 System.out.println(divider);
                 line = in.nextLine();
             } else {
                 lists[number++] = line;
                 System.out.println(divider);
-                System.out.print("    ");
-                System.out.println("added: " + line);
+                System.out.println("    added: " + line);
                 System.out.println(divider);
                 line = in.nextLine();
             }
