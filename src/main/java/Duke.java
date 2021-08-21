8dd95f1ec191d116ec7a5f798b9e0433d641fa95
Sridharan Arvind Srinivasan <arv1920@gmail.com>
2021-08-21 21:28:30 +0800

    Implement add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String line;
+        int count = 0;
+        String[] tasks = new String[100];
         Scanner in = new Scanner(System.in);
 
         System.out.println("____________________________________\n");
@@ -13,9 +15,21 @@ public class Duke {
         //Echo
         line = in.nextLine();
         while (line.compareTo("bye") != 0) {
-            System.out.println("____________________________________\n");
-            System.out.println(line);
-            System.out.println("____________________________________\n");
+
+            switch (line) {
+                case "list":
+                    System.out.println("____________________________________\n");
+                    for (int i = 0; i < count; i++) {
+                        System.out.println(Integer.toString(i + 1) + "." + tasks[i]);
+                    }
+                    System.out.println("____________________________________\n");
+                    break;
+                default:
+                    System.out.println("____________________________________\n");
+                    System.out.println("Added: " + line);
+                    System.out.println("____________________________________\n");
+                    tasks[count++] = line;
+            }
             line = in.nextLine();
         }
 
