ad0742f1df65e9e798da88a0600a7c1cf858fd58
LAPTOP-8836A17E\eugen <eugenechong20011@yahoo.com>
2021-08-21 15:54:50 +0800

    Organise different print statements into multiple functions called by main()

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,15 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Duke {
-    public static void printTaskList(Task[] tasks) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Here are the tasks in your list:");
-        for (int i = 0; i < tasks.length; i++) {
-            System.out.println((i + 1) + ". " + "[" + tasks[i].getStatusIcon() +"] " + tasks[i].description);
-        }
-        System.out.println("____________________________________________________________");
-    }
-    public static void main(String[] args) {
+    public static void printLogoAndGreet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -20,43 +12,74 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println(" Hey there! I'm Duke.");
         System.out.println(" How may I help you?");
+    }
+
+    public static void printTaskList(Task[] tasks) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Here are the tasks in your list:");
+        for (int i = 0; i < tasks.length; i++) {
+            System.out.println((i + 1) + ". " + "[" + tasks[i].getStatusIcon() +"] " + tasks[i].description);
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printGoodbye() {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Goodbye! Hope to see you again soon.");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printTaskDone(Task[] tasks, int taskCount, String doneTask) {
+        String[] doneSentence = doneTask.split(" ");
+        if (doneSentence.length != 2) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Your command is of the wrong format! To mark a task as done, enter \"done {ID of done task}\"");
+            System.out.println("____________________________________________________________");
+            return;
+        }
+
+        int taskToMarkDone = Integer.parseInt(doneSentence[1]);
+        if (taskToMarkDone > taskCount || taskToMarkDone <= 0) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Sorry, the task is not in the list! Try again.");
+            System.out.println("____________________________________________________________");
+        } else {
+            tasks[taskToMarkDone - 1].markDone();
+            System.out.println("____________________________________________________________");
+            System.out.println(" Great! I have marked the following task as done: ");
+            System.out.println("   [" + tasks[taskToMarkDone - 1].getStatusIcon() + "] " + tasks[taskToMarkDone - 1].description);
+            System.out.println("____________________________________________________________");
+        }
+    }
+
+    public static void printTaskAdded(Task[] tasks, int taskCount, String addedTask) {
+        tasks[taskCount] = new Task(addedTask);
+        System.out.println("____________________________________________________________");
+        System.out.println(" I have added a task: " + addedTask);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        printLogoAndGreet();
 
         String line;
         Scanner in = new Scanner(System.in);
-        String exitString = "bye";
 
         int taskCount = 0;
         Task[] tasks = new Task[100];
         line = in.nextLine();
 
-        while (!line.equalsIgnoreCase(exitString)) {
+        while (!line.equalsIgnoreCase("bye")) {
             if (line.equalsIgnoreCase("list")) {
                 printTaskList(Arrays.copyOf(tasks, taskCount));
             } else if (line.toLowerCase().startsWith("done")) {
-                int taskToMarkDone = Integer.parseInt(line.split(" ")[1]);
-                if (taskToMarkDone > taskCount || taskToMarkDone <= 0) {
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" Sorry, the task is not in the list! Try again.");
-                    System.out.println("____________________________________________________________");
-                } else {
-                    tasks[taskToMarkDone - 1].markDone();
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" Great! I have marked the following task as done: ");
-                    System.out.println("   [" + tasks[taskToMarkDone - 1].getStatusIcon() + "] " + tasks[taskToMarkDone - 1].description);
-                    System.out.println("____________________________________________________________");
-                }
+                printTaskDone(tasks, taskCount, line);
             } else {
-                tasks[taskCount] = new Task(line);
+                printTaskAdded(tasks, taskCount, line);
                 taskCount++;
-                System.out.println("____________________________________________________________");
-                System.out.println(" I have added a task: " + line);
-                System.out.println("____________________________________________________________");
             }
             line = in.nextLine();
         }
-
-        System.out.println("____________________________________________________________");
-        System.out.println(" Goodbye! Hope to see you again soon.");
-        System.out.println("____________________________________________________________");
+        printGoodbye();
     }
 }
