14edbb6af1d1e6ec24aeedfc33299c56de8b38c6
Kair0s3 <stianboon@gmail.com>
2021-08-21 17:11:59 +0800

    Increment Level-2. Add, List
    Added functionality to store text entered by user and listing them when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
 
@@ -18,7 +19,8 @@ public class Duke {
     // Print Level-0. Skeletal Barebone, Greeting and Farewell
     public static void printGreeting(){
         String greeting = "____________________________________________________________\n"
-                + " Hello! You probably know that Iron Man has the best AI-assistant called Jarvis and Spiderman has hmmm, maybe his tingly spidey senses?\n"
+                + " Hello! You probably know that Iron Man has the best AI-assistant called Jarvis\n"
+                + " and Spiderman has hmmm, maybe his tingly spidey senses?\n"
                 + " But don't worry! You have me, Duke! I am your personal SIDEKICK that does \"something\"!\n"
                 + " What is \"something\" you want me to do?\n"
                 + "____________________________________________________________";
@@ -27,11 +29,22 @@ public class Duke {
 
     public static void printFarewell(){
         String farewell = "____________________________________________________________\n"
-                + " Bye, have a nice day! From your friendly neighbourhood assistant, Duke~ (it's time to DUDUDUKEEEE)\n"
+                + " Bye, have a nice day! From your friendly neighbourhood assistant, Duke~\n"
+                + " (NOICE, I can finally binge watch Rick and Morty~)\n"
                 + "____________________________________________________________";
         System.out.println(farewell);
     }
 
+    // Level-2 List method to print Task in the current Tasklist
+    public static void printTasklist(String [] tasklist, int taskCounter){
+        String [] validTasklist = Arrays.copyOf(tasklist, taskCounter);
+        System.out.println(" EEEEEOOOOOO~ ALL RIGHT~ Oops was jamming away in my virtual garage, here's the stuff you forgot to do...");
+        for (int i = 0; i < taskCounter; i++){
+            System.out.println(" " + (i+1) + ". " + validTasklist[i]);
+        }
+        System.out.println("____________________________________________________________");
+    }
+    
     public static void main(String[] args) {
         printLogo();
         printGreeting();
@@ -39,18 +52,26 @@ public class Duke {
         Scanner sc = new Scanner(System.in); // initialize the user input "scanner"
         String userCommand = "";
         String returnOutput = "";
+        String [] tasklist = new String[100];
+        int taskCounter = 0;
         while (true){
-            System.out.print(" What is your command : ");
+            System.out.print(" What's your plans/command for today (No... I am not hitting on you) : ");
             userCommand = sc.nextLine();
             if (userCommand.equalsIgnoreCase("Bye")){
                 break;
             }
-            returnOutput = "____________________________________________________________\n"
-                    + " Here you go...\n "
-                    + userCommand
-                    + "\n"
-                    + "____________________________________________________________";
-            System.out.println(returnOutput);
+            if (userCommand.equalsIgnoreCase("List") || userCommand.equals("")){
+                printTasklist(tasklist, taskCounter);
+            }
+            else {
+                tasklist[taskCounter++] = userCommand;
+                returnOutput = "____________________________________________________________\n"
+                        + " Here you go...\n Added to stuff you would definitely forget to do (*facepalm*): "
+                        + userCommand
+                        + "\n"
+                        + "____________________________________________________________";
+                System.out.println(returnOutput);
+            }
         }
         printFarewell();
     }
