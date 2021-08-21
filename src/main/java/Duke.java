499c6bf1e05db174e27b04db90994370b6f50f43
Sridharan Arvind Srinivasan <arv1920@gmail.com>
2021-08-21 20:43:37 +0800

    Implement echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo);
+        String line;
+        Scanner in = new Scanner(System.in);
 
         System.out.println("____________________________________\n");
         System.out.println("Hello! I'm Duke\n");
-        System.out.println("What can I do for you?"); 
-        System.out.println("____________________________________\n"); 
-        System.out.println("Bye. Hope to see you again soon!"); 
-        System.out.println("____________________________________\n"); 
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________\n");
+
+        //Echo
+        line = in.nextLine();
+        while (line.compareTo("bye") != 0) {
+            System.out.println("____________________________________\n");
+            System.out.println(line);
+            System.out.println("____________________________________\n");
+            line = in.nextLine();
+        }
+
+        System.out.println("____________________________________\n");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________\n");
     }
 }
