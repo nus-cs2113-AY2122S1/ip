a29b0dbb71a5e7dc8af4e010acc5a59f6f002697
shixi <fanshixi123@gmail.com>
2021-08-21 15:25:09 +0800

    Add greet user feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,15 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        /** greet **/
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("_____________________________");
+        
+        /** exit **/
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_____________________________");
     }
 }
