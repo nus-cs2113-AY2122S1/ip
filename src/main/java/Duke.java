82ee09f2316f718992d879e50e7bfa9be5341d63
AnShengLee <anshengl@yahoo.com.sg>
2021-08-21 18:22:53 +0800

    Add list mechanism that tracks user's inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final String HORIZONTAL_LINE = "___________________" +
+            "_________________________________________\n";
+
+    private static final String LOGO = "\n" +
+            "██████╗░░█████╗░████████╗███╗░░░███╗░█████╗░███╗░░██╗\n" +
+            "██╔══██╗██╔══██╗╚══██╔══╝████╗░████║██╔══██╗████╗░██║\n" +
+            "██████╦╝███████║░░░██║░░░██╔████╔██║███████║██╔██╗██║\n" +
+            "██╔══██╗██╔══██║░░░██║░░░██║╚██╔╝██║██╔══██║██║╚████║\n" +
+            "██████╦╝██║░░██║░░░██║░░░██║░╚═╝░██║██║░░██║██║░╚███║\n" +
+            "╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝";
+
+    private static final String[] userInputs = new String[100];
+    private static int numOfUserInput = 0;
+
+    private static void printList() {
+        System.out.print(HORIZONTAL_LINE);
+        for (int i = 0; i < numOfUserInput; i++) {
+            System.out.println(i+1 + ". " + userInputs[i]);
+        }
+        System.out.print(HORIZONTAL_LINE);
+    }
+
     public static void main(String[] args) {
-        final String HORIZONTAL_LINE = "___________________" +
-                "_________________________________________\n";
-        final String LOGO = "\n" +
-                "██████╗░░█████╗░████████╗███╗░░░███╗░█████╗░███╗░░██╗\n" +
-                "██╔══██╗██╔══██╗╚══██╔══╝████╗░████║██╔══██╗████╗░██║\n" +
-                "██████╦╝███████║░░░██║░░░██╔████╔██║███████║██╔██╗██║\n" +
-                "██╔══██╗██╔══██║░░░██║░░░██║╚██╔╝██║██╔══██║██║╚████║\n" +
-                "██████╦╝██║░░██║░░░██║░░░██║░╚═╝░██║██║░░██║██║░╚███║\n" +
-                "╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝";
 
         System.out.println("Hello from\n" + LOGO);
-        System.out.println(HORIZONTAL_LINE + "Hello! I'm BATMAN\n" + "What can I do for you?\n" +
+        System.out.print(HORIZONTAL_LINE + "Hello! I'm BATMAN\n" + "What can I do for you?\n" +
                 HORIZONTAL_LINE);
 
         while (true) {
@@ -23,11 +36,17 @@ public class Duke {
             if (line.equals("bye")) {
                 break;
             }
-            System.out.println(HORIZONTAL_LINE + line + "\n" +
+            else if (line.equals("list")) {
+                printList();
+                continue;
+            }
+            userInputs[numOfUserInput] = line;
+            numOfUserInput++;
+            System.out.print(HORIZONTAL_LINE + "added: " + line + "\n" +
                     HORIZONTAL_LINE);
         }
 
-        System.out.println(HORIZONTAL_LINE + "Bye. Hope to see you again soon!\n" +
+        System.out.print(HORIZONTAL_LINE + "Bye. Hope to see you again soon!\n" +
                 HORIZONTAL_LINE);
     }
 }
