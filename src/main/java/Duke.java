5a80b60fc6aede13a77c7205f1eed2052d1e69a2
KZQ1999 <e0543569@u.nus.edu>
2021-08-21 19:23:13 +0800

    Add increment level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class Duke {
 
     public static void printLine() {
@@ -9,36 +11,49 @@ public class Duke {
     }
 
     public static void echo(String line){
-        System.out.println("OWL: " + line);
+        System.out.println("Owl: I've remembered that!\nOwl: You said this: " + line);
+    }
+
+    public static void list(String[] datas){
+        int dataIndex = 0;
+        int index = 1;
+        System.out.println("This is all the things I've remembered for you:");
+        for(String data:datas) {
+            System.out.println(index + ". " + datas[dataIndex]);
+            index++;
+            dataIndex++;
+        }
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
 
-        String logo2 = " ______   _       _   _\n"
+        String logo = " ______   _       _   _\n"
                 + "|  __  | | | ___ | | | | \n"
                 + "| |  | | | |/   \\| | | | \n"
                 + "| |__| | |   / \\   | | |____\n"
                 + "|______| |__/   \\__| |______|\n";
-
-
-
-
-        String line;
-        System.out.println(logo2);
+        System.out.println(logo);
         printLine();
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("SQUAWK!!!");
         System.out.println("How can I help you?");
         printLine();
+
+        String line;
+        String[] datas = new String[100];
+        int dataIndex = 0;
         line = in.nextLine();
         while(!line.equals("bye")){//while the command is not bye it keeps asking for more
-            if(!line.isEmpty()) {
+            if(line.equals("list")){
+                if(dataIndex > 0) {
+                    list(Arrays.copyOf(datas, dataIndex));
+                }
+            }
+            if(!line.isEmpty() && !line.equals("list")) {
+                datas[dataIndex] = line;
+                dataIndex++;
                 printLine();
                 echo(line);
                 printLine();
@@ -46,7 +61,7 @@ public class Duke {
             line = in.nextLine();
         }
         printLine();
-        System.out.println("SQUAWK! See you next time!");
+        System.out.println("SQUAWK! See you next time! :)");
         printLine();
     }
 }
