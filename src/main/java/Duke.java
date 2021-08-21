b36b2b9bd75e8e36a9d3b9eee6bc033fafafa84f
siyuancheng178 <e0556728@u.nus.edu>
2021-08-21 13:32:20 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String divisionLine = "____________________________________________________________\n";
+        String greetings = " Hello! I'm Duke\n" +
+                " What can I do for you?\n";
+        String bye = " Bye. Hope to see you again soon!\n";
+        System.out.println(divisionLine + greetings + divisionLine + bye + divisionLine);
     }
 }
