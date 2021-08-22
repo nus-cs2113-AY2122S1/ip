427fb8666d16d2fd0adc813f4fadb22236ffd011
xuanlc113 <xuan.lc113@gmail.com>
2021-08-22 11:17:18 +0800

    add level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
 
-    private static final String DIVIDER = "____________________________________________________________";
+    private static final String DIVIDER = "    ____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println(DIVIDER);
+        Scanner scan = new Scanner(System.in);
         greet();
+        while (true) {
+            System.out.println("");
+            String command = scan.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                echo(command);
+            }
+        }
         bye();
+        scan.close();
     }
 
     public static void greet() {
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
         System.out.println(DIVIDER);
     }
 
     public static void bye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
+    }
+
+    public static void echo(String command) {
+        System.out.println(DIVIDER);
+        System.out.println("     " + command);
         System.out.println(DIVIDER);
     }
 }
