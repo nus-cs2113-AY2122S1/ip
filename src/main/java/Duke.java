54599557c0aa04df15686ddf6073d501cd47042a
kishorkumar11 <h.kishorkumar7@gmail.com>
2021-08-22 17:46:51 +0800

    Add increment coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,7 @@ public class Duke {
         String input;
         Task[] lists = new Task[100];
         Scanner in = new Scanner(System.in);
+        String line = "________________________________________________________";
 
         //logo
         String logo = " ____        _        \n"
@@ -15,43 +16,54 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Welcome!\n" + logo);
 
-        System.out.println("________________________________________________________");
+        System.out.println(line);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
-        System.out.println("________________________________________________________");
+        System.out.println(line);
 
-        //ask user for input
+        //Ask user for input
         input = in.nextLine();
+
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                System.out.println("________________________________________________________");
+                System.out.println(line);
                 System.out.println("Here are the tasks in your list:");
+
+                //Lists down all the tasks added along with its status
                 for (int i = 0; i < count; i++) {
                     String taskStatus = lists[i].getStatusIcon();
                     System.out.println((i + 1) + ".[" + taskStatus + "] " + lists[i].description);
                 }
-                System.out.println("________________________________________________________");
+
+                System.out.println(line);
             } else if (input.contains("done")) {
+
+                //Takes the index number needed to change the status
                 String[] temp = input.split(" ");
                 int index = Integer.parseInt(temp[1]);
                 index = index - 1;
                 lists[index].markAsDone();
                 String taskStatus = lists[index].getStatusIcon();
-                System.out.println("________________________________________________________");
+
+                System.out.println(line);
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("[" + taskStatus + "] " + lists[index].getDescription());
-                System.out.println("________________________________________________________");
+                System.out.println(line);
             } else {
-                System.out.println("________________________________________________________");
+                System.out.println(line);
                 System.out.println("Added: " + input);
+
+                //Object created for each task
                 Task t = new Task(input);
                 lists[count] = t;
                 count++;
-                System.out.println("________________________________________________________");
+
+                System.out.println(line);
             }
             input = in.nextLine();
         }
-        System.out.println("________________________________________________________");
+
+        System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________________________________");
+        System.out.println(line);
     }
 }
