1dd98138bcd105bbf7e8e60085fe240b9224f5d1
Joshua Harsha Dass <joshua.h.dass@gmail.com>
2021-08-22 20:31:57 +0800

    add list feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
 
-         */
+
+    public static void main(String[] args) {
 
         String line;
         Scanner in = new Scanner(System.in);
@@ -19,10 +12,26 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         line = in.nextLine();
+        String[] list = new String[100];
+        int size = 0;
+
 
         while ( !line.equals("bye") ) {
-            System.out.println(line);
+
+            if( line.equals("list")) {
+                for( int i = 0 ; i < size ; i ++ ) {
+                    int no = i + 1;
+                    System.out.println( no + ". " + list[i] );
+                }
+            }
+            else {
+                System.out.println("     added: " + line);
+                list[ size ] = line;
+                size ++;
+            }
+
             line = in.nextLine();
+
         }
 
         System.out.println("Bye. Hope to see you again soon!");
