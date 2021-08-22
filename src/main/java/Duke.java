f0796817e87bbfba377ad00ab5ca160afca1e2c6
jushg <e0550425@u.nus.edu>
2021-08-22 18:47:39 +0800

    Add Increment: Level 1-Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,50 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void sayHello() {
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+    private static final int DEFAULT_LINE_LENGTH = 32;
+    private static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public static void greet() {
+        drawHorizontalLine();
+        System.out.println("Hello! I'm Duke, your personal assistant");
+        System.out.println("How can I help you?");
+        drawHorizontalLine();
     }
 
     public static void sayGoodbye() {
+        drawHorizontalLine();
         System.out.println("Bye. Hope to see you again soon!");
+        drawHorizontalLine();
     }
 
-    public static void drawHorizontalLine(int length) {
-        for (int i = 0; i < length; i++) {
+    public static void echo(String input) {
+        drawHorizontalLine();
+        System.out.println(input);
+        drawHorizontalLine();
+    }
+
+    public static void drawHorizontalLine() {
+        for (int i = 0; i < DEFAULT_LINE_LENGTH; i++) {
             System.out.print("\u2500");
         }
         System.out.println("");
     }
-    
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        drawHorizontalLine(32);
-        sayHello();
-        drawHorizontalLine(32);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello from\n" + LOGO);
+        greet();
+        System.out.print(">> ");
+        String input = in.nextLine();
+        while (!input.equals("bye")) {
+            echo(input);
+            System.out.print(">> ");
+            input = in.nextLine();
+        }
         sayGoodbye();
-        drawHorizontalLine(32);
     }
 }
