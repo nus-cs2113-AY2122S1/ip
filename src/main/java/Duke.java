e9643b0fb35c9919d65187d677aa7528ab296617
NonRNP <69129667+NonRNP@users.noreply.github.com>
2021-08-22 09:44:48 +0800

    Level 1 - Greet, Echo, Exit
    Keep repeating the user's input until the command "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void addLine() {
         System.out.println("----------------------");
     }
 
     public static void dukeGreet() {
+        addLine();
         System.out.println("Hello!, I'm Duke");
         System.out.println("How can I help you?");
+        addLine();
+    }
+
+    public static void dukeEcho(String userInput) {
+        addLine();
+        System.out.println(userInput);
+        addLine();
     }
 
     public static void dukeBye() {
+        addLine();
         System.out.println("Bye, see you again!");
+        addLine();
     }
 
     public static void main(String[] args) {
@@ -19,12 +31,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        addLine();
 
         dukeGreet();
-        addLine();
-        
+        String userInput;
+        Scanner input = new Scanner(System.in);
+        userInput = input.nextLine();
+        while (!userInput.equals("bye")) {
+            dukeEcho(userInput);
+            userInput = input.nextLine();
+        }
         dukeBye();
-        addLine();
     }
 }
