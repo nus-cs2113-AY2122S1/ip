eace31dd95ec0b017e4f35e6864ee7a64b597256
xuanlc113 <xuan.lc113@gmail.com>
2021-08-22 11:32:24 +0800

    add level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
     private static final String DIVIDER = "    ____________________________________________________________";
+    private static List<String> store = new ArrayList<String>();
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -13,7 +16,13 @@ public class Duke {
             if (command.equals("bye")) {
                 break;
             } else {
-                echo(command);
+                switch (command) {
+                case "list":
+                    list();
+                    break;
+                default:
+                    add(command);
+                }
             }
         }
         bye();
@@ -36,6 +45,23 @@ public class Duke {
     public static void echo(String command) {
         System.out.println(DIVIDER);
         System.out.println("     " + command);
+        System.out.println(DIVIDER);
+    }
+
+    public static void add(String command) {
+        System.out.println(DIVIDER);
+        store.add(command);
+        System.out.println("     added: " + command);
+        System.out.println(DIVIDER);
+    }
+
+    public static void list() {
+        System.out.println(DIVIDER);
+        int index = 1;
+        for (String item : store) {
+            System.out.println("     " + index + ". " + item);
+            index++;
+        }
         System.out.println(DIVIDER);
     }
 }
