469a79df845a653bd3682d081faf00e3b2704f69
Bryan Elmer <bryan.elmer.mulijono@gmail.com>
2021-08-22 20:01:10 +0800

    Level-3
    Added task class and mark as done feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,54 +1,18 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static int entryCount = 0;
-    private static String[] entries = new String[100];
-
-    public static void reply(String input) {
-        if (input.equals("list")) {
-            getAndPrintList();
-        } else {
-            printReply(addEntry(input));
-        }
-    }
-
-    public static void printReply(String input) {
-        System.out.println("   ____________________________________________________________\n" +
-                "       " + input + "\n" +
-                "   ____________________________________________________________");
-    }
-
-    public static String addEntry(String input) {
-        entries[entryCount] = input;
-        entryCount++;
-        return "added: " + input;
-    }
-
-    public static void getAndPrintList() {
-        //String fullList = "";
-        int i = 0;
-
-        System.out.println("   ____________________________________________________________");
-        for (String entry : entries) {
-            if (i >= entryCount) {
-                System.out.println("   ____________________________________________________________");
-                return;
-            }
-            //fullList += Integer.toString(i) + ". " + entry + "\n";
-            System.out.println("       " + (i + 1) + ". " + entry);
-            i++;
-        }
-    }
+    private static int taskCount = 0;
+    private static Task[] tasks = new Task[100];
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String line;
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "    ____        _        \n"
+                + "   |  _ \\ _   _| | _____ \n"
+                + "   | | | | | | | |/ / _ \\\n"
+                + "   | |_| | |_| |   <  __/\n"
+                + "   |____/ \\__,_|_|\\_\\___|";
 
         String greetStart = "   ____________________________________________________________\n" +
                 "       Hello! I'm Duke\n" +
@@ -59,6 +23,7 @@ public class Duke {
                 "       Bye. Hope to see you again soon!\n" +
                 "   ____________________________________________________________";
 
+        System.out.println(logo);
         System.out.println(greetStart);
         line = in.nextLine();
         while (!line.equals("bye")) {
@@ -66,5 +31,53 @@ public class Duke {
             line = in.nextLine();
         }
         System.out.println(greetEnd);
+    }
+
+    public static void reply(String line) {
+        String[] input = line.split(" ");
+        if (input[0].equals("list")) {
+            getAndPrintList();
+        } else if (input[0].equals("done")) {
+            setDone(Integer.parseInt(input[1]));
+        } else {
+            _printReply(addTask(line));
+        }
+    }
+
+    private static void _printReply(String input) {
+        System.out.println("   ____________________________________________________________\n" +
+                "       " + input + "\n" +
+                "   ____________________________________________________________");
+    }
+
+    public static String addTask(String input) {
+        tasks[taskCount] = new Task(input);
+        taskCount++;
+        return "added: " + input;
+    }
+
+    public static void getAndPrintList() {
+        int i = 0;
+
+        System.out.println("   ____________________________________________________________");
+        System.out.println("       Here are the tasks in your list:");
+        for (Task task : tasks) {
+            if (i >= taskCount) {
+                System.out.println("   ____________________________________________________________");
+                return;
+            }
+            System.out.println("       " + (i + 1) + ".[" + task.getStatusIcon() + "] " + task.getDescription());
+            i++;
+        }
+    }
+
+    public static void setDone(int index) {
+        if (index - 1 >= taskCount) {
+            _printReply("Entry does not exist.");
+            return;
+        }
+        tasks[index - 1].markAsDone();
+        _printReply("Nice! I've marked this task as done:\n" +
+                "         [X] " + tasks[index - 1].getDescription());
     }
 }
