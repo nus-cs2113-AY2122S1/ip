767c35530fd0297ade103ef9aef7954c7a05fa3a
lezong <lezongmun@gmail.com>
2021-08-23 01:13:33 +0800

    add level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,35 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+        String logo = " \n" +
+                "                        .-\"\"\"-.\n" +
+                "                       / .//\". \\\n" +
+                "                       \\/ o o \\/\n" +
+                "                       ( \\___/ )\n" +
+                "          ________oooo__\\_____/_____________\n" +
+                "         /                                  \\\n" +
+                "        |         Hello! I'm Bobby :)        |\n" +
+                "        |     What can I can do for you?     |\n" +
+                "         \\______________________oooo________/\n" ;
         System.out.println(logo);
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.print("    ");
+            System.out.println(input);
+            System.out.println("    ____________________________________________________________");
+            input = in.nextLine();
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.print("    ");
+        System.out.println("Bye :(, my friend. hehe");
+        System.out.println("    ____________________________________________________________");
+
+
+
     }
 }
