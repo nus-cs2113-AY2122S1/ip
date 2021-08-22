68d7e616df1c40643b2f6f827932a2914fc9c980
ThaddeusLim99 <lcqthad@gmail.com>
2021-08-22 14:26:54 +0800

    Added Javadoc comments and general comments. Tweaked the code a bit to follow coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,7 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String line = "____________________________________________________________";
 
+        //Duke himself
         System.out.println("⠄⠰⠛⠋⢉⣡⣤⣄⡉⠓⢦⣀⠙⠉⠡⠔⠒⠛⠛⠛⠶⢶⣄⠘⢿⣷⣤⡈⠻⣧");
         System.out.println("⢀⡔⠄⠄⠄⠙⣿⣿⣿⣷⣤⠉⠁⡀⠐⠒⢿⣿⣿⣿⣶⣄⡈⠳⢄⣹⣿⣿⣾⣿");
         System.out.println("⣼⠁⢠⡄⠄⠄⣿⣿⣿⣿⡟⠄⡐⠁⡀⠄⠈⣿⣿⣿⣿⣿⣷⣤⡈⠻⣿⣿⣿⣿");
@@ -28,45 +29,54 @@ public class Duke {
 
         int listIndex = 0;
         int listLength = 100;
-        Task[] list = new Task[listLength];
+        Task[] Tasks = new Task[listLength];
 
-        while(true){
+        while (true) {
             command = in.nextLine();
 
-            if(command.equalsIgnoreCase("list")){
-                if(listIndex == 0){
+            if (command.equalsIgnoreCase("list")) {
+                //no tasks in list
+                if (listIndex == 0) {
                     System.out.println("No tasks added yet! :)");
-                }else {
+                } else {
+                    //when there are tasks in list
                     for (int i = 1; i <= listIndex; i++) {
-                        System.out.println(i + ".[" + list[i - 1].getStatusIcon() + "] " + list[i - 1].getDescription());
+                        System.out.println(i + ".[" + Tasks[i - 1].getStatusIcon() + "] "
+                                + Tasks[i - 1].getDescription());
                     }
                 }
-            }else if(command.equalsIgnoreCase("bye")){
+            } else if (command.equalsIgnoreCase("bye")) {
+                //end the program
                 System.out.println(line);
                 System.out.println("Bye my little pogchamp! Hope to see you again soon!");
                 System.out.println(line);
                 System.exit(0);
-            }else if(command.toLowerCase().contains("done")){
+            } else if(command.toLowerCase().contains("done")) {
+                //extract particular list item to mark as done
                 String taskdone = command.replaceAll("[^0-9]","");
                 int index = Integer.parseInt(taskdone);
-
-                if(index <= 0){
+                //index cannot be <= 0
+                if (index <= 0) {
                     System.out.println("Invalid index");
-                }else if(index <= listIndex){
-                    list[index - 1].markDone();
+                } else if(index <= listIndex) {
+                    //valid index
+                    Tasks[index - 1].markDone();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("[X] " + list[index - 1].getDescription());
-                }else{
+                    System.out.println("[X] " + Tasks[index - 1].getDescription());
+                } else {
+                    //not enough tasks to make it to that index
                     System.out.println("No task at that index");
                 }
-            }else{
-                if(listIndex < listLength){
-                    list[listIndex] = new Task(command);
+            } else {
+                //add task
+                if (listIndex < listLength) {
+                    Tasks[listIndex] = new Task(command);
                     System.out.println(line);
-                    System.out.println("added: " + list[listIndex].getDescription());
+                    System.out.println("added: " + Tasks[listIndex].getDescription());
                     System.out.println(line);
                     listIndex++;
-                }else{
+                } else {
+                    //>= 100 tasks
                     System.out.println("List is full! :(");
                 }
             }
