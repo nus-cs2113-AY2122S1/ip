b01860e375af384d20554189ac81f50a0c0d5fdb
crabnuggets <alvinpang321@gmail.com>
2021-08-22 23:36:14 +0800

    Add Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String greetingMessage = "____________________________________________________________\n"
+                + " Hello! I'm Duke\n"
+                + " What can I do for you?";
+
+        String goodbyeMessage = "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+
+        System.out.println(greetingMessage);
+        System.out.println(goodbyeMessage);
     }
 }
