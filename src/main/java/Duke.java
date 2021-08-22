daaef83dbe4ca9409fae61f01f56c42f72f3255a
Yap Joon Siong <yapjoonsiong@gmail.com>
2021-08-23 02:02:10 +0800

    Implement a skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,32 @@
+import java.util.*;
+
 public class Duke {
+    public static void lineBreak() {
+        System.out.println("\t____________________________________________________________\n");
+    }
+    public static void response(String input) {
+        System.out.println("\t" + input + "\n");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String linebreak = "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo + linebreak);
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n" + linebreak); // greet
-        System.out.println("Bye. Hope to see you again soon!\n" + linebreak); //exit message
+        System.out.println("Hello from\n" + logo);
+        lineBreak();
+        System.out.println("\tHello! I'm Duke\n" + "\tWhat can I do for you?\n"); // greet
+        lineBreak();
+        Scanner sc= new Scanner(System.in);
+        String input = sc.nextLine();
+        lineBreak();
+        while (!Objects.equals(input, "bye")) {
+            response(input);
+            lineBreak();
+            input = sc.nextLine();
+            lineBreak();
+        }
+        System.out.println("Bye. Hope to see you again soon!\n"); //exit message
+        lineBreak();
     }
 }
