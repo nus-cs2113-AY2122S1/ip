b5d8bfde5a52798917812e7036b85352e2dba08f
MAZJ124 <yuangazi@hotmail.com>
2021-08-22 14:58:29 +0800

    Add features to greet user, echo messages and exit program

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,11 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "------------------------------------";
+        String input;
         System.out.println("Hello from\n" + logo);
         System.out.println(line);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println(line);
+        do {
+            Scanner in = new Scanner(System.in);
+            input = in.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+
+        } while (!input.equals("bye"));
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
     }
