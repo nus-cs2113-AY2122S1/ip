8e4859742c8847ef2bede3931f62d64d87bb92ff
dlimyh98 <dlimyh98@hotmail.com>
2021-08-22 16:22:13 +0800

    Duke Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        System.out.print("Bye. Hope to see you again soon!");
+        System.out.println("What can I do for you today boss");
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("See you later alligator");
+                break;
+            }
+
+            System.out.println(input);
+        }
     }
 }
