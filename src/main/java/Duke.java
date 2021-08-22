ba49262ec317559ef92090be5750687576613358
zonglun99 <zonglun99@gmail.com>
2021-08-22 12:21:11 +0800

    Add user interaction.
    Create a scanner variable in, and string variable userCommand to read input.
    Exits program when user inputs a "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________\n"
-        + "Hello! I'm Duke\n" + "What can I do for you?\n" +
-        "____________________________________________________________\n" +
-        "Bye. Hope to see you soon!\n" +
+        + "Hey there! I'm Chai\n" + "What do we have today?\n" +
+        "____________________________________________________________");
+        String userCommand;
+        Scanner in = new Scanner(System.in);
+        userCommand = in.nextLine();
+        while (!userCommand.equals("bye")) {
+            System.out.println("____________________________________________________________\n" + userCommand);
+            System.out.println("____________________________________________________________");
+            userCommand = in.nextLine();
+        }
+        System.out.println("____________________________________________________________\n" + "GoodBye!\n" +
         "____________________________________________________________");
     }
 }
