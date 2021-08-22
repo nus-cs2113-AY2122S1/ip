d258572c705bf0305838657cd55cadad6dc16354
Vignesh Kumaravel <k_vignesh@hotmail.com>
2021-08-22 11:19:19 +0800

    Do Greet, Echo, Exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,57 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
+    final static String HOR_LINE = "_".repeat(30);
+
     public static void greet() {
-        System.out.println("_".repeat(30));
-        System.out.println("Hello! I'm Duke\n" +
-                "What can I do for you?");
-        System.out.println("_".repeat(30));
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_".repeat(30));
+        System.out.println("\t" + HOR_LINE);
+        System.out.println("\tHi there! I'm Duke\n" +
+                "\tWhat can I do for you today?");
+        System.out.println("\t" + HOR_LINE + System.lineSeparator());
+    }
+
+    public static void echoCommand(String cmd) {
+        System.out.println("\t" + HOR_LINE);
+        System.out.println("\tYou have entered: " + cmd);
+        System.out.println("\t" + HOR_LINE + System.lineSeparator());
+    }
+
+    public static void echo() {
+        String command;
+        Scanner in = new Scanner(System.in);
+        command = in.nextLine();
+        String cmdLowerC = command.toLowerCase();
+        while (!cmdLowerC.equals("bye")) {
+            while (command.equals("") | command.equals(" ")) {
+                System.out.println("\t" + HOR_LINE);
+                System.out.println("\tPlease enter a valid command!");
+                System.out.println("\t" + HOR_LINE + System.lineSeparator());
+                command = in.nextLine();
+            }
+
+            echoCommand(command);
+            command = in.nextLine();
+            cmdLowerC = command.toLowerCase();
+        }
+    }
+
+    public static void exit() {
+        System.out.print("\tDo you really want to exit chatbot (type y or n)? ");
+        Scanner input = new Scanner(System.in);
+        String exit_pref = input.nextLine();
+
+        if (exit_pref.equals("y")) {
+            System.out.println("\t" + HOR_LINE);
+            System.out.println("\tBye. Hope to see you again soon!");
+            System.out.println("\t" + HOR_LINE + System.lineSeparator());
+        }
+        else {
+            System.out.println("\n\tOk that's great! Continue keying in commands. :)");
+            System.out.println("\t" + HOR_LINE + System.lineSeparator());
+            echo();
+            exit();
+        }
     }
 
     public static void main(String[] args) {
@@ -15,7 +61,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Actions
         greet();
-        //Test line
+        echo();
+        exit();
     }
 }
