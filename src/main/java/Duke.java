50b5a122b86e092a29add25bc2c335ee0a08fc7b
xuanlc113 <xuan.lc113@gmail.com>
2021-08-22 12:01:17 +0800

    add level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Duke {
 
     private static final String DIVIDER = "    ____________________________________________________________";
-    private static List<String> store = new ArrayList<String>();
+    private static List<Task> store = new ArrayList<Task>();
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -13,12 +13,16 @@ public class Duke {
         while (true) {
             System.out.println("");
             String command = scan.nextLine();
-            if (command.equals("bye")) {
+            String[] parts = command.split(" ");
+            if (parts[0].equals("bye")) {
                 break;
             } else {
-                switch (command) {
+                switch (parts[0]) {
                 case "list":
                     list();
+                    break;
+                case "done":
+                    setTaskDone(Integer.parseInt(parts[1]));
                     break;
                 default:
                     add(command);
@@ -48,20 +52,36 @@ public class Duke {
         System.out.println(DIVIDER);
     }
 
-    public static void add(String command) {
+    public static void add(String description) {
         System.out.println(DIVIDER);
-        store.add(command);
-        System.out.println("     added: " + command);
+        Task task = new Task(description);
+        store.add(task);
+        System.out.println("     added: " + description);
         System.out.println(DIVIDER);
     }
 
     public static void list() {
         System.out.println(DIVIDER);
+        System.out.println("     Here are the tasks in your list:");
         int index = 1;
-        for (String item : store) {
-            System.out.println("     " + index + ". " + item);
+        for (Task item : store) {
+            System.out.print("     " + index + ". ");
+            item.print();
             index++;
         }
+        System.out.println(DIVIDER);
+    }
+
+    public static void setTaskDone(int index) {
+        if (index >= store.size()) {
+            return;
+        }
+        Task task = store.get(index - 1);
+        task.markAsDone();
+        System.out.println(DIVIDER);
+        System.out.println("     Nice! I've marked this task as done: ");
+        System.out.print("       ");
+        task.print();
         System.out.println(DIVIDER);
     }
 }
