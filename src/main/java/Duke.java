7db9fcd956325df55178d5b820a13f8dd5b80d84
crabnuggets <alvinpang321@gmail.com>
2021-08-23 00:32:40 +0800

    Add Level-1: Greet, Echo, Exit
    * Update logo and bot name to "Billy"
    * Add echo for user input
    * Add case-insensitive exit when user inputs "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
 
-        String greetingMessage = "____________________________________________________________\n"
-                + " Hello! I'm Duke\n"
-                + " What can I do for you?";
+        String logo = " ______    _   __   __            \n" +
+                "|_   _ \\  (_) [  | [  |           \n" +
+                "  | |_) | __   | |  | |   _   __  \n" +
+                "  |  __'.[  |  | |  | |  [ \\ [  ] \n" +
+                " _| |__) || |  | |  | |   \\ '/ /  \n" +
+                "|_______/[___][___][___][\\_:  /   \n" +
+                "                         \\__.'    \n";
 
-        String goodbyeMessage = "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+        String horizontalLine = "____________________________________________________________\n";
+        String greetingMessage = horizontalLine +
+                " Hello! I'm Billy\n" +
+                " What can I do for you?\n" +
+                horizontalLine;
 
-        System.out.println(greetingMessage);
-        System.out.println(goodbyeMessage);
+        String goodbyeMessage = horizontalLine +
+                " Bye. Hope to see you again soon!\n" +
+                horizontalLine;
+        boolean programIsRunning = true;
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello from\n" + logo + greetingMessage);
+
+        while(programIsRunning) {
+            userInput = in.nextLine();
+            if (userInput.toLowerCase().equals("bye")) {
+                System.out.println(goodbyeMessage);
+                programIsRunning = false;
+            } else {
+                System.out.println(horizontalLine + userInput + "\n" + horizontalLine);
+            }
+        }
+
     }
 }
