b5318b4b8db37a55b0590e64b6b18f9da99eda41
Woolicious98 <e527269@u.nus.edu>
2021-08-22 14:26:17 +0800

    Add increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,28 @@
+import java.util.Scanner;
 
 public class Duke {
+
     final static String line = "____________________________________________________________";
+
     public static void main(String[] args) {
         System.out.println(line);
         printWelcomeMesage();
-        printExitMessage();
+        getMenu();
     }
-    public static void printExitMessage(){
+
+    public static void printExitMessage() {
+        System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
     }
-    public static void printWelcomeMesage(){
+
+    public static void printWelcomeMesage() {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println(line);
     }
-    public static void printLogoMessage(){
+
+    public static void printLogoMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -23,5 +30,22 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println(line);
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(line);
+        System.out.println(message);
+        System.out.println(line);
+    }
+
+    public static void getMenu() {
+        String userInputs;
+        Scanner in = new Scanner(System.in);
+        userInputs = in.nextLine();
+        while (!userInputs.equals("bye")) {
+            printMessage(userInputs);
+            userInputs = in.nextLine();
+        }
+        printExitMessage();
     }
 }
