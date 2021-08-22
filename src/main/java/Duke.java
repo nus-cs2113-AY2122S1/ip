cab09a9a1d595b727c33904764bb93ee2c3f2311
NonRNP <69129667+NonRNP@users.noreply.github.com>
2021-08-22 10:01:01 +0800

    Level 2 - Add, List
    Add function for duke to store text from input and to show them to user when asked

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,6 +24,21 @@ public class Duke {
         addLine();
     }
 
+    public static void showTaskList(String[] tasks, int taskCount) {
+        addLine();
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+        addLine();
+    }
+
+    public static void addTask(String[] tasks, int taskCount, String userInput) {
+        tasks[taskCount] = userInput;
+        addLine();
+        System.out.println("Task added: " + userInput);
+        addLine();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -36,8 +51,17 @@ public class Duke {
         String userInput;
         Scanner input = new Scanner(System.in);
         userInput = input.nextLine();
+
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (!userInput.equals("bye")) {
-            dukeEcho(userInput);
+            if (userInput.equals("list")) {
+                showTaskList(tasks, taskCount);
+            } else {
+                addTask(tasks, taskCount, userInput);
+                taskCount++;
+            }
             userInput = input.nextLine();
         }
         dukeBye();
