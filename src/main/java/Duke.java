a77a2c0ea99b9d9b52a12f20d79fcafa13715b73
irvinseet <irvinseet@gmail.com>
2021-08-22 15:47:29 +0800

    Level-1
    Implement echo on user input, exits on "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void Echo(String message) {
+        if (message.equalsIgnoreCase("bye")) {
+            return;
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     " + message);
+        System.out.println("    ____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        String line = "";
+        Scanner in = new Scanner(System.in);
+        while (!line.equals("bye")) { //while user input != bye, case insensitive
+            line = in.nextLine();
+            Echo(line);
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ___________________________________________________________");
+        System.exit(0);//end of program
     }
 }
