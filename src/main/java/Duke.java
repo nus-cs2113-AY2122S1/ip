6d3a5a924285ef2fd5473cc71da71c90d7e76272
zonglun99 <zonglun99@gmail.com>
2021-08-22 13:16:53 +0800

    Add list to record task from user
    Added a new logo design.
    Introduce a list array to record down the input tasks.
    Input command "list" to print out task in the lists.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,15 +2,39 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n"
-        + "Hey there! I'm Chai\n" + "What do we have today?\n" +
-        "____________________________________________________________");
         String userCommand;
+        String[] taskList = new String[100];
+        int listSize = 0;
+        String logo =
+                " ______    __                     __ \n" +
+                "/  ____|  |  |                   |__|\n" +
+                "|  |      |  |_____    _______    __ \n" +
+                "|  |      |   __   \\  /  __   |  |  |\n" +
+                "|  |____  |  |  |  |  | |__|  |  |  |\n" +
+                "\\______|  |__|  |__|  \\____/\\_|  |__|\n" ;
+
+        System.out.println("This is\n" + logo);
+        System.out.println("____________________________________________________________\n"
+        + "Hey there! I'm Chai\n" + "What are you doing today?\n" +
+        "____________________________________________________________");
+
         Scanner in = new Scanner(System.in);
         userCommand = in.nextLine();
         while (!userCommand.equals("bye")) {
-            System.out.println("____________________________________________________________\n" + userCommand);
-            System.out.println("____________________________________________________________");
+            if (userCommand.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < listSize; i++) {
+                    System.out.println(taskList[i]);
+                }
+                System.out.println("____________________________________________________________");
+            }
+            else {
+                System.out.println("____________________________________________________________\n" +
+                        "Added to the list: " + userCommand);
+                System.out.println("____________________________________________________________");
+                taskList[listSize] = (listSize + 1) + ". " + userCommand;
+                listSize++;
+            }
             userCommand = in.nextLine();
         }
         System.out.println("____________________________________________________________\n" + "GoodBye!\n" +
