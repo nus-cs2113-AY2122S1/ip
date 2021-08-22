1237225d42437ba725b801c2a9013820e54a3395
Uxinnn <xinyu.foong@gmail.com>
2021-08-22 16:12:01 +0800

    added echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("\nBye. Hope to see you again soon!");
+        String text;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\tHello! I'm Duke \uD83D\uDE04");
+        System.out.println("\tWhat can I do for you?");
+        text = in.nextLine();
+        while (!text.equals("bye")) {
+            System.out.println("\t" + text);
+            text = in.nextLine();
+        }
+        System.out.println("\tBye. Hope to see you again soon! \uD83D\uDC4B");
     }
 }
