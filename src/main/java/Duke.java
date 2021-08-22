44481c330f780c78d7543839772f34ca6c32aa12
Yap Wei Xuan <e0533997@u.nus.edu>
2021-08-22 22:07:32 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,10 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________\n";
+        System.out.println(line + logo +
+                " Hello! I'm Duke\n" +
+                " What can I do for you?\n" + line +
+                " Bye. Hope to see you again soon!\n" + line);
     }
 }
