96a3d0f8ad62b4623acf4611bf5cc43ead1b4e90
Renzo Canare <renzo_canare@hotmail.com>
2021-08-24 04:39:18 +0800

    Level 2. Add, List Add ability to store text entered into a list, and display to user when requested via list command Remove echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String line;
+        ArrayList<String> storedItems = new ArrayList<String>();
+
         String welcomeMessage = "____________________________________________________________\n"
                 + "Hello! I'm your friendly chatbot, John!\n"
                 + "How can I help?\n"
@@ -12,10 +15,20 @@ public class Duke {
         line = in.nextLine();
 
         while (!line.equals("bye")) {
-            String repeat = "____________________________________________________________\n"
-                    + line + "\n"
-                    + "____________________________________________________________\n";
-            System.out.println(repeat);
+            if (line.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < storedItems.size(); i ++) {
+                    System.out.println(i + 1 + ". " + storedItems.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            }
+            else {
+                storedItems.add(line);
+                String repeatMessage = "____________________________________________________________\n"
+                        + "\"" + line + "\"" + " has been added to the list!\n"
+                        + "____________________________________________________________\n";
+                System.out.println(repeatMessage);
+            }
             line = in.nextLine();
         }
 
