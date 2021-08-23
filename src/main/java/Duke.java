41f6e2a475bf8f20b8048e9f1a9fed4042590f54
mohamad-adam8991 <69377976+mohamad-adam8991@users.noreply.github.com>
2021-08-23 23:21:26 +0800

    Added ability to store and list user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,14 +10,25 @@ public class Duke {
         System.out.println("**************************************************");
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         System.out.println("**************************************************");
+        String[] userList = new String[100];
         Scanner scanner = new Scanner(System.in);
+        int x = 0;
         String userInput = "";
-        do{
+        while(!userInput.equals("bye")){
             userInput = scanner.nextLine();
             System.out.println("**************************************************");
-            System.out.println(userInput);
-            System.out.println("**************************************************");
-        } while(!userInput.equals("bye"));
+            if(userInput.equals("list")){
+                for(int i = 0; i < x; i++){
+                    System.out.println((i+1) + ". " + userList[i]);
+                }
+                System.out.println("**************************************************");
+            } else if(!userInput.equals("bye")){
+                userList[x] = userInput;
+                System.out.println("Added: "+ userInput);
+                x++;
+                System.out.println("**************************************************");
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("**************************************************");
     }
