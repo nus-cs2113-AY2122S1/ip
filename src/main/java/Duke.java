9c339994397dbf708552852baae99d70f4330ac5
datn02 <ducanh02.work@gmail.com>
2021-08-23 20:53:20 +0800

    Add echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,11 +11,25 @@ public class Duke {
         String greet =  "------------------------\n"
                 +       "Hello! I'm Duke\n"
                 +       "What can I do for you?\n"
-                +       "------------------------\n"
-                +       "Bye. Hope to see you again soon!\n"
                 +       "------------------------\n";
+
+        String userCommand;
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Hello from\n" + logo);
         System.out.println(greet);
+
+        userCommand = input.nextLine();
+
+        while (!userCommand.equals("bye")) {
+            System.out.println("------------------------");
+            System.out.printf("\t%s\n", userCommand);
+            System.out.println("------------------------");
+            userCommand = input.nextLine();
+        }
+
+        System.out.println("------------------------\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "------------------------");
     }
 }
