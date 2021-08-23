5586c88b75384f3a013ef7dd60859e4dca7b51fc
JaCh23 <jach2355@gmail.com>
2021-08-23 14:34:47 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,21 @@
 package ip.src.main.java;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner in = new Scanner(System.in);
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
+
+        String line = null;
+        while (!Objects.equals(line, "bye")) {
+            line = in.nextLine();
+            if (!Objects.equals(line, "bye")) System.out.println(line);
+        }
+
         System.out.println("Bye. Hope to see you again soon!\n");
 
     }
