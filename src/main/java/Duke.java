4ddbf1798476868a0ae965530c0b8441e21399a9
SvethaMahadevan <svets321@gmail.com>
2021-08-23 23:39:15 +0800

    Add tasks feature with add task and list task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,39 +1,64 @@
 import java.util.*;
 public class Duke {
-    public static void main(String[] args) {
-        boolean notBye = true;
-        String userCommand;
-        String horizontalLine = "____________________________________________________________";
+    private static String[] taskList;
+    private static int taskNum;
 
-        Scanner in = new Scanner(System.in);
+    public static void addTask(String task) {
+
+        taskList[taskNum] = task;
+        taskNum += 1;
+        Output("added: " + task);
+
+    }
+
+    public static void listTask() {
+        String tasks = "";
+        for(int i = 0; i < taskNum; i++) {
+            tasks = tasks.concat((i + 1) + ". " + taskList[i] + "\n");
+        }
+
+        tasks = tasks.substring(0, tasks.length() - 1);
+        Output(tasks);
+    }
+
+    public static void Output(String output) {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
+        System.out.println(output);
+        System.out.println(horizontalLine);
+    }
+
+    public static void main(String[] args) {
+
+        taskList = new String[100];
+        taskNum= 0;
+        boolean notBye = true;
+        String userInput;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        //greeting
-        System.out.println(horizontalLine);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
+        Output("Hello! I'm Duke\n" + "What can I do for you?");
 
-        //echo
+
+        Scanner in = new Scanner(System.in);
+        userInput = in.nextLine();
         while(notBye)
         {
-            userCommand = in.nextLine();
-            if(userCommand.equals("bye")) {
+            if(userInput.equals("bye"))
                 break;
+
+            if(userInput.equals("list")) {
+                listTask();
             }
-            System.out.println(horizontalLine);
-            System.out.println(userCommand);
-            System.out.println(horizontalLine);
+            else
+                addTask(userInput);
+
+            userInput = in.nextLine();
 
         }
-
-        //exit
-        System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        Output("Bye. Hope to see you again soon!");
     }
 }
