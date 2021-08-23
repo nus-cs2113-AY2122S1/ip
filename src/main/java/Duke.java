2cbf2fe3a8d28ddcf14e4b04e269337bfec4670f
JaCh23 <jach2355@gmail.com>
2021-08-23 15:27:01 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,33 @@
 package ip.src.main.java;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
+
+    public static String[] addLine(String[] arr, String line)
+    {
+        int i;
+        String[] newarr = new String[arr.length + 1];
+
+        for (i = 0; i < arr.length; i++) newarr[i] = arr[i];
+
+        newarr[i] = line;
+
+        return newarr;
+
+
+    }
+
+    public static void printList(String[] arr)
+    {
+        for (int i = 0; i < arr.length; i++) System.out.println(i+1 + ". " + arr[i]);
+    }
+
+
     public static void main(String[] args) {
+        int i=0;
+        String[] arr = {};
 
         Scanner in = new Scanner(System.in);
 
@@ -11,9 +35,15 @@ public class Duke {
         System.out.println("What can I do for you?\n");
 
         String line = null;
+
         while (!Objects.equals(line, "bye")) {
             line = in.nextLine();
-            if (!Objects.equals(line, "bye")) System.out.println(line);
+            if (Objects.equals(line, "list")) printList(arr);
+            else {
+                arr = addLine(arr, line);
+                if (!Objects.equals(line, "bye")) System.out.println("added: "+ line);
+            }
+
         }
 
         System.out.println("Bye. Hope to see you again soon!\n");
