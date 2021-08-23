b0fdc78c1f694375f28467fb29db925e034c5bc8
madhanse <madhan132000@gmail.com>
2021-08-23 21:03:38 +0800

    Echoing the input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,7 +8,20 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n"
-                          + "What can I do for you?\n\n");
-        System.out.println("Bye. Hope to see you again soon!");
+                          + "What can I do for you?");
+        String line;
+        do {
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            System.out.println("___________________TASK ACTIVATED__________________");
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("___________________SHUTTING DOWN___________________");
+            } else {
+                System.out.println(line);
+                System.out.println("___________________TASK COMPLETED__________________");
+                System.out.println("Anything else?");
+            }
+        } while (!line.equals("bye"));
     }
 }
