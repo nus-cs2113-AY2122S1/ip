85ce40b2b883a5d509f34c485cc34104bc8e03a9
lezong <lezongmun@gmail.com>
2021-08-23 12:10:08 +0800

    add level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] list = new String[100];
+        int listIndex = 0;
+
         String logo = " \n" +
                 "                        .-\"\"\"-.\n" +
                 "                       / .//\". \\\n" +
@@ -18,18 +21,33 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         input = in.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("    ____________________________________________________________");
-            System.out.print("    ");
-            System.out.println(input);
-            System.out.println("    ____________________________________________________________");
+            // if user inputs list, showcase the list
+            if (input.equals("list")){
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < listIndex; i ++){
+                    System.out.printf("    %s\n", list[i]);
+
+                }
+                System.out.println("    ____________________________________________________________");
+            }
+            // else is a task to be added to list
+            else {
+                System.out.println("    ____________________________________________________________");
+                System.out.printf("    Okay I've added %s into the list!", input);
+                System.out.println("\n    ____________________________________________________________");
+                list[listIndex] = String.format("%d. %s", listIndex + 1, input);
+                listIndex ++;
+            }
+
+            //inserting tasks into the list
+//            list[listIndex] = String.format("%d. %s", listIndex + 1, input);
+//            listIndex ++;
             input = in.nextLine();
         }
         System.out.println("    ____________________________________________________________");
         System.out.print("    ");
         System.out.println("Bye :(, my friend. hehe");
         System.out.println("    ____________________________________________________________");
-
-
 
     }
 }
