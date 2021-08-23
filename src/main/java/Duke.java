40605232b5a51fa0cc5881d98d5e2e6dcd014d48
alvintan01 <35805635+alvintan01@users.noreply.github.com>
2021-08-23 17:11:00 +0800

    Adhered to coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,57 +9,90 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        //print welcome message
+        // Prints welcome message
         String welcomeMessage = "Hello! I'm Duke\n";
         welcomeMessage += "     What can I do for you?";
-        custom_print(welcomeMessage);
+        customPrint(welcomeMessage);
 
+        // ArrayList to store all the tasks
         ArrayList<Task> tasks = new ArrayList<>();
 
+        // String to store user's input
         String description = "";
 
         Scanner in = new Scanner(System.in);
 
         while (true) {
+            // Reads user input
             description = in.nextLine();
 
             // Handle the bye case
             if (description.equals("bye")) {
-                custom_print("Bye. Hope to see you again soon!");
+                customPrint("Bye. Hope to see you again soon!");
                 break;
-            } else if (description.equals("list")) { //handle list case
-                print_list(tasks);
-            } else if (description.startsWith("done")) { //handle done case
+            } else if (description.equals("list")) {
+                // Handle list case
+                listTasks(tasks);
+            } else if (description.startsWith("done")) {
+                // Handle done case
                 int taskId = Integer.parseInt(description.split(" ")[1]);
-                mark_done(tasks, taskId - 1); //-1 as index starts from 0
+                if (taskId <= 0 || taskId > tasks.size()) {
+                    customPrint("You have entered an invalid task ID!");
+                    continue;
+                }
+                // Checks if task has been completed
+                Task currentTask = tasks.get(taskId - 1);
+                if (currentTask.isDone()) {
+                    customPrint("You have already completed the task [" + currentTask.getDescription() + "]!");
+                } else {
+                    markDone(tasks, taskId - 1); // -1 as index starts from 0
+                }
             } else {
+                // Add new task
                 tasks.add(new Task(description));
-                custom_print("added: " + description);
+                customPrint("added: " + description);
             }
         }
     }
 
-    public static void custom_print(String statement) {
+    /**
+     * Prints out a statement with borders.
+     *
+     * @param statement Statement to be printed.
+     */
+    public static void customPrint(String statement) {
         System.out.println("    ____________________________________________________________");
         System.out.println("     " + statement);
         System.out.println("    ____________________________________________________________");
     }
 
-    public static void print_list(ArrayList<Task> tasks) {
+    /**
+     * Prints out all the Tasks with borders.
+     *
+     * @param tasks Arraylist of tasks entered by user.
+     */
+    public static void listTasks(ArrayList<Task> tasks) {
         System.out.println("    ____________________________________________________________");
         System.out.println("     Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
             Task currentTask = tasks.get(i);
-            System.out.println("     " + String.valueOf(i + 1) + ".[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
+            System.out.println("     " + (i + 1) + ".[" + currentTask.getStatusIcon() + "] "
+                    + currentTask.getDescription());
         }
         System.out.println("    ____________________________________________________________");
     }
 
-    public static void mark_done(ArrayList<Task> tasks, int taskId) {
+    /**
+     * Marks a task as done and calls customPrint to print the completed message.
+     *
+     * @param tasks  Arraylist of tasks entered by user.
+     * @param taskId ID of task to be marked as completed.
+     */
+    public static void markDone(ArrayList<Task> tasks, int taskId) {
         Task currentTask = tasks.get(taskId);
-        currentTask.markAsDone(); //mark task as done
+        currentTask.markAsDone(); // Mark current task as done
         String textToPrint = "Nice! I've marked this task as done:\n";
         textToPrint += "       [" + currentTask.getStatusIcon() + "] " + currentTask.getDescription();
-        custom_print(textToPrint);
+        customPrint(textToPrint);
     }
 }
