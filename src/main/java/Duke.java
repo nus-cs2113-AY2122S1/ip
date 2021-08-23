3ca056b258d9d4db70036feaf505ea76a3904103
Ho Zhen Hong <hozhenhong99@gmail.com>
2021-08-24 00:42:16 +0800

    remove unnecessary comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,6 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        // String logo = " ____        _        \n"
-        //         + "|  _ \\ _   _| | _____ \n"
-        //         + "| | | | | | | |/ / _ \\\n"
-        //         + "| |_| | |_| |   <  __/\n"
-        //         + "|____/ \\__,_|_|\\_\\___|\n";
-        // System.out.println("Hello from\n" + logo);
         String line = "____________________________________________________________";
         System.out.println(line + "\n Hello! I'm Duke\n What can I do for you?\n" + line);
         Boolean isCompleted = false;
