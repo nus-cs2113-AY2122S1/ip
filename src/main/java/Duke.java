c183190b834cf993206afe75ffd061725695b4cd
Li Xi Yuan <lixiyuan.owlsong@gmail.com>
2021-08-23 22:46:34 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,16 +4,18 @@ public class Duke {
     public static void line(){
         System.out.println("____________________________________________________________");
     }
-
     private static Boolean isFinished = false;
+    private static int itemCount = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] items = new String[100];
 
         line();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         line();
+        System.out.println();
 
         while(!isFinished){
             String userInput = sc.nextLine();
@@ -21,9 +23,16 @@ public class Duke {
                 isFinished = true;
                 break;
             }
-            line();
-            System.out.println(userInput);
-            line();
+            else if (userInput.equals("list")){
+                for (int i = 0; i < itemCount; i++){
+                    System.out.println("\t" + (i+1) + ". " + items[i]);
+                }
+            }
+            else {
+                items[itemCount] = userInput;
+                System.out.println("\tadded: " + userInput);
+                itemCount++;
+            }
         }
 
         line();
