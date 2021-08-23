ca440f480a1e672a9f2f2b898a7b61dd38819390
ShaoYurui <rrayshao@gmail.com>
2021-08-23 19:27:23 +0800

    Implement Level 0 Greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void printLine()
+    {
+        System.out.println("\t____________________________________________________________");
+    }
+    public static void startDuke()
+    {
+        printLine();
+        String logo = "\t ____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|";
+        System.out.println("\tHello from\n" + logo);
+        printLine();
+    }
+    public static void endDuke()
+    {
+        System.out.println("\tBye. Hope to see you again soon!");
+        printLine();
+    }
+    public static void greet()
+    {
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        printLine();
+    }
+    public static void main(String[] args)
+    {
+        startDuke();
+        greet();
+        endDuke();
     }
 }
