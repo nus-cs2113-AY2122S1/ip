33f65633eea5ae2fc2445aeafb298e6c5d980c27
SvethaMahadevan <svets321@gmail.com>
2021-08-24 01:03:01 +0800

    Add Task Class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,34 @@
 import java.util.*;
 public class Duke {
-    private static String[] taskList;
+    private static Task[] taskList;
     private static int taskNum;
-    private static boolean[] taskDone;
+
 
     public static void addTask(String task) {
 
-        taskList[taskNum] = task;
+        taskList[taskNum] = new Task(task);
         taskNum += 1;
         Output("added: " + task);
 
     }
 
     public static void listTask() {
-        String tasks = "";
-        for(int i = 0; i < taskNum; i++) {
-            if(taskDone[i])
-                tasks = tasks.concat((i + 1) + ". [X] " + taskList[i] + "\n");
-            else
-                tasks = tasks.concat((i + 1) + ". [ ] " + taskList[i] + "\n");
 
+        String tasks = "";
+
+        for(int i = 0; i < taskNum; i++) {
+            tasks = tasks.concat((i + 1) + ". [" +
+                    taskList[i].getStatusIcon() + "] " +
+                    taskList[i].description + "\n");
         }
 
+
         tasks = tasks.substring(0, tasks.length() - 1);
+
         Output(tasks);
+
     }
+
     public static void dispTaskDone(String task) {
         int taskIndex = Integer.parseInt(task.replace("done ", "")) -  1;
 
@@ -32,9 +36,11 @@ public class Duke {
             Output(" The task " + (taskIndex + 1) + " doesn't exist.\nMake sure a valid task number is entered.");
             return;
         }
-        taskDone[taskIndex] = true;
-        Output("Nice! I've marked this task as done:\n" +
-                "[X] " + taskList[taskIndex]
+        Task current = taskList[taskIndex];
+
+        current.markAsDone();
+        Output("Nice! I've marked this task as done:\n [" +
+                current.getStatusIcon() + "] " + current.description
         );
     }
 
@@ -49,10 +55,9 @@ public class Duke {
 
     public static void main(String[] args) {
 
-        taskList = new String[100];
-        taskDone = new boolean[100];
+        taskList = new Task[100];
         taskNum= 0;
-        boolean notBye = true;
+
         String userInput;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -65,12 +70,9 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         userInput = in.nextLine();
-        while(notBye)
+        while(!userInput.equals("bye"))
         {
-            if(userInput.equals("bye"))
-                break;
-
-            else if(userInput.equals("list")) {
+            if(userInput.equals("list")) {
                 listTask();
             }
             else if(userInput.startsWith("done")) {
