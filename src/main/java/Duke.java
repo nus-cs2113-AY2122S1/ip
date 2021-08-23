76105ce476cce66b3e43f3c08fbb26b76d8e5a93
WU_LUOYU_SERENA <siriuswusss@gmail.com>
2021-08-23 21:48:10 +0800

    Add Store and List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,44 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         greeting();
-        echo();
+        //echo();
+        Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
+        int total = 0;
+        while (true) {
+            String command = in.nextLine();
+            if (command.equals("list")) {
+                list(list, total);
+                continue;
+            } else if (command.equals("bye")) {
+                break;
+            } else {
+                store(list, total, command);
+                total++;
+            }
+        }
+        exit();
+    }
+
+    private static void list(String[] list, int total) {
         drawDivider();
-        System.out.println("ヾ(￣▽￣)Bye~Bye~ Hope to see you again soon meow.");
+        if (total == 0) {
+            System.out.println("Karlett can't remember anything...(ФДФ)");
+        }
+        for (int i = 0; i < total; i++) {
+            System.out.println("ฅ" + (i+1) + " " + list[i]);
+        }
         drawDivider();
+    }
+
+    public static void store(String[] list, int index, String command) {
+        drawDivider();
+        System.out.println("Karlett remembers: " + command);
+        drawDivider();
+        list[index] = command;
     }
 
     public static void greeting() {
@@ -29,6 +61,12 @@ public class Duke {
             System.out.println(command + " meow!");
             drawDivider();
         }
+    }
+
+    public static void exit() {
+        drawDivider();
+        System.out.println("Bye~Bye~ヾ(￣▽￣) Hope to see you again soon meow.");
+        drawDivider();
     }
 
     public static void drawDivider() {
