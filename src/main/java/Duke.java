b4b0acd5e3da5cf4a5022c14ecfc87654e5e8d1f
leowyy99 <leowyy99@hotmail.com>
2021-08-23 16:46:38 +0800

    Add add and list features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String separator = "\t____________________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
 
-        String separator = "\t____________________________________________________________";
+        System.out.println("Hello from\n" + logo);
         System.out.println(separator);
         System.out.println("\tHello I'm Duke");
         System.out.println("\tWhat can I do for you?");
@@ -17,16 +17,34 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         String command;
-        command = in.nextLine();
-        while (!command.equals("bye")) {
-            System.out.println(separator);
-            System.out.println("\t" + command);
-            System.out.println(separator);
-            command = in.nextLine();
-        }
+        boolean isDone = false;
+        int numOfTasks = 0;
+        String[] taskList = new String[100];
 
-        System.out.println(separator);
-        System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println(separator);
+        do {
+            command = in.nextLine();
+            switch (command){
+            case "bye":
+                isDone = true;
+                System.out.println(separator);
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            case "list":
+                System.out.println(separator);
+                for (int i = 0; i < numOfTasks; i++) {
+                    System.out.printf("\t%d. %s\n", i+1, taskList[i]);
+                }
+                System.out.println(separator);
+                break;
+            default:
+                taskList[numOfTasks] = command;
+                numOfTasks++;
+                System.out.println(separator);
+                System.out.println("\tadded: " + command);
+                System.out.println(separator);
+                break;
+            }
+        } while (!isDone);
     }
 }
