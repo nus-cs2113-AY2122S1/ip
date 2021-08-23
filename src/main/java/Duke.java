32683c09b71710c7da267bd689f8c0c97e7e4574
Rebecca Chua <e0564171@u.nus.edu>
2021-08-23 21:56:06 +0800

    Add echo for user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+
+        Scanner s1 = new Scanner(System.in);
+        String input = s1.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = s1.nextLine();
+        }
 
         System.out.println("Bye. Hope to see you again soon!");
 
