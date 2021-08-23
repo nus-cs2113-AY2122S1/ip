8bd95af42910a4f646263d5445263dc04d6d6d52
tttyyzzz <tohyizhi9@gmail.com>
2021-08-24 00:21:02 +0800

    add greet class

---
+++ src/main/java/Greet.java
@@ -0,0 +1,11 @@
+public class Greet {
+    public static void miniDuke() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
