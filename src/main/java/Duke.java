a7ea184ecfc516669b882db0a48d0e02942aa4d1
Alvin.Avocados <alvinpang321@gmail.com>
2021-08-23 15:05:12 +0800

    Add Level-2: Add, List
    * Update userInput to trim excess whitespaces
    * Store user input in a list
    * Implement printList method in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static final String horizontalLine = "____________________________________________________________\n";
+
     public static void main(String[] args) {
 
         String logo = " ______    _   __   __            \n" +
@@ -11,29 +16,41 @@ public class Duke {
                 "|_______/[___][___][___][\\_:  /   \n" +
                 "                         \\__.'    \n";
 
-        String horizontalLine = "____________________________________________________________\n";
         String greetingMessage = horizontalLine +
                 " Hello! I'm Billy\n" +
                 " What can I do for you?\n" +
                 horizontalLine;
-
         String goodbyeMessage = horizontalLine +
                 " Bye. Hope to see you again soon!\n" +
                 horizontalLine;
+
         boolean programIsRunning = true;
         String userInput;
+        List<String> list = new ArrayList<String>();
         Scanner in = new Scanner(System.in);
         System.out.println("Hello from\n" + logo + greetingMessage);
 
-        while(programIsRunning) {
-            userInput = in.nextLine();
+        while (programIsRunning) {
+            userInput = in.nextLine().trim();
+
             if (userInput.toLowerCase().equals("bye")) {
                 System.out.println(goodbyeMessage);
                 programIsRunning = false;
+            } else if (userInput.toLowerCase().equals("list")) {
+                printList(list);
             } else {
-                System.out.println(horizontalLine + userInput + "\n" + horizontalLine);
+                list.add(userInput);
+                System.out.println(horizontalLine + "added: " + userInput + "\n" + horizontalLine);
             }
         }
 
+    }
+
+    public static void printList(List<String> list) {
+        System.out.println(horizontalLine);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i+1) + ": " + list.get(i));
+        }
+        System.out.println(horizontalLine);
     }
 }
