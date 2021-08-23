78b989e0591eb573db77446a0b84eca725c2f32a
shixi <fanshixi123@gmail.com>
2021-08-23 13:55:08 +0800

    Add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,11 @@
+import java.util.*;
+
 public class Duke {
+    public static void echo(String args) {
+        System.out.println(args);
+        System.out.println("_____________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,9 +18,19 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("_____________________________");
-        
-        /** exit **/
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_____________________________");
+
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String str = sc.nextLine();
+            if (str.equals("bye")) {
+                /** exit **/
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("_____________________________");
+                break;
+            } else {
+                echo(str);
+                continue;
+            }
+        }
     }
 }
