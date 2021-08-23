c462050f0299d5db1f102314c8abbc5a532b8513
kyun99 <limkayyun@gmail.com>
2021-08-23 20:24:13 +0800

    Add Echo response feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +9,12 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String request = in.nextLine();
+        while (!request.equals("bye")) {
+            System.out.println(request);
+            request = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
