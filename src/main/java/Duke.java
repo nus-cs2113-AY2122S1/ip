45ae75d9d66da7f7f81307e41c216485555722bf
alwinangys <alwinang99@gmail.com>
2021-08-23 22:01:06 +0800

    Add greet, echo, exit in main

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,24 @@
- public class Duke {
+import java.util.Scanner;
+
+public class Duke {
+
     public static void main(String[] args) {
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-
-        System.out.println(logo);
         System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("What can I do for you?");
+
+        String line;
+        boolean isBye = false;
+
+        Scanner in = new Scanner(System.in);
+        while (isBye == false) {
+            line = in.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                isBye = true;
+            } else {
+                System.out.println(line);
+            }
+        }
     }
 }
