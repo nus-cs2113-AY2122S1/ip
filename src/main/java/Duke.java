14b03aea74fb824ac5edcec56de711ba83e28a8d
pos0414 <gimins1202@gmail.com>
2021-08-24 01:33:40 +0900

    Add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,32 @@
 import javax.swing.*;
+import java.util.Scanner;
+
 public class Duke {
+    public static void echo(String line) {
+        String separator = "____________________________________________________________";
+        System.out.println(separator);
+        System.out.println(" " + line);
+        System.out.println(separator);
+    }
+
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        System.out.println(line);
+        String separator = "____________________________________________________________";
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(separator);
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
-        System.out.println(line);
+        System.out.println(separator);
+
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            echo(line);
+            line = in.nextLine();
+        }
+
+        System.out.println(separator);
         System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(separator);
     }
 }
