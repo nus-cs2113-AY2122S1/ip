f896d60d4be6bb8f1be46bb1c4f32ffeca95d10d
markuslyq <markuslyq@gmail.com>
2021-08-23 19:06:29 +0800

    Update Duke.java
    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,6 +11,17 @@ public class Duke {
 
         System.out.println("Hello I'm Duke");
         System.out.println("What can I do for you?");
+
+        String input = "";
+
+        do {
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+
+            if (!input.equals("bye")) {
+                System.out.println(input);
+            }
+        } while (!input.equals("bye"));
 
         System.out.println("Bye. Hope to see you again soon!");
     }
