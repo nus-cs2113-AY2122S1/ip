88fb6e8548a6968fb0a14d28c8c09694172af855
yanjia1777 <yanjia1777@gmail.com>
2021-08-23 23:01:43 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,17 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!(line.equals("bye"))){
+            System.out.println(line);
+            line = in.nextLine();
+        }
         System.out.println(" Bye. Hope to see you again soon!");
     }
 }
