512e46dc44bf304fa547af12d1454be3e4bdef6e
Alvin.Avocados <alvinpang321@gmail.com>
2021-08-23 18:04:57 +0800

    Add markTasksAsDone method in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -57,6 +57,45 @@ public class Duke {
     }
 
     /**
+     * Marks and prints given tasks as done. Accepts multiple tasks in one input and provides feedback
+     * if invalid inputs are detected.
+     *
+     * @param userInput String of user input containing task numbers to be marked as done.
+     * @param list List of tasks 
+     */
+    public static void markTasksAsDone(String userInput, List<Task> list) {
+        int[] tasksToMarkDone = extractInt(userInput);
+
+        // Error handling: if user inputs no numbers or task number 0
+        if (tasksToMarkDone[0] == 0 || tasksToMarkDone[0] == -1) {
+            System.out.println(horizontalLine +
+                    "Please provide me with a valid task to mark as done/not done!\n" +
+                    "Usage example:\n" +
+                    "-> Done 1, 2, 3\n" +
+                    "-> Done 1 2 3\n" +
+                    horizontalLine);
+            return;
+        }
+
+        System.out.println("Nice! Let me mark your given tasks as done:");
+
+        for (int taskNumber : tasksToMarkDone) {
+            if (taskNumber == 0) {
+                continue;
+            }
+
+            if (taskNumber >= list.size()) {
+                System.out.println("Oops! You've given be an invalid task number. Skipping...");
+                continue;
+            }
+            list.get(taskNumber - 1).markAsDone();
+            System.out.println("[" + list.get(taskNumber - 1).getStatusIcon() + "] " +
+                    list.get(taskNumber - 1).getDescription());
+        }
+        System.out.println("All done!\n" + horizontalLine);
+    }
+
+    /**
      * Extracts integers from a given String input and returns them in an array of Ints.
      * Method will extract integers separated by any character.
      * If there are no integers in the input, return -1 at the first element of the array.
