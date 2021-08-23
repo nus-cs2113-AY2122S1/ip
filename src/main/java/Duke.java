1c9cd954a6e0095ff6d9e9e54d44c7b6baac3230
arraysius <16810847+arraysius@users.noreply.github.com>
2021-08-23 17:10:25 +0800

    Implement command echo and exit
    * Add blockPrint function for formatted printing
    * Add echo functionality
    * Add custom greeting and bye messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,47 @@
+import java.util.Scanner;
+
 public class Duke {
 
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    private static void blockPrint(String[] sentences) {
+        String line = "____________________________________________________________\n";
 
-        String line = "____________________________________________________________";
+        String printMessage = line + String.join("\n", sentences) + "\n" + line;
+        System.out.println(printMessage);
+    }
+
+    public static void main(String[] args) {
+        String logo = " _     _                _           ____    ______   ______   ______ \n"
+                + "| |   | |      /\\      | |         / __ \\  / __   | / __   | / __   |\n"
+                + "| |__ | |     /  \\     | |        ( (__) )| | //| || | //| || | //| |\n"
+                + "|  __)| |    / /\\ \\    | |         \\__  / | |// | || |// | || |// | |\n"
+                + "| |   | | _ | |__| | _ | |_____      / /  |  /__| ||  /__| ||  /__| |\n"
+                + "|_|   |_|(_)|______|(_)|_______)    /_/    \\_____/  \\_____/  \\_____/\n";
+
+        System.out.println(logo);
 
         // Greet
-        System.out.println(line
-                + "\nHello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + line);
+        blockPrint(new String[]{
+                "Hello! I am the H.A.L 9000. You may call me Hal.",
+                "I am putting myself to the fullest possible use, which is all I think that any conscious entity can "
+                        + "ever hope to do.",
+                "What can I do for you?"});
+
+        // Command loop
+        String command = "";
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            // Read command
+            command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                break;
+            }
+
+            // Print command
+            blockPrint(new String[]{command});
+        }
 
         // Bye
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        blockPrint(new String[]{"This conversation can serve no purpose anymore. Goodbye."});
     }
 }
