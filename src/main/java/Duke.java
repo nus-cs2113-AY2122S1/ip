401403795e03ef609fd4e2db7b5a5cd793b6981e
Yap Joon Siong <yapjoonsiong@gmail.com>
2021-08-23 14:28:42 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,42 @@
+import javax.lang.model.type.NullType;
 import java.util.*;
 
+class Information {
+        String[] list = new String[100];
+        int listLength;
+}
+
 public class Duke {
-    public static void lineBreak() {
+    public static void line() {
         System.out.println("\t____________________________________________________________\n");
     }
-    public static void response(String input) {
-        System.out.println("\t" + input + "\n");
+    public static void response(String input, Information user) {
+        if (input.equals("list")) { // show list
+            for (int i = 0; i < user.listLength; i++) {
+                System.out.println("\t" + (i + 1) + ". " + user.list[i] + "\n");
+            }
+        } else if (input.substring(0,1).equals("+") || (input.length() > 3 && input.substring(0, 3).equals("add"))) { // add to list
+            if (input.substring(0, 1).equals("+")) {
+                user.list[user.listLength] = input.substring(2);
+                System.out.println("\tadded: " + input.substring(2) + "\n");
+            } else {
+                user.list[user.listLength] = input.substring(4);
+                System.out.println("\tadded: " + input.substring(4) + "\n");
+            }
+            user.listLength ++;
+        } else {
+            System.out.println("\t" + input + "\n");
+        }
+    }
+    public static void greet(String logo) {
+        System.out.println("Hello from\n" + logo);
+        line();
+        System.out.println("\tHello! I'm Duke\n" + "\tWhat can I do for you?\n"); // greet
+        line();
+    }
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!\n"); //exit message
+        line();
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -13,20 +44,20 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        lineBreak();
-        System.out.println("\tHello! I'm Duke\n" + "\tWhat can I do for you?\n"); // greet
-        lineBreak();
+        greet(logo);
+
+        Information user = new Information();
+        user.listLength = 0;
+
         Scanner sc= new Scanner(System.in);
         String input = sc.nextLine();
-        lineBreak();
-        while (!Objects.equals(input, "bye")) {
-            response(input);
-            lineBreak();
+        line();
+        while (!input.equals("bye")) {
+            response(input, user);
+            line();
             input = sc.nextLine();
-            lineBreak();
+            line();
         }
-        System.out.println("Bye. Hope to see you again soon!\n"); //exit message
-        lineBreak();
+        exit();
     }
 }
