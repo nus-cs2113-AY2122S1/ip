bcea81d9c2c1bda50b368ed7070ead95c6c9a1ca
CrimsonTitan79 <CrimsonTitan79>
2021-08-23 11:25:48 +0800

    Add Scanner class to echo user input, exit when bye. Add indentations

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
+
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "\t ____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
         System.out.println(logo);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________\n");
+
+        Scanner in = new Scanner(System.in);
+        String user_input = in.nextLine();
+        while (!user_input.equals("bye")) {
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + user_input);
+            System.out.println("\t____________________________________________________________");
+            user_input = in.nextLine();
+        }
+
+
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
     }
 }
