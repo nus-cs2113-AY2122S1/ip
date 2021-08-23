f48b107cf3416dddc63ce03495823f448690e34e
owensoh <owinsoh@gmail.com>
2021-08-23 15:02:24 +0800

    Add Level-1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static boolean done = false;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,19 +14,39 @@ public class Duke {
         System.out.println(lines());
         System.out.println(greeting());
         System.out.println(lines());
-        System.out.println(bye());
-        System.out.println(lines());
+
+        Scanner in = new Scanner(System.in);
+        while (!done) {
+            String input = in.nextLine();
+            parseInput(input);
+        }
+
+
+
     }
 
     public static String lines() {
-        return "____________________________________________________________";
+        return "    ____________________________________________________________";
     }
 
     public static String greeting() {
-        return "Hello! I'm Duke\nWhat can I do for you?";
+        return "    Hello! I'm Duke\n    What can I do for you?";
     }
 
     public static String bye() {
-        return "Bye. Hope to see you again soon!";
+        return "    Bye. Hope to see you again soon!";
+    }
+
+    private static void parseInput(String input) {
+        System.out.println(lines());
+        if (input.equalsIgnoreCase("Bye")) {
+            System.out.println(bye());
+            done = true;
+        }
+        else {
+            System.out.print("    ");
+            System.out.println(input);
+        }
+        System.out.println(lines());
     }
 }
