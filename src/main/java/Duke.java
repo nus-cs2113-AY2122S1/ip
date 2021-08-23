79244385d9bde94e178d03ddf4a7d8c66e4f6191
markuslyq <markuslyq@gmail.com>
2021-08-23 19:42:02 +0800

    Update Duke.java
    Added the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,22 +7,37 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
 
         System.out.println("Hello I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println("---------------------------------------------------------------------");
 
         String input = "";
+        int i = 0;
+        String[] tasks = new String[100];
 
         do {
             Scanner scanner = new Scanner(System.in);
             input = scanner.nextLine();
+            System.out.println("---------------------------------------------------------------------");
 
             if (!input.equals("bye")) {
-                System.out.println(input);
+                if (!input.equals("list")) {
+                    tasks[i] = input;
+                    System.out.println("added: " + input);
+                    System.out.println("---------------------------------------------------------------------");
+                    i++;
+                } else {
+                    for (int j = 0; j < i; j++){
+                        System.out.println(j+1 + ": " + tasks[j]);
+                    }
+                    System.out.println("---------------------------------------------------------------------");
+                }
             }
         } while (!input.equals("bye"));
 
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("---------------------------------------------------------------------");
     }
 }
