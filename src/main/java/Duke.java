4c32899eb3f12b48a4799803e3e9588c5a91e7a5
Li Xi Yuan <lixiyuan.owlsong@gmail.com>
2021-08-23 22:03:44 +0800

    Add level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void line(){
         System.out.println("____________________________________________________________");
     }
 
+    private static Boolean isFinished = false;
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         line();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         line();
+
+        while(!isFinished){
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")){
+                isFinished = true;
+                break;
+            }
+            line();
+            System.out.println(userInput);
+            line();
+        }
+
+        line();
         System.out.println("Bye. Hope to see you again soon!");
         line();
+
     }
 }
