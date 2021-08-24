edab04be23929f3b8fcb23a4d2636826100ff803
ChewyChair <69298017+ChewyChair@users.noreply.github.com>
2021-08-24 18:18:09 +0800

    Add exceptions and more error handling, abstract several more functions

--- src/main/java/Shika.java
+++ src/main/java/Shika.java
@@ -23,11 +23,11 @@ public class Shika {
                 + " /        \\|   Y  \\  |    <  / __ \\_\n"
                 + "/_______  /|___|  /__|__|_ \\(____  /\n"
                 + "        \\/      \\/        \\/     \\/ \n";
-        System.out.println(logo + "\nHello, friend! Shika at your service!\n");
+        System.out.println(logo + "\nHello, friend! Shika at your service! (`･ω･´)ゞ\n");
     }
 
     /**
-     * Function that calls scan() in a loop to run Shika. Loop can be exited by inputting "bye".
+     * Function that calls getCommand() in a loop to run Shika. Loop can be exited by inputting "bye".
      */
     public static void runShika() {
         Task[] taskList = new Task[100];
@@ -37,24 +37,14 @@ public class Shika {
         while(in.hasNextLine()) {
             text = in.nextLine();
             if (text.trim().equals("bye")) {
-                System.out.println(line + "> Bye friend!\n> See you again! :3\n" + line);
+                System.out.println(line + "> Bye friend!\n> See you again! (*・ω・)ﾉ\n" + line);
                 return;
             }
-            scan(taskList, text);
-        }
-    }
-
-    /**
-     * Function that input is an integer. It checks if value can be parsed by parseInt() without throwing an exception.
-     * @param input String to be parsed.
-     * @return Returns false if parseInt() throws an exception, returns true otherwise.
-     */
-    public static boolean isInteger(String input) {
-        try {
-            Integer.parseInt(input);
-            return true;
-        } catch (final NumberFormatException e) {
-            return false;
+            try {
+                getCommand(taskList, text);
+            } catch (InvalidCommandException e) {
+                System.out.println(line + "> Sorry friend, I don't know what that means. (´･ω･`)?\n" + line);
+            }
         }
     }
 
@@ -71,12 +61,13 @@ public class Shika {
      * Function that waits for user input, then executes user command.
      * "list" will list all current tasks.
      * "done x" will mark task x as done, where x is the number of the task.
-     * "todo", "deadline" or "event" will add the task.
+     * "todo", "deadline" or "event" will attempt to add the task.
      * Any other string will print an error message.
      * @param taskList Array containing all recorded tasks.
      * @param text String containing user input.
+     * @throws InvalidCommandException thrown when command is invalid.
      */
-    public static void scan(Task[] taskList, String text) {
+    public static void getCommand(Task[] taskList, String text) throws InvalidCommandException {
         text = text.trim();
         if (text.equals("list")) {
             printTasks(taskList);
@@ -85,13 +76,13 @@ public class Shika {
         } else if (isAddCommand(text)) {
             addTask(taskList, text);
         } else {
-            System.out.print(line + "Please enter a valid command.\n" + line);
+            throw new InvalidCommandException();
         }
     }
 
     /**
-     * This function adds the task specified by the user to the list. It checks for the category of the task
-     * as specified by the user and executes accordingly.
+     * This function attempts to add the task specified by the user to the list and catches exceptions if the input
+     * is invalid, printing error messages.
      * @param taskList Array containing all recorded tasks.
      * @param text String containing user input.
      */
@@ -99,9 +90,19 @@ public class Shika {
         if (text.startsWith("todo")) {
             addTodo(taskList, text);
         } else if (text.startsWith("deadline")) {
-            addDeadline(taskList, text);
+            try {
+                addDeadline(taskList, text);
+            } catch (InvalidDeadlineException e) {
+                System.out.print(line + "Please follow the format [NAME] /by [DEADLINE]. " +
+                        "Thank you! (。・ω・。)\n" + line);
+            }
         } else {
-            addEvent(taskList, text);
+            try {
+                addEvent(taskList, text);
+            } catch (InvalidEventException e) {
+                System.out.print(line + "Please follow the format [NAME] /at [DURATION]. " +
+                        "Thank you! (。・ω・。)\n" + line);
+            }
         }
     }
 
@@ -114,65 +115,81 @@ public class Shika {
         String str = text.substring(text.indexOf("todo") + 4).trim();
         taskList[Task.count] = new Todo(str);
         Task.count += 1;
-        System.out.println(line + "> Added: " + "\n\t" + Task.count + ". " + taskList[Task.count - 1].toString());
+        System.out.println(line + "> Added: (∩•̀ω•́)⊃-*⋆ " + "\n\t"
+                + Task.count + ". " + taskList[Task.count - 1].toString());
     }
 
     /**
      * This function adds the deadline specified by the user to the list.
      * @param taskList Array containing all recorded tasks.
      * @param text String containing user input.
+     * @throws InvalidDeadlineException is thrown when command syntax is not followed.
      */
-    public static void addDeadline(Task[] taskList, String text) {
+    public static void addDeadline(Task[] taskList, String text) throws InvalidDeadlineException  {
         if (!text.contains("/by")) {
-            System.out.print(line + "Please follow the format [NAME] /by [DEADLINE]. Thank you!\n" + line);
-            return;
+            throw new InvalidDeadlineException();
         }
         String str = text.substring(text.indexOf("deadline") + 8, text.indexOf("/")).trim();
         String by = text.substring(text.indexOf("/by") + 3).trim();
         taskList[Task.count] = new Deadline(str, by);
         Task.count += 1;
-        System.out.println(line + "> Added: " + "\n\t" + Task.count + ". " + taskList[Task.count - 1].toString());
+        System.out.println(line + "> Added: (∩•̀ω•́)⊃-*⋆" + "\n\t"
+                + Task.count + ". " + taskList[Task.count - 1].toString());
     }
 
     /**
      * This function adds the event specified by the user to the list.
      * @param taskList Array containing all recorded tasks.
      * @param text String containing user input.
+     * @throws InvalidEventException is thrown when command syntax is not followed.
      */
-    public static void addEvent(Task[] taskList, String text) {
+    public static void addEvent(Task[] taskList, String text) throws InvalidEventException {
         if (!text.contains("/at")) {
-            System.out.print(line + "Please follow the format [NAME] /at [DURATION]. Thank you!\n" + line);
-            return;
+            throw new InvalidEventException();
         }
         String str = text.substring(text.indexOf("event") + 5, text.indexOf("/")).trim();
         String at = text.substring(text.indexOf("/at") + 3).trim();
         taskList[Task.count] = new Event(str, at);
         Task.count += 1;
-        System.out.println(line + "> Added: " + "\n\t" + Task.count + ". " + taskList[Task.count - 1].toString());
+        System.out.println(line + "> Added: (∩•̀ω•́)⊃-*⋆" + "\n\t"
+                + Task.count + ". " + taskList[Task.count - 1].toString());
     }
 
     /**
-     * Function that marks the task with the input number as done.
-     * If the String given is not a number or is out of bounds, it will print an error message.
+     * Function that attempts to mark a task done by calling markAsDone. It prints error messages if any exceptions
+     * are caught from both parseInt and markAsDone.
+     * If the String given is not a number or is out of bounds, it will catch the exception and print an error message.
      * @param taskList Array containing all recorded tasks.
      * @param text String that is supposed to be the number of the task.
      */
     public static void doTask(Task[] taskList, String text) {
         String str = text.substring(text.indexOf("done") + 4).trim();
-        if (!isInteger(str)) {
-            System.out.print(line + "> Please key in a number :/\n" + line);
-            return;
+        try {
+            int index = Integer.parseInt(str);
+            markAsDone(taskList, index - 1);
+        } catch (NumberFormatException e) {
+            System.out.print(line + "> Please key in a number. (´･ω･`)?\n" + line);
+        } catch (TaskNegativeException e) {
+            System.out.print(line + "> ...Stop trying to break me... ( ; ω ; )\n" + line);
+        } catch (TaskNotFoundException e) {
+            System.out.print(line + "> Oopsie! That task does not exist... yet! (。・ω・。)\n" + line);
         }
-        int index = Integer.parseInt(str);
-        if (index > Task.count) {
-            System.out.print(line + "> Oopsie! That task does not exist... yet!\n" + line);
-        } else if (index < 1) {
-            System.out.print(line + "> ...Stop trying to break me :<\n" + line);
-        } else {
-            taskList[index - 1].markAsDone();
-            System.out.print(line + "> Otsukare! You've done:"
-                    + "\n\t" + index + ". " + taskList[index - 1].toString() + "\n" + line);
+    }
+
+    /**
+     * Function throws exceptions if the index of the task is invalid and marks task as done if it is valid.
+     * @param taskList Array containing all recorded tasks.
+     * @param index Index of the task to be marked as done.
+     * @throws TaskNegativeException If index is negative.
+     * @throws TaskNotFoundException If index is of a task that has not been created yet.
+     */
+    public static void markAsDone(Task[] taskList, int index) throws TaskNegativeException, TaskNotFoundException {
+        if (index < 0) {
+            throw new TaskNegativeException();
+        } else if (index >= Task.count) {
+            throw new TaskNotFoundException();
         }
+        taskList[index].markAsDone();
     }
 
     /**
@@ -180,7 +197,7 @@ public class Shika {
      * @param taskList Array containing all recorded tasks.
      */
     public static void printTasks(Task[] taskList) {
-        System.out.println(line + "> Here is your list of tasks:") ;
+        System.out.println(line + "> Here is your list of tasks: (∩•̀ω•́)⊃-*⋆") ;
         for (int i = 0; i < Task.count; i++) {
             System.out.println("\t" + (i + 1) + ". " + taskList[i].toString());
         }
---
+++ src/main/java/TaskNegativeException.java
@@ -0,0 +1,2 @@
+public class TaskNegativeException extends Exception{
+}
