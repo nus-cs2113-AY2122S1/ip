520b407ce784d18cbe105249655e8129fab85cfa
richwill28 <e0550368@u.nus.edu>
2021-08-24 11:25:30 +0800

    Minor customization and comply to coding standard
    - Minor customization (e.g., logo, name, greeting)
    - Add JavaDoc comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,63 +1,94 @@
 import java.util.Scanner;
 
-public class Duke {
+/**
+ * This class implements the Project Duke, a Personal
+ * Assistant Chatbot that helps a user to keep track
+ * of various things. Currently, Duke can greet user;
+ * add/list user tasks, and mark them as "done".
+ *
+ * @author richwill28
+ */
+class Duke {
+    /** Decorative logo */
     private static final String LOGO =
-            "      ____        _        \n"     +
-            "     |  _ \\ _   _| | _____ \n"    +
-            "     | | | | | | | |/ / _ \\\n"    +
-            "     | |_| | |_| |   <  __/\n"     +
-            "     |____/ \\__,_|_|\\_\\___|\n";
+            "     ,---.    ,---.   ____    .-./`)  ______           \n" +
+            "     |    \\  /    | .'  __ `. \\ .-.')|    _ `''.     \n" +
+            "     |  ,  \\/  ,  |/   '  \\  \\/ `-' \\| _ | ) _  \\ \n" +
+            "     |  |\\_   /|  ||___|  /  | `-'`\"`|( ''_'  ) |    \n" +
+            "     |  _( )_/ |  |   _.-`   | .---. | . (_) `. |      \n" +
+            "     | (_ o _) |  |.'   _    | |   | |(_    ._) '      \n" +
+            "     |  (_,_)  |  ||  _( )_  | |   | |  (_.\\.' /      \n" +
+            "     |  |      |  |\\ (_ o _) / |   | |       .'       \n" +
+            "     '--'      '--' '.(_,_).'  '---' '-----'`          \n";
 
+    /** Decorative line */
     private static final String LINE =
-            "    ____________________________________________________________" + "\n";
+            "    ____________________________________________________________\n";
 
+    /** Decorative padding */
     private static final String PADDING = "     ";
 
-    // Duke greeting
+    /**
+     * Displays logo and greets user.
+     */
     private static void greet() {
         System.out.print(LINE);
         System.out.println(LOGO);
-        System.out.println(PADDING + "Hello! I'm Duke");
+        System.out.println(PADDING + "Hello! I'm your personal maid. Call me Maid-chan!");
         System.out.println(PADDING + "What can I do for you?");
         System.out.println(LINE);
     }
 
-    // Duke exits
+    /**
+     * Says goodbye to user and exits the program.
+     */
     private static void exit() {
         System.out.print(LINE);
         System.out.println(PADDING + "Bye. Hope to see you again soon!");
         System.out.print(LINE);
     }
 
-    // An array of tasks (no more than 100)
+    /** An array of tasks (no more than 100) */
     private static Task[] tasks = new Task[100];
-    private static int taskNumber = 0;
 
-    // Add user command to the tasks array
+    /** The total number of tasks */
+    private static int totalNumberOfTasks = 0;
+
+    /**
+     * Adds user command to the tasks.
+     *
+     * @param command User command.
+     */
     private static void add(String command) {
-        tasks[taskNumber] = new Task(command);
-        taskNumber++;
+        tasks[totalNumberOfTasks] = new Task(command);
+        totalNumberOfTasks++;
         System.out.print(LINE);
         System.out.println(PADDING + "added: " + command);
         System.out.println(LINE);
     }
 
-    // List all the tasks
+    /**
+     * Lists all the tasks added.
+     */
     private static void list() {
         System.out.print(LINE);
-        for (int i = 1; i <= taskNumber; i++) {
+        for (int i = 1; i <= totalNumberOfTasks; i++) {
             System.out.println(PADDING + i + "." + tasks[i - 1]);
         }
         System.out.println(LINE);
     }
 
-    // Mark a task as done and display it
-    private static void done(int number) {
-        if (number <= taskNumber) {
-            tasks[number - 1].markAsDone();
+    /**
+     * Marks a task as "done" and displays it.
+     *
+     * @param taskNumber Task number.
+     */
+    private static void done(int taskNumber) {
+        if (taskNumber <= totalNumberOfTasks) {
+            tasks[taskNumber - 1].markAsDone();
             System.out.print(LINE);
             System.out.println(PADDING + "Nice! I've marked this task as done:");
-            System.out.println(PADDING + "  " + tasks[number - 1]);
+            System.out.println(PADDING + "  " + tasks[taskNumber - 1]);
             System.out.println(LINE);
         } else {
             System.out.println(LINE);
@@ -65,7 +96,12 @@ public class Duke {
         }
     }
 
-    // Duke main program
+    /**
+     * The main method of Duke which implements its
+     * chatting functionality.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         greet();
         while (true) {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,20 +1,50 @@
-public class Task {
+/**
+ * The Task class represents a task with a description
+ * and a status (whether it's done or not).
+ *
+ * @author richwill28
+ */
+class Task {
+    /** Description of the task */
     protected String description;
+
+    /** Status of the task (whether it's done or not) */
     protected boolean isDone;
 
+    /**
+     * The constructor method. Assigns the task
+     * description and set initial status to "not done".
+     *
+     * @param description Task description.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the status of the task, whether it's done
+     * or not.
+     *
+     * @return "X" if task is done, otherwise returns " ".
+     */
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        // mark done task with X
+        return (isDone ? "X" : " ");
     }
 
+    /**
+     * Marks the task as "done".
+     */
     public void markAsDone() {
-        this.isDone = true;
+        isDone = true;
     }
 
+    /**
+     * Returns the string representation of this task.
+     *
+     * @return The string representation of this task.
+     */
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
