278e2e93add29fa5d13e51f863035c6019cc3650
Jon-the-MELON <limyongkiat@yahoo.com.sg>
2021-08-24 15:47:45 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-//        String logo = " ____        _        \n"
+    public static void greeting(boolean isStart) {
+        if (isStart) {
+            //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-        String greeting = "____________________________________________________________\n" +
+            String greeting = "____________________________________________________________\n" +
                     " Hello! I'm Duke\n" +
                     " What can I do for you?\n" +
-                    "____________________________________________________________\n" +
-                    " Bye. Hope to see you again soon!\n" +
                     "____________________________________________________________\n";
-        System.out.println(greeting);
+            System.out.println(greeting);
+        }
     }
+
+    public static void echo(String x) {
+        String output = "____________________________________________________________\n" +
+                 x + "\n" +
+                "____________________________________________________________\n";
+        System.out.println(output);
+    }
+    public static void main(String[] args) {
+        greeting(true);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            echo(sc.nextLine());
+        }
+    }
+
 }
