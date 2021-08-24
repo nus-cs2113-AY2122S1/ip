32831d0a607b9776c0c38a97934d6fa24d8fc5ff
yanjia1777 <yanjia1777@gmail.com>
2021-08-24 22:35:39 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,26 +5,47 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
-        String line;
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int count = 0;
         Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        while(!(line.equals("bye"))){
-            if(line.equals("list")){
-                int num = 1;
-                for(String value : list){
-                    if(value==null){
+        Task task = new Task(in.nextLine());
+        task.index = count + 1;
+        String input = "null";
+        if(task.description.length() > 4) {
+            input = task.description.substring(0, 4);
+        }
+        while(!(task.description.equals("bye"))){
+            if(task.description.equals("list")){
+                for(Task value : list){
+                    System.out.println(value.index + ". " + "[" + value.getStatusIcon() + "] " + value.description);
+                    if(value.index == count){
                         break;
                     }
-                    System.out.println(num + ". " + value);
-                    num++;
                 }
+            }else if(input.equals("done")){
+                char num = task.description.charAt(5);
+                int numm = Character.getNumericValue(num);
+                System.out.println(list[0].description);
+                for(Task value : list){
+                    if(value.index==numm){
+                        value.markAsDone();
+                        System.out.println("Nice! I've marked this task as done: ");
+                        System.out.println("[" + value.getStatusIcon() + "] " + value.description);
+                        break;
+                    }else if(value.index == count){
+                        break;
+                    }
+                }
+            }else {
+                System.out.println("added: " + task.description);
+                list[count] = task;
+                count++;
             }
-            System.out.println("added: " + line);
-            list[count] = line;
-            count++;
-            line = in.nextLine();
+            task = new Task(in.nextLine());
+            task.index = count + 1;
+            if(task.description.length() > 4) {
+                input = task.description.substring(0, 4);
+            }
         }
 
         System.out.println(" Bye. Hope to see you again soon!");
---
+++ src/main/java/Task.java
@@ -0,0 +1,19 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    protected int index;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+}
