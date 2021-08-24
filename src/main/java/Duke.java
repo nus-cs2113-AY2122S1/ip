800ed78fa2191649f4e2e4c888dd59184f1d4f6b
JmattChiam <joelmchiam@gmail.com>
2021-08-24 12:19:26 +0800

    Added list-making, level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,6 +11,31 @@ public class Duke {
             line = in.nextLine();
         }
     }
+
+    public static void printList(String[] list, int listLength) {
+        for (int i = 0; i < listLength; i++) {
+            System.out.println((i+1) + ". " + list[i]);
+        }
+    }
+
+    public static void createList() {
+        String[] list = new String[100];
+        int listLength = 0;
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            if (line.equals("list")) {
+                printList(list, listLength);
+            } else {
+                list[listLength] = line;
+                listLength++;
+                System.out.println("added: " + line);
+            }
+            line = in.nextLine();
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -21,7 +46,7 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can i do for you?");
-        echoInput();
+        createList();
         System.out.println("Bye. Hope to see you again!");
     }
 }
