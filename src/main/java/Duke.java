a271b595fe6d4528001426ecdfe184a47219742b
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-08-24 19:21:59 +0800

    Level-1, Chat Bot parrots whatever user mention except bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Duke {
+	
+	public static void printParrotText(){
+        try {
+            // Note that the text file must be placed at the base java directory!
+            FileReader readParrot = new FileReader("ParrotText.txt");
+            int singleCharacters;
+
+            while ((singleCharacters = readParrot.read()) != -1) {
+                // Print each character individually.
+                System.out.print((char) singleCharacters);
+            }
+            readParrot.close();
+        } catch (IOException except) {
+            except.printStackTrace();
+        }
+
+        System.out.println("");
+	}
+	
     public static void main(String[] args) {
+        String userInputString;
+        Scanner userInput = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,9 +36,24 @@ public class Duke {
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Duke\n" +
-                " What can I do for you?");
+                " What can I do for you? c(＞ω＜)ゞ");
+        System.out.println("I'm currently in parrot mode! ");
+
+        printParrotText();
+
+        while (true) {
+            userInputString = userInput.nextLine();
+
+            if (userInputString.equals("bye")) {
+                break;
+            }
+
+            System.out.print(" ");
+            System.out.println(userInputString);
+        }
+
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(" Bye. Hope to see you again soon!ヾ(´￢｀)ﾉ");
         System.out.println("____________________________________________________________");
     }
 }
