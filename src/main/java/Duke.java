2b6b1d935f5d24b57ab5bd0db0d65141d780f9e4
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-08-24 16:03:47 +0800

    Add support for storing and displaying a list of items

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static final String HORIZONTAL_LINE = "    ____________________________________________________________";
+    private static final String INDENTED_HORIZONTAL_LINE = "    ____________________________________________________________";
+    private static final String[] items = new String[100];
+    private static int itemCount = 0;
 
     private static String indent(String text) {
         String[] lines = text.split(System.lineSeparator());
@@ -12,37 +14,58 @@ public class Duke {
     }
 
     private static void printTextWithHorizontalLineAndIndentation(String text) {
-        System.out.println(HORIZONTAL_LINE);
+        System.out.println(INDENTED_HORIZONTAL_LINE);
         System.out.println(indent(text));
-        System.out.println(HORIZONTAL_LINE);
+        System.out.println(INDENTED_HORIZONTAL_LINE);
         System.out.println();
     }
 
-    private static void greet() {
+    private static void printGreeting() {
         String greeting = "Hello! I'm Duke" + System.lineSeparator()
                 + "What can I do for you?";
         printTextWithHorizontalLineAndIndentation(greeting);
     }
 
-    private static void exit() {
+    private static void printItems() {
+        String[] itemsWithNumbers = new String[itemCount];
+        for (int i = 0; i < itemCount; i++) {
+            itemsWithNumbers[i] = (i + 1) + ". " + items[i];
+        }
+        String formattedItems = String.join(System.lineSeparator(), itemsWithNumbers);
+        printTextWithHorizontalLineAndIndentation(formattedItems);
+    }
+
+    private static void addItem(String item) {
+        items[itemCount] = item;
+        itemCount++;
+        printTextWithHorizontalLineAndIndentation("added: " + item);
+    }
+
+    private static void handleCommands() {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String command = in.nextLine();
+            switch (command) {
+            case "list":
+                printItems();
+                break;
+            case "bye":
+                return;
+            default:
+                addItem(command);
+                break;
+            }
+        }
+    }
+
+    private static void printFarewell() {
         String farewell = "Bye. Hope to see you again soon!";
         printTextWithHorizontalLineAndIndentation(farewell);
     }
 
-    private static void echoCommands() {
-        Scanner in = new Scanner(System.in);
-        while (true) {
-            String command = in.nextLine();
-            if (command.equals("bye")) {
-                break;
-            }
-            printTextWithHorizontalLineAndIndentation(command);
-        }
-    }
-
     public static void main(String[] args) {
-        greet();
-        echoCommands();
-        exit();
+        printGreeting();
+        handleCommands();
+        printFarewell();
     }
 }
