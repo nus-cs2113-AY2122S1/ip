3b6aef1d7d533480fead9853b92c557b4187c203
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-08-24 14:23:50 +0800

    Level-1
    The chat bot echos commands typed by the user.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
+
 public class Duke {
-    public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?\n");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+    public static void printMessage(String text) {
+        System.out.println("\n____________________________________________________________");
+        System.out.println(text);
+        System.out.println("____________________________________________________________\n");
+    }
+
+    public static void printLine() {
         System.out.println("____________________________________________________________");
     }
-}
 
+    public static void main(String[] args) {
+        printLine();
+        System.out.println("Howdy there! I'm Fluke");
+        System.out.println("What can I do for you today master?");
+        printLine();
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        while (!message.equals("bye")) {
+            printMessage(message);
+            message = scanner.nextLine();
+        }
+        printMessage("Bye. Hope to serve you again master!");
+        scanner.close();
+    }
+}
