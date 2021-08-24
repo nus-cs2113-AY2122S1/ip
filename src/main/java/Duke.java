4885a39f2fb7570cd1715cefbb546ad6b7dc9da2
Kureans <leonardtengys@gmail.com>
2021-08-25 06:10:06 +0800

    Added ability to mark tasks as done Changed Greeting Banner from Duke to Lizzy

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,85 +2,82 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static void greeting() {
-        line();
+    public static void printGreeting(LizTextBanner liz) {
+        System.out.println("Howdy! It's\n" + liz.getLizText() + liz.getLizLogo());
+        printLine();
         System.out.println("Hey! I'm Lizzy the Lizard!");
         System.out.println("What can I do for you?");
         System.out.println("");
     }
 
-    public static void exit() {
-        line();
+    public static void printExit() {
+        printLine();
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static void line() {
+    public static void printLine() {
         for (int i = 0; i < 40; i++) {
             System.out.print("_");
         }
         System.out.println("");
     }
 
-    /*
-    Function that handles task tracking, task display and exit. Boolean return value
-    tells Main whether to continue execution of the program, or to exit.
-     */
-    public static void addTask(Scanner in) {
+    public static void printTaskErrorMessage() {
+        System.out.println("Sorry bud, you can't check off what is not yet there :/");
+    }
 
-        String tasks[] = new String[100];
-        int taskIndex = 0;
-        String line = in.nextLine();
-        while (!line.equalsIgnoreCase("bye")) {
-            line();
-            if (line.equalsIgnoreCase("list")) {
-                for (int i = 0; i < taskIndex; i++) {
-                    System.out.printf("%d. %s%n", i+1, tasks[i]);
-                }
-            } else {
-                tasks[taskIndex] = line;
-                System.out.println("Added: " + tasks[taskIndex]);
-                taskIndex++;
-            }
-            line();
-            line = in.nextLine();
+    public static void printList(int taskIndex, Task tasks[]) {
+        for (int i = 0; i < taskIndex; i++) {
+            System.out.printf("%d.[%s] %s%n", i+1, tasks[i].getStatusIcon(), tasks[i].getDescription());
         }
+    }
 
-        exit();
+    public static void addTask(String description, int taskIndex, Task tasks[]) {
+        tasks[taskIndex] = new Task(description);
+        System.out.println("Added: " + tasks[taskIndex].getDescription());
+    }
+
+    public static void markAsDone(Task task) {
+        if (task.getStatusIcon().equals("X")) {
+            System.out.println("Slow down there bud! You've already completed this task!");
+        } else {
+            task.setDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("[X] " + task.getDescription());
+        }
     }
 
     public static void main(String[] args) {
 
-        String lizText = "      ___                   ___           ___           ___     \n" +
-                "     /\\__\\      ___        /\\  \\         /\\  \\         |\\__\\    \n" +
-                "    /:/  /     /\\  \\       \\:\\  \\        \\:\\  \\        |:|  |   \n" +
-                "   /:/  /      \\:\\  \\       \\:\\  \\        \\:\\  \\       |:|  |   \n" +
-                "  /:/  /       /::\\__\\       \\:\\  \\        \\:\\  \\      |:|__|__ \n" +
-                " /:/__/     __/:/\\/__/ _______\\:\\__\\ _______\\:\\__\\     /::::\\__\\\n" +
-                " \\:\\  \\    /\\/:/  /    \\::::::::/__/ \\::::::::/__/    /:/~~/~   \n" +
-                "  \\:\\  \\   \\::/__/      \\:\\~~\\~~      \\:\\~~\\~~       /:/  /     \n" +
-                "   \\:\\  \\   \\:\\__\\       \\:\\  \\        \\:\\  \\        \\/__/      \n" +
-                "    \\:\\__\\   \\/__/        \\:\\__\\        \\:\\__\\                  \n" +
-                "     \\/__/                 \\/__/         \\/__/                  \n";
-
-        String lizLogo = "                      ____...---...___\n" +
-                "___.....---\"\"\"                .                   \"\"--..____\n" +
-                "     .                  .            .\n" +
-                " .             _.--._       /|\n" +
-                "        .    .'()..()`.    / /\n" +
-                "            ( `-.__.-' )  ( (    .\n" +
-                "   .         \\        /    \\ \\\n" +
-                "       .      \\      /      ) )        .\n" +
-                "            .' -.__.- `.-.-'_.'\n" +
-                " .        .'  /-____-\\  `.-'       .\n" +
-                "          \\  /-.____.-\\  /-.\n" +
-                "           \\ \\`-.__.-'/ /\\|\\|           .\n" +
-                "          .'  `.    .'  `.\n" +
-                "          |/\\/\\|    |/\\/\\|";
-        System.out.println("Howdy! It's\n" + lizText + lizLogo);
+        LizTextBanner liz = new LizTextBanner();
         Scanner in = new Scanner(System.in);
-        greeting();
-        addTask(in);
 
+        Task tasks[] = new Task[100];
+        int taskIndex = 0;
 
+        printGreeting(liz);
+        String line = in.nextLine();
+        while (!line.equalsIgnoreCase("bye")) {
+            printLine();
+            if (line.equalsIgnoreCase("list")) {
+                printList(taskIndex, tasks);
+            } else {
+                String[] lineArgs = line.split(" ");
+                if (lineArgs.length == 2 && lineArgs[0].equalsIgnoreCase("done")) {
+                    int doneIndex = Integer.parseInt(lineArgs[1]) - 1;
+                    if (doneIndex >= taskIndex || doneIndex < 0) {
+                        printTaskErrorMessage();
+                    } else {
+                        markAsDone(tasks[doneIndex]);
+                    }
+                } else {
+                    addTask(line, taskIndex, tasks);
+                    taskIndex++;
+                }
+            }
+            printLine();
+            line = in.nextLine();
+        }
+        printExit();
     }
 }
