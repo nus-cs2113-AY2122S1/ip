acfa7d07e4f77dd3a9fcec844e857bdc8333a6a4
EdwardZYWang <wangzy.edward@u.nus.edu>
2021-08-25 02:31:36 +0800

    Add greet, echo, exit. level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!\n");
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+
+        //beginning of level-1
+        line = in.nextLine();
+        while (!(line.equals("bye"))) {
+            System.out.println("    " + line);
+            line = in.nextLine();
+        }
+        System.out.println("    " + "Bye. Hope to see you again soon!");
 
     }
 }
