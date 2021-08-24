3392e9c099f210cb599f0c9111ad154255f71b6f
giterator <pranavvenkatram@gmail.com>
2021-08-24 15:31:58 +0800

    Comply with naming and formatting standards

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,25 +1,25 @@
 public class Storage {
-    private static final String[] inputText = new String[100];
-    private static int inputTextSize = 0;
+    private static final String[] inputTasks = new String[100];
+    private static int inputTasksSize = 0;
 
     public static void storeInput(String input) {
-        inputText[inputTextSize] = "[ ] " + input;
-        inputTextSize++;
+        inputTasks[inputTasksSize] = "[ ] " + input;
+        inputTasksSize++;
         Response.echo("added: " + input);
     }
 
     public static void list() {
         System.out.println(Response.getLine());
 
-        for (int i = 0; i < inputTextSize; i++) {
-            System.out.println((i + 1) + ". " + inputText[i]);
+        for (int i = 0; i < inputTasksSize; i++) {
+            System.out.println((i + 1) + ". " + inputTasks[i]);
         }
 
         System.out.println(Response.getLine());
     }
 
-    public static void markComplete(int completedTask){
-        inputText[completedTask - 1] = "[X" + inputText[completedTask - 1].substring(2);
-        Response.echo("Nice! I've marked this task as done: \n" + inputText[completedTask - 1]);
+    public static void markComplete(int completedTask) {
+        inputTasks[completedTask - 1] = "[X" + inputTasks[completedTask - 1].substring(2);
+        Response.echo("Nice! I've marked this task as done: \n" + inputTasks[completedTask - 1]);
     }
 }
