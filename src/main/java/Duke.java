0de4c01370aaa33847db8ca7f0f86c68ee3e23db
Alvinlj00 <limjunalvin@gmail.com>
2021-08-24 20:18:18 +0800

    Add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,30 +2,45 @@ import java.util.Scanner;
 
 public class Duke {
 
+    public static String horizontal = ("____________________________________________________________\n");
+
     public static void sayHello() {
-        String horizontal = ("____________________________________________________________\n");
         System.out.println(horizontal + "Hello! I'm Duke\n" + "What can I do for you?\n" + horizontal);
     }
 
     public static void sayBye() {
-        String horizontal = ("____________________________________________________________\n");
         System.out.println(horizontal + "Bye. Hope to see you again soon!\n" + horizontal);
     }
 
-    public static void echo() {
-        String line;
-        Scanner in = new Scanner(System.in);
-        String horizontal = ("____________________________________________________________\n");
-        line = in.nextLine();
-        while (!line.equals("bye")) {
-            System.out.println(horizontal + line + "\n" + horizontal);
-            line = in.nextLine();
+    public static void addTask(String task, String[] taskList, int numberOfTasks) {
+        System.out.println(horizontal + "added: " + task + "\n" + horizontal);
+        taskList[numberOfTasks] = task;
+    }
+
+    public static void listTasks(String[] taskList, int numberOfTasks) {
+        System.out.print(horizontal);
+        for (int i = 1; i <= numberOfTasks; i++) {
+            System.out.println(i + ". " + taskList[i - 1]);
         }
-        sayBye();
+        System.out.println(horizontal);
     }
 
     public static void main(String[] args) {
+        String[] taskList = new String[100];
+        int numberOfTasks = 0;
         sayHello();
-        echo();
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            if (line.equals("list")) {
+                listTasks(taskList, numberOfTasks);
+            } else {
+                addTask(line, taskList, numberOfTasks);
+                numberOfTasks++;
+            }
+            line = in.nextLine();
+        }
+        sayBye();
     }
 }
