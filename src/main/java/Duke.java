ad2a32f621b9108e10308ae42d7faee7111d9392
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-08-24 22:44:56 +0800

    Level-3. Added "mark task as done" functionality & comments. Move some code from main method to some other method.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,59 @@
 import java.util.Scanner;
-
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Duke {
-	
+    /* Task Counter */
+    public static int numberOfTasks = 0;
+
+    /* This is an array of task & Total task quantity should not exceed 100! */
+    public static Task[] tasks = new Task[100];
+
+    /**
+     * Prints a "Hi" message when the user first initialize the chat bot.
+     */
+    public static void printHelloText() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+
+        printParrotText();
+
+        System.out.println(" Hello! I'm Parrot Boi\n" +
+                " What can I not do for you (I'm Lazy)? c(＞ω＜)ゞ");
+        System.out.println(" I'm currently in parrot mode! ");
+    }
+
+    /**
+     * Prints a "bye" message when the user input "bye" into the chat bot.
+     */
+    public static void printByeText() {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to not see you again soon!ヾ(´￢｀)ﾉ");
+        System.out.println("____________________________________________________________");
+    }
+
+    /**
+     * Reads a .txt file in ASCII format and then print every single character (converted
+     * ASCII index to characters) inside that .txt file onto the terminal. It also looks for
+     * exception and print it out if any.
+     *
+     * @throws IOException Trying to read the .txt file which user do not have permission or a
+     * .txt file that does not exist.
+     */
 	public static void printParrotText(){
         try {
-            // Note that the text file must be placed at the base java directory!
+            /**
+             * Note that the text file must be placed at the base java directory,
+             * and the name of the text file is placed here!
+             */
             FileReader readParrot = new FileReader("ParrotText.txt");
+
             int singleCharacters;
 
             while ((singleCharacters = readParrot.read()) != -1) {
@@ -23,32 +68,47 @@ public class Duke {
         System.out.println("");
 	}
 
-    public static void printStoreList(String[] storeList, int len){
-	    for (int i = 0; i < len; i += 1) {
-            System.out.print((i + 1) + ". ");
-            System.out.println(storeList[i]);
+    /**
+     * Adds a new task with the task name provided.
+     */
+    public static void printTaskList(){
+	    for (int i = 0; i < numberOfTasks; i += 1) {
+            System.out.print((i + 1) + ".");
+            tasks[i].printStatus();
         }
     }
 
+    /**
+     * Adds a new task with the task name provided.
+     *
+     * @param taskName Name of the new task.
+     */
+    public static void addTask(String taskName) {
+        tasks[numberOfTasks] = new Task(taskName);
+        numberOfTasks += 1;
+    }
+
+    /**
+     * Marks a certain task based on task number as finished.
+     *
+     * @param taskNumber X coordinate of position..
+     */
+    public static void finishTask(int taskNumber) {
+        if (taskNumber <= numberOfTasks) {
+            tasks[taskNumber - 1].markAsDone();
+        } else {
+            System.out.println("Please Enter the Legit Task Number... (ง'̀-'́)ง");
+        }
+    }
+
+    /**
+     * Main method of the chat bot app.
+     */
     public static void main(String[] args) {
         String userInputString;
         Scanner userInput = new Scanner(System.in);
-        String[] storeList = new String[100];
-        int storeListQuantity = 0;
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke\n" +
-                " What can I do for you? c(＞ω＜)ゞ");
-        System.out.println("I'm currently in parrot mode! ");
-
-        printParrotText();
+        printHelloText();
 
         while (true) {
             userInputString = userInput.nextLine();
@@ -56,20 +116,16 @@ public class Duke {
             if (userInputString.equals("bye")) {
                 break;
             } else if (userInputString.equals("list")) {
-                printStoreList(storeList, storeListQuantity);
+                printTaskList();
                 continue;
+            } else if (userInputString.split(" ")[0].equals("done")) {
+                finishTask(Integer.parseInt(userInputString.split(" ")[1]));
+            } else {
+                addTask(userInputString);
+                System.out.println("added: " + userInputString);
             }
-
-            storeList[storeListQuantity] = userInputString;
-            System.out.println("added: " + userInputString);
-            storeListQuantity += 1;
-
-            // System.out.print(" ");
-            // System.out.println(userInputString);
         }
 
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!ヾ(´￢｀)ﾉ");
-        System.out.println("____________________________________________________________");
+        printByeText();
     }
 }
