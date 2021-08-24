87524cf61632018d653c9dd9523063691a9e4521
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-08-24 21:49:02 +0800

    Add Javadoc comments and conform to coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 
+/**
+ * A personal assistant chatbot.
+ */
 public class Duke {
-    private static final String INDENTED_HORIZONTAL_LINE = "    ____________________________________________________________";
+    private static final String INDENTED_HORIZONTAL_LINE = " ".repeat(4) + "_".repeat(60);
+    /** Array of Task objects */
     private static final Task[] tasks = new Task[100];
+    /** Number of tasks */
     private static int taskCount = 0;
 
     private static String indent(String text) {
@@ -13,7 +18,13 @@ public class Duke {
         return String.join(System.lineSeparator(), lines);
     }
 
-    private static void printTextWithHorizontalLineAndIndentation(String text) {
+    /**
+     * Prints out the specified text formatted as a response block.
+     * Horizontal lines will be printed before and after the specified text, and the text will be indented.
+     *
+     * @param text Text to be printed out.
+     */
+    private static void printResponseBlock(String text) {
         System.out.println(INDENTED_HORIZONTAL_LINE);
         System.out.println(indent(text));
         System.out.println(INDENTED_HORIZONTAL_LINE);
@@ -23,34 +34,39 @@ public class Duke {
     private static void printGreeting() {
         String greeting = "Hello! I'm Duke" + System.lineSeparator()
                 + "What can I do for you?";
-        printTextWithHorizontalLineAndIndentation(greeting);
+        printResponseBlock(greeting);
     }
 
+    /** Prints out the list of tasks (numbered) together with their status icons */
     private static void printTasks() {
-        String[] tasksWithNumbersAndStatusIcon = new String[taskCount];
+        String[] tasksWithNumbersAndStatusIcons = new String[taskCount];
         for (int i = 0; i < taskCount; i++) {
-            tasksWithNumbersAndStatusIcon[i] = String.format("%d.[%s] %s", i + 1, tasks[i].getStatusIcon(),
+            tasksWithNumbersAndStatusIcons[i] = String.format("%d.[%s] %s", i + 1, tasks[i].getStatusIcon(),
                     tasks[i].getDescription());
         }
-        String formattedTaskList = String.join(System.lineSeparator(), tasksWithNumbersAndStatusIcon);
-        printTextWithHorizontalLineAndIndentation("Here are the tasks in your list:" + System.lineSeparator()
+        String formattedTaskList = String.join(System.lineSeparator(), tasksWithNumbersAndStatusIcons);
+        printResponseBlock("Here are the tasks in your list:" + System.lineSeparator()
                 + formattedTaskList);
     }
 
     private static void markTaskAsDone(int taskId) {
-        tasks[taskId].setAsDone();
-        String formattedTask = String.format("  [%s] %s", tasks[taskId].getStatusIcon(),
-                tasks[taskId].getDescription());
-        printTextWithHorizontalLineAndIndentation("Nice! I've marked this task as done:" + System.lineSeparator() +
-                formattedTask);
+        Task task = tasks[taskId];
+        task.setAsDone();
+        String formattedTask = String.format("  [%s] %s", task.getStatusIcon(), task.getDescription());
+        printResponseBlock("Nice! I've marked this task as done:" + System.lineSeparator()
+                + formattedTask);
     }
 
     private static void addTask(String description) {
         tasks[taskCount] = new Task(description);
         taskCount++;
-        printTextWithHorizontalLineAndIndentation("added: " + description);
+        printResponseBlock("added: " + description);
     }
 
+    /**
+     * Reads input commands from the user and executes the appropriate actions.
+     * Stops waiting for user input and returns upon receiving the "bye" command.
+     */
     private static void handleCommands() {
         Scanner in = new Scanner(System.in);
         while (true) {
@@ -75,7 +91,7 @@ public class Duke {
 
     private static void printFarewell() {
         String farewell = "Bye. Hope to see you again soon!";
-        printTextWithHorizontalLineAndIndentation(farewell);
+        printResponseBlock(farewell);
     }
 
     public static void main(String[] args) {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,20 +1,41 @@
+/**
+ * Represents a task.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Creates a task with the specified description.
+     * By default, task is not yet completed.
+     *
+     * @param description Description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon for the task (based on whether it is completed).
+     * The icon is a String of length 1 ("X" if completed, or " " if not).
+     *
+     * @return Status icon for the task.
+     */
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "X" : " ");
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return Description of the task.
+     */
     public String getDescription() {
         return description;
     }
 
+    /** Marks task as completed */
     public void setAsDone() {
         isDone = true;
     }
