71730a100697d58526b47703f62d75ea4eec1a54
Wai Kit Lau <waikit.lau98@gmail.com>
2021-08-24 20:30:37 +0800

    Level2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] savedMessages= new String[100];
+        int savedMessagesArrayCnter = 1;
         String welcomeMessage = "______________________________\n"
                 + "Hello! I'm Friday\n"
                 + "What can I do for you?\n"
@@ -13,11 +15,25 @@ public class Duke {
         System.out.print(welcomeMessage);
         String userInput = sc.nextLine();
         while(!"bye".equals(userInput)){
-            System.out.println("______________________________\n");
-            System.out.println(userInput + "\n");
-            System.out.println("______________________________\n");
+            if("list".equals(userInput)) {
+                System.out.println("______________________________\n");
+                for(int i = 1; i < savedMessagesArrayCnter; i++)
+                {
+                    System.out.print(i + " : ");
+                    System.out.print(savedMessages[i] + "\n");
+                }
+                System.out.println("______________________________\n");
+            } else {
+                savedMessages[savedMessagesArrayCnter] = userInput;
+                savedMessagesArrayCnter++;
+                System.out.println("______________________________\n");
+                System.out.println("added " + userInput + "\n");
+                System.out.println("______________________________\n");
+            }
             userInput = sc.nextLine();
         }
-        System.out.print(goodbyeMessage);
+        if("bye".equals(userInput)) {
+            System.out.print(goodbyeMessage);
+        }
     }
 }
