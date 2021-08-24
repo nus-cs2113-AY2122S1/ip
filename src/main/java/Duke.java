e49bc1d8e20ececb827ed3ec42b072ddc549e626
madhanse <madhan132000@gmail.com>
2021-08-24 17:27:23 +0800

    Adding and displaying the items in the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,8 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n"
                           + "What can I do for you?");
         String line;
+        String[] tasks = new String[100];
+        int count = 0;
         do {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
@@ -17,8 +19,21 @@ public class Duke {
             if (line.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("___________________SHUTTING DOWN___________________");
+            } else if (line.equals("list")) {
+                int i;
+                if (count == 0) {
+                    System.out.println("No items were added into the list.");
+                } else {
+                    for (i = 0; i < count; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+                System.out.println("___________________TASK COMPLETED__________________");
+                System.out.println("Anything else?");
             } else {
-                System.out.println(line);
+                tasks[count] = line;
+                count++;
+                System.out.println("\"" + line + "\" added into the list.");
                 System.out.println("___________________TASK COMPLETED__________________");
                 System.out.println("Anything else?");
             }
