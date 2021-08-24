5ff4390a6185993d85d3790a86ef678c4a5ecec0
Shi Zhansen <71874189+DuckWillow@users.noreply.github.com>
2021-08-24 23:20:49 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
                 + "Hello! I'm Duke\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
+
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(true){
+            switch (input){
+                case "list":
+                    logo = "____________________________________________________________\n"
+                    + "list\n"
+                    + "____________________________________________________________\n";
+                    System.out.println(logo);
+                    break;
+                case "blah":
+                    logo ="____________________________________________________________\n"
+                            + "blah\n"
+                            + "____________________________________________________________\n";
+                    System.out.println(logo);
+                    break;
+                case "bye":
+                    logo = "____________________________________________________________\n"
+                            + "Bye. Hope to see you again soon!\n"
+                            + "____________________________________________________________\n";
+                    System.out.println(logo);
+                    break;
+                default:
+                    break;
+            }
+            input = sc.nextLine();
+        }
     }
 }
