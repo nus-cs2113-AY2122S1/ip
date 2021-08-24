dec5bd39b960756ef428f7e646b9024481787b5d
Alvinlj00 <limjunalvin@gmail.com>
2021-08-24 19:45:14 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    public static void sayHello() {
         String horizontal = ("____________________________________________________________\n");
-        System.out.println(horizontal + "Hello! I'm Duke\n" + "What can I do for you?\n"
-                + horizontal + "Bye. Hope to see you again soon!\n" + horizontal);
+        System.out.println(horizontal + "Hello! I'm Duke\n" + "What can I do for you?\n" + horizontal);
+    }
+
+    public static void sayBye() {
+        String horizontal = ("____________________________________________________________\n");
+        System.out.println(horizontal + "Bye. Hope to see you again soon!\n" + horizontal);
+    }
+
+    public static void echo() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        String horizontal = ("____________________________________________________________\n");
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(horizontal + line + "\n" + horizontal);
+            line = in.nextLine();
+        }
+        sayBye();
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+        echo();
     }
 }
