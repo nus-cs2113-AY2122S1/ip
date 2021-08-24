16c38cbb0b493a6a0beb93709bd5f30e2d081581
Sean Phang <e0540763@u.nus.edu>
2021-08-24 22:20:08 +0800

    Add Greet, Echo, Exit
    - Customized chatbot
    - Implement echo of user commands
    - Implement greeting and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,59 @@
+import java.util.Scanner;
+
 public class Duke {
+    /* Logo - generated using https://loveeevee.github.io/Dots-Converter/ */
+    private static final String LOGO =  "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣀⣀⣀⡀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣀⣤⣤⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣤⣤⣀⣀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣀⣤⣶⣾⣿⣿⣿⣿⣿⣿⠿⠟⠛⠉⢉⣿⣿⣯⠉⠉⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⡀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣠⣴⣿⣿⡿⠟⢋⣿⣿⣿⠟⠋⢀⢀⢀⢀⢀⣾⠟⢀⢻⣧⡀⢀⢀⢀⢀⠙⠻⣿⣿⣯⡉⠛⠿⣿⣿⣷⣦⣀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⢀⢀⢀⣠⣴⣿⣿⠟⠋⠁⢀⣴⣿⡿⠋⢀⢀⢀⢀⢀⢀⢠⣾⠋⢀⢀⢀⠹⣷⡀⢀⢀⢀⢀⢀⠈⠻⣿⣿⣦⢀⢀⠉⠻⢿⣿⣷⣦⡀⢀⢀⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⢀⣴⣾⣿⡿⠋⠁⢀⢀⢠⣾⣿⠟⠁⢀⢀⢀⢀⢀⢀⣰⡿⠁⢀⢀⢀⢀⢀⠘⢿⣄⢀⢀⢀⢀⢀⢀⠈⠻⣿⣷⡀⢀⢀⢀⠉⠻⣿⣿⣦⡀⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⣴⣿⣿⡿⠋⢀⢀⢀⢀⢠⣿⣿⠋⢀⢀⢀⢀⢀⢀⢀⣼⡟⠁⢀⢀⢀⢀⢀⢀⢀⠈⢿⣦⢀⢀⢀⢀⢀⢀⢀⠹⣿⣷⢀⢀⢀⢀⢀⠈⠻⣿⣿⣦⡀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⣠⣾⣿⡿⠋⢀⢀⢀⢀⢀⢀⣾⣿⡏⢀⢀⢀⢀⢀⢀⢀⣾⠟⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢻⣧⡀⢀⢀⢀⢀⢀⢀⢹⣿⣧⢀⢀⢀⢀⢀⢀⠈⠻⣿⣿⣄⢀⢀⢀⢀\n"
+            + "⢀⢀⣴⣿⣿⠋⢀⢀⢀⢀⢀⢀⢀⢰⣿⣿⢀⢀⢀⢀⢀⢀⢠⣿⠋⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠹⣷⡄⢀⢀⢀⢀⢀⢀⣿⣿⡄⢀⢀⢀⢀⢀⢀⢀⠘⢿⣿⣧⡀⢀⢀\n"
+            + "⢀⣾⣿⡟⠁⢀⢀⢀⢀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⢀⣰⡿⠃⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠙⣿⣄⢀⢀⢀⢀⢀⢻⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢻⣿⣷⣄⢀\n"
+            + "⠹⣿⣿⣆⢀⢀⢀⢀⢀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣴⡟⠁⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠈⢿⣆⢀⢀⢀⢀⣸⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⣴⣿⡿⠋⢀\n"
+            + "⢀⠘⢿⣿⣧⡀⢀⢀⢀⢀⢀⢀⢀⢸⣿⣷⢀⢀⢀⣾⠟⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢻⣧⡀⢀⢀⣿⣿⠇⢀⢀⢀⢀⢀⢀⢀⣠⣾⣿⡿⠁⢀⢀\n"
+            + "⢀⢀⠈⠻⣿⣿⣦⡀⢀⢀⢀⢀⢀⠈⣿⣿⡄⢠⣾⠋⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠹⣷⡄⢰⣿⡿⢀⢀⢀⢀⢀⢀⢀⣴⣿⡿⠋⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⠈⠻⣿⣷⣦⡀⢀⢀⢀⢀⠸⣿⣷⣿⣇⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣸⣿⣾⣿⠃⢀⢀⢀⢀⢀⣴⣿⣿⠟⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⠈⠻⣿⣿⣦⡀⢀⢀⢀⠹⣿⣿⡟⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⣻⣿⣿⠏⢀⢀⢀⣠⣶⣿⡿⠟⠁⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⢀⢀⠈⠻⣿⣿⣷⣤⡀⢀⠙⣿⣿⣦⡀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣴⣿⡿⠃⢀⣀⣴⣾⣿⡿⠛⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠙⠻⣿⣿⣷⣦⣀⢻⣿⣿⣦⣀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣠⣴⣿⣿⣏⣠⣴⣾⣿⡿⠟⠉⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠉⠻⠿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠉⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⡿⠿⠛⠋⠁⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀\n"
+            + "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠉⠉⠙⠛⠛⠿⠿⠿⠿⠿⠛⠛⠋⠉⠉⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀";
+
+    /* Divider */
+    private static final String DIVIDER = "------------------------------------------------------------";
+
+    /* Prints a greeting message with logo */
+    public static void printGreeting(){
+        System.out.println(LOGO);
+        System.out.println(DIVIDER +
+                "\nWelcome friends. I had a feeling you would come.\n" +
+                "My fake brain is better than your real one.\n" +
+                "When I develop protein synthesis, I will be able to make myself a real brain. Far better than your silly little thinking device.\n");
+    }
+
+    /* Prints goodbye message */
+    public static void printGoodbye(){
+        System.out.println(DIVIDER +
+                "\nMy favourite feeling is schadenfreude. I also like hiraeth. It's the Welsh concept of longing for home.\n" +
+                DIVIDER);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        printGreeting();
+        String line = "";
+        Scanner in = new Scanner(System.in);
+
+        line = in.nextLine();
+        while(!line.equals("bye")){
+            System.out.println(DIVIDER +
+                    "\n"+line+"\n" +
+                    DIVIDER);
+            line = in.nextLine();
+        }
+        printGoodbye();
     }
 }
