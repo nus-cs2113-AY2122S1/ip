0649820078faba54e21577cda4d132af1d805710
antrikshv <antriksh9954v@gmail.com>
2021-08-25 02:48:14 +0800

    Added Greetings and Farewells

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String lineBreak = "__________________" +
+                 "_________________________________________";
         System.out.println("Hello from\n" + logo);
+        System.out.println(lineBreak);
+        System.out.println("What can I do for you today?");
+        System.out.println(lineBreak);
+        System.out.println("GoodBye, Hope to see again soon!");
+        System.out.println(lineBreak);
     }
 }
