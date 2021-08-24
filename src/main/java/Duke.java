48f654df3d5953998bf3abb03ada6bbf9f5e4ac7
ng-andre <ng.andr3@gmail.com>
2021-08-24 16:08:40 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,24 @@
 package main.java;
 
+
+import java.util.Scanner;
+
 class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            input = in.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
