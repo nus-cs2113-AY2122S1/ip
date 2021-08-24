7a3531b95743f4efe3e1d1134fd322daa2f2a219
deon <dchung2422@gmail.com>
2021-08-24 12:42:30 +0800

    level 1 greet,echo,exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+        String input;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -10,8 +13,22 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("_____________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_____________________________________________");
+
+        Scanner in = new Scanner(System.in);
+
+
+        while (true) {
+            //System.out.println("_____________________________________________");
+            input = in.nextLine();
+            System.out.println(input);
+            System.out.println("_____________________________________________");
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("_____________________________________________");
+                break;
+            }
+        }
+
 
     }
 }
