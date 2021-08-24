f1e5cedcee290b0376734ff00bd1b0b38f7e1d41
izdiyadfrhn <izdiyadfarhan@gmail.com>
2021-08-24 15:13:02 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,50 @@
-public class Duke {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Duke {
+    public static void printWithLines(String text) {
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(text);
+        System.out.println(line);
+    }
+
+    public static void printHelloMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String helloMessage = "Hello! I'm Duke\n" +
+                "What can I do for you?";
 
-        //System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Hello from\n" + logo);
+
+        printWithLines(helloMessage);
+    }
+
+    public static void printByeMessage() {
+        String byeMessage = "Bye. Hope to see you again soon!";
+
+        printWithLines(byeMessage);
+    }
+
+    public static void main(String[] args) {
+        printHelloMessage();
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        boolean flag = true;
+        while (flag) {
+            if (line.equals("bye")) {
+                printByeMessage();
+                flag = false;
+
+            } else {
+                System.out.println(line);
+                line = in.nextLine();
+            }
+        }
     }
 }
