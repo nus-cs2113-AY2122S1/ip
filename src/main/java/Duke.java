5770681fb5b383451486903bccb5015de43f0ec4
ptejasv <e0564194@u.nus.edu>
2021-08-24 14:29:42 +0800

    Edit coding style to follow course coding standards
    Add comments to methods and clean up indentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,13 +9,18 @@ public class Duke {
         handleCommands();
     }
 
-    // greet the user and prints a line
+    /**
+     * Prints a greeting to standard output
+     */
     public static void greetUser() {
         printString("Hello! I'm Duke\n" +
                 " What can I do for you?");
     }
 
-    // handles all commands by directing them to helper functions
+    /**
+     * Reads commands to Duke from standard input and passes
+     * them to separate functions to handle them
+     */
     public static void handleCommands() {
         Scanner in = new Scanner(System.in);
         String command = "";
@@ -35,6 +40,10 @@ public class Duke {
     }
 
     // helper functions used in command handlers
+
+    /**
+     * Prints Duke logo to the standard output with a greeting
+     */
     public static void printLogo() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -44,6 +53,11 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
     }
 
+    /**
+     * Formats and prints strings to standard output
+     *
+     * @param response Duke's response to be printed out
+     */
     public static void printString(String response) {
         System.out.println(line);
         System.out.println(" " + response);
@@ -52,18 +66,27 @@ public class Duke {
 
     // all command handlers
 
-    // exits the programme after printing the greeting and a line
+    /**
+     * Prints goodbye to output
+     */
     public static void exitChatbot() {
         printString("Bye. Hope to see you again soon!");
     }
 
-    // adds commands to an array of commands
+    /**
+     * Adds tasks to the array of tasks stored by Duke
+     *
+     * @param command name of the task to be added
+     */
     public static void addTask(String command) {
         tasks[Task.taskCount] = new Task(command);
         printString("added: " + command);
     }
 
-    // lists all tasks added to duke
+    /**
+     * Lists out all tasks stored by Duke, printing to standard output
+     * Tasks marked as completed will have an [X] beside their names
+     */
     public static void listTasks() {
         System.out.println(line);
         System.out.println(" Here are the tasks in your list:");
@@ -75,7 +98,13 @@ public class Duke {
         System.out.println(line);
     }
 
-    // marks a task as done
+    /**
+     * Marks task n from the stored array of tasks as done when Duke
+     * receives the command "done n" and prints an acknowledgement
+     * to the standard output
+     *
+     * @param command the "done n", command, such that the nth command is marked as done
+     */
     public static void doneTask(String command) {
         int taskIndex = Integer.parseInt(command.substring(command.indexOf(" ") + 1));
         tasks[taskIndex - 1].setDone();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,6 +3,13 @@ public class Task {
     protected boolean isDone;
     public static int taskCount = 0;
 
+    /**
+     * Creates a new Task object to be stored in Duke's list of Tasks,
+     * sets the name of the task as the name passed in by the user and
+     * marks the task as incomplete, incrementing the total task count
+     *
+     * @param description the name of the task to be created
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -25,6 +32,12 @@ public class Task {
         isDone = true;
     }
 
+    /**
+     * Returns the icon to be printed while listing out tasks stored by Duke
+     * Done tasks are denoted by X
+     *
+     * @return the icon to be used while listing tasks on Duke to standard output
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
