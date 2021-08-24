40f9375a77a5ed5368672a00648d26f0d6075dfa
AnShengLee <anshengl@yahoo.com.sg>
2021-08-24 20:12:30 +0800

    Add exception handling and printLine function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static final String HORIZONTAL_LINE = "____________________________________________________________\n";
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
 
     private static final String LOGO = "\n" +
             "██████╗░░█████╗░████████╗███╗░░░███╗░█████╗░███╗░░██╗\n" +
@@ -15,19 +15,24 @@ public class Duke {
     private static int numOfUserItems = 0;
 
     private static void printList() {
-        System.out.print(HORIZONTAL_LINE);
+        printLine();
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < numOfUserItems; i++) {
             System.out.println(i + 1 + "." + userItems[i].getStatus() + " " + userItems[i].getDescription());
         }
-        System.out.print(HORIZONTAL_LINE);
+        printLine();
+    }
+
+    private static void printLine() {
+        System.out.println(HORIZONTAL_LINE);
     }
 
     public static void main(String[] args) {
 
         System.out.println("Hello from\n" + LOGO);
-        System.out.print(HORIZONTAL_LINE + "Hello! I'm BATMAN\n" + "What can I do for you?\n" + HORIZONTAL_LINE);
-
+        printLine();
+        System.out.print("Hello! I'm BATMAN\n" + "What can I do for you?\n");
+        printLine();
         while (true) {
             String line;
             Scanner sc = new Scanner(System.in);
@@ -41,26 +46,41 @@ public class Duke {
             String[] words = line.split(" ");
             if (words[0].equals("done")) {
                 if (words.length == 1) {
-                    System.out.print(HORIZONTAL_LINE + "Invalid command.\n" + HORIZONTAL_LINE);
+                    printLine();
+                    System.out.print("Invalid command.\n");
+                    printLine();
                     continue;
                 }
-                int number = Integer.parseInt(words[1]);
-                if (number > numOfUserItems) {
-                    System.out.print(HORIZONTAL_LINE + "Invalid number.\n" + HORIZONTAL_LINE);
+                int number;
+                try {
+                    number = Integer.parseInt(words[1]);
+                    if (number > numOfUserItems || number <= 0) {
+                        printLine();
+                        System.out.print("Invalid number.\n");
+                        printLine();
+                        continue;
+                    }
+                } catch (Exception e) {
+                    printLine();
+                    System.out.print("Invalid number.\n");
+                    printLine();
                     continue;
                 }
                 userItems[number - 1].markDone();
-                System.out.print(HORIZONTAL_LINE);
+                printLine();
                 System.out.println("Cool I have eliminated this task:");
                 System.out.println(userItems[number - 1].getStatus() + " " + userItems[number - 1].getDescription());
-                System.out.print(HORIZONTAL_LINE);
+                printLine();
                 continue;
             }
             userItems[numOfUserItems] = new Task(line);
             numOfUserItems++;
-            System.out.print(HORIZONTAL_LINE + "added: " + line + "\n" + HORIZONTAL_LINE);
+            printLine();
+            System.out.print("added: " + line + "\n");
+            printLine();
         }
-
-        System.out.print(HORIZONTAL_LINE + "Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE);
+        printLine();
+        System.out.print("Bye. Hope to see you again soon!\n");
+        printLine();
     }
 }
