9e66b8e1d415f9eb4a3b3cf80742d2867a7d0da0
gavienwz <e0544185@u.nus.edu>
2021-08-24 21:40:06 +0800

    Add Level-2 Add, List, Task Class
    Add: Inputs will add a Task to a fixed size array
    List: Lists Tasks in input order
    Task class: Contains string description of task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static Task[] tasks = new Task[100];
+    private static int taskCount = 0;
+
+    public static void printList() {
+        String line = "____________________________________________________________\n";
+        System.out.print(line);
+        int count = 1;
+        for (Task item : tasks) {
+            if (item == null) {
+                break;
+            }
+            System.out.println(" " + count + ". " + item.description);
+            count = count + 1;
+        }
+        System.out.println(line);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,18 +31,29 @@ public class Duke {
         System.out.println(greet);
         String command;
         Scanner in = new Scanner(System.in);
-        String echo;
-        do {
-            command = in.nextLine();
-            echo = "____________________________________________________________\n"
-                    +  command
-                    + "\n"
-                    + "____________________________________________________________\n";
-            System.out.println(echo);
-        } while (!command.equals("bye"));
+        String addition;
         String bye = "____________________________________________________________\n"
                 +  " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
+        do {
+            command = in.nextLine();
+            switch (command) {
+            case "bye":
+                break;
+            case "list":
+                printList();
+                break;
+            default:
+                tasks[taskCount] = new Task(command);
+                taskCount = taskCount + 1;
+                addition = "____________________________________________________________\n"
+                        + " added: "
+                        + command
+                        + "\n"
+                        + "____________________________________________________________\n";
+                System.out.println(addition);
+            }
+        } while (!command.equals("bye"));
         System.out.println(bye);
     }
 }
