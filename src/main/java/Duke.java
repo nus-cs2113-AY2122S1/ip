a1621c8dd15eba09aa7706baee9e77ca53705233
poppolette <seashanty13.9@gmail.com>
2021-08-24 20:40:08 +0800

    Format code and add function headers for readability

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,21 +2,58 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static void greetingsMessage() {
-        String helloMessage = "Greetings, I'm DAHNAM. I'm definitely a human, not a machine!\nHow may I help you?\n";
+    /**
+     * Prints a message when the user runs the program.
+     */
+    public static void greetUser() {
+        String helloMessage = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣶⣾⣿⣿⣷⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣨⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣅⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⣠⣶⡿⠿⠿⢿⣶⣴⣾⠿⠛⠋⠁⠀⠉⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣶⣄⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⣼⣿⠁⠀⠀⣠⣾⠿⢋⣀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠛⠛⠿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⠛⠀⠈⢿⣧⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⣿⣿⠀⢀⣾⣿⣿⡿⠛⠉⠉⠉⠉⢉⣭⣭⣉⠙⠳⣶⡆⠀⠐⣶⠶⠋⣉⣭⣭⣉⠉⠉⠉⠉⠙⢻⣿⣿⣷⡀⠀⣸⣿⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠘⣿⣦⣿⠏⠈⢿⡇⠀⠀⠀⠀⢸⣿⣿⣿⣿⣧⠀⣸⡗⠀⠐⣿⠀⢸⣿⣿⣿⣿⣧⠀⠀⠀⠀⢀⣿⠉⢻⣷⣴⣿⠏⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⣽⣿⠀⠀⠘⢷⡀⠀⠀⠀⠸⣿⣿⣿⣿⠏⣴⠟⠀⠀⠀⠘⣷⡸⣿⣿⣿⣿⠏⠀⠀⠀⠀⣼⠃⠀⠀⣿⡏⠁⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⢻⣿⠀⠀⠀⠈⠻⠦⣄⣀⣀⣀⣉⣩⡴⠟⠁⠀⠀⠀⠀⠀⠈⠛⠶⢭⣉⣀⣀⣀⣀⠤⠞⠁⠀⠀⠀⣿⡇⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠘⣿⡆⠀⠀⠀⠀⠀⠀⠀⠈⣽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢧⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⠁⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⢹⣯⠀⠀⠀⠀⠀⠀⠀⣰⠃⢠⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡄⠘⡇⠀⠀⠀⠀⠀⠀⢰⡿⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠄⠀⠀⠀⠂⠀⠀⡟⠀⠈⠛⠿⢷⣶⣶⣶⣶⣶⣶⣶⡾⠿⠋⠁⠀⠱⠀⠀⠀⠀⠀⢰⣿⠃⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣆⠀⢈⠀⠀⠀⠀⠀⠀⢷⠀⠀⠀⠀⡌⠉⠉⠉⠁⠀⠄⠀⣤⠀⠀⠀⠀⠀⠀⠀⠀⣿⡏⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠇⢀⠘⠁⠁⠀⡀⠀⡀⠘⢷⣤⣀⣀⠃⠀⠀⠀⣀⣈⣤⠾⠃⠀⠀⢀⢠⠀⡁⠀⢸⣿⠁⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡔⣀⡀⡐⣦⠀⠀⠀⠂⠀⠀⠈⠉⠉⠉⠛⠛⠋⠏⠉⠀⠀⣰⡤⠐⠈⢠⣠⠃⠀⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⠀⣿⡀⡁⠹⣆⠀⠀⠀⠀⠀⠀⠀⠐⠀⠁⠈⠀⠀⢄⣸⡾⠫⠐⠀⠀⣼⠋⠀⢱⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠘⢧⡁⠀⠙⠳⢶⣤⣄⣀⣀⣠⣀⣢⣦⣤⠶⠞⠛⠁⠀⠀⠀⢠⡼⠃⠢⢠⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠈⠻⣦⡀⠀⠀⠀⠉⠉⢹⠉⠉⠙⡁⡀⠀⠀⠀⠀⣀⣠⡶⠋⠀⠀⢠⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣷⣄⡄⠀⡊⠙⠳⠶⠶⠶⢾⣤⣤⣤⣤⣧⣷⢶⠶⠞⢻⠉⠁⢠⠠⢈⣴⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢷⣧⣤⣀⣀⣀⣄⣄⣀⣀⣀⣀⣀⣂⣠⣀⣡⣠⣀⣠⣦⣴⡾⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠋⠙⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                        + "Greetings, I'm DAHNAM. I'm definitely a human, not a machine!\nHow may I help you?\n";
         System.out.println(helloMessage);
     }
 
-    public static void goodbyeMessage() {
-        String goodbyeMessage = "Alas, our lovely time together has to come to an end. Au revoir!\n";
+    /**
+     * Prints out a message when the user inputs 'bye'
+     */
+    public static void bidGoodbye() {
+        String goodbyeMessage = "Alas, our lovely time together comes to an end. Au revoir!\n";
         System.out.println(goodbyeMessage);
     }
 
+    /**
+     * Prints a message denying any and all allegations of DAHNAM being a bot when prompted with 'are you a bot?'
+     */
     public static void denyBotNature() {
         String denyBotNature = "No, I am definitely not a bot. Why do you ask?\n";
         System.out.println(denyBotNature);
     }
 
+    /**
+     * Enumerates through an array of tasks and prints out all tasks input by user
+     * @param taskList An array of tasks. Max size of 100
+     */
     public static void listAllTasks(Task[] taskList) {
         int i = 0;
         while (taskList[i] != null) {
@@ -25,10 +62,19 @@ public class Duke {
         }
     }
 
+    /**
+     * Notifies user of what item is added to the task list upon receiving a valid task input.
+     * @param t user input task
+     */
     public static void echoUserInput(Task t) {
         System.out.println("Added: " + t.taskDescription);
     }
 
+    /**
+     * Adds the task 't' specified by user into the task list
+     * @param taskList An array of tasks. Max size of 100
+     * @param t user input task
+     */
     public static void addToTaskList(Task[] taskList, Task t) {
         int i = 0;
         while (taskList[i] != null) {
@@ -37,25 +83,37 @@ public class Duke {
         taskList[i] = t;
     }
 
+    /**
+     * Modifies a task and sets its boolean isDone to true. Prints out an acknowledgement after.
+     * @param taskList An array of tasks. Max size of 100
+     * @param i Index of the task shown when user inputs 'list'
+     */
     public static void completeTask(Task[] taskList, int i) {
         //Navigate to the given index and change the sign
         String lineBar = "____________________________________________________________\n";
-        if ((i < 1) || (taskList[i-1] == null)) {
+        if ((i < 1) || (taskList[i - 1] == null)) {
             System.out.println(lineBar);
-            System.out.println("No task found with the given number. Please type 'list' to view your current task "
+            System.out.println("No task found with the given number. \nPlease type 'list' to view your current task "
                     + "list");
             System.out.println(lineBar);
             return;
         }
-        taskList[i-1].markAsDone();
+        i--;
+        taskList[i].markAsDone();
         System.out.println(lineBar);
-        System.out.println("Bueno! The following task is marked as done: \n[" + taskList[i-1].getStatusIcon() + "] " + taskList[i-1].taskDescription);
+        System.out.println(
+                "Bueno! The following task is marked as done: \n[" + taskList[i].getStatusIcon() + "] "
+                        + taskList[i].taskDescription);
         System.out.println(lineBar);
     }
 
+    /**
+     * This function is called upon program execution.
+     * @param args System arguments that are added to the program
+     */
     public static void main(String[] args) {
         String lineBar = "____________________________________________________________\n";
-        greetingsMessage();
+        greetUser();
         Scanner readUserInput = new Scanner(System.in);
         Task[] taskList = new Task[100];
         String userInput;
@@ -67,12 +125,11 @@ public class Duke {
                 String[] splitInput = userInput.split(" ");
                 int taskNumber = Integer.parseInt(splitInput[1]);
                 completeTask(taskList, taskNumber);
-            }
-            else {
+            } else {
                 switch (userInput) {
                 case "bye":
                     System.out.println(lineBar);
-                    goodbyeMessage();
+                    bidGoodbye();
                     System.out.println(lineBar);
                     return;
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,16 +1,30 @@
+/**
+ * Represents a task input by the user
+ */
 public class Task {
     protected String taskDescription;
     protected boolean isDone;
 
+    /**
+     * Creates a task and assigns the input to taskDescription
+     * @param taskDescription A String that the user input
+     */
     public Task(String taskDescription) {
         this.taskDescription = taskDescription;
         isDone = false;
     }
 
+    /**
+     * Retrieves the completion status of a specified task
+     * @return "X" if task is completed, " " otherwise
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); //Mark completed tasks with an X
     }
 
+    /**
+     * Sets boolean isDone to true
+     */
     public void markAsDone() {
         isDone = true;
     }
