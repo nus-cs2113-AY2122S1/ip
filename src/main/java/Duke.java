1df467e295918ca13057641501059719ed9ca60e
antrikshv <antriksh9954v@gmail.com>
2021-08-25 03:04:00 +0800

    Text-UI testing added
    Duke can now echo!

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String response = null;
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,10 +13,19 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String lineBreak = "__________________" +
                  "_________________________________________";
+
         System.out.println("Hello from\n" + logo);
         System.out.println(lineBreak);
         System.out.println("What can I do for you today?");
         System.out.println(lineBreak);
+        response = in.nextLine();
+        System.out.println(lineBreak);
+        while (!response.equals("bye")) {
+            System.out.println(response);
+            System.out.println(lineBreak);
+            response = in.nextLine();
+            System.out.println(lineBreak);
+        }
         System.out.println("GoodBye, Hope to see again soon!");
         System.out.println(lineBreak);
     }
