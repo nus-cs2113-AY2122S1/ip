9f092c094bab4c7888b537c92c7edb759a551e15
poppolette <seashanty13.9@gmail.com>
2021-08-24 16:56:44 +0800

    Add list, remove echoUserInput

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,51 +2,52 @@ import java.util.Scanner;
 public class Duke {
 
     public static void greetingsMessage() {
-        String lineBar = "____________________________________________________________\n";
         String helloMessage = "Greetings, I'm DAHNAM. I'm definitely a human, not a machine!\nHow may I help you?\n";
-        System.out.println(lineBar + helloMessage + lineBar);
+        System.out.println(helloMessage);
     }
 
     public static void goodbyeMessage() {
-        String lineBar = "____________________________________________________________\n";
         String goodbyeMessage = "Alas, our lovely time together has to come to an end. Au revoir!\n";
-        System.out.println(lineBar + goodbyeMessage + lineBar);
-    }
-
-    public static void echoUserInput(String input) {
-        String lineBar = "____________________________________________________________\n";
-        System.out.println(lineBar + input + "\n" + lineBar);
+        System.out.println(goodbyeMessage);
     }
 
     public static void denyBotNature() {
-        String lineBar = "____________________________________________________________\n";
-        System.out.println(lineBar + "No, I am definitely not a bot. Why do you ask?\n" + lineBar);
+        String denyBotNature = "No, I am definitely not a bot. Why do you ask?\n";
+        System.out.println(denyBotNature);
     }
 
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
-
+        String lineBar = "____________________________________________________________\n";
         greetingsMessage();
         Scanner readUserInput = new Scanner(System.in);
+        Task taskList = new Task();
         String userInput;
+
         while (true) {
             userInput = readUserInput.nextLine();
             switch(userInput) {
             case "bye":
+                System.out.println(lineBar);
                 goodbyeMessage();
+                System.out.println(lineBar);
                 break;
 
             case "are you a bot?":
+                System.out.println(lineBar);
                 denyBotNature();
+                System.out.println(lineBar);
+                continue;
+
+            case "list":
+                System.out.println(lineBar);
+                taskList.displayTasks();
+                System.out.println(lineBar);
                 continue;
 
             default:
-                echoUserInput(userInput);
+                System.out.println(lineBar);
+                taskList.setUserInput(userInput);
+                System.out.println(lineBar);
             }
         }
     }
