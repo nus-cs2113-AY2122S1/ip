a05464fcf0108070fe05a77c24c723ff7581e863
teoziyiivy <e0559117@u.nus.edu>
2021-08-24 14:42:36 +0800

    Level 0: Greet

---
+++ src/main/java/Greet.java
@@ -0,0 +1,6 @@
+public class Greet {
+    public static void main (String[] args) {
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
