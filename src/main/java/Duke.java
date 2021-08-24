f4397655fee08cfeeaab9c1bae9c1b8b0f4a2de0
Hu Jialun <hujialun@comp.nus.edu.sg>
2021-08-24 20:01:22 +0800

    Implement bye command
    The bye command terminates input reading. A farewell message is printed and the program exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,39 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static void handleOneInputLine(String line) {
+
+    private static void initialise() {
+        String greeting = "Hello! I'm Duke";
+        String assist = "What can I do for you?";
+        System.out.println(greeting);
+        System.out.println(assist);
+    }
+    private enum inputHandleStatus {
+        OK, END, ERROR
+    }
+    private static inputHandleStatus handleOneInputLine(String line) {
+        if (line.equals("bye")) {
+            return inputHandleStatus.END;
+        }
         System.out.println(line);
+        return inputHandleStatus.OK;
+    }
+
+    private static void finalise() {
+        String farewell = "Bye. Hope to see you again soon!";
+        System.out.println(farewell);
     }
 
     public static void main(String[] args) {
+        initialise();
 	    Scanner sc = new Scanner(System.in);
-        String greeting = "Hello! I'm Duke";
-        String assist = "What can I do for you?";
-        String farewell = "Bye. Hope to see you again soon!";
-        String hline = "_".repeat(61);
-        System.out.println(hline);
-        System.out.println(greeting);
-        System.out.println(assist);
 	    while (sc.hasNextLine()) {
-	        handleOneInputLine(sc.nextLine());
+	        switch (handleOneInputLine(sc.nextLine())) {
+                case END:
+                    finalise();
+                    return;
+            }
 	    }
-        System.out.println(hline);
-        System.out.println(farewell);
-        System.out.println(hline);
+	    finalise();
     }
 }
