9e6591409a7ccaeea113f871f30dc7109aaa1052
Eric Bryan <e0555789@u.nus.edu>
2021-08-24 11:30:48 +0800

    Add opening text, echo user input, and exit text.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void printWelcomeMessage() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void printExitMessage() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void echoMessage(String message) {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     " + message);
+        System.out.println("    ____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+
+        printWelcomeMessage();
+
+        while(true) {
+            String userInput = sc.nextLine();
+            if(userInput.equals("bye")) {
+                break;
+            }
+            echoMessage(userInput);
+        }
+
+        printExitMessage();
     }
 }
