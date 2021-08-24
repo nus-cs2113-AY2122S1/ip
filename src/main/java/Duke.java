4f8cdaaca1ca500ec16fbd7599632207b03ebb5b
owensoh <owinsoh@gmail.com>
2021-08-24 09:52:21 +0800

    Add Level-2 Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,58 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
+/**
+ * This represents the Duke Chat bot.
+ */
 public class Duke {
-    private static boolean done = false;
 
+    /**
+     * boolean done signifies whether the program is done running.
+     */
+    public static boolean done = false;
+    /**
+     * String[] list stores the commands input by user. It has a fixed size of 100.
+     */
+    public static String[] list = new String[100];
+    /**
+     * int listIndexTracker tracks the index for list to add elements in.
+     */
+    public static int listIndexTracker = 0;
+
+    /**
+     * Main function that is called upon program execution.
+     *
+     * @param args System Arguments added to program.
+     */
     public static void main(String[] args) {
+
+        /**
+         * ASCII art source: https://www.twitchquotes.com/copypastas/2059
+         */
+        String sadge = "   ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠋⠉⣉⣉⠙⠿⠋⣠⢴⣊⣙⢿⣿⣿\n"
+                + "   ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀⢀⠔⡩⠔⠒⠛⠧⣾⠊⢁⣀⣀⣀⡙⣿\n"
+                + "   ⣿⣿⣿⣿⣿⣿⣿⠟⠛⠁⠀⠀⠀⠀⠀⡡⠊⠀⠀⣀⣠⣤⣌⣾⣿⠏⠀⡈⢿⡜\n"
+                + "   ⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠡⣤⣶⠏⢁⠈⢻⡏⠙⠛⠀⣀⣁⣤⢢\n"
+                + "   ⣿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣄⡀⠣⣌⡙⠀⣘⡁⠜⠈⠑⢮⡭⠴⠚⠉⠀\n"
+                + "   ⠁⠀⢀⠔⠁⣀⣤⣤⣤⣤⣤⣄⣀⠀⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠁⠀⢀⣠⢠\n"
+                + "   ⡀⠀⢸⠀⢼⣿⣿⣶⣭⣭⣭⣟⣛⣛⡿⠷⠶⠶⢶⣶⣤⣤⣤⣶⣶⣾⡿⠿⣫⣾\n"
+                + "   ⠇⠀⠀⠀⠈⠉⠉⠉⠉⠉⠙⠛⠛⠻⠿⠿⠿⠷⣶⣶⣶⣶⣶⣶⣶⣶⡾⢗⣿⣿\n"
+                + "   ⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣿⣶⣾⣿⣿⣿\n"
+                + "   ⣿⣿⣿⣷⣶⣤⣄⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣝⡻⣿⣿⣿⣿⣿⣿⣿⣿\n"
+                + "   ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡹⣿⣿⣿⣿⣿⣿\n";
+
+        /**
+         * ASCII art source: https://fsymbols.com/text-art/
+         */
+        String text = "█▀ ▄▀█ █▀▄ █▀▀ █▀▀   █▀▄ █░█ █▄▀ █▀▀\n"
+                + "▄█ █▀█ █▄▀ █▄█ ██▄   █▄▀ █▄█ █░█ ██▄";
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + sadge + text);
 
         System.out.println(lines());
         System.out.println(greeting());
@@ -22,31 +65,66 @@ public class Duke {
         }
 
 
-
     }
 
+    /**
+     * This function prints some lines.
+     */
     public static String lines() {
         return "    ____________________________________________________________";
     }
 
+    /**
+     * This function prints a greeting message.
+     */
     public static String greeting() {
-        return "    Hello! I'm Duke\n    What can I do for you?";
+        return "    Hello... I'm Sadge Duke\n    What can I do for you? :(";
     }
 
+    /**
+     * This function prints a bye message.
+     */
     public static String bye() {
         return "    Bye. Hope to see you again soon!";
     }
 
-    private static void parseInput(String input) {
+    /**
+     * This function parses the input given by user and prints the appropriate response.
+     *
+     * @param input input given by the user.
+     */
+    public static void parseInput(String input) {
         System.out.println(lines());
         if (input.equalsIgnoreCase("Bye")) {
             System.out.println(bye());
             done = true;
-        }
-        else {
-            System.out.print("    ");
+        } else if (input.equalsIgnoreCase("List")) {
+            printList();
+        } else {
+            addToList(input);
+            System.out.print("    added: ");
             System.out.println(input);
         }
         System.out.println(lines());
+    }
+
+    /**
+     * This function adds the input string into list
+     *
+     * @param input input given by the user.
+     */
+    public static void addToList(String input) {
+        list[listIndexTracker] = input;
+        listIndexTracker++;
+    }
+
+    /**
+     * This function prints the individual elements in list
+     */
+    public static void printList() {
+        for (int i = 0; i < listIndexTracker; i++) {
+            System.out.format("    %d. ", i + 1);
+            System.out.println(list[i]);
+        }
     }
 }
