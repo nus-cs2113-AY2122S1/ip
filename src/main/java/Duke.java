e23bf07cd2f9a4d2b2d6e6c090d8df943c67d423
Poopies99 <>
2021-08-24 20:12:04 +0800

    Add echo feature that repeats user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
 
     public static void main(String[] args) {
-        String intro = "    ____________________________________________________________\n"
+        String intro = "     ____________________________________________________________\n"
                 + "     Hello! I'm\n"
                 + "     ____        _        \n"
                 + "    |  _ \\ _   _| | _____ \n"
