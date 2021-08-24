425492fdd0312e932f019954a87bb1492c5946d1
Poopies99 <>
2021-08-24 20:44:18 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Duke {
-
     public static void main(String[] args) {
         String intro = "     ____________________________________________________________\n"
                 + "     Hello! I'm\n"
