b38aa0665bdbbad72f906c77009cb38f5264a676
tlyi <t.leyi2305@gmail.com>
2021-08-24 22:44:35 +0800

    Add Task class to handle tasks Add ability to mark tasks as done, with some error handling for invalid user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,41 +19,89 @@ public class Duke {
         printDivider();
     }
 
-    /**
-     * Continuously adds user inputs into list of tasks.
-     * Prints list of tasks when user inputs "List" (not case-sensitive),
-     * Exits when user inputs "Bye" (not case-sensitive)
-     */
-    public static void addTask() {
-        String[] listOfTasks = new String[100];
-        Scanner in = new Scanner(System.in);
-        String input;
-        boolean hasSaidBye = false;
-        int numTasks = 0;
-        do {
-            input = in.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
-                hasSaidBye = true;
-                exit();
-            } else if (input.equalsIgnoreCase("list")) {
-                if (numTasks == 0) {
+    public static void addTask(Task[] listOfTasks, String description) {
+        Task t = new Task(description);
+        listOfTasks[Task.getNumTasks()] = t; //index 0 in listOfTasks is empty
+        printDivider();
+        System.out.println("Added to list: " + description);
+        System.out.println("Add another task now or type \"list\" to see current list!");
+        printDivider();
+    }
+
+    public static void printTasks(Task[] listOfTasks) {
+        if (Task.getNumTasks() == 0) {
+            printDivider();
+            System.out.println("No tasks yet, add a task now!");
+            printDivider();
+        } else {
+            printDivider();
+            System.out.println("These are your current tasks:");
+            for (int i = 1; i <= Task.getNumTasks(); i++) {
+                System.out.println(i + ". " + listOfTasks[i].getStatusIcon()
+                        + " " + listOfTasks[i].getDescription());
+            }
+            printDivider();
+        }
+    }
+
+    public static void markTaskAsDone(Task[] listOfTasks, String input) {
+        String[] words = input.split(" ");
+        if (words.length == 1) {
+            //error if user inputs only "done" with no number behind
+            printDivider();
+            System.out.println("Invalid format! Please input a task number to be marked as done, " +
+                    "in the format \"done X\", where X is the task number!");
+            printDivider();
+        } else {
+            try {
+                int taskNum = Integer.parseInt(words[1]);
+                if (taskNum > Task.getNumTasks() || taskNum < 1) {
+                    //error if user inputs a task number that does not exist
                     printDivider();
-                    System.out.println("No tasks yet, add a task now!");
+                    System.out.println("Please input a valid task number from 1 to " + Task.getNumTasks() + "!");
                     printDivider();
                 } else {
                     printDivider();
-                    for (int i = 1; i <= numTasks; i++) {
-                        System.out.println(i + ". " + listOfTasks[i-1]);
-                    }
+                    listOfTasks[taskNum].markAsDone();
+                    System.out.println(listOfTasks[taskNum].getStatusIcon()
+                            + " " + listOfTasks[taskNum].getDescription());
                     printDivider();
                 }
+            } catch (NumberFormatException e) {
+                //error if user did not input a valid integer for task number
+                printDivider();
+                System.out.println("Invalid format! Please input a task number to be marked as done, " +
+                        "in the format \"done X\", where X is the task number!");
+                printDivider();
+            }
+        }
+    }
+
+    /**
+     * Continuously adds user inputs into list of tasks.
+     * Prints list of tasks when user inputs "List" (not case-sensitive),
+     * Marks task number X as done when user inputs "done X" (not case-sensitive),
+     * Exits when user inputs "Bye" (not case-sensitive)
+     */
+    public static void addTask() {
+        Task[] listOfTasks = new Task[100];
+        Scanner in = new Scanner(System.in);
+        String input;
+        boolean hasSaidBye = false;
+        do {
+            input = in.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                //exit loop
+                hasSaidBye = true;
+            } else if (input.equalsIgnoreCase("list")) {
+                //print task
+                printTasks(listOfTasks);
+            } else if (input.toLowerCase().startsWith("done")) {
+                //mark as done
+                markTaskAsDone(listOfTasks, input);
             } else {
-                listOfTasks[numTasks] = input;
-                numTasks++;
-                printDivider();
-                System.out.println("Added to list: " + input);
-                System.out.println("Add another task now or type \"list\" to see current list!");
-                printDivider();
+                //add task
+                addTask(listOfTasks, input);
             }
         } while (!hasSaidBye);
     }
@@ -61,6 +109,7 @@ public class Duke {
     public static void main(String[] args) {
       welcome();
       addTask();
+      exit();
     }
 }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,37 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+    public static int numTasks = 0;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        numTasks++;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "[X]" : "[ ]"); //marks task done with "X"
+    }
+
+    public void markAsDone() {
+        if (!this.isDone) {
+            this.isDone = true;
+            System.out.println("Well done! I've marked this task as done. ヽ༼｡> ل͜ <｡༽ﾉ ");
+        } else {
+            System.out.println("Task has already been marked as done! Good job!ヽ༼◔ل͜◔༽ﾉ");
+            System.out.println("Try marking another task as done!");
+        }
+    }
+
+    public static int getNumTasks() {
+        return numTasks;
+    }
+}
