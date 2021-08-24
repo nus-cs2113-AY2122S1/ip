f4abe8bf747d03601f3fc4a5dbc7b842b14a1df7
tlyi <t.leyi2305@gmail.com>
2021-08-24 19:32:08 +0800

    Add tasks to list and print list when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,40 +1,66 @@
 import java.util.Scanner;
-public class Duke {
 
+public class Duke {
+    
+    public static void printDivider() {
+        System.out.println("________________________________________________________________");
+    }
+    
     public static void welcome() {
-        System.out.println("_____________________________________________________");
-        System.out.println("Hello! I'm Dude ヽ༼ ・ ل͜ ・ ༽ﾉ" + System.lineSeparator() + "What can I do for you?");
-        System.out.println("_____________________________________________________");
+        printDivider();
+        System.out.println("Hello! I'm Dude ヽ༼ ・ ل͜ ・ ༽ﾉ");
+        System.out.println("Type a task and i'll add it to your list real quick!");
+        printDivider();
     }
 
     public static void exit() {
-        System.out.println("_____________________________________________________");
+        System.out.println("________________________________________________________________");
         System.out.println("Bye! Hope to see you again soon! ヽ༼ ͠° ͟ل͜ ͠° ༽ﾉ ");
-        System.out.println("_____________________________________________________");
+        printDivider();
     }
 
-    /** Continuously echoes user inputs and exits when user inputs "Bye" (not case-sensitive) **/
-    public static void echo() {
+    /**
+     * Continuously adds user inputs into list of tasks.
+     * Prints list of tasks when user inputs "List" (not case-sensitive),
+     * Exits when user inputs "Bye" (not case-sensitive)
+     */
+    public static void addTask() {
+        String[] listOfTasks = new String[100];
         Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
+        String input;
         boolean hasSaidBye = false;
+        int numTasks = 0;
         do {
+            input = in.nextLine();
             if (input.equalsIgnoreCase("bye")) {
                 hasSaidBye = true;
                 exit();
+            } else if (input.equalsIgnoreCase("list")) {
+                if (numTasks == 0) {
+                    printDivider();
+                    System.out.println("No tasks yet, add a task now!");
+                    printDivider();
+                } else {
+                    printDivider();
+                    for (int i = 1; i <= numTasks; i++) {
+                        System.out.println(i + ". " + listOfTasks[i-1]);
+                    }
+                    printDivider();
+                }
             } else {
-                System.out.println("_____________________________________________________");
-                System.out.println(input);
-                System.out.println("_____________________________________________________");
-                input = in.nextLine();
+                listOfTasks[numTasks] = input;
+                numTasks++;
+                printDivider();
+                System.out.println("Added to list: " + input);
+                System.out.println("Add another task now or type \"list\" to see current list!");
+                printDivider();
             }
         } while (!hasSaidBye);
     }
 
-
     public static void main(String[] args) {
       welcome();
-      echo();
+      addTask();
     }
 }
 
