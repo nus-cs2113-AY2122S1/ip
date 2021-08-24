c7c41250c39e7046a00e998a95ce54f360be4d3c
Hu Jialun <hujialun@comp.nus.edu.sg>
2021-08-24 22:43:19 +0800

    Enforce coding style
    Correct several mistakes in coding style.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,26 +5,26 @@ public class Duke {
 
     static ArrayList<Task> taskList = new ArrayList<>();
 
-    private static inputHandleStatus handleOneInputLine(String line) {
+    private static InputHandleStatus handleOneInputLine(String line) {
         String[] splitted = line.split("\\s+");
         switch (splitted[0]) {
-            case "bye":
-                return inputHandleStatus.END;
-            case "list":
-                for (int i = 1; i <= taskList.size(); i += 1) {
-                    System.out.println(i + ": " + taskList.get(i - 1));
-                }
-                return inputHandleStatus.OK;
-            case "done":
-                Task target = taskList.get(Integer.parseInt(splitted[1]) - 1);
-                target.setDoneStatus(true);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(target);
-                return inputHandleStatus.OK;
-            default:
-                taskList.add(new Task(line));
-                System.out.println("added: " + line);
-                return inputHandleStatus.OK;
+        case "bye":
+            return InputHandleStatus.END;
+        case "list":
+            for (int i = 1; i <= taskList.size(); i += 1) {
+                System.out.println(i + ": " + taskList.get(i - 1));
+            }
+            return InputHandleStatus.OK;
+        case "done":
+            Task target = taskList.get(Integer.parseInt(splitted[1]) - 1);
+            target.setDoneStatus(true);
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(target);
+            return InputHandleStatus.OK;
+        default:
+            taskList.add(new Task(line));
+            System.out.println("added: " + line);
+            return InputHandleStatus.OK;
         }
     }
 
@@ -35,11 +35,11 @@ public class Duke {
         System.out.println(assist);
     }
 
-    private enum inputHandleStatus {
+    private enum InputHandleStatus {
         OK, END, ERROR
     }
 
-    static private class Task {
+    static class Task {
 
         private String title = "";
         private boolean doneStatus = false;
@@ -83,12 +83,12 @@ public class Duke {
         initialise();
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
-	        switch (handleOneInputLine(sc.nextLine())) {
-                case END:
-                    finalise();
-                    return;
+            switch (handleOneInputLine(sc.nextLine())) {
+            case END:
+                finalise();
+                return;
             }
-	    }
-	    finalise();
+        }
+        finalise();
     }
 }
