670503d6048515a0dd3b152aeb39675afd6e6693
Xuefei2001 <2758875958@qq.com>
2021-08-25 01:28:01 +0800

    enable add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,102 @@
 import java.util.Scanner;
 
+class List {
+    private int size;
+    private String[] list = new String[size];
+}
+
 public class Duke {
+
+    static String[] addtoList(String[] list, int size, String input) {
+        String[] newlist = new String[size];
+        if (size == 1) {
+            newlist[0] = input;
+        } else {
+            for (int i = 0; i < (size-1); i++) {
+                newlist[i] = list[i];
+            }
+            newlist[size - 1] = input;
+        }
+        return newlist;
+    }
+
     public static void main(String[] args) {
+        int size = 0;
+        String[] list = new String[size];
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println("type in your command please");
         System.out.println("____________________________________________________________");
         Scanner in = new Scanner(System.in);
-        String echo = in.nextLine();
+        String command = in.nextLine();
         while (true) {
-            if (echo.equals("bye")) {
+            if (command.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (command.equals("echo")){
+                System.out.println("____________________________________________________________");
+                System.out.println("Enter echo mode! please note that typing quit will exit the mode");
+                System.out.println("____________________________________________________________");
+                while (true) {
+                    String echo = in.nextLine();
+                    if (echo.equals("quit")) {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("exit current mode");
+                        System.out.println("____________________________________________________________");
+                        break;
+                    } else {
+                        System.out.println("____________________________________________________________");
+                        System.out.println(echo);
+                        System.out.println("____________________________________________________________");
+                    }
+                }
+                System.out.println("type in your command please");
+                System.out.println("____________________________________________________________");
+                command = in.nextLine();
+            } else if (command.equals("add to list")) {
+                while (true) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("what do you want to add? please note that typing quit will exit this mode");
+                    System.out.println("____________________________________________________________");
+                    String add = in.nextLine();
+                    if (add.equals("quit")) {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("exit current mode");
+                        System.out.println("____________________________________________________________");
+                        break;
+                    } else {
+                        size++;
+                        String[] transit = new String[size];
+                        transit = addtoList(list, size, add);
+                        list = new String[size];
+                        for (int i = 0; i < size; i++) {
+                            list[i] = transit[i];
+                        }
+                        System.out.println("____________________________________________________________");
+                        System.out.println("added: " + add);
+                    }
+                }
+                System.out.println("type in your command please");
+                System.out.println("____________________________________________________________");
+                command = in.nextLine();
+            } else if (command.equals("show list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 1; i <= size; i++) {
+                    System.out.println(i + ". " + list[i - 1]);
+                }
+                System.out.println("____________________________________________________________");
+                System.out.println("type in your command please");
+                System.out.println("____________________________________________________________");
+                command = in.nextLine();
             } else {
                 System.out.println("____________________________________________________________");
-                System.out.println(echo);
+                System.out.println("invalid command, pls try again");
                 System.out.println("____________________________________________________________");
-                echo = in.nextLine();
+                command = in.nextLine();
             }
         }
     }
