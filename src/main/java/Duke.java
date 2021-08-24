fef941266b8f9a35b92ce723dd58a9ca67520ab5
ng-andre <ng.andr3@gmail.com>
2021-08-24 19:08:17 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 class Duke {
 
     public static void main(String[] args) {
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int index = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n"
-
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(logo);
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
         String input;
         Scanner in = new Scanner(System.in);
@@ -21,11 +21,19 @@ class Duke {
         while(!input.equals("bye")) {
             if (input.equals("list")) {
                 for (int i = 0; i < index; i++) {
-                    System.out.println((i+1) + ". " + tasks[i]);
+                    System.out.println(tasks[i].getStatus() + (i+1) + ". " + tasks[i].getTaskName()
+                    );
                 }
             }
+            else if (input.contains("done")) {
+                input = input.trim();
+                int completedIndex = Integer.parseInt(input.substring(input.length() - 1));
+                tasks[completedIndex - 1].finishTask();
+                System.out.println("Task " + completedIndex + " mark as done!");
+            }
             else {
-                tasks[index] = input;
+                Task temp = new Task(input);
+                tasks[index] = temp;
                 System.out.println("added: " + input);
                 index++;
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+package main.java;
+
+public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getStatus() {
+        return isDone ? "[X]" : "[ ]";
+    }
+
+    public void finishTask() {
+        isDone = true;
+    }
+}
