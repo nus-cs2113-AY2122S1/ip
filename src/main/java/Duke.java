5af2f9847028dff62a6009dd870488708bf1d54c
tlyi <t.leyi2305@gmail.com>
2021-08-24 18:18:42 +0800

    Implement Greet, Echo, Exit (Level 1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
+
+    public static void welcome() {
         System.out.println("_____________________________________________________");
-        System.out.println("Hello! I'm Dude.\nWhat can I do for you?");
-        System.out.println("_____________________________________________________");
-        System.out.println("Bye! Hope to see you again soon! :)");
+        System.out.println("Hello! I'm Dude ヽ༼ ・ ل͜ ・ ༽ﾉ" + System.lineSeparator() + "What can I do for you?");
         System.out.println("_____________________________________________________");
     }
+
+    public static void exit() {
+        System.out.println("_____________________________________________________");
+        System.out.println("Bye! Hope to see you again soon! ヽ༼ ͠° ͟ل͜ ͠° ༽ﾉ ");
+        System.out.println("_____________________________________________________");
+    }
+
+    /** Continuously echoes user inputs and exits when user inputs "Bye" (not case-sensitive) **/
+    public static void echo() {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        boolean hasSaidBye = false;
+        do {
+            if (input.equalsIgnoreCase("bye")) {
+                hasSaidBye = true;
+                exit();
+            } else {
+                System.out.println("_____________________________________________________");
+                System.out.println(input);
+                System.out.println("_____________________________________________________");
+                input = in.nextLine();
+            }
+        } while (!hasSaidBye);
+    }
+
+
+    public static void main(String[] args) {
+      welcome();
+      echo();
+    }
 }
+
 
