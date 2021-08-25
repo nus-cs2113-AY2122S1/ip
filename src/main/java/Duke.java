6d54331ea193dc1fbb5768f7e2be9ef2f6fdbc4a
ngoivanessa <ngoivanessa@yahoo.com>
2021-08-25 23:18:54 +0800

    add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("Hope to see you again soon!");
-         */
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
-        if (args[0].equals("bye") == false){
-            System.out.println(args[0]);
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        boolean isBye = false;
+
+        while (isBye == false ){
+            if (line.equalsIgnoreCase(("bye"))){
+                isBye = true;
+            }
+            else {
+                System.out.println(line);
+                line = in.nextLine();
+            }
         }
-        else{
-            System.out.println("Bye. Hope to see you again soon!");
-            return;
-        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 
 
