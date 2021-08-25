1a5d9435117ce60fd8586d71e1fef457e610d0a6
e0550472@u.nus.edu <tryyang2001@gmail.com>
2021-08-25 20:43:09 +0800

    Level 0 - Greet: - print some messages for displaying

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("I am a helper robot that aims to ease your works!");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye! Hope to see you soon :D");
     }
 }
