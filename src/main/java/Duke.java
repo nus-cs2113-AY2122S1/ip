8641994318e6437448013d8adf21a4a74eadadfa
admin <798180744@qq.com>
2021-08-25 17:11:35 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -6,7 +7,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-        String greetings = " Hello! I'm Duke\n" + " What can I do for you?\n\n" + " Bye. Hope to see you again soon!";
+        String greetings = " Hello! I'm Duke\n" + " What can I do for you?\n";
         System.out.println(greetings);
+        Scanner sc= new Scanner(System.in); //System.in is a standard input stream
+        boolean flag = true;
+        do{
+            String str= sc.nextLine();
+            if(str.equals("bye")){
+                str = "Bye. Hope to see you again soon!";
+                flag = false;
+            }
+            int length = str.length();
+            System.out.print("      ");
+            for(int i = 0; i < length; i++) System.out.print("_");
+            System.out.println("");
+            System.out.println("     |" + str + "|");
+            System.out.print("     |");
+            for(int i = 0; i < length; i++) System.out.print("_");
+            System.out.println("|");
+        }while(flag);
     }
 }
