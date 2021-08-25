d2a4b288c7b7183039f5cc7faf3feda1de3f7947
LAPTOP-61K9T4RL\Nai Jie <yeatnaijie@gmail.com>
2021-08-25 15:11:45 +0800

    Add Task list
    Add Tasks class and PrintUtils class to implement task list feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,31 +2,30 @@ import java.util.Scanner;
 
 public class Duke {
     private static boolean isExit = false;
+    private static Tasks task = new Tasks();
 
-    public static void printHorizontalLine(int length) {
-        for(int i = 0; i < length; i++) {
-            System.out.print("-");
-        }
-        System.out.println();
-    }
 
     public static void echoInput(String input) {
-        printHorizontalLine(100);
+        PrintUtils.printHorizontalLine(100);
         System.out.println(input);
-        printHorizontalLine(100);
+        PrintUtils.printHorizontalLine(100);
     }
+
 
     public static void readInput(String input) {
         if (input.equalsIgnoreCase("bye")) {
             isExit = true;
-            printHorizontalLine(100);
+            PrintUtils.printHorizontalLine(100);
             System.out.println("Thank you for using our application. We hope to see you again soon");
-            printHorizontalLine(100);
+            PrintUtils.printHorizontalLine(100);
+        } else if (input.equalsIgnoreCase("list")) {
+            task.printTasks();
         } else {
-            echoInput(input);
+            task.addTask(input);
         }
 
     }
+
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
@@ -37,7 +36,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-        printHorizontalLine(100);
+        PrintUtils.printHorizontalLine(100);
         while (!isExit) {
             line = in.nextLine();
             readInput(line);
---
+++ src/main/java/PrintUtils.java
@@ -0,0 +1,8 @@
+public class PrintUtils {
+    public static void printHorizontalLine(int length) {
+        for(int i = 0; i < length; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+}
