fb515fd285645a664dea883db395a39966ade46e
yipwayyne <E0533998@u.nus.edu>
2021-08-25 13:20:30 +0800

    Committed Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void echo(String args){
+        if (args.length() > 0) {
+            System.out.println(args);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,11 +14,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String greet = "Hello I'm Duke\n"
-                + "What can I do for you?\n";
+                + "What can I do for you?";
         String exit = "Bye. Hope to see you again soon!";
+        Scanner in = new Scanner(System.in);
+        String line = "";
 
         System.out.println("Hello from\n" + logo);
         System.out.println(greet);
+
+        while (!line.equals("bye")) {
+            echo(line);
+            line = in.nextLine();
+        }
+        
         System.out.println(exit);
     }
 }
