91ce339c1e1070426187f9c3e42362442111edff
vincentlauhl <vincentlauhl@gmail.com>
2021-08-25 11:51:42 +0800

    Add echo and method for printing output

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String divider = "___________________________________________________________";
+    public static String indentation = "      ";
+
+    public static void printIndentationAndDivider(){
+        System.out.print(indentation);
+        System.out.println(divider);
+    }
+    public static void printWordsWithIndentation(String words){
+        System.out.print(indentation);
+        System.out.println(words);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,12 +20,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String divider = "________________________________________________________________";
-        System.out.println(divider);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println(divider);
-        System.out.println("Bye, hope to see you again soon!");
-        System.out.println(divider);
+        printIndentationAndDivider();
+        printWordsWithIndentation("Hello! I'm Duke");
+        printWordsWithIndentation("What can I do for you? :D");
+        printIndentationAndDivider();
+        System.out.println();
+
+        Scanner in = new Scanner (System.in);
+        String input = in.nextLine();
+        while(!input.equals("bye")){
+            printIndentationAndDivider();
+            printWordsWithIndentation(input);
+            printIndentationAndDivider();
+            System.out.println();
+            input = in.nextLine();
+        }
+
+        printIndentationAndDivider();
+        printWordsWithIndentation("Bye, hope to see you again soon! :)");
+        printIndentationAndDivider();
     }
 }
