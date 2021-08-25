a292893e3bcce1fc5af080e72385c7c2e2f95cc9
YipWayne <yipwayyne@gmail.com>
2021-08-26 01:26:13 +0800

    Committed Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void echo(String args){
+
+    private static Scanner in = new Scanner(System.in);
+    private static String line = "";
+    private static String[] list = new String[100];
+    private static int tasks_num = 0;
+
+    public static void add(String args){
         if (args.length() > 0) {
-            System.out.println(args);
+            System.out.println("added: " + args);
+            list[tasks_num] = args;
+            tasks_num++;
+        }
+    }
+
+    private static void list_out(){
+        for (int i = 1; i <= tasks_num; i++) {
+            System.out.println(i + ". " + list[i - 1]);
         }
     }
 
@@ -16,14 +30,17 @@ public class Duke {
         String greet = "Hello I'm Duke\n"
                 + "What can I do for you?";
         String exit = "Bye. Hope to see you again soon!";
-        Scanner in = new Scanner(System.in);
-        String line = "";
 
         System.out.println("Hello from\n" + logo);
         System.out.println(greet);
 
         while (!line.equals("bye")) {
-            echo(line);
+            if (line.equals("list")) {
+                list_out();
+            }
+            else {
+                add(line);
+            }
             line = in.nextLine();
         }
 
