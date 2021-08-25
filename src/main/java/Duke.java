e007bb2da6cf24faf86bfa4a02b01758032137b0
kahhe <69589263+kahhe@users.noreply.github.com>
2021-08-26 01:33:21 +0800

    Add Increment: Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,25 @@
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class Duke {
 
-    private static String[] list = new String[100];
-    private static int itemCount = 0;
+    private static final Task[] list = new Task[100];
+    private static int taskCount = 0;
 
     public static void main(String[] args) {
         greetUser();
-        engageUser(args);
+        engageUser();
         byeUser();
     }
 
-    // That's one dope logo.
-    public static void printDukeLogo() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
+//    That's one dope logo.
+//    public static void printDukeLogo() {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n and poop";
+//        System.out.println("Hello from\n" + logo);
+//    }
 
     public static void printLine() {
         System.out.println("    ____________________________________________________________");
@@ -37,17 +36,45 @@ public class Duke {
     }
 
     public static void printList() {
-        for (int i = 0; i < itemCount; i++) {
-            System.out.println("      " + (i + 1) + ". " + list[i]);
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("      " + (i + 1) + ".[" + list[i].getStatusIcon() + "] " + list[i].description);
         }
     }
+    public static void addTask(String userInput) {
+//        list[taskCount].description = userInput;
+        list[taskCount] = new Task(userInput);
+        taskCount++;
 
-    public static void engageUser(String[] args) {
+        printLine();
+        System.out.println("      added: " + userInput);
+        printLine();
+    }
+    public static void crossOutTask(int taskNumber) {
+        printLine();
+        if (taskNumber < 0 || taskNumber >= taskCount) {
+            System.out.println("Task does not exist :P");
+        } else {
+            list[taskNumber].isDone = true;
+            System.out.println("      Nice! I've marked this task as done:");
+            System.out.println("        [X] " + list[taskNumber].description);
+        }
+        printLine();
+    }
+
+    public static void engageUser() {
         Scanner text = new Scanner(System.in);
         String userInput;
+        String taskNumber = "100";
 
         do {
-            userInput = text.nextLine();
+            userInput = text.next();
+            if (userInput.equals("done")) {
+                taskNumber = text.next();
+            } else {
+                userInput = userInput + text.nextLine();
+            }
+
             switch (userInput) {
             case "bye":
                 break;
@@ -56,13 +83,11 @@ public class Duke {
                 printList();
                 printLine();
                 break;
+            case "done":
+                crossOutTask(Integer.parseInt(taskNumber) - 1);
+                break;
             default:
-                list[itemCount] = userInput;
-                itemCount++;
-
-                printLine();
-                System.out.println("      added: " + userInput);
-                printLine();
+                addTask(userInput);
             }
         } while (!userInput.equals("bye"));
     }
