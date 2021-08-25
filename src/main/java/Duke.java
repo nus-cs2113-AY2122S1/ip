a878a4f6e5f1c5ee6ff9811676d38861878763a8
ShaoYurui <rrayshao@gmail.com>
2021-08-25 20:10:00 +0800

    Adjust to coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
-    public static void printLine()
-    {
+    public static void printLine() {
         System.out.println("\t____________________________________________________________");
     }
-    public static void startDuke()
-    {
+    public static void startDuke() {
         printLine();
         String logo = "\t ____        _        \n"
                     + "\t|  _ \\ _   _| | _____ \n"
@@ -14,19 +12,16 @@ public class Duke {
         System.out.println("\tHello from\n" + logo);
         printLine();
     }
-    public static void endDuke()
-    {
+    public static void endDuke() {
         System.out.println("\tBye. Hope to see you again soon!");
         printLine();
     }
-    public static void greet()
-    {
+    public static void greet() {
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
         printLine();
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         startDuke();
         greet();
         endDuke();
