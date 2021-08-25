c77d04f2f228809cdb74e67fc3f4747c74e21d15
silinche <chensilin011025@gmail.com>
2021-08-25 22:41:33 +0800

    fix minor bug for increment 3

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,7 +4,7 @@ public class Task {
 
     public Task(String description) {
         this.description = description;
-//        this.isDone = false;
+        this.isDone = false;
     }
 
     public void setDone() {
