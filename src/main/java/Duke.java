2b39fd3cad10f9813329bb555a9d61d960a8efe8
Koh Qianqi <qianqikoh@protonmail.com>
2021-08-25 15:43:28 +0800

    Add the ability to store text entered by the user and display them back to the user.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
@@ -7,20 +6,36 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("    Hello from\n" + logo);
         System.out.println("    _______________________________");
         System.out.println("    What up Dawg! I'm Duke");
         System.out.println("    What can I do for you?");
-        String task = scanner.nextLine();
-        String endTask = "bye";
-        while (!task.equals(endTask)) {
-            System.out.println("    _______________________________");
-            System.out.println("    " + task);
-            System.out.println("    _______________________________");
-            task = scanner.nextLine();
+
+        String command = scanner.nextLine();
+        String[] tasks = new String[100];
+        int numOfTasks = 0;
+
+        while (!command.equals("bye")) {
+            if (!command.equals("list")) {
+                tasks[numOfTasks] = command;
+                System.out.println("    _______________________________");
+                System.out.println("    added: " + tasks[numOfTasks]);
+                System.out.println("    _______________________________");
+                numOfTasks++;
+            }
+            else {
+                int index = 1;
+                System.out.println("    _______________________________");
+                for (int i = 0; i < numOfTasks; i++, index++) {
+                    System.out.println("    " + index + ". " + tasks[i]);
+                }
+                System.out.println("    _______________________________");
+            }
+            command = scanner.nextLine();
         }
+
         System.out.println("    _______________________________");
         System.out.println("    Bye. Hope to see you again soon!");
         System.out.println("    ______________________________");
