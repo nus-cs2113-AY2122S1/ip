0f87ec53f3cdf321a453868faa2a3cefffd573d7
Aayush <aayushmathur7@gmail.com>
2021-08-25 12:36:05 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,24 @@
-public class Duke {
-    public static void main(String[] args) {
-        String horizontal = " __________________________________________________";
-        String introduction = "   Hello! I'm Duke\n"
-                + "   What can I do for you?";
-        String farewell = "   Bye. Hope to see you again soon!";
+import java.util.Scanner;
 
-        System.out.println(horizontal);
+public class Duke {
+    static String horizontal = " __________________________________________________\n";
+    static String introduction = horizontal + "   Hello! I'm Duke\n" + "   What can I do for you?\n" + horizontal;
+    static String farewell = horizontal + "   Bye. Hope to see you again soon!\n" + horizontal;
+
+    public static void echoCommand(String line) {
+        System.out.println(horizontal + "    " + line + "\n" + horizontal);
+    }
+
+    public static void main(String[] args) {
         System.out.println(introduction);
-        System.out.println(horizontal);
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            echoCommand(line);
+            line = in.nextLine();
+        }
         System.out.println(farewell);
-        System.out.println(horizontal);
     }
 }
