a7ed4a1bbbf14443b3d16b2946bba1fa597b1124
Tan Teck Hwee <tan.brian98@gmail.com>
2021-08-25 17:11:35 +0800

    Add Increment Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,45 @@
+import com.sun.source.tree.WhileLoopTree;
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String LINE = "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=";
+
+
+    /**
+     * Prints given string in the middle of 2 horizontal lines.
+     *
+     * @param message String to be printed
+     */
+    public static void printMessage(String message){
+        System.out.println(LINE);
+        System.out.println(message);
+        System.out.println(LINE);
+    }
+
+    public static void EchoUserInput(){
+
+    }
     public static void main(String[] args) {
-        String logo = "_________________________________________________________\n"
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + "_________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "_________________________________________________________\n";
-        System.out.println(logo);
+        String welcomeMessage = "Hello! I'm Gaben.\n"
+                + "Steam sales is coming! Prepare your wallet.\n"
+                + "Anyways, what can I do for you today?";
+        String exitMessage = "Thank you for using Gaben.\n"
+                + "Remember to keep your wallet stacked before using me again!";
+
+        printMessage(welcomeMessage);
+
+        Scanner in = new Scanner(System.in);
+        while (true){
+            String userInput = in.nextLine();
+            if(userInput.equalsIgnoreCase("bye")){
+                printMessage(exitMessage);
+                break;
+            }
+            else{
+                printMessage(userInput);
+            }
+        }
+
     }
 }
