5bb050758f3e3ab6ab9089e8e91b8612fea533b8
ngoivanessa <ngoivanessa@yahoo.com>
2021-08-25 23:36:38 +0800

    add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] text = new String[100];
+
+    public static void addTask(String text, String[] arr, int count){
+        arr[count] = text;
+        System.out.println("added: " + arr[count]);
+    }
+
+    public static void requestList (int count) {
+        for (int i = 1; i < count; i++) {
+            System.out.println(i + "." + text[i]);
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Hello! I'm Duke");
@@ -10,14 +23,20 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
 
+        int taskCount = 1;
         boolean isBye = false;
 
-        while (isBye == false ){
+        while (!isBye){
             if (line.equalsIgnoreCase(("bye"))){
                 isBye = true;
             }
+            else if (line.equalsIgnoreCase("list")){
+                requestList(taskCount);
+                line = in.nextLine();
+            }
             else {
-                System.out.println(line);
+                addTask(line, text, taskCount);
+                taskCount++;
                 line = in.nextLine();
             }
         }
