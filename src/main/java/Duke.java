c935cd21d2a81ca62a2261f1638f74ddcb0fbe78
Jiexiong <jiexiong123@hotmail.com>
2021-08-25 14:11:33 +0800

    Greet and Bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greet();
+        bye();
     }
+
+    public static void greet(){
+        printLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void bye(){
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void printLine(){
+        for(int i = 0; i <30 ; i ++){
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
 }
