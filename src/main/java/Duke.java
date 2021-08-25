d1ef0ffb215ef2028fcbbc776cfd287e7741b722
Ashraf Jaffar <a.ashrafjfr@gmail.com>
2021-08-25 20:03:27 +0800

    Added ability to echo user input Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-        + "Hello! I'm Duke\n"
-        + "What can I do for you?\n"
-        + "____________________________________________________________\n"
-        + "Bye. Hope to see you again soon!\n"
-        + "____________________________________________________________\n";
-        System.out.println(logo);
+        String greet = "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+        System.out.println(greet);
 
-        //test for sourcetree push 2
+        String exit = "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+
+        Scanner in = new Scanner(System.in);
+        String word;
+
+        do{
+            word = in.next();
+            if (word.toLowerCase().equals("bye")){
+                System.out.println(exit);
+                break;
+            }
+            System.out.println("____________________________________________________________\n"
+                    + word + "\n"
+                    + "____________________________________________________________\n");
+        } while (!word.toLowerCase().equals("bye"));
+
     }
 }
