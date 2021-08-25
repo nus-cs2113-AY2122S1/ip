cba7da6c594e00c9b814077e4fa539f800c364a6
STAung07 <swann1999@gmail.com>
2021-08-25 18:20:49 +0800

    Added increment: Level-3 Mark as Done + Extension Task Class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,39 @@
 import java.util.Scanner;
 
+class Task {
+    private boolean isDone;
+    private String taskName;
+
+    public Task(boolean isDone, String taskName) {
+        this.isDone = isDone;
+        this.taskName = taskName;
+    }
+
+    public Task() {
+        this(false, "Nothing");
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+}
+
 public class Duke {
     public static void main(String[] args) {
         // list to store all tasks
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int tasksCounter = 0;
         String friday = "  __      _     _             \n"
                 + " / _|    (_)   | |            \n"
@@ -30,28 +60,55 @@ public class Duke {
             if (userInput.equals("bye")) {
                 break;
             }
+            // if user lists
             if (userInput.equals("list")) {
                 System.out.print(dashes);
                 System.out.println("Listing all tasks for today.");
-                for (String task : tasks) {
+                for (Task task : tasks) {
                     if (task == null) {
                         break;
                     }
                     System.out.print(">");
-                    System.out.println(task);
+                    String checkbox = "[ ]";
+                    if (task.isDone()) {
+                        checkbox = "[X]";
+                    }
+                    System.out.println(checkbox + task.getTaskName());
                 }
                 System.out.print(dashes);
                 continue;
             }
-            /*
-            String echo = "____________________________________________________________\n"
-                    + userInput + "\n"
-                    + "____________________________________________________________\n";
-             */
+            // if user wants to mark as done
+            if (userInput.startsWith("done")) {
+                // get index of task to change
+                int taskIndex = Integer.parseInt(userInput.substring(userInput.indexOf(" ") + 1)) - 1;
+                // if task out of bounds
+                if (taskIndex < 0 || taskIndex > 99) {
+                    System.out.println(dashes);
+                    System.out.println("Apologies sir, there is no such task in your list.");
+                    System.out.println(dashes);
+                    continue;
+                }
+                // change task to done
+                Task currTask = tasks[taskIndex];
+                if (currTask == null) {
+                    System.out.println(dashes);
+                    System.out.println("Apologies sir, there are currently no tasks in your list");
+                    System.out.println(dashes);
+                    continue;
+                }
+                currTask.setDone(true);
+                System.out.println(dashes);
+                System.out.println("Your task \"" + currTask.getTaskName() + "\" is indicated as complete.");
+                System.out.println("[X]" + currTask.getTaskName());
+                System.out.println(dashes);
+                continue;
+            }
             // add to list
-            tasks[tasksCounter++] = userInput;
+            Task newTask = new Task(false, userInput);
+            tasks[tasksCounter++] = newTask;
             System.out.print(dashes);
-            System.out.println("Very well, adding task " + userInput);
+            System.out.println("Very well, adding task \"" + userInput + "\"");
             System.out.print(dashes);
         }
 
