afb94fb3f3f2e3a29e5b9ade98e909707af96948
shoibloya <52075633+shoibloya@users.noreply.github.com>
2021-08-25 23:36:43 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+
+    public static void printList(String[] list){
+        for (int i=0; i < Arrays.stream(list).count(); i++){
+            System.out.println((i+1) + ". " + list[i]);
+        }
+    }
+
     public static void main(String[] args) {
         //Implementing Increment Level-0: Initial skeleton of Duke
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+        String[] list = new String[100];
+        int lastItem = 0;
         while(true){
             String line;
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
-            if(!line.split(" ")[0].equals("bye")){
-                System.out.println(line);
-            }else{
+            if(line.split(" ")[0].equals("bye")){
                 break;
+            }else if(line.split(" ")[0].equals("list")){
+                printList(Arrays.copyOf(list, lastItem));
+            }else{
+                System.out.println("added: " + line);
+                list[lastItem] = line;
+                lastItem++;
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
