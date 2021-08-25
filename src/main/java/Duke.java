efba6a1b246dbfa4fa65344e3d135413a6a2ee66
ngoivanessa <ngoivanessa@yahoo.com>
2021-08-25 17:14:28 +0800

    skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,28 @@
 public class Duke {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
+        /*
+        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
+        System.out.println("Hello from\n" + logo);
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("Hope to see you again soon!");
+         */
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        if (args[0].equals("bye") == false){
+            System.out.println(args[0]);
+        }
+        else{
+            System.out.println("Bye. Hope to see you again soon!");
+            return;
+        }
     }
 
 
