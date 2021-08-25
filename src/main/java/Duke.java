ca6b0fe15564092edd17385864aee26b0ad10379
LAPTOP-H8FATR55\kairo <kairoskoh.1212@gmail.com>
2021-08-25 22:37:56 +0800

    Add Level-2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+
+        // Welcome Message
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -17,20 +20,38 @@ public class Duke {
         System.out.println("");
         System.out.println("--------------------");
 
+        // Echo Chamber
+
         boolean isBye = false;
         String input;
         Scanner in = new Scanner(System.in);
+        int counter = 0;
+        String[] list = new String[100];
 
         while(!isBye){
             input = in.nextLine();
-            if(!input.equals("bye")){
+            if(!input.equals("bye") && !input.equals("list") ){
                 System.out.println("--------------------");
-                System.out.println(input);
+                System.out.println("added: " + input);
+                System.out.println("--------------------");
+                list[counter] = input;
+                counter += 1;
+            } else if (input.equals("list")) {
+                System.out.println("--------------------");
+                if (list[0] != null){
+                    for(int i = 0; i < counter; i += 1){
+                        System.out.println(i + 1 + ". " +list[i]);
+                    }
+                } else {
+                    System.out.println("List is empty. Time to get productive!");
+                }
                 System.out.println("--------------------");
             } else {
                 isBye = true;
             }
         }
+
+        // Goodbye Message
 
         System.out.println("--------------------");
         System.out.println("Bye. Hope to see you again soon!");
