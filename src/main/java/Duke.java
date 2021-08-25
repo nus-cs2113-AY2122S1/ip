cc07e809ed18b48b32d8241ba682139ed84f8875
LAPTOP-61K9T4RL\Nai Jie <yeatnaijie@gmail.com>
2021-08-25 14:45:54 +0800

    Add echo and exit
    -Add echo and exit commands to Duke.java
    -Add horizontal lines for formatting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static boolean isExit = false;
+
     public static void printHorizontalLine(int length) {
         for(int i = 0; i < length; i++) {
             System.out.print("-");
         }
-        System.out.println("");
+        System.out.println();
+    }
+
+    public static void echoInput(String input) {
+        printHorizontalLine(100);
+        System.out.println(input);
+        printHorizontalLine(100);
+    }
+
+    public static void readInput(String input) {
+        if (input.equalsIgnoreCase("bye")) {
+            isExit = true;
+            printHorizontalLine(100);
+            System.out.println("Thank you for using our application. We hope to see you again soon");
+            printHorizontalLine(100);
+        } else {
+            echoInput(input);
+        }
+
     }
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -14,7 +38,9 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
         printHorizontalLine(100);
-        System.out.println("Thank you for using our application. We hope to see you again soon");
-        printHorizontalLine(100);
+        while (!isExit) {
+            line = in.nextLine();
+            readInput(line);
+        }
     }
 }
