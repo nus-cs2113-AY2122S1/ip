9c09053ceec88f2e7572a4d551f43d11a6499442
DESKTOP-4HCLPEK\Eyan Chua <e0543484@u.nus.edu>
2021-08-25 15:27:57 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -14,14 +15,27 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("_______________________________________________________");
         System.out.println("Your instructions, my Liege.");
+        String[] storage = new String[100];
+        int counter = 0;
         while (true) {
             Scanner myObj = new Scanner(System.in);
             String userInput = myObj.nextLine();
             if (Objects.equals(userInput, "bye")) {
-                System.out.println("_______________________________________________________\n" + "Bye! Hope to see you again soon." + "\n" + "_______________________________________________________\n");
+                System.out.println("_______________________________________________________\n" + "Farewell, my Lord." + "\n" + "_______________________________________________________\n");
                 break;
+            } else if (Objects.equals(userInput, "list")) {
+                for (int i = 0; i < 100; i++) {
+                    if (storage[i] == null) {
+                        break;
+                    } else {
+                        System.out.println((i+1) + "." + storage[i]);
+                    }
+                }
+                continue;
             }
+            storage[counter] = userInput;
             System.out.println("_______________________________________________________\n" + userInput + "\n" + "_______________________________________________________\n");
+            counter++;
         }
     }
 }
