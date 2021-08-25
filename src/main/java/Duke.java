0cde5bb1665650aea2088ae72940a24e66b06618
Anderson <andersonleong@outlook.com>
2021-08-25 16:38:23 +0800

    Level-1 commit: Added Greet, Echo, and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n"
-                + "____________________________________________________________\n"
-                + "Hello! I'm Duke\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String line = "\t____________________________________________________________\n\t";
+        String greeting = line
+                + "Hello! I'm Duke\n\t"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
+                + line;
+        String bye = line
                 + "Bye. Hope to you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo);
+                + line;
+
+        String command;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\tHello from\n" + logo);
+        System.out.println(greeting);
+        command = in.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(line + command + "\n" + line);
+            command = in.nextLine();
+        }
+
+        System.out.println(bye);
     }
 }
