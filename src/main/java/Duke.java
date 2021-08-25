c687e7333607d4aec690b4627ca9637718a9c10f
shoibloya <52075633+shoibloya@users.noreply.github.com>
2021-08-25 23:59:56 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,17 +3,32 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static void printList(String[] list){
-        for (int i=0; i < Arrays.stream(list).count(); i++){
-            System.out.println((i+1) + ". " + list[i]);
+    public static class taskList{
+        String[] list = new String[100];
+        String[] doneList = new String[100];
+        int lastItem = 0;
+        public void printList(){
+            for (int i=0; i < Arrays.stream(list).count(); i++){
+                if (list[i] != null){
+                    if (doneList[i] != null){
+                        System.out.println((i+1) + ".[" + doneList[i] + "] " + list[i]);
+                    }else{
+                        System.out.println((i+1) + "[ ]" + list[i]);
+                    }
+
+                }else{
+                    break;
+                }
+            }
         }
     }
+
+
 
     public static void main(String[] args) {
         //Implementing Increment Level-0: Initial skeleton of Duke
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
-        String[] list = new String[100];
-        int lastItem = 0;
+        taskList newList = new taskList();
         while(true){
             String line;
             Scanner in = new Scanner(System.in);
@@ -21,11 +36,15 @@ public class Duke {
             if(line.split(" ")[0].equals("bye")){
                 break;
             }else if(line.split(" ")[0].equals("list")){
-                printList(Arrays.copyOf(list, lastItem));
-            }else{
+                newList.printList();
+            }else if(line.split(" ")[0].equals("done")){
+                int index = Integer.valueOf(line.split(" ")[1]);
+                newList.doneList[index-1] = "X";
+            }
+            else{
                 System.out.println("added: " + line);
-                list[lastItem] = line;
-                lastItem++;
+                newList.list[newList.lastItem] = line;
+                newList.lastItem++;
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
