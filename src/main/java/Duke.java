78f114109e6e8b29b404f4748a1fce52b938245d
avellinwong01 <avellin.wong@gmail.com>
2021-08-25 23:25:48 +0800

    Add Increment Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,13 +14,9 @@ public class Duke {
         System.out.println(" What can I do for you?\n");
         System.out.println(horizontal_line + "\n");
         String line;
-        String[] tasks = new String[100]; // fixed size array for now
-        int curr_count = 0;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while (!line.equals("bye")) {
-            tasks[curr_count] = line;
-            curr_count += 1;
             System.out.println(horizontal_line + "\n");
             System.out.println(line + "\n");
             System.out.println(horizontal_line + "\n");
