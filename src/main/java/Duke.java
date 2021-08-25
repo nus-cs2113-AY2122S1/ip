5fc8d7a228d70cee8329e02f77d609df27780180
rebecca <e0560032@u.nus.edu>
2021-08-25 11:50:34 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,29 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
         String line;
+        String[] taskList = new String[100];
         Scanner in = new Scanner(System.in);
+        int taskCount = 0;
+
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         line = in.nextLine();
         while (!line.equals("bye")) {
-            System.out.println(line);
+            if(line.equals("list")) {
+                String[] printList = Arrays.copyOf(taskList, taskCount);
+                int i = 1;
+                for(String task: printList) {
+                    System.out.println(i + ". " + task);
+                    i += 1;
+                }
+            } else {
+                System.out.println("added: " + line);
+                taskList[taskCount] = line;
+                taskCount += 1;
+            }
             line = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
