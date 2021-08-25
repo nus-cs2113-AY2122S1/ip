bd042e5a35e35757589ba0b46cce831ce674a016
shoibloya <52075633+shoibloya@users.noreply.github.com>
2021-08-25 23:23:40 +0800

    Level - 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         //Implementing Increment Level-0: Initial skeleton of Duke
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+        while(true){
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if(!line.split(" ")[0].equals("bye")){
+                System.out.println(line);
+            }else{
+                break;
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
