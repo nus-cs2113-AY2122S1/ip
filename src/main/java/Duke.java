74a56b1b29bbcb6c19afbed252e7d2e2e47a909d
Anderson <andersonleong@outlook.com>
2021-08-25 22:13:48 +0800

    fixed Coding Standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,19 +16,19 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        String LOGO = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "\t____________________________________________________________\n\t";
-        String greeting = line
+        String LINE = "\t____________________________________________________________\n\t";
+        String greeting = LINE
                 + "Hello! I'm Anderson\n\t"
                 + "What do you need to do?\n"
-                + line;
-        String bye = line
+                + LINE;
+        String BYE = LINE
                 + "Bye. try not to procrastinate!\n"
-                + line;
+                + LINE;
 
         String command;
         Scanner in = new Scanner(System.in);
@@ -36,24 +36,24 @@ public class Duke {
         ToDo[] unfilteredToDos = new ToDo[100];
         int unfilteredCounter = 0;
 
-        System.out.println("\tHello from\n" + logo);
+        System.out.println("\tHello from\n" + LOGO);
         System.out.println(greeting);
         command = in.nextLine();
 
         while (!command.equals("bye")) {
             if (command.equals("\n")) {
-                System.out.println(line);
+                System.out.println(LINE);
                 command = in.nextLine();
             } else if (command.contains("done")) {
-                System.out.println(line + "Nice! I've marked this task as done:");
+                System.out.println(LINE + "Nice! I've marked this task as done:");
                 String[] removeWords = command.split(" ");
                 int toDoNumber = Integer.parseInt(removeWords[1]);
                 ToDo completedToDo = unfilteredToDos[(toDoNumber - 1)];
                 completedToDo.markAsDone();
-                System.out.println("\t\t" + completedToDo.getStatusIcon() + completedToDo.getTask() + "\n" + line);
+                System.out.println("\t\t" + completedToDo.getStatusIcon() + completedToDo.getTask() + "\n" + LINE);
                 command = in.nextLine();
             } else if (!command.equals("list")) {
-                System.out.println(line + "added: " + command + "\n" + line);
+                System.out.println(LINE + "added: " + command + "\n" + LINE);
                 unfilteredToDos[unfilteredCounter] = new ToDo(command);
                 unfilteredCounter++;
                 command = in.nextLine();
@@ -61,20 +61,20 @@ public class Duke {
                 ToDo[] filteredNull = FilterNulls(unfilteredToDos);
                 int count = 0;
                 if (filteredNull[0] == null) {
-                    System.out.println(line + "\tAll DONE!\n" + line);
+                    System.out.println(LINE + "\tAll DONE!\n" + LINE);
                     command = in.nextLine();
                 } else {
-                    System.out.println(line + "Here are the tasks in your list:\n");
+                    System.out.println(LINE + "Here are the tasks in your list:\n");
                     for (ToDo toDo : filteredNull) {
                         count++;
                         System.out.println("\t" + count + "." + toDo.getStatusIcon() + toDo.getTask());
                     }
-                    System.out.println(line);
+                    System.out.println(LINE);
                     command = in.nextLine();
                 }
             }
         }
 
-        System.out.println(bye);
+        System.out.println(BYE);
     }
 }
