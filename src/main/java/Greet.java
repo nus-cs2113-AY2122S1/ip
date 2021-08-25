efd9e5bc401cb761aa42cc5f85d28ca235fcf735
teoziyiivy <e0559117@u.nus.edu>
2021-08-26 00:25:38 +0800

    Level-1

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Greet {
     public static void main (String[] args) {
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("Hello! I'm Duke");
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.println("What can I do for you?");
+        line = in.nextLine();
+        boolean bool1 = false;
+        while (!bool1) {
+            System.out.println(line);
+            line = in.nextLine();
+            if (line.contains("bye")) {
+                bool1 = true;
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
