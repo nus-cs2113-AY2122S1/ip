dadc8d319706e574b6226ca9e81821b8fb3ad5a4
xingjie99 <69759370+xingjie99@users.noreply.github.com>
2021-08-26 02:30:00 +0800

    Update Duke.java
    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\n");
-        System.out.println("What can I do for you?\n");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        while(!line.equals("bye")) {
+            System.out.println(line);
+            line = in.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
-
