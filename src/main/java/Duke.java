9ba862eea76ef50fd46a72189bb07a8089f36e92
Poopies99 <christophernge99@gmail.com>
2021-08-25 14:04:33 +0800

    Update README.md and adjusted Duke.java to comply with Java Coding Standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Duke {
-
     public static void main(String[] args) {
         String intro = "     ____________________________________________________________\n"
                 + "     Hello! I'm\n"
@@ -16,34 +15,43 @@ public class Duke {
         String line;
         Scanner in = new Scanner(System.in);
 
-        //Initialise List (elements are initialised as null)
-        String[] toDo = new String[10];
+        //Initialize List (elements are initialized as null)
+        Task[] toDo = new Task[10];
         int trackIndex = 0;
-        //Initialise bool to check whether program should exit loop
-        boolean shouldStay = true;
+        //Initialize bool to check whether program should exit loop
+        boolean isProgress = true;
 
-        while (shouldStay) {
+        while (isProgress) {
             line = in.nextLine();
 
             String output;
 
             if (line.equals("bye")) {
-                output = "    ____________________________________________________________\n" +
-                        "     Bye. Hope to see you again soon!\n" +
-                        "    ____________________________________________________________";
-                shouldStay = false;
+                output = "    ____________________________________________________________\n"
+                        + "     Bye. Hope to see you again soon!\n"
+                        + "    ____________________________________________________________";
+                isProgress = false;
             } else if (line.equals("list")) {
                 output = "    ____________________________________________________________\n";
                 for (int i = 0; trackIndex > i; i++) {
-                    String record = "     " + (i+1)+ ". " + toDo[i] + "\n";
+                    String record = "     " + (i + 1) + ".["
+                            + toDo[i].getStatusIcon() + "] "+ toDo[i].getDescription() + "\n";
                     output = output.concat(record);
                 }
                 output = output.concat("    ____________________________________________________________\n");
+            } else if (line.contains("done")) {
+                int dividerPosition = line.indexOf(" ");
+                int numberInTaskArray = Integer.parseInt(line.substring(dividerPosition + 1)) - 1;
+                toDo[numberInTaskArray].setIsDone();
+                output = "____________________________________________________________\n"
+                        + "     Nice! I've marked this task as done: \n"
+                        + "       [X] " + toDo[numberInTaskArray].getDescription() + "\n"
+                        + "    ____________________________________________________________";
             } else {
-                output = "    ____________________________________________________________\n" +
-                        "     added: " + line + "\n" +
-                        "    ____________________________________________________________\n";
-                toDo[trackIndex] = line;
+                output = "    ____________________________________________________________\n"
+                        + "     added: " + line + "\n"
+                        + "    ____________________________________________________________\n";
+                toDo[trackIndex] = new Task(line);
                 trackIndex++;
             }
             System.out.println(output);
