c2ff0412805dcc8dd65015ee835fa7fa3c82f70d
Koh Qianqi <qianqikoh@protonmail.com>
2021-08-25 18:23:52 +0800

    Add class Task.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -13,31 +14,46 @@ public class Duke {
         System.out.println("    What up Dawg! I'm Duke");
         System.out.println("    What can I do for you?");
 
-        String command = scanner.nextLine();
-        String[] tasks = new String[100];
-        int numOfTasks = 0;
+        String input = scanner.nextLine();
 
-        while (!command.equals("bye")) {
-            if (!command.equals("list")) {
-                tasks[numOfTasks] = command;
-                System.out.println("    _______________________________");
-                System.out.println("    added: " + tasks[numOfTasks]);
-                System.out.println("    _______________________________");
-                numOfTasks++;
-            }
-            else {
-                int index = 1;
-                System.out.println("    _______________________________");
-                for (int i = 0; i < numOfTasks; i++, index++) {
-                    System.out.println("    " + index + ". " + tasks[i]);
+        // Initialise tasks
+        int tasksCount = 0;
+        Task[] tasks = new Task[100];
+
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                System.out.println("\t_______________________________");
+                for (int i = 0; i < tasksCount; i++) {
+                    int index = i + 1;
+                    System.out.println("\t" + index + ". " + tasks[i].getStatusIcon() + tasks[i].description);
                 }
-                System.out.println("    _______________________________");
+                System.out.println("\t_______________________________");
+            } else {
+                if (input.length() >= 6 && input.substring(0,3) == "done") {
+                    //int a = Integer.parseInt(arr[1]) - 1;
+                    System.out.println("ok");
+                    /*
+                    if (a >= 1 && a <= tasksCount) {
+                        tasks[a].markAsDone();
+                        System.out.println("\t_______________________________");
+                        System.out.println("\tNice! I've marked this as done.");
+                        System.out.println("\t\t[X] " + tasks[a].description);
+                    }
+
+                     */
+                } else { // add task
+                    tasks[tasksCount] = new Task(input);
+                    System.out.println("\t_______________________________");
+                    System.out.println("\tadded: " + tasks[tasksCount].description);
+                    System.out.println("\t_______________________________");
+                    tasksCount++;
+                }
             }
-            command = scanner.nextLine();
+            input = scanner.nextLine();
         }
 
-        System.out.println("    _______________________________");
-        System.out.println("    Bye. Hope to see you again soon!");
-        System.out.println("    ______________________________");
+        System.out.println("\t_______________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t______________________________");
     }
 }
