2aa83a3076560e5d714b12b8fb819f4bcd84d3a9
Nirmaleshwar <e0415906@u.nus.edu>
2021-08-25 18:43:34 +0800

    Added javadoc comments

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,24 +1,53 @@
+
 public class Task {
+
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Creates new task with description as name.
+     * Sets task default as not complete with boolean isDone.
+     *
+     * @param description string name of task inputted.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Gets the name of the selected task.
+     *
+     * @return description of task.
+     */
     public String getDescription() {
         return description;
     }
 
-    public String getStatusIcon() {
-        return (isDone ? "[X]" : "[ ]");
-    }
+    /**
+     * Checks if task is already marked as done.
+     *
+     * @return boolean value: True = done and False = not done.
+     */
     public boolean isDone() {
         return isDone;
     }
 
+    /**
+     * Changes task status to completed.
+     *
+     * @param done True if not marked and False if already marked as done.
+     */
     public void changeStatusDone(boolean done) {
         isDone = done;
+    }
+
+    /**
+     * Gets "X" icon to mark completed tasks.
+     *
+     * @return X if done and [ ] if not done.
+     */
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");
     }
 }
