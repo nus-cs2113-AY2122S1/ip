6aa04fc4ae67d33dd8364ac2b6da4eabe86de914
Koh Qianqi <qianqikoh@protonmail.com>
2021-08-25 12:33:40 +0800

    Add greet, echo, exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,13 +7,22 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("_______________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("_______________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("______________________________");
+        System.out.println("    Hello from\n" + logo);
+        System.out.println("    _______________________________");
+        System.out.println("    What up Dawg! I'm Duke");
+        System.out.println("    What can I do for you?");
+        String task = scanner.nextLine();
+        String endTask = "bye";
+        while (!task.equals(endTask)) {
+            System.out.println("    _______________________________");
+            System.out.println("    " + task);
+            System.out.println("    _______________________________");
+            task = scanner.nextLine();
+        }
+        System.out.println("    _______________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ______________________________");
     }
 }
