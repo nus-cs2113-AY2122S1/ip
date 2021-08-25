64ce830542fe35342b34a5c27f6b2c686fe2cfe6
exetr <exetrtck@gmail.com>
2021-08-25 23:08:10 +0800

    add function to echo commands and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        printBanner();
+        String cmd;
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.print("> ");
+            cmd = in.nextLine();
+            if (cmd.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + cmd);
+                System.out.println("____________________________________________________________");
+            }
+        }
+    }
+
+    public static void printBanner() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -10,8 +32,6 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
