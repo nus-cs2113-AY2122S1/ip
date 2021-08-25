36787d5e0e25c634bb86f47b183567c4858e9f70
Aayush <aayushmathur7@gmail.com>
2021-08-25 12:59:44 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,19 @@ public class Duke {
     static String horizontal = " __________________________________________________\n";
     static String introduction = horizontal + "   Hello! I'm Duke\n" + "   What can I do for you?\n" + horizontal;
     static String farewell = horizontal + "   Bye. Hope to see you again soon!\n" + horizontal;
+    static String[] taskList = new String[100];
+    static int taskCount = 0;
 
-    public static void echoCommand(String line) {
-        System.out.println(horizontal + "    " + line + "\n" + horizontal);
+    public static void echoCommandList(String[] tasklist) {
+        System.out.println(horizontal);
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("    " + (i+1) + "." + taskList[i]);
+        }
+        System.out.println(horizontal);
+    }
+
+    public static void echoCommandAdded(String line) {
+        System.out.println(horizontal + "    added: " + line + "\n" + horizontal);
     }
 
     public static void main(String[] args) {
@@ -16,7 +26,12 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while (!line.equals("bye")) {
-            echoCommand(line);
+            if (line.equals("list")) {
+                echoCommandList(taskList);
+            }
+            echoCommandAdded(line);
+            taskList[taskCount] = line;
+            taskCount++;
             line = in.nextLine();
         }
         System.out.println(farewell);
