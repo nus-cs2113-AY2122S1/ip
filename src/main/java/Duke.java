8747f622c6e6a5d7e9d365e9c794af1179070874
Zhihuang Wang <e0543682@u.nus.edu>
2021-08-25 14:47:36 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String text = "____________________________________________________________\n"
-                + " Hello! I'm Duke\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-        System.out.println(text);
+        Scanner in = new Scanner(System.in);
+        String linebreak = "    ____________________________________________________________\n";
+        String greetings = linebreak
+                + "     Welcome to the Duke Chatbot\n"
+                + "     What would you like to do today?\n"
+                + linebreak;
+        String farewell = "     Bye. Hope to see you again soon!\n"
+                + linebreak;
+
+        System.out.println(greetings);
+        while (true) {
+            String command = in.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(farewell);
+                break;
+            }
+            System.out.println(linebreak);
+            System.out.println("     " + command);
+            System.out.println(linebreak);
+        }
+
     }
 }
