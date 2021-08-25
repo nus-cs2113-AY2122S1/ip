2348813fc3db7adf3e37897fdce946455ab30266
e0550472@u.nus.edu <tryyang2001@gmail.com>
2021-08-25 21:53:51 +0800

    Level 1 - Greet, Echo, Exit - draw the logo of the bot and print the greeting messages - implement the echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,67 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void printLogo(){
+        //print the logo when the program starts
+        System.out.println("\t              ##*                                 ");
+        System.out.println("\t   .      . .&####%                               ");
+        System.out.println("\t    #%%%%((&%######%**.                           ");
+        System.out.println("\t    %#############%#%&&%%%%%##(*.                 ");
+        System.out.println("\t . .%%#######%%%%%##############%%%%#(.           ");
+        System.out.println("\t/##&@%####&%####&#####%&&&&&&&&&%%%####&/  .      ");
+        System.out.println("\t&######%%######%&%%%#################%%%%&#       ");
+        System.out.println("\t(@####&#####%&%########################%###@(     ");
+        System.out.println("\t,&%#%%%%##%%############%%%%%%%%%%%##########%.   ");
+        System.out.println("\t*&#&%%&#%&########&%#(%#/((((((/****#&%&%#####%,  ");
+        System.out.println("\t  ,&#%%%%#####%%#(%(((&#(&(((((((((/%#(((%##%%#%. ");
+        System.out.println("\t  (%##%%####%%(((/%&((%&#%##((((((((&#(((%#(((&&, ");
+        System.out.println("\t  (%#%&####%#((##(#(/###.%#.*(##(((%, /##((%#(#( .");
+        System.out.println("\t  *&(&%###%#(((((#/@%%%&,. ...     &%%&%  (&###*  ");
+        System.out.println("\t . (%%%###%(((((#/#####&, .. .... /&(#(&,.%(((#(  ");
+        System.out.println("\t    *&&##%%(((((%*(%###%. .. .. . *%(##&, (%((#(  ");
+        System.out.println("\t      (&#%%((((#%/ .**,. ..... . ...,,,. . (#(%(  ");
+        System.out.println("\t        .(%(((((&/ .   .  ...... .. .. . .(%/#%,  ");
+        System.out.println("\t          .(%(((#&*,,.           . .,*/*%#((#&*   ");
+        System.out.println("\t              ,/(#(    .,,******,.     ####%*     ");
+
+        System.out.println("  __          ________ _      _____ ____  __  __ ______    ");
+        System.out.println("   \\ \\        / /  ____| |    / ____/ __ \\|  \\/  |  ____|  ");
+        System.out.println("    \\ \\  /\\  / /| |__  | |   | |   | |  | | \\  / | |__     ");
+        System.out.println("     \\ \\/  \\/ / |  __| | |   | |   | |  | | |\\/| |  __|    ");
+        System.out.println("      \\  /\\  /  | |____| |___| |___| |__| | |  | | |____   ");
+        System.out.println("     __\\/_ \\/   |______|______\\_____\\____/|_|  |_|______|_ ");
+        System.out.println("    / ____| |  | |_   _|  \\/  |   /\\   |  __ \\|_   _| \\ | |");
+        System.out.println("   | (___ | |__| | | | | \\  / |  /  \\  | |__) | | | |  \\| |");
+        System.out.println("    \\___ \\|  __  | | | | |\\/| | / /\\ \\ |  _  /  | | | . ` |");
+        System.out.println("    ____) | |  | |_| |_| |  | |/ ____ \\| | \\ \\ _| |_| |\\  |");
+        System.out.println("   |_____/|_|  |_|_____|_|  |_/_/    \\_\\_|  \\_\\_____|_| \\_|");
+
+        //greeting
+        System.out.println("\nKonichiwa! Hi there! My name is Shima Rin and I am a chat robot that can help you do some wonderful jobs!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("I am a helper robot that aims to ease your works!");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye! Hope to see you soon :D");
+        printLogo();
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("|For now I am a echo bot and I will repeat anything you said :P|");
+        System.out.println("|You can type \"exit\" or \"bye\" to stop me if you want :)        |");
+        System.out.println("----------------------------------------------------------------");
+
+        System.out.println("Let's start:");
+        Scanner in = new Scanner(System.in);
+        String cmd = "START";
+        while (!cmd.toUpperCase().equals("EXIT") && !cmd.toUpperCase().equals("BYE")){
+            cmd = in.nextLine();
+            if (cmd.equals("")){
+                System.out.println("\t(Empty)");
+            }
+            else if (!cmd.toUpperCase().equals("EXIT") && !cmd.toUpperCase().equals("BYE")){
+                System.out.println("\t" + cmd);
+            }
+        }
+        if (cmd.toUpperCase().equals("EXIT") || cmd.toUpperCase().equals("BYE")){
+            System.out.println("Bye! Hope to see you again :D");
+        }
     }
 }
