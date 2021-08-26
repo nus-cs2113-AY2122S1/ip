8e248b8af0ba5db1889d043c87c9c2a9513aeaa2
arvejw <jiewen.tehh@gmail.com>
2021-08-26 20:30:20 +0800

    Change logo and horizontal bar to static final variables since they should be constants
    Renamed final variables according to convention

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static String logo = System.lineSeparator() +
+    private static final String LOGO = System.lineSeparator() +
             "    ___   ______   ________  _______      ___   ____  _____   .--." + System.lineSeparator() +
             "  .'   `.|_   _ \\ |_   __  ||_   __ \\   .'   `.|_   \\|_   _|.'_\\/_'." + System.lineSeparator() +
             " /  .-.  \\ | |_) |  | |_ \\_|  | |__) | /  .-.  \\ |   \\ | |  '. /\\,.'" + System.lineSeparator() +
@@ -12,7 +12,7 @@ public class Duke {
             "                                                           (/\\\\||/" + System.lineSeparator() +
             "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\\||^^^^";
 
-    private static String horizontalBar = "__________________" +
+    private static final String HORIZONTAL_BAR = "__________________" +
             "__________________" +
             "__________________" +
             "_______________";
@@ -20,24 +20,24 @@ public class Duke {
     private static ArrayList<Task> taskList = new ArrayList<Task>();
 
     public static void greetingMessage() {
-        System.out.println(horizontalBar);
-        System.out.println(logo);
+        System.out.println(HORIZONTAL_BAR);
+        System.out.println(LOGO);
         System.out.println("  Hello! I'm Oberon");
         System.out.println("  What can I do for you?");
-        System.out.println(horizontalBar);
+        System.out.println(HORIZONTAL_BAR);
     }
 
     public static void farewellMessage() {
-        System.out.println(horizontalBar);
+        System.out.println(HORIZONTAL_BAR);
         System.out.println("  Goodbye. Hope to see you again soon!");
-        System.out.println(logo);
-        System.out.println(horizontalBar);
+        System.out.println(LOGO);
+        System.out.println(HORIZONTAL_BAR);
     }
 
     public static void echo(String input) {
-        System.out.println(horizontalBar);
+        System.out.println(HORIZONTAL_BAR);
         System.out.println("  " + input);
-        System.out.println(horizontalBar);
+        System.out.println(HORIZONTAL_BAR);
     }
 
     public static void editTaskList(Task taskInput) {
@@ -47,35 +47,37 @@ public class Duke {
 
     public static void printTaskList() {
         if (taskList.isEmpty()) {
-            System.out.println(horizontalBar);
+            System.out.println(HORIZONTAL_BAR);
             System.out.println("  List is empty!");
-            System.out.println(horizontalBar);
+            System.out.println(HORIZONTAL_BAR);
         } else {
-            System.out.println(horizontalBar);
+            System.out.println(HORIZONTAL_BAR);
+            // Traverse down the ArrayList and prints out each task
             for (int i = 0; i < taskList.size(); i++) {
                 int currentIndexInOnesIndexing = i + 1;
                 System.out.println(Integer.toString(currentIndexInOnesIndexing)
                         + ". " + taskList.get(i).getStatusIcon()
                         + " " + taskList.get(i).taskDescription);
             }
-            System.out.println(horizontalBar);
+            System.out.println(HORIZONTAL_BAR);
         }
     }
 
     public static void markTaskAsDone(String input) {
+        // Extracting task number as an integer from input
         int taskNumber = Integer.parseInt(input.replace("done", "").trim(), 10);
         boolean taskNumberInRange = (taskNumber <= taskList.size()) && (taskNumber >= 1);
         if (taskNumberInRange) {
             taskList.get(taskNumber - 1).setDone();
-            System.out.println(horizontalBar);
+            System.out.println(HORIZONTAL_BAR);
             System.out.println("Task " + Integer.toString(taskNumber) + ": "
                     + taskList.get(taskNumber - 1).taskDescription);
             System.out.println("  Marked as done!");
         } else {
-            System.out.println(horizontalBar);
+            System.out.println(HORIZONTAL_BAR);
             System.out.println("  Invalid Task number!");
         }
-        System.out.println(horizontalBar);
+        System.out.println(HORIZONTAL_BAR);
     }
 
     public static void main(String[] args) {
