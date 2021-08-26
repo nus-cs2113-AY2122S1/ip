7e2f13a98b0b3975ab9c16beb8821f1e26585eb7
Edwin <zizo_edwin@hotmail.com>
2021-08-26 22:01:06 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static Task[] items = new Task[100];
+    private static int i = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,24 +15,34 @@ public class Duke {
         System.out.println("What do you want?\n" + border);
         System.out.println("Type bye to exit\n" + border);
         String line;
-        String[] items = new String[100];
-        int i = 0;
+
         do {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
             if (line.matches("list")) {
                 int j = 1;
                 System.out.println(border);
-                for (String item : items) {
+                for (Task item : items) {
                     if (item != null) {
-                        System.out.println(j + ". " + item);
+                        System.out.println(j + ".[" + item.getStatusIcon() + "] " + item.getDescription());
                         j++;
                     }
                 }
                 System.out.println(border);
+
+            } else if (line.contains("done")) {
+                int dividerPosition = line.indexOf(" ") + 1;
+                int endPosition = line.length();
+                if (endPosition > 5) {
+                    String num = line.substring(dividerPosition, endPosition);
+                    int taskNum = Integer.parseInt(num) - 1;
+                    items[taskNum].markDone();
+                    System.out.println(border + "Nice! task is done " + '\n' + border);
+                }
             } else if (!line.matches("bye")) {
                 System.out.println(border + "added: " + line + '\n' + border);
-                items[i] = line;
+                Task newItem = new Task(line);
+                items[i] = newItem;
                 i++;
             }
         }
