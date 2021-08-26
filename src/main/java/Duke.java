44bded2d3978a0a04102a90620bd7e01a763cc91
markuslyq <markuslyq@gmail.com>
2021-08-26 16:02:48 +0800

    Added the ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,7 +11,7 @@ public class Duke {
 
         System.out.println("\tHey, how are you?");
         System.out.println("\tWhat can I do for you today?");
-        System.out.println("\t---------------------------------------------------------------------");
+        System.out.println("\t----------------------------------------------------------------------");
 
         String input = "";
         int i = 0;
@@ -20,7 +20,7 @@ public class Duke {
         do {
             Scanner scanner = new Scanner(System.in);
             input = scanner.nextLine();
-            System.out.println("\t---------------------------------------------------------------------");
+            System.out.println("\t----------------------------------------------------------------------");
 
             if (!input.equals("bye")) {
                 String[] splittedInput = input.split(" ");
@@ -30,23 +30,23 @@ public class Duke {
                         int itemNumber = j + 1;
                         System.out.println("\t" + itemNumber + ". [" + tasks[j].getStatusIcon() + "] " + tasks[j].description);
                     }
-                    System.out.println("\t---------------------------------------------------------------------");
+                    System.out.println("\t----------------------------------------------------------------------");
                 } else if (splittedInput[0].equals("done")) {
                     int taskInt = Integer.parseInt(splittedInput[1]) - 1;
                     tasks[taskInt].markAsDone();
                     System.out.println("\tGood job! I've marked this task as done: ");
                     System.out.println("\t[" + tasks[taskInt].getStatusIcon() + "] " + tasks[taskInt].description);
-                    System.out.println("\t---------------------------------------------------------------------");
+                    System.out.println("\t----------------------------------------------------------------------");
                 } else {
                     tasks[i] = new Task(input);
                     System.out.println("\tnew task added: " + input);
-                    System.out.println("\t---------------------------------------------------------------------");
+                    System.out.println("\t----------------------------------------------------------------------");
                     i++;
                 }
             }
         } while (!input.equals("bye"));
 
         System.out.println("\tGoodbye! Hope to see you again soon!");
-        System.out.println("\t---------------------------------------------------------------------");
+        System.out.println("\t----------------------------------------------------------------------");
     }
 }
