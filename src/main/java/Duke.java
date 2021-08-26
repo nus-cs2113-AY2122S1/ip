6fc6fbfc934fbb7f37e10369df2a54387f1c9517
husysg <husysg@icloud.com>
2021-08-26 14:54:02 +0800

    Add Echo to Duke(Level-1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,28 @@
+// import libraries here
+import java.util.Scanner;
+
 public class Duke {
     public static void greeting() {
-        System.out.println("____________________________________________________________");
-        System.out.println("  Hello! I'm Duke");
-        System.out.println("  What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("  Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+    }
+    public static void echo() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        // get user input
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + line);
+            System.out.println("    ____________________________________________________________");
+            line = in.nextLine();
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -15,5 +32,6 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greeting();
+        echo();
     }
 }
