fafd67e580c394c531030d2c0f5e7a90a7361c85
Dmitri <dmitriyam.rex1738@gmail.com>
2021-08-26 23:42:05 +0800

    Coding standards - indexes are in samll letters                  - Classes sart with a Capital letter                  - inputs have the word user before followed by the input with the first letter capatalized

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,10 +5,9 @@ public class Duke {
 
     public static void main(String[] args) {
 
-        String user_input;
+        String user_Input;
         Scanner in = new Scanner(System.in);
         Task[] t = new Task[100];
-
         int list_index = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,31 +16,31 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        user_input = in.nextLine();
+        user_Input = in.nextLine();
 
-        while (!(user_input.contentEquals("bye"))) {
-            if(user_input.contentEquals("list")){
+        while (!(user_Input.contentEquals("bye"))) {
+            if(user_Input.contentEquals("list")){
                 System.out.println("List so far: ");
                 for(int i = 0; i < list_index; i++) {
                     System.out.println( (i+1) + "[" + t[i].getStatus() + "]" + t[i].name);
                 }
-                user_input = in.nextLine();
+                user_Input = in.nextLine();
             }
             else {
-                if(user_input.contains("done")){
-                    int task_index = Integer.parseInt(String.valueOf(user_input.charAt(0)));
+                if(user_Input.contains("done")){
+                    int task_index = Integer.parseInt(String.valueOf(user_Input.charAt(0)));
                     t[task_index - 1].task_done();
                     System.out.println("List so far: ");
                     for(int i = 0; i < list_index; i++) {
                         System.out.println(i + "[" + t[i].getStatus() + "]" + t[i].name);
                     }
-                    user_input = in.nextLine();
+                    user_Input = in.nextLine();
                 }
                 else {
-                    System.out.println("added: " + user_input);
-                    t[list_index] = new Task(user_input);
+                    System.out.println("added: " + user_Input);
+                    t[list_index] = new Task(user_Input);
                     list_index++;
-                    user_input = in.nextLine();
+                    user_Input = in.nextLine();
                 }
             }
         }
