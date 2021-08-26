5871858381dd6447ce996a4774c7396c528c0775
Jia Yixuan <jyxhazcake@yahoo.com>
2021-08-26 14:57:12 +0800

    Add echo command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,12 +7,21 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String dashes = "____________________________________________________________\n";
         System.out.println(logo);
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        System.out.println(dashes +
+                " Hello! I'm Duke!\n" +
+                " I don't have many functions now but I recently bought a new speaker!\n" +
+                " Tell me some stuff and I'll repeat them back to you! Type 'bye' to exit.\n" + dashes);
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(dashes + line + "\n" + dashes);
+            line = in.nextLine();
+        }
+        System.out.println(dashes +
+                " Bye. Do visit next time! Upgrades coming soon :-)\n" +
+                dashes);
     }
 }
