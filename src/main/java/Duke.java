9b01139be08e124e38d322eea6370b94255f2f54
pragyan_01 <samal@u.nus.edu>
2021-08-26 23:27:46 +0800

    Echo's user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String line = "------------------------------------------------------------------------------------------\n";
@@ -6,6 +8,16 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(line + "Hello! I'm Duke.\n" + logo + "What can i do for you?\n" + line + "Bye! Hope to see you again soon.\n" + line);
+        System.out.println(line + "Hello! I'm Duke.\n" + logo + "What can i do for you?\n" + line);
+
+        // getting user input and echoing it
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(line + "\n" + input + "\n" + line);
+            input = scan.nextLine();
+        }
+            System.out.println(line + "\n" + input + ". Hope to see you again soon!\n" + line);
     }
 }
