67680876afddd91b76df0e288d7652e318b5de05
astralum <minyue.j@gmail.com>
2021-08-26 15:52:09 +0800

    Update Duke to echo user input if it is not "bye".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void echoUserInput(String input) {
+        System.out.println("  >>> " + input);
+        System.out.println("********************************************");
+    }
+
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,6 +17,12 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        line = in.nextLine();
+        while (!line.toLowerCase().equals("bye")) {
+          echoUserInput(line);
+          line = in.nextLine();
+        }
+        System.out.println("Farewell. Hope to see you again soon!");
     }
 }
