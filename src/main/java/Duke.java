b3532423bedd203ada1870b7b230b4942ba882ab
LimJiaJing <jiajinglim06.study@gmail.com>
2021-08-26 22:02:52 +0800

    Complete Level2.
    Changed method echo to talkToUser.
    Added methods addToList and printList.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
+    private static ArrayList<String> items = new ArrayList<String>();
+    private static String divider = "\t__________________________________________________________";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,22 +11,35 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
         Greeting.openingGreet();
-        echo();
+        talkToUser();
         Greeting.closingGreet();
     }
-    private static void echo() {
+    private static void talkToUser() {
         Scanner in=new Scanner(System.in);
-        String divider = "\t__________________________________________________________";
         String userInput;
         do {
             System.out.println();
             userInput = in.nextLine();
             System.out.println(divider);
-            if (userInput.equals("bye")){
+            if(userInput.equals("list")){
+                printList();
+                continue;
+            }
+            else if (userInput.equals("bye")){
                 break;
             }
-            System.out.println("\t" + userInput);
+            addToList(userInput);
+            System.out.println("\tadded: " + userInput);
             System.out.println(divider);
         } while(true);
+    }
+    private static void addToList(String userInput){
+        items.add(userInput);
+    }
+    private static void printList(){
+        for (int i=0; i<items.size();i++){
+            System.out.println((i+1)+". "+items.get(i));
+        }
+        System.out.println(divider);
     }
 }
