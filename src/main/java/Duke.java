0d65493e2f578684840e90beae937cc5fc4c85af
astralum <minyue.j@gmail.com>
2021-08-26 16:25:54 +0800

    Add ability to add tasks to list and print list contents. Remove echo user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,45 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void echoUserInput(String input) {
-        System.out.println("  >>> " + input);
+    public static void printStartOfInput() {
+        System.out.print(">>> ");
+    }
+    public static void printStartOfOutputSpace() {
+        System.out.print("  ");
+    }
+    public static void printOutputSeparator() {
         System.out.println("********************************************");
+    }
+    public static void echoUserInput(String input) {
+        printOutputSeparator();
+        printStartOfOutputSpace();
+        System.out.println(">>> " + input);
+        printOutputSeparator();
+    }
+
+    public static void printAddItemToList(String item) {
+        printOutputSeparator();
+        printStartOfOutputSpace();
+        System.out.println("As you command. Added: " + item);
+        printOutputSeparator();
+    }
+
+    public static void printFormattedList(String[] list) {
+        int listItemNum = 0;
+
+        printOutputSeparator();
+        for (int i = 0; i < list.length && list[i] != null; i++) {
+            listItemNum = i + 1;
+            printStartOfOutputSpace();
+            System.out.println(listItemNum + ". " + list[i]);
+        }
+        printOutputSeparator();
     }
 
     public static void main(String[] args) {
         String line;
+        String[] list = new String[100];
+        int listIndex = 0;
         Scanner in = new Scanner(System.in);
 
         String logo = " ____        _        \n"
@@ -18,9 +50,17 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
 
+        printStartOfInput();
         line = in.nextLine();
         while (!line.toLowerCase().equals("bye")) {
-          echoUserInput(line);
+          if (line.toLowerCase().equals("list")) {
+              printFormattedList(list);
+          } else {
+              list[listIndex] = line;
+              listIndex++;
+              printAddItemToList(line);
+          }
+          printStartOfInput();
           line = in.nextLine();
         }
         System.out.println("Farewell. Hope to see you again soon!");
