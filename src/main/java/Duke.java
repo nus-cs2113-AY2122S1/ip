c113fdf30680f519318a57a7130ec24354ec4fad
pragyan_01 <samal@u.nus.edu>
2021-08-27 01:05:13 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,14 +10,28 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(line + "Hello! I'm Duke.\n" + logo + "What can i do for you?\n" + line);
 
-        // getting user input and echoing it
+        // getting user input
+        String[] array = new String[100];
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
+        int arrayCount = 0;
 
         while (!input.equals("bye")) {
-            System.out.println(line + "\n" + input + "\n" + line);
+
+            if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < arrayCount; i++) {
+                        System.out.println((i+1) + ". " + array[i] + "\n");
+                }
+                System.out.println(line);
+            } else {
+                array[arrayCount] = input;
+                arrayCount++;
+                System.out.println(line + "added: " + input + "\n" + line);
+            }
             input = scan.nextLine();
         }
-            System.out.println(line + "\n" + input + ". Hope to see you again soon!\n" + line);
+        System.out.println(line + "\n" + input + "! Hope to see you again soon!\n" + line);
     }
 }
+
