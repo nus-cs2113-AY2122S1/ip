f4c8e804ceaeef0379c104e07666341951166637
Kong Dak Nam <kongdaknam@gmail.com>
2021-08-26 23:23:27 +0800

    Increment Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,20 +10,37 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("____________________________________________");
+        String horizontalLine = "____________________________________________";
+        System.out.println(horizontalLine);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
         Scanner in = new Scanner(System.in);
         String userInput = in.nextLine();
+        String[] tasks = new String[100];
+        int taskCount = 0;
         while (!userInput.equals("bye")){
-            System.out.println("____________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________");
+            if (userInput.equals("list")){
+                String[] taskList = Arrays.copyOf(tasks, taskCount);
+                int taskNum = 1;
+                System.out.println(horizontalLine);
+                for (int i = 0; i < taskCount; i++){
+                    System.out.println(taskNum + "." + taskList[i]);
+                    taskNum++;
+                }
+                System.out.println(horizontalLine);
+            }
+            else {
+                tasks[taskCount] = userInput;
+                taskCount ++;
+                System.out.println(horizontalLine);
+                System.out.println("added: " + userInput);
+                System.out.println(horizontalLine);
+            }
             userInput = in.nextLine();
         }
-        System.out.println("____________________________________________");
+        System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________");
+        System.out.println(horizontalLine);
     }
 }
