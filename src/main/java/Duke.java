e3334a1ca192f2da5e181921e90e79d3f66fcf11
Zhang Haoyu <haoyusimon927@gmail.com>
2021-08-26 22:22:47 +0800

    A-CodingStandard
    Modified the code to comply with the given coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Duke {
 
     public static void main(String[] args) {
-        Task[] task_list = new Task[100];
+        int MAXIMUM = 100;
+        Task[] task_list = new Task[MAXIMUM];
         int task_num = 0;
-        String Greetings = "     _______________________\n"
+        String GREETINGS = "     _______________________\n"
                 + "     Hello! I'm Duke\n"
                 + "     What can I do for you?\n"
                 + "     _______________________\n";
 
-        System.out.println(Greetings);
+        System.out.println(GREETINGS);
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
@@ -21,7 +22,7 @@ public class Duke {
                 break;
             case "done":
                 int current_task = Integer.parseInt(line.split(" ")[1]) - 1;
-                task_list[current_task].setIsDone();
+                task_list[current_task].markAsDone();
                 String update = "     _______________________\n"
                         + "     Nice! I've marked this task as done: \n"
                         + "       [X] " + task_list[current_task].getContent() + "\n"
@@ -49,7 +50,7 @@ public class Duke {
         protected String content;
         protected Boolean isDone;
 
-        public Task(String content){
+        public Task(String content) {
             this.content = content;
             isDone = false;
         }
@@ -58,7 +59,7 @@ public class Duke {
             return (isDone? "X":" ");
         }
 
-        public void setIsDone(){
+        public void markAsDone() {
             this.isDone = true;
         }
 
@@ -69,7 +70,7 @@ public class Duke {
 
     public static void list(int task_num, Task[] task_list ) {
         String list_output = "     _______________________\n";
-        for (int i=0; i<task_num; i++) {
+        for (int i = 0; i < task_num; i++) {
             list_output += "     " + (i+1) + ". "
                     + "[" + task_list[i].TaskStatus() + "]"
                     + task_list[i].getContent() + "\n";
