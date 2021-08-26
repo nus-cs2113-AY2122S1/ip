6e7dd1f1076bef95e40ec47dcd6067389d8922ed
EdwardZYWang <wangzy.edward@u.nus.edu>
2021-08-26 18:04:03 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,29 +6,55 @@ public class Duke {
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
-        String[] storage = new String[100];
-        int i = 0;
+        Task[] storage = new Task[100];
+        int inputCount = 0;
+        String status;
+        String[] lineSplitter;
+        int taskNumber = 0;
 
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
 
-        //beginning of level-1
         line = in.nextLine();
         while (!(line.equals("bye"))) {
-            if (!(line.equals("list"))) {
-                storage[i] = line;
-                i++;
-                //System.out.println(i); //check which iteration i is at
-                System.out.println("    added: " + line);
-                line = in.nextLine();
-            }
 
             if (line.equals("list")) {
-                for (int j = 0; j < i; j++) {
-                    System.out.println("    " + (j + 1) + ". " + storage[j]);
+                System.out.println("    Here are the tasks in your list:");
+
+                for (int outputCount = 0; outputCount < inputCount; outputCount++) {
+                    status = storage[outputCount].getStatusIcon();
+                    System.out.println("    " + (outputCount + 1) + ".[" + status + "] " + storage[outputCount].description);
                 }
+                line = in.nextLine();
+
+            } else if (line.contains("done")) {
+                lineSplitter = line.split(" ");
+                taskNumber = Integer.parseInt(lineSplitter[1]);
+                taskNumber = taskNumber - 1;
+
+                if (((taskNumber + 1) > 0) && ((taskNumber + 1) < inputCount)) { //check for invalid inputs
+
+                    storage[taskNumber].markAsDone(); //mark x in [ ]
+                    System.out.println("    Nice! I've marked this task as done:");
+                    status = storage[taskNumber].getStatusIcon();
+
+                    System.out.println("    [" + status + "] " + storage[taskNumber].description);
+                    line = in.nextLine();
+
+                } else {
+                    System.out.println("    Sorry, Invalid Input! Please check the list again!");
+                    line = in.nextLine();
+                }
+
+            } else {
+                System.out.println("    added: " + line);
+                Task t = new Task(line);
+                storage[inputCount] = t;
+                inputCount++;
+
                 line = in.nextLine();
             }
         }
         System.out.println("    " + "Bye. Hope to see you again soon!");
     }
 }
+
