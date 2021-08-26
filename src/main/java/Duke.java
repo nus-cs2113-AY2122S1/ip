da7c42bad992f14640a26255749509044ef70449
Edwin <zizo_edwin@hotmail.com>
2021-08-26 20:36:43 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,14 +12,29 @@ public class Duke {
         System.out.println("What do you want?\n" + border);
         System.out.println("Type bye to exit\n" + border);
         String line;
+        String[] items = new String[100];
+        int i = 0;
         do {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
-            System.out.println(border + line + '\n' + border);
+            if (line.matches("list")) {
+                int j = 1;
+                System.out.println(border);
+                for (String item : items) {
+                    if (item != null) {
+                        System.out.println(j + ". " + item);
+                        j++;
+                    }
+                }
+                System.out.println(border);
+            } else if (!line.matches("bye")) {
+                System.out.println(border + "added: " + line + '\n' + border);
+                items[i] = line;
+                i++;
+            }
         }
-        while(!line.matches("bye"));
-            System.out.println("chat again next time!\n" + border);
-        }
-
-       // System.out.println("Hello from\n" + logo);
+        while (!line.matches("bye"));
+        System.out.println(border);
+        System.out.println("chat again next time!\n" + border);
+    }
 }
