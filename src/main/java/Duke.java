f77e4b650662c609cd6e77e93fc95fc74f1b6a56
husysg <husysg@icloud.com>
2021-08-26 15:51:45 +0800

    add addList() and showList() to Duke(Level-2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,12 @@
 // import libraries here
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> list = new ArrayList<String>();
+//    private static ArrayList<Integer> done = new ArrayList<Integer>();
+//    private static int count = 0;
+
     public static void greeting() {
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Duke");
@@ -24,6 +29,33 @@ public class Duke {
         System.out.println("     Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
     }
+    public static void showList() {
+        System.out.println("    ____________________________________________________________");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("     %d. %s", i + 1, list.get(i));
+            System.out.println();
+        }
+        System.out.println("    ____________________________________________________________");
+    }
+    public static void addList() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        // get user input
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            if (line.equals("list")) {showList(); line = in.nextLine(); continue;}
+            System.out.println("    ____________________________________________________________");
+            list.add(line);
+//            done.add(0);
+//            count++;
+            System.out.println("     added: " + line);
+            System.out.println("    ____________________________________________________________");
+            line = in.nextLine();
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -32,6 +64,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greeting();
-        echo();
+//        echo();
+        addList();
     }
 }
