42d970360f1d2fd5fe724552f07c5710aeda9773
kahhe <69589263+kahhe@users.noreply.github.com>
2021-08-26 17:12:53 +0800

    Add comments and adjust to coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static final Task[] list = new Task[100];
+    private static final Task list[] = new Task[100];
     private static int taskCount = 0;
 
     public static void main(String[] args) {
@@ -21,47 +21,76 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
 //    }
 
+    /**
+     * Prints horizontal line to demarcate text from Chatbot.
+     */
     public static void printLine() {
         System.out.println("    ____________________________________________________________");
     }
+
+    /**
+     * Prints greeting message to user when code is ran.
+     */
     public static void greetUser() {
         printLine();
-        System.out.println("      你好! I'm Duke\n      What can I do for you?");
+        System.out.println("      Hello! I'm Duke\n      What can I do for you?");
         printLine();
     }
+
+    /**
+     * Prints farewell message to user.
+     */
     public static void byeUser() {
         printLine();
         System.out.println("      Bye. Hope to see you again soon!");
         printLine();
     }
 
+    /**
+     * Prints the list of tasks collated by the chatbot.
+     */
     public static void printList() {
-        System.out.println("Here are the tasks in your list:");
+        System.out.println("      Here are the tasks in your list:");
         for (int i = 0; i < taskCount; i++) {
             System.out.println("      " + (i + 1) + ".[" + list[i].getStatusIcon() + "] " + list[i].description);
         }
     }
+
+    /**
+     * Adds input from user to list[] to keep track of user's tasks.
+     *
+     * @param userInput text the user has typed as task to add to list.
+     */
     public static void addTask(String userInput) {
-//        list[taskCount].description = userInput;
         list[taskCount] = new Task(userInput);
         taskCount++;
-
         printLine();
         System.out.println("      added: " + userInput);
         printLine();
     }
+
+    /**
+     * Marks tasks in list[] as done, if they exist or has not been completed.
+     *
+     * @param taskNumber Number n associated with the task, where n is the nth task in list.
+     */
     public static void crossOutTask(int taskNumber) {
         printLine();
         if (taskNumber < 0 || taskNumber >= taskCount) {
-            System.out.println("Task does not exist :P");
+            System.out.println("      Task does not exist :P");
+        } else if (list[taskNumber].isDone) {
+            System.out.println("      Task has already been done.");
         } else {
             list[taskNumber].isDone = true;
             System.out.println("      Nice! I've marked this task as done:");
-            System.out.println("        [X] " + list[taskNumber].description);
+            System.out.println("        [" + list[taskNumber].getStatusIcon() + "] " + list[taskNumber].description);
         }
         printLine();
     }
 
+    /**
+     * Engages user base on what the user has typed and executes that command.
+     */
     public static void engageUser() {
         Scanner text = new Scanner(System.in);
         String userInput;
@@ -69,6 +98,7 @@ public class Duke {
 
         do {
             userInput = text.next();
+
             if (userInput.equals("done")) {
                 taskNumber = text.next();
             } else {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,13 +2,23 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructor for Task, generates name and status of task.
+     *
+     * @param description Name of task
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Checks if task has been completed and returns the corresponding symbol.
+     *
+     * @return Whitespace if task is not completed, tick if task is completed.
+     */
     public String getStatusIcon() {
-        return (isDone ? "X" : " ");
+        return (isDone ? "✔" : " ");
     }
 
 }
