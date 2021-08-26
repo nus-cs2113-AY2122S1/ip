c31964df04a07c2b3e04ed9f1fea0e6ed27f2062
mudkip8 <mahvin1970@hotmail.com>
2021-08-26 09:51:09 +0800

    Edited to fit Coding Standards

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -2,30 +2,32 @@ public class TaskManager {
     private Task[] tasks = new Task[100];
     private int numTask = 0;
 
+    private static String GREETING = "****************************\n"
+            + "*  ____             ____   *\n"
+            + "* |  _ \\    ____   |  _ \\  *\n"
+            + "* | |_| |  / __ \\  | |_| | *\n"
+            + "* |  _  | | |  | | |  _  | *\n"
+            + "* | |_| | | |__| | | |_| | *\n"
+            + "* |____/   \\____/  |____/  *\n"
+            + "****************************\n"
+            + "_________________________________________\n"
+            + "Have no fear, Bob is here!\n"
+            + "What is it that you require?\n"
+            + "_________________________________________\n";
+
+    private static String EXIT_MESSAGE = "_________________________________________\n"
+            + "Bye. Have a nice day!\n"
+            + "_________________________________________\n";
+
     public TaskManager() {
     }
 
     public void greet() {
-        String greeting = "****************************\n"
-                + "*  ____             ____   *\n"
-                + "* |  _ \\    ____   |  _ \\  *\n"
-                + "* | |_| |  / __ \\  | |_| | *\n"
-                + "* |  _  | | |  | | |  _  | *\n"
-                + "* | |_| | | |__| | | |_| | *\n"
-                + "* |____/   \\____/  |____/  *\n"
-                + "****************************\n"
-                + "_________________________________________\n"
-                + "Have no fear, Bob is here!\n"
-                + "What is it that you require?\n"
-                + "_________________________________________\n";
-        System.out.println(greeting);
+        System.out.println(GREETING);
     }
 
     public void exitMessage() {
-        String message = "_________________________________________\n"
-                + "Bye. Have a nice day!\n"
-                + "_________________________________________\n";
-        System.out.println(message);
+        System.out.println(EXIT_MESSAGE);
     }
 
     public void addTask(String task) {
