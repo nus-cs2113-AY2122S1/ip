1814bd1edf5636476a63a7c6d0167a9fa4dc6857
teoziyiivy <e0559117@u.nus.edu>
2021-08-26 10:28:33 +0800

    Level-3

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -2,28 +2,33 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Greet {
-    public static void main (String[] args) {
-        String[] tasks = new String[100];
+    public static void main(String[] args) {
+        Task[] tasks = new Task[100];
         int ntasks = 0;
         System.out.println("Hello! I'm Duke");
         String line;
         Scanner in = new Scanner(System.in);
         System.out.println("What can I do for you?");
         line = in.nextLine();
-        boolean bool1 = false;
-        while (!bool1) {
-            System.out.println("added: " + line);
-            tasks[ntasks]= line;
-            ntasks += 1;
-            line = in.nextLine();
+        while (!line.equals("bye")) {
             if (line.equals("list")) {
                 for (int i = 0; i < ntasks; i += 1) {
-                    System.out.println((i+1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
             }
-            if (line.contains("bye")) {
-                bool1 = true;
+            else if (line.contains("done")) {
+                String[] splitTask = line.split(" ");
+                int index = Integer.parseInt(splitTask[1]) - 1;
+                tasks[index].setDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + tasks[index].getStatusIcon() + "] " + tasks[index].getDescription());
             }
+            else {
+                tasks[ntasks] = new Task(line);
+                ntasks += 1;
+                System.out.println("added: " + line);
+            }
+            line = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
