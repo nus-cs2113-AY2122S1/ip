61266391d4bbfdf3a3ef7aac2900a1ec0dd63331
Yap Wei Xuan <e0533997@u.nus.edu>
2021-08-26 22:18:43 +0800

    Level-1, Implement Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String input;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "____________________________________________________________\n";
+        Scanner in = new Scanner(System.in);
         System.out.println(line + logo +
                 " Hello! I'm Duke\n" +
-                " What can I do for you?\n" + line +
-                " Bye. Hope to see you again soon!\n" + line);
+                " What's up? :p\n" + line);
+
+        input = in.nextLine();
+        while(true) {
+            if(input.equals("bye")) {
+                break;
+            }
+            System.out.println(line + input + "\n" + line);
+            input = in.nextLine();
+        }
+
+        System.out.println(line + "Bye, seeya!\n" + line);
     }
 }
