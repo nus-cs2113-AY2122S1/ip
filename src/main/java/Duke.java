6f09f0415ab3f1c52e9adfc8c9c45ec125731754
Demonshaha <e0492492@u.nus.edu>
2021-08-26 17:04:55 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -14,6 +16,15 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
         System.out.println("____________________________________________________________");
+        Scanner in = new Scanner(System.in);
+        String order = in.nextLine();
+        while (!order.equals("bye")){
+            System.out.println("____________________________________________________________");
+            System.out.println(order);
+            System.out.println("____________________________________________________________");
+            order = in.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you a gain soon!\n");
         System.out.println("____________________________________________________________");
     }
