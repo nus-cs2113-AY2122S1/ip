0033e6ef0702b451f703a77b03b06e8890b56667
pragyan_01 <samal@u.nus.edu>
2021-08-26 23:23:08 +0800

    Add Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
+        String line = "------------------------------------------------------------------------------------------\n";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(line + "Hello! I'm Duke.\n" + logo + "What can i do for you?\n" + line + "Bye! Hope to see you again soon.\n" + line);
     }
 }
