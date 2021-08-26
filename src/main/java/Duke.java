ea682be445e9548400bb97b2ecc25fb22388da19
Herrekt <herrickkohyukan@gmail.com>
2021-08-26 15:13:10 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String greeting = "Hello! I'm Duke" + "\n"
                 + "What can I do for you?";
+
         String farewell = "Bye. Hope to see you again soon!";
+
         System.out.println(greeting);
+
+        Scanner sc = new Scanner(System.in);
+
+        String phrase = sc.nextLine();
+
+        while (!phrase.equals("bye")) {
+            System.out.println(phrase);
+            phrase = sc.nextLine();
+        }
+
         System.out.println(farewell);
     }
 }
