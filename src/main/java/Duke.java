2a642b79629962bc75d79cb58c9a8b046bef84fe
alwinangys <alwinang99@gmail.com>
2021-08-27 00:32:50 +0800

    Add function to print horizontal line, ability to undo tasks, chat bot logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,37 +5,85 @@ public class Duke {
     public static void addTask(String input, Task[] tasks, int taskCount) {
         Task t = new Task(input);
         tasks[taskCount] = t;
+        printHorizontalLine();
         System.out.println("added: " + input);
+        printHorizontalLine();
     }
 
     public static void requestList(Task[] tasks, int taskCount) {
+        printHorizontalLine();
         if (taskCount == 1) {
             System.out.println("There are no tasks!");
+            printHorizontalLine();
             return;
         }
         System.out.println("Here are the tasks in your list:");
         for (int i = 1; i < taskCount; i++) {
             System.out.println(tasks[i].taskId + "." + tasks[i].getStatusIcon() + " " + tasks[i].description);
         }
+        printHorizontalLine();
     }
 
     public static void inputBye() {
+        printHorizontalLine();
         System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
     }
 
     public static void inputDone(String line, Task[] tasks) {
         int dividePos = line.indexOf(" ");
         int taskNumber = Integer.parseInt(line.trim().substring(dividePos + 1));
         Task t = tasks[taskNumber];
+        if (t.isDone) {
+            printHorizontalLine();
+            System.out.println("This task has already been done, complete something else!");
+            printHorizontalLine();
+            return;
+        }
         t.markAsDone();
+        printHorizontalLine();
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(t.getStatusIcon() + " " + t.description);
+        printHorizontalLine();
+    }
+
+    public static void undoDone(String line, Task[] tasks) {
+        int dividePos = line.indexOf(" ");
+        int taskNumber = Integer.parseInt(line.trim().substring(dividePos + 1));
+        Task t = tasks[taskNumber];
+        if (!t.isDone) {
+            printHorizontalLine();
+            System.out.println("This task has not been done yet!");
+            printHorizontalLine();
+            return;
+        }
+        t.markAsNotDone();
+        printHorizontalLine();
+        System.out.println("I've undone this task for you: ");
+        System.out.println(t.getStatusIcon() + " " + t.description);
+        printHorizontalLine();
+    }
+
+    public static void printHorizontalLine() {
+       System.out.println("____________________________________________________________") ;
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        String logo = "    #    ####### #          #     #####  \n"
+                + "   # #      #    #         # #   #     # \n"
+                + "  #   #     #    #        #   #  #       \n"
+                + " #     #    #    #       #     #  #####  \n"
+                + " #######    #    #       #######       # \n"
+                + " #     #    #    #       #     # #     # \n"
+                + " #     #    #    ####### #     #  #####  \n";
+
+        System.out.println("Hello from\n" + logo);
+
+        printHorizontalLine();
+        System.out.println("Hello! I'm Atlas!");
+        System.out.println("What can I do for you today?");
+        printHorizontalLine();
 
         String line;
         boolean isBye = false;
@@ -53,6 +101,8 @@ public class Duke {
                 requestList(tasks, taskCount);
             } else if (line.contains("done")) {
                 inputDone(line, tasks);
+            } else if (line.contains("undo")) {
+                undoDone(line, tasks);
             } else {
                 addTask(line, tasks, taskCount);
                 taskCount++;
