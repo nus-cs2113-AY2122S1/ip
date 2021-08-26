bb72c84a97e4c4f8f3482f82a10a46ffe536af2f
swongts <70316271+swongts@users.noreply.github.com>
2021-08-26 21:57:12 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,51 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String greeting = " Hello! I'm Duke\n" +
+    public static void greeting() {
+        String greeting = " Hello! I'm pepepopo\n" +
                         " What can I do for you?";
+        System.out.println(greeting);
+        divider();
+    }
 
-        String bye = " Bye. Hope to see you again soon!";
-
+    public static void divider() {
         String divider = "\n____________________________________________________________";
-
         System.out.println(divider);
-        System.out.println(greeting + divider);
-        System.out.println(bye + divider);
+    }
 
+    public static void bye() {
+        String bye = " Bye. Hope to see you again soon!";
+        System.out.println(bye);
+        divider();
+
+    }
+
+    public static String input() {
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        line = in.nextLine();
+        return line;
+    }
+
+    public static void echo(String text) {
+        if (!text.equals("bye")) {
+            System.out.println(text);
+            divider();
+
+            echo(input());
+        }
+        else {
+            bye();
+        }
+    }
+
+    public static void main(String[] args) {
+//        String line;
+//        Scanner in = new Scanner(System.in);
+
+        greeting();
+//        line = in.nextLine();
+        echo(input());
     }
 }
