9b36019e5810112adc23b1d3e1696d5c879a0363
LimJiaJing <jiajinglim06.study@gmail.com>
2021-08-26 22:27:45 +0800

    Coding standard.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class Duke {
     private static ArrayList<Task> tasks = new ArrayList<Task>();
     private static String divider = "\t__________________________________________________________";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,57 +16,58 @@ public class Duke {
         talkToUser();
         Greeting.closingGreet();
     }
+
     private static void talkToUser() {
-        Scanner in=new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         String userInput;
         do {
             System.out.println();
             userInput = in.nextLine();
             System.out.println(divider);
-            if (userInput.contains("done")){
-                int taskNumber=Integer.parseInt(userInput.substring(5))-1;
+            if (userInput.contains("done")) {
+                int taskNumber = Integer.parseInt(userInput.substring(5)) - 1;
                 markDone(taskNumber);
                 continue;
-            }
-            else if(userInput.equals("list")){
+            } else if (userInput.equals("list")) {
                 printTasks();
                 continue;
-            }
-            else if (userInput.equals("bye")){
+            } else if (userInput.equals("bye")) {
                 break;
             }
             addTask(userInput);
             System.out.println("\tadded: " + userInput);
             System.out.println(divider);
-        } while(true);
+        } while (true);
     }
-    private static void addTask(String userInput){
-        Task newTask= new Task(userInput);
+
+    private static void addTask(String userInput) {
+        Task newTask = new Task(userInput);
         tasks.add(newTask);
     }
-    private static void printTasks(){
-        for (int i=0; i<tasks.size();i++){
+
+    private static void printTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
             boolean isDone = tasks.get(i).getIsDone();
-            System.out.print(i+1);
-            if(isDone==true){
+            System.out.print(i + 1);
+            if (isDone == true) {
                 System.out.print(".[X] ");
-            }
-            else{
+            } else {
                 System.out.print(".[ ] ");
             }
             System.out.println(tasks.get(i).getName());
         }
         System.out.println(divider);
     }
-    private static void markDone(int taskNumber){
-        if (taskNumber>=tasks.size()){
+
+    private static void markDone(int taskNumber) {
+        if (taskNumber >= tasks.size()) {
             System.out.println("No such task");
             System.out.println(divider);
             return;
         }
         tasks.get(taskNumber).setIsDone();
         System.out.println("\tNice! I've marked this task as done:");
-        System.out.println("\t\t[X] "+ tasks.get(taskNumber).getName());
+        System.out.println("\t\t[X] " + tasks.get(taskNumber).getName());
         System.out.println(divider);
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,16 +1,20 @@
 public class Task {
     String name;
-    boolean isDone=false;
-    public Task(String name){
-        this.name=name;
+    boolean isDone = false;
+
+    public Task(String name) {
+        this.name = name;
     }
+
     public String getName() {
         return name;
     }
-    public boolean getIsDone(){
+
+    public boolean getIsDone() {
         return isDone;
     }
-    public void setIsDone(){
-        isDone=true;
+
+    public void setIsDone() {
+        isDone = true;
     }
 }
