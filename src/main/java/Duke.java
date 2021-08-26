2c94e9f94002cf27d637ac9a5d898d280dafbcd7
Jon-the-MELON <limyongkiat@yahoo.com.sg>
2021-08-26 15:22:18 +0800

    Level 2.0: Echo + List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,77 @@
 import java.util.Scanner;
 
+
 public class Duke {
-    public static void greeting( boolean isStart) {
+    public static void greeting(boolean isStart) {
         if (isStart) {
+            //        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
             String greeting = "____________________________________________________________\n" +
-                    " Hello! I'm Duke\n" +
-                    " What can I do for you?\n" +
+                    " Hello! I'm Duke, the listing and echoing bot!\n" +
+                    " What can I do for you today?\n" +
                     "____________________________________________________________\n";
             System.out.println(greeting);
         }
     }
 
-    public static void echo (String x){
-        String output = "____________________________________________________________\n" +
+    public static void echo(String x) {
+        String output = "____________________________________________________________\n" + " " +
                 x + "\n" +
                 "____________________________________________________________\n";
         System.out.println(output);
     }
-    public static void main (String[]args) {
+
+    public static void main(String[] args) {
         greeting(true);
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        while (!input.equals("bye")) {
-            echo(input);
-            input = sc.nextLine();
+        String[] List = new String[100];
+        String LINES = "____________________________________________________________\n";
+        int listCount = 0;
+        boolean isBye = false;
+        while (!isBye) {
+            String input = sc.nextLine();
+            //echo function
+            if (input.equalsIgnoreCase("echo")) {
+                System.out.println(LINES + " echoing begins\n" + LINES);
+                boolean echoState = true;
+                while (echoState) {
+                    String s = sc.nextLine();
+                    if (s.equalsIgnoreCase("quit")) {
+                        System.out.println(LINES + " quit echoing\n" + LINES);
+                        echoState = false;
+                    } else {
+                        echo(s);
+                    }
+                }
+            }
+            else if (input.equalsIgnoreCase("bye")) {
+                String byeMsg = LINES +
+                        " Bye. Hope to see you again soon!\n" +
+                        LINES;
+                System.out.println(byeMsg);
+                isBye = true;
+            }
+            else if (input.equalsIgnoreCase("list")) {
+                System.out.print(LINES);
+                for (int i = 0; i < listCount; i++) {
+                    int j = i + 1; //
+                    System.out.println(j + ". " + List[i]);
+                }
+                System.out.print(LINES);
+            }
+            else if (input.contains("birthday")) {
+                System.out.println(LINES + " ^o^ Happy birthday to you! ^o^\n" + LINES);
+            }
+            else {
+                List[listCount] = input;
+                listCount += 1;
+                String addMsg = LINES + " added: " + input + "\n" + LINES;
+                System.out.println(addMsg);
+            }
         }
-        System.out.println("____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
     }
 }
