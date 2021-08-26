ece52c83f555d04b4489a596f384d477ef9ffdc2
nishantrai-nus <e0560182@u.nus.edu>
2021-08-26 10:13:10 +0800

    add level 2 tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,11 +4,21 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke. \nWhat can I do for you?");
         String line;
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while (!line.equals("bye")) {
-            System.out.println(line);
-            line = in.nextLine();
+            if (line.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                line = in.nextLine();
+            } else {
+                System.out.println("added: " + line);
+                tasks[taskCount++] = line;
+                line = in.nextLine();
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
