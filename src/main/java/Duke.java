cac3d8e5690d66855f69f761fca84f6fadbd3eee
Mist0919 <XUEY0013@e.ntu.edu.sg>
2021-08-26 22:56:36 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,31 @@
 import java.util.Scanner;
-public class Duke {
+
+public class Main {
+
+    private static String[] l = new String[100];
+    private static int listSize = 0;
+
+    public static void addToList(String input) {
+        l[listSize] =  input;
+        listSize++;
+        System.out.println("    ____________________________________________________________\n"+
+                "Added: "+input + "\n" +
+                "    ____________________________________________________________\n");
+    }
+
+    public static void printItems(){
+        if (listSize == 0){
+            System.out.println("The list is empty.");
+        }
+        else{
+            System.out.println("    ____________________________________________________________\n");
+            for (int i = 1; i<=listSize; i++){
+                System.out.println(i + ". " + l[i-1]);
+            }
+            System.out.println("    ____________________________________________________________\n");
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,22 +34,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm Duke\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n");
+        System.out.println("    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n");
         Scanner input = new Scanner(System.in);
         while (true){
             String str = input.nextLine();
-            if (str.equals("bye")) {
-                System.out.println("____________________________________________________________\n" +
-                        "Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________\n");
+            if (str.equals("bye")){
+                System.out.println("    ____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "    ____________________________________________________________\n");
                 break;
             }
-            System.out.println("____________________________________________________________\n" +
-                    str + "\n" +
-                    "____________________________________________________________\n");
+            else if (str.equals("list")){
+                printItems();
+            }
+            else {
+                addToList(str);
+            }
         }
+
+
     }
 }
