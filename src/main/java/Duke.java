a2cbad2a3c9afa82eb6aa79a42f87d0dd83b6825
Demonshaha <e0492492@u.nus.edu>
2021-08-26 17:09:21 +0800

    level 2 and level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,24 +8,64 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         greeting();
     }
 
-    public static void greeting(){
+    public static void showList(Task list[], int x){
+        if(x == 0)  System.out.println("\n____________________________________________________________");
+        else{
+            System.out.println("\n____________________________________________________________");
+            for(int i = 0; i < x; i++){
+                System.out.println("[" + list[i].getStatusIcon() + "]" + list[i].getDescription());
+            }
+            System.out.println("\n____________________________________________________________");
+        }
+    }
+
+    public static void doneTask(Task list[], String order){
+        String dummy = order.trim();
+        int index = Integer.parseInt(dummy.substring(5,dummy.length()));
+        list[index-1].markAsDone();
+        System.out.println("\n____________________________________________________________");
+        System.out.println("Nice Work! I've marked it as done");
+        System.out.println("[" + list[index-1].getStatusIcon() + "]" + list[index-1].getDescription());
+        System.out.println("\n____________________________________________________________");
+    }
+
+    public static void greeting () {
+        Task list[] = new Task[100];
+        int maxcount = 0;
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
-        System.out.println("____________________________________________________________");
-        Scanner in = new Scanner(System.in);
-        String order = in.nextLine();
-        while (!order.equals("bye")){
-            System.out.println("____________________________________________________________");
-            System.out.println(order);
-            System.out.println("____________________________________________________________");
-            order = in.nextLine();
-        }
+        String order = getMission();
 
-        System.out.println("Bye. Hope to see you a gain soon!\n");
+        while(!order.equals("bye")){
+            if (order.equals("list")){
+                showList(list, maxcount);
+                order = getMission();
+                continue;
+            }
+            else if(order.contains("done")){
+                doneTask(list, order);
+                order = getMission();
+                continue;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println("added: " + order);
+            System.out.println("____________________________________________________________\n");
+            list[maxcount] = new Task(order);
+            maxcount++;
+            order = getMission();
+        }
+        System.out.println("Bye.Hope to see you again soon!\n");
         System.out.println("____________________________________________________________");
+    }
+
+    public static String getMission(){
+        String mission;
+        Scanner in = new Scanner(System.in);
+        mission = in.nextLine();
+        return mission;
     }
 }
