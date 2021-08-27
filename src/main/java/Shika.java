3f390f72010cc06f98e1fd1d489e6d1ac21d3fcd
ChewyChair <69298017+ChewyChair@users.noreply.github.com>
2021-08-27 22:22:33 +0800

    Extract UI as a class, update slight changes to EXPECTED.txt.

--- src/main/java/Shika.java
+++ src/main/java/Shika.java
@@ -16,19 +16,28 @@ import shikabot.exception.TaskNegativeException;
 import shikabot.exception.TaskNotFoundException;
 import shikabot.exception.InvalidEventException;
 import shikabot.exception.InvalidDeadlineException;
+import shikabot.ui.TextUi;
 
 public class Shika {
 
-    public static String line = "____________________________________________________________________________\n";
     public static ArrayList<Task> tasks = new ArrayList<>();
 
-    /**
-     * Main function that calls other functions to run Shika.
-     * @param args Command line arguments.
-     */
+    public TextUi ui;
+
     public static void main(String[] args) {
-        printLogo();
-        startShika();
+        new Shika().run();
+    }
+
+    /**
+     * Function that calls other functions to run Shika.
+     */
+    public void run() {
+        try {
+            setupShika();
+        } catch (FileNotFoundException e) {
+            System.out.println("I can't find the save file AHHHHHHH.\n");
+        }
+        runShika();
     }
 
     /**
@@ -36,10 +45,11 @@ public class Shika {
      * it creates them.
      * @throws FileNotFoundException when ShikaTasks.txt is not found.
      */
-    public static void setupShika() throws FileNotFoundException {
+    public void setupShika() throws FileNotFoundException {
+        this.ui = new TextUi();
         File f = new File("data/ShikaTasks.txt");
-        String greeting = (setupSave(f)) ? "Hello, friend! " : "Welcome back, friend! ";
-        System.out.println(greeting + "Shika at your service! ^-^\n");
+        ui.printLogo();
+        ui.printWelcomeMessage(setupSave(f));
         loadTasks(f);
     }
 
@@ -60,7 +70,7 @@ public class Shika {
      * @param f File to create.
      * @return true if file is created, false otherwise
      */
-    private static boolean setupSave(File f) {
+    private boolean setupSave(File f) {
         try {
             if (!f.exists()) {
                 f.getParentFile().mkdirs();
@@ -69,9 +79,9 @@ public class Shika {
                 return true;
             }
         } catch (IOException e) {
-            System.out.println("Something went wrong during file creation :/");
+            ui.printFileErrorMessage();
         } catch (SecurityException e) {
-            System.out.println("Shika isn't allowed to write in this location :<");
+            ui.printSecurityErrorMessage();
         }
         return false;
     }
@@ -123,63 +133,40 @@ public class Shika {
      * This function saves tasks to data/ShikaTasks.txt. It rewrites the txt from scratch.
      * @throws IOException when the saving operation is interrupted.
      */
-    public static void saveTasks() throws IOException {
+    public void saveTasks() throws IOException {
         FileWriter fw = new FileWriter("data/ShikaTasks.txt");
         fw.close();
         for (int i = 0; i < Task.count; i++) {
             try {
                 tasks.get(i).saveTask();
             } catch (IOException e) {
-                System.out.println("Problem occurred when saving T.T");
+                ui.printSaveErrorMessage();
             }
         }
     }
 
-    /**
-     * Function that prints Shika logo.
-     */
-    public static void printLogo() {
-        String logo = "  _________.__    .__ __            \n"
-                + " /   _____/|  |__ |__|  | _______   \n"
-                + " \\_____  \\ |  |  \\|  |  |/ /\\__  \\  \n"
-                + " /        \\|   Y  \\  |    <  / __ \\_\n"
-                + "/_______  /|___|  /__|__|_ \\(____  /\n"
-                + "        \\/      \\/        \\/     \\/ \n";
-        System.out.println(logo);
-    }
 
-    /**
-     * This function setups Shika, then runs Shika.
-     */
-    public static void startShika() {
-        try {
-            setupShika();
-        } catch (FileNotFoundException e) {
-            System.out.println("I can't find the save file AHHHHHHH.\n");
-        }
-        runShika(tasks);
-    }
+
 
     /**
      * Function that calls getCommand() in a loop to run Shika. Loop can be exited by inputting "bye".
-     * @param tasks Arraylist containing all recorded tasks.
      */
-    public static void runShika(ArrayList<Task> tasks) {
+    public void runShika() {
         Scanner in = new Scanner(System.in);
         String text;
         while(in.hasNextLine()) {
             text = in.nextLine();
             if (text.trim().equals("bye")) {
-                System.out.print(line + "> Bye friend!\n> See you again! :)\n" + line);
+                ui.printExitMessage();
                 return;
             }
             try {
                 getCommand(text);
                 saveTasks();
             } catch (InvalidCommandException e) {
-                System.out.print(line + "> Sorry friend, I don't know what that means. :/\n" + line);
+                ui.printUnknownCommandMessage();
             } catch (IOException e) {
-                System.out.println("Problem occurred when saving T.T");
+                ui.printSaveErrorMessage();
             }
         }
     }
@@ -189,7 +176,7 @@ public class Shika {
      * @param text String containing user input.
      * @return true if string starts with "todo", "deadline" or "event" and false otherwise.
      */
-    public static boolean isAddCommand(String text) {
+    public boolean isAddCommand(String text) {
         return text.startsWith("todo") || text.startsWith("deadline") || text.startsWith("event");
     }
 
@@ -202,10 +189,10 @@ public class Shika {
      * @param text String containing user input.
      * @throws InvalidCommandException thrown when command is invalid.
      */
-    public static void getCommand(String text) throws InvalidCommandException {
+    public void getCommand(String text) throws InvalidCommandException {
         text = text.trim();
         if (text.equals("list")) {
-            printTasks();
+            ui.printTasks(tasks, Task.count);
         } else if (text.startsWith("done")) {
             doTask(text);
         } else if (text.startsWith("delete")) {
@@ -221,20 +208,18 @@ public class Shika {
      * Function that deletes specified task from the Arraylist.
      * @param text String containing user input.
      */
-    public static void deleteTask(String text) {
+    public void deleteTask(String text) {
         String str = text.substring(text.indexOf("delete") + 6).trim();
         try {
             int index = Integer.parseInt(str) - 1;
-            System.out.println(line + "> You've removed: " + "\n\t"
-                    + (index + 1) + ". " + tasks.get(index).toString());
+            ui.printDeleteTaskMessage(tasks, index);
             tasks.remove(index);
             Task.count -= 1;
-            printTaskCount();
-            System.out.print(line);
+            ui.printTaskCount(Task.count);
         } catch (NumberFormatException e) {
-            System.out.print(line + "> Please key in a number.\n" + line);
+            ui.printNumberFormatMessage();
         } catch (IndexOutOfBoundsException e) {
-            System.out.print(line + "> That task does not exist.\n" + line);
+            ui.printInvalidTaskMessage();
         }
     }
 
@@ -243,22 +228,20 @@ public class Shika {
      * is invalid, printing error messages.
      * @param text String containing user input.
      */
-    public static void addTask(String text) {
+    public void addTask(String text) {
         if (text.startsWith("todo")) {
             addTodo(text);
         } else if (text.startsWith("deadline")) {
             try {
                 addDeadline(text);
             } catch (InvalidDeadlineException e) {
-                System.out.print(line + "Please follow the format [NAME] /by [DEADLINE]. " +
-                        "Thank you!\n" + line);
+                ui.printDeadlineSyntaxMessage();
             }
         } else {
             try {
                 addEvent(text);
             } catch (InvalidEventException e) {
-                System.out.print(line + "Please follow the format [NAME] /at [DURATION]. " +
-                        "Thank you!\n" + line);
+                ui.printEventSyntaxMessage();
             }
         }
     }
@@ -267,14 +250,12 @@ public class Shika {
      * This function adds the todo specified by the user to the list.
      * @param text String containing user input.
      */
-    public static void addTodo(String text) {
+    public void addTodo(String text) {
         String str = text.substring(text.indexOf("todo") + 4).trim();
         tasks.add(new Todo(str));
         Task.count += 1;
-        System.out.println(line + "> Added: " + "\n\t"
-                + Task.count + ". " + tasks.get(Task.count - 1).toString());
-        printTaskCount();
-        System.out.print(line);
+        ui.printAddTaskMessage(tasks, Task.count - 1);
+        ui.printTaskCount(Task.count);
     }
 
     /**
@@ -282,7 +263,7 @@ public class Shika {
      * @param text String containing user input.
      * @throws InvalidDeadlineException is thrown when command syntax is not followed.
      */
-    public static void addDeadline(String text) throws InvalidDeadlineException  {
+    public void addDeadline(String text) throws InvalidDeadlineException  {
         if (!text.contains("/by")) {
             throw new InvalidDeadlineException();
         }
@@ -290,10 +271,8 @@ public class Shika {
         String by = text.substring(text.indexOf("/by") + 3).trim();
         tasks.add(new Deadline(str, by));
         Task.count += 1;
-        System.out.println(line + "> Added: " + "\n\t"
-                + Task.count + ". " + tasks.get(Task.count - 1).toString());
-        printTaskCount();
-        System.out.print(line);
+        ui.printAddTaskMessage(tasks, Task.count - 1);
+        ui.printTaskCount(Task.count);
     }
 
     /**
@@ -301,7 +280,7 @@ public class Shika {
      * @param text String containing user input.
      * @throws InvalidEventException is thrown when command syntax is not followed.
      */
-    public static void addEvent(String text) throws InvalidEventException {
+    public void addEvent(String text) throws InvalidEventException {
         if (!text.contains("/at")) {
             throw new InvalidEventException();
         }
@@ -309,10 +288,8 @@ public class Shika {
         String at = text.substring(text.indexOf("/at") + 3).trim();
         tasks.add(new Event(str, at));
         Task.count += 1;
-        System.out.println(line + "> Added: " + "\n\t"
-                + Task.count + ". " + tasks.get(Task.count - 1).toString());
-        printTaskCount();
-        System.out.print(line);
+        ui.printAddTaskMessage(tasks, Task.count - 1);
+        ui.printTaskCount(Task.count);
     }
 
     /**
@@ -321,17 +298,17 @@ public class Shika {
      * If the String given is not a number or is out of bounds, it will catch the exception and print an error message.
      * @param text String that is supposed to be the number of the task.
      */
-    public static void doTask(String text) {
+    public void doTask(String text) {
         String str = text.substring(text.indexOf("done") + 4).trim();
         try {
             int index = Integer.parseInt(str);
             markAsDone(index - 1);
         } catch (NumberFormatException e) {
-            System.out.print(line + "> Please key in a number.\n" + line);
+            ui.printNumberFormatMessage();
         } catch (TaskNegativeException e) {
-            System.out.print(line + "> ...Stop trying to break me...\n" + line);
+            ui.printNegativeIndexMessage();
         } catch (TaskNotFoundException e) {
-            System.out.print(line + "> Oops! That task does not exist yet!\n" + line);
+            ui.printInvalidTaskMessage();
         }
     }
 
@@ -341,36 +318,14 @@ public class Shika {
      * @throws TaskNegativeException If index is negative.
      * @throws TaskNotFoundException If index is of a task that has not been created yet.
      */
-    public static void markAsDone(int index) throws TaskNegativeException, TaskNotFoundException {
+    public void markAsDone(int index) throws TaskNegativeException, TaskNotFoundException {
         if (index < 0) {
             throw new TaskNegativeException();
         } else if (index >= Task.count) {
             throw new TaskNotFoundException();
         }
         tasks.get(index).markAsDone();
-        System.out.println(line + "> You've done: " + "\n\t"
-                + (index + 1) + ". " + tasks.get(index).toString());
-        System.out.print(line);
-    }
-
-    /**
-     * Function to print all tasks in tasks.
-     */
-    public static void printTasks() {
-        System.out.println(line + "> Here is your list of tasks: ") ;
-        for (int i = 0; i < Task.count; i++) {
-            System.out.println("\t" + (i + 1) + ". " + tasks.get(i).toString());
-        }
-        printTaskCount();
-        System.out.print(line);
-    }
-
-    /**
-     * Function that prints the current task count.
-     */
-    public static void printTaskCount() {
-        String taskForm = (Task.count == 1) ? "task" : "tasks";
-        System.out.println("> You have " + Task.count + " " + taskForm + " on your list. -w-") ;
+        ui.printDoneTaskMessage(tasks, index);
     }
 }
 
---
+++ src/main/java/shikabot/ui/TextUi.java
@@ -0,0 +1,104 @@
+package shikabot.ui;
+
+import java.util.ArrayList;
+import shikabot.task.Task;
+
+public class TextUi {
+    public static String line = "____________________________________________________________________________\n";
+
+    /**
+     * Function that prints Shika logo.
+     */
+    public void printLogo() {
+        String logo = "  _________.__    .__ __            \n"
+                + " /   _____/|  |__ |__|  | _______   \n"
+                + " \\_____  \\ |  |  \\|  |  |/ /\\__  \\  \n"
+                + " /        \\|   Y  \\  |    <  / __ \\_\n"
+                + "/_______  /|___|  /__|__|_ \\(____  /\n"
+                + "        \\/      \\/        \\/     \\/ \n";
+        System.out.println(logo);
+    }
+
+    public void printWelcomeMessage(boolean isFirstTime) {
+        String greeting = (isFirstTime) ? "Hello, friend! " : "Welcome back, friend! ";
+        System.out.println(greeting + "Shika at your service! ^-^\n");
+    }
+
+    public void printFileErrorMessage() {
+        System.out.println("Something went wrong during file creation :/");
+    }
+
+    public void printSecurityErrorMessage() {
+        System.out.println("Shika isn't allowed to write in this location :<");
+    }
+
+    public void printSaveErrorMessage() {
+        System.out.println("Problem occurred when saving T.T");
+    }
+
+    public void printExitMessage() {
+        System.out.print(line + "> Bye friend!\n> See you again! :)\n" + line);
+    }
+
+    public void printUnknownCommandMessage() {
+        System.out.print(line + "> Sorry friend, I don't know what that means. :/\n" + line);
+    }
+
+    public void printDeleteTaskMessage(ArrayList<Task> tasks, int index) {
+        System.out.println(line + "> You've removed: " + "\n\t"
+                + (index + 1) + ". " + tasks.get(index).toString());
+    }
+
+    /**
+     * Function that prints the current task count.
+     */
+    public void printTaskCount(int count) {
+        String taskForm = (count == 1) ? "task" : "tasks";
+        System.out.println("> You have " + count + " " + taskForm + " on your list. -w-");
+        System.out.print(line);
+    }
+
+    public void printNumberFormatMessage() {
+        System.out.print(line + "> Please key in a number.\n" + line);
+    }
+
+    public void printInvalidTaskMessage() {
+        System.out.print(line + "> Oops! That task does not exist.\n" + line);
+    }
+
+    public void printDeadlineSyntaxMessage() {
+        System.out.print(line + "Please follow the format [NAME] /by [DEADLINE]. " +
+                "Thank you!\n" + line);
+    }
+
+    public void printEventSyntaxMessage() {
+        System.out.print(line + "Please follow the format [NAME] /at [DURATION]. " +
+                "Thank you!\n" + line);
+    }
+
+    public void printAddTaskMessage(ArrayList<Task> tasks, int index) {
+        System.out.println(line + "> Added: " + "\n\t"
+                + (index + 1) + ". " + tasks.get(index).toString());
+    }
+
+    public void printNegativeIndexMessage() {
+        System.out.print(line + "> ...Stop trying to break me...\n" + line);
+    }
+
+    public void printDoneTaskMessage(ArrayList<Task> tasks, int index) {
+        System.out.println(line + "> You've done: " + "\n\t"
+                + (index + 1) + ". " + tasks.get(index).toString());
+        System.out.print(line);
+    }
+
+    /**
+     * Function to print all tasks in tasks.
+     */
+    public void printTasks(ArrayList<Task> tasks, int count) {
+        System.out.println(line + "> Here is your list of tasks: ") ;
+        for (int i = 0; i < count; i++) {
+            System.out.println("\t" + (i + 1) + ". " + tasks.get(i).toString());
+        }
+        printTaskCount(count);
+    }
+}
