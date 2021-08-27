b7aeff5100b7e10c3e47e7b451e050207eb0f18f
alvynben <alvynben@gmail.com>
2021-08-27 21:30:34 +0800

    Refactor code to simplify Triss/Duke
    - Store logo, hasUserSaidBye, tasks, noOfTasks as private static
    variable in Duke class
    - Create method printWelcomeMessage() to print welcome message
    - Create method printShutdownMessage() to print shutdown message
    - Create method printAllTasks() to print all tasks
    - Create method handleUserMarkingTaskAsDone(String userInput) to
    handle cases when users wants to mark a task as done
    - Create method handleUserCreatingTask(String userInput) to handle
    cases when users want to create a new task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    private static String logo = "████████ ██████  ██ ███████ ███████ \n" +
+            "   ██    ██   ██ ██ ██      ██      \n" +
+            "   ██    ██████  ██ ███████ ███████ \n" +
+            "   ██    ██   ██ ██      ██      ██ \n" +
+            "   ██    ██   ██ ██ ███████ ███████ \n";
+
+    // Initialise boolean to track if user has said "bye"
+    private static boolean hasUserSaidBye = false;
+
+    // Initialise array to keep track of user's tasks
+    private static Task[] tasks = new Task[100];
+    private static int noOfTasks = 0;
+
+    // Initialise variable to track user input
+    private static String userInput = null;
+
+
     public static void main(String[] args) {
-        String logo = "████████ ██████  ██ ███████ ███████ \n" +
-                "   ██    ██   ██ ██ ██      ██      \n" +
-                "   ██    ██████  ██ ███████ ███████ \n" +
-                "   ██    ██   ██ ██      ██      ██ \n" +
-                "   ██    ██   ██ ██ ███████ ███████ \n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Triss :)");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        // Print logo and welcome text
+        printWelcomeMessage();
 
-        // Initialise boolean to track if user has said "bye"
-        boolean hasUserSaidBye = false;
+        // Initialise user input reader
         Scanner in = new Scanner(System.in);
-
-        // Initialise array to keep track of user's tasks
-        Task[] tasks = new Task[100];
-        int noOfTasks = 0;
 
         // While user has not said "bye", check for next line of input
         while (!hasUserSaidBye) {
@@ -29,45 +34,74 @@ public class Duke {
 
             // Check if user has said "bye"
             if (userInput.equals("bye")) {
-                // If user said "bye", update hasUserSaidBye and print closing phrase
-                hasUserSaidBye = true;
-                System.out.println("Thanks for coming. Auf wiedersehen!");
+                printShutdownMessage();
             } else if (userInput.equals("list")) {
-                // If user said "list", print a list of all saved tasks
-                for (int i = 0; i < noOfTasks; i++) {
-                    System.out.println(Integer.toString(i + 1) + "." + tasks[i].getDoneStatusAsSymbol()
-                            + " " + tasks[i].getName());
-                }
+                printAllTasks();
             } else if (userInput.contains("done")) {
-                // Get number of task after the term "done"
-                int indexOfCompletedTask = Integer.parseInt(userInput.split(" ")[1]) - 1;
-                if (indexOfCompletedTask > noOfTasks - 1 || indexOfCompletedTask < 0) {
-                    // If task does not exist, do not delete any task
-                    System.out.println("Apologies! That task does not exist.");
-                } else {
-                    Task chosenTask = tasks[indexOfCompletedTask];
-                    if (chosenTask.isDone()) {
-                        // If task was already done, let user know
-                        System.out.println("Oh! This task was already marked as done:");
-                    } else {
-                        chosenTask.setDone(true);
-                        // Else, let user know task has been marked as done
-                        System.out.println("Wunderbar! This task has been marked as done:");
-                    }
-                    // Prints out the task in the following format: "    [X] Task"
-                    System.out.println("    "
-                            + chosenTask.getDoneStatusAsSymbol() + " " + chosenTask.getName());
-                }
+                handleUserMarkingTaskAsDone(userInput);
             } else {
-                // If user has not said any other command, store user input as task
-                tasks[noOfTasks] = new Task(userInput);
-                noOfTasks++;
-                // Then, echo the task
-                System.out.println("I've added: " + userInput);
+                handleUserCreatingTask(userInput);
             }
 
             System.out.println("____________________________________________________________");
 
         }
+    }
+
+    private static void handleUserCreatingTask(String userInput) {
+        // If user has not said any other command, store user input as task
+        tasks[noOfTasks] = new Task(userInput);
+        noOfTasks++;
+        // Then, echo the task
+        System.out.println("I've added: " + userInput);
+    }
+
+    private static void handleUserMarkingTaskAsDone(String userInput) {
+        // Get number of task after the term "done"
+        int indexOfCompletedTask = Integer.parseInt(userInput.split(" ")[1]) - 1;
+
+        // If task does not exist, do not delete any task
+        if (indexOfCompletedTask > noOfTasks - 1 || indexOfCompletedTask < 0) {
+            System.out.println("Apologies! That task does not exist.");
+            return;
+        }
+
+        // Find task since it exists
+        Task chosenTask = tasks[indexOfCompletedTask];
+
+        // If task was already done, let user know
+        if (chosenTask.isDone()) {
+            System.out.println("Oh! This task was already marked as done:");
+            return;
+        }
+
+        // If task exists, and is not done, mark it as done
+        chosenTask.setDone(true);
+        System.out.println("Wunderbar! This task has been marked as done:");
+
+        // Print out the task in the following format: "    [X] Task"
+        System.out.println("    " + chosenTask.getDoneStatusAsSymbol() + " " + chosenTask.getName());
+    }
+
+    private static void printAllTasks() {
+        // If user said "list", print a list of all saved tasks
+        for (int i = 0; i < noOfTasks; i++) {
+            System.out.println(Integer.toString(i + 1) + "." + tasks[i].getDoneStatusAsSymbol()
+                    + " " + tasks[i].getName());
+        }
+    }
+
+    private static void printShutdownMessage() {
+        // If user said "bye", update hasUserSaidBye and print closing phrase
+        hasUserSaidBye = true;
+        System.out.println("Thanks for coming. Auf wiedersehen!");
+    }
+
+    private static void printWelcomeMessage() {
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Triss :)");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
     }
 }
