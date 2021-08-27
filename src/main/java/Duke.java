06ed8a949e42195ad49f83b57f62360a3a99469d
arvejw <jiewen.tehh@gmail.com>
2021-08-28 02:21:04 +0800

    Refactor in a few areas and some minor format/naming tweaks
    Changed printing of messages to use echo method where applicable since most of them require a horizontal bar before and after the message
    Extracted a method getCommand out from command manager
    Same level of abstraction within the main
    Some simple comments added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,51 +2,55 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static final String LOGO = System.lineSeparator() +
-            "    ___   ______   ________  _______      ___   ____  _____   .--." + System.lineSeparator() +
-            "  .'   `.|_   _ \\ |_   __  ||_   __ \\   .'   `.|_   \\|_   _|.'_\\/_'." + System.lineSeparator() +
-            " /  .-.  \\ | |_) |  | |_ \\_|  | |__) | /  .-.  \\ |   \\ | |  '. /\\,.'" + System.lineSeparator() +
-            " | |   | | |  __'.  |  _| _   |  __ /  | |   | | | |\\ \\| |    \"||\"" + System.lineSeparator() +
-            " \\  `-'  /_| |__) |_| |__/ | _| |  \\ \\_\\  `-'  /_| |_\\   |_    || /\\`" + System.lineSeparator() +
-            "  `.___.'|_______/|________||____| |___|`.___.'|_____|\\____|/\\ ||//\\)" + System.lineSeparator() +
-            "                                                           (/\\\\||/" + System.lineSeparator() +
-            "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\\||^^^^";
+    private static final String LOGO = System.lineSeparator()
+            + "    ___   ______   ________  _______      ___   ____  _____   .--." + System.lineSeparator()
+            + "  .'   `.|_   _ \\ |_   __  ||_   __ \\   .'   `.|_   \\|_   _|.'_\\/_'." + System.lineSeparator()
+            + " /  .-.  \\ | |_) |  | |_ \\_|  | |__) | /  .-.  \\ |   \\ | |  '. /\\,.'" + System.lineSeparator()
+            + " | |   | | |  __'.  |  _| _   |  __ /  | |   | | | |\\ \\| |    \"||\"" + System.lineSeparator()
+            + " \\  `-'  /_| |__) |_| |__/ | _| |  \\ \\_\\  `-'  /_| |_\\   |_    || /\\`" + System.lineSeparator()
+            + "  `.___.'|_______/|________||____| |___|`.___.'|_____|\\____|/\\ ||//\\)" + System.lineSeparator()
+            + "                                                           (/\\\\||/" + System.lineSeparator()
+            + "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\\||^^^^";
 
-    private static final String HORIZONTAL_BAR = "__________________" +
-            "__________________" +
-            "__________________" +
-            "_______________";
+    private static final String HORIZONTAL_BAR = "__________________"
+            + "__________________"
+            + "__________________"
+            + "_______________";
 
 
     private static final Scanner scanner = new Scanner(System.in);
     private static ArrayList<Task> tasks = new ArrayList<Task>();
 
-    public static void printGreetingMessage() {
-        System.out.println(HORIZONTAL_BAR);
-        System.out.println(LOGO);
-        System.out.println("  Hello! I'm Oberon");
-        System.out.println("  What can I do for you?");
-        System.out.println(HORIZONTAL_BAR);
+    /**
+     * Print greeting message upon starting the program
+     */
+    public static void displayGreetingMessage() {
+        echo(LOGO + System.lineSeparator() + "  Hello! I'm Oberon"
+                + System.lineSeparator() + "  What can I do for you?");
     }
 
-    public static void printFarewellMessage() {
-        System.out.println(HORIZONTAL_BAR);
-        System.out.println("  Goodbye. Hope to see you again soon!");
-        System.out.println(LOGO);
-        System.out.println(HORIZONTAL_BAR);
+    /**
+     * Print farewell message upon exiting the program
+     */
+    public static void displayFarewellMessage() {
+        echo("  Goodbye. Hope to see you again soon!" + System.lineSeparator() + LOGO);
     }
 
+    /**
+     * Reads in a string and prints it with a horizontal bar above & below it
+     * @param input String to be echo-ed
+     */
     public static void echo(String input) {
-        System.out.println(HORIZONTAL_BAR);
-        System.out.println("  " + input);
-        System.out.println(HORIZONTAL_BAR);
+        System.out.println(HORIZONTAL_BAR + System.lineSeparator() + input
+                + System.lineSeparator() + HORIZONTAL_BAR);
     }
 
+    /**
+     * Prints current list of tasks
+     */
     public static void printTasks() {
         if (tasks.isEmpty()) {
-            System.out.println(HORIZONTAL_BAR);
-            System.out.println("  List is empty!");
-            System.out.println(HORIZONTAL_BAR);
+            echo("  List is empty!");
         } else {
             System.out.println(HORIZONTAL_BAR);
             // Traverse down the ArrayList and prints out each task
@@ -65,40 +69,36 @@ public class Duke {
         boolean taskNumberInRange = (taskNumber <= tasks.size()) && (taskNumber >= 1);
         if (taskNumberInRange) {
             tasks.get(taskNumber - 1).setDone();
-            System.out.println(HORIZONTAL_BAR);
-            System.out.println("Task " + Integer.toString(taskNumber) + ": "
-                    + tasks.get(taskNumber - 1).taskDescription);
-            System.out.println("  Marked as done!");
+            echo("Task " + taskNumber + ": " + tasks.get(taskNumber - 1).taskDescription
+                    + System.lineSeparator() + "  Marked as done!");
         } else {
-            System.out.println(HORIZONTAL_BAR);
-            System.out.println("  Invalid Task number!");
+            echo("  Invalid Task number!");
         }
-        System.out.println(HORIZONTAL_BAR);
     }
-    
+
     public static void addToDo (String description) {
         tasks.add(new ToDo(description));
-        echo("You have successfully added the task:" + System.lineSeparator()
+        echo("  You have successfully added the task:" + System.lineSeparator()
                 + "    " + tasks.get(tasks.size() - 1).getTaskDescriptionWithStatus() + System.lineSeparator()
                 + "  You now have " + tasks.size() + " tasks in your list!");
     }
 
     public static void addEvent (String description, String at) {
         tasks.add(new Event(description, at));
-        echo("You have successfully added the task:" + System.lineSeparator()
+        echo("  You have successfully added the task:" + System.lineSeparator()
                 + "    " + tasks.get(tasks.size() - 1).getTaskDescriptionWithStatus() + System.lineSeparator()
                 + "  You now have " + tasks.size() + " tasks in your list!");
     }
 
     public static void addDeadline (String description, String by) {
         tasks.add(new Deadline(description, by));
-        echo("You have successfully added the task:" + System.lineSeparator()
+        echo("  You have successfully added the task:" + System.lineSeparator()
                 + "    " + tasks.get(tasks.size() - 1).getTaskDescriptionWithStatus() + System.lineSeparator()
                 + "  You now have " + tasks.size() + " tasks in your list!");
     }
-    
+
     public static void printMessageForInvalidInput () {
-        echo("Invalid Input..." + System.lineSeparator()
+        echo("  Invalid Input..." + System.lineSeparator()
                 + "  Please enter a valid input!");
     }
 
@@ -114,7 +114,6 @@ public class Duke {
             break;
         case ADD_EVENT:
             String eventInput[] = restOfInput.split("/at", 2);
-
             if (eventInput.length > 1) {
                 // result[0] equals description, result[1] equals "at"
                 addEvent(eventInput[0].trim(), eventInput[1].trim()); 
@@ -143,36 +142,40 @@ public class Duke {
         }
     }
 
+    private static Command getCommand(String input) {
+        Command inputCommand;
+        if (input.equals("todo")) {
+            inputCommand = Command.ADD_TODO;
+        } else if (input.equals("event")) {
+            inputCommand = Command.ADD_EVENT;
+        } else if (input.equals("deadline")) {
+            inputCommand = Command.ADD_DEADLINE;
+        } else if (input.equals("done")) {
+            inputCommand = Command.DONE_TASK;
+        } else {
+            inputCommand = Command.INVALID;
+        }
+        return inputCommand;
+    }
+
     public static void inputManager() {
         String input = scanner.nextLine();
         Command currentCommand = Command.INVALID;
         String restOfInput = "";
         while (true) {
             if (input.trim().equals("bye")) {
-                break; //exit inputManager
-            } else if (input.trim().equals("list")) {
+                break; // exit inputManager
+            }
+            if (input.trim().equals("list")) {
                 currentCommand = Command.SHOW_LIST;
-            } else if (input.trim().indexOf(" ") > -1) { // input more than one word
+            } else if (input.trim().indexOf(" ") > -1) { // input is more than one word
                 String result[] = input.trim().split(" ", 2);
                 String firstWord = result[0];
                 String otherWords = result[1];
-                if (firstWord.equals("todo")) {
-                    currentCommand = Command.ADD_TODO;
-                    restOfInput = otherWords;
-                } else if (firstWord.equals("event")) {
-                    currentCommand = Command.ADD_EVENT;
-                    restOfInput = otherWords;
-                } else if (firstWord.equals("deadline")) {
-                    currentCommand = Command.ADD_DEADLINE;
-                    restOfInput = otherWords;
-                } else if (firstWord.equals("done")) {
-                    currentCommand = Command.DONE_TASK;
-                    restOfInput = otherWords;
-                } else {
-                    currentCommand = Command.INVALID;
-                    restOfInput = "";
-                }
-            } else {
+                currentCommand = getCommand(firstWord);
+                // if invalid return empty string
+                restOfInput = (currentCommand != Command.INVALID) ? otherWords : "";
+            } else { // all other cases are also considered invalid inputs
                 currentCommand = Command.INVALID;
                 restOfInput = "";
             }
@@ -182,8 +185,8 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        printGreetingMessage();
+        displayGreetingMessage();
         inputManager();
-        printFarewellMessage();
+        displayFarewellMessage();
     }
 }
