36ecc2cda32fbbe5726168d9f1e21087ea1720d3
ChewyChair <69298017+ChewyChair@users.noreply.github.com>
2021-08-27 20:57:56 +0800

    Abstract more functions.

--- src/main/java/Shika.java
+++ src/main/java/Shika.java
@@ -28,23 +28,31 @@ public class Shika {
      */
     public static void main(String[] args) {
         printLogo();
-        setupShika();
+        startShika();
     }
 
     /**
-     * Function that loads tasks from ShikaTasks.txt. If the file or parent directories do not exist,
+     * Function that attempts to load tasks from ShikaTasks.txt. If the file or parent directories do not exist,
      * it creates them.
      * @throws FileNotFoundException when ShikaTasks.txt is not found.
      */
-    public static void loadTasks() throws FileNotFoundException {
+    public static void setupShika() throws FileNotFoundException {
         File f = new File("data/ShikaTasks.txt");
-        boolean isFirstLaunch = setupSave(f);
+        String greeting = (setupSave(f)) ? "Hello, friend! " : "Welcome back, friend! ";
+        System.out.println(greeting + "Shika at your service! ^-^\n");
+        loadTasks(f);
+    }
+
+    /**
+     * Function that loads tasks from the given filepath.
+     * @param f path to file.
+     * @throws FileNotFoundException
+     */
+    private static void loadTasks(File f) throws FileNotFoundException {
         Scanner s = new Scanner(f);
         while (s.hasNext()) {
             loadTask(s.nextLine());
         }
-        String greeting = (isFirstLaunch) ? "Hello, friend! " : "Welcome back, friend! ";
-        System.out.println(greeting + "Shika at your service! ^-^\n");
     }
 
     /**
@@ -69,22 +77,41 @@ public class Shika {
     }
 
     /**
-     * This function adds tasks to taskList by parsing the String inputted.
+     * This function loads tasks by parsing the String inputted, then calling addSavedTask to add it to
+     * taskList.
      * @param s String to be parsed.
      */
     public static void loadTask(String s) {
         int firstDiv = s.indexOf("|") + 1;
         int secondDiv = s.indexOf("|", firstDiv) + 1;
         int thirdDiv = s.indexOf("|", secondDiv) + 1;
+        char type = s.charAt(0);
         String atBy = s.substring(firstDiv, secondDiv - 1).trim();
         String name = s.substring(secondDiv, thirdDiv - 1).trim();
         String done = s.substring(thirdDiv).trim();
-        if (s.startsWith("T")) {
+        addSavedTask(type, atBy, name, done);
+    }
+
+    /**
+     * This function directly adds a task to tasks without printing any messages. Used for loading saved tasks.
+     * @param type type of task.
+     * @param atBy at/by of task, if applicable.
+     * @param name name of task.
+     * @param done if the task is done or not.
+     */
+    private static void addSavedTask(char type, String atBy, String name, String done) {
+        switch(type) {
+        case 'T':
             tasks.add(new Todo(name));
-        } else if (s.startsWith("D")) {
+            break;
+        case 'D':
             tasks.add(new Deadline(name, atBy));
-        } else {
+            break;
+        case 'E':
             tasks.add(new Event(name, atBy));
+            break;
+        default:
+            return;
         }
         if (done.equals("true")) {
             tasks.get(Task.count).markAsDone();
@@ -122,13 +149,11 @@ public class Shika {
     }
 
     /**
-     * This function initialises the array containing all tasks, tries to load tasks, then runs Shika.
+     * This function setups Shika, then runs Shika.
      */
-    public static void setupShika() {
-
-        Task.count = 0;
+    public static void startShika() {
         try {
-            loadTasks();
+            setupShika();
         } catch (FileNotFoundException e) {
             System.out.println("I can't find the save file AHHHHHHH.\n");
         }
