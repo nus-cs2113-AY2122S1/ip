8cc4c03b459ae08516d9b4e389daa0530ca44c01
arcturusz <48401155+arcturusz@users.noreply.github.com>
2021-08-27 22:31:44 +0800

    Add echo and exit command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static final String DIVIDER = "-----------------------------------------";
+
     public static void main(String[] args) {
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
-        System.out.println("________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
+        boolean isExit;
+        do {
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+
+            isExit = line.equals("bye");
+            if (isExit) {
+                System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(line);
+            }
+            System.out.println(DIVIDER);
+        } while (!isExit);
     }
 }
