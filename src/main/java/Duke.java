6be2c01359371564be944c66b1e1eac080cb2d8b
theeugenechong <eugenechong20011@yahoo.com>
2021-08-29 02:13:02 +0800

    Add Level-4: ToDos, Events, Deadlines along with some error checking

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,7 +19,7 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println(" Here are the tasks in your list:");
         for (int i = 0; i < tasks.length; i++) {
-            System.out.println(" " + (i + 1) + ". " + "[" + tasks[i].getStatusIcon() +"] " + tasks[i].description);
+            System.out.println(" " + (i + 1) + ". " + tasks[i].toString());
         }
         System.out.println("____________________________________________________________");
     }
@@ -42,10 +42,11 @@ public class Duke {
     public static void markAndPrintTaskDone(Task[] tasks, int taskCount, String doneTask) {
         String[] doneSentence = doneTask.split(" ");
 
-        // Checks if a string does not follow the correct format of "done {task ID}".
+        // Checks if done command entered does not follow the correct format of "done {task ID}".
         if (doneSentence.length != 2) {
             System.out.println("____________________________________________________________");
-            System.out.println(" Your input is of the wrong format! To mark a task as done, enter \"done {ID of done task}\".");
+            System.out.println(" Your command is of the wrong format!");
+            System.out.println(" To mark a task as done, enter \"done {ID of done task}\".");
             System.out.println("____________________________________________________________");
             return;
         }
@@ -53,7 +54,8 @@ public class Duke {
         // Checks if the task ID entered is numeric.
         if (!isNumeric(doneSentence[1])) {
             System.out.println("____________________________________________________________");
-            System.out.println(" Your command is of the wrong format! Enter a number for your task ID.");
+            System.out.println(" Your command is of the wrong format!");
+            System.out.println(" Enter a number for your task ID.");
             System.out.println("____________________________________________________________");
             return;
         }
@@ -68,15 +70,127 @@ public class Duke {
             tasks[taskToMarkDone - 1].setDone(true);
             System.out.println("____________________________________________________________");
             System.out.println(" Great! I have marked the following task as done: ");
-            System.out.println("   [" + tasks[taskToMarkDone - 1].getStatusIcon() + "] " + tasks[taskToMarkDone - 1].description);
+            System.out.println("   " + tasks[taskToMarkDone - 1].toString());
             System.out.println("____________________________________________________________");
         }
     }
 
-    public static void addAndPrintTask(Task[] tasks, int taskCount, String addedTask) {
-        tasks[taskCount] = new Task(addedTask);
+    public static boolean canAddAndPrintTodo(Task[] tasks, int taskCount, String addedTodo) {
+        String[] splitTodo = addedTodo.split("\\s+", 2);
+
+        // Makes sure that the todo entered is of the format "todo {description}"
+        if(splitTodo.length != 2) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Your command is of the wrong format!");
+            System.out.println(" To add a todo, enter \"todo {description}\".");
+            System.out.println("____________________________________________________________");
+            return false;
+        }
+
+        String todoDescription = splitTodo[1];
+        Todo newTodo = new Todo(todoDescription);
+        tasks[taskCount] = newTodo;
         System.out.println("____________________________________________________________");
-        System.out.println(" I have added a task: " + addedTask);
+        System.out.println(" I have added a task: ");
+        System.out.println("   " + newTodo.toString());
+        System.out.println(" You now have " + (taskCount + 1) + " task(s) in the list.");
+        System.out.println("____________________________________________________________");
+        return true;
+    }
+
+    public static boolean canAddAndPrintDeadline(Task[] tasks, int taskCount, String addedDeadline) {
+        String[] splitDeadline = addedDeadline.split("\\s+", 2);
+        boolean wasAdded = false;
+
+        // Makes sure that the deadline entered is of the format "deadline {description} /by {deadline}"
+        if(splitDeadline.length != 2) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Your command is of the wrong format!");
+            System.out.println(" To add a deadline, enter \"deadline {description} /by {deadline}\".");
+            System.out.println("____________________________________________________________");
+            return wasAdded;
+        }
+
+        String[] deadlineDescriptionAndDeadline = splitDeadline[1].split("/by", 2);
+        if(deadlineDescriptionAndDeadline.length != 2) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Your command is of the wrong format!");
+            System.out.println(" To add a deadline, enter \"deadline {description} /by {deadline}\".");
+            System.out.println("____________________________________________________________");
+            return wasAdded;
+        }
+
+        String deadlineDescription = deadlineDescriptionAndDeadline[0].trim();
+        String deadlineDeadline = deadlineDescriptionAndDeadline[1].trim();
+        boolean isValidDeadline = !deadlineDescription.isEmpty() && !deadlineDeadline.isEmpty();
+        // deadline with valid format is added to task list
+        if (isValidDeadline) {
+            Deadline newDeadline = new Deadline(deadlineDescription, deadlineDeadline);
+            tasks[taskCount] = newDeadline;
+            wasAdded = true;
+            System.out.println("____________________________________________________________");
+            System.out.println(" I have added a task: ");
+            System.out.println("   " + newDeadline.toString());
+            System.out.println(" You now have " + (taskCount + 1) + " task(s) in the list.");
+            System.out.println("____________________________________________________________");
+        } else {
+            wasAdded = false;
+            System.out.println("____________________________________________________________");
+            System.out.println(" Your command lacks an argument!");
+            System.out.println(" To add a deadline, enter \"deadline {description} /by {deadline}\".");
+            System.out.println("____________________________________________________________");
+        }
+        return wasAdded;
+    }
+
+    public static boolean canAddAndPrintEvent(Task[] tasks, int taskCount, String addedEvent) {
+        String[] splitEvent = addedEvent.split("\\s+", 2);
+        boolean wasAdded = false;
+
+        // Makes sure that the event entered is of the format "event {description} /at {event time}"
+        if(splitEvent.length != 2) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Your command is of the wrong format!");
+            System.out.println(" To add an event, enter \"event {description} /at {event time}\".");
+            System.out.println("____________________________________________________________");
+            return wasAdded;
+        }
+
+        String[] eventDescriptionAndTime = splitEvent[1].split("/at", 2);
+        if (eventDescriptionAndTime.length != 2) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Your command is of the wrong format!");
+            System.out.println(" To add an event, enter \"event {description} /at {event time}\".");
+            System.out.println("____________________________________________________________");
+            return wasAdded;
+        }
+
+        String eventDescription = eventDescriptionAndTime[0].trim();
+        String eventTime = eventDescriptionAndTime[1].trim();
+        boolean isValidEvent = !eventDescription.isEmpty() && !eventTime.isEmpty();
+        // event with valid format is added to task list
+        if (isValidEvent) {
+            Event newEvent = new Event(eventDescription, eventTime);
+            tasks[taskCount] = newEvent;
+            wasAdded = true;
+            System.out.println("____________________________________________________________");
+            System.out.println(" I have added a task: ");
+            System.out.println("   " + newEvent.toString());
+            System.out.println(" You now have " + (taskCount + 1) + " task(s) in the list.");
+            System.out.println("____________________________________________________________");
+        } else {
+            wasAdded = false;
+            System.out.println("____________________________________________________________");
+            System.out.println(" Your command lacks an argument!");
+            System.out.println(" To add an event, enter \"event {description} /at {event time}\".");
+            System.out.println("____________________________________________________________");
+        }
+        return wasAdded;
+    }
+
+    public static void printInvalidCommandMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Please enter a valid command!");
         System.out.println("____________________________________________________________");
     }
 
@@ -95,9 +209,20 @@ public class Duke {
                 printTaskList(Arrays.copyOf(tasks, taskCount));
             } else if (line.trim().toLowerCase().startsWith("done")) {
                 markAndPrintTaskDone(tasks, taskCount, line);
+            } else if (line.trim().toLowerCase().startsWith("todo")){
+                if (canAddAndPrintTodo(tasks, taskCount, line)) {
+                    taskCount++;
+                }
+            } else if (line.trim().toLowerCase().startsWith("deadline")) {
+                if (canAddAndPrintDeadline(tasks, taskCount, line)) {
+                    taskCount++;
+                }
+            } else if (line.trim().toLowerCase().startsWith("event")) {
+                if (canAddAndPrintEvent(tasks, taskCount, line)) {
+                    taskCount++;
+                }
             } else {
-                addAndPrintTask(tasks, taskCount, line);
-                taskCount++;
+                printInvalidCommandMessage();
             }
             line = in.nextLine();
         }
