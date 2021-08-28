7a241b2df4289cfd7c1aa6f3d5825332532219a4
Adric <adricpjw@gmail.com>
2021-08-28 15:33:52 +0800

    touched up coding standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;  // Import the Scanner class
-
+import java.util.Scanner; // Import the Scanner class
 
 public class Duke {
-    
-    private static Scanner myScan= new Scanner(System.in);
+    private static Scanner myScan = new Scanner(System.in);
     private static Tasks tasks = new Tasks();
 
     public static void main(String[] args) {
@@ -12,37 +10,32 @@ public class Duke {
         run();
     }
 
-
     public static void run() {
-
         while (true) {
             String input = myScan.nextLine();
             String arr[] = input.split(" ");
-            switch(arr[0]) {
-                case "bye":
-                    System.out.println("Bye. Hope to see you again soon!");
-                    myScan.close();
-                    return;
-                case "list":
-                    tasks.listTasks();
-                    break;
-                case "done" :
-                    int idx = Integer.parseInt(arr[1])-1;
-                    tasks.updateTask(idx, 1);
-                    System.out.println("Nice! I've marked this task as done: ");
-                    tasks.showTask(idx);
-                    break;
-                default:
-                    System.out.println("added: " + input);
-                    tasks.addTask(input);
-                    
+            switch (arr[0]) {
+            case "bye":
+                System.out.println("Bye. Hope to see you again soon!");
+                myScan.close();
+                return;
+            case "list":
+                tasks.listTasks();
+                break;
+            case "done":
+                int idx = Integer.parseInt(arr[1]) - 1;
+                tasks.updateTask(idx, 1);
+                System.out.println("Nice! I've marked this task as done: ");
+                tasks.showTask(idx);
+                break;
+            default:
+                System.out.println("added: " + input);
+                tasks.addTask(input);
             }
         }
     }
 
-
     public static void level1() {
-
         while (true) {
             String input = myScan.nextLine();
             if (input.equals("bye")) {
@@ -50,9 +43,8 @@ public class Duke {
                 myScan.close();
                 return;
             }
-            System.out.println(input);  // Output user input
+            System.out.println(input); // Output user input
         }
-        
     }
 
     public static void greet() {
@@ -65,7 +57,5 @@ public class Duke {
         // System.out.println("_________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-
     }
 }
-
