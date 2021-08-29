5e5a6b0f2e6df2d6422aadef95ed8d2ac3d0ba1e
pos0414 <gimins1202@gmail.com>
2021-08-29 16:06:11 +0900

    Follow coding standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,55 +4,64 @@ import java.util.Arrays;
 
 public class Duke {
 
+    public static final String LINE = "    ____________________________________________________________";
+    public static final String INDENT = "     ";
+    public static final String LINE_SEPARATOR = System.lineSeparator();
+
     public static void echo(String line) {
-        String separator = "____________________________________________________________";
-        System.out.println(separator);
-        System.out.println(" " + line);
-        System.out.println(separator);
+        System.out.println(LINE + LINE_SEPARATOR + INDENT + line + LINE_SEPARATOR + LINE);
     }
 
     public static void showList(Task[] tasks) {
-        System.out.println(" Here are the tasks in your list:");
+        System.out.println(INDENT + "Here are the tasks in your list:");
         for (int i = 0; i < tasks.length; i++) {
-            System.out.println(" " + Integer.toString(i + 1) + "."
+            System.out.println(INDENT + Integer.toString(i + 1) + "."
                     + "[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
         }
     }
 
-    public static void main(String[] args) {
-        String separator = "____________________________________________________________";
+    public static void executeRequest() {
         String line;
         Scanner in = new Scanner(System.in);
         int taskCount = 0;
         Task[] tasks = new Task[100];
 
-        System.out.println(separator);
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println(separator);
-
         line = in.nextLine();
+        String[] words = line.split(" ");
+
         while (!line.equals("bye")) {
-            System.out.println(separator);
-            if (line.equals("list")) {
+            System.out.println(LINE);
+            switch (words[0]) {
+            case "list":
                 showList(Arrays.copyOf(tasks, taskCount));
-            } else if (line.toLowerCase().startsWith("done")) {
-                //"done" is not case-sensitive now
-                String[] words = line.split(" ");
-                //Assuming the 2nd word is the index of the task
+                break;
+            case "done":
                 int taskIndex = Integer.parseInt(words[1]) - 1;
                 tasks[taskIndex].markAsDone();
-            } else {
+                break;
+            default:
                 tasks[taskCount] = new Task(line);
-                System.out.println(" added: " + line);
+                System.out.println(INDENT + "added: " + line);
                 taskCount++;
+                break;
             }
-            System.out.println(separator);
+            System.out.println(LINE);
             line = in.nextLine();
+            words = line.split(" ");
         }
+    }
 
-        System.out.println(separator);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(separator);
+    public static void greetUser() {
+        echo("Hello! I'm Duke." + LINE_SEPARATOR + INDENT + "What can I do for you?");
+    }
+
+    public static void farewellUser() {
+        echo("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        greetUser();
+        executeRequest();
+        farewellUser();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -15,8 +15,8 @@ public class Task {
     }
     public void markAsDone() {
         isDone = true;
-        System.out.println(" Nice! I've marked this task as done: ");
-        System.out.println("  [X] " + description);
+        System.out.println(Duke.INDENT + "Nice! I've marked this task as done: ");
+        System.out.println(Duke.INDENT + "  [X] " + description);
     }
 
 }
