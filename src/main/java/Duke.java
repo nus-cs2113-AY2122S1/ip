87dd4cc1a956a503f04c82889846c0656baee3d9
conradwee <conradwee@gmail.com>
2021-08-29 14:51:39 +0800

    Level-1 V2 (only undercase bye works)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
 
     private static final int LINE_WIDTH = 60;
@@ -22,6 +26,18 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println();
         printHorizontalLine();
+        String line = null;
+        Scanner in = new Scanner(System.in);
+
+        while(!(Objects.equals(line, "bye" )) ){
+
+            line = in.nextLine();
+            printHorizontalLine();
+            if(!Objects.equals(line, "bye")){
+                System.out.println(line);
+                printHorizontalLine();}
+        }
+        //printHorizontalLine();
         System.out.println( "Bye. Hope to see you again soon!");
         System.out.println();
         printHorizontalLine();
