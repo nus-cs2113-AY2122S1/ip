51cd38e587345217aebd88cd928a9ecaceed9867
Woolicious98 <e527269@u.nus.edu>
2021-08-29 15:40:19 +0800

    Add format error message in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
 
-    /**
-     * A string to append before and after a print message.
-     */
     final static String LINE = "____________________________________________________________";
+
+    final static String DONE_FORMAT = "done <task number(can be seen using the list command)>";
+    final static String TODO_FORMAT = "todo <task description>";
+    final static String DEADLINE_FORMAT = "deadline <task description> /by <due date>";
+    final static String EVENT_FORMAT = "event <task description> /at <start date>";
 
     /**
      * Main function to run the bot.
@@ -17,6 +20,7 @@ public class Duke {
         printLogoMessage();
         printWelcomeMessage();
         getMenu();
+        printExitMessage();
     }
 
     /**
@@ -49,20 +53,33 @@ public class Duke {
     }
 
     /**
+     * Print a format error message with the given format usage.
+     *
+     * @param format Constant string containing the format usage of the command.
+     */
+    public static void printFormatErrorMessage(String format){
+        System.out.println("Error: Incorrect format detected.");
+        System.out.println("Usage: " + format);
+    }
+
+    /**
      * Get the menu for user to input commands for the bot to perform.
      */
     public static void getMenu() {
         Scanner in = new Scanner(System.in);
         String userInputs = in.nextLine();
         TaskManager taskManager = new TaskManager();
-        menuLoop:
         while (true) {
             System.out.println(LINE);
             switch (getUserCommand(userInputs)) {
             case "todo":
+                addToDoTask(taskManager,userInputs);
+                break;
             case "event":
+                addEventTask(taskManager,userInputs);
+                break;
             case "deadline":
-                processTaskInfo(taskManager, userInputs);
+                addDeadlineTask(taskManager,userInputs);
                 break;
             case "done":
                 int taskIndex = getTaskIndexFromUserInputs(userInputs);
@@ -72,10 +89,9 @@ public class Duke {
                 taskManager.printAllTasks();
                 break;
             case "bye":
-                printExitMessage();
-                break menuLoop;
+                return;
             default:
-                System.out.println("Error: Invalid command.");
+                System.out.println("Error: Command not found.");
                 break;
             }
             System.out.println(LINE);
@@ -111,46 +127,97 @@ public class Duke {
         try {
             result = Integer.parseInt(userInputs.split(" ")[1]);
         } catch (Exception exception) {
-            System.out.println("Error: Incorrect format detected.");
+            printFormatErrorMessage(DONE_FORMAT);
         }
         return result;
     }
 
     /**
-     * Method to add task to task list that is handled by a TaskManager object.
+     * Method to create a ToDo task.
      *
-     * @param taskManager TaskManager object that handles the tasks list
-     * @param userInputs  Raw user inputs from scanner
+     * @param taskManager TaskManager object that handles the tasks list.
+     * @param userInputs Raw user inputs from scanner.
      */
-    public static void processTaskInfo(TaskManager taskManager, String userInputs) {
+    public static void addToDoTask(TaskManager taskManager, String userInputs) {
         String payload = getUserPayload(userInputs);
-        String userCommand = getUserCommand(userInputs);
-        String[] newPayload;
-        if (payload.equals("")) {
-            System.out.println("Error: Missing Task Info.");
+        if (isStringEmpty(payload)) {
+            printFormatErrorMessage(TODO_FORMAT);
             return;
         }
-        switch (userCommand) {
-        case "todo":
-            taskManager.createToDoTask(payload);
-            break;
-        case "deadline":
-            newPayload = payload.split("/by");
-            if (newPayload.length < 2) {
-                System.out.println("Error: Missing /by tag.");
-            } else {
-                taskManager.createDeadlineTask(newPayload[0], newPayload[1]);
-            }
-            break;
-        case "event":
-            newPayload = payload.split("/at");
-            if (newPayload.length < 2) {
-                System.out.println("Error: Missing /at tag.");
-            } else {
-                taskManager.createEventTask(newPayload[0], newPayload[1]);
-            }
-            break;
+        taskManager.createToDoTask(payload);
+    }
+
+    /**
+     * Method to create a Deadline Task.
+     *
+     * @param taskManager TaskManager object that handles the tasks list.
+     * @param userInputs Raw user inputs from scanner.
+     */
+    public static void addDeadlineTask(TaskManager taskManager, String userInputs) {
+        String payload = getUserPayload(userInputs);
+        if (isStringEmpty(payload)) {
+            printFormatErrorMessage(DEADLINE_FORMAT);
+            return;
         }
+        String[] taskInfo = payload.split("/by");
+        if (taskInfo.length < 2) {
+            printFormatErrorMessage(DEADLINE_FORMAT);
+        } else if (isAnyStringEmpty(taskInfo)) {
+            printFormatErrorMessage(DEADLINE_FORMAT);
+        } else {
+            taskManager.createDeadlineTask(taskInfo[0], taskInfo[1]);
+        }
+
+    }
+
+    /**
+     * Method to create a Event task.
+     *
+     * @param taskManager TaskManager object that handles the tasks list.
+     * @param userInputs Raw user inputs from scanner.
+     */
+    public static void addEventTask(TaskManager taskManager, String userInputs) {
+        String payload = getUserPayload(userInputs);
+        if (isStringEmpty(payload)) {
+            printFormatErrorMessage(EVENT_FORMAT);
+            return;
+        }
+        String[] taskInfo = payload.split("/at");
+        if (taskInfo.length < 2) {
+            printFormatErrorMessage(EVENT_FORMAT);
+        } else if (isAnyStringEmpty(taskInfo)) {
+            printFormatErrorMessage(EVENT_FORMAT);
+        } else {
+            taskManager.createEventTask(taskInfo[0], taskInfo[1]);
+        }
+
+    }
+
+    /**
+     * Check if a string is empty. Returning true if it does not exist or is empty.
+     *
+     * @param s Given string to check.
+     * @return boolean if the string is empty.
+     */
+    public static boolean isStringEmpty(String s) {
+        return (s == null || s.isEmpty());
+    }
+
+    /**
+     * Check if any string in a given string array is empty.
+     *
+     * @param strings Given String array.
+     * @return True if any of a string in the string array is empty.
+     */
+    public static boolean isAnyStringEmpty(String[] strings) {
+        boolean isEmpty = false;
+        for (String s : strings) {
+            if (isStringEmpty(s.trim())) {
+                isEmpty = true;
+                break;
+            }
+        }
+        return isEmpty;
     }
 
 }
