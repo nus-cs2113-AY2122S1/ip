b612f86e7632cb0ddfcaa335225a2d9ccd77f015
conradwee <conradwee@gmail.com>
2021-08-29 14:09:51 +0800

    Duke Level-0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,13 @@
 public class Duke {
+
+    private static final int LINE_WIDTH = 60;
+
+
+    private static void printHorizontalLine() {
+        System.out.println("_".repeat(LINE_WIDTH));
+    }
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +15,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        printHorizontalLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        printHorizontalLine();
+        System.out.println( "Bye. Hope to see you again soon!");
+        System.out.println();
+        printHorizontalLine();
+
     }
 }
