55e0de902ccfedbab9e3c0314db94e772a59bde2
Alvin.Avocados <alvinpang321@gmail.com>
2021-08-30 01:13:48 +0800

    Fix typo in toString override in Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -9,6 +9,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return super.toString() + String.format("(at: %s", dateTime);
+        return super.toString() + String.format("(at: %s)", dateTime);
     }
 }
