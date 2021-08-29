3c0be2381bdb6b4aea0dfd9f70063f0ea2b59ac5
dyahnafisah <59493341+dyahnafisah@users.noreply.github.com>
2021-08-29 21:06:45 +0800

    add a method to add tasks to the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,24 +2,57 @@ import java.util.Scanner;
 
 public class Duke {
     private static final String COMMAND_EXIT_WORD = "bye";
+    private static final String COMMAND_LIST_WORD = "list";
+    private static final int MAX_TASKS_NUMBER = 100;
     private static final Scanner INPUT_COMMAND = new Scanner(System.in);
 
     public static void main(String[] args) {
         showWelcomeMessage();
-        executeCommand();
+        Task[] tasksList = new Task[MAX_TASKS_NUMBER];
+        int taskCount = 0;
+        executeCommand(tasksList, taskCount);
     }
 
-    private static void executeCommand() {
-        String inputLine = INPUT_COMMAND.nextLine();
-        while(!inputLine.equals(COMMAND_EXIT_WORD)) {
-            System.out.println("  Task: " + inputLine);
-            System.out.println("  Anything else?");
+    public static void executeCommand(Task[] tasksList, int taskCount) {
+        String inputLine;
+        do {
             inputLine = INPUT_COMMAND.nextLine();
-        }
-        exitProgram();
+            switch (inputLine) {
+            case COMMAND_LIST_WORD:
+                printTasks(tasksList, taskCount);
+                break;
+            case COMMAND_EXIT_WORD:
+                exitProgram();
+            default:
+                taskCount = addTask(tasksList, taskCount, inputLine);
+            }
+        } while (!inputLine.equals(COMMAND_EXIT_WORD));
     }
 
-    private static void showWelcomeMessage() {
+    public static void printTasks(Task[] tasksList, int taskCount) {
+        if (taskCount == 0) {
+            System.out.println("No task added yet!");
+        } else {
+            for (int i = 1; i < taskCount + 1; i++) {
+                System.out.println("  " + i + ". " + tasksList[i-1].getTaskName());
+            }
+        }
+    }
+
+    public static int addTask(Task[] tasksList, int taskCount, String inputLine) {
+        tasksList[taskCount] = new Task(inputLine);
+        taskCount++;
+        System.out.println("  Task added: " + inputLine);
+        System.out.println("  Anything else?");
+        return taskCount;
+    }
+
+    public static void exitProgram() {
+        System.out.println("  Bye! Have a nice day and hope to see you again!");
+        System.exit(0);
+    }
+
+    public static void showWelcomeMessage() {
         String logo = "▒▒▒▒▒▒▒█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"
                 + "▒▒▒▒▒▒▒█░▒▒▒▒▒▒▒▓▒▒▓▒▒▒▒▒▒▒░█\n"
                 + "▒▒▒▒▒▒▒█░▒▒▓▒▒▒▒▒▒▒▒▒▄▄▒▓▒▒░█░▄▄\n"
@@ -36,10 +69,5 @@ public class Duke {
         System.out.println("           This is your chatbot assistant, Neko Duke! :)");
         System.out.println("                What can I do for you today?");
         System.out.println("  ____________________________________________________________\n");
-    }
-
-    private static void exitProgram() {
-        System.out.println("  Bye! Have a nice day and hope to see you again!");
-        System.exit(0);
     }
 }
