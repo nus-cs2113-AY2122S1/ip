526afbdcf6e70dd46d3a296a15702213deb2d517
Alvin.Avocados <alvinpang321@gmail.com>
2021-08-29 23:59:15 +0800

    Add Event class
    * Inherits from Task class
    * dateTime is treated as a String for now
    * Override toString method to include the set deadline

---
+++ src/main/java/Event.java
@@ -0,0 +1,14 @@
+public class Event extends Task {
+    private String dateTime;
+
+    public Event(String description, String dateTime) {
+        super(description);
+        this.type = TaskType.EVENT;
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("(at: %s", dateTime);
+    }
+}
