d3d3d380b53a767285200694af65fd6bd6e4ee67
ngnigel99 <e0543444@u.nus.edu>
2021-08-29 14:55:20 +0800

    Clean code, reformat and extract methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static final int STOP_ADD = -1;
     private static boolean isFail = false;
-
+    private static int taskCount = 0;
     public static void printLine() {
         System.out.println("                 ...                 ");
     }
@@ -50,19 +51,11 @@ public class Duke {
 
     public static void runDuke() {
         Scanner in = new Scanner(System.in);
-        if (Duke.getIsFail()) {
-            System.out.println("        bot failed, sometimes we all need a break       ");
-            return;
-        }
-        AccountDetail user = new AccountDetail();
-        System.out.print("Username [dukeBot]: ");
-        AccountDetail.setUsername(in.nextLine());
-        System.out.print("Password [bukeDot]: ");
-        AccountDetail.setPassword(in.nextLine());
+        if (isDukeFail()) return;
+        setupUsernamePassword(in);
         sayHi(AccountDetail.getUsername());
         String command;
         Task[] taskList = new Task[100];
-        int taskCount = 0;
         do {
             command = in.nextLine();
             switch (command) {
@@ -70,37 +63,16 @@ public class Duke {
                 listTasks(taskList);
                 break;
             case ("add"):
-                String taskName;
-                do {
-                    taskName = in.nextLine();
-                    if (taskName.equals("stop")) {
-                        break;
-                    }
-                    taskList[taskCount] = new Task(taskName);
-                    taskCount++;
-                } while (!taskName.equals("stop"));
-                System.out.println("Finished adding tasks!");
+                taskCount = addTaskToList(in, taskList, taskCount);
                 break;
-
             case ("done"):
-                String number = in.nextLine();
-                String[] numberList = number.split(" ");
-                System.out.print("remove ");
-                for (String i : numberList) {
-                    int index = Integer.parseInt(i) - 1;
-                    taskList[index].setDone(true);
-                    System.out.print(taskList[index].getDescription() + ", ");
-                }
-                System.out.println("\n / done tasks, good job! / ");
+                markTasksAsDone(in, taskList);
                 break;
             case ("clear"):
-                taskList = new Task[100];
-                taskCount = 0;
+                taskList = clearTaskList();
                 break;
             case ("mascot"):
-                Mascot Jim = new Mascot();
-                String text = in.nextLine();
-                Mascot.penguinSay(text);
+                mascotSay(in);
                 break;
             case("echo"):
                 echoCommand();
@@ -112,6 +84,60 @@ public class Duke {
             }
         } while (!command.equals("bye"));
         sayGoodbye();
+    }
+
+    private static boolean isDukeFail() {
+        if (Duke.getIsFail()) {
+            System.out.println("        bot failed, sometimes we all need a break       ");
+            return true;
+        }
+        return false;
+    }
+
+    private static void mascotSay(Scanner in) {
+        Mascot Jim = new Mascot();
+        String text = in.nextLine();
+        Mascot.penguinSay(text);
+    }
+
+    private static void setupUsernamePassword(Scanner in) {
+        AccountDetail user = new AccountDetail();
+        System.out.print("Username [dukeBot]: ");
+        AccountDetail.setUsername(in.nextLine());
+        System.out.print("Password [bukeDot]: ");
+        AccountDetail.setPassword(in.nextLine());
+    }
+
+    private static Task[] clearTaskList() {
+        Task[] taskList = new Task[100];
+        taskCount = 0;
+        return taskList;
+    }
+
+    private static void markTasksAsDone(Scanner in, Task[] taskList) {
+        String number = in.nextLine();
+        String[] numberList = number.split(" ");
+        System.out.print("remove ");
+        for (String i : numberList) {
+            int index = Integer.parseInt(i) - 1;
+            taskList[index].setDone(true);
+            System.out.print(taskList[index].getDescription() + ", ");
+        }
+        System.out.println("\n / done tasks, good job! / ");
+    }
+
+    private static int addTaskToList(Scanner in, Task[] taskList, int taskCount) {
+        String taskName;
+        do {
+            taskName = in.nextLine();
+            if (taskName.equals("stop")) {
+                return STOP_ADD;
+            }
+            taskList[taskCount] = new Task(taskName);
+            taskCount++;
+        } while (!taskName.equals("stop"));
+        System.out.println("Finished adding tasks!");
+        return taskCount;
     }
 
     public static void main(String[] args) {
