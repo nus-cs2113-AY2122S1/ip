ee8ddc3f42aeda2fc938bc32bc453cf956a22a80
jhsee5 <70202843+jhsee5@users.noreply.github.com>
2021-08-30 21:26:27 +0800

    add Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,12 +7,27 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("");
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("____________________________________________________________");
+
+        //Create scanner object to take in input
+        String echo;
+        Scanner in = new Scanner(System.in);
+        do {
+            echo = in.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(echo);
+            System.out.println("____________________________________________________________");
+        } while (!echo.equals("bye"));
+
+        if (echo.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("____________________________________________________________");
+            return;
+        }
     }
 }
