53ba63b58a5e86bb1fa1ad7e75bfaa443b4f6125
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-08-31 00:07:57 +0800

    Refactor code (initial) to attempt SLAP style coding

--- src/main/java/Alfred.java
+++ src/main/java/Alfred.java
@@ -2,66 +2,16 @@ import java.util.Scanner;
 
 public class Alfred {
     public static void main(String[] args) {
-        String logo =
-                " **********************************\n" +
-                " *     _    _  __              _  *\n" +
-                " *    / \\  | |/ _|_ __ ___  __| | *\n" +
-                " *   / _ \\ | | |_| '__/ _ \\/ _` | *\n" +
-                " *  / ___ \\| |  _| | |  __/ (_| | *\n" +
-                " * /_/   \\_\\_|_| |_|  \\___|\\__,_| *\n" +
-                " **********************************\n";
-        System.out.println(logo);
-        System.out.println(
-                "____________________________________________________________\n" +
-                " Welcome back, Master Wayne.\n" +
-                " How may I be of service to you?\n" +
-                "____________________________________________________________\n"
-        );
-
-        Task[] toDoList = new Task[100];
-        int listIndex = 0;
+        TaskManager taskManager = new TaskManager();
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        while (!userInput.equalsIgnoreCase("bye")) {
-            if (userInput.equals("list")) {
-                System.out.println("____________________________________________________________\n");
-                if (listIndex == 0) {
-                    System.out.println(" Your schedule is clear, Master Wayne.");
-                } else {
-                    System.out.println(" Your tasks, sir:");
-                    for (int i = 0; i < listIndex; i++) {
-                        System.out.print(" ");
-                        System.out.print(i+1);
-                        System.out.println(".[" + toDoList[i].getStatusIcon() + "] " + toDoList[i].getDescription());
-                    }
-                }
-                System.out.println("____________________________________________________________\n");
-            } else if (userInput.startsWith("done")) {
-                String[] destructuredInput = userInput.split(" ");
-                int index = Integer.parseInt(destructuredInput[1]) - 1;
-                toDoList[index].setTaskDone();
-                System.out.println("____________________________________________________________\n");
-                System.out.println(" Duly noted on completion of task, sir.");
-                System.out.println("   [X] " + toDoList[index].getDescription());
-                System.out.println("____________________________________________________________\n");
-            } else {
-                Task t = new Task(userInput);
-                toDoList[listIndex] = t;
-                listIndex++;
-                System.out.println(
-                        "____________________________________________________________\n" +
-                        " I shall put this in your schedule, Master Wayne: \"" +
-                        userInput + "\".\n" +
-                        "____________________________________________________________\n"
-                );
+        String userInput;
+        while (true) {
+            userInput = scanner.nextLine().trim();
+            if (userInput.equalsIgnoreCase("bye")) {
+                taskManager.shutdownMessage();
+                return;
             }
-
-            userInput = scanner.nextLine();
+            taskManager.processInput(userInput);
         }
-        System.out.println(
-                "____________________________________________________________\n" +
-                " Very well sir, I shall leave you to your own devices.\n" +
-                "____________________________________________________________\n"
-        );
     }
 }
