90cd506da6be3d2c5fb6bbbec613aa41da7bde32
Rakesh12000 <ravindiranrakesh12@gmail.com>
2021-08-30 14:12:52 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "_________________________________________________\n";
 
-        System.out.println(logo + "Hello! I'm Duke\n" + "What can I do for you\n" + logo + "Bye. Hope to see you again soon!\n" + logo);
+        System.out.println(logo + "Hello! I'm Duke\n" + "Whatchu want\n" + logo);
+
+        Scanner in = new Scanner(System.in);
+        String echo = "hi";
+
+        do {
+            echo = in.nextLine();
+            if (!isSame(echo)) {
+                System.out.println(logo + echo + "\n" + logo);
+            }
+        } while (!isSame(echo));
+
+        System.out.println(logo + "Bye. Hope to see you again soon!\n" + logo);
+
+    }
+
+    public static boolean isSame(String word) {
+        if (word.equals("Bye") || word.equals("bye")) {
+            return true;
+        }
+        else return false;
     }
 }
