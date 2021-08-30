a3c95f480061ea4010fc36dac3176990d90d2c91
alwinangys <alwinang99@gmail.com>
2021-08-31 00:57:32 +0800

    Fix toString function

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -9,6 +9,6 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + "(at: " + duration + ")";
+        return "[E]" + super.toString() + "(at:" + duration + ")";
     }
 }
