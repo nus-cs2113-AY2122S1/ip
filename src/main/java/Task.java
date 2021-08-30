d2350a04fdf90ae7e163cf52c3dcec89431ded69
Huang Che Yen <huangcheyen@gmail.com>
2021-08-30 13:30:12 +0800

    Added Task Class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description)
+    {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public Task()
+    {
+        this.description = "empty";
+        this.isDone = false;
+    }
+
+    public void markAsDone()
+    {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon()
+    {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
 }
