2aaa7f9cf67b997d92ce8d5e832ce08b34a0f99d
Huang Che Yen <huangcheyen@gmail.com>
2021-08-30 13:27:37 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -31,42 +31,54 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        // Level-2
+        // Level-3
         String line;
         Scanner in = new Scanner(System.in);
         System.out.print("Type something: ");
         line = in.nextLine();
 
+        // List of variables
         String[] task_list = new String[100];
-
+        Task[] schedule = new Task[100];
         int total_tasks = 0;
+
         while (true)
         {
+            // Exit
+
             if (line.equals("bye"))
             {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
 
+            // List down
             if (line.equals("list"))
             {
                 for (int i = 0; i < total_tasks; i += 1)
                 {
-                    System.out.println((i+1) + ". " + task_list[i]);
+                    System.out.print((i+1) + "." + "[" + schedule[i].getStatusIcon() + "] ");
+                    System.out.println(schedule[i].description);
                 }
-                line = in.nextLine();
             }
+            
+            //Done
+            else if ((line.length() > 4) && line.substring(0, 4).equals("done"))
+            {
+                int number = Character.getNumericValue(line.charAt(5));
+                schedule[number - 1].markAsDone();
+            }
+
+            // Check for duplicates and add accordingly
             else if (diff_string_check(task_list, line))
             {
-                System.out.println("added: " + line);
+                Task t = new Task(line);
+                schedule[total_tasks] = t;
                 task_list[total_tasks] = line;
                 total_tasks += 1;
-                line = in.nextLine();
             }
-            else
-            {
-                line = in.nextLine();
-            }
+
+            line = in.nextLine();
         }
 
     }
