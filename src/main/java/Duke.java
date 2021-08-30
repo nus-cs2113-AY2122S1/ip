445e75e410bb2563ead1f8367e241ed3d015c214
jiangweichen835 <e0425445@u.nus.edu>
2021-08-30 22:40:45 +0800

    Add level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,21 +11,35 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
         System.out.println("_______________________________________\n");
+        inputTask();
+    }
 
+    public static void inputTask() {
         Scanner in = new Scanner(System.in);
+        String inputTask = in.nextLine();
+        String[] tasks = new String[100];
+        int count = 0;
+        boolean isTrue = true;
 
-        String list = in.nextLine();
-        System.out.println(list + "\n");
-        System.out.println("_______________________________________\n");
-
-        String blah = in.nextLine();
-        System.out.println(blah + "\n");
-        System.out.println("_______________________________________\n");
-
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("_______________________________________\n");
-
+        while(!inputTask.equals("bye")) {
+            if (inputTask.equals("list")) {
+                System.out.println("_______________________________________\n");
+                System.out.println("To do list:\n");
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                System.out.println("_______________________________________\n");
+                System.out.println("added: " + inputTask);
+                tasks[count] = inputTask;
+                count++;
+            }
+            inputTask = in.nextLine();
+        }
+        if (inputTask.equals("bye")) {
+            System.out.println("_______________________________________\n");
+            System.out.println("Bye. Hope to see you again soon!\n");
+        }
         in.close();
     }
 }
-
