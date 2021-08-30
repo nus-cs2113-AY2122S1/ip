345b93443a0a181f2d77e89bf16e086690330deb
leecheokfeng <leecheokfeng@gmail.com>
2021-08-30 11:19:56 +0800

    Add and use echo command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void echoCommand() {
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        line = in.nextLine();
+
+        if (line.equals("bye")) {
+            System.out.println("-------------------------------------");
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("-------------------------------------");
+        } else {
+            System.out.println("-------------------------------------");
+            System.out.println(line);
+            System.out.println("-------------------------------------");
+            echoCommand();
+        }
+
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,6 +31,9 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-------------------------------------");
+
+        echoCommand();
+        
     }
 }
