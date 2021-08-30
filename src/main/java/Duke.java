1030885bc010af2402ecc888976b60cf70cf3fd0
jhsee5 <70202843+jhsee5@users.noreply.github.com>
2021-08-30 22:10:49 +0800

    add Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,14 +13,29 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
 
-        //Create scanner object to take in input
-        String echo;
-        Scanner in = new Scanner(System.in);
+        String echo; //echo variable to take in user input
+        String[] taskList = new String[100]; //create array to store text entered by user
+        int taskCount = 0; //track number of tasks
+
+        Scanner in = new Scanner(System.in); //Create scanner object to take in input
         do {
-            echo = in.nextLine();
+            echo = in.nextLine(); //user input
+
             System.out.println("____________________________________________________________");
-            System.out.println(echo);
+            switch(echo) {
+            case "list":
+                for(int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + taskList[i]);
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            default:
+            System.out.println("added: " + echo);
+            taskList[taskCount] = echo;
+            taskCount++;
             System.out.println("____________________________________________________________");
+            continue;
+            }
         } while (!echo.equals("bye"));
 
         if (echo.equals("bye")) {
