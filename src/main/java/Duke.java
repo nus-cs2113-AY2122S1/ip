b34b84a343da68f507ed7b10afa3ae03cb376202
Huang Che Yen <huangcheyen@gmail.com>
2021-08-30 10:35:29 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static boolean diff_string_check(String[] arr, String toCheckValue)
+    {
+        boolean test = true;
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (toCheckValue.equals(arr[i]))
+            {
+                test = false;
+                break;
+            }
+            else if (arr[i] == null)
+            {
+                break;
+            }
+        }
+
+        return test;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,30 +31,43 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        // Level-0
-        System.out.println("What can I do for you today?");
-
-        // Level-1
+        // Level-2
         String line;
         Scanner in = new Scanner(System.in);
         System.out.print("Type something: ");
         line = in.nextLine();
+
+        String[] task_list = new String[100];
+
+        int total_tasks = 0;
         while (true)
         {
             if (line.equals("bye"))
             {
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
+            }
+
+            if (line.equals("list"))
+            {
+                for (int i = 0; i < total_tasks; i += 1)
+                {
+                    System.out.println((i+1) + ". " + task_list[i]);
+                }
+                line = in.nextLine();
+            }
+            else if (diff_string_check(task_list, line))
+            {
+                System.out.println("added: " + line);
+                task_list[total_tasks] = line;
+                total_tasks += 1;
+                line = in.nextLine();
             }
             else
             {
-                System.out.println(line);
                 line = in.nextLine();
             }
         }
-        
-
-
-
 
     }
 }
