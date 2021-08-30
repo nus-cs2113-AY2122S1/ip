be4fd76ff9a8eab40be8cfe06d7c86dc1e74d3cc
Huang Che Yen <huangcheyen@gmail.com>
2021-08-30 09:46:26 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,6 +8,31 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Level-0
         System.out.println("What can I do for you today?");
+
+        // Level-1
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Type something: ");
+        line = in.nextLine();
+        while (true)
+        {
+            if (line.equals("bye"))
+            {
+                break;
+            }
+            else
+            {
+                System.out.println(line);
+                line = in.nextLine();
+            }
+        }
+        
+
+
+
+
     }
 }
