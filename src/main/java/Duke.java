250e19d5b8e9b909de8cd997a895e58f7d660e89
rebecca <e0560032@u.nus.edu>
2021-08-31 14:12:03 +0800

    Added todo, deadline and event, added personality to bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,19 @@ import java.util.Arrays;
 
 public class Duke {
 
-    public static void printTasks(Task[] list, int totalTasks) {
-        Task[] printList = Arrays.copyOf(list, totalTasks);
+    public static void printTask(Task newTask, int taskCount) {
+        String toPrint = newTask.toString();
+        System.out.println("Ok! I've added this task:");
+        System.out.println(newTask);
+        System.out.println("Now you have " + taskCount + " tasks in the list uwu");
+    }
+
+    public static void printTaskList(Task[] list, int totalTasks) {
+        Task[] printList = Arrays.copyOf(list, totalTasks); // only copy until the part you want
         int i = 1;
+        System.out.println("Here are the tasks in your list:");
         for (Task task : printList) {
-            System.out.println(i + "." + "[" + task.getStatusIcon() + "] " + task.description);
+            System.out.println(i + "." + task);
             i += 1;
         }
     }
@@ -16,29 +24,46 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         int taskCount = 0;
 
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("Hello bbygirl! I'm Your Boyfriend <3");
+        System.out.println("How can I help you today? ;)");
         Task t = new Task(in.nextLine());
-        System.out.println(t.description);
         Task[] taskList = new Task[100];
 
         while (!t.description.equals("bye")) {
             if (t.description.equals("list")) {
-                System.out.println("Here are the tasks on your list:");
-                printTasks(taskList, taskCount);
+                printTaskList(taskList, taskCount);
             } else if (t.description.contains("done")) {
+                // marking task as done
                 String number = t.description.substring(5);
                 int doneIndex = Integer.parseInt(number) - 1;
                 taskList[doneIndex].isDone = true;
                 System.out.println("Good job! I've marked these tasks as done:");
-                printTasks(taskList, taskCount);
+                printTaskList(taskList, taskCount);
             } else {
-                System.out.println("added: " + t.description);
-                taskList[taskCount] = t;
+                // adding a new task
+                Task newTask = new Task("not initialised");
+                if (t.description.contains("todo")) { // create a new todo
+                    newTask = new Todo(t.description.substring(4));
+                } else if (t.description.contains("deadline")) {
+                    // create a new deadline
+                    int start = t.description.indexOf('/');
+                    // find the date
+                    String task = t.description.substring(9, start - 1);
+                    String date = t.description.substring(start + 3);
+                    newTask = new Deadline(task, date);
+                } else if (t.description.contains("event")) {
+                    // find the date
+                    int start = t.description.indexOf('/');
+                    String task = t.description.substring(6, start - 1);
+                    String date = t.description.substring(start + 3);
+                    newTask = new Event(task, date);
+                }
+                taskList[taskCount] = newTask;
                 taskCount += 1;
+                printTask(newTask, taskCount);
             }
             t = new Task(in.nextLine());
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Goodbye. I will miss you sooo much :(");
     }
 }
