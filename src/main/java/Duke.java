b90d4e3a19587c74f7932b6e85e7561c812024f9
LAPTOP-AHT6DECF\edly1 <edly1999@gmail.com>
2021-08-31 19:21:14 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
-    public static void echo(String userMessage){
-        if (userMessage.equalsIgnoreCase("bye")){
-            System.out.println("____________________________________________________________");
-            System.out.println("Bye. Hope to see you again soon!");
-            System.out.println("____________________________________________________________");
-        }
-        else {
-            System.out.println(userMessage);
-            Scanner in = new Scanner(System.in);
-            String nextMessage = in.nextLine();
-            echo(nextMessage);
-        }
-    }
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        String[] entries = new String[100];
+        int entriesIndex = 0;
         Scanner in = new Scanner(System.in);
         String userMessage = in.nextLine();
-        echo(userMessage);
+        while(!userMessage.equalsIgnoreCase("bye")){
+            if (!userMessage.equalsIgnoreCase("list")){
+                System.out.println("Added " + userMessage);
+                entries[entriesIndex] = userMessage;
+                entriesIndex++;
+                userMessage = in.nextLine();
+            }
+            else {
+                System.out.println(Arrays.toString(Arrays.copyOf(entries,entriesIndex)));
+                userMessage = in.nextLine();
+            }
+        }
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
