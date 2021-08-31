eca446a7f03a80fc73c45e621ffbfa8e1b1647de
LAPTOP-AHT6DECF\edly1 <edly1999@gmail.com>
2021-08-31 21:43:45 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,21 +8,32 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        String[] entries = new String[100];
-        int entriesIndex = 0;
+        Task[] entries = new Task[100];
+        int entriesCount = 0;
         Scanner in = new Scanner(System.in);
         String userMessage = in.nextLine();
         while(!userMessage.equalsIgnoreCase("bye")){
-            if (!userMessage.equalsIgnoreCase("list")){
-                System.out.println("Added " + userMessage);
-                entries[entriesIndex] = userMessage;
-                entriesIndex++;
-                userMessage = in.nextLine();
+            if (userMessage.equalsIgnoreCase("list")){
+                System.out.println("Here are the tasks in your list:");
+                int i = 0;
+                for(Task entry : Arrays.copyOf(entries,entriesCount)){
+                    System.out.println((i+1) + ". " + "[" + entry.getStatusIcon() + "] " + entry.description);
+                    i++;
+                }
+            }
+            else if (userMessage.startsWith("done")){
+                String stringTaskIndex = userMessage.substring(userMessage.indexOf(" ") + 1);
+                int intTaskIndex = Integer.parseInt(stringTaskIndex) - 1;
+                entries[intTaskIndex].isDone = true;
+                System.out.println("Nice! I've marked this task as done:\n" + "   [" + entries[intTaskIndex].getStatusIcon() + "] " + entries[intTaskIndex].description);
             }
             else {
-                System.out.println(Arrays.toString(Arrays.copyOf(entries,entriesIndex)));
-                userMessage = in.nextLine();
+                System.out.println("Added " + userMessage);
+                //Task t = new Task(userMessage);
+                entries[entriesCount] = new Task(userMessage);
+                entriesCount++;
             }
+            userMessage = in.nextLine();
         }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
