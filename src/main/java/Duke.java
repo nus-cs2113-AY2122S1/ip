f6b89e2bb3301ce6ad8b649641d96aeba627e1b0
alvynben <alvynben@gmail.com>
2021-08-31 12:41:43 +0800

    Add new methods for parsing user input
    - createNewInputReader abstracts away the creation of a new Scanner
    - printLine abstracts aways System.out.println
    - parseUserInput abstracts away parsing the user input for a specific index
    - getUserInput abstracts away in.nextLine and also reacts if user types empty string

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,6 +9,9 @@ public class Duke {
             "   ██    ██   ██ ██      ██      ██ \n" +
             "   ██    ██   ██ ██ ███████ ███████ \n";
 
+    /** String of underscores to separate user input and Triss output */
+    public static final String SEPARATOR_LINE = "____________________________________________________________";
+
     /** Boolean to track if user has said "bye" */
     private static boolean hasUserSaidBye = false;
 
@@ -33,16 +36,17 @@ public class Duke {
         printWelcomeMessage();
 
         // Initialise user input reader
-        Scanner in = new Scanner(System.in);
+        Scanner in = createNewInputReader();
 
         // While user has not said "bye", check for next line of input
         while (!hasUserSaidBye) {
 
-            String userInput = in.nextLine();
-            String userCommand = userInput.split(" ")[0];
-            System.out.println("____________________________________________________________");
+            // Get the next line of input, and parse it to find the user's command (first word in input)
+            String userInput = getUserInput(in);
+            String userCommand = parseUserInput(userInput, 0);
+            printLine(SEPARATOR_LINE);
 
-            // Check if user has said "bye"
+            // Perform actions based on user's command
             if (userCommand.equals("bye")) {
                 printShutdownMessage();
             } else if (userCommand.equals("list")) {
@@ -53,15 +57,36 @@ public class Duke {
                 handleUserCreatingTask(userInput);
             }
 
-            System.out.println("____________________________________________________________");
+            printLine(SEPARATOR_LINE);
 
         }
     }
 
+    private static Scanner createNewInputReader() {
+        return new Scanner(System.in);
+    }
+
+    private static void printLine(String s) {
+        System.out.println(s);
+    }
+
+    private static String parseUserInput(String userInput, int i) {
+        return userInput.split(" ")[i];
+    }
+
+    private static String getUserInput(Scanner in) {
+        userInput = in.nextLine();
+
+        while (userInput.equals(null) || userInput.equals("")) {
+            printLine("Stop with the silent treatment! Say something?");
+            printLine(SEPARATOR_LINE);
+            userInput = in.nextLine();
+        }
+        return userInput;
+    }
+
     private static void handleUserCreatingTask(String userInput) {
-        String taskType = userInput.split(" ")[0];
-        String taskName;
-        String deadlineDate;
+        String taskType = parseUserInput(userInput, 0);
 
         if (taskType.equals("deadline")) {
             deadlineDate = userInput.substring(userInput.indexOf("/") + 1).trim();
@@ -82,7 +107,7 @@ public class Duke {
         // Increase current number of tasks by 1
         noOfTasks++;
         // Then, echo the task
-        System.out.println("I've added: " + tasks[noOfTasks - 1].printTask());
+        printLine("I've added: " + tasks[noOfTasks - 1].printTask());
     }
 
     private static void handleUserMarkingTaskAsDone(String userInput) {
@@ -90,16 +115,16 @@ public class Duke {
         int indexOfCompletedTask;
         // Throw exception if user did not type a number after "done"
         try {
-            indexOfCompletedTask = Integer.parseInt(userInput.split(" ")[1]) - 1;
+            indexOfCompletedTask = Integer.parseInt(parseUserInput(userInput, 1)) - 1;
         } catch (Exception e) {
-            System.out.println("Ach, nee! That task does not exist.");
+            printLine("Ach, nee! That task does not exist.");
             return;
         }
 
 
         // If task does not exist, do not delete any task
         if (indexOfCompletedTask > noOfTasks - 1 || indexOfCompletedTask < 0) {
-            System.out.println("Apologies! That task does not exist.");
+            printLine("Apologies! That task does not exist.");
             return;
         }
 
@@ -108,38 +133,38 @@ public class Duke {
 
         // If task was already done, let user know
         if (chosenTask.isDone()) {
-            System.out.println("Oh! This task was already marked as done:");
+            printLine("Oh! This task was already marked as done:");
             // Print out the task in the following format: "    [X] Task"
-            System.out.println("    " + chosenTask.printTask());
+            printLine("    " + chosenTask.printTask());
             return;
         }
 
         // If task exists, and is not done, mark it as done
         chosenTask.setDone(true);
-        System.out.println("Wunderbar! This task has been marked as done:");
+        printLine("Wunderbar! This task has been marked as done:");
 
         // Print out the task in the following format: "    [X] Task"
-        System.out.println("    " + chosenTask.printTask());
+        printLine("    " + chosenTask.printTask());
     }
 
     private static void printAllTasks() {
         // If user said "list", print a list of all saved tasks
         for (int i = 0; i < noOfTasks; i++) {
-            System.out.println(Integer.toString(i + 1) + "." + tasks[i].printTask());
+            printLine(Integer.toString(i + 1) + "." + tasks[i].printTask());
         }
     }
 
     private static void printShutdownMessage() {
         // If user said "bye", update hasUserSaidBye and print closing phrase
         hasUserSaidBye = true;
-        System.out.println("Thanks for coming. Auf wiedersehen!");
+        printLine("Thanks for coming. Auf wiedersehen!");
     }
 
     private static void printWelcomeMessage() {
-        System.out.println("Hello from\n" + LOGO);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Triss :)");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        printLine("Hello from\n" + LOGO);
+        printLine(SEPARATOR_LINE);
+        printLine("Hello! I'm Triss :)");
+        printLine("What can I do for you?");
+        printLine(SEPARATOR_LINE);
     }
 }
