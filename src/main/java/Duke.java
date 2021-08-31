35c951fe421dc33d819574bd1af9bc722c84c31f
Rakesh12000 <ravindiranrakesh12@gmail.com>
2021-08-31 21:56:24 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,40 +9,52 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
 
-        String[] List = new String[100];
         Task[] Task = new Task[100];
         String word;
+        String date = "";
         int Count = 0;
 
         do {
             word = in.nextLine();
-            String[] words = word.split(" ");
+            String[] words = word.split(" "); // stores string of multiple words in a string Array
 
-            if (isDone(words[0])) {
-                int i = Integer.parseInt(words[1]) -1;
-                Task[i].markAsDone();
-                System.out.println(logo + "Nice! I've marked this task as done:\n" + "[" + Task[i].getStatusIcon() + "] " + List[i] + "\n" + logo);
-                //System.out.println(Arrays.toString(words) + Task[number].getStatusIcon());
+            if (isDone(words[0])) { //complete Tasks
+                int i = Integer.parseInt(words[1]) - 1; // gets Task number
+                Task[i].setDone();
+                System.out.println(logo + "Nice! I've marked this task as done:\n" + Task[i] + "\n" + logo);
             }
 
-            else if (!isBye(word) && !isList(word)) {
-                List[Count] = word;
-                Task[Count] = new Task(word);
-                System.out.println(logo + "added: " + word + "\n" + logo);
+            else if (!isBye(word) && !isList(word)) { //Task words
+                if (isTodo(words[0])) { //Todo task
+                    Task[Count] = new Todo(word);
+                    Count++;
+                }
+                else if (isDeadline(words[0])) { //Deadline task
+                    date = word.substring(word.lastIndexOf("/") + 1);
+                    Task[Count] = new Deadline(word, date);
+                    Count++;
+                }
 
-                Count++;
+                else if (isEvent(words[0])) { //Event task
+                    date = word.substring(word.lastIndexOf("/") + 1);
+                    Task[Count] = new Event(word, date);
+                    Count++;
+                }
+
+                System.out.println(logo + "Added:\n" + Task[Count-1] + "\n" + "Now you have " + Count  + " tasks in the list.\n" + logo);
             }
 
-            else if (isList(word)) {
+            else if (isList(word)) { //List tasks
                 System.out.println(logo + "Here are the tasks in your list:");
                 for (int i = 0; i < Count; i++) {
-                    System.out.println((i + 1) + ". [" + Task[i].getStatusIcon() + "] " + List[i]);
+                    System.out.print(i + 1 + ". ");
+                    System.out.println(Task[i]);
                 }
                 System.out.print(logo);
             }
 
 
-        } while (!isBye(word));
+        } while (!isBye(word)); //Exit
 
         System.out.println(logo + "Bye. Hope to see you again soon!\n" + logo);
 
@@ -60,5 +72,15 @@ public class Duke {
         return word.equals("Done") || word.equals("done");
     }
 
+    public static boolean isTodo(String word) {
+        return word.equals("Todo") || word.equals("todo");
+    }
 
+    public static boolean isDeadline(String word) {
+        return word.equals("Deadline") || word.equals("deadline");
+    }
+
+    public static boolean isEvent(String word) {
+        return word.equals("Event") || word.equals("event");
+    }
 }
