e3c87b90adc1d86c149356726b57b229577d01d4
KZQ1999 <e0543569@u.nus.edu>
2021-08-31 18:26:25 +0800

    Added Deadline class which is a subclass of Task

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,11 @@
+public class Deadline extends Task{
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+    public void printList(Task theTask, int index) {
+        System.out.println(index + ".[D][" + (theTask.getStatus()) + "] "+ theTask.getDescription() + "(by: " + by + ")");
+    }
+}
