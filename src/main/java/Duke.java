7879f30b23fbe02d135ae1b3384005bf4985ef14
Kureans <leonardtengys@gmail.com>
2021-09-01 06:50:16 +0800

    Refactor codebase to be more readable/maintainable Add a new TaskManager class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,107 +22,17 @@ public class Duke {
         System.out.println("");
     }
 
-    public static void printInvalidDoneMessage() {
-        System.out.println("Sorry bud, you can't check off what is not yet there :/");
-    }
-
-    public static void printGenericErrorMessage() {
-        System.out.println("Error! Something is up with your inputs!");
-    }
-
-    public static void printTaskList(int taskIndex, Task tasks[]) {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < taskIndex; i++) {
-            if (tasks[i].getType().equals("T")) {
-                System.out.printf("[%s][%s] %s%n",tasks[i].getType(), tasks[i].getStatusIcon(),tasks[i].getDescription());
-            } else if (tasks[i].getType().equals("D")){
-                System.out.printf("[%s][%s] %s(by:%s)%n",tasks[i].getType(), tasks[i].getStatusIcon(),tasks[i].getDescription(), tasks[i].getByDateTime());
-            } else {
-                System.out.printf("[%s][%s] %s(at:%s)%n",tasks[i].getType(), tasks[i].getStatusIcon(), tasks[i].getDescription(), tasks[i].getStartAndEndTime());
-            }
-        }
-    }
-
-    public static void addNewTodo(String description, int taskIndex, Task tasks[]) {
-        tasks[taskIndex] = new ToDo(description);
-    }
-
-    public static void addNewDeadline(String description, String byDateTime, int taskIndex, Task tasks[]) {
-        tasks[taskIndex] = new Deadline(description, byDateTime);
-    }
-
-    public static void addNewEvent(String description, String startAndEndTime, int taskIndex, Task tasks[]) {
-        tasks[taskIndex] = new Event(description, startAndEndTime);
-    }
-
-    public static void printAddedTask(Task task, int taskIndex) {
-        System.out.println("Got it. I've added this task:");
-        if (task.getType().equals("T")) {
-            System.out.printf("[%s][ ] %s%n",task.getType(),task.getDescription());
-        } else if (task.getType().equals("D")){
-            System.out.printf("[%s][ ] %s(by:%s)%n",task.getType(),task.getDescription(), task.getByDateTime());
-        } else {
-            System.out.printf("[%s][ ] %s(at:%s)%n",task.getType(),task.getDescription(), task.getStartAndEndTime());
-        }
-
-        System.out.println("Now you have " + (taskIndex + 1) + " tasks in the list.");
-    }
-
-
-
-    public static void markAsDone(Task task) {
-        if (task.getStatusIcon().equals("X")) {
-            System.out.println("Slow down there bud! You've already completed this task!");
-        } else {
-            task.setDone();
-            System.out.println("Nice! I've marked this task as done:");
-            System.out.printf("[%s][X] %s%n",task.getType(), task.getDescription());
-        }
+    public static void readInputs(TaskManager manager) {
+        manager.parseUserInput();
     }
 
     public static void main(String[] args) {
 
         LizTextBanner liz = new LizTextBanner();
-        Scanner in = new Scanner(System.in);
+        TaskManager manager = new TaskManager();
 
-        Task tasks[] = new Task[100];
-        int taskIndex = 0;
         printGreetingMessage(liz);
-        String line = in.nextLine();
-
-        while (!line.equals("bye")) {
-            printLine();
-            String[] lineArgs = line.split(" ");
-            if (line.equals("list")) {
-                printTaskList(taskIndex, tasks);
-            } else if (lineArgs[0].equals("todo")) {
-                addNewTodo(line.substring(5), taskIndex, tasks);
-                printAddedTask(tasks[taskIndex], taskIndex);
-                taskIndex++;
-            } else if (lineArgs[0].equals("deadline")) {
-                int backslashIndex = line.indexOf('/');
-                addNewDeadline(line.substring(9, backslashIndex), line.substring(backslashIndex + 3), taskIndex, tasks);
-                printAddedTask(tasks[taskIndex], taskIndex);
-                taskIndex++;
-            } else if (lineArgs[0].equals("event")) {
-                int backslashIndex = line.indexOf('/');
-                addNewEvent(line.substring(6, backslashIndex), line.substring(backslashIndex + 3), taskIndex, tasks);
-                printAddedTask(tasks[taskIndex], taskIndex);
-                taskIndex++;
-            } else if (lineArgs[0].equals("done")) {
-                int doneIndex = Integer.parseInt(lineArgs[1]) - 1;
-                if (doneIndex >= taskIndex || doneIndex < 0) {
-                    printInvalidDoneMessage();
-                } else {
-                    markAsDone(tasks[doneIndex]);
-                }
-            } else {
-                printGenericErrorMessage();
-            }
-
-            printLine();
-            line = in.nextLine();
-        }
+        readInputs(manager);
         printExitMessage();
     }
 }
---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,191 @@
+import java.util.Scanner;
+
+public class TaskManager {
+    private static final String COMMAND_BYE = "bye";
+    private static final String COMMAND_LIST = "list";
+    private static final String COMMAND_TODO = "todo";
+    private static final String COMMAND_DEADLINE = "deadline";
+    private static final String COMMAND_EVENT = "event";
+    private static final String COMMAND_DONE = "done";
+
+    private static final String SEPARATOR_SPACE = " ";
+    private static final String SEPARATOR_BY = "/by";
+    private static final String SEPARATOR_AT = "/at";
+
+    private static final String TASK_TYPE_ICON_TODO = "T";
+    private static final String TASK_TYPE_ICON_DEADLINE = "D";
+    private static final String TASK_TYPE_ICON_EVENT = "E";
+    private static final String ICON_DONE = "X";
+
+    private static final int MAX_TASK_COUNT = 100;
+    private static final int TODO_DESCRIPTION_START_INDEX = 5;
+    private static final int DEADLINE_DESCRIPTION_START_INDEX = 9;
+    private static final int EVENT_DESCRIPTION_START_INDEX = 6;
+
+    private Task tasks[];
+    private int taskCount;
+
+    public TaskManager() {
+        this.tasks = new Task[MAX_TASK_COUNT];
+        this.taskCount = 0;
+    }
+
+    public static void printLine() {
+        for (int i = 0; i < 40; i++) {
+            System.out.print("_");
+        }
+        System.out.println("");
+    }
+
+    public static void printInvalidDoneMessage() {
+        System.out.println("Sorry bud, you can't check off what is not yet there :/");
+    }
+
+    public static void printAlreadyDoneMessage() {
+        System.out.println("Slow down there bud! You've already completed this task!");
+    }
+
+    public static void printGenericErrorMessage() {
+        System.out.println("Oops! Something went wrong :(");
+    }
+
+    public void printMarkAsDoneMessage(Task task) {
+
+        String taskType = task.getType();
+        String taskStatus = task.getStatusIcon();
+        String taskDescription = task.getDescription();
+
+        System.out.println("Nice! I've marked this task as done:");
+        if (taskType.equals(TASK_TYPE_ICON_TODO)) {
+            System.out.printf("[%s][%s] %s%n", taskType, taskStatus, taskDescription);
+        } else if (taskType.equals(TASK_TYPE_ICON_DEADLINE)) {
+            String taskByTime = task.getByDateTime();
+            System.out.printf("[%s][%s] %s(by:%s)%n", taskType, taskStatus, taskDescription, taskByTime);
+        } else if (taskType.equals(TASK_TYPE_ICON_EVENT)) {
+            String taskAtTime = task.getStartAndEndTime();
+            System.out.printf("[%s][%s] %s(at:%s)%n", taskType, taskStatus, taskDescription, taskAtTime);
+        } else {
+            printGenericErrorMessage();
+        }
+    }
+
+    public static void printTaskList(int taskCount, Task tasks[]) {
+
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+
+            String taskType = tasks[i].getType();
+            String taskStatus = tasks[i].getStatusIcon();
+            String taskDescription = tasks[i].getDescription();
+
+            if (taskType.equals(TASK_TYPE_ICON_TODO)) {
+                System.out.printf("[%s][%s] %s%n", taskType, taskStatus, taskDescription);
+            } else if (taskType.equals(TASK_TYPE_ICON_DEADLINE)) {
+                String taskByTime = tasks[i].getByDateTime();
+                System.out.printf("[%s][%s] %s(by:%s)%n", taskType, taskStatus, taskDescription, taskByTime);
+            } else if (taskType.equals(TASK_TYPE_ICON_EVENT)) {
+                String taskAtTime = tasks[i].getStartAndEndTime();
+                System.out.printf("[%s][%s] %s(at:%s)%n", taskType, taskStatus, taskDescription, taskAtTime);
+            } else {
+                printGenericErrorMessage();
+            }
+        }
+    }
+
+    public void addNewTodo(String description) {
+        tasks[taskCount] = new ToDo(description);
+        taskCount++;
+    }
+
+    public void addNewDeadline(String description, String byDateTime) {
+        tasks[taskCount] = new Deadline(description, byDateTime);
+        taskCount++;
+    }
+
+    public void addNewEvent(String description, String startAndEndTime) {
+        tasks[taskCount] = new Event(description, startAndEndTime);
+        taskCount++;
+    }
+
+    public void printAddedTaskMessage(Task task) {
+
+        String taskType = task.getType();
+        String taskDescription = task.getDescription();
+
+        System.out.println("Got it. I've added this task:");
+        if (taskType.equals(TASK_TYPE_ICON_TODO)) {
+            System.out.printf("[%s][ ] %s%n", taskType, taskDescription);
+        } else if (taskType.equals(TASK_TYPE_ICON_DEADLINE)) {
+            String byTime = task.getByDateTime();
+            System.out.printf("[%s][ ] %s(by:%s)%n", taskType, taskDescription, byTime);
+        } else if (taskType.equals((TASK_TYPE_ICON_EVENT))) {
+            String atTime = task.getStartAndEndTime();
+            System.out.printf("[%s][ ] %s(at:%s)%n", taskType, taskDescription, atTime);
+        } else {
+            printGenericErrorMessage();
+        }
+
+        System.out.println("Now you have " + taskCount + " tasks in the list.");
+    }
+
+    public void markAsDone(Task task) {
+        if (task.getStatusIcon().equals(ICON_DONE)) {
+            printAlreadyDoneMessage();
+        } else {
+            task.setDone();
+            printMarkAsDoneMessage(task);
+        }
+    }
+
+    public void parseUserInput() {
+
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            String line = in.nextLine();
+            if (line.equals(COMMAND_BYE)) {
+                break;
+            }
+            printLine();
+            String[] lineArgs = line.split(SEPARATOR_SPACE);
+            String command = lineArgs[0];
+            if (line.equals(COMMAND_LIST)) {
+                printTaskList(taskCount, tasks);
+            } else if (command.equals(COMMAND_DONE)) {
+                int doneIndex = Integer.parseInt(lineArgs[1]) - 1;
+                if (doneIndex >= taskCount || doneIndex < 0) {
+                    printInvalidDoneMessage();
+                } else {
+                    markAsDone(tasks[doneIndex]);
+                }
+            } else {
+                if (command.equals(COMMAND_TODO)) {
+
+                    addNewTodo(line.substring(TODO_DESCRIPTION_START_INDEX));
+                    printAddedTaskMessage(tasks[taskCount - 1]);
+
+                } else if (command.equals(COMMAND_DEADLINE)) {
+
+                    String[] descriptionAndByTimeArray = line.split(SEPARATOR_BY);
+                    String byTime = descriptionAndByTimeArray[1];
+                    String description = descriptionAndByTimeArray[0].substring(DEADLINE_DESCRIPTION_START_INDEX);
+                    addNewDeadline(description, byTime);
+                    printAddedTaskMessage(tasks[taskCount - 1]);
+
+                } else if (command.equals(COMMAND_EVENT)) {
+
+                    String[] descriptionAndAtTimeArray = line.split(SEPARATOR_AT);
+                    String atTime = descriptionAndAtTimeArray[1];
+                    String description = descriptionAndAtTimeArray[0].substring(EVENT_DESCRIPTION_START_INDEX);
+                    addNewEvent(description, atTime);
+                    printAddedTaskMessage(tasks[taskCount - 1]);
+
+                } else {
+                    printGenericErrorMessage();
+                }
+            }
+
+            printLine();
+        }
+    }
+}
