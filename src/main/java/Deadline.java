be1551e6d4125f7fd78f0e9ee2e6643ef1aadcd5
alvynben <alvynben@gmail.com>
2021-08-31 13:18:34 +0800

    Add comments to all class members and methods in Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,30 @@
 public class Deadline extends Task{
-    /**
-     * Represents the date this task should be done by
-     */
+    /** Represents the date this task should be done by */
     private String dueDate;
 
+    /**
+     * Creates a deadline with task type [D], and dueDate based on user's input.
+     * @param name The name of the deadline.
+     * @param dueDate The due date of the deadline.
+     */
     public Deadline(String name, String dueDate) {
         super(name);
         this.dueDate = dueDate;
         this.typeOfTask = "[D]";
     }
 
+    /**
+     * Get the due date of the deadline.
+     * @return Due date of the deadline.
+     */
     public String getDueDate() {
         return dueDate;
     }
 
+    /**
+     * Prints the deadline in a human-readable format.
+     * @return [Type of Task][Completion Status] [Name of Task] ([Due Date of Task])
+     */
     public String printTask() {
         return this.getTypeOfTask() + this.getDoneStatusAsSymbol() + " "
                 + this.getName() + " (" + this.getDueDate() + ")";
