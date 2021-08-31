c2b19f9b21a1208e100a8572b2ac570694140fa5
tlyi <t.leyi2305@gmail.com>
2021-08-31 15:35:34 +0800

    Remove option for echo mode

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,29 +2,39 @@ import java.util.Scanner;
 
 public class Duke {
 
-    //Commonly used message formats in UI
-    private static final String DIVIDER = "________________________________________________________________";
-    private static final String LS = System.lineSeparator();
-    private static final String MESSAGE_WELCOME_DUDE = "Hello! I'm Dude ヽ༼ ・ ل͜ ・ ༽ﾉ";
-    private static final String MESSAGE_BYE = "Bye! Hope to see you again soon! ヽ༼ ͠° ͟ل͜ ͠° ༽ﾉ ";
-    private static final String MESSAGE_WELCOME_ECHO = "Welcome to the Echo mode!" + LS
-            + "Type anything and i'll echo it right back!ヽ༼ ≧ ل͜ ≦ ༽ﾉ";
-    private static final String MESSAGE_WELCOME_TODO = "Welcome to the To-do List mode!" + LS
-            + "Type a task and i'll add it to your list real quick! ヽ༼ ˘ل͜ ˘ ༽ﾉ";
-    private static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid format! %1$s";
 
     //Commands
-    private static final String COMMAND_BYE = "Bye";
+    private static final String COMMAND_BYE = "bye";
     private static final String COMMAND_TODO = "todo";
     private static final String COMMAND_LIST = "list";
     private static final String COMMAND_DEADLINE = "deadline";
     private static final String COMMAND_EVENT = "event";
     private static final String COMMAND_DONE = "done";
 
+    //Commonly used message formats in UI
+    private static final String DIVIDER = "________________________________________________________________";
+    private static final String LS = System.lineSeparator();
+    private static final String QUOTATION = "\"";
+    private static final String MESSAGE_WELCOME_DUDE = "Hello! I'm Dude ヽ༼ ・ ل͜ ・ ༽ﾉ";
+    private static final String MESSAGE_BYE = "Bye! Hope to see you again soon! ヽ༼ ͠° ͟ل͜ ͠° ༽ﾉ ";
+    private static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid format! %1$s";
+    private static final String MESSAGE_COMMAND_LIST = "Commands:" + LS
+            + QUOTATION + COMMAND_TODO + " X" + QUOTATION + " : Add Todo task X" + LS
+            + QUOTATION + COMMAND_DEADLINE + " X Y" + QUOTATION + " : Add task X with deadline Y" + LS
+            + QUOTATION + COMMAND_EVENT + " X Y" + QUOTATION + " : Add event X with date/time details Y" + LS
+            + QUOTATION + COMMAND_LIST + QUOTATION + " : See lists of tasks" + LS
+            + QUOTATION + COMMAND_DONE + " X" + QUOTATION + " : Mark task number X as done" + LS
+            + QUOTATION + COMMAND_BYE + QUOTATION + " : Stop Dude :(";
+
+
     public static void showMessage(String... lines) {
         for (String line : lines) {
             System.out.println(line);
         }
+    }
+
+    public static void showListOfCommands() {
+        showMessage(DIVIDER, MESSAGE_COMMAND_LIST, DIVIDER);
     }
 
     public static void printDivider() {
@@ -41,61 +51,6 @@ public class Duke {
         printDivider();
         System.out.println("Bye! Hope to see you again soon! ヽ༼ ͠° ͟ل͜ ͠° ༽ﾉ ");
         printDivider();
-    }
-
-    /** Continuously asks user to select mode until valid mode is selected */
-    public static void selectMode() {
-        Scanner in = new Scanner(System.in);
-        String input;
-        boolean hasSelectedMode = false;
-        System.out.println("Please select my mode by typing in the number: ");
-        System.out.println("[1] Echo" + System.lineSeparator() + "[2] To-do List");
-        printDivider();
-        do {
-            input = in.nextLine();
-            try {
-                int modeNum = Integer.parseInt(input);
-                if (modeNum == 1) {
-                    hasSelectedMode = true;
-                    enterEchoMode();
-                } else if (modeNum == 2) {
-                    hasSelectedMode = true;
-                    enterToDoListMode();
-                } else {
-                    printDivider();
-                    System.out.println("Please input an existing mode number!");
-                    printDivider();
-                }
-            } catch (NumberFormatException e) {
-                //error if user did not input a valid integer for task number
-                printDivider();
-                System.out.println("Please input a valid mode number in numerical form!");
-                printDivider();
-            }
-        } while (!hasSelectedMode);
-    }
-
-    /** Continuously echoes user inputs and exits when user inputs "Bye" (not case-sensitive) */
-    public static void enterEchoMode() {
-        printDivider();
-        System.out.println("Welcome to the Echo mode!");
-        System.out.println("Type anything and i'll echo it right back!ヽ༼ ≧ ل͜ ≦ ༽ﾉ");
-        System.out.println("Other commands:" + System.lineSeparator()
-                + "\"bye\" : Stop Dude :(");
-        printDivider();
-        Scanner in = new Scanner(System.in);
-        String input;
-        boolean hasSaidBye = false;
-        do {
-            input = in.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
-                hasSaidBye = true;
-            } else {
-                printDivider();
-                System.out.println(input);
-                printDivider();
-            }
-        } while (!hasSaidBye);
     }
 
     public static void addTask(Task[] listOfTasks, String description) {
@@ -168,14 +123,7 @@ public class Duke {
      * Exits when user inputs "Bye" (not case-sensitive)
      */
     public static void enterToDoListMode() {
-        printDivider();
-        System.out.println("Welcome to the To-do List mode!");
-        System.out.println("Type a task and i'll add it to your list real quick! ヽ༼ ˘ل͜ ˘ ༽ﾉ");
-        System.out.println("Other commands:" + System.lineSeparator()
-                + "\"list\" : See lists of tasks" + System.lineSeparator()
-                + "\"done X\" : Mark task number X as done" + System.lineSeparator()
-                + "\"bye\" : Stop Dude :(");
-        printDivider();
+        showListOfCommands();
         Task[] listOfTasks = new Task[100];
         Scanner in = new Scanner(System.in);
         String input;
@@ -201,7 +149,7 @@ public class Duke {
 
     public static void main(String[] args) {
       welcome();
-      selectMode();
+      enterToDoListMode();
       exit();
     }
 }
