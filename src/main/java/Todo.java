ea90f394d2dc20c6415f336c286bec02e547e783
KZQ1999 <e0543569@u.nus.edu>
2021-08-31 18:26:10 +0800

    Added Todo class which is a subclass of Task

---
+++ src/main/java/Todo.java
@@ -0,0 +1,9 @@
+public class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    public void printList(Task theTask,int index) {
+        System.out.println(index + ".[T][" + (theTask.getStatus()) + "] "+ theTask.getDescription());
+    }
+}
