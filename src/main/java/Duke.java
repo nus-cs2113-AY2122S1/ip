44ec76dbbb4d6520e91ebd0a5d90eef0bbe5f791
Cuiminjing <e0492441@u.nus.edu>
2021-08-31 16:13:14 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,24 +2,24 @@ import java.util.Scanner;
 
 public class Duke {
 
+    public static final String Line = "    ____________________________________________________________";
     private static Task[] tasks = new Task[110];
     private static int taskSum = 0;
 
     public static void printList() {
         Task now;
-        System.out.println("____________________________________________________________");
+        System.out.println(Line);
+        System.out.println("     Here are the tasks in your list:");
         for(int i=0; i<taskSum; i++){
             now = tasks[i];
-            System.out.println(" " + (i+1) + ".[" + now.getInformation() + "]" + now.content);
+            System.out.println("     " + (i+1) + "." + now.toString());
         }
-        System.out.println("____________________________________________________________");
+        System.out.println(Line);
     }
     public static void bye() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(Line + "\n" + "     Bye. Hope to see you again soon!" + "\n" + Line);
     }
-    public static void main(String[] args) {
+    public static void greeting() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -30,18 +30,46 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+    }
+    public static void main(String[] args) {
+        greeting();
         String command;
         Scanner in = new Scanner(System.in);
-
         do{
             command = in.nextLine();
             if(command.contains("done")){
                 String[] number = command.split(" ");
                 int taskIndex = Integer.parseInt(number[1]) - 1;
                 tasks[taskIndex].complete();
-                System.out.println("____________________________________________________________");
-                System.out.println("[" + tasks[taskIndex].getInformation() + "]" + tasks[taskIndex].content);
-                System.out.println("____________________________________________________________");
+                System.out.println(Line + "\n" + "     Nice! I've marked this task as done:" + "\n       [" + tasks[taskIndex].getInformation() + "]" + tasks[taskIndex].content + "\n" + Line);
+            }
+            else if(command.contains("todo")){
+                int first = command.indexOf(" ");
+                String item = command.substring(first,command.length());
+                tasks[taskSum] = new Todo(item);
+                taskSum = taskSum + 1;
+                System.out.println(Line + "\n" + "     Got it. I've added this task: " + "\n" + "       [T][ ] " + item + "\n" + "     Now you have " + taskSum + " tasks in the list");
+                System.out.println(Line);
+            }
+            else if(command.contains("deadline")){
+                int first = command.indexOf(" ");
+                int itemEnd = command.indexOf("/");
+                String item = command.substring(first,itemEnd);
+                String by = command.substring(itemEnd + 2,command.length());
+                tasks[taskSum] = new Deadline(item,by);
+                taskSum = taskSum + 1;
+                System.out.println(Line + "\n" + "     Got it. I've added this task: " + "\n" + "       [D][ ] " + item + " (" + by + ")" + "\n" + "     Now you have " + taskSum + " tasks in the list");
+                System.out.println(Line);
+            }
+            else if(command.contains("event")){
+                int first = command.indexOf(" ");
+                int itemEnd = command.indexOf("/");
+                String item = command.substring(first,itemEnd);
+                String at = command.substring(itemEnd + 2,command.length());
+                tasks[taskSum] = new Event(item,at);
+                taskSum = taskSum + 1;
+                System.out.println(Line + "\n" + "     Got it. I've added this task: " + "\n" + "       [E][ ] " + item + " (" + at + ")" + "\n" + "     Now you have " + taskSum + " tasks in the list");
+                System.out.println(Line);
             }
             else{
                 switch (command){
@@ -51,17 +79,10 @@ public class Duke {
                     case "bye":
                         break;
                     default:
-                        tasks[taskSum] = new Task(command);
-                        taskSum = taskSum + 1;
-                        System.out.println("____________________________________________________________");
-                        System.out.println("added:" + command);
-                        System.out.println("____________________________________________________________");
 
                 }
             }
         }while(!command.equals("bye"));
         bye();
-
-
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -14,4 +14,8 @@ public class Task {
     public String getInformation() {
         return (Done ? "X" : " ");
     }
+    @Override
+    public String toString() {
+        return "[" + getInformation() + "]" + content;
+    }
 }
