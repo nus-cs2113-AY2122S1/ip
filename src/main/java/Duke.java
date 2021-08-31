10ba1da8f4eeec6d86c00d817822082c1fecf58e
LAPTOP-AHT6DECF\edly1 <edly1999@gmail.com>
2021-08-31 18:36:40 +0800

    Add Level - 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void echo(String userMessage){
+        if (userMessage.equalsIgnoreCase("bye")){
+            System.out.println("____________________________________________________________");
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("____________________________________________________________");
+        }
+        else {
+            System.out.println(userMessage);
+            Scanner in = new Scanner(System.in);
+            String nextMessage = in.nextLine();
+            echo(nextMessage);
+        }
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner in = new Scanner(System.in);
+        String userMessage = in.nextLine();
+        echo(userMessage);
     }
 }
