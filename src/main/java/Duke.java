e9762c4be3be1a34c4dac16e6286e6751959be8c
Jon-the-MELON <limyongkiat@yahoo.com.sg>
2021-08-31 23:36:15 +0800

    Level 4.3: Improved Code Quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,21 +2,58 @@ import java.util.Scanner;
 
 
 public class Duke {
-    public static final String LINES = "____________________________________________________________\n";
+    //Array of tasks
+    private static Task[] tasks;
+    //Total number of tasks
+    private static int taskCount;
 
-    //Intro message
-    public static void greeting(boolean isStart) {
-        if (isStart) {
-            String greeting = LINES + " HeLLO! I'm Jim, a real person who definitely passes reCaptchas!\n" +
-                    " What can I do for you today?\n" + LINES;
-            System.out.println(greeting);
-        }
+    private static final String LINES = "____________________________________________________________\n";
+    private static final String GREETING = " HeLLO! I'm Jim, a real person who definitely passes" +
+            " reCaptchas!\n";
+    private static final String BIRTHDAY_MESSAGE = " ^o^ Happy birthday to you! ^o^\n";
+    private static final String ECHO_MESSAGE = " Echoing after you!\n";
+    private static final String QUIT_MESSAGE = " That was annoying huh...\n";
+    private static final String NO_TASK_MESSAGE = "No such task! You're not THAT productive...\n";
+    private static final String TASK_COMPLETE_MESSAGE = "Nice! You're a real champ for finishing this: \n";
+    private static final String BYE_MESSAGE = " Bye! Remember, stay out of fire, suuuuuuper high level " +
+            "tactic yea?\n";
+    private static final String ADD_MESSAGE = " Got it. I've added this task:\n   ";
+    private static final String INVALID_COMMAND = "I don't quite understand :/\n";
+    private static final String HELP_MESSAGE = " Here are the commands for the things I can do:\n" +
+            "    1. todo [task] = adds a Todo task\n" +
+            "    2. deadline [task] /[deadline] = adds a Deadline task\n" +
+            "    3. event [task] /[event range] = adds an Event task\n" +
+            "    4. done [taskIndex] = marks the inputted task as done\n" +
+            "    5. list = lists out all current tasks with their taskIndex\n" +
+            "    6. echo = turn into a huge cave and echo your inputs back to you!!\n" +
+            "    7. quit (only in echo mode) = turn back to normal and stop echoing\n" +
+            "    8. genshin = begin the genshin helper\n" +
+            "    9. bye = shuts me down... ;-;\n";
+
+
+    private static final Scanner sc = new Scanner(System.in);
+    
+
+
+    //Prints greeting message
+    public static void greeting() {
+        System.out.println(LINES + GREETING + LINES);
     }
 
-    //echo function
-    public static void echo(String x) {
-        String output = LINES + " " + x + "\n" + LINES;
-        System.out.println(output);
+    //Begins echo function
+    public static void echo() {
+        System.out.println(LINES + ECHO_MESSAGE + LINES);
+        boolean echoState = true;
+        while (echoState) {
+            String s = sc.nextLine();
+            if (s.equalsIgnoreCase("quit")) {
+                System.out.println(LINES + QUIT_MESSAGE + LINES);
+                echoState = false;
+            } else {
+                String output = LINES + " " + s + "\n" + LINES;
+                System.out.println(output);
+            }
+        }
     }
 
     //list function
@@ -29,96 +66,113 @@ public class Duke {
         System.out.print(LINES);
     }
 
-    //adds task
-    public static int addTask(Task[] tasks, String input, int listCount) {
-        if (input.toUpperCase().startsWith("TODO ")) {
-            tasks[listCount] = new Todo(input.substring(5));
-            System.out.println(LINES + " Got it. I've added this task:\n   " + tasks[listCount]);
-            listCount += 1;
-            System.out.println(" Now you have " + listCount + " tasks in the list.\n" + LINES);
-        }
-        else if (input.toUpperCase().startsWith("DEADLINE ")) {
-            int slash = input.indexOf("/");
-            tasks[listCount] = new Deadline(input.substring(9, slash-1), input.substring(slash+1));
-            System.out.println(LINES + " Got it. I've added this task:\n   " + tasks[listCount]);
-            listCount += 1;
-            System.out.println(" Now you have " + listCount + " tasks in the list.\n" + LINES);
-        }
-        else if (input.toUpperCase().startsWith("EVENT ")) {
-            int slash = input.indexOf("/");
-            tasks[listCount] = new Event(input.substring(6, slash-1), input.substring(slash+1));
-            System.out.println(LINES + " Got it. I've added this task:\n   " + tasks[listCount]);
-            listCount += 1;
-            System.out.println(" Now you have " + listCount + " tasks in the list.\n" + LINES);
+    //Marks a specified task input as done
+    public static void markDone(String input, Task[] tasks, int taskCount) {
+        //isolate 'x' from 'done x', where x is a number
+        int index = Integer.parseInt(input.substring(5));
+        if (index > taskCount) {
+            System.out.println(LINES + NO_TASK_MESSAGE + LINES);
         }
         else {
-            System.out.println(LINES + "I don't quite understand :/\n" + LINES);
+            tasks[index - 1].markAsDone();
+            System.out.print(LINES + TASK_COMPLETE_MESSAGE + tasks[index-1] + "\n" + LINES);
         }
-        return listCount;
+    }
+    //Adds task to tasks
+    public static int addTask(Task[] tasks, String input, int taskCount) {
+        if (input.toUpperCase().startsWith("TODO ")) {
+            tasks[taskCount] = new Todo(input.substring(5));
+        }
+        else if (input.toUpperCase().startsWith("DEADLINE ")) {
+            int slashIndex = input.indexOf("/");
+            String task = input.substring(9, (slashIndex-1));
+            String dueDate = input.substring(slashIndex+1);
+            tasks[taskCount] = new Deadline(task, dueDate);
+        }
+        else {
+            int slashIndex = input.indexOf("/");
+            String task = input.substring(6, (slashIndex-1));
+            String timeRange = input.substring(slashIndex+1);
+            tasks[taskCount] = new Event(task, timeRange);
+        }
+        System.out.println(LINES + ADD_MESSAGE + tasks[taskCount]);
+        taskCount += 1;
+        System.out.println(" Now you have " + taskCount + " tasks in the list.\n" + LINES);
+        return taskCount;
     }
 
+    //Clears all tasks
+    public static void initJim() {
+        tasks = new Task[100];
+        taskCount = 0;
+    }
+
+    public static String getUserInput() {
+        System.out.print(" How can I help you: ");
+        return sc.nextLine();
+    }
+
+    public static void genshinHelper() {
+        int totalMats = 0;
+        System.out.print(LINES + " How many Top Tier: ");
+        String top = sc.nextLine();
+        totalMats += Integer.parseInt(top)*27;
+        System.out.print(" How many High Tier: ");
+        String high = sc.nextLine();
+        totalMats += Integer.parseInt(high)*9;
+        System.out.print(" How many Mid Tier: ");
+        String mid = sc.nextLine();
+        totalMats += Integer.parseInt(mid)*3;
+        System.out.print(" How many Low Tier: ");
+        String low = sc.nextLine();
+        totalMats += Integer.parseInt(low);
+        System.out.println(" Total low tier mats required: " + totalMats + "\n" + LINES);
+    }
+
+    //Executes next line commands
+    public static void commandExecute(String input) {
+        //adding to task list + update taskCount
+        if (input.toUpperCase().startsWith("TODO ") || input.toUpperCase().startsWith("DEADLINE ")
+                || input.toUpperCase().startsWith("EVENT ")) {
+            taskCount = addTask(tasks, input, taskCount);
+        }
+        //Mark input task as done
+        else if (input.toLowerCase().startsWith("done ")) {
+            markDone(input, tasks, taskCount);
+        }
+        //Show all tasks
+        else if (input.equalsIgnoreCase("list")) {
+            list(tasks);
+        }
+        //Call echo function
+        else if (input.equalsIgnoreCase("echo")) {
+            echo();
+        }
+        //Wish a happy birthday
+        else if (input.toUpperCase().contains("BIRTHDAY")) {
+            System.out.println(LINES + BIRTHDAY_MESSAGE + LINES);
+        }
+        else if (input.equalsIgnoreCase("help")) {
+            System.out.println(LINES + HELP_MESSAGE + LINES);
+        }
+        else if (input.equalsIgnoreCase("genshin")) {
+            genshinHelper();
+        }
+        //Exit
+        else if (input.equalsIgnoreCase("bye")) {
+            System.out.println(LINES + BYE_MESSAGE + LINES);
+            System.exit(0);
+        }
+        else {
+            System.out.println(LINES + INVALID_COMMAND + LINES);
+        }
+    }
     public static void main(String[] args) {
-        greeting(true);
-        Scanner sc = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int listCount = 0;
-        boolean isBye = false;
-        while (!isBye) {
-            String input = sc.nextLine();
-
-            //echo function
-            if (input.equalsIgnoreCase("echo")) {
-                System.out.println(LINES + " Echoing after you!\n" + LINES);
-                boolean echoState = true;
-                while (echoState) {
-                    String s = sc.nextLine();
-                    if (s.equalsIgnoreCase("quit")) {
-                        System.out.println(LINES + " That was annoying huh...\n" + LINES);
-                        echoState = false;
-                    } else {
-                        echo(s);
-                    }
-                }
-            }
-
-            //birthday
-            else if (input.toUpperCase().contains("BIRTHDAY")) {
-                System.out.println(LINES + " ^o^ Happy birthday to you! ^o^\n" + LINES);
-            }
-
-            //show list
-            else if (input.equalsIgnoreCase("list")) {
-                list(tasks);
-            }
-
-            //mark as done
-            else if (input.toLowerCase().startsWith("done ")) {
-                //isolate 'x' from 'done x', where x is a number
-                int index = Integer.parseInt(input.substring(5));
-                if (index > listCount) {
-                    System.out.println(LINES + "No such task! You're not THAT productive...\n" + LINES);
-                }
-                else {
-                    tasks[index - 1].markAsDone();
-                    System.out.print(LINES + "Nice! You're a real champ for finishing this: \n"
-                            + tasks[index-1] + "\n" + LINES);
-                }
-            }
-
-            //exit function
-            else if (input.equalsIgnoreCase("bye")) {
-                String byeMsg = LINES +
-                        " Bye! Remember, stay out of fire, suuuuuuper high level tactic yea?\n" +
-                        LINES;
-                System.out.println(byeMsg);
-                isBye = true;
-            }
-
-            //adding to list
-            else {
-                int newCount = addTask(tasks, input, listCount);
-                listCount = newCount;
-            }
+        greeting();
+        initJim();
+        while (true) {
+            String userCommand = getUserInput();
+            commandExecute(userCommand);
         }
     }
 }
