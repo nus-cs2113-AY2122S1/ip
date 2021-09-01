afe0fbced505e1aee762502315ebc9e35fa2ee61
Shi Zhansen <71874189+DuckWillow@users.noreply.github.com>
2021-09-02 01:15:15 +0800

    A-CodingStandard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,45 +10,45 @@ public class Duke {
         System.out.println(logo);
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        Task todo [] = new Task[100];
+        Task[] todo = new Task[100];
         int todo_index = 0;
-        while(true) {
+        while (true) {
             if (input.substring(0, 4).equals("done")) {
-                int i=Integer.parseInt(input.substring(5));
-                todo[i-1].markAsDone();
+                int i = Integer.parseInt(input.substring(5));
+                todo[i - 1].markAsDone();
                 System.out.println("____________________________________________________________");
                 System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(todo[i-1].toString());
+                System.out.println(todo[i - 1].toString());
                 System.out.println("____________________________________________________________");
             } else {
                 switch (input) {
-                    case "list":
-                        System.out.println("____________________________________________________________");
-                        for (int i = 0; i < todo_index; i++) {
-                            System.out.println(i + 1 + ". " + todo[i].toString());
-                        }
-                        System.out.println("____________________________________________________________");
-                        break;
-                    case "blah":
-                        logo = "____________________________________________________________\n"
-                                + "blah\n"
-                                + "____________________________________________________________\n";
-                        System.out.println(logo);
-                        break;
-                    case "bye":
-                        logo = "____________________________________________________________\n"
-                                + "Bye. Hope to see you again soon!\n"
-                                + "____________________________________________________________\n";
-                        System.out.println(logo);
-                        break;
-                    default:
-                        todo[todo_index] = new Task(input);
-                        System.out.println("____________________________________________________________");
-                        System.out.println("added: " + input);
-                        System.out.println("____________________________________________________________");
+                case "list":
+                    System.out.println("____________________________________________________________");
+                    for (int i = 0; i < todo_index; i++) {
+                        System.out.println(i + 1 + ". " + todo[i].toString());
+                    }
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "blah":
+                    logo = "____________________________________________________________\n"
+                            + "blah\n"
+                            + "____________________________________________________________\n";
+                    System.out.println(logo);
+                    break;
+                case "bye":
+                    logo = "____________________________________________________________\n"
+                            + "Bye. Hope to see you again soon!\n"
+                            + "____________________________________________________________\n";
+                    System.out.println(logo);
+                    break;
+                default:
+                    todo[todo_index] = new Task(input);
+                    System.out.println("____________________________________________________________");
+                    System.out.println("added: " + input);
+                    System.out.println("____________________________________________________________");
 
-                        todo_index += 1;
-                        break;
+                    todo_index += 1;
+                    break;
                 }
             }
             input = sc.nextLine();
