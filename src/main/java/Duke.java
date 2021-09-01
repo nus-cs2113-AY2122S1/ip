5bab4c25a37b0de57801df21fef32a8100e86fbe
Isabella-L <isabellalu01@gmail.com>
2021-09-02 03:20:54 +0800

    Add support for tracking:  todo, deadline, event

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
     public static class Task {
         protected String description;
         protected boolean isDone;
+        protected char taskType;
 
         public Task(String description) {
             this.description =  description;
@@ -25,11 +26,19 @@ public class Duke {
     static Task[] commands = new Task[100];
 
     public static void greet() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "       ___                             | '  \\\n" +
+                "   ___  \\ /  ___         ,'\\_           | .-. \\        /|\n" +
+                "   \\ /  | |,'__ \\  ,'\\_  |   \\          | | | |      ,' |_   /|\n" +
+                " _ | |  | |\\/  \\ \\ |   \\ | |\\_|    _    | |_| |   _ '-. .-',' |_   _\n" +
+                "// | |  | |____| | | |\\_|| |__    //    |     | ,'_`. | | '-. .-',' `. ,'\\_\n" +
+                "\\\\_| |_,' .-, _  | | |   | |\\ \\  //    .| |\\_/ | / \\ || |   | | / |\\  \\|   \\\n" +
+                " `-. .-'| |/ / | | | |   | | \\ \\//     |  |    | | | || |   | | | |_\\ || |\\_|\n" +
+                "   | |  | || \\_| | | |   /_\\  \\ /      | |`    | | | || |   | | | .---'| |\n" +
+                "   | |  | |\\___,_\\ /_\\ _      //       | |     | \\_/ || |   | | | |  /\\| |\n" +
+                "   /_\\  | |           //_____//       .||`  _   `._,' | |   | | \\ `-' /| |\n" +
+                "        /_\\           `------'        \\ |               `.\\  | |  `._,' /_\\\n" +
+                "                                       \\|                    `.\\\n" +
+                "  S. Lu - I solemnly swear that I am up to no good.\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n"
                 + "What can I do for you?\n");
@@ -39,9 +48,14 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static void add(String command, int item_num) {
-        commands[item_num] = new Task(command);
-        System.out.println("added: " + command);
+    //index is the position in string, index+1 is the bullet number
+    public static void add(String command, int index) {
+        commands[index] = new Task(command);
+        commands[index].taskType = getTask(command,index);
+        System.out.println("Got it. I've added this task: \n" +
+                "\t[" + commands[index].taskType + "]" + "[ ] " +
+                commands[index].description);
+        System.out.println("Now you have " + (index+1) + (index == 0?" task":" tasks") + " in the list.");
     }
 
     public static void list() {
@@ -49,8 +63,9 @@ public class Duke {
         for(int i = 0; commands[i] != null; i++) {
             int bullet_num = i+1;
             String statusIcon = commands[i].getStatusIcon();
+            char taskType = commands[i].taskType;
             System.out.print(bullet_num +". ");
-            System.out.print("["+ statusIcon +"] ");
+            System.out.print("[" + taskType +"]" + "["+ statusIcon +"] ");
             System.out.println(commands[i].description);
         }
     }
@@ -59,6 +74,7 @@ public class Duke {
         //command will be "done X"
         String cmd[] = command.split(" ", 2);
         String firstword = cmd[0];
+        //command.contains(word) is not used to obtain the number behind
         if (firstword.equals(doneTrigger)) {
             int index = Integer.parseInt(cmd[1])-1;
             commands[index].markAsDone();
@@ -67,6 +83,30 @@ public class Duke {
             return true;
         }
         return false;
+    }
+
+    public static char getTask(String command, int i) {
+        //.replaceAll method remove word or char from string
+        //to obtain strings after certain words, we can use
+        //StringUtil.substringAfter(String,word)
+        //however, to avoid external library download, i use a more tedious method here
+        if (command.contains("todo")) {
+            commands[i].description = command.replaceAll("todo","");
+            return 'T';
+        }
+        if (command.contains("deadline")) {
+            String time = command.substring(command.indexOf("/by")+4);
+            String task = command.substring(command.indexOf("deadline")+9,command.indexOf("/by"));
+            commands[i].description = task + "(by: " + time + ")";
+            return 'D';
+        }
+        if (command.contains("event")) {
+            String time = command.substring(command.indexOf("/at")+4);
+            String task = command.substring(command.indexOf("event")+6,command.indexOf("/at"));
+            commands[i].description = task + "(at: " + time + ")";
+            return 'E';
+        }
+        return '?';
     }
 
     public static void main(String[] args) {
