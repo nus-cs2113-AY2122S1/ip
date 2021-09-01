b5f44415b6e4f5d17e2de74c618b77980e1189f1
jiangweichen835 <e0425445@u.nus.edu>
2021-09-01 15:01:16 +0800

    Fix coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Duke {
+
+    public static final String LINE = "_______________________________________\n";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,48 +12,43 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
-        System.out.println("_______________________________________\n");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n" + LINE);
         inputTask();
     }
 
     public static void inputTask() {
         Scanner in = new Scanner(System.in);
         String inputTask = in.nextLine();
-        Task[] task = new Task[100];
+        Task[] tasks = new Task[100];
         int count = 0;
 
         while(!inputTask.equals("bye")) {
             if (inputTask.equals("list")) {
-                System.out.println("_______________________________________\n");
-                System.out.println("Here are the tasks in your list:\n");
+                System.out.println(LINE + "Here are the tasks in your list:\n");
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + task[i].getStatusIcon() + " " + task[i].getDescription());
+                    System.out.println((i + 1) + ". " + tasks[i].getStatusIcon() + " " + tasks[i].getDescription());
                 }
             } else if (inputTask.contains("done")) {
                 int sep = inputTask.indexOf(" ");
                 int number = Integer.parseInt(inputTask.substring(5));
-                MarkasDone(task, number);
+                MarkasDone(tasks, number);
             }
             else {
-                System.out.println("_______________________________________\n");
-                System.out.println("added: " + inputTask);
-                task[count] = new Task(inputTask);
+                System.out.println(LINE + "added: " + inputTask);
+                tasks[count] = new Task(inputTask);
                 count++;
             }
             inputTask = in.nextLine();
         }
         if (inputTask.equals("bye")) {
-            System.out.println("_______________________________________\n");
-            System.out.println("Bye. Hope to see you again soon!\n");
+            System.out.println(LINE + "Bye. Hope to see you again soon!\n");
         }
         in.close();
     }
 
     public static void MarkasDone(Task[] task, int number) {
         task[number-1].setDone();
-        System.out.println("_______________________________________\n");
-        System.out.println("Nice! I've marked this task as done:\n");
+        System.out.println(LINE + "Nice! I've marked this task as done:\n");
         System.out.println( task[number-1].getStatusIcon() + " " + task[number-1].getDescription());
     }
 }
