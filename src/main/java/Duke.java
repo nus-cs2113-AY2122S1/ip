58398a497eb8665301e37d4c7f9e270216489c91
wingho2 <e0543725@u.nus.edu>
2021-09-02 03:19:38 +0800

    Add a echo functions that repeat commands entered by the user and exit on bye
    Import a Scanner class and add a to that echo input.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,11 +7,24 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("H____________________________________________________________\n" +
+        System.out.println( logo +
+                "____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________" + logo);
+                "____________________________________________________________\n");
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            String echo = in.nextLine();
+            String echoLower = echo.toLowerCase();
+            if (echoLower.equals("bye")) {
+                System.out.println(
+                        "____________________________________________________________\n" +
+                        "Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________");
+                break;
+            }else {
+                System.out.println("____________________________________________________________\n" + echo + System.lineSeparator() + "____________________________________________________________");
+            }
+        }
     }
 }
