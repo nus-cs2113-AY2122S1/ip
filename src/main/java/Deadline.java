b1c24369f0e31cfb9095d419b3cc0c7a3b38944f
21varun12 <21varun12@gmail.com>
2021-09-02 22:37:51 +0800

    Add deadline

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,14 @@
+public class Deadline extends Task{
+    private static String SYMBOL = "D";
+    private String dueDate;
+
+    public Deadline(String name, String dueDate) {
+        super(name);
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + SYMBOL + "]" + super.toString() + " (by:" + dueDate + ")";
+    }
+}
