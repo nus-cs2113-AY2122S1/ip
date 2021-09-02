431e205a6d526a294070a82959ce876559a33713
VishalJeyaram <vishalsonic2000@gmail.com>
2021-09-02 16:55:49 +0800

    Coding quality and standard improved

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,22 +27,22 @@ public class Duke {
             byeFlag = 1;
         } else if (line.equals("list")) {
             if (positionCheck == 0) {
-                sayError("empty");
+                sayEmpty();
             } else {
                 printList();
             }
         } else if (line.equals("done")) {
-            sayError("done not specified");
+            sayNotSpecified();
         } else if (input[0].equals("done")) {
             if ((Integer.parseInt(input[1]) > positionCheck ) || (Integer.parseInt(input[1]) <= 0)) {
-                sayError("done number wrong");
+                sayInvalidNumber();
             } else if (positionCheck<=0) {
-                sayError("empty");
+                sayEmpty();
             } else {
                 markDone(Integer.parseInt(input[1])-1);
             }
         } else if (positionCheck >= 100) {
-            sayError("exceeded");
+            sayExceededCapacity();
         } else {
             checkTypeOfTask(line);
         }
@@ -53,16 +53,84 @@ public class Duke {
         System.out.println("____________________________________________________________\n");
     }
 
+    public static void sayEmpty() {
+        System.out.println("There is no data in your list master!");
+    }
 
-    public static void sayError(String error) {
-        if (error.equals("exceeded")) {
-            System.out.println("Oh dear me! We have exceeded my system's maximum capacity!");
-        } else if (error.equals("empty")) {
-            System.out.println("There is no data in your list master!");
-        } else if (error.equals("done not specified")){
-            System.out.println("Oh no master, I am not quite sure which task you would like me to mark as done");
-        } else if (error.equals("done number wrong")) {
-            System.out.println("Please type in a valid number master! Type \"list\" to check the index number of your list data");
+    public static void sayExceededCapacity() {
+        System.out.println("Oh dear me! We have exceeded my system's maximum capacity!");
+    }
+
+    public static void sayNotSpecified() {
+        System.out.println("Oh no master, I am not quite sure which task you would like me to mark as done");
+    }
+
+    public static void sayInvalidNumber() {
+        System.out.println("Please type in a valid number master! Type \"list\" to check the index number of your list data");
+    }
+
+    public static void addDeadline(String[] input, int length) {
+        String description;
+        String by;
+        for (int i = 1 ; i < length ; i++) {
+            if ((input[i].equals("/by")) && (i != 1) && (i != (length-1))) {
+                description = input[1];
+                by = input[i+1];
+                for (int j = 2 ; j < i ; j++) {
+                    description += (" " + input[j]);
+                }
+                for (int k = i+2 ; k < length ; k++) {
+                    by += (" " + input[k]);
+                }
+                commands[positionCheck] = new Deadline(description,by);
+                System.out.println("Added to Galactic database:" );
+                System.out.println(commands[positionCheck]);
+                positionCheck += 1;
+                return;
+            }
+        }
+        System.out.println("Sorry Master! I don't think you have properly keyed in the parameters. Please enter the task, followed by \"/by\",\n" +
+                "followed by the due date to specify the deadline Master!");
+    }
+
+    public static void addEvent(String[] input, int length) {
+        String description;
+        String at;
+        for (int i = 1 ; i < length ; i++) {
+            if ((input[i].equals("/at")) && (i != 1) && (i != (length-1))) {
+                description = input[1];
+                at = input[i+1];
+                for (int j = 2 ; j < i ; j++) {
+                    description += (" " + input[j]);
+                }
+                for (int k = i+2 ; k < length ; k++) {
+                    at += (" " + input[k]);
+                }
+                commands[positionCheck] = new Event(description,at);
+                System.out.println("Added to Galactic database:" );
+                System.out.println(commands[positionCheck]);
+                positionCheck += 1;
+                return;
+            }
+        }
+        System.out.println("Sorry Master! I don't think you have properly keyed in the parameters. \n" +
+                "Please enter the event, followed by \"/at\", followed by the event duration to specify \n" +
+                "the timing of the event Master!");
+    }
+
+    public static void addTodo (String[] input, int length) {
+        if (length == 1) {
+            System.out.println("Sorry Master! I don't think you have properly keyed in the parameters.\n" +
+                    " Please enter the task you wish to add to your Todo list Master!");
+        } else {
+            String description = input[1];
+            for (int i = 2 ; i < length ; i++) {
+                description += (" " + input[i]);
+            }
+            commands[positionCheck] = new Todo(description);
+            System.out.println("Added to Galactic database:" );
+            System.out.println(commands[positionCheck]);
+            positionCheck += 1;
         }
     }
 
@@ -70,84 +138,21 @@ public class Duke {
         String[] input = line.split(" ");
         int length = input.length;
         if (input[0].toLowerCase().equals("deadline")) {
-            String description;
-            String by;
-            for (int i = 1 ; i < length ; i++) {
-                if ((input[i].equals("/by")) && (i != 1) && (i != (length-1))) {
-                    description = input[1];
-                    by = input[i+1];
-                    for (int j = 2 ; j < i ; j++) {
-                        description += (" " + input[j]);
-                    }
-                    for (int k = i+2 ; k < length ; k++) {
-                        by += (" " + input[k]);
-                    }
-                    commands[positionCheck] = new Deadline(description,by);
-                    System.out.println("Added to Galactic database:" );
-                    System.out.println(commands[positionCheck]);
-                    positionCheck += 1;
-                    return;
-                }
-            }
-            System.out.println("Sorry Master! I don't think you have properly keyed in the parameters. Please enter the task, followed by \"/by\",\n" +
-                    "followed by the due date to specify the deadline Master!");
-            return;
+            addDeadline(input,length);
         } else if (input[0].toLowerCase().equals("event")) {
-            String description;
-            String at;
-            for (int i = 1 ; i < length ; i++) {
-                if ((input[i].equals("/at")) && (i != 1) && (i != (length-1))) {
-                    description = input[1];
-                    at = input[i+1];
-                    for (int j = 2 ; j < i ; j++) {
-                        description += (" " + input[j]);
-                    }
-                    for (int k = i+2 ; k < length ; k++) {
-                        at += (" " + input[k]);
-                    }
-                    commands[positionCheck] = new Event(description,at);
-                    System.out.println("Added to Galactic database:" );
-                    System.out.println(commands[positionCheck]);
-                    positionCheck += 1;
-                    return;
-                }
-            }
-            System.out.println("Sorry Master! I don't think you have properly keyed in the parameters. \n" +
-                    "Please enter the event, followed by \"/at\", followed by the event duration to specify \n" +
-                    "the timing of the event Master!");
-            return;
+            addEvent(input,length);
         } else if (input[0].toLowerCase().equals("todo")) {
-            if (length == 1) {
-                System.out.println("Sorry Master! I don't think you have properly keyed in the parameters.\n" +
-                        " Please enter the task you wish to add to your Todo list Master!");
-            } else {
-                String description = input[1];
-                for (int i = 2 ; i < length ; i++) {
-                    description += (" " + input[i]);
-                }
-                commands[positionCheck] = new Todo(description);
-                System.out.println("Added to Galactic database:" );
-                System.out.println(commands[positionCheck]);
-                positionCheck += 1;
-            }
+            addTodo(input,length);
         } else {
             System.out.println("Sorry Master! Please specify the type of task that you wish to add as well!");
         }
     }
-
-
 
     private static void printList() {
         System.out.println("Accessing archives...");
         for (int i = 0; i < positionCheck; i++) {
             System.out.println((i+1) + ". " + commands[i]);
         }
-    }
-
-    private static void addToList(String line) {
-        commands[positionCheck] = new Task(line);
-        positionCheck += 1;
-        System.out.println("Added to Galactic database: " + line);
     }
 
     private static void markDone(int doneTaskNumber) {
