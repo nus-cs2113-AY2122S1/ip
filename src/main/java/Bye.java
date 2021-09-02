18613b76cf504a5da99c500f124a7540aab614c8
peng-217 <pengfei20020217@163.com>
2021-09-02 21:24:43 +0800

    delete prevoius useless classes

--- src/main/java/Bye.java
+++
@@ -1,9 +0,0 @@
-public class Bye {
-    private static String byeCommand = Add.getAddString();
-    public static void bye(){
-        if(byeCommand.equals("bye")){
-            System.out.println();
-            System.out.println("Bye. Hope to see you again soon!");
-        }
-    }
-}
--- src/main/java/Greet.java
+++
@@ -1,7 +0,0 @@
-public class Greet {
-    public static void greet(){
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("\n");
-    }
-}
