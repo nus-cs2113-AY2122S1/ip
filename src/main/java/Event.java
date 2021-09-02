82d95e3de64c3ee8848e095e4eb766f4a7870456
21varun12 <21varun12@gmail.com>
2021-09-02 22:42:08 +0800

    Add event

---
+++ src/main/java/Event.java
@@ -0,0 +1,14 @@
+public class Event extends Task{
+    private static String SYMBOL = "E";
+    private String timeslot;
+
+    public Event(String name, String timeslot) {
+        super(name);
+        this.timeslot = timeslot;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + SYMBOL + "]" + super.toString() + " (at:" + timeslot + ")";
+    }
+}
