ebb84bebc737c123595ebde5163f0d0a08b6766f
EdwardZYWang <wangzy.edward@u.nus.edu>
2021-09-02 20:09:08 +0800

    Add new methods printList, testTaskDone, taskDone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,54 +20,30 @@ public class Duke {
         while (!(input.equals("bye"))) {
 
             if (input.equals("list")) {
-                System.out.println("    Here are the tasks in your list:");
-
-                for (int outputCount = 0; outputCount < inputCount; outputCount++) {
-                    statusOfTask = tasks[outputCount].getStatusIcon();
-                    System.out.println("    " + (outputCount + 1) + "." + tasks[outputCount]);
-                }
-                input = in.nextLine();
+                input = printList(in);
 
             } else if (input.contains("done")) {
-                inputSplitter = input.split(" ");
-                taskNumber = Integer.parseInt(inputSplitter[1]);
-                taskNumber = taskNumber - 1;
-
-                if (((taskNumber + 1) > 0) && ((taskNumber + 1) <= inputCount)) { //check for invalid inputs
-                    System.out.println("    Nice! I've marked this task as done:" + System.lineSeparator());
-                    System.out.println("    " + tasks[taskNumber] + " has been updated to -->");
-
-                    tasks[taskNumber].markAsDone(); //mark x in [ ]
-                    statusOfTask = tasks[taskNumber].getStatusIcon();
-
-                    System.out.println("    " + tasks[taskNumber]);
-                    input = in.nextLine();
-
-                } else {
-                    System.out.println("    Invalid input! Please check the list again!");
-                    input = in.nextLine();
-                }
+                input = testTaskDone(input, in);
 
             } else if (input.contains("todo")) {
                 Task whatToDo = toDoMethod(input);
-
                 System.out.println("    Got it. I've added this task:\n" + "    " + whatToDo + "\n"
                         + "    Now you have " + inputCount + " tasks in the list.");
-                input = in.nextLine();
 
+                input = in.nextLine();
 
             } else if (input.contains("deadline")) {
                 Task whatDeadline = deadlineMethod(input);
-
                 System.out.println("    Got it. I've added this task:\n" + "    " + whatDeadline + "\n"
                         + "    Now you have " + inputCount + " tasks in the list.");
+
                 input = in.nextLine();
 
             } else if (input.contains("event")) {
                 Task whatEvent = eventMethod(input);
-
                 System.out.println("    Got it. I've added this task:\n" + "    " + whatEvent + "\n"
                         + "    Now you have " + inputCount + " tasks in the list.");
+
                 input = in.nextLine();
 
             } else { //adding in general orders
@@ -77,10 +53,55 @@ public class Duke {
                 inputCount++;
 
                 input = in.nextLine();
+
             }
         }
         System.out.println("    Bye! Hope to see you again soon!");
 
+    }
+
+    private static String printList(Scanner in) {
+        String statusOfTask;
+        String input;
+        System.out.println("    Here are the tasks in your list:");
+
+        for (int outputCount = 0; outputCount < inputCount; outputCount++) {
+            statusOfTask = tasks[outputCount].getStatusIcon();
+            System.out.println("    " + (outputCount + 1) + "." + tasks[outputCount]);
+        }
+        input = in.nextLine();
+        return input;
+    }
+
+    private static String testTaskDone(String input, Scanner in) {
+        int taskNumber;
+        String[] inputSplitter;
+        inputSplitter = input.split(" ");
+        taskNumber = Integer.parseInt(inputSplitter[1]);
+        taskNumber = taskNumber - 1;
+
+        if (((taskNumber + 1) > 0) && ((taskNumber + 1) <= inputCount)) { //check for invalid inputs
+            input = taskDone(in, tasks[taskNumber]);
+
+        } else {
+            System.out.println("    Invalid input! Please check the list again!");
+            input = in.nextLine();
+        }
+        return input;
+    }
+
+    private static String taskDone(Scanner in, Task task) {
+        String statusOfTask;
+        String input;
+        System.out.println("    Nice! I've marked this task as done:" + System.lineSeparator());
+        System.out.println("    " + task + " has been updated to -->");
+
+        task.markAsDone(); //mark x in [ ]
+        statusOfTask = task.getStatusIcon();
+
+        System.out.println("    " + task);
+        input = in.nextLine();
+        return input;
     }
 
     public static Task toDoMethod(String input) {
@@ -94,29 +115,28 @@ public class Duke {
     }
 
     public static Task deadlineMethod(String input) {
-        String[] deadline_splitter = input.substring(9).split(" /by ");
-        String deadlineDescription = deadline_splitter[0]; //before /by
-        String deadlineBy = deadline_splitter[1]; // after /by
+        String[] deadlineSplitter = input.substring(9).split(" /by ");
+        String deadlineDescription = deadlineSplitter[0]; //before /by
+        String deadlineBy = deadlineSplitter[1]; // after /by
 
         Task description = new Deadline(deadlineDescription, deadlineBy);
         tasks[inputCount] = description;
         inputCount++;
 
         return description;
-
     }
 
     public static Task eventMethod(String input) {
-        String[] event_splitter = input.substring(6).split(" /at ");
-        String eventDescription = event_splitter[0]; //before /at
-        String evenAt = event_splitter[1]; // after /at
+        String[] eventSplitter = input.substring(6).split(" /at ");
+        String eventDescription = eventSplitter[0]; //before /at
+        String eventAt = eventSplitter[1]; // after /at
 
-        Task description = new Event(eventDescription, evenAt);
+        Task description = new Event(eventDescription, eventAt);
         tasks[inputCount] = description;
         inputCount++;
 
         return description;
-
     }
+
 }
 
