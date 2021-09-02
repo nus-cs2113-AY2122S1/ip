6141721ad6757213107c95bc3265c8ff5024a5dc
Mist0919 <XUEY0013@e.ntu.edu.sg>
2021-09-02 11:08:16 +0800

    commit

--- src/main/java/list.java
+++ src/main/java/list.java
@@ -1,43 +1,41 @@
-public class list {
-    private static String[] l = new String[100];
+public class List {
+    private static String[] currentList = new String[100];
     public static int listSize = 0;
     private static int[] isDone = new int[100];
 
     public static void addToList(String input) {
-        l[listSize] =  input;
+        currentList[listSize] =  input;
         listSize++;
-        System.out.println("____________________________________________________________\n"+
-                "Added: "+input + "\n" +
-                "____________________________________________________________\n");
+        System.out.println("____________________________________________________________\n"
+                + "Added: " + input + "\n"
+                + "____________________________________________________________\n");
     }
 
-    public static void printItems(){
-        if (listSize == 0){
+    public static void printItems() {
+        if (listSize == 0) {
             System.out.println("The list is empty.");
-        }
-        else{
-            for (int i = 1; i<=listSize; i++){
+        } else {
+            for (int i = 1; i <= listSize; i++) {
                 System.out.print(i + ". " );
                 System.out.print("[");
-                if (isDone[i-1] == 1){
+                if (isDone[i-1] == 1) {
                     System.out.print("X");
-                }
-                else{
+                } else {
                     System.out.print(" ");
                 }
-                System.out.print("] " + l[i-1] + "\n");
+                System.out.print("] " + currentList[i-1] + "\n");
             }
         }
     }
 
-    public static void mark(int number){
+    public static void mark(int number) {
         isDone[number-1] = 1;
     }
 
-    public static void markedItems(){
-        for (int i=0;i<isDone.length;i++){
+    public static void markedItems() {
+        for (int i = 0; i < isDone.length; i++) {
             if (isDone[i] == 1) {
-                System.out.println("[X] " + l[i]);
+                System.out.println("[X] " + currentList[i]);
             }
         }
     }
