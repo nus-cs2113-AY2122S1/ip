a58b496c451660c1f683bdc46af1d9883666b008
dyahnafisah <59493341+dyahnafisah@users.noreply.github.com>
2021-09-05 01:20:14 +0800

    add methods to create todos, deadlines, and events

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,9 @@ public class Duke {
     private static final String COMMAND_EXIT_WORD = "bye";
     private static final String COMMAND_LIST_WORD = "list";
     private static final String COMMAND_DONE_WORD = "done";
+    private static final String COMMAND_TODO_WORD = "todo";
+    private static final String COMMAND_DEADLINE_WORD = "deadline";
+    private static final String COMMAND_EVENT_WORD = "event";
     private static final int MAX_TASKS_NUMBER = 100;
     private static final Scanner INPUT_COMMAND = new Scanner(System.in);
 
@@ -12,55 +15,73 @@ public class Duke {
         Task[] tasksList = new Task[MAX_TASKS_NUMBER];
         int taskCount = 0;
         executeCommand(tasksList, taskCount);
+        exitProgram();
     }
 
     public static void executeCommand(Task[] tasksList, int taskCount) {
         String inputLine;
-        int listDoneIndex = 0;
         do {
             inputLine = INPUT_COMMAND.nextLine();
-            if (inputLine.startsWith("done")) {
-                listDoneIndex = Integer.parseInt(inputLine.split(" ")[1]) - 1;
-                inputLine = COMMAND_DONE_WORD;
-            }
-            switch (inputLine) {
-            case COMMAND_LIST_WORD:
+            if (inputLine.startsWith(COMMAND_TODO_WORD)) {
+                taskCount = addToDo(tasksList, taskCount, inputLine);
+            } else if (inputLine.startsWith(COMMAND_DEADLINE_WORD)) {
+                taskCount = addDeadline(tasksList, taskCount, inputLine);
+            } else if (inputLine.startsWith(COMMAND_EVENT_WORD)) {
+                taskCount = addEvent(tasksList, taskCount, inputLine);
+            } else if (inputLine.equals(COMMAND_LIST_WORD)) {
                 printTasks(tasksList, taskCount);
-                break;
-            case COMMAND_EXIT_WORD:
-                exitProgram();
-                break;
-            case COMMAND_DONE_WORD:
-                markAsDone(tasksList[listDoneIndex]);
-                break;
-            default:
-                taskCount = addTask(tasksList, taskCount, inputLine);
+            } else if (inputLine.startsWith(COMMAND_DONE_WORD)) {
+                markAsDone(tasksList, inputLine);
+            } else if (inputLine.equals(COMMAND_EXIT_WORD)) {
+                continue;
+            } else {
+                System.out.println("Sorry, command is not recognized. Please input it correctly.");
             }
         } while (!inputLine.equals(COMMAND_EXIT_WORD));
     }
 
-    public static void markAsDone(Task task) {
-        task.setDone(true);
+    private static void markAsDone(Task[] tasksList, String inputLine) {
+        int taskCompletedIndex = Integer.parseInt(inputLine.split("done ")[1])-1;
+        tasksList[taskCompletedIndex].setDone(true);
         System.out.print("  Congratulations! You have completed the task:");
-        System.out.println(" " + task.getTaskName());
+        System.out.println(" " + tasksList[taskCompletedIndex]);
+    }
+
+    private static int addEvent(Task[] tasksList, int taskCount, String inputLine) {
+        String description = inputLine.split(" /at ")[0];
+        String date = inputLine.split(" /at ")[1];
+        tasksList[taskCount] = new Event(description, date);
+        System.out.println("  Ok, I've added this task:");
+        System.out.println("  " + tasksList[taskCount]);
+        taskCount++;
+        System.out.printf("  Now you have %d tasks in the list. Anything else?\n", taskCount);
+        return taskCount;
+    }
+
+    public static int addDeadline(Task[] tasksList, int taskCount, String inputLine) {
+        String description = inputLine.split(" /by ")[0];
+        String date = inputLine.split(" /by ")[1];
+        tasksList[taskCount] = new Deadline(description, date);
+        System.out.println("  Ok, I've added this task:");
+        System.out.println("  " + tasksList[taskCount]);
+        taskCount++;
+        System.out.printf("  Now you have %d tasks in the list. Anything else?\n", taskCount);
+        return taskCount;
+    }
+
+    public static int addToDo(Task[] tasksList, int taskCount, String inputLine) {
+        tasksList[taskCount] = new ToDo(inputLine);
+        System.out.println("  Ok, I've added this task:");
+        System.out.println("  " + tasksList[taskCount]);
+        taskCount++;
+        System.out.printf("  Now you have %d tasks in the list. Anything else?\n", taskCount);
+        return taskCount;
     }
 
     public static void printTasks(Task[] tasksList, int taskCount) {
         for (int i = 1; i < taskCount + 1; i++) {
-            if (tasksList[i - 1].isDone()) {
-                System.out.println("  " + i + ". " + "[X] " + tasksList[i - 1].getTaskName());
-            } else {
-                System.out.println("  " + i + ". " + "[ ] " + tasksList[i - 1].getTaskName());
-            }
+            System.out.println("  " + i + "." + tasksList[i - 1]);
         }
-    }
-
-    public static int addTask(Task[] tasksList, int taskCount, String inputLine) {
-        tasksList[taskCount] = new Task(inputLine);
-        taskCount++;
-        System.out.println("  Task added: " + inputLine);
-        System.out.println("  Anything else?");
-        return taskCount;
     }
 
     public static void exitProgram() {
