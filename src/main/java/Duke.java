f7c3bf4aef9ee211eaf0596f26caad6b6a08af59
Jiexiong <jiexiong123@hotmail.com>
2021-09-04 23:25:32 +0800

    change duke structure implement simpler task selection

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,42 +5,40 @@ public class Duke {
     private static int taskIndex = 0;
 
     public static void main(String[] args) {
-
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
         greet();
+        instructions();
         chooseTask();
     }
 
+
     private static void chooseTask() {
-        printLine();
-        System.out.println("Choose a task to perform:");
-        System.out.println("1. Echo");
-        System.out.println("2. Add task");
-        System.out.println("3. Display list");
-        System.out.println("4. Mark task complete");
-        printLine();
-        String line;
+        String line, taskType,  taskDescription = null;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
 
-        switch (line) {
-        case "1":
-            echo();
+        try {
+            int typePos = line.indexOf(" ");
+            taskType = line.substring(0, typePos);
+            taskDescription = line.substring(typePos).trim();
+        } catch (StringIndexOutOfBoundsException e) {
+            taskType = line;
+        }
+
+        switch (taskType) {
+        case "todo":
+            addTask("todo", taskDescription);
             break;
-        case "2":
-            addTask();
+        case "deadline":
+            addTask("deadline", taskDescription);
             break;
-        case "3":
+        case "event":
+            addTask("event", taskDescription);
+            break;
+        case "list":
             displayList();
             break;
-        case "4":
-            markTaskComplete();
+        case "done":
+            markTaskComplete(taskDescription);
             break;
         case "bye":
             bye();
@@ -52,18 +50,32 @@ public class Duke {
     }
 
     private static void greet() {
-        printLine();
+        printDividerLine();
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
     }
 
-    private static void bye() {
-        printLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printLine();
+    public static void instructions() {
+        System.out.println("modus tolens");
+        System.out.println("1. Echo");
+        System.out.println("2. Add task");
+        System.out.println("3. Display list");
+        System.out.println("4. Mark task complete");
     }
 
-    private static void printLine() {
+    private static void bye() {
+        printDividerLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printDividerLine();
+    }
+
+    private static void printDividerLine() {
         for (int i = 0; i < 30; i++) {
             System.out.print("-");
         }
@@ -74,10 +86,10 @@ public class Duke {
         String line;
         Scanner in = new Scanner(System.in);
 
-        printLine();
+        printDividerLine();
         System.out.println("What do you want me to repeat?");
         System.out.println("*Type 'exit echo' to leave*");
-        printLine();
+        printDividerLine();
 
         line = in.nextLine();
         if (line.equals("exit echo")) {
@@ -87,19 +99,7 @@ public class Duke {
         echo();
     }
 
-    private static void addTask() {
-        String line;
-        Scanner in = new Scanner(System.in);
-
-        printLine();
-        System.out.println("What task do you want to add?");
-        printLine();
-
-        line = in.nextLine();
-        //Split the input into type and description
-        int typePos = line.indexOf(" ");
-        String taskType = line.substring(0, typePos);
-        String taskDescription = line.substring(typePos).trim();
+    private static void addTask(String taskType, String taskDescription) {
 
         switch (taskType) {
         case "todo":
@@ -114,44 +114,44 @@ public class Duke {
         }
         taskIndex++;
 
-        printLine();
+        printDividerLine();
         System.out.println("I have added this task:");
-        System.out.println("[" + taskList[taskIndex-1].getType() +"][" + taskList[taskIndex-1].getStatusIcon() + "] " + taskList[taskIndex-1].getDescription());
+        System.out.println("[" + taskList[taskIndex - 1].getType() + "][" + taskList[taskIndex - 1].getStatusIcon() + "] " + taskList[taskIndex - 1].getDescription());
         System.out.println("You have " + taskIndex + " task(s) in the list.");
-        printLine();
+        printDividerLine();
     }
 
     private static void displayList() {
-        printLine();
+        printDividerLine();
         if (taskIndex == 0) {
             System.out.println("to-do list is empty! add something");
             return;
         }
         System.out.println("The current to-do list is as follows:");
         for (int i = 0; i < taskIndex; i++) {
-            System.out.println(i + 1 + ". [" + taskList[i].getType() +"][" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
+            System.out.println(i + 1 + ". [" + taskList[i].getType() + "][" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
         }
-        printLine();
+        printDividerLine();
     }
 
-    private static void markTaskComplete() {
-        printLine();
-        System.out.println("Which Task is complete?");
-        printLine();
+    private static void markTaskComplete(String taskDescription) {
+        int taskNumber;
 
-        String line;
-        Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        int taskNumber = Integer.parseInt(line) - 1;
-
-        if (taskNumber > taskIndex) {
+        try {
+            taskNumber = Integer.parseInt(taskDescription);
+            if (taskNumber > taskIndex) {
+                System.out.println("Error! This task does not exist!");
+                return;
+            }
+            taskNumber--;
+            taskList[taskNumber].markAsDone();
+            printDividerLine();
+            System.out.println("I have marked it as completed!");
+            System.out.println(taskNumber + 1 + ". [" + taskList[taskNumber].getType() + "][" + taskList[taskNumber].getStatusIcon() + "] " + taskList[taskNumber].getDescription());
+            printDividerLine();
+        } catch (NumberFormatException e) {
             System.out.println("Error! This task does not exist!");
-            return;
         }
-        taskList[taskNumber].markAsDone();
-        printLine();
-        System.out.println("I have marked it as completed!");
-        System.out.println(taskNumber + 1 + ". [" + taskList[taskNumber].getType() +"][" + taskList[taskNumber].getStatusIcon() + "] " + taskList[taskNumber].getDescription());
-        printLine();
+
     }
 }
