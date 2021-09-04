87fd327bff176819b25872f13904c7952981daba
DESKTOP-TO5H2A5\Ishaa <ishaan.vyas@hotmail.com>
2021-09-04 17:29:06 +0800

    Added automation testing for Level 5 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -58,33 +58,7 @@ public class Duke {
             String inputCommand = SCANNER.nextLine().trim();
             String command = getFirstWord(inputCommand);
             try {
-                switch (command) {
-                case "list":
-                    TASK_MANAGER.printTasks();
-                    break;
-                case "done":
-                    int indexOfTaskDone = getIntegerFromCommand(inputCommand);
-                    TASK_MANAGER.setTaskAsDone(indexOfTaskDone);
-                    break;
-                case "bye":
-                    conversationIsOver = true;
-                    break;
-                case "deadline":
-                    String deadlineInput = removeFirstWordInSentence(inputCommand, 8);
-                    TASK_MANAGER.addDeadline(deadlineInput);
-                    break;
-                case "todo":
-                    String todoInput = removeFirstWordInSentence(inputCommand, 4);
-                    TASK_MANAGER.addTodo(todoInput);
-                    break;
-                case "event":
-                    String eventInput = removeFirstWordInSentence(inputCommand, 5);
-                    TASK_MANAGER.addEvent(eventInput);
-                    break;
-                default:
-                    printMessage(HELP_MESSAGE);
-                    break;
-                }
+                conversationIsOver = processCommand(conversationIsOver, inputCommand, command);
             } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                 // for now these exceptions only occur when a number is passed after done
                 // so we can assume the user made an error using the 'done' command
@@ -104,6 +78,43 @@ public class Duke {
                 printMessage("This task is already completed");
             }
         }
+    }
+
+    private static boolean processCommand(boolean conversationIsOver, String inputCommand, String command)
+            throws DukeInvalidTaskIndex,
+            DukeTaskAlreadyCompletedException,
+            DukeEmptyDescriptionException,
+            DukeExceedMaxTaskException,
+            DukeEmptyTimeException,
+            DukeMissingKeywordException {
+        switch (command) {
+        case "list":
+            TASK_MANAGER.printTasks();
+            break;
+        case "done":
+            int indexOfTaskDone = getIntegerFromCommand(inputCommand);
+            TASK_MANAGER.setTaskAsDone(indexOfTaskDone);
+            break;
+        case "bye":
+            conversationIsOver = true;
+            break;
+        case "deadline":
+            String deadlineInput = removeFirstWordInSentence(inputCommand, 8);
+            TASK_MANAGER.addDeadline(deadlineInput);
+            break;
+        case "todo":
+            String todoInput = removeFirstWordInSentence(inputCommand, 4);
+            TASK_MANAGER.addTodo(todoInput);
+            break;
+        case "event":
+            String eventInput = removeFirstWordInSentence(inputCommand, 5);
+            TASK_MANAGER.addEvent(eventInput);
+            break;
+        default:
+            printMessage(HELP_MESSAGE);
+            break;
+        }
+        return conversationIsOver;
     }
 
     private static String removeFirstWordInSentence(String inputCommand, int i) {
