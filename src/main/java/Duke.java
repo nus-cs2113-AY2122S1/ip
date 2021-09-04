babe1617a41db691185066469e73346520866582
Mist0919 <XUEY0013@e.ntu.edu.sg>
2021-09-04 11:19:00 +0800

    level4 A codeQuality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.lang.String;
 
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,43 +15,54 @@ public class Duke {
                 + "Hello! I'm Duke\n"
                 + "What can I do for you?\n"
                 + "____________________________________________________________\n");
+
+        list.initializing();
         Scanner input = new Scanner(System.in);
         while (true) {
             String str = input.nextLine();
+            //input = NULL
+            if (str.trim().equals("")) {
+                continue;
+            }
+            //input = bye
             if (str.equals("bye")) {
                 System.out.println("____________________________________________________________\n"
                         + "Bye. Hope to see you again soon!\n"
                         + "____________________________________________________________\n");
                 break;
             }
+            //input = list
             else if (str.equals("list")) {
-                System.out.println("____________________________________________________________\n"
-                        + "Here are the tasks in your list: ");
-                List.printItems();
+                System.out.println("____________________________________________________________\n" +
+                        "Here are the tasks in your list: ");
+                list.printItems();
                 System.out.println("____________________________________________________________");
             }
-            else if (str.contains("Done ") == true) {
+            //input = Done xx
+            else if (str.contains("Done ") == true){
                 String nums = "";
-                for (int i = 0; i < str.length(); i++) {
+                for (int i=0;i<str.length();i++){
                     char c = str.charAt(i);
-                    if (Character.isDigit(c) == true) {
+                    if (Character.isDigit(c) == true){
                         nums += c;
                     }
                 }
                 int index = Integer.parseInt(nums);
-                if (index > List.listSize) {
-                    System.out.println("____________________________________________________________\n"
-                            + "The index is out of range.\n"
-                            + "____________________________________________________________\n");
-                } else {
-                    List.mark(index);
-                    System.out.println("____________________________________________________________\n"
-                            + "Nice! I've marked this task as done: ");
-                    List.markedItems();
+                if (index > list.listSize){
+                    System.out.println("____________________________________________________________\n" +
+                            "The index is out of range.\n" +
+                            "____________________________________________________________\n");
+                }
+                else{
+                    list.mark(index);
+                    System.out.println("____________________________________________________________\n" +
+                            "Nice! I've marked this task as done: ");
+                    list.printMarkedItems();
                     System.out.println("____________________________________________________________");
                 }
             } else {
-                List.addToList(str);
+                //input = task
+                list.addToList(str);
             }
         }
     }
