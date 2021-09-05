662f0335ad0e97fe80b6a42601703fe51908d66e
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-09-05 12:33:24 +0800

    Add corresponding exception handling messages in MessageManager

--- src/main/java/alfred/manager/MessageManager.java
+++ src/main/java/alfred/manager/MessageManager.java
@@ -13,54 +13,69 @@ public abstract class MessageManager {
             " * /_/   \\_\\_|_| |_|  \\___|\\__,_| *\n" +
             " **********************************\n";
 
-    public static void invalidCommandMessage() {
-        System.out.println(
-                LINE +
-                " Perhaps you could rephrase that in a way us civilians could comprehend.\n" +
-                LINE
-        );
-    };
-
-    public static void emptyDescriptionMessage() {
-        System.out.println(
-                LINE +
-                "Master Wayne, if you do not specify your task, I'm afraid I cannot note it down.\n" +
-                LINE
-        );
+    private static void printMessageTemplate(String s) {
+        System.out.println(LINE + s + LINE);
     }
 
     public static void initMessage() {
-        System.out.println(
-                LOGO + "\n" + LINE +
-                " Welcome back, Master Wayne.\n" +
-                " How may I be of service to you?\n" +
-                LINE
-        );
+        String messageString = " Welcome back, Master Wayne.\n" + " How may I be of service to you?\n";
+        System.out.println(LOGO + "\n");
+        printMessageTemplate(messageString);
     }
 
     public static void shutdownMessage() {
-        System.out.println(
-                LINE +
-                " Very well sir, I shall leave you to your own devices.\n" +
-                LINE
-        );
+        printMessageTemplate(" Very well sir, I shall leave you to your own devices.\n");
     }
 
     public static void completeTaskMessage(int index, String taskDescription) {
-        System.out.println(
-                LINE +
-                " Duly noted on completion of task, sir.\n" +
-                "    " + index + "." + taskDescription + "\n" +
-                LINE
-        );
+        String messageString =  "Duly noted on completion of task, sir.\n" + "    " + index + "." + taskDescription + "\n";
+        printMessageTemplate(messageString);
     }
 
-    public static void addTaskMessage(Task t, int listIndex) {
-        System.out.println(
-                LINE +
-                " I shall put this in your schedule, Master Wayne: \n    " + t.toString() + "\n" +
-                " Sir, the number of Tasks you have scheduled currently amounts to " + listIndex + "." + "\n" +
-                LINE
-        );
+    public static void addTaskMessage(Task t, int numberOfTasks) {
+        String messageString = " I shall put this in your schedule, Master Wayne: \n    " + t.toString() + "\n" +
+                " Sir, the number of Tasks you have scheduled currently amounts to " + numberOfTasks + "." + "\n";
+        printMessageTemplate(messageString);
+    }
+
+    // Error Messages
+    public static void invalidCommandMessage() {
+        printMessageTemplate(" Perhaps you could rephrase that in a way us civilians could comprehend.\n");
+    };
+
+    public static void emptyDescriptionMessage() {
+        printMessageTemplate(" Master Wayne, if you do not specify your task, I'm afraid I cannot note it down.\n");
+    }
+
+    public static void invalidIndexMessage() {
+        String messageString = " Sir, the bats must've gone to your head.\n" + " Do try again with a number that " +
+                "identifies your task to mark for completion.\n";
+        printMessageTemplate(messageString);
+    }
+
+    public static void uninitialisedTaskIndexMessage(int numberOfTasks) {
+        String noTaskMessageString = " Sir, you have nothing scheduled, and thus nothing to complete.\n";
+        String singularMessageString = " Sir, might I remind you that you only have 1 task.\n" +
+                " Try again with a number in that range.\n";
+        String pluralMessageString = " Sir, might I remind you that you only have " + numberOfTasks + " tasks.\n" +
+                " Try again with a number in that range.\n";
+        switch (numberOfTasks) {
+        case 0:
+            printMessageTemplate(noTaskMessageString);
+            break;
+        case 1:
+            printMessageTemplate(singularMessageString);
+            break;
+        default:
+            printMessageTemplate(pluralMessageString);
+        }
+    }
+
+    public static void indexOutOfBoundsMessage() {
+        printMessageTemplate(" My old age only allows up to 100 tasks, sir.\n");
+    }
+
+    public static void invalidDateMessage() {
+        printMessageTemplate(" Is there not a date for your so-called deadline or event, sir?\n");
     }
 }
