e7c8ad4f41121c3cd8b1904cf76f253b16cee577
Cuiminjing <e0492441@u.nus.edu>
2021-09-05 18:52:13 +0800

    exception

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,10 @@
+public class DukeException extends Exception{
+    public DukeException(String s) {
+        super(s);
+    }
+
+    @Override
+    public String toString() {
+        return super.getMessage();
+    }
+}
