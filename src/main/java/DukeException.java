44e9cf5dd65455df788d7cfda74be0dd846467b6
Vignesh Kumaravel <k_vignesh@hotmail.com>
2021-09-06 11:00:00 +0800

    Update DukeException class

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,2 +1,15 @@
 public class DukeException extends Exception {
+    private String errorMessage;
+
+    final static String HOR_LINE = "_".repeat(30);
+
+    public DukeException(String message) {
+        this.errorMessage = message;
+    }
+
+    public void printErrorMessage() {
+        System.out.println(HOR_LINE);
+        System.out.println(this.errorMessage);
+        System.out.println(HOR_LINE);
+    }
 }
