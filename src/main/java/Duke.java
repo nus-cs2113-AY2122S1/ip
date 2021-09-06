39a8c6e5692f6ee98e6ab2fb6047bff08ab57241
WU_LUOYU_SERENA <siriuswusss@gmail.com>
2021-09-07 00:08:40 +0800

    Right the wrong formatting of switch statements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,108 +28,108 @@ public class Duke {
         String[] taskDescriptionWords;
 
         switch (typeOfTask) {
-            case "todo":
-                if (taskContentWords.length == 0) {
-                    printEmptyTaskErrorMessage();
-                    break;
-                }
-                taskDescription = String.join(" ", taskContentWords);
-                list[Task.getNumberOfTasks()] = new Task(taskDescription);
+        case "todo":
+            if (taskContentWords.length == 0) {
+                printEmptyTaskErrorMessage();
                 break;
-            case "deadline":
-                if (taskContentWords.length == 0) {
-                    printEmptyTaskErrorMessage();
-                    break;
-                }
-                /*-------------locate keyword "/by" in the user input-------------*/
-                int indexOfBy = -1;
-                for (int i = 0; i < taskContentWords.length; i++) {
-                    if (taskContentWords[i].equals("/by")) {
-                        indexOfBy = i;
-                        break;
-                    }
-                }
-                /*----------------------invalid input cases-----------------------*/
-                boolean hasNoTaskBeforeBy = indexOfBy == 0;
-                if (hasNoTaskBeforeBy) {
-                    printEmptyTaskErrorMessage();
-                    break;
-                }
-                boolean hasNoKeywordBy = (indexOfBy == -1);
-                boolean hasNoDeadlineAfterBy = (indexOfBy == taskContentWords.length - 1);
-                if (hasNoKeywordBy | hasNoDeadlineAfterBy) {
-                    printMissingDeadlineErrorMessage();
-                    break;
-                }
-                /*-----------------------valid input cases------------------------*/
-                taskDescriptionWords = Arrays.copyOf(taskContentWords, indexOfBy);
-                taskDescription = String.join(" ", taskDescriptionWords);
-                String[] taskDeadlineWords = Arrays.copyOfRange(taskContentWords,
-                        indexOfBy + 1, taskContentWords.length);
-                String taskDeadline = String.join(" ", taskDeadlineWords);
-                list[Task.getNumberOfTasks()] = new Deadline(taskDescription, taskDeadline);
+            }
+            taskDescription = String.join(" ", taskContentWords);
+            list[Task.getNumberOfTasks()] = new Task(taskDescription);
+            break;
+        case "deadline":
+            if (taskContentWords.length == 0) {
+                printEmptyTaskErrorMessage();
                 break;
-            case "event":
-                if (taskContentWords.length == 0) {
-                    printEmptyTaskErrorMessage();
+            }
+            /*-------------locate keyword "/by" in the user input-------------*/
+            int indexOfBy = -1;
+            for (int i = 0; i < taskContentWords.length; i++) {
+                if (taskContentWords[i].equals("/by")) {
+                    indexOfBy = i;
                     break;
                 }
-                /*-------------locate keyword "/at" in the user input-------------*/
-                int indexOfAt = -1;
-                for (int i = 0; i < taskContentWords.length; i++) {
-                    if (taskContentWords[i].equals("/at")) {
-                        indexOfAt = i;
-                        break;
-                    }
-                }
-                /*----------------------invalid input cases-----------------------*/
-                boolean hasNoTaskBeforeAt = (indexOfAt == 0);
-                if (hasNoTaskBeforeAt) {
-                    printEmptyTaskErrorMessage();
-                    break;
-                }
-                boolean hasNoKeywordAt = (indexOfAt == -1);
-                boolean hasNoTimeAfterAt = (indexOfAt == taskContentWords.length - 1);
-                if (hasNoKeywordAt | hasNoTimeAfterAt) {
-                    printMissingTimeErrorMessage();
-                    break;
-                }
-                /*-----------------------valid input cases------------------------*/
-                taskDescriptionWords = Arrays.copyOf(taskContentWords, indexOfAt);
-                taskDescription = String.join(" ", taskDescriptionWords);
-                String[] taskTimeWords = Arrays.copyOfRange(taskContentWords,
-                        indexOfAt + 1, taskContentWords.length);
-                String taskTime = String.join(" ", taskTimeWords);
-                list[Task.getNumberOfTasks()] = new Event(taskDescription, taskTime);
+            }
+            /*----------------------invalid input cases-----------------------*/
+            boolean hasNoTaskBeforeBy = indexOfBy == 0;
+            if (hasNoTaskBeforeBy) {
+                printEmptyTaskErrorMessage();
                 break;
-            case "list":
-                if (taskContentWords.length == 0) {
+            }
+            boolean hasNoKeywordBy = (indexOfBy == -1);
+            boolean hasNoDeadlineAfterBy = (indexOfBy == taskContentWords.length - 1);
+            if (hasNoKeywordBy | hasNoDeadlineAfterBy) {
+                printMissingDeadlineErrorMessage();
+                break;
+            }
+            /*-----------------------valid input cases------------------------*/
+            taskDescriptionWords = Arrays.copyOf(taskContentWords, indexOfBy);
+            taskDescription = String.join(" ", taskDescriptionWords);
+            String[] taskDeadlineWords = Arrays.copyOfRange(taskContentWords,
+                    indexOfBy + 1, taskContentWords.length);
+            String taskDeadline = String.join(" ", taskDeadlineWords);
+            list[Task.getNumberOfTasks()] = new Deadline(taskDescription, taskDeadline);
+            break;
+        case "event":
+            if (taskContentWords.length == 0) {
+                printEmptyTaskErrorMessage();
+                break;
+            }
+            /*-------------locate keyword "/at" in the user input-------------*/
+            int indexOfAt = -1;
+            for (int i = 0; i < taskContentWords.length; i++) {
+                if (taskContentWords[i].equals("/at")) {
+                    indexOfAt = i;
+                    break;
+                }
+            }
+            /*----------------------invalid input cases-----------------------*/
+            boolean hasNoTaskBeforeAt = (indexOfAt == 0);
+            if (hasNoTaskBeforeAt) {
+                printEmptyTaskErrorMessage();
+                break;
+            }
+            boolean hasNoKeywordAt = (indexOfAt == -1);
+            boolean hasNoTimeAfterAt = (indexOfAt == taskContentWords.length - 1);
+            if (hasNoKeywordAt | hasNoTimeAfterAt) {
+                printMissingTimeErrorMessage();
+                break;
+            }
+            /*-----------------------valid input cases------------------------*/
+            taskDescriptionWords = Arrays.copyOf(taskContentWords, indexOfAt);
+            taskDescription = String.join(" ", taskDescriptionWords);
+            String[] taskTimeWords = Arrays.copyOfRange(taskContentWords,
+                    indexOfAt + 1, taskContentWords.length);
+            String taskTime = String.join(" ", taskTimeWords);
+            list[Task.getNumberOfTasks()] = new Event(taskDescription, taskTime);
+            break;
+        case "list":
+            if (taskContentWords.length == 0) {
+                Task.printList(list);
+                break;
+            } else {
+                printPendingConfirmationToListMessage();
+                String userConfirmation = in.nextLine().toLowerCase(Locale.ROOT);
+                if (userConfirmation.equals("y")) {
                     Task.printList(list);
-                    break;
-                } else {
-                    printPendingConfirmationToListMessage();
-                    String userConfirmation = in.nextLine().toLowerCase(Locale.ROOT);
-                    if (userConfirmation.equals("y")) {
-                        Task.printList(list);
-                    }
-                    break;
                 }
-            case "done":
-                try {
-                    int index = Integer.parseInt(taskContentWords[0]);
-                    list[index - 1].markAsDone();
-                    break;
-                } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
-                    printDoneFormatErrorMessage();
-                    break;
-                } catch (NullPointerException ex) {
-                    printOutOfBoundErrorMessage();
-                    break;
-                }
-            case "bye":
-                Task.exit();
-            default:
-                printGeneralErrorMessage();
+                break;
+            }
+        case "done":
+            try {
+                int index = Integer.parseInt(taskContentWords[0]);
+                list[index - 1].markAsDone();
+                break;
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
+                printDoneFormatErrorMessage();
+                break;
+            } catch (NullPointerException ex) {
+                printOutOfBoundErrorMessage();
+                break;
+            }
+        case "bye":
+            Task.exit();
+        default:
+            printGeneralErrorMessage();
         }
     }
 
