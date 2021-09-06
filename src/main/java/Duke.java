1b6170ca0b4162cd7dc7d91656a23711790dc7ef
pos0414 <gimins1202@gmail.com>
2021-09-07 00:08:52 +0900

    Handle exception

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,10 @@ public class Duke {
             LINE_SEPARATOR_AND_INDENT + " To add an event: event /on event_date" +
             LINE_SEPARATOR_AND_INDENT + " To add a deadline: deadline /by deadline_date" +
             LINE_SEPARATOR_AND_INDENT + " To view the list: list" +
+<<<<<<< Updated upstream
+=======
+            LINE_SEPARATOR_AND_INDENT + " To get help: help" +
+>>>>>>> Stashed changes
             LINE_SEPARATOR_AND_INDENT + " To end the program: bye";
 
     public static void echo(String line) {
@@ -27,6 +31,7 @@ public class Duke {
 
         while (!line.equals("bye")) {
             System.out.println(LINE);
+<<<<<<< Updated upstream
             switch (words[0]) {
             case "list":
                 TaskHandler.printTaskList();
@@ -48,6 +53,36 @@ public class Duke {
                 break;
             default:
                 System.out.println(INDENT + "Please enter a valid command");
+=======
+            try {
+                switch (words[0]) {
+                case "list":
+                    TaskHandler.printTaskList();
+                    break;
+                case "done":
+                    TaskHandler.markTaskAsDone(words);
+                    break;
+                case "deadline":
+                    TaskHandler.addDeadline(line);
+                    break;
+                case "event":
+                    TaskHandler.addEvent(line);
+                    break;
+                case "todo":
+                    TaskHandler.addTodo(line);
+                    break;
+                case "help":
+                    TaskHandler.showHelp();
+                    break;
+                default:
+                    TaskHandler.handleWrongCommand();
+                    break;
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+>>>>>>> Stashed changes
             }
             System.out.println(LINE);
             line = in.nextLine();
