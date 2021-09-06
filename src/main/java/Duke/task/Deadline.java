975141e671c79cc03bcd7595ab73507874707274
markuslyq <markuslyq@gmail.com>
2021-09-06 22:44:32 +0800

    Added error messages when incorrect inputs are entered by the user

--- src/main/java/Duke/task/Deadline.java
+++ src/main/java/Duke/task/Deadline.java
@@ -11,8 +11,12 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
+
+        //get the preposition used
         int spaceIndex = by.indexOf(' ');
         String preposition = by.substring(0, spaceIndex);
         String dueDate = by.substring(spaceIndex+1);
+
+        //output message
         return "[D]" + super.getStatusIcon() + super.toString() + " (" + preposition + ": " + dueDate + ")";    }
 }
