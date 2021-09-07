32e848d383b79d212cb2c94eee14bea0706248c3
avellinwong01 <avellin.wong@gmail.com>
2021-09-07 23:58:38 +0800

    Revert "Make changes to code from PR feedback"
    This reverts commit ad7b0dc065ca97483deb6dff86a88743ec81f365.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -34,10 +34,10 @@ public class Duke {
         printHorizontalLine();
         System.out.println(" Here are the tasks in your list:");
         Task[] taskList = Arrays.copyOf(tasks, currCount);
-        int taskCount = 1;
+        int count = 1;
         for (Task elem : taskList) {
-            System.out.println(" " + taskCount + ". " + elem.toString());
-            taskCount += 1;
+            System.out.println(" " + count + ". " + elem.toString());
+            count += 1;
         }
         printHorizontalLine();
     }
