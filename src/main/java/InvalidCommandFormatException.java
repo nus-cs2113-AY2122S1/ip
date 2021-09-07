77134cc90f9a9c9dde97b9650393bc6739205b75
tlyi <t.leyi2305@gmail.com>
2021-09-07 17:39:34 +0800

    Create and Implement InvalidCommandFormatException to handle invalid command input formats

---
+++ src/main/java/InvalidCommandFormatException.java
@@ -0,0 +1,12 @@
+public class InvalidCommandFormatException extends Exception{
+    protected final String errorMessage;
+    public InvalidCommandFormatException(String errorMessage) {
+        super(errorMessage);
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public String toString() {
+        return this.errorMessage;
+    }
+}
