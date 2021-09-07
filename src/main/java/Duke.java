a7525e3968e90cdcad5c72a3f81d00469bf824a0
yanjia1777 <yanjia1777@gmail.com>
2021-09-07 17:54:53 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,26 +6,27 @@ public class Duke {
     protected Task[] list = new Task[100];
     protected Scanner in;
 
-    public static void printAdded(Task task, int count){
+    public static void printAdded(Task task, int count) {
         System.out.println("Got it. I've added this task:");
         task.printTask();
         System.out.println("Now you have " + count + " tasks in the list.");
     }
-    public int executeCommand(String input){
+
+    public int executeCommand(String input) throws DukeException{
         String command;
         String description = null;
         String time = null;
         String temp;
-        if(input.contains(" ")){
+        if (input.contains(" ")) {
             command = input.substring(0, input.indexOf(" "));
-            temp = input.substring(input.indexOf(" ")+1);
-            if(temp.contains("/")) {
+            temp = input.substring(input.indexOf(" ") + 1);
+            if (temp.contains("/")) {
                 description = input.substring(input.indexOf(" ") + 1, input.indexOf("/") - 1);
-                time = input.substring(input.indexOf("/")+4);
-            }else{
+                time = input.substring(input.indexOf("/") + 4);
+            } else {
                 description = temp;
             }
-        }else{
+        } else {
             command = input;
         }
         switch(command){
@@ -36,10 +37,9 @@ public class Duke {
                     break;
                 }
             }
-                break;
+            return 0;
         case "done":
-            char num = input.charAt(5);
-            int index = Character.getNumericValue(num);
+            int index = Integer.parseInt(description);
             for (Task task : list) {
                 if (task.index == index) {
                     task.markAsDone();
@@ -47,44 +47,72 @@ public class Duke {
                     System.out.println("[" + task.getStatusIcon() + "] " + task.description);
                     break;
                 } else if (task.index == count) {
-                    break;
+                    throw new StringIndexOutOfBoundsException();
                 }
             }
-            break;
+            return 0;
         case "todo":
             count++;
             Todo todo = new Todo(description, count);
+            if (todo.description==null) {
+                throw new DukeException("nullTodo");
+            }
             printAdded(todo,count);
             list[count-1] = todo;
-            break;
+            return 0;
         case "deadline":
             count++;
             Deadline deadline = new Deadline(description, count, time);
+            if (deadline.deadline==null) {
+                throw new DukeException("nullDeadline");
+            }
             printAdded(deadline,count);
             list[count-1] = deadline;
-            break;
+            return 0;
         case "event":
             count++;
             Event event = new Event(description, count, time);
+            if (event.event==null) {
+                throw new DukeException("nullEvent");
+            }
             printAdded(event,count);
             list[count-1] = event;
-            break;
+            return 0;
         case "bye":
             System.out.println("Bye. Hope to see you again soon!");
             return -1;
         }
-
-        return 0;
+        throw new DukeException("unknownCommand");
     }
 
-    public void run(){
+    public void run() {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         in = new Scanner(System.in);
         String input = in.nextLine();
-        while(true){
-            if (executeCommand(input) == -1) {
-                break;
+        while (true) {
+            try {
+                if (executeCommand(input) == -1) {
+                    break;
+                }
+            } catch (DukeException e) {
+                if (e.error.equals("unknownCommand")) {
+                    System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                } else if (e.error.equals("nullTodo")) {
+                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                } else if (e.error.equals("nullDeadline")) {
+                    System.out.println("☹ OOPS!!! The time of a deadline cannot be empty.");
+                } else if (e.error.equals("nullEvent")) {
+                    System.out.println("☹ OOPS!!! The time of a event cannot be empty.");
+                } else {
+                    System.out.println("☹ OOPS!!! There is an unknown error.");
+                }
+            } catch (NullPointerException e) {
+                System.out.println("☹ OOPS!!! There are no items in the list.");
+            } catch (StringIndexOutOfBoundsException e) {
+                System.out.println("☹ OOPS!!! There is no such item to be done.");
+            } catch (NumberFormatException e) {
+                System.out.println("☹ OOPS!!! Please input the index of the item to be done.");
             }
             input = in.nextLine();
         }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,6 @@
+public class DukeException extends Exception{
+    public String error;
+    public DukeException(String error){
+        this.error = error;
+    }
+}
