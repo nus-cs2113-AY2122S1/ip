cf8ec30f9e4e7544ec96cd6f139bec44f697e086
aditichadha1310 <70324480+aditichadha1310@users.noreply.github.com>
2021-09-07 17:13:58 +0800

    A-Exceptions
    Handled errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -56,16 +56,21 @@ public class Duke {
         userInput = in.nextLine();
 
         while (!(userInput.equalsIgnoreCase("bye"))) {
-            if (userInput.equalsIgnoreCase("list")) {
-                list();
-            } else if (userInput.startsWith("done")) {
-                markTaskAsDone(userInput);
-            } else {
-                addTaskToList(userInput);
+            try {
+                if (userInput.equalsIgnoreCase("list")) {
+                    list();
+                } else if (userInput.startsWith("done")) {
+                    markTaskAsDone(userInput);
+                } else {
+                    addTaskToList(userInput);
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
             printLine();
             userInput = in.nextLine();
         }
+
     }
 
     /**
@@ -82,48 +87,76 @@ public class Duke {
      * Displays appropriate message if task is valid and is created successfully, vice versa.
      * Displays the total number of tasks in the list as well.
      *
-     * @param taskDescription TaskDescription Stores the description of the task.
-     * @param taskDueBy       TaskDueBy stores the deadline of the task which is created as a deadline.
-     * @param taskDueAt       TaskDueAt stores the event timing of the task created as an event.
-     * @param isTaskValid     IsTaskValid stores true if the task statement entered by the user is a valid task creation statement and false, otherwise.
-     * @param index           Index stores the index of the "/" in the entered String
+     * @param isTaskValid IsTaskValid stores true if the task statement entered by the user is a valid task creation statement and false, otherwise.
+     * @param index       Index stores the index of the "/" in the entered String
      */
-    private static void addTaskToList(String userInput) {
+    private static void addTaskToList(String userInput) throws DukeException {
 
-        String taskDescription;
-        String taskDueBy;
-        String taskDueAt;
         int index;
         boolean isTaskValid = true;
+        String firstWord;
+        String[] split = userInput.split(" ", 2);
+        String taskDescription;
+        String timeDueAt;
+        String timeDueBy;
+        firstWord = split[0].toLowerCase();
 
-        if (userInput.startsWith("todo")) {
-            scheduledTasks[taskCounter] = new Todo(userInput);
-        } else if (userInput.startsWith("deadline")) {
+        switch (firstWord) {
+        case "todo":
+            if (split.length < 2 || split[1].isEmpty() == true) {
+                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+            } else {
+                scheduledTasks[taskCounter] = new Todo(userInput);
+            }
+            break;
+
+        case "deadline":
             index = userInput.indexOf("/");
+            if (split.length < 2 || split[1].isEmpty() == true || index == -1) {
+                throw new DukeException("☹ OOPS!!! The description or the deadline of the task cannot be empty.");
+            }
+            taskDescription = split[1].split("/by", 2)[0];
+            timeDueBy = split[1].split("/by", 2)[1];
+            if (taskDescription.isEmpty() == true || timeDueBy.isEmpty() == true) {
+                throw new DukeException("☹ OOPS!!! The description of the task seems incomplete.");
+            }
             scheduledTasks[taskCounter] = new Deadline(userInput.substring(0, index), userInput.substring(index + 3));
-        } else if (userInput.startsWith("event")) {
+            break;
+
+        case "event":
             index = userInput.indexOf("/");
+            if (split.length < 2 || split[1].isEmpty() == true || index == -1) {
+                throw new DukeException("☹ OOPS!!! The description and time schedule of the event cannot be empty.");
+            }
+            taskDescription = split[1].split("/at", 2)[0];
+            timeDueAt = split[1].split("/at", 2)[1];
+            if (taskDescription.isEmpty() == true || timeDueAt.isEmpty() == true) {
+                throw new DukeException("☹ OOPS!!! The description or time schedule of the event seems incomplete.");
+            }
             scheduledTasks[taskCounter] = new Event(userInput.substring(0, index), userInput.substring(index + 3));
-        } else {
+            break;
+
+        default:
             isTaskValid = false;
-            System.out.println("Invalid task creation statement");
+            throw new DukeException(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
 
         if (isTaskValid) {
-            taskCounter++;
             printLine();
             System.out.println("Got it. I've added this task:");
-            System.out.println(" " + scheduledTasks[taskCounter - 1]);
+            System.out.println(" " + scheduledTasks[taskCounter]);
+            taskCounter++;
             System.out.println("Now you have " + taskCounter + " tasks in the list.");
         }
     }
+
 
     /**
      * Updates the status of the task by marking it as done in the task list.
      *
      * @param taskNumberCompleted TaskNumberCompleted stores the task number which has been completed by the user.
      */
-    private static void markTaskAsDone(String userInput) {
+    private static void markTaskAsDone(String userInput) throws DukeException {
         printLine();
         int taskNumberCompleted = Integer.parseInt(userInput.substring(userInput.indexOf(" ") + 1));
 
@@ -132,7 +165,7 @@ public class Duke {
             System.out.println("Nice! I have marked this task as done:");
             System.out.println(scheduledTasks[taskNumberCompleted - 1]);
         } else {
-            System.out.println("Sorry, no task is assigned at this number, you might want to re-check?");
+            throw new DukeException("Sorry, no task is assigned at this number, you might want to re-check?");
         }
     }
 
@@ -142,12 +175,12 @@ public class Duke {
      *
      * @param taskCompletionStatus TaskCompletionStatus stores true if the task is completed, false otherwise.
      */
-    private static void list() {
+    private static void list() throws DukeException {
         int i;
         String taskCompletionStatus;
         printLine();
         if (taskCounter == 0) {
-            System.out.println("Sorry, no tasks have been added to the list as yet!\n" +
+            throw new DukeException("Sorry, no tasks have been added to the list as yet!\n" +
                     "You can add tasks to this list simply by typing and pressing \"Enter\"!!");
         } else {
             System.out.println("Here are the tasks in your list:");
