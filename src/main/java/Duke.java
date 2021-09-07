58db3d29b6995e4e3825b4a1b5f81156daf58247
avellinwong01 <avellin.wong@gmail.com>
2021-09-08 02:04:49 +0800

    Add Increment Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+    private static final int MAX_TASKS = 100;
+    private static final int TODO_MIN_LENGTH = 6;
+    private static final int TODO_DESCRIPTION_START = 5;
+    private static final String TODO_DESCRIPTION_ERROR = "The todo description cannot be empty";
+    private static final int EVENT_MIN_LENGTH = 7;
+    private static final int EVENT_DESCRIPTION_START = 6;
+    private static final String EVENT_EMPTY_ERROR = "The event must contain an '/at'";
+    private static final String EVENT_DESCRIPTION_ERROR = "The event description cannot be empty";
+    private static final int DEADLINE_MIN_LENGTH = 10;
+    private static final int DEADLINE_DESCRIPTION_START = 9;
+    private static final String DEADLINE_EMPTY_ERROR = "The deadline must contain a '/by'";
+    private static final String DEADLINE_DESCRIPTION_ERROR = "The deadline description cannot be empty";
+    private static final int DONE_MIN_LENGTH = 2;
+    private static final String DONE_DESCRIPTION_ERROR = "The task to be marked as done cannot be empty";
+    private static final String DONE_EXCEED_ERROR = "The task to be marked as done does not exist in the list of tasks";
+    private static final int EVENT_DEADLINE_TIME = 3;
+    private static final String COMMAND_ERROR = "Sorry, I don't understand what you are saying";
+
+
     public static void printHorizontalLine() {
-        String horizontal_line = "____________________________________________________________";
-        System.out.println(horizontal_line);
+        System.out.println(HORIZONTAL_LINE);
     }
 
     public static void welcome() {
@@ -20,9 +39,15 @@ public class Duke {
         printHorizontalLine();
     }
 
-    public static void markAsDone(Task[] tasks, String line) {
+    public static void markAsDone(Task[] tasks, int currCount, String line) throws DukeException {
         String[] input = line.split(" ");
+        if (input.length < DONE_MIN_LENGTH) {
+            throw new DukeException(DONE_DESCRIPTION_ERROR);
+        }
         int index = Integer.parseInt(input[1]) - 1;
+        if (index >= currCount) {
+            throw new DukeException(DONE_EXCEED_ERROR);
+        }
         tasks[index].markAsDone();
         printHorizontalLine();
         System.out.println(" Nice! I've marked this task as done:");
@@ -42,18 +67,7 @@ public class Duke {
         printHorizontalLine();
     }
 
-    public static int addTask(Task[] tasks, int currCount, String line) {
-        if (line.contains("todo")) {
-            // take the description part of the input string
-            tasks[currCount] = new Todo(line.substring(5));
-        } else if (line.contains("deadline")) {
-            String[] input = line.substring(9).split("/");
-            // first elem: description, second elem: deadline
-            tasks[currCount] = new Deadline(input[0], input[1].substring(3));
-        } else if (line.contains("event")) {
-            String[] input = line.substring(6).split("/");
-            tasks[currCount] = new Event(input[0], input[1].substring(3));
-        }
+    public static int completeAddTask(Task[] tasks, int currCount) {
         printHorizontalLine();
         System.out.println(" Got it. I've added this task:");
         System.out.println(" " + tasks[currCount].toString());
@@ -64,6 +78,37 @@ public class Duke {
         return currCount;
     }
 
+    public static void addEvent(Task[] tasks, int currCount, String line) throws DukeException {
+        if (line.length() < EVENT_MIN_LENGTH) {
+            throw new DukeException(EVENT_DESCRIPTION_ERROR);
+        }
+        if (!line.contains("/at")) {
+            throw new DukeException(EVENT_EMPTY_ERROR);
+        }
+        String[] input = line.substring(EVENT_DESCRIPTION_START).split("/");
+        tasks[currCount] = new Event(input[0], input[1].substring(EVENT_DEADLINE_TIME));
+    }
+
+    public static void addDeadline(Task[] tasks, int currCount, String line) throws DukeException {
+        if (line.length() < DEADLINE_MIN_LENGTH) {
+            throw new DukeException(DEADLINE_DESCRIPTION_ERROR);
+        }
+        if (!line.contains("/by")) {
+            throw new DukeException(DEADLINE_EMPTY_ERROR);
+        }
+        String[] input = line.substring(DEADLINE_DESCRIPTION_START).split("/");
+        // first elem: description, second elem: deadline
+        tasks[currCount] = new Deadline(input[0], input[1].substring(EVENT_DEADLINE_TIME));
+    }
+
+    public static void addTodo(Task[] tasks, int currCount, String line) throws DukeException {
+        if (line.length() < TODO_MIN_LENGTH) {
+            throw new DukeException(TODO_DESCRIPTION_ERROR);
+        }
+        // take the description part of the input string
+        tasks[currCount] = new Todo(line.substring(TODO_DESCRIPTION_START));
+    }
+
     public static void goodbye() {
         printHorizontalLine();
         System.out.println(" Bye. Hope to see you again soon!");
@@ -72,17 +117,33 @@ public class Duke {
 
     public static void main(String[] args) {
         welcome();
-        Task[] tasks = new Task[100]; // fixed size array for now, each contains a Task element
+        Task[] tasks = new Task[MAX_TASKS]; // fixed size array for now, each contains a Task element
         int currCount = 0;
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
         while (!line.equals("bye")) {
-            if (line.contains("done")) { // mark task as done
-                markAsDone(tasks, line);
-            } else if (!line.equals("list")) { // user inputs a task
-                currCount = addTask(tasks, currCount, line);
-            } else { // print the list
-                printList(tasks, currCount);
+            try {
+                if (line.contains("done")) { // mark task as done
+                    markAsDone(tasks, currCount, line);
+                } else if (line.contains("todo")) {
+                    addTodo(tasks, currCount, line);
+                    currCount = completeAddTask(tasks, currCount);
+                } else if (line.contains("deadline")) {
+                    addDeadline(tasks, currCount, line);
+                    currCount = completeAddTask(tasks, currCount);
+                } else if (line.contains("event")) {
+                    addEvent(tasks, currCount, line);
+                    currCount = completeAddTask(tasks, currCount);
+                } else if (line.equals("list")){ // print the list
+                    printList(tasks, currCount);
+                }
+                else {
+                    throw new DukeException(COMMAND_ERROR);
+                }
+            } catch (DukeException e) {
+                printHorizontalLine();
+                System.out.println(e.getMessage());
+                printHorizontalLine();
             }
             line = in.nextLine();
         }
