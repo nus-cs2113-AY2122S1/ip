8881bd549260ad970bd8d6e9a06e65ec27029951
leowyy99 <leowyy99@hotmail.com>
2021-09-08 00:24:26 +0800

    Add packages

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,3 +1,12 @@
+package duke;
+
+import duke.exceptions.EmptyDescriptionException;
+import duke.exceptions.EmptyTimeFieldException;
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.Todo;
+
 import java.util.Scanner;
 
 /**
