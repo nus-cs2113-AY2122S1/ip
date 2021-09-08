14a2865498c0e6b56ae7f59e1881b209e7051bb9
vincentlauhl <vincentlauhl@gmail.com>
2021-09-08 18:04:54 +0800

    Add exception handling and exception class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,11 +15,18 @@ public class Duke {
     public static final String DEADLINE_DESCRIPTION_AND_DATE_SPLITTER = " /by ";
     public static final String EVENT_DESCRIPTION_AND_DATE_TIME_SPLITTER = " /at ";
     public static final String GOODBYE_MESSAGE = "Bye, hope to see you again soon! :)";
-    public static final String TYPE_SUITABLE_COMMAND_MESSAGE = "Sorry, you are using me wrongly. Please type in a suitable command :)";
-    public static final boolean IS_FINE = true;
+    public static final String TYPE_SUITABLE_COMMAND_MESSAGE = "Sorry, I don't know what you mean. " +
+            "Please give me a suitable command :)";
     public static final int TODO_STARTING_INDEX = 5;
     public static final int DEADLINE_STARTING_INDEX = 9;
     public static final int EVENT_STARTING_INDEX = 6;
+    public static final String PROMPT_TASK_DESCRIPTION = "Please tell me what do you need to do :)";
+    public static final String INPUT_MORE_THAN_NUMBER_OF_TASKS = "Woah, you don't have THAT many things to do!";
+    public static final String LIST_IS_EMPTY = "You're a free man :)";
+    public static final String MISSING_ARGUMENTS_FOR_EVENT_AND_DEADLINE = "Sorry," +
+            " you're missing some arguments, do type 'help' if you're unsure :)";
+    public static final String PROMPT_TASK_NUMBER = "Please tell me which task you finished :)";
+    public static final String PROMPT_SENSIBLE_INDEX = "Please give a number between 1-100 :)";
     public static Task[] tasks = new Task[100];
 
     public static void main(String[] args) {
@@ -60,23 +67,54 @@ public class Duke {
 
     private static void processInput(String input) {
         switch(input.split(" ")[0].toLowerCase()) {
-        case "list" :
+        case "list":
             executeListCase();
             break;
-        case "done" :
-            executeDoneCase(input);
+        case "done":
+            try {
+                executeDoneCase(input);
+
+            } catch (NullPointerException error) {
+                printErrorMessage(INPUT_MORE_THAN_NUMBER_OF_TASKS);
+
+            } catch (DukeException error) {
+                printErrorMessage(PROMPT_TASK_NUMBER);
+
+            } catch (ArrayIndexOutOfBoundsException error) {
+                printErrorMessage(PROMPT_SENSIBLE_INDEX);
+
+            } catch (NumberFormatException error) {
+                printErrorMessage(PROMPT_SENSIBLE_INDEX);
+
+            }
             break;
-        case "todo" :
-            executeTaskCase(input, TODO_STARTING_INDEX,TaskType.TODO);
+        case "todo":
+            try {
+                executeTaskCase(input, TODO_STARTING_INDEX,TaskType.TODO);
+
+            } catch (StringIndexOutOfBoundsException error) {
+                printErrorMessage(PROMPT_TASK_DESCRIPTION);
+            }
             break;
-        case "deadline" :
-            executeTaskCase(input, DEADLINE_STARTING_INDEX,TaskType.DEADLINE);
+        case "deadline":
+            try {
+                executeTaskCase(input, DEADLINE_STARTING_INDEX,TaskType.DEADLINE);
+
+            } catch (StringIndexOutOfBoundsException error) {
+                printErrorMessage(PROMPT_TASK_DESCRIPTION);
+
+            }
             break;
-        case "event" :
-            executeTaskCase(input, EVENT_STARTING_INDEX,TaskType.EVENT);
+        case "event":
+            try {
+                executeTaskCase(input, EVENT_STARTING_INDEX,TaskType.EVENT);
+
+            } catch (StringIndexOutOfBoundsException error) {
+                printErrorMessage(PROMPT_TASK_DESCRIPTION);
+            }
             break;
-        default :
-            printIncorrectInputMessage();
+        default:
+            printErrorMessage(TYPE_SUITABLE_COMMAND_MESSAGE);
         }
     }
 
@@ -89,20 +127,21 @@ public class Duke {
         System.out.println();
     }
 
-    private static void printIncorrectInputMessage() {
+    private static void printErrorMessage(String error_message) {
         printIndentationAndDivider();
-        printWordsWithIndentation(TYPE_SUITABLE_COMMAND_MESSAGE);
+        printWordsWithIndentation(error_message);
         printIndentationAndDivider();
         System.out.println();
     }
 
     private static void executeTaskCase(String input, int starting_index, TaskType type) {
         String description = input.strip().substring(starting_index);
-        boolean isFine = addTask(description, type);
-        if(isFine) {
+        try {
+            addTask(description, type);
             printTaskMessage();
-        }else {
-            printIncorrectInputMessage();
+
+        } catch (DukeException error) {
+            printErrorMessage(MISSING_ARGUMENTS_FOR_EVENT_AND_DEADLINE);
         }
     }
 
@@ -112,11 +151,9 @@ public class Duke {
         case TODO:
             parsedOutput = new String[]{input};
             break;
-
         case DEADLINE:
             parsedOutput = input.split(DEADLINE_DESCRIPTION_AND_DATE_SPLITTER);
             break;
-
         case EVENT:
             parsedOutput = input.split(EVENT_DESCRIPTION_AND_DATE_TIME_SPLITTER);
             break;
@@ -124,7 +161,7 @@ public class Duke {
         return parsedOutput;
     }
 
-    private static boolean addTask(String input,TaskType type) {
+    private static void addTask(String input,TaskType type) throws StringIndexOutOfBoundsException, DukeException {
         String[] parsedOutput = parseInputForDifferentTask(input,type);
         switch(type){
         case TODO:
@@ -132,29 +169,28 @@ public class Duke {
             break;
         case EVENT:
             if(parsedOutput.length < 2) {
-                return !IS_FINE;
+                throw new DukeException();
             }
             tasks[Task.getTotalTasks()] = new Event(parsedOutput[0], parsedOutput[1]);
             break;
         case DEADLINE:
             if(parsedOutput.length < 2) {
-                return !IS_FINE;
+                throw new DukeException();
             }
             tasks[Task.getTotalTasks()] = new Deadline(parsedOutput[0], parsedOutput[1]);
             break;
         }
         Task.setTotalTasks(Task.getTotalTasks() + 1);
-        return IS_FINE;
     }
 
     private static String notifyNumberOfTasks() {
         return "Now you have " + Task.getTotalTasks() + " task(s) in the list";
     }
 
-    private static void executeDoneCase(String input) {
-        if(input.length() != 6) {
-            printIncorrectInputMessage();
-            return;
+    private static void executeDoneCase(String input) throws NullPointerException,DukeException,
+            ArrayIndexOutOfBoundsException, NumberFormatException{
+        if(input.length() < 6) {
+            throw new DukeException();
         }
         int index = Integer.parseInt(input.split(" ")[1]) - 1;
         tasks[index].markAsDone();
@@ -171,6 +207,9 @@ public class Duke {
 
     private static void executeListCase() {
         printIndentationAndDivider();
+        if(Task.getTotalTasks() == 0) {
+            printWordsWithIndentation(LIST_IS_EMPTY);
+        }
         for (int i = 0; i < Task.getTotalTasks(); i++) {
             printWordsWithIndentation(i + 1 + "." + tasks[i].getStatusIconAndDescription());
         }
