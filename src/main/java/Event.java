de1d879a071d56f617f0f242874e0601786ae9fd
rondayvoo <69455258+rondayvoo@users.noreply.github.com>
2021-09-08 09:00:02 +0800

    Merge branch 'branch-A-Packages'

--- src/main/java/Event.java
+++
@@ -1,8 +0,0 @@
-public class Event extends Deadline {
-    protected String time;
-
-    public Event(String description, String time) {
-        super(description, time);
-        this.type = 'E';
-    }
-}
