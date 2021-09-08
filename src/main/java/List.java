b4f3fee555f4fd8e97d8012a1354a7f05d4261d9
chuhan wang <WA0022AN@e.ntu.edu.sg>
2021-09-09 00:30:51 +0800

    Level 5 and change the structure of the whole program

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,81 +1,15 @@
 import java.lang.String;
 
 public class List {
+    protected String description;
+    protected boolean isDone;
 
-    public static String[] list = new String[100];
-    public static String[] done = new String[100];
-    public static int count = 0;
-    public static int doneCount = 0;
-
-    public static void printTask() {
-        System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n     "
-                + "Here are the tasks in your list:");
-        for (int i = 0; i < count; i++) {
-            System.out.println("     " + list[i]);
-        }
-
-        System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    public List(String description) {
+        this.description = "[ ] " + description;
+        this.isDone = false;
     }
 
-    public static void printDoneTask() {
-        System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n      "
-                + "Nice! I've marked this task as done: ");
-        for (int i = 0; i < doneCount; i++) {
-            System.out.println("     " + done[i]);
-        }
-        System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    }
-
-    public static void addTask(String task) {
-        if (task.indexOf("/") != -1) {
-            list[count] = (count + 1) + ".["
-                + task.toUpperCase().charAt(0)
-                + "][ ] "
-                + task.substring(task.indexOf(" "), task.indexOf("/"))
-                + "(" + task.substring(task.indexOf("/") + 1, task.indexOf("/") + 3)
-                + ": " + task.substring(task.indexOf("/") + 4) + ")";
-        } else {
-            list[count] = (count + 1) + ".["
-                + task.toUpperCase().charAt(0)
-                + "][ ] "
-                + task.substring(task.indexOf(" "));
-        }
-        count++;
-        if (count == 1) {
-            System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                    + "    Got it. I've added this task: \n      "
-                    + list[count - 1].substring(2)
-                    + "\n    Now you have " + count + " task in the list."
-                    + "\n    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        } else {
-            System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                    + "    Got it. I've added this task: \n      "
-                    + list[count - 1].substring(2)
-                    + "\n    Now you have " + count + " tasks in the list."
-                    + "\n    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        }
-    }
-
-    public static void doneTask(String task) {
-        task = task.trim();
-        String str = "";
-        if (task != null && !"".equals(task)) {
-            for (int i = 0; i < task.length(); i++) {
-                if (task.charAt(i) >= 48 && task.charAt(i) <= 57) {
-                    str = str + task.charAt(i);
-                }
-            }
-        }
-        for (int i = 0; i < count; i++) {
-            if (str.equals(list[i].substring(0, str.length()))) {
-                done[doneCount] = list[i].substring(str.length() + 1);
-                list[i] = list[i].replaceFirst(" ", "X");
-                done[doneCount] = done[doneCount].replaceFirst(" ", "X");
-                doneCount++;
-                printDoneTask();
-                break;
-            }
-        }
-
+    public String toString() {
+        return description;
     }
 }
---
+++ src/main/java/Todo.java
@@ -0,0 +1,12 @@
+public class Todo extends List {
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString() ;
+    }
+
+}
