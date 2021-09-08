69e077befa6a74f362f7d73b706e9d48c8fdb3ab
mohamad-adam8991 <69377976+mohamad-adam8991@users.noreply.github.com>
2021-09-09 01:11:43 +0800

    Added error messages for invalid user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,20 +4,14 @@ public class Duke {
     public static final String TEXT_SPACER = "    ";
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
+        printWelcome();
         printLineSpacer();
         System.out.println(TEXT_SPACER + "Hey, what's up!\n" + TEXT_SPACER + "What can I help you with today?");
         printLineSpacer();
 
         Task[] userLists = new Task[100];
         Scanner scanner = new Scanner(System.in);
-        String userLineInput = "";
+        String userLineInput;
         int numOfTasks = 0;
 
         do {
@@ -26,29 +20,51 @@ public class Duke {
             String userCommand = splitInputs[0];
             printLineSpacer();
             if (!userCommand.equals(("bye"))) {
-                switch (userCommand) {
-                case "list":
-                    printList(userLists, numOfTasks);
-                    break;
-                case "done":
-                    completeTask(userLists, splitInputs);
-                    break;
-                case "event":
-                case "deadline":
-                case "todo":
-                    storeTasks(userLists, userLineInput, numOfTasks, userCommand);
-                    numOfTasks++;
-                    break;
-                default:
-                    System.out.println(TEXT_SPACER + "Invalid Input!");
-                    printLineSpacer();
-                    break;
+                try {
+                    switch (userCommand) {
+                    case "list":
+                        printList(userLists, numOfTasks);
+                        break;
+                    case "done":
+                        try {
+                            completeTask(userLists, splitInputs);
+                        } catch (ArrayIndexOutOfBoundsException | NullPointerException e) {
+                            System.out.println("Please choose a valid list entry.");
+                        }
+                        break;
+                    case "event":
+                    case "deadline":
+                    case "todo":
+                        try {
+                            storeTasks(userLists, userLineInput, numOfTasks, userCommand);
+                            numOfTasks++;
+                        } catch (StringIndexOutOfBoundsException e) {
+                            System.out.println(TEXT_SPACER +
+                                    "Description of your task can't be empty. At least enter something!");
+                        }
+                        break;
+                    default:
+                        throw new DukeExceptions(TEXT_SPACER +
+                                "Sorry mate, I don't understand what you are trying to get me to do.");
+                    }
+                } catch (DukeExceptions e) {
+                    System.out.println(e.getMessage());
                 }
+                printLineSpacer();
             }
         } while(!userLineInput.equals("bye"));
 
         System.out.println(TEXT_SPACER + "Aight. See you soon mate.");
         printLineSpacer();
+    }
+
+    private static void printWelcome() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
     }
 
     private static void printList(Task[] userLists, int numOfTasks) {
@@ -60,7 +76,6 @@ public class Duke {
                 System.out.println("    " + (i + 1) + "." + userLists[i].toString());
             }
         }
-        printLineSpacer();
     }
 
     private static void completeTask(Task[] userLists, String[] splitInput) {
@@ -69,10 +84,10 @@ public class Duke {
         userLists[chosenEntry].markAsDone();
         System.out.println(TEXT_SPACER + "Good job on completing a task!");
         System.out.println(TEXT_SPACER + userLists[chosenEntry].toString());
-        printLineSpacer();
     }
 
-    private static void storeTasks(Task[] userLists, String userLineInput, int numOfTasks, String userCommand) {
+    private static void storeTasks(Task[] userLists, String userLineInput,
+                                   int numOfTasks, String userCommand) throws DukeExceptions{
         if (userCommand.equals("event")){
             storeEvent(userLists, userLineInput, numOfTasks);
         } else if(userCommand.equals("deadline")) {
@@ -81,7 +96,6 @@ public class Duke {
             storeToDo(userLists, userLineInput, numOfTasks);
         }
         System.out.println(TEXT_SPACER + "Now you have " + (numOfTasks + 1) + " tasks in your list");
-        printLineSpacer();
     }
 
     private static void storeToDo(Task[] userLists, String userLineInput, int numOfTasks) {
@@ -90,16 +104,26 @@ public class Duke {
         System.out.println(TEXT_SPACER + userLists[numOfTasks].toString());
     }
 
-    private static void storeDeadline(Task[] userLists, String userLineInput, int numOfTasks) {
+    private static void storeDeadline(Task[] userLists, String userLineInput, int numOfTasks) throws DukeExceptions{
         int infoIndex = userLineInput.indexOf("/");
+        //checks for "/by" in the user input
+        if ((infoIndex <= 9) && userLineInput.length() > 8) {
+            throw new DukeExceptions(TEXT_SPACER + "Hey, please use proper formatting." +
+                    System.lineSeparator() + TEXT_SPACER + "You should add a /by before your due date.");
+        }
         String taskDescription = userLineInput.substring(9, infoIndex);
         String dueDate = userLineInput.substring(infoIndex + 3);
         userLists[numOfTasks] = new Deadline(taskDescription, dueDate);
         System.out.println(TEXT_SPACER + userLists[numOfTasks].toString());
     }
 
-    private static void storeEvent(Task[] userLists, String userLineInput, int numOfTasks) {
+    private static void storeEvent(Task[] userLists, String userLineInput, int numOfTasks) throws DukeExceptions{
         int infoIndex = userLineInput.indexOf("/");
+        //checks for "/at" in the user input
+        if ((infoIndex <= 6) && userLineInput.length() > 5) {
+            throw new DukeExceptions(TEXT_SPACER + "Hey, please use proper formatting." +
+                    System.lineSeparator() + TEXT_SPACER + "You should add a /at before your event date");
+        }
         String taskDescription = userLineInput.substring(6, infoIndex);
         String eventTime = userLineInput.substring(infoIndex + 3);
         userLists[numOfTasks] = new Event(taskDescription, eventTime);
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,7 +8,7 @@ public class Event extends Task{
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "[E]" + super.toString() + " (at:" + eventTime + ")";
     }
 }
