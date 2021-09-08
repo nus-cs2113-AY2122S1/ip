7da26768ad288f359fb626ef7e822dd6e52cb1d1
Aayush <aayushmathur7@gmail.com>
2021-09-08 20:17:40 +0800

    Implemented Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,19 +8,6 @@ public class Duke {
     static int taskCount = 0;
     static int doneIndex = 0;
 
-    public static void echoCommandList(Task[] tasklist) {
-        System.out.println(horizontal + "Here are the tasks in your list:\n");
-        for (int i = 0; i < taskCount; i++) {
-            System.out.println("    " + (i+1) + ". [" + taskList[i].getTaskType() + "]" + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
-        }
-        System.out.println(horizontal);
-    }
-
-    public static void echoCommandDone(String line) {
-        System.out.println(horizontal + "Nice! I\'ve marked this task as done:\n" + "   [" + taskList[doneIndex-1].getTaskType() + "][X] " + line + "\n" + horizontal);
-    }
-
-
     public static void main(String[] args) {
         System.out.println(introduction);
 
@@ -38,34 +25,39 @@ public class Duke {
 
             splicedLine = line.split(" ");
             commandWord = splicedLine[0];
-
-            if (line.equals("list")) {
-                echoCommandList(taskList);
-            }
-            else if (commandWord.equals("done")) {
+            if ((commandWord.equals("done") || commandWord.equals("todo") || commandWord.equals("deadline") || commandWord.equals("event")) && splicedLine.length == 1) {
+                System.out.println(horizontal + "Oops! The description of " + commandWord + " cannot be empty!\n" + horizontal);
+            } else if (line.equals("list")) {
+                if (taskCount == 0) {
+                    System.out.println(horizontal + "You don't have any items on your list! Add something :)\n" + horizontal);
+                } else {
+                    System.out.println(horizontal + "Here are the tasks in your list:\n");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("    " + (i + 1) + ". [" + taskList[i].getTaskType() + "]" + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
+                    }
+                    System.out.println(horizontal);
+                }
+            } else if (commandWord.equals("done")) {
                 doneIndex = Integer.parseInt(splicedLine[1]);
-                echoCommandDone(taskList[doneIndex-1].getDescription());
+                line = taskList[doneIndex-1].getDescription();
+                System.out.println(horizontal + "Nice! I\'ve marked this task as done:\n" + "   [" + taskList[doneIndex-1].getTaskType() + "][X] " + line + "\n" + horizontal);
                 taskList[doneIndex-1].markAsDone();
-            }
-            else if (commandWord.equals("todo")) {
+            } else if (commandWord.equals("todo")) {
                 task = line.substring(line.indexOf(' '));
                 taskList[taskCount] = new Todo(task);
                 System.out.println(horizontal + taskList[taskCount].toString() + "Now you have " + ++taskCount + " tasks on the list.\n" + horizontal);
-            }
-            else if (commandWord.equals("deadline")) {
+            } else if (commandWord.equals("deadline")) {
                 task = line.substring(line.indexOf(' ')+1, line.indexOf('/')-1);
                 taskDate = line.substring(line.indexOf('/')+4);
                 taskList[taskCount] = new Deadline(task, taskDate);
                 System.out.println(horizontal + taskList[taskCount].toString() + "Now you have " + ++taskCount + " tasks on the list.\n" + horizontal);
-            }
-            else if (commandWord.equals("event")) {
+            } else if (commandWord.equals("event")) {
                 task = line.substring(line.indexOf(' ')+1, line.indexOf('/')-1);
                 taskDate = line.substring(line.indexOf('/')+4);
                 taskList[taskCount] = new Event(task, taskDate);
                 System.out.println(horizontal + taskList[taskCount].toString() + "Now you have " + ++taskCount + " tasks on the list.\n" + horizontal);
-            }
-            else {
-                System.out.println("Please indicate whether your task is a Todo, Deadline, or Event!");
+            } else {
+                System.out.println(horizontal + "Oops! I don't know what that means :-(\n" + horizontal);
             }
             line = in.nextLine();
         }
