cd9e1264920ec510f8ff6dcf4fcf8024639f837a
Hu Jialun <hujialun@comp.nus.edu.sg>
2021-09-09 00:09:09 +0800

    Use exceptions to handle illegal input
    Propagation of exception through the stack is utilised to provide an
    unified solution for user-friendly notification.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,28 +7,33 @@ public class Duke {
     static ArrayList<Task> taskList = new ArrayList<>();
 
 
-    private static InputHandleStatus handleOneInputLine(String line) {
+    private static boolean handleOneInputLine(String line) {
         String[] splitted = line.split("\\s+");
         switch (splitted[0]) {
             case "bye":
-                return InputHandleStatus.END;
+		return true;
             case "list":
                 for (int i = 1; i <= taskList.size(); i += 1) {
                     System.out.println(i + ": " + taskList.get(i - 1));
                 }
-                return InputHandleStatus.OK;
+		break;
             case "done":
                 Task target = taskList.get(Integer.parseInt(splitted[1]) - 1);
                 target.setDoneStatus(true);
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(target);
-                return InputHandleStatus.OK;
+		break;
             default:
-                Task newTask = TaskParser.parseTask(splitted);
+		Task newTask = null;
+		try {
+		    newTask = TaskParser.parseTask(splitted);
+		} catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+		    throw new ArrayIndexOutOfBoundsException("OOPS! Some required arguments are missing.");
+		}
                 taskList.add(newTask);
                 System.out.println("added: " + newTask);
-                return InputHandleStatus.OK;
         }
+	return false;
     }
 
     private static void initialise() {
@@ -36,10 +41,6 @@ public class Duke {
         String assist = "What can I do for you?";
         System.out.println(greeting);
         System.out.println(assist);
-    }
-
-    private enum InputHandleStatus {
-        OK, END, ERROR
     }
 
     private static void finalise() {
@@ -51,12 +52,15 @@ public class Duke {
         initialise();
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
-            switch (handleOneInputLine(sc.nextLine())) {
-            case END:
-                finalise();
-                return;
-            }
-        }
+	    try {
+                if (handleOneInputLine(sc.nextLine())) {
+                    finalise();
+		    return;
+                }
+            } catch (Exception e) {
+		System.out.println(e.getMessage());
+	    }
+	}
         finalise();
     }
 }
