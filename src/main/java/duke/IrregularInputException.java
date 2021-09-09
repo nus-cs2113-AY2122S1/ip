75b9441bfd4f48e936d0b5e6ca3436a1d488acd6
Lily Peng <c200218@e.ntu.edu.sg>
2021-09-10 03:50:27 +0800

    delete useless exception class

--- src/main/java/duke/IrregularInputException.java
+++
@@ -1,7 +0,0 @@
-package duke;
-
-public class IrregularInputException extends Exception{
-    public String getMessage() {
-        return "OOPS!!! I'm sorry, but I don't know what that means :-(";
-    }
-}
