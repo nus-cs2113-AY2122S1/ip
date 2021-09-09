620607d212c427e605757ee9cf6a88b6da177d1e
Zhang Haoyu <haoyusimon927@gmail.com>
2021-09-09 11:38:47 +0800

    A-Packages
    Organised the classes into suitable

--- src/main/java/duke/command/Event.java
+++ src/main/java/duke/command/Event.java
@@ -1,3 +1,7 @@
+package duke.command;
+
+import duke.task.Task;
+
 public class Event extends Task {
     protected String time;
     public Event(String content, String time) {
--- src/main/java/duke/command/ToDo.java
+++ src/main/java/duke/command/ToDo.java
@@ -1,3 +1,7 @@
+package duke.command;
+
+import duke.task.Task;
+
 public class ToDo extends Task {
     public ToDo(String content) {
         super(content);
