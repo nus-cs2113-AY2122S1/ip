b78c057248103bc51e234778a78d36cb5ad9930f
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-09-09 16:14:10 +0800

    Level-5
    Added more Exception handling for the thrown exception.

--- src/main/java/duke/exceptionHandler/DukeException.java
+++ src/main/java/duke/exceptionHandler/DukeException.java
@@ -1,1 +1,31 @@
+package duke.exceptionHandler;
 
+/**
+ * This is the DukeException Handler and is only targeted at Duke.java class.
+ *
+ * @author YEOWEIHNGWHYELAB
+ */
+
+public class DukeException extends Exception {
+    public static String openingString = "     ☹ OOPS!!! ";
+
+    /**
+     * This calls the superclass Exception constructor and creates the exception
+     * object with the error message.
+     *
+     * @param errorMessage The error message string.
+     */
+    public DukeException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    /**
+     * Prints the error message in a formatted fashion by cocatenation.
+     */
+    public void printErrorMessage() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println(openingString + this.getMessage());
+        System.out.println("    ____________________________________________________________");
+    }
+
+}
