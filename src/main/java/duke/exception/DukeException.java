3a407eba05e4e8971a1f8752682b398b9bd57191
rondayvoo <69455258+rondayvoo@users.noreply.github.com>
2021-09-11 03:08:40 +0800

    Add delete functionality

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -3,8 +3,10 @@ package duke.exception;
 public class DukeException extends Exception {
     public static final String TASK_ALREADY_DONE = "Task already completed.";
     public static final String TASK_ARRAY_FULL = "No more memory to store tasks.";
-    public static final String TASK_INDEX_OOB = "Index of task outside range.";
-    public static final String TASK_INDEX_INVALID = "Index of task is not a valid integer.";
+
+    public static final String INDEX_OOB = "Index outside range.";
+    public static final String INDEX_INVALID = "Index is not a valid integer.";
+
     public static final String COMMAND_INVALID = "Command given cannot be recognized.";
 
     public static final String TODO_BLANK_DESC = "Description of todo is blank.";
