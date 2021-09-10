645d0154669ed271f489611f413ee2cf0d5e2ce5
Joshua Harsha Dass <joshua.h.dass@gmail.com>
2021-09-10 17:43:50 +0800

    Replace List with ArrayList Add delete task function

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,11 +1,7 @@
 package duke;
 
-import duke.Deadlines;
-import duke.Events;
-import duke.Task;
-import duke.ToDos;
-
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
@@ -14,38 +10,54 @@ public class Duke {
         System.out.println("What can I do for you?");
     }
 
-    public static void printList(Task[] list, int size) {
+    public static void printList(ArrayList<Task> list, int size) {
         int position = 1;
         System.out.println("    Here are the tasks in your list:");
         for( int i = 0 ; i < size ; i ++ ) {
-            System.out.println( "    " + position  + "." + list[i]);
+            System.out.println( "    " + position  + "." + list.get(i));
             position ++;
         }
     }
 
-    public static void markTaskDone(Task[] list , String word) {
+    public static void markTaskDone(ArrayList<Task> list , String word) {
         //convert string number to int number
         int position = Integer.parseInt(word);
         //if task does not exist, return
-        if (position > list.length) {
+        if (position > list.size()) {
             System.out.println("    Sorry! No such task!");
             return;
         }
         //if task exist, mark task as done
-        Task task = list[position - 1];
+        Task task = list.get(position - 1);
         task.setDone();
         //print notification
         System.out.println("    Nice! I've marked this task as done:");
         System.out.println("    " + task);
     }
 
-    public static void addTask(Task[] list, Task newTask, int size) {
+    public static void addTask(ArrayList<Task> list, Task newTask, int size) {
         //add task to list
-        list[size] = newTask;
+        list.add(newTask);
         //print statements
         System.out.println("    Got it. I've added this task:");
-        System.out.println("      " + list[size]);
+        System.out.println("      " + list.get(size));
         System.out.println("    Now you have " + (size + 1) + " tasks in the list.");
+    }
+
+    public static void deleteTask(ArrayList<Task> list, String word, int size ) {
+        //convert string number to int number
+        int position = Integer.parseInt(word);
+        //if task does not exist, return
+        if (position > list.size()) {
+            System.out.println("    Sorry! No such task!");
+            return;
+        }
+        Task task = list.get(position - 1);
+        list.remove(position - 1);
+        //print statements
+        System.out.println("    Noted. I've removed this task:");
+        System.out.println("      " + task);
+        System.out.println("    Now you have " + (size - 1) + " tasks in the list.");
     }
 
     public static Deadlines createNewDeadline(String[] words) {
@@ -82,7 +94,7 @@ public class Duke {
         return newEvent;
     }
 
-    public static int distinguishCommand(String command , Task[] list, int size) {
+    public static int distinguishCommand(String command , ArrayList<Task> list, int size) {
         //split into word array
         String[] words = command.split(" ");
         String firstWord = words[0];
@@ -128,6 +140,14 @@ public class Duke {
                 System.out.println("    OOPS!!! The description of a event cannot be empty.");
             }
             break;
+        case "delete":
+            try {
+                deleteTask(list, words[1], size);
+                taskCount --;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("    OOPS!!! The description of a event cannot be empty.");
+            }
+            break;
         default:
                 System.out.println("    OOPS!!! I'm sorry, but I don't know what that means :-(");
 
@@ -146,7 +166,7 @@ public class Duke {
         showWelcomeMessage();
         line = in.nextLine();
 
-        Task[] list = new Task[100];
+        ArrayList<Task> list = new ArrayList<>();
         int taskCount = 0;
 
         while (!line.equals("bye")) {
