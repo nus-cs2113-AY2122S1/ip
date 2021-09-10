7879e849f4bf1a1a78723c9abbaf1374586b6f9d
mohamad-adam8991 <69377976+mohamad-adam8991@users.noreply.github.com>
2021-09-10 20:50:06 +0800

    Added error messages for invalid user inputs

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,23 +2,22 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
-    public Task(String description){
+    public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
-    public String getStatusIcon(){
+    public String getStatusIcon() {
         return (isDone ? "[X] " : "[ ] ");
     }
 
-    public void markAsDone(){
+    public void markAsDone() {
         this.isDone = true;
     }
 
-    public String toString(){
+    public String toString() {
         return getStatusIcon() + this.description;
     }
 }
-
 
 
