c61a0ec0856ef19d6713777b9ff58e378e0c8ae6
Mohamed Irfan <irfancuber@gmail.com>
2021-09-10 21:01:58 +0800

    Restructure code to use Collection class ArrayList instead of normal arrays

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,6 +6,7 @@ import duke.task.Task;
 import duke.task.Todo;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static final String LINE = "____________________________________________________________";
@@ -15,8 +16,7 @@ public class Duke {
     private static final String DEADLINE_ERROR = "The description of a deadline cannot be empty and must have a '/by'.";
     private static final String EVENT_ERROR = "The description of an event cannot be empty and must have a '/at'.";
     private static final String[] taskTypes = {"todo", "deadline", "event"};
-    private static final int MAX_TASKS = 100;
-    private static Task[] tasks = new Task[MAX_TASKS];
+    private static ArrayList<Task> tasks = new ArrayList<Task>();
 
     public static void main(String[] args) {
         showHelloGreeting();
@@ -26,41 +26,37 @@ public class Duke {
 
     private static void executeResponses() {
         Scanner in = new Scanner(System.in);
-        int index = 0;
         String text;
         text = in.nextLine();
         while (!text.equals("bye")) {
             System.out.println(LINE);
             String[] words = text.split(" ");
-
+            int numOfTasks = tasks.size();
             try {
                 switch (words[0]) {
                 case "list":
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < index; i++) {
-                        System.out.println((i + 1) + "." + tasks[i].toString());
+                    for (int i = 0; i < numOfTasks; i++) {
+                        System.out.println((i + 1) + "." + tasks.get(i).toString());
                     }
                     break;
                 case "done":
                     int taskNum = Integer.parseInt(words[words.length - 1]);
-                    tasks[taskNum - 1].setDone();
-                    System.out.println("Nice! I've marked this duke.task as done:");
-                    System.out.println("[x] " + tasks[taskNum - 1].getDescription());
+                    tasks.get(taskNum - 1).setDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[x] " + tasks.get(taskNum - 1).getDescription());
                     break;
                 case "todo":
-                    addTodo(index, text);
-                    index++;
-                    System.out.println("Now you have " + index + " tasks in the list.");
+                    addTodo(text);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     break;
                 case "deadline":
-                    addDeadline(index, text);
-                    index++;
-                    System.out.println("Now you have " + index + " tasks in the list.");
+                    addDeadline(text);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     break;
                 case "event":
-                    addEvent(index, text);
-                    index++;
-                    System.out.println("Now you have " + index + " tasks in the list.");
+                    addEvent(text);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     break;
                 default:
                     showErrorMessage();
@@ -74,16 +70,17 @@ public class Duke {
         }
     }
 
-    private static void addTodo(int index, String text) throws DukeException {
+    private static void addTodo(String text) throws DukeException {
         if (text.length() <= taskTypes[0].length()) {
             throw new DukeException(TODO_ERROR);
         }
         String[] todoTaskInfo = extractInfo(text, "todo");
-        tasks[index] = new Todo(todoTaskInfo[0]);
+        Task newTodo = new Todo(todoTaskInfo[0]);
+        tasks.add(newTodo);
         System.out.println(ADD_TASK_MSG);
     }
 
-    private static void addDeadline(int index, String text) throws DukeException {
+    private static void addDeadline(String text) throws DukeException {
         if (text.length() <= taskTypes[1].length()) {
             throw new DukeException(DEADLINE_ERROR);
         }
@@ -92,11 +89,12 @@ public class Duke {
             throw new DukeException(DEADLINE_ERROR);
         }
         String[] deadlineTaskInfo = extractInfo(text, "deadline");
-        tasks[index] = new Deadline(deadlineTaskInfo[0], deadlineTaskInfo[1]);
+        Task newDeadline = new Deadline(deadlineTaskInfo[0], deadlineTaskInfo[1]);
+        tasks.add(newDeadline);
         System.out.println(ADD_TASK_MSG);
     }
 
-    private static void addEvent(int index, String text) throws DukeException {
+    private static void addEvent(String text) throws DukeException {
         if (text.length() <= taskTypes[2].length()) {
             throw new DukeException(EVENT_ERROR);
         }
@@ -105,7 +103,8 @@ public class Duke {
             throw new DukeException(EVENT_ERROR);
         }
         String[] eventTaskInfo = extractInfo(text, "event");
-        tasks[index] = new Event(eventTaskInfo[0], eventTaskInfo[1]);
+        Task newEvent = new Event(eventTaskInfo[0], eventTaskInfo[1]);
+        tasks.add(newEvent);
         System.out.println(ADD_TASK_MSG);
     }
 
