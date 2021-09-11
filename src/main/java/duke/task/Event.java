786fab8b9297afa98c1cfbd1b7174f9ea68bbe9e
Justin Wong <justinfidelis@gmail.com>
2021-09-11 12:01:59 +0800

    Fix incorrect print message for events

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -10,6 +10,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " " + "(by: " + atDate + ")";
+        return "[E]" + super.toString() + " " + "(at: " + atDate + ")";
     }
 }
