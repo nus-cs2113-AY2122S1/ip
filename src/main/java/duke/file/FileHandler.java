08b26ee027588903530751d2d318e982b050ec07
Woolicious98 <e527269@u.nus.edu>
2021-09-12 20:03:29 +0800

    Reformat class FileHandler to java coding standard

--- src/main/java/duke/file/FileHandler.java
+++ src/main/java/duke/file/FileHandler.java
@@ -28,18 +28,18 @@ public class FileHandler {
     /**
      * Pass inputs from file to task manager to process into the tasks list.
      *
-     * @param fileName Given file name that contains the many tasks information.
+     * @param fileName    Given file name that contains the many tasks information.
      * @param taskManager TaskManager that handles any task related operation.
      */
     public void loadToTaskManager(String fileName, TaskManager taskManager) {
         File f = new File(fileDirectory, fileName);
-        try{
+        try {
             Scanner s = new Scanner(f);
-            while(s.hasNext()){
+            while (s.hasNext()) {
                 taskManager.addTaskFromFile(s.nextLine());
             }
-        }catch(FileNotFoundException e){
-            System.out.printf("Notice: File %s not found.\n",fileName);
+        } catch (FileNotFoundException e) {
+            System.out.printf("Notice: File %s not found.\n", fileName);
             return;
         }
     }
