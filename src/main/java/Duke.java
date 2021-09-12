9735e335e53c0ef3c47415f9615eb978551ab549
Ho Zhen Hong <hozhenhong99@gmail.com>
2021-09-13 04:42:58 +0800

    implement level-7, save

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 
 public class Duke {
 
+    private static final String FILEPATH = "C:\\Users\\User\\Documents\\NUS\\CS2113T\\IP\\ip\\data\\duke.txt";
     private static final int TODO_NAME_CONSTANT = 5;
     private static final int DEADLINE_NAME_CONSTANT = 9;
     private static final int DEADLINE_BY_CONSTANT = 5;
@@ -11,6 +17,89 @@ public class Duke {
     public static final String LINE = "____________________________________________________________";
 
     private static ArrayList<Task> list = new ArrayList<Task>();
+
+    public static void initList() {
+        String filePath = FILEPATH;
+        try {
+            File file = new File(filePath);
+            Scanner s = new Scanner(file); // create a Scanner using the File as the source
+            while (s.hasNext()) {
+                String input = s.nextLine();
+                String[] inputWords = input.split(" / ");
+                switch(inputWords[0]) {
+                case "T":
+                    list.add(new Todo(inputWords[2]));
+                    if (inputWords[1].equals("1")) {
+                        list.get(list.size()-1).markAsDone();
+                    }
+                    break;
+                case "D":
+                    list.add(new Deadline(inputWords[2], inputWords[3]));
+                    if (inputWords[1].equals("1")) {
+                        list.get(list.size()-1).markAsDone();
+                    }
+                    break;
+                case "E":
+                    list.add(new Event(inputWords[2], inputWords[3]));
+                    if (inputWords[1].equals("1")) {
+                        list.get(list.size()-1).markAsDone();
+                    }
+                    break;
+                default:
+                    throw new DukeException();
+                }
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(LINE + "\nFile not found\n" + LINE);
+        } catch (DukeException e) {
+            System.out.println(LINE + "\nInput format within file is wrong!\n" + LINE);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(LINE + "\nInput format within file is wrong!\n" + LINE);
+        }
+    }
+
+    private static void appendToFile(String textToAppend) throws IOException {
+        String filePath = FILEPATH;
+        File file = new File(FILEPATH);
+        if (file.length() != 0) {
+            textToAppend = System.lineSeparator() + textToAppend;
+        }
+        FileWriter fw = new FileWriter(filePath, true); // create a FileWriter in append mode
+        fw.write(textToAppend);
+        fw.close();
+    }
+
+    private static void updateFile() {
+        try {
+            clearFile();
+            for (int i = 0; i < list.size(); i += 1) {
+                String textToAppend = "";
+                if (list.get(i) instanceof Todo) {
+                    String isDone = list.get(i).isDone() ? "1" : "0";
+                    textToAppend = "T / " + isDone + " / " + list.get(i).getName();
+                } else if (list.get(i) instanceof Deadline) {
+                    Deadline deadline = (Deadline) list.get(i);
+                    String isDone = deadline.isDone() ? "1" : "0";
+                    textToAppend = "D / " + isDone + " / " + deadline.getName() + " / " + deadline.getDeadline();
+                } else if (list.get(i) instanceof Event) {
+                Event event = (Event) list.get(i);
+                String isDone = event.isDone() ? "1" : "0";
+                textToAppend = "E / " + isDone + " / " + event.getName() + " / " + event.getAt();
+                }
+                appendToFile(textToAppend);
+            }
+        } catch(IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+    }
+
+    private static void clearFile() throws IOException {
+        String filePath = FILEPATH;
+        FileWriter fw = new FileWriter(filePath);
+        fw.write("");
+        fw.close();
+    }
 
     public static void printList() {
         System.out.println(LINE);
@@ -31,6 +120,7 @@ public class Duke {
                     System.out.println(LINE + "\nThat task is already done!\n" + LINE);
                 } else {
                     list.get(taskIndex).markAsDone();
+                    updateFile();
                     System.out.println(LINE + "\nNice! I've marked this task as done:");
                     System.out.println(list.get(taskIndex) + "\n" + LINE);
                 }
@@ -51,6 +141,7 @@ public class Duke {
                 System.out.format(LINE + "\nNoted. I've removed this task: \n   " + list.get(taskIndex) 
                 + "\nNow you have %d tasks in the list.\n" + LINE + "\n", list.size()-1);
                 list.remove(taskIndex);
+                updateFile();
             } else {
                 System.out.println("That task does not exist!\n" + LINE);
             }
@@ -67,17 +158,20 @@ public class Duke {
         System.out.format("Now you have %d tasks in the list.\n" + LINE + "\n", list.size());
     }
 
-    public static void addTodo(String input, String[] inputWords) {
+    public static void addTodo(String input) {
         try {
             String taskName = input.substring(TODO_NAME_CONSTANT);
             list.add(new Todo(taskName));
+            appendToFile("T / 0 / " + taskName);
             printAddedTask();
         } catch(StringIndexOutOfBoundsException e) {
             System.out.println(LINE + "\n☹ OOPS!!! The description of a todo cannot be empty.\n" + LINE);
+        } catch(IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
         }
     }
 
-    public static void addDeadline(String input, String[] inputWords) {
+    public static void addDeadline(String input) {
         try {
             if (!input.contains("/by")) {
                 System.out.println(LINE + "\nIncorrect format for entering deadline!\n" + LINE);
@@ -87,13 +181,16 @@ public class Duke {
             String taskName = input.substring(DEADLINE_NAME_CONSTANT, taskEndIndex);
             String deadline = input.substring(taskEndIndex + DEADLINE_BY_CONSTANT);
             list.add(new Deadline(taskName, deadline));
+            appendToFile("D / 0 / " + taskName + " / " + deadline);
             printAddedTask();
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println(LINE + "\n☹ OOPS!!! The description of a deadline cannot be empty.\n" + LINE);
+        } catch(IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
         }
     }
 
-    public static void addEvent(String input, String[] inputWords) {
+    public static void addEvent(String input) {
         try {
             if (!input.contains("/at")) {
                 System.out.println(LINE + "\nIncorrect format for entering event!\n" + LINE);
@@ -103,9 +200,12 @@ public class Duke {
             String taskName = input.substring(EVENT_NAME_CONSTANT, taskEndIndex);
             String at = input.substring(taskEndIndex + EVENT_AT_CONSTANT);
             list.add(new Event(taskName, at));
+            appendToFile("E / 0 / " + taskName + " / " + at);
             printAddedTask();
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println(LINE + "\n☹ OOPS!!! The description of an event cannot be empty.\n" + LINE);
+        } catch(IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
         }
     }
 
@@ -114,13 +214,13 @@ public class Duke {
         try {
             switch(taskType) {
             case "todo":
-                addTodo(input, inputWords);
+                addTodo(input);
                 break;
             case "deadline":
-                addDeadline(input, inputWords);
+                addDeadline(input);
                 break;
             case "event":
-                addEvent(input, inputWords);
+                addEvent(input);
                 break;
             default:
                 throw new DukeException();
@@ -133,6 +233,7 @@ public class Duke {
     public static void main(String[] args) { 
         System.out.println(LINE + "\n Hello! I'm Duke\n What can I do for you?\n" + LINE);
         Boolean isCompleted = false;
+        initList();
         while (!isCompleted) {
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
