770364bc179971c87828559652f39f093706befb
powzx <69313768+powzx@users.noreply.github.com>
2021-09-12 16:53:12 +0800

    Refactor TaskManager

--- src/main/java/duke/task/TaskManager.java
+++ src/main/java/duke/task/TaskManager.java
@@ -7,9 +7,8 @@ import java.util.ArrayList;
 public class TaskManager {
 
     private static final ExceptionChecker CHECKER = new ExceptionChecker();
+    private static final ArrayList<Task> TASKS = new ArrayList<>();
     private static int currentTasksCount = 0;
-
-    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static boolean isValidTaskNumber(int taskNumber) {
         return (taskNumber <= currentTasksCount && taskNumber > 0);
@@ -20,43 +19,43 @@ public class TaskManager {
     }
 
     public void clearAllTasks() {
-        tasks.clear();
+        TASKS.clear();
         currentTasksCount = 0;
     }
 
     public Task addTodo(String taskDescription) {
-        tasks.add(new ToDo(taskDescription));
+        TASKS.add(new ToDo(taskDescription));
         currentTasksCount++;
-        return tasks.get(currentTasksCount - 1);
+        return TASKS.get(currentTasksCount - 1);
     }
 
     public Task addDeadline(String taskDescription, String taskDue) {
-        tasks.add(new Deadline(taskDescription, taskDue));
+        TASKS.add(new Deadline(taskDescription, taskDue));
         currentTasksCount++;
-        return tasks.get(currentTasksCount - 1);
+        return TASKS.get(currentTasksCount - 1);
     }
 
     public Task addEvent(String taskDescription, String eventDateTime) {
-        tasks.add(new Event(taskDescription, eventDateTime));
+        TASKS.add(new Event(taskDescription, eventDateTime));
         currentTasksCount++;
-        return tasks.get(currentTasksCount - 1);
+        return TASKS.get(currentTasksCount - 1);
     }
 
     public void listTasks() {
         for (int i = 0; i < currentTasksCount; i++) {
-            System.out.println((i + 1) + ". " + tasks.get(i).toString());
+            System.out.println((i + 1) + ". " + TASKS.get(i).toString());
         }
     }
 
     public Task deleteTask(int taskIndex) {
-        Task deletedTask = tasks.get(taskIndex - 1);
-        tasks.remove(taskIndex - 1);
+        Task deletedTask = TASKS.get(taskIndex - 1);
+        TASKS.remove(taskIndex - 1);
         currentTasksCount--;
         return deletedTask;
     }
 
     public Task markTaskDone(int taskIndex) {
-        Task doneTask = tasks.get(taskIndex - 1);
+        Task doneTask = TASKS.get(taskIndex - 1);
         doneTask.setDone();
         return doneTask;
     }
@@ -66,7 +65,7 @@ public class TaskManager {
         ArrayList<String> fileLines = new ArrayList<>();
 
         for (int i = 0; i < currentTasksCount; i++) {
-            fileLines.add(tasks.get(i).toData() + "\n");
+            fileLines.add(TASKS.get(i).toData() + "\n");
         }
 
         CHECKER.tryToWriteFile(fileLines);
