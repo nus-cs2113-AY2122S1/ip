46e6c2aa45f8ed11a5f5b9d52b4f643a8a094bfb
conradwee <conradwee@gmail.com>
2021-09-12 19:40:42 +0800

    week4draft1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,81 +1,25 @@
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
-
-    private static final int LINE_WIDTH = 60;
-
-    //to draw a straight line
-    private static void printHorizontalLine() {
-        System.out.println("_".repeat(LINE_WIDTH));
-    }
-
-    public static String[] taskLister(String[] todos) {
-
-        int todoCount = 0;
-        String[] result = new String[100];
-        for (String todo : todos) {
-            //System.out.println(word);
-            if (todo != null) {
-                result[todoCount] = todo;
-                todoCount++;
-            } else {
-                break;
-
-            }
-        }
-        return Arrays.copyOf(result, todoCount);
-    }
 
     public static boolean splitString(String line) {
         String[] words = line.split(" ");
         for (String word : words) {
             if (word.equals("done")) {
                 {
-
                     return words[1].matches(".*\\d.*");
-
                 }
-
             } else return false;
         }
-
         return false;
     }
 
-
-    public static String[] checkMarker(String[] tobeCheckeds, int doneNumber, int listedcount) {
-        String[] checkedMarks = new String[tobeCheckeds.length];
-
-        for (int i =0; i<listedcount; i++ ) {
-            if (tobeCheckeds[i]== null ) { //&& !tobeCheckeds[i].equals("X")
-                checkedMarks[i] = " ";
-            } else
-                {
-                    checkedMarks[i] = tobeCheckeds[i];
-                }
-            }
-
-        checkedMarks[doneNumber] = "X";
-        return checkedMarks;
-    }
-
-
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        //Level0
-        printHorizontalLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println();
-        printHorizontalLine();
+        choose.printHorizontalLine();
         String line = "";
         Scanner in = new Scanner(System.in);
 
@@ -95,41 +39,37 @@ public class Duke {
                 int doneNumber = 0; //no choice have to make 0 if not ide wont let me run
                 String[] findNumbers = line.split(" ");
                 doneNumber = Integer.parseInt(findNumbers[1]);
-                checkMarked = checkMarker(tobeCheckeds, doneNumber, counter+1);
-                String[] listeds = taskLister(todos);
+                checkMarked = choose.checkMarker(tobeCheckeds, doneNumber, counter + 1);
+                String[] listeds = choose.taskLister(todos);
                 System.out.println("Here are the tasks in your list:");
                 for (String listed : listeds) {
-                    System.out.println(listedcount + "." + "[" + checkMarked[listedcount] + "]" + " " + listed);
+                    System.out.println(listedcount + "." + "[" + checkMarked[listedcount] + "]" + "[" + checkMarked[listedcount] + "]" + " " + listed);
                     listedcount++;
                 }
-                tobeCheckeds=checkMarked;
-                printHorizontalLine();
+                tobeCheckeds = checkMarked;
+                choose.printHorizontalLine();
             } else if (line.equals("list")) {
-                String[] listeds = taskLister(todos);
-                int listedcount = 1;
-                for (String listed : listeds) {
-                    System.out.println("Here are the tasks in your list:");
-                    System.out.println(listedcount + "." + "[" + checkMarked[listedcount] + "]" + " " + listed);
-                    listedcount++;
-                }
-                printHorizontalLine();
-            } else if (!line.equals("bye")) {
-                printHorizontalLine();
-                System.out.println("added: " + line);
-                printHorizontalLine();
-                todos[counter] = line; //save text input to array
-                counter++;  // prepare to take in the next array item
-
+                //String[] listeds = taskLister(todos);
+                choose.list();
             } else {
-                printHorizontalLine();
+                choose.printHorizontalLine();
+                System.out.println("added: " + line);
+                choose.printHorizontalLine();
+                choose.addTask(line);
+//                todos[counter] = line; //save text input to array
+//                counter++;  // prepare to take in the next array item
+
             }
+            choose.printHorizontalLine();
+
         }
 
 
         //printHorizontalLine();
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println();
-        printHorizontalLine();
+        choose.printHorizontalLine();
 
     }
 }
+
