d3262d71ae9c25e1a03b5f8985250e8bc18f0c43
conradwee <conradwee@gmail.com>
2021-09-12 20:13:23 +0800

    Week4 Coding Quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,20 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
-
-    public static boolean splitString(String line) {
-        String[] words = line.split(" ");
-        for (String word : words) {
-            if (word.equals("done")) {
-                {
-                    return words[1].matches(".*\\d.*");
-                }
-            } else return false;
-        }
-        return false;
-    }
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println();
@@ -23,47 +10,30 @@ public class Duke {
         String line = "";
         Scanner in = new Scanner(System.in);
 
-        //initialise the 100 todos items array
-//        String[] todos = new String[100];
-//        String[] checkMarked = new String[100];
-//        Arrays.fill(checkMarked, " ");
-//        String[] tobeCheckeds = new String[100];
-
-        int counter = 0;
-
         while (!(line.equals("bye") || line.equals("Bye"))) {
             line = in.nextLine();
             line = line.toLowerCase();
 
             if (line.equals("list")) {
-                //String[] listeds = taskLister(todos);
                 choose.list();
             } else if((line.split(" ")[0].equals("done"))){
                 int doneNumber= Integer.parseInt((line.split(" ")[1]));
                 choose.setDone(doneNumber);
-            } else if (line.split(" ")[0].equals("deadline")) {
+            } else if (line.split(" ")[0].equals("deadline") && line.contains("/by")) {
                 choose.setDeadline(line.split("deadline ")[1], line.split("/by")[1]);
-            }else if (line.split(" ")[0].equals("event")) {
+            }else if (line.split(" ")[0].equals("event")&& line.contains("/at")) {
                 choose.setEvent(line.split("event ")[1], line.split("/at")[1]);
             }else if ((line.split(" ")[0].equals("todo"))){
                 choose.setTodo(line.split("todo ")[1]);
             }
-
-
-
-
             else {
                 choose.printHorizontalLine();
                 System.out.println("added: " + line);
                 choose.addTask(line);
-//                todos[counter] = line; //save text input to array
-//                counter++;  // prepare to take in the next array item(
             }
             choose.printHorizontalLine();
 
         }
-
-
         //printHorizontalLine();
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println();
--- src/main/java/choose.java
+++ src/main/java/choose.java
@@ -5,7 +5,7 @@ public class choose {
     private static int tasksAdded = 0;
     private static final int LINE_WIDTH = 60;
     private static Task[] List = new Task[100];
-    private static int listSize=0;
+    private static int listSize = 0;
 
 
     public static void printHorizontalLine() {
@@ -14,121 +14,50 @@ public class choose {
 
     public static void addTask(String line) {
         List[listSize] = new Task(line);
-       // listlast();
         listSize++;
     }
 
     public static void list() {
         for (int i = 0; i < listSize; i++) {
-            System.out.println(i+1 + "." + List[i]);
+            System.out.println(i + 1 + "." + List[i]);
         }
     }
+
     public static void listLast() {
-            System.out.println(List[listSize]);
+        System.out.println(List[listSize]);
     }
 
-    public static void setDone(int doneNumber){
-        List[doneNumber-1].setDone("X");
+    public static void setDone(int doneNumber) {
+        List[doneNumber - 1].setDone("X");
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println(List[doneNumber-1]);
+        System.out.println(List[doneNumber - 1]);
     }
 
-    public static void setDeadline(String item, String timing){
+    public static void setDeadline(String item, String timing) {
         List[listSize] = new Deadline(item, timing);
         System.out.println("Got it. I've added this task:");
         listLast();
         listSize++;
-        System.out.println(" Now you have "+ listSize +" tasks in the list.");
+        System.out.println(" Now you have " + listSize + " tasks in the list.");
     }
-    public static void setEvent(String item, String timing){
+
+    public static void setEvent(String item, String timing) {
         List[listSize] = new Event(item, timing);
         System.out.println("Got it. I've added this task:");
         listLast();
         listSize++;
-        System.out.println(" Now you have "+ listSize +" tasks in the list.");
+        System.out.println(" Now you have " + listSize + " tasks in the list.");
     }
-    public static void setTodo(String item){
+
+    public static void setTodo(String item) {
         List[listSize] = new Todo(item);
         System.out.println("Got it. I've added this task:");
         listLast();
         listSize++;
-        System.out.println(" Now you have "+ listSize +" tasks in the list.");
-    }
-
-//    public static void checkDoneTask(int taskNumber) {
-//        int taskIndex = taskNumber - 1;
-//        list[taskIndex].markAsDone();
-//    }
-
-
-//    public static void printWelcomeMessage() {
-//        printHorizontalLine();
-//        System.out.println("     Hello! I'm Duke");
-//        System.out.println("     What can I do for you?");
-//        printHorizontalLine();
-//    }
-//
-//
-//    public static void printGoodbyeMessage() {
-//        printHorizontalLine();
-//        System.out.println("     Bye. Hope to see you again soon!");
-//        printHorizontalLine();
-//    }
-
-
-    public static void printList() {
-
-        printHorizontalLine();
-        if (tasksAdded != 0) {
-            System.out.println("     Here are the tasks in your list:");
-            for (int i = 0; i < tasksAdded; i++) {
-                int numbering = i + 1;
-                // need to replace this with to string
-                System.out.println("     " + numbering + ". " + list[i]);
-            }
-        } else {
-            System.out.println("     You have no tasks in the list at the moment. Please add a new task to begin.");
-        }
-        printHorizontalLine();
-    }
-
-    public static void printAddNewTask(Task newTask) {
-        printHorizontalLine();
-        System.out.println("     " + newTask);
-        System.out.println("     Now you have " + tasksAdded + " tasks in the list.");
-        printHorizontalLine();
-    }
-
-
-
-    public static String[] taskLister(String[] todos) {
-        int todoCount = 0;
-        String[] result = new String[100];
-        for (String todo : todos) {
-            //System.out.println(word);
-            if (todo != null) {
-                result[todoCount] = todo;
-                todoCount++;
-            } else {
-                break;
-            }
-        }
-        return Arrays.copyOf(result, todoCount);
-    }
-
-    public static String[] checkMarker(String[] tobeCheckeds, int doneNumber, int listedcount) {
-        String[] checkedMarks = new String[tobeCheckeds.length];
-        for (int i = 0; i < listedcount; i++) {
-            if (tobeCheckeds[i] == null) { //&& !tobeCheckeds[i].equals("X")
-                checkedMarks[i] = " ";
-            } else {
-                checkedMarks[i] = tobeCheckeds[i];
-            }
-        }
-        checkedMarks[doneNumber] = "X";
-        return checkedMarks;
+        System.out.println(" Now you have " + listSize + " tasks in the list.");
     }
 }
+
 
 
 
