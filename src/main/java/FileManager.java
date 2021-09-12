775d35030f61cf21d8d401a6c9dd7bf5272da463
CrimsonTitan79 <CrimsonTitan79>
2021-09-12 13:27:31 +0800

    Update FileManager.initializeFile() to be more succinct, Reformat code

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -1,46 +1,35 @@
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.FileNotFoundException;
-
-import java.nio.file.Files;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import java.util.Scanner;
 
 public class FileManager {
     private static final String home = System.getProperty("user.home");
     private static final Path dukeDirPath = Paths.get(home, "Duke");
-    private static final Path dataPath = Paths.get(home, "Duke/data.txt");
-
     private static final File dukeDir = new File(dukeDirPath.toString());
+    private static final Path dataPath = Paths.get(home, "Duke/data.txt");
     private static final File data = new File(dataPath.toString());
 
     public static void initializeFile() throws IOException {
+        //dukeDir directory did not exist
+        //create Duke directory
+        if (dukeDir.mkdir()) {
+            data.createNewFile();
 
-        if (dukeDir.exists()) {
-            if (data.exists()) {
-                //TODO read from Duke/data
+        } else { //dukeDir directory exists
+            //create Duke/data.txt if does not exist
+            // run the if statement if Duke/data.txt exists already
+            if (!data.createNewFile()) {
                 try {
                     readFile();
-
                 } catch (FileNotFoundException e) {
-                    System.out.println("$HOME/Duke/data.txt not found!!!");
-
+                    System.out.println(home + "/Duke/data.txt not found!!!");
                 }
                 Duke.taskManager.listTasks();
-
-
-            } else {
-                data.createNewFile();
-
             }
-        } else {
-            dukeDir.mkdir();
-            data.createNewFile();
         }
-        
     }
 
     private static void readFile() throws FileNotFoundException {
@@ -61,33 +50,30 @@ public class FileManager {
         String taskLetter = taskDetails[0];
 
         String isDoneString = taskDetails[1];
-        Integer isDoneInteger = Integer.parseInt(isDoneString);
-        Boolean isDone = (isDoneInteger.intValue() == 1) ? true : false; //TODO how to account for errors in textfile... beside 1, 0
+        int isDoneInt = Integer.parseInt(isDoneString);
+        boolean isDone = (isDoneInt == 1); //TODO how to account for errors in textfile... beside 1, 0
 
         String description = taskDetails[2];
         String date;
 
         switch (taskLetter) {
-            case "T":
-                Duke.taskManager.addTodoFromFile(description, isDone);
-                break;
+        case "T":
+            Duke.taskManager.addTodoFromFile(description, isDone);
+            break;
 
-            case "D":
-                date = taskDetails[3];
-                Duke.taskManager.addDeadlineFromFile(description, date, isDone);
-                break;
+        case "D":
+            date = taskDetails[3];
+            Duke.taskManager.addDeadlineFromFile(description, date, isDone);
+            break;
 
-            case "E":
-                date = taskDetails[3];
-                Duke.taskManager.addEventFromFile(description, date, isDone);
-                break;
+        case "E":
+            date = taskDetails[3];
+            Duke.taskManager.addEventFromFile(description, date, isDone);
+            break;
         }
-        
-        
+
 
     }
-
-
 
 
 }
