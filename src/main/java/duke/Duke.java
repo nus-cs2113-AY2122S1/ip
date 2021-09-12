329f36f9e185e9df0c2d3d4a51e04af97fdb23c6
kyun99 <limkayyun@gmail.com>
2021-09-13 00:55:49 +0800

    Add getFile() method that stores TaskList data in duke.txt or creates the file /data/duke.txt if it is not found

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -4,10 +4,13 @@ import duke.exceptions.DukeException;
 import duke.exceptions.IncompleteInformationException;
 import duke.exceptions.InvalidRequestException;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -17,6 +20,10 @@ public class Duke {
         System.out.println("What can I do for you?");
         Scanner in = new Scanner(System.in);
         TaskList list = new TaskList();
+        //if file exist, load file into TaskList
+        File f = getFile();
+        System.out.println(f.getAbsolutePath());
+
         String request = in.nextLine();
         while (Request.isBye(request)) {
             try {
@@ -46,6 +53,19 @@ public class Duke {
                 request = in.nextLine();
             }
         }
+        //write taskList into the txt file
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static File getFile() throws IOException {
+        File file = new File("data/duke.txt");
+        if (!file.exists()) {
+            File dir = new File("data");
+            dir.mkdir();
+            File newFile = new File("data/duke.txt");
+            newFile.createNewFile();
+            return newFile;
+        }
+        return file;
     }
 }
