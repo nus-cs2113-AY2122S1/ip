acb3a2e48778629e586906310b353c116851b100
RemusTeo <remustck@gmail.com>
2021-09-12 23:32:34 +0800

    Add Increment: Level-6, A-Collections

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Duke {
     private static ArrayList<Task> tasks = new ArrayList<>();
 
-    public static void displayGreetingMessage() {
+    private static void displayGreetingMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -21,14 +21,14 @@ public class Duke {
         System.out.println(greet);
     }
 
-    public static void printOutput(String output) {
+    private static void printOutput(String output) {
         String niceOutput = "____________________________________________________________\n"
                 + output
                 + "____________________________________________________________\n";
         System.out.println(niceOutput);
     }
 
-    public static String[] splitByDelimiter(String delimiter, String arguments) {
+    private static String[] splitByDelimiter(String delimiter, String arguments) {
         String[] splitValues = new String[2];
         int indexOfDelimiter = arguments.indexOf(delimiter);
         splitValues[0] = arguments.substring(0, indexOfDelimiter).trim();
@@ -36,22 +36,18 @@ public class Duke {
         return splitValues;
     }
 
-    public static void addTodoTask(String arguments) {
+    private static void addTodoTask(String arguments) {
         if (arguments.equals("")) {
             String output = " ☹ OOPS!!! The description of a todo cannot be empty.\n";
             printOutput(output);
         } else {
             Todo newTodo = new Todo(arguments);
             tasks.add(newTodo);
-
-            String output = " Got it. I've added this task:\n"
-                    + "   " + newTodo + "\n"
-                    + " Now you have " + tasks.size() + " tasks in the list.\n";
-            printOutput(output);
+            acknowledgeAddedTask(newTodo);
         }
     }
 
-    public static void addDeadlineTask(String arguments) {
+    private static void addDeadlineTask(String arguments) {
         if (arguments.equals("")) {
             String output = " ☹ OOPS!!! The description of a deadline cannot be empty.\n";
             printOutput(output);
@@ -64,19 +60,14 @@ public class Duke {
 
                 Deadline newDeadline = new Deadline(description, by);
                 tasks.add(newDeadline);
-
-                String output = " Got it. I've added this task:\n"
-                        + "   " + newDeadline + "\n"
-                        + " Now you have " + tasks.size() + " tasks in the list.\n";
-                printOutput(output);
+                acknowledgeAddedTask(newDeadline);
             } catch (StringIndexOutOfBoundsException e) {
-                String output = " ☹ OOPS!!! Could not find delimiter\n";
-                printOutput(output);
+                displayDelimiterErrorMessage();
             }
         }
     }
 
-    public static void addEventTask(String arguments) {
+    private static void addEventTask(String arguments) {
         if (arguments.equals("")) {
             String output = " ☹ OOPS!!! The description of an event cannot be empty.\n";
             printOutput(output);
@@ -89,19 +80,26 @@ public class Duke {
 
                 Event newEvent = new Event(description, at);
                 tasks.add(newEvent);
-
-                String output = " Got it. I've added this task:\n"
-                        + "   " + newEvent + "\n"
-                        + " Now you have " + tasks.size() + " tasks in the list.\n";
-                printOutput(output);
+                acknowledgeAddedTask(newEvent);
             } catch (StringIndexOutOfBoundsException e) {
-                String output = " ☹ OOPS!!! Could not find delimiter\n";
-                printOutput(output);
+                displayDelimiterErrorMessage();
             }
         }
     }
 
-    public static void listTasks() {
+    private static void deleteTask(String arguments) {
+        int taskNumber = Integer.parseInt(arguments);
+        if (taskNumber <= 0 || taskNumber > tasks.size()) {
+            displayTaskDoesNotExistMessage();
+        } else {
+            int taskIndex = taskNumber - 1;
+            Task taskToDelete = tasks.get(taskIndex);
+            tasks.remove(taskIndex);
+            displayDeleteMessage(taskToDelete);
+        }
+    }
+
+    private static void listTasks() {
         String output = " Here are the tasks in your list:\n";
         for (int i = 1; i < tasks.size() + 1; i++) {
             output = output + " " + i + "." + tasks.get(i - 1).toString() + "\n";
@@ -109,31 +107,59 @@ public class Duke {
         printOutput(output);
     }
 
-    public static void acknowledgeDone(String arguments) {
+    private static void markDone(String arguments) {
         int taskNumber = Integer.parseInt(arguments) - 1;
         tasks.get(taskNumber).markAsDone();
+        displayDoneMessage(taskNumber);
+    }
+
+    private static void acknowledgeAddedTask(Task addedTask) {
+        String output = " Got it. I've added this task:\n"
+                + "   " + addedTask + "\n"
+                + " Now you have " + tasks.size() + " tasks in the list.\n";
+        printOutput(output);
+    }
+
+    private static void displayDelimiterErrorMessage() {
+        String output = " ☹ OOPS!!! Could not find delimiter\n";
+        printOutput(output);
+    }
+
+    private static void displayTaskDoesNotExistMessage() {
+        String output = " ☹ OOPS!!! The task specified does not exist.\n";
+        printOutput(output);
+    }
+
+    private static void displayDeleteMessage(Task taskToDelete) {
+        String output = " Noted. I've removed this task:\n"
+                + "   " + taskToDelete + "\n"
+                + " Now you have " + tasks.size() + " tasks in the list.\n";
+        printOutput(output);
+    }
+
+    private static void displayDoneMessage(int taskNumber) {
         String output = " Nice! I've marked this task as done:\n"
                 + "   " + tasks.get(taskNumber).toString() + "\n";
         printOutput(output);
     }
 
-    public static void displayByeMessage() {
+    private static void displayByeMessage() {
         String output = " Bye. Hope to see you again soon!\n";
         printOutput(output);
     }
 
-    public static void displayUnknownCommandResponse() {
+    private static void displayUnknownCommandResponse() {
         String unknownCommandResponse = " ☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n";
         printOutput(unknownCommandResponse);
     }
 
-    public static void executeCommand(String command, String arguments) throws DukeException {
+    private static void executeCommand(String command, String arguments) throws DukeException {
         switch (command) {
         case "list":
             listTasks();
             break;
         case "done":
-            acknowledgeDone(arguments);
+            markDone(arguments);
             break;
         case "todo":
             addTodoTask(arguments);
@@ -143,6 +169,9 @@ public class Duke {
             break;
         case "event":
             addEventTask(arguments);
+            break;
+        case "delete":
+            deleteTask(arguments);
             break;
         case "bye":
             displayByeMessage();
@@ -172,6 +201,5 @@ public class Duke {
                 displayUnknownCommandResponse();
             }
         }
-
     }
 }
