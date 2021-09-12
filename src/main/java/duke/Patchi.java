e30cc89a26dbee88c34f614f0792d9b314e04a13
aaapppsss <adelinebooqw@gmail.com>
2021-09-12 13:48:20 +0800

    Level-7

--- src/main/java/duke/Patchi.java
+++ src/main/java/duke/Patchi.java
@@ -3,11 +3,14 @@ package duke;
 import duke.exception.InvalidCommandException;
 import duke.exception.MissingDescriptionException;
 import duke.exception.MissingTimingException;
+import duke.exception.NoTaskSelectedException;
 import duke.task.Deadline;
 import duke.task.Event;
 import duke.task.Task;
 import duke.task.Todo;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -17,96 +20,143 @@ public class Patchi {
     static String patchiDataPath = "data/patchidata.txt";
 
     public static void main(String[] args) {
-        File patchiData = new File("data/patchidata.txt");
-        System.out.println("full path: " + patchiData.getAbsolutePath());
-        System.out.println("file exists?: " + patchiData.exists());
-
         Scanner in = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int nextTaskIndex = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
-        System.out.println("─── .o * : *. ¤ .* : ¤ o. ───");
-        System.out.println("Patchi: Hello! I'm Patchi the tasks fairy Œ(ˊᵕˋ)B\n " +
+        try {
+            loadData(tasks);
+        } catch (FileNotFoundException e) {
+            println("<patchidata.txt file not found>");
+        }
+
+        println("─── .o * : *. ¤ .* : ¤ o. ───");
+        println("Patchi: Hello! I'm Patchi the tasks fairy Œ(ˊᵕˋ)B\n " +
                 "What can I do for you today? Œ(ˊVˋ)B");
+        println("─── .o * : typing... : ¤ o. ───");
+        print("Me: ");
 
-        printTransition();
         String input = in.nextLine();
 
         while (input.equals("bye") == false) {
             if (input.equals("list") == true) {
-                listTasks(tasks, nextTaskIndex);
+                listTasks(tasks);
             } else if (input.startsWith("done")) {
-                markTaskAsDone(tasks, nextTaskIndex, input);
+                try {
+                    markTaskAsDone(tasks, input);
+                } catch (NoTaskSelectedException e) {
+                    println("Patchi: You need to select a task... Œ(~ˊnˋ~)B");
+                }
+            } else if (input.startsWith("delete")) {
+                try {
+                    deleteTask(tasks, input);
+                } catch (NoTaskSelectedException e) {
+                    println("Patchi: You need to select a task... Œ(~ˊnˋ~)B");
+                }
             } else { //add task OR invalid command
                 try {
-                    addTask(tasks, nextTaskIndex, input);
-                    System.out.println("Patchi: Got it! I have added " + tasks[nextTaskIndex].toString() +
+                    addTask(tasks, input);
+                    try {
+                        appendToFile(patchiDataPath, input + ",f" + System.lineSeparator());
+                    } catch (IOException e) {
+                    }
+                    println("Patchi: Got it! I have added " + tasks.get(tasks.size() - 1).toString() +
                             " to your task list! Œ(ˆOˆ)B");
-                    nextTaskIndex++;
-                    System.out.println("Patchi: You have " + nextTaskIndex + " tasks now" +
+                    println("Patchi: You have " + tasks.size() + " tasks now" +
                             "... Too much work... Œ(ˊnˋ)B");
                 } catch (InvalidCommandException e) {
-                    System.out.println("Patchi: I'm sorry, I don't understand what that means... Œ(ˊnˋ)B");
-                } catch (NullPointerException e) {
-                    System.out.println("<Failed to add new task>");
                 }
             }
 
-            printTransition();
+            println("─── .o * : typing... : ¤ o. ───");
+            print("Me: ");
             input = in.nextLine();
         }
 
-        System.out.println("Patchi: Bye! Hope to see you again soon! Œ(~ˊᵕˋ~)B");
-        System.out.println("─── .o * : *. ¤ .* : ¤ o. ───");
+        println("Patchi: Bye! Hope to see you again soon! Œ(~ˊᵕˋ~)B");
+        println("─── .o * : *. ¤ .* : ¤ o. ───");
     }
 
-    private static void markTaskAsDone(Task[] tasks, int nextTaskIndex, String input) {
-        int taskIndex = Integer.parseInt(input.substring(5)) - 1;
-        if (nextTaskIndex > taskIndex) {
-            tasks[taskIndex].setDone(true);
-            System.out.println("Patchi: Good job! I've marked this task as done on your list. " +
+    private static void markTaskAsDone(ArrayList<Task> tasks, String input) throws NoTaskSelectedException {
+        int taskIndex;
+        try {
+            taskIndex = Integer.parseInt(input.substring(5)) - 1;
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new NoTaskSelectedException();
+        }
+        if (tasks.size() > taskIndex) {
+            tasks.get(taskIndex).setDone(true);
+
+            //change patchidata
+            String line;
+            try {
+                line = getLine(taskIndex);
+                line = line.replace(",f", ",t");
+                overwriteLine(taskIndex, line);
+            } catch (IOException e) {
+            }
+
+            println("Patchi: Good job! I've marked this task as done on your list. " +
                     "Time for a break? Œ(ˊwˋ)B");
         } else {
-            System.out.println("Patchi: That task doesn't seem to exist...... Œ(ˊnˋ)B");
+            println("Patchi: That task doesn't seem to exist...... Œ(ˊnˋ)B");
         }
     }
 
-    private static void listTasks(Task[] tasks, int nextTaskIndex) {
-        if (nextTaskIndex > 0) {
-            System.out.println("Patchi: Here is the list of tasks you currently have! Work hard~ Œ(˙O˙)B");
-            for (int i = 0; i < nextTaskIndex; i++) {
-                System.out.println((i + 1) + ". " + tasks[i].toString());
+    private static void listTasks(ArrayList<Task> tasks) {
+        if (tasks.size() > 0) {
+            println("Patchi: Here is the list of tasks you currently have! Work hard~ Œ(˙O˙)B");
+            for (int i = 0; i < tasks.size(); i++) {
+                println((i + 1) + ". " + tasks.get(i).toString());
             }
         } else {
-            System.out.println("Patchi: You have no tasks for now! Go and relax~ Œ(ˊuˋ)B");
+            println("Patchi: You have no tasks for now! Go and relax~ Œ(ˊuˋ)B");
         }
     }
 
-    private static void addTask(Task[] tasks, int nextTaskIndex, String input)
+    private static void addTask(ArrayList<Task> tasks, String input)
             throws InvalidCommandException {
         try {
             if (input.startsWith("todo")) {
-                addTodo(tasks, nextTaskIndex, input);
+                addTodo(tasks, input);
             } else if (input.startsWith("deadline")) {
-                addDeadline(tasks, nextTaskIndex, input);
+                addDeadline(tasks, input);
             } else if (input.startsWith("event")) {
-                addEvent(tasks, nextTaskIndex, input);
+                addEvent(tasks, input);
             } else {
                 throw new InvalidCommandException();
             }
-
-           
-
         } catch (MissingDescriptionException e) {
-            System.out.println("Patchi: You need to add a task description... Œ(ˊnˋ)B");
+            println("Patchi: You need to add a task description... Œ(ˊnˋ)B");
+            throw new InvalidCommandException();
         } catch (MissingTimingException e) {
-            System.out.println("Patchi: You need to add a timing for this kind of task! Œ(ˊnˋ)B");
+            println("Patchi: You need to add a timing for this kind of task! Œ(ˊnˋ)B");
+            throw new InvalidCommandException();
         } catch (InvalidCommandException e) {
-            System.out.println("Patchi: I'm sorry, I don't understand...Did you format your command correctly? Œ(ˊnˋ)B");
+            println("Patchi: I'm sorry, I don't understand what that means... Œ(ˊnˋ)B");
+            throw new InvalidCommandException();
         }
     }
 
-    private static void addTodo(Task[] tasks, int nextTaskIndex, String input)
+    private static void deleteTask(ArrayList<Task> tasks, String input) throws NoTaskSelectedException {
+        int taskIndex;
+        try {
+            taskIndex = Integer.parseInt(input.substring(7)) - 1;
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new NoTaskSelectedException();
+        }
+
+        if (tasks.size() > taskIndex) {
+            tasks.remove(taskIndex);
+            deleteLine(taskIndex);
+            println("Patchi: Whoosh! I've magicked that task away! Œ(ˊwˋ)B");
+            println("Patchi: You now have " + tasks.size() + " tasks! Œ(ˊwˋ)B");
+        } else {
+            println("Patchi: That task doesn't seem to exist...... Œ(ˊnˋ)B");
+        }
+
+    }
+
+    private static void addTodo(ArrayList<Task> tasks, String input)
             throws MissingDescriptionException {
         String description;
 
@@ -116,10 +166,10 @@ public class Patchi {
             throw new MissingDescriptionException();
         }
 
-        tasks[nextTaskIndex] = new Todo(description);
+        tasks.add(new Todo(description));
     }
 
-    private static void addDeadline(Task[] tasks, int nextTaskIndex, String input)
+    private static void addDeadline(ArrayList<Task> tasks, String input)
             throws InvalidCommandException, MissingTimingException, MissingDescriptionException {
         int indexOfBy = input.indexOf("/by");
         String by;
@@ -141,10 +191,10 @@ public class Patchi {
             throw new MissingTimingException();
         }
 
-        tasks[nextTaskIndex] = new Deadline(description, by);
+        tasks.add(new Deadline(description, by));
     }
 
-    private static void addEvent(Task[] tasks, int nextTaskIndex, String input)
+    private static void addEvent(ArrayList<Task> tasks, String input)
             throws InvalidCommandException, MissingDescriptionException, MissingTimingException {
         int indexOfAt = input.indexOf("/at");
         String at;
@@ -166,18 +216,95 @@ public class Patchi {
             throw new MissingTimingException();
         }
 
-        tasks[nextTaskIndex] = new Event(description, at);
+        tasks.add(new Event(description, at));
     }
 
-    public static void printTransition() {
-        System.out.println("─── .o * : typing... : ¤ o. ───");
-        System.out.print("Me: ");
+    public static void println(String message) {
+        System.out.println(message);
     }
 
-    private static void writeToFile(String filePath, String textToAdd) throws IOException {
-        FileWriter fw = new FileWriter(filePath);
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    private static void appendToFile(String filePath, String textToAdd) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true);
         fw.write(textToAdd);
         fw.close();
     }
 
+    private static void overwriteFile(String filePath, String text) throws IOException {
+        FileWriter fw = new FileWriter(filePath, false);
+        fw.write(text);
+        fw.close();
+    }
+
+    private static void loadData(ArrayList<Task> tasks) throws FileNotFoundException {
+        File patchiData = new File(patchiDataPath);
+        Scanner s = new Scanner(patchiData);
+        while (s.hasNext()) {
+            String[] taskInfo = s.nextLine().split(",", 0);
+            String input = taskInfo[0];
+            Boolean isDone = taskInfo[1].equals("t") ? true : false; //t or f
+            try {
+                addTask(tasks, input);
+                tasks.get(tasks.size() - 1).setDone(isDone);
+            } catch (InvalidCommandException e) {
+            }
+        }
+    }
+
+    private static void overwriteLine(Integer lineIndex, String newLine) throws IOException {
+        File patchiData = new File(patchiDataPath);
+        Scanner s = new Scanner(patchiData);
+        String newFileData = "";
+        Integer i = 0;
+
+        while (s.hasNext()) {
+            if (i == lineIndex) {
+                newFileData += newLine + System.lineSeparator();
+                s.nextLine();
+            } else {
+                newFileData += s.nextLine() + System.lineSeparator();
+            }
+            i++;
+        }
+        overwriteFile(patchiDataPath, newFileData);
+    }
+
+    private static void deleteLine(Integer lineIndex) {
+        try {
+            File patchiData = new File(patchiDataPath);
+            Scanner s = new Scanner(patchiData);
+            String newFileData = "";
+            Integer i = 0;
+
+            while (s.hasNext()) {
+                if (i == lineIndex) {
+                    s.nextLine();
+                } else {
+                    newFileData += s.nextLine() + System.lineSeparator();
+                }
+                i++;
+            }
+            overwriteFile(patchiDataPath, newFileData);
+        } catch (IOException e) {
+        }
+    }
+
+
+    private static String getLine(Integer lineIndex) throws IOException {
+        File patchiData = new File(patchiDataPath);
+        Scanner s = new Scanner(patchiData);
+        Integer i = 0;
+
+        while (s.hasNext()) {
+            if (i == lineIndex) {
+                return s.nextLine();
+            }
+            i++;
+        }
+
+        return null;
+    }
 }
---
+++ src/main/java/duke/exception/NoTaskSelectedException.java
@@ -0,0 +1,4 @@
+package duke.exception;
+
+public class NoTaskSelectedException extends Exception{
+}
