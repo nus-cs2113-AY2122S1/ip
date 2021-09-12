0625bc52be0004b32a62fb32b45c1d95be97d355
theeugenechong <eugenechong20011@yahoo.com>
2021-09-12 12:16:11 +0800

    Add DataManager class which helps in loading and saving of Duke's data

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,6 +1,7 @@
 package duke.task;
 
 public class Deadline extends Task {
+    protected final String TASK_TYPE = "D";
     protected String by;
 
     public Deadline(String description, String by) {
@@ -18,6 +19,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[" + TASK_TYPE + "]" + super.toString() + " (by: " + by + ")";
     }
 }
