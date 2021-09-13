98f9de00d72e76374e40cc1c079cfb0c9e9b660b
LAPTOP-61K9T4RL\Nai Jie <yeatnaijie@gmail.com>
2021-09-13 21:16:26 +0800

    Refactor code
    - Refactored code in FileManager class

--- src/main/java/duke/FileManager.java
+++ src/main/java/duke/FileManager.java
@@ -16,19 +16,63 @@ import duke.task.*;
 public class FileManager {
     private static final String ROOT = System.getProperty("user.dir");
     private static final Path FILE_PATH = Paths.get(ROOT, "data", "duke.txt");
+    private static final Path DIRECTORY_PATH = Paths.get(ROOT, "data");
+
+    public static final String ERROR_WRITING_TO_SAVE_FILE = "Error writing to save file";
+    public static final String ERROR_CREATING_SAVE_FILE = "Error creating save file";
+    public static final String ERROR_CREATING_DATA_DIRECTORY = "Error creating data directory";
+    public static final String ERROR_DUKE_UNKNOWN = "Unknown error with Duke occurred";
+    public static final String ERROR_CREATING_SAVE_FILE1 = "Error creating save file";
+    public static final String ERROR_READING_SAVE_FILE = "Error reading save file, some tasks may have been lost";
+    public static final String ERROR_CONVERTING_SAVE_FILE = "Error converting file to task list, some tasks may have been lost";
 
 
     public static void writeTaskListToFile(TaskList taskList) {
-        if (!Files.exists(FILE_PATH)) {
-            createFile();
-        }
-        ;
         try {
+            if (!Files.exists((DIRECTORY_PATH))) {
+                createDataDirectory();
+            }
+            if (!Files.exists(FILE_PATH)) {
+                createFile();
+            }
             FileWriter fw = new FileWriter(FILE_PATH.toString());
             fw.write(taskList.toFile());
             fw.close();
         } catch (IOException e) {
-            System.out.println("Error writing to save file");
+            PrintUtils.printErrorMessage(ERROR_WRITING_TO_SAVE_FILE);
+        } catch (DukeException e) {
+            switch (e.getMessage()) {
+            case ExceptionMessages.EXCEPTION_CREATE_FILE_FAIL:
+                PrintUtils.printErrorMessage(ERROR_CREATING_SAVE_FILE);
+                break;
+            case ExceptionMessages.EXCEPTION_CREATE_DIRECTORY_FAIL:
+                PrintUtils.printErrorMessage(ERROR_CREATING_DATA_DIRECTORY);
+                break;
+            default:
+                PrintUtils.printErrorMessage(ERROR_DUKE_UNKNOWN);
+            }
+
+        }
+    }
+
+    private static void createDataDirectory() throws DukeException {
+        File newDirectory = new File(DIRECTORY_PATH.toString());
+        boolean createSuccess = newDirectory.mkdir();
+        if (!createSuccess) {
+            throw new DukeException(ExceptionMessages.EXCEPTION_CREATE_DIRECTORY_FAIL);
+        }
+    }
+
+
+    private static void createFile() throws DukeException {
+        try {
+            File newFile = new File(FILE_PATH.toString());
+            boolean createSuccess = newFile.createNewFile();
+            if (!createSuccess) {
+                throw new DukeException(ExceptionMessages.EXCEPTION_CREATE_FILE_FAIL);
+            }
+        } catch (IOException e) {
+            PrintUtils.printErrorMessage(ERROR_CREATING_SAVE_FILE1);
         }
     }
 
@@ -47,9 +91,9 @@ public class FileManager {
                 tasks.addTaskWithoutMessage(newTask);
             }
         } catch (IOException e) {
-            PrintUtils.printErrorMessage("Error reading save file, some tasks may have been lost");
+            PrintUtils.printErrorMessage(ERROR_READING_SAVE_FILE);
         } catch (DukeException e) {
-            PrintUtils.printErrorMessage("Error converting file to task list, some tasks may have been lost");
+            PrintUtils.printErrorMessage(ERROR_CONVERTING_SAVE_FILE);
         }
         return tasks;
     }
@@ -75,13 +119,5 @@ public class FileManager {
         }
     }
 
-    private static void createFile() {
-        try {
-            File newFile = new File(FILE_PATH.toString());
-            newFile.createNewFile();
-        } catch (IOException e) {
-            PrintUtils.printErrorMessage("Error creating save file");
-        }
-    }
 
 }
--- src/main/java/duke/exception/ExceptionMessages.java
+++ src/main/java/duke/exception/ExceptionMessages.java
@@ -6,4 +6,6 @@ public class ExceptionMessages {
     public static final String EXCEPTION_NO_TASK_NUMBER = "No Task Number Given";
     public static final String EXCEPTION_INVALID_TASK_NUMBER = "Invalid Task Number";
     public static final String EXCEPTION_INVALID_FILE_LINE = "Invalid File Line";
+    public static final String EXCEPTION_CREATE_DIRECTORY_FAIL = "Create Directory Fail";
+    public static final String EXCEPTION_CREATE_FILE_FAIL = "Create File Fail";
 }
