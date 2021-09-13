eb68a8e86a9dcefdfa2d3dcbd33accdd2d5a81a3
Lily Peng <c200218@e.ntu.edu.sg>
2021-09-13 21:39:19 +0800

    make use of ArrayList

--- src/main/java/duke/TaskManager.java
+++ src/main/java/duke/TaskManager.java
@@ -2,23 +2,23 @@ package duke;
 
 import duke.exception.TaskIndexOutOfBound;
 
+import java.util.ArrayList;
+
 public class TaskManager {
     public static final int MAX_TASKS = 100;
-    private Task[] tasks = new Task[MAX_TASKS];
-    private int tasksCount = 0;
-
+    private ArrayList<Task> tasks = new ArrayList<>();
 
     public void addTask(Task task){
-        tasks[tasksCount++] = task;
+        tasks.add(task);
         System.out.println("Got it. I've added this task: ");
         System.out.println("  " + task.getTask());
-        System.out.println("Now you have " + tasksCount + " tasks on the list.");
+        System.out.println("Now you have " + tasks.size() + " tasks on the list.");
     }
 
     public boolean searchTask(Task task){
         boolean taskFound = false;
-        for(int i=0; i<tasksCount; i++){
-            if(tasks[i].getTask().equals(task.getTask()) && !tasks[i].isDone()){   //be aware that maybe it is added but it is done, so your can add another one
+        for(int i=0; i<tasks.size(); i++){
+            if(tasks.get(i).getTask().equals(task.getTask()) && !tasks.get(i).isDone()){   //be aware that maybe it is added but it is done, so your can add another one
                 taskFound = true;
             }
         }
@@ -27,21 +27,18 @@ public class TaskManager {
 
     public void deleteTask(String taskDelete) throws TaskIndexOutOfBound {
         int indexOfTask = getIndexOfTask(taskDelete);
-        Task taskToDelete = tasks[indexOfTask - 1];
-        for(int i = indexOfTask; i < tasksCount; i++) {
-            tasks[i - 1] = tasks[i];
-        }
-        tasksCount--;
+        Task taskToDelete = tasks.get(indexOfTask - 1);
+        tasks.remove(indexOfTask - 1);
         System.out.println("Noted! I've removed this task: ");
         System.out.println(taskToDelete.getTask());
-        System.out.println("Now you have" +  tasksCount  + "tasks in the list.");
+        System.out.println("Now you have" +  tasks.size()  + "tasks in the list.");
     }
 
     public void tasksDone(String taskDone) throws TaskIndexOutOfBound {
         int indexOfTask = getIndexOfTask(taskDone);
-        tasks[indexOfTask-1].markAsDone();
+        tasks.get(indexOfTask-1).markAsDone();
         System.out.println("Nice! I've marked this task as done: ");
-        System.out.println(tasks[indexOfTask-1].getTask());
+        System.out.println(tasks.get(indexOfTask-1).getTask());
     }
 
     // Since I find that codes for tasksDone and removeTasks can be duplicated, so I refactor the common part to this function
@@ -49,7 +46,7 @@ public class TaskManager {
     private int getIndexOfTask(String taskDone) throws TaskIndexOutOfBound {
         String[] words = taskDone.split(" ");
         int indexOfTask = Integer.parseInt(words[1]);
-        if(indexOfTask < 1 || indexOfTask > tasksCount){
+        if(indexOfTask < 1 || indexOfTask > tasks.size()){
             throw new TaskIndexOutOfBound("Ops! your task index is out of bound!");
         }
         return indexOfTask;
@@ -58,9 +55,9 @@ public class TaskManager {
 
     public void listTasks(){
         System.out.println("List of tasks:");
-        for(int i=0; i<tasksCount; i++){
+        for(int i=0; i<tasks.size(); i++){
             int id = i + 1;
-            System.out.println(id + "." + tasks[i].getTask());
+            System.out.println(id + "." + tasks.get(i).getTask());
         }
     }
 
