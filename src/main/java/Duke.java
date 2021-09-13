739ca48a04cf5bff7242cbaa43926d5d6096dbf3
EdwardZYWang <wangzy.edward@u.nus.edu>
2021-09-13 18:57:11 +0800

    Level 6. Delete Add support for deleting tasks from the list.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static int inputCount = 0;
-    public static Task[] tasks = new Task[100];
+    public static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         String input;
@@ -18,8 +19,17 @@ public class Duke {
             if (input.equals("list")) {
                 input = printList(in);
 
+
             } else if (input.contains("done")) {
+                try{
                 input = testTaskDone(input, in);
+                } catch (NumberFormatException e3) {
+                    System.out.println("Did you type in a number?");
+                    input = in.nextLine();
+                } catch (ArrayIndexOutOfBoundsException e4) {
+                    System.out.println("Finish the command!");
+                    input = in.nextLine();
+                }
 
             } else if (input.contains("todo")) {
                 try {
@@ -27,10 +37,10 @@ public class Duke {
 
                     Task whatToDo = toDoMethod(input);
                     System.out.println("    Got it. I've added this task:\n" + "    " + whatToDo + "\n"
-                            + "    Now you have " + inputCount + " tasks in the list.");
+                            + "    Now you have " + tasks.size() + " tasks in the list.");
 
                     input = in.nextLine();
-                } catch (DukeExceptions e3) {
+                } catch (DukeExceptions e5) {
                     System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
                     input = in.nextLine();
                 }
@@ -40,7 +50,7 @@ public class Duke {
                 try {
                     Task whatDeadline = deadlineMethod(input);
                     System.out.println("    Got it. I've added this task:\n" + "    " + whatDeadline + "\n"
-                            + "    Now you have " + inputCount + " tasks in the list.");
+                            + "    Now you have " + tasks.size() + " tasks in the list.");
 
                     input = in.nextLine();
                 } catch (StringIndexOutOfBoundsException e1) {
@@ -55,18 +65,31 @@ public class Duke {
                 try {
                     Task whatEvent = eventMethod(input);
                     System.out.println("    Got it. I've added this task:\n" + "    " + whatEvent + "\n"
-                            + "    Now you have " + inputCount + " tasks in the list.");
+                            + "    Now you have " + tasks.size() + " tasks in the list.");
                     
                     input = in.nextLine();
-                } catch (StringIndexOutOfBoundsException e1) {
+                } catch (StringIndexOutOfBoundsException e6) {
                     System.out.println("input = " + input + "\n☹ OOPS!!! Please retype your input!");
                     input = in.nextLine();
-                } catch (ArrayIndexOutOfBoundsException e2) {
+                } catch (ArrayIndexOutOfBoundsException e7) {
                     System.out.println("you have typed in = " + input + "\nw☹ OOPS!!! where is your /at brother? ");
                     input = in.nextLine();
                 }
 
-            } else {
+            }
+            else if (input.contains("delete")) {
+                try{
+                    input = testDeleteTask(input, in);
+                } catch (NumberFormatException e8) {
+                    System.out.println("Did you type in a number?");
+                    input = in.nextLine();
+                } catch (ArrayIndexOutOfBoundsException e9) {
+                    System.out.println("Finish the command!");
+                    input = in.nextLine();
+                }
+
+            }
+            else {
                 System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");//overall error check
                 input = in.nextLine();
 
@@ -80,8 +103,8 @@ public class Duke {
         String input;
         System.out.println("    Here are the tasks in your list:");
 
-        for (int outputCount = 0; outputCount < inputCount; outputCount++) {
-            System.out.println("    " + (outputCount + 1) + "." + tasks[outputCount]);
+        for (int outputCount = 0; outputCount < tasks.size(); outputCount++) {
+            System.out.println("    " + (outputCount + 1) + "." + tasks.get(outputCount));
         }
         input = in.nextLine();
         return input;
@@ -94,8 +117,25 @@ public class Duke {
         taskNumber = Integer.parseInt(inputSplitter[1]);
         taskNumber = taskNumber - 1;
 
-        if (((taskNumber + 1) > 0) && ((taskNumber + 1) <= inputCount)) { //check for invalid inputs
-            input = taskDone(in, tasks[taskNumber]);
+        if (((taskNumber + 1) > 0) && ((taskNumber + 1) <= tasks.size())) { //check for invalid inputs
+            input = taskDone(in, tasks.get(taskNumber));
+
+        } else {
+            System.out.println("    Invalid input! Please check the list again!");
+            input = in.nextLine();
+        }
+        return input;
+    }
+
+    private static String testDeleteTask(String input, Scanner in) {
+        int taskNumber;
+        String[] inputSplitter;
+        inputSplitter = input.split(" ");
+        taskNumber = Integer.parseInt(inputSplitter[1]); //get which task to delete
+        taskNumber = taskNumber - 1;
+
+        if (((taskNumber + 1) > 0) && ((taskNumber + 1) <= tasks.size())) { //check for invalid inputs
+            input = taskDelete(in, tasks.remove(taskNumber));
 
         } else {
             System.out.println("    Invalid input! Please check the list again!");
@@ -115,12 +155,20 @@ public class Duke {
         input = in.nextLine();
         return input;
     }
+    private static String taskDelete(Scanner in, Task task) {
+        String input;
+        System.out.println("    Noted. I've removed this task:" );
+        System.out.println("    " + task);
+        System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
+        input = in.nextLine();
+        return input;
+    }
 
     public static Task toDoMethod(String input) {
         String toDoDescription = input.substring(4).trim();
 
         Task tasksToDo = new Todo(toDoDescription);
-        tasks[inputCount] = tasksToDo;
+        tasks.add(tasksToDo);
         inputCount++;
 
         return tasksToDo;
@@ -134,7 +182,7 @@ public class Duke {
         String deadlineBy = deadlineSplitter[1]; // after /by
 
         Task description = new Deadline(deadlineDescription, deadlineBy);
-        tasks[inputCount] = description;
+        tasks.add(description);
         inputCount++;
 
         return description;
@@ -147,7 +195,7 @@ public class Duke {
         String eventAt = eventSplitter[1]; // after /at
 
         Task description = new Event(eventDescription, eventAt);
-        tasks[inputCount] = description;
+        tasks.add(description);
         inputCount++;
 
         return description;
