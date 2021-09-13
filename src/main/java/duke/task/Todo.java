e540258bbd406442559cc69869ad8da1916cd24a
Alvin.Avocados <alvinpang321@gmail.com>
2021-09-14 02:44:26 +0800

    Update constructors for Task.java and subclasses
    An additional type param of enum TaskType is now required.

--- src/main/java/duke/task/Todo.java
+++ src/main/java/duke/task/Todo.java
@@ -1,8 +1,19 @@
 package duke.task;
 
 public class Todo extends Task {
-    public Todo(String description) {
-        super(description);
+    public Todo(String description, TaskType type) {
+        super(description, type);
         this.type = TaskType.TODO;
+    }
+
+    public Todo(String description, TaskType type, boolean isDone) {
+        super(description, type);
+        this.type = TaskType.TODO;
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String getFileStringFormat() {
+        return String.format("T | %s", super.getFileStringFormat());
     }
 }
