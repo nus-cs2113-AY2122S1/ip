ab23190e14894c1df6c32df48c90e581bb465ef4
Daren Tan <daren9901@gmail.com>
2021-09-13 11:07:13 +0800

    Cleanup Duke
    Updated documentation and test files

--- src/main/java/duke/TaskManager.java
+++ src/main/java/duke/TaskManager.java
@@ -1,7 +1,10 @@
 package duke;
 
-import duke.task.*;
-
+import duke.task.Task;
+import duke.task.TaskType;
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Todo;
 import java.util.ArrayList;
 
 public class TaskManager {
