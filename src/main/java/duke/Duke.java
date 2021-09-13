6b58331c6e237a3d49369b343de89eef1d35843e
Edison <edison.zhong0107@gmail.com>
2021-09-13 19:40:06 +0800

    Revert "Add Delete function"
    This reverts commit d519543c1d609c5b85220f0f3f53a1f491a96665.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,12 +7,11 @@ import duke.task.Task;
 
 import java.util.Objects;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Duke {
 
     public static int taskNum = 0;
-    public static ArrayList<Task> tasks = new ArrayList<>();
+    public static Task[] tasks = new Task[100];
 
     public static void main(String[] args) {
         String userCommand;
@@ -24,18 +23,16 @@ public class Duke {
             if (userCommand.equals("bye")) {
                 break;
             } else if (userCommand.equals("list")) {
-                printList();
+                printList(tasks, taskNum);
             } else if (userCommand.contains("done")) {
                 int id = Integer.parseInt(userCommand.substring(5));
-                taskDone(tasks.get(id - 1));
+                taskDone(tasks[id - 1]);
             } else if (userCommand.startsWith("todo")) {
                 addTodo(userCommand);
             } else if (userCommand.startsWith("event")) {
                 addEvent(userCommand);
             } else if (userCommand.startsWith("deadline")) {
                 addDeadline(userCommand);
-            } else if (userCommand.startsWith("delete")) {
-                deleteTask(userCommand);
             } else {
                 printSign();
                 System.out.println(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
@@ -64,9 +61,9 @@ public class Duke {
         try {
             int contentStart = 5;
             String description = userCommand.substring(contentStart);
-            tasks.add(new Todo(description));
+            tasks[taskNum] = new Todo(description);
             taskNum++;
-            printTotalNumOfTasks(tasks.get(taskNum - 1), taskNum);
+            printTotalNumOfTasks(tasks, taskNum);
         }
         catch (StringIndexOutOfBoundsException e) {
             printSign();
@@ -83,9 +80,9 @@ public class Duke {
             int atStart = userCommand.indexOf("/at") + 4;
             String description = userCommand.substring(contentStart, contentEnd);
             String at = userCommand.substring(atStart);
-            tasks.add(new Event(description, at));
+            tasks[taskNum] = new Event(description, at);
             taskNum++;
-            printTotalNumOfTasks(tasks.get(taskNum - 1), taskNum);
+            printTotalNumOfTasks(tasks, taskNum);
         }
         catch (StringIndexOutOfBoundsException e) {
             printSign();
@@ -102,9 +99,9 @@ public class Duke {
             int byStart = userCommand.indexOf("/by") + 4;
             String description = userCommand.substring(contentStart, contentEnd);
             String by = userCommand.substring(byStart);
-            tasks.add(new Deadline(description, by));
+            tasks[taskNum] = new Deadline(description, by);
             taskNum++;
-            printTotalNumOfTasks(tasks.get(taskNum - 1), taskNum);
+            printTotalNumOfTasks(tasks, taskNum);
         }
         catch (StringIndexOutOfBoundsException e) {
             printSign();
@@ -113,56 +110,31 @@ public class Duke {
         }
     }
 
-    public static void deleteTask(String userCommand) {
-        try {
-            int contentStart = 7;
-            int id = Integer.parseInt(userCommand.substring(contentStart));
-            System.out.println("Noted. I've removed this task: ");
-            System.out.println(tasks.get(id - 1).toString());
-            tasks.remove(id - 1);
-            taskNum--;
-            System.out.println("Now you have " + taskNum + " tasks in the list.");
-            printSign();
-        }
-        catch (StringIndexOutOfBoundsException e) {
-            printSign();
-            System.out.println("☹ OOPS!!! The index of the task to be deleted cannot be empty.");
-            printSign();
-        }
-    }
-
     public static void taskDone(Task args) {
         // Mark the relevant task as "done", and print out a line indicates that the task is marked as done
-        try {
-            args.markAsDone();
-            printSign();
-            System.out.println("Nice! I've marked this task as done:");
-            System.out.println(args.toString());
-            printSign();
-        }
-        catch (StringIndexOutOfBoundsException e) {
-            printSign();
-            System.out.println("☹ OOPS!!! The index of the task to be marked as done cannot be empty.");
-            printSign();
-        }
+        args.markAsDone();
+        printSign();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(args.toString());
+        printSign();
     }
 
-    public static void printList() {
+    public static void printList(Task[] args, int length) {
         // Print out the whole task list with their status icons
         printSign();
         System.out.println("Here are the tasks in your list:");
-        for (int i = 1; i <= tasks.size(); i++) {
-            System.out.print(i + ".");
-            System.out.println(tasks.get(i - 1).toString());
+        for (int i = 0; i < length; i++) {
+            System.out.print(i + 1 + ".");
+            System.out.println(args[i].toString());
         }
         printSign();
     }
 
-    public static void printTotalNumOfTasks(Task args, int total) {
+    public static void printTotalNumOfTasks(Task[] args, int total) {
         // Print out the total number of the tasks and what is added to do
         printSign();
         System.out.println("Got it. I've added this task:");
-        System.out.println(args.toString());
+        System.out.println(args[total-1].toString());
         System.out.println("Now you have " + total + " tasks in the list");
         printSign();
     }
