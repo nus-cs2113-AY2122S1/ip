961be181e836c787330d5c19fcfdaf37e061d244
Eric Bryan <e0555789@u.nus.edu>
2021-09-13 19:31:39 +0800

    add save feature to a text file

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,10 +1,13 @@
 package duke;
 
+import java.io.FileNotFoundException;
+
 public class Duke {
     private static final String WELCOME_MESSAGE = "     Hello! I'm duke.Duke\n     What can I do for you?";
     private static final String EXIT_MESSAGE = "     Bye. Hope to see you again soon!";
 
     private static final String DIVIDER = "    ____________________________________________________________";
+
 
     public static void printHorizontalLine() {
         System.out.println(DIVIDER);
@@ -26,8 +29,19 @@ public class Duke {
         taskManager.processUserInput();
     }
 
+    public static void loadSavedData(TaskManager taskManager) {
+        try {
+            taskManager.loadSavedData();
+        } catch (FileNotFoundException e) {
+            System.out.println("     file from hard disk is not found. Unable to load data");
+        }
+
+    }
+
     public static void main(String[] args) {
         TaskManager taskManager = new TaskManager();
+
+        loadSavedData(taskManager);
 
         printWelcomeMessage();
         readUserInputUntilBye(taskManager);
