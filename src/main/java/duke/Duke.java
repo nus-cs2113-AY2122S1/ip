2e47762cf653bab02d0cd726ce6960a841923e83
antrikshv <antriksh9954v@gmail.com>
2021-09-14 05:41:47 +0800

    Comments added

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -31,7 +31,9 @@ public class Duke {
         goodbyeMessage();
     }
 
-    /*Below is the method to run the app*/
+    /**
+     * Runs Ikaros, manages the instructions of ikaros
+     */
     private static void runIkaros() {
         boolean isRunning = true;
 
@@ -62,6 +64,9 @@ public class Duke {
             case "bye":
                 isRunning = false;
                 break;
+            case "help":
+                help();
+                break;
             default:
                 System.out.println("I didn't catch that!");
                 lineBreak();
@@ -70,6 +75,38 @@ public class Duke {
         }
     }
 
+    /**
+     * displays the commands usable for the user
+     */
+    private static void help() {
+        System.out.println("Below are the list of acceptable commands"
+                + System.lineSeparator() + System.lineSeparator()
+                + "add todo -> add a todo task to the list [add todo todo_name]"
+                + System.lineSeparator()
+                + "add event -> adds an event task to the list [add event event_name " +
+                "/at event_date"
+                + System.lineSeparator()
+                + "add deadline -> adds a deadline task to the list " +
+                "[add deadline deadline_name /by deadline_date"
+                + System.lineSeparator()
+                + "remove -> removes a task [remove task_number]"
+                + System.lineSeparator()
+                + "done -> marks a task as complete [done task_number]"
+                + System.lineSeparator()
+                + "undo -> marks a task as undone [undo task_number]"
+                + System.lineSeparator()
+                + "list -> Display the list of tasks"
+                + System.lineSeparator()
+                + "echo -> echo mode activated, where system echos your input, " +
+                "type [exit] to leave mode"
+                + System.lineSeparator()
+                + "bye -> kill ikaros");
+        lineBreak();
+    }
+
+    /**
+     * Loads the task list, to Ikaros
+     */
     private static void LoadTasks() {
         String[] command;
         File ikarosTaskData = new File("ikarosTaskData.txt");
@@ -95,6 +132,29 @@ public class Duke {
         }
     }
 
+    /**
+     * Writes down the new tasks to the saved file
+     */
+    private static void SaveTasks() {
+        try {
+            FileWriter fw = new FileWriter("ikarosTaskData.txt");
+            for (Task task : taskList) {
+                fw.write(task.getTaskType() + ">"
+                        + task.getStatusIcon() + ">"
+                        + task.getDescription() + ">"
+                        + task.getDate() + System.lineSeparator());
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Something went wrong" + e.getMessage());
+        }
+    }
+
+    /**
+     * Checks and adds the tasks, from the saved .txt file
+     *
+     * @param command a String[] containing task information stored in the file
+     */
     private static void loadingManager(String[] command) {
         switch (command[0]) {
         case "todo":
@@ -117,22 +177,12 @@ public class Duke {
         }
     }
 
-    private static void SaveTasks() {
-        try {
-            FileWriter fw = new FileWriter("ikarosTaskData.txt");
-            for (Task task : taskList) {
-                fw.write(task.getTaskType() + ">"
-                        + task.getStatusIcon() + ">"
-                        + task.getDescription() + ">"
-                        + task.getDate() + System.lineSeparator());
-            }
-            fw.close();
-        } catch (IOException e) {
-            System.out.println("Something went wrong" + e.getMessage());
-        }
-    }
-
-    /*Below are the methods for the managers of the functions for the app*/
+    /**
+     * Checks to see which command the user has input
+     *
+     * @param command  the set of instructions the user input in a String array
+     * @param response the string of the user input
+     */
     private static void instructionManager(String[] command, String response) {
         try {
             if (command[0].equalsIgnoreCase("add")) {
@@ -147,6 +197,12 @@ public class Duke {
         }
     }
 
+    /**
+     * Deciphers the specific task that the user wants to add to the list
+     *
+     * @param command  the set of instructions the user input in a String array
+     * @param response the string of the user input
+     */
     private static void taskManager(String[] command, String response) {
         String TaskType = command[1];
         try {
@@ -166,6 +222,12 @@ public class Duke {
         }
     }
 
+    /**
+     * Deciphers whether the user wants to do or undo a specific task
+     *
+     * @param command  the set of instructions the user input in a String array
+     * @param response the string of the user input
+     */
     private static void doUndoManager(String[] command, String response) {
         try {
             if (command[0].equalsIgnoreCase("done")) {
@@ -180,7 +242,11 @@ public class Duke {
         }
     }
 
-    /*Below are the methods for the functions in the Ikaros app*/
+    /**
+     * removes a task from the list
+     *
+     * @param command a String[] containing task information stored in the file
+     */
     private static void remove(String[] command) throws RemoveException {
         int i = Integer.parseInt(command[1]);
         if (i > taskList.size()) {
@@ -192,6 +258,11 @@ public class Duke {
         System.out.println("Tasks remaining = " + taskList.size());
     }
 
+    /**
+     * Adds an event to the list of Tasks
+     *
+     * @param response the string of the user input
+     */
     private static void Event(String response)
             throws TimeException {
         String timing = " (at: " + response.substring(response.indexOf("/") + TIME_COMMAND)
@@ -207,6 +278,11 @@ public class Duke {
 
     }
 
+    /**
+     * Adds an deadline to the list of Tasks
+     *
+     * @param response the string of the user input
+     */
     private static void deadLine(String response)
             throws TimeException {
         String timing = " (by: " + response.substring(response.indexOf("/") + TIME_COMMAND)
@@ -222,11 +298,21 @@ public class Duke {
         addToList(work);
     }
 
+    /**
+     * Adds a toDo to the list of Tasks
+     *
+     * @param response the string of the user input
+     */
     private static void toDo(String response) {
         ToDo task = new ToDo(response.substring(TODO_LENGTH));
         addToList(task);
     }
 
+    /**
+     * marks task as done
+     *
+     * @param response the string of the user input
+     */
     private static void done(String response) throws DoneUndoException {
         int i = Integer.parseInt(response.substring(5)) - 1;
         if (taskList.get(i).isDone) {
@@ -238,6 +324,11 @@ public class Duke {
                 + taskList.get(i).getDescription());
     }
 
+    /**
+     * marks task as undone
+     *
+     * @param response the string of the user input
+     */
     private static void undo(String response) throws DoneUndoException {
         int i = Integer.parseInt(response.substring(5)) - 1;
         if (!taskList.get(i).isDone) {
@@ -249,12 +340,20 @@ public class Duke {
                 + taskList.get(i).getDescription());
     }
 
+    /**
+     * Adds a task to the list
+     *
+     * @param task task that is being added into the list
+     */
     private static void addToList(Task task) {
         taskList.add(task);
         System.out.println("Task added: " + task);
         System.out.println("Total no. of Tasks = " + (taskList.size()));
     }
 
+    /**
+     * Below goes into a mode in ikaros, where user input is echoed
+     */
     public static void echo() {
         System.out.println("Life is a mirror and will reflect back to "
                 + "the thinker what\nhe thinks into it, echoing commencing");
@@ -271,6 +370,9 @@ public class Duke {
         }
     }
 
+    /**
+     * prints the list of tasks
+     */
     public static void printList() {
         int i = 1;
         System.out.println("Here are the tasks in your list:");
@@ -282,13 +384,18 @@ public class Duke {
         }
     }
 
-    /*Below are the methods for drawing and standard replies on the console*/
+    /**
+     * line separator
+     */
     private static void lineBreak() {
         String lineBreak = "..........................." +
                 ".......................................";
         System.out.println(lineBreak);
     }
 
+    /**
+     * Greet user
+     */
     private static void IntroductoryMessage() {
         String logo = "  /\\ _ /\\\n"
                 + " #  @ @  #    Welcome to IKAROS!\n"
@@ -304,6 +411,9 @@ public class Duke {
         lineBreak();
     }
 
+    /**
+     * Bid Farewell to the user
+     */
     private static void goodbyeMessage() {
         System.out.println("GoodBye, Ikaros awaits for future commands");
         lineBreak();
