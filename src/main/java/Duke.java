2231be2f5ce4ce085e4420dcdb68868eb7955b5f
Vignesh Kumaravel <k_vignesh@hotmail.com>
2021-09-14 22:27:13 +0800

    Update document comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -252,6 +252,11 @@ public class Duke {
         }
     }
 
+    /**
+     * Marks a task in list as complete.
+     *
+     * @param completed Command input by user to mark a task as complete.
+     */
     private static void completeTask(String completed) {
         try {
             int taskNo = Integer.parseInt(completed.replaceAll("[^0-9]", "")) - 1;
@@ -270,6 +275,11 @@ public class Duke {
         }
     }
 
+    /**
+     * Deletes a task from list.
+     *
+     * @param toRemove Command input by user to delete a task.
+     */
     private static void deleteTask(String toRemove) {
         try {
             int taskNo = Integer.parseInt(toRemove.replaceAll("[^0-9]", "")) - 1;
@@ -327,6 +337,11 @@ public class Duke {
         }
     }
 
+    /**
+     * Populates list at the beginning of session with items from previous session.
+     *
+     * @throws FileNotFoundException If there was no file saved from previous session.
+     */
     public static void initializeList() throws FileNotFoundException {
         File f = new File(filePath);
         Scanner s = new Scanner(f);
@@ -347,6 +362,14 @@ public class Duke {
         }
     }
 
+    /**
+     * Parses lines in the local file where items in list are saved, and
+     * constructs a user-style CLI command to be input into createTask()
+     * method to create the task in current session.
+     *
+     * @param lineFragments A line of the saved local file.
+     * @return toReturn is the CLI command that a user will input to create Task.
+     */
     public static String createCommand(String[] lineFragments) {
         String toReturn = null;
         if (lineFragments.length == 3) {
@@ -359,6 +382,13 @@ public class Duke {
         return toReturn;
     }
 
+    /**
+     * Structures items in list to an appropriate string to be written
+     * into locally stored file.
+     *
+     * @param t A Task item in the current session's list.
+     * @return toReturn contains a string to be written onto the local file.
+     */
     public static String createLineForFile(Task t) {
         String toReturn = null;
         String doneStatus;
@@ -382,6 +412,11 @@ public class Duke {
         return toReturn;
     }
 
+    /**
+     * Writes Tasks in list to a file as readable string.
+     *
+     * @throws IOException If error occurs while writing to file.
+     */
     public static void saveToFile() throws IOException {
         FileWriter fw = new FileWriter(filePath, false);
         for (Task item: tasksList) {
