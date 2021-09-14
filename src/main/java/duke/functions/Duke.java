b1aef3efbdc09ef9acd3a2bf190a0b45ad798944
Li Xi Yuan <lixiyuan.owlsong@gmail.com>
2021-09-14 18:18:36 +0800

    Fix Save function, works properly now

--- src/main/java/duke/functions/Duke.java
+++ src/main/java/duke/functions/Duke.java
@@ -13,7 +13,7 @@ public class Duke {
 
     private static Boolean isFinished = false;
     private static int itemCount = 0;
-    private static String filepath = "savedlist.txt";
+    private static final String filepath = "SAVEDLIST.txt";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,7 +21,7 @@ public class Duke {
 
         printIntro();
         try {
-            readSavedList();
+            readSavedList(items);
         } catch (IOException e) {
             System.out.println("Error creating/reading task list");
         }
@@ -45,7 +45,7 @@ public class Duke {
                     System.out.println("\tInvalid argument, please enter a valid task number!");
                 } catch (NullPointerException | IndexOutOfBoundsException e) {
                     System.out.println("\tNo such task!");
-                } catch (EmptyArgException e){
+                } catch (EmptyArgException e) {
                     System.out.println("\tWhich task is done?");
                 }
                 try {
@@ -61,6 +61,11 @@ public class Duke {
                 } catch (EmptyArgException e) {
                     System.out.println("\tDescription of todo cannot be empty!");
                 }
+                try {
+                    writeToFile(items);
+                } catch (IOException e) {
+                    System.out.println("\tError writing to file");
+                }
                 break;
             }
             case "deadline": {
@@ -71,6 +76,11 @@ public class Duke {
                 } catch (WrongFormatException e) {
                     System.out.println("\tWrong format! Try \"Deadline [description] \\by [due date]\"");
                 }
+                try {
+                    writeToFile(items);
+                } catch (IOException e) {
+                    System.out.println("\tError writing to file");
+                }
                 break;
             }
             case "event": {
@@ -80,6 +90,11 @@ public class Duke {
                     System.out.println("\tDescription for deadline cannot be empty!");
                 } catch (WrongFormatException e) {
                     System.out.println("\tWrong format! Try \"Event [description] \\by [due date]\"");
+                }
+                try {
+                    writeToFile(items);
+                } catch (IOException e) {
+                    System.out.println("\tError writing to file");
                 }
                 break;
             }
@@ -94,30 +109,58 @@ public class Duke {
 
     private static void writeToFile(Task[] items) throws IOException {
         FileWriter fw = new FileWriter(filepath);
-        for (Task i : items){
-            fw.write(i + System.lineSeparator());
+        for (Task i : items) {
+            if (i instanceof Todo) {
+                fw.write("T" + "--" + i.getStatusIcon() + "--" + i.description);
+                fw.write(System.lineSeparator());
+            } else if (i instanceof Deadline) {
+                fw.write("D" + "--" + i.getStatusIcon() + "--" + i.description + "--" + ((Deadline) i).getBy());
+                fw.write(System.lineSeparator());
+
+            } else if (i instanceof Event) {
+                fw.write("E" + "--" + i.getStatusIcon() + "--" + i.description + "--" + ((Event) i).getAt());
+                fw.write(System.lineSeparator());
+            }
         }
         fw.close();
     }
 
-    private static void readSavedList() throws IOException {
+    private static void readSavedList(Task[] items) throws IOException {
         File f = new File(filepath);
-        if (f.createNewFile()){
+        if (f.createNewFile()) {
             System.out.println("\tNo saved task list, new file created");
-        }
-        else {
+        } else {
             Scanner fs = new Scanner(f);
             System.out.println("\tYour list of saved task:");
             while (fs.hasNext()) {
-                System.out.println("\t\t" + fs.nextLine());
-            }
-        }
+                String[] feed = fs.nextLine().split("--");
 
+                switch (feed[0]) {
+                case "T":
+                    items[itemCount] = new Todo(feed[2]);
+                    break;
+                case "D":
+                    items[itemCount] = new Deadline(feed[2], feed[3]);
+                    break;
+                case "E":
+                    items[itemCount] = new Event(feed[2], feed[3]);
+                    break;
+                default:
+                    break;
+                }
+
+                if (feed[1].equals("X")) {
+                    items[itemCount].markAsDone();
+                }
+                itemCount++;
+            }
+            printTaskList(items);
+        }
     }
 
     private static void handleDone(Task[] items, String userInput) throws EmptyArgException {
         String[] arg = userInput.split(" ");
-        if (arg.length < 2){
+        if (arg.length < 2) {
             throw new EmptyArgException();
         }
         int indexToMark = Integer.parseInt(arg[1]) - 1;
@@ -128,11 +171,11 @@ public class Duke {
 
     private static void handleEvent(Task[] items, String userInput) throws EmptyArgException, WrongFormatException {
         String[] arg = userInput.split(" ", 2);
-        if (arg.length < 2){
+        if (arg.length < 2) {
             throw new EmptyArgException();
         }
         String[] splitArg = arg[1].split("/", 2);
-        if (splitArg.length<2){
+        if (splitArg.length < 2) {
             throw new WrongFormatException();
         }
         String description = splitArg[0].trim();
@@ -144,11 +187,11 @@ public class Duke {
 
     private static void handleDeadline(Task[] items, String userInput) throws EmptyArgException, WrongFormatException {
         String[] arg = userInput.split(" ", 2);
-        if (arg.length < 2){
+        if (arg.length < 2) {
             throw new EmptyArgException();
         }
         String[] splitArg = arg[1].split("/", 2);
-        if (splitArg.length < 2){
+        if (splitArg.length < 2) {
             throw new WrongFormatException();
         }
         String description = splitArg[0].trim();
