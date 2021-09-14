bae6dd5450a01d55ac67694d9c9bc2020c175381
Poopies99 <>
2021-09-14 15:09:51 +0800

    Level-7

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -1,6 +1,9 @@
 package tasks;
 
-public class Deadline extends Todo {
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Deadline extends Task {
     protected String by;
 
     public Deadline(String description, String date) {
@@ -24,5 +27,12 @@ public class Deadline extends Todo {
     @Override
     public String getClassType() {
         return "[D]";
+    }
+
+    @Override
+    public void saveTask(String filePath) throws IOException {
+        FileWriter fileWrite = new FileWriter(filePath, true);
+        fileWrite.write("D | " + getDescription() + " | " + getBy() + " | " + getDone() + "\n");
+        fileWrite.close();
     }
 }
