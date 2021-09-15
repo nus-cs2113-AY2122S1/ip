0ce30faf371d6456794cc8f87bd1c5d48ee33d54
Hukairui <kairuih12@gmail.com>
2021-09-15 12:07:10 +0800

    Add deletion

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static final String MESSAGE = "Here are the tasks in your list:";
-    private static String logo = " ____        _        \n"
+    private static final String LOGO = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
-    private static String line = "___________________________________________________";
+    private static final String LINE = "___________________________________________________";
 
     public static void main(String[] args) {
-        printWelcome(logo, line);
-        String[] arrayInput = new String[100];
-        int[] taskStatus = new int[100];
-        initiateStatus(taskStatus);
-        checkCommand(taskStatus, arrayInput);
-    }
+        printWelcome(LOGO, LINE);
+        ArrayList<String> arrayInput = new ArrayList<>();
 
-    private static void initiateStatus(int[] taskStatus) {
-        for (int i = 0; i < 100; i++) {
-            taskStatus[i] = 0;
-        }
+        ArrayList<Integer> taskStatus = new ArrayList<>();
+        checkCommand(taskStatus, arrayInput);
     }
 
     public static void printWelcome(String logo, String line) {
@@ -30,6 +25,7 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println(line);
     }
+
     public static void checkTodo(String userCommand) throws DukeException {
         if(userCommand.length() <= 5) {     //generate error when receiving invalid Todoinput
             DukeException e = new DukeException();
@@ -52,20 +48,42 @@ public class Duke {
         }
     }
 
-    public static void checkCommand(int[] taskStatus, String[] arrayInput) {
+    public static void checkDelete(String userCommand) throws DukeException {
+        if(userCommand.length() <= 7) {     //generate error when receiving invalid delete input
+            DukeException e = new DukeException();
+            throw e;
+        } else if(!isInt(userCommand.valueOf(7))) {
+            DukeException e = new DukeException();
+            throw e;
+        }
+    }
+
+    public static boolean isInt(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch(NumberFormatException e) {
+            return false;
+        } catch(NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void checkCommand(ArrayList<Integer> taskStatus, ArrayList<String> arrayInput) {
         Scanner userInput = new Scanner(System.in);
         String userCommand = userInput.nextLine();
-        String[] taskType = new String[100];
+        ArrayList<String> taskType = new ArrayList<>();
         int taskIDInt;
         int inputCount = 0;
         char taskID;
         while (!userCommand.equals("bye")) {
             if (userCommand.equals("list")) {
-                printList(taskType, taskStatus, arrayInput, userInput, inputCount);
+                System.out.println("status = "+taskStatus.get(0));
+                printList(taskType, taskStatus, arrayInput, inputCount);
                 userCommand = userInput.nextLine();
                 continue;
             } else if (userCommand.contains("done")) {
-                printDone(taskStatus, arrayInput, userCommand);
+                printDone(taskStatus, taskType, arrayInput, userCommand);
                 userCommand = userInput.nextLine();
                 continue;
             } else if (userCommand.contains("todo")) {
@@ -73,12 +91,13 @@ public class Duke {
                     checkTodo(userCommand);
                 } catch (DukeException e) {
                     System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
-                    System.out.println(line);
+                    System.out.println(LINE);
                     userCommand = userInput.nextLine();
                     continue;
                 }
                 inputCount = printTodo(arrayInput, userCommand, inputCount);
-                taskType[inputCount - 1] = "T";
+                taskType.add(inputCount - 1, "T");
+                taskStatus.add(inputCount - 1, 0);
                 userCommand = userInput.nextLine();
                 continue;
             } else if (userCommand.contains("deadline")) {
@@ -86,12 +105,13 @@ public class Duke {
                     checkDeadline(userCommand);
                 } catch(DukeException e) {
                     System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
-                    System.out.println(line);
+                    System.out.println(LINE);
                     userCommand = userInput.nextLine();
                     continue;
                 }
                 inputCount = printDeadline(arrayInput, userCommand, inputCount);
-                taskType[inputCount - 1] = "D";
+                taskType.add(inputCount - 1, "D");
+                taskStatus.add(inputCount - 1, 0);
                 userCommand = userInput.nextLine();
                 continue;
             } else if (userCommand.contains("event")) {
@@ -99,23 +119,33 @@ public class Duke {
                     checkEvent(userCommand);
                 } catch(DukeException e) {
                     System.out.println("☹ OOPS!!! The description of a event cannot be empty.");
-                    System.out.println(line);
+                    System.out.println(LINE);
                     userCommand = userInput.nextLine();
                     continue;
                 }
                 inputCount = printEvent(arrayInput, userCommand, inputCount);
-                taskType[inputCount - 1] = "E";
+                taskType.add(inputCount - 1, "E");
+                taskStatus.add(inputCount - 1, 0);
                 userCommand = userInput.nextLine();
                 continue;
+            } else if(userCommand.contains("delete")) {
+                try{
+                    checkDelete(userCommand);
+                } catch (DukeException e) {
+                    System.out.println("☹ OOPS!!! The delete command is invalid.");
+                    System.out.println(LINE);
+                    userCommand = userInput.nextLine();
+                    continue;
+                }
+                inputCount = printDelete(taskType, taskStatus, arrayInput, inputCount, userCommand);
+                userCommand = userInput.nextLine();
+
             } else if(!userCommand.equals("bye")) {
                 printInvalid();
-                System.out.println(line);
+                System.out.println(LINE);
                 userCommand = userInput.nextLine();
                 continue;
-
             }
-            inputCount = printUpdate(arrayInput, userCommand, inputCount);
-            userCommand = userInput.nextLine();
         }
 
         if (userCommand.equals("bye")) {
@@ -123,95 +153,107 @@ public class Duke {
         }
     }
 
+    public static int printDelete(ArrayList<String> taskType, ArrayList<Integer> taskStatus,
+                                  ArrayList<String> arrayInput, int inputCount, String userCommand) {
+        int position = Integer.parseInt(userCommand.substring(7)) - 1;
+        System.out.println("Noted. I've removed this task:");
+        if (taskStatus.get(position) == 1) {
+            System.out.println("  " + "[" + taskType.get(position) + "]"
+                    + "[X] " + arrayInput.get(position));
+        } else {
+            System.out.println("  " + "[" + taskType.get(position) + "]"
+                    + "[ ] " + arrayInput.get(position));
+        }
+        inputCount--;
+        System.out.println("Now you have " + inputCount + " tasks in the list.");
+        System.out.println(LINE);
+        arrayInput.remove(position);
+        taskStatus.remove(position);
+        taskType.remove(position);
+        return inputCount;
+    }
+
     public static void printInvalid() {
         System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
 
-    private static int printEvent(String[] arrayInput, String userCommand, int inputCount) {
+    private static int printEvent(ArrayList<String> arrayInput, String userCommand, int inputCount) {
         String at;
         int index = userCommand.indexOf("-");
         at = userCommand.substring(index - 5);
         Event event = new Event(userCommand, at, index);
         inputCount++;
-        System.out.println(line);
+        System.out.println(LINE);
         System.out.println("Got it. I've added this task:");
         System.out.println(event);
         System.out.println("Now you have " + inputCount + " tasks in the list.");
-        System.out.println(line);
+        System.out.println(LINE);
         String taskName = event.description.substring(6, index - 10);
         String eventName = taskName + " (at: " + at + ")";
-        arrayInput[inputCount - 1] = eventName;
+        arrayInput.add(inputCount-1, eventName);
         return inputCount;
     }
 
-    private static int printDeadline(String[] arrayInput, String userCommand, int inputCount) {
+    private static int printDeadline(ArrayList<String> arrayInput, String userCommand, int inputCount) {
         String by;
         int index = userCommand.indexOf("by");
         by = userCommand.substring(index + 3);
         Deadline deadline = new Deadline(userCommand, by, index);
         inputCount++;
-        System.out.println(line);
+        System.out.println(LINE);
         System.out.println("Got it. I've added this task:");
         System.out.println(deadline);
         System.out.println("Now you have " + inputCount + " tasks in the list.");
-        System.out.println(line);
+        System.out.println(LINE);
         String taskName = deadline.description.substring(9, index - 2);
         String deadlineName = taskName + " (by: " + by + ")";
-        arrayInput[inputCount - 1] = deadlineName;
+        arrayInput.add(inputCount-1, deadlineName);
         return inputCount;
     }
 
-    private static int printTodo(String[] arrayInput, String userCommand, int inputCount) {
+    private static int printTodo(ArrayList<String> arrayInput, String userCommand, int inputCount) {
         Todo todo = new Todo(userCommand);
         inputCount++;
-        System.out.println(line);
+        System.out.println(LINE);
         System.out.println("Got it. I've added this task:");
         System.out.println(todo);
         System.out.println("Now you have " + inputCount + " tasks in the list.");
-        System.out.println(line);
-        arrayInput[inputCount - 1] = todo.description.substring(5);
-        return inputCount;
-    }
-
-    private static int printUpdate(String[] arrayInput, String userCommand, int inputCount) {
-        arrayInput[inputCount++] = userCommand;
-        System.out.println(line);
-        System.out.println("added: " + userCommand);
-        System.out.println(line);
+        System.out.println(LINE);
+        String todoName = todo.description.substring(5);
+        arrayInput.add(inputCount-1, todoName);
         return inputCount;
     }
 
     private static void printBye() {
-        System.out.println(line);
+        System.out.println(LINE);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(LINE);
     }
 
-    private static void printDone(int[] taskStatus, String[] arrayInput, String userCommand) {
+    private static void printDone(ArrayList<Integer> taskStatus, ArrayList<String> taskType, ArrayList<String> arrayInput, String userCommand) {
         char taskID;
         int taskIDInt;
         int len = userCommand.length();
         taskID = userCommand.charAt(len - 1);
         taskIDInt = taskID - 49;
-        taskStatus[taskIDInt] = 1;
+        taskStatus.set(taskIDInt, 1);
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println("	" + "[X] " + arrayInput[taskIDInt]);
-        System.out.println(line);
+        System.out.println("	" + "[" + taskType.get(taskIDInt) + "]" +
+                "[X] " + arrayInput.get(taskIDInt));
+        System.out.println(LINE);
     }
 
-    public static void printList(String[] taskType, int[] taskStatus,
-                                 String[] arrayInput, Scanner userInput, int inputCount) {
-        String userCommand;
-        System.out.println(line);
+    public static void printList(ArrayList<String> taskType, ArrayList<Integer> taskStatus,
+                                 ArrayList<String> arrayInput, int inputCount) {
+        System.out.println(LINE);
         System.out.println(MESSAGE);
-
         for (int i = 1; i <= inputCount; i++) {
-            if (taskStatus[i - 1] == 1) {
-                System.out.println(i + ".[" + taskType[i - 1] + "]" + "[X] " + arrayInput[i - 1]);
+            if (taskStatus.get(i - 1) == 1) {
+                System.out.println(i + ".[" + taskType.get(i - 1) + "]" + "[X] " + arrayInput.get(i - 1));
             } else {
-                System.out.println(i + ".[" + taskType[i - 1] + "]" + "[ ] " + arrayInput[i - 1]);
+                System.out.println(i + ".[" + taskType.get(i - 1) + "]" + "[ ] " + arrayInput.get(i - 1));
             }
         }
-        System.out.println(line);
+        System.out.println(LINE);
     }
 }
