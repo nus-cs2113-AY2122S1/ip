6e72f70a9f955c738be8919b0d35a57db473ff6d
ChrisLangton <christwlangton@gmail.com>
2021-09-15 15:52:46 +0800

    add Level 7

--- src/main/java/duke/templar/Duke.java
+++ src/main/java/duke/templar/Duke.java
@@ -2,6 +2,8 @@ package duke.templar;
 
 import duke.exception.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -9,9 +11,10 @@ import java.util.ArrayList;
 public class Duke {
 
     public static final String MESSAGE_DIVIDER = "____________________________________________________________";
+    public static final String FILE_PATH = "data/tasks.txt";
     public static ArrayList<Task> tasks = new ArrayList<>();
 
-    public static void main(String[] args) throws CommandInvalidException, DeadlineInvalidFormatException, TodoInvalidFormatException, EventInvalidFormatException, TaskNumberInvalidException, NoSuchTaskException {
+    public static void main(String[] args) throws CommandInvalidException, DeadlineInvalidFormatException, TodoInvalidFormatException, EventInvalidFormatException, TaskNumberInvalidException, NoSuchTaskException, IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -24,6 +27,8 @@ public class Duke {
         System.out.println("Greetings, Hero. Meet Vector Unit 202 - codename DUKE.\n" + "The state of the art AI assassin you requested. Give DUKE a mission, and it shall be done.");
         System.out.println(MESSAGE_DIVIDER);
 
+        DataManager activateDataManager = new DataManager(FILE_PATH);
+        TaskManager activateTaskManager = activateDataManager.loadSave(tasks);
 
         boolean endSession = false;
 
@@ -35,11 +40,11 @@ public class Duke {
             String line; // the task
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
-
-            TaskManager activateTaskManager = new TaskManager();
+            
             activateTaskManager.processInput(line, tasks);
 
             if (line.contains("bye")) {
+                activateDataManager.writeSave(activateTaskManager);
                 System.out.println(MESSAGE_DIVIDER);
                 System.out.println("[The Templar:]\n" + "DUKE shall carry out his mission. Farewell, Hero.");
                 System.out.println(MESSAGE_DIVIDER);
