b363b4d2caa762e1d02966c24cdc7befb773e551
Aayush <aayushmathur7@gmail.com>
2021-09-15 22:06:35 +0800

    Implemented Level 6. Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     static String horizontal = " __________________________________________________\n";
     static String introduction = horizontal + "   Hello! I'm Duke\n" + "   What can I do for you?\n" + horizontal;
     static String farewell = horizontal + "   Bye. Hope to see you again soon!\n" + horizontal;
-    static Task[] taskList = new Task[100];
+//    static Task[] taskList = new Task[100];
+    static ArrayList<Task> taskList = new ArrayList<>();
     static int taskCount = 0;
-    static int doneIndex = 0;
+    static int descriptionIndex = 0;
 
     public static void main(String[] args) {
         System.out.println(introduction);
@@ -25,6 +27,7 @@ public class Duke {
 
             splicedLine = line.split(" ");
             commandWord = splicedLine[0];
+
             if ((commandWord.equals("done") || commandWord.equals("todo") || commandWord.equals("deadline") || commandWord.equals("event")) && splicedLine.length == 1) {
                 System.out.println(horizontal + "Oops! The description of " + commandWord + " cannot be empty!\n" + horizontal);
             } else if (line.equals("list")) {
@@ -33,29 +36,35 @@ public class Duke {
                 } else {
                     System.out.println(horizontal + "Here are the tasks in your list:\n");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println("    " + (i + 1) + ". [" + taskList[i].getTaskType() + "]" + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
+                        System.out.println("    " + (i + 1) + ". [" + taskList.get(i).getTaskType() + "]" + "[" + taskList.get(i).getStatusIcon() + "] " + taskList.get(i).getDescription());
                     }
                     System.out.println(horizontal);
                 }
             } else if (commandWord.equals("done")) {
-                doneIndex = Integer.parseInt(splicedLine[1]);
-                line = taskList[doneIndex-1].getDescription();
-                System.out.println(horizontal + "Nice! I\'ve marked this task as done:\n" + "   [" + taskList[doneIndex-1].getTaskType() + "][X] " + line + "\n" + horizontal);
-                taskList[doneIndex-1].markAsDone();
+                descriptionIndex = Integer.parseInt(splicedLine[1]);
+                line = taskList.get(descriptionIndex-1).getDescription();
+                System.out.println(horizontal + "Nice! I\'ve marked this task as done:\n" + "   [" + taskList.get(descriptionIndex-1).getTaskType() + "][X] " + line + "\n" + horizontal);
+                taskList.get(descriptionIndex-1).markAsDone();
             } else if (commandWord.equals("todo")) {
                 task = line.substring(line.indexOf(' '));
-                taskList[taskCount] = new Todo(task);
-                System.out.println(horizontal + taskList[taskCount].toString() + "Now you have " + ++taskCount + " tasks on the list.\n" + horizontal);
+                taskList.add(new Todo(task));
+                System.out.println(horizontal + taskList.get(taskCount).toString() + "Now you have " + ++taskCount + " tasks on the list.\n" + horizontal);
             } else if (commandWord.equals("deadline")) {
                 task = line.substring(line.indexOf(' ')+1, line.indexOf('/')-1);
                 taskDate = line.substring(line.indexOf('/')+4);
-                taskList[taskCount] = new Deadline(task, taskDate);
-                System.out.println(horizontal + taskList[taskCount].toString() + "Now you have " + ++taskCount + " tasks on the list.\n" + horizontal);
+                taskList.add(new Deadline(task, taskDate));
+                System.out.println(horizontal + taskList.get(taskCount).toString() + "Now you have " + ++taskCount + " tasks on the list.\n" + horizontal);
             } else if (commandWord.equals("event")) {
                 task = line.substring(line.indexOf(' ')+1, line.indexOf('/')-1);
                 taskDate = line.substring(line.indexOf('/')+4);
-                taskList[taskCount] = new Event(task, taskDate);
-                System.out.println(horizontal + taskList[taskCount].toString() + "Now you have " + ++taskCount + " tasks on the list.\n" + horizontal);
+                taskList.add(new Event(task, taskDate));
+                System.out.println(horizontal + taskList.get(taskCount).toString() + "Now you have " + ++taskCount + " tasks on the list.\n" + horizontal);
+            } else if (commandWord.equals("delete")) {
+                descriptionIndex = Integer.parseInt(splicedLine[1]);
+                line = taskList.get(descriptionIndex - 1).getDescription();
+                System.out.println(horizontal + "Noted. I\'ve removed this task:\n" + "   [" + taskList.get(descriptionIndex-1).getTaskType() + "][" + taskList.get(descriptionIndex-1).getStatusIcon() + "] " + line);
+                System.out.println("Now you have " + --taskCount + " tasks on the list.\n" + horizontal);
+                taskList.remove(taskList.get(descriptionIndex-1));
             } else {
                 System.out.println(horizontal + "Oops! I don't know what that means :-(\n" + horizontal);
             }
