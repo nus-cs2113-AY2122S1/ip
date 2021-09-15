e8d8f41c5f01ba15b5507626701b4aa462ff14d4
LAPTOP-H8FATR55\kairo <kairoskoh.1212@gmail.com>
2021-09-15 21:37:24 +0800

    Add Level 6. Delete and A-Collections

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,8 +1,12 @@
 package duke;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    public static ArrayList<Task> list = new ArrayList<>();
+    public static int counter = 0;
 
     public static void main(String[] args) {
 
@@ -20,8 +24,7 @@ public class Duke {
         boolean isBye = false;
         String input;
         Scanner in = new Scanner(System.in);
-        int counter = 0;
-        Task[] list = new Task[100];
+        //Task[] list = new Task[100];
 
         while(!isBye){
             //store input
@@ -30,55 +33,99 @@ public class Duke {
             if (input.equals("bye")){ //check if bye
                 isBye = true;
             } else if (input.equals("list")) { //check if list
-                processList(counter, list);
+                processList();
             } else if (input.contains("done") ) { //check if done
-                processDone(input, list);
+                processDone(input);
+            } else if (input.contains("delete") ) { //check if delete
+                processDelete(input);
             } else {
                 try {
-                    counter = processTasks(input, counter, list); //process tasks
+                    counter = processTasks(input); //process tasks
                 } catch ( IllegalTaskException e ) {
                     System.out.println("Please include /by for deadline and /at for event");
                 }
             }
+            //test size
+            System.out.println(list.size());
         }
     }
 
-    private static void processList(int counter, Task[] list) {
-        if (list[0] != null){
-            printListMessage(counter, list);
+    private static void processDelete(String input) {
+        int deletePos = input.indexOf("delete");
+        if (list.size() != 0) {
+            if (input.length() < deletePos + 7) {
+                printDeleteButNotSpecificMessage(); //when input contains delete but no number
+            } else {
+                processValidDelete(input, deletePos); //when input contains delete and specified number
+            }
+        } else {
+            printDeleteButEmptyMessage(); //when input contains delete but list is empty
+        }
+    }
+
+    private static void printDeleteButEmptyMessage() {
+        System.out.println("--------------------");
+        System.out.println("Unable to delete.");
+        System.out.println("List is empty. Time to get productive!");
+        System.out.println("--------------------");
+    }
+
+    private static void processValidDelete(String input, int deletePos) {
+        String itemNumDone = input.substring(deletePos + 7, deletePos + 8);
+        int itemNum = Integer.parseInt(itemNumDone);
+        printDeleteMessage(itemNum);
+        list.remove(itemNum - 1);
+        counter -= 1;
+    }
+
+    private static void printDeleteMessage(int itemNum) {
+        System.out.println("--------------------");
+        System.out.println("Noted. I've removed this task:");
+        System.out.println( itemNum + ".[" + list.get(itemNum - 1).getStatusIcon() + "] " + list.get(itemNum - 1).getDescription() );
+        System.out.println("Now you have " + ( list.size() - 1 )+ " tasks in the list.");
+        System.out.println("--------------------");
+    }
+
+    private static void printDeleteButNotSpecificMessage() {
+        System.out.println("Please specify which task is to be deleted.");
+    }
+
+    private static void processList() {
+        if (list.size() != 0){
+            printListMessage(counter);
         } else {
             printListButEmptyMessage();
         }
     }
 
-    private static void processDone(String input, Task[] list) {
+    private static void processDone(String input) {
         int donePos = input.indexOf("done");
-        if (list[0] != null) {
+        if (list.size() != 0) {
             if (input.length() < donePos + 5) {
                 printDoneButNotSpecificMessage(); //when input contains done but no number
             } else {
-                processValidDone(input, list, donePos); //when input contains done and specified number
+                processValidDone(input, donePos); //when input contains done and specified number
             }
         } else {
             printDoneButEmptyMessage(); //when input contains done but list is empty
         }
     }
 
-    private static void processValidDone(String input, Task[] list, int donePos) {
+    private static void processValidDone(String input, int donePos) {
         String itemNumDone = input.substring(donePos + 5, donePos + 6);
         int itemNum = Integer.parseInt(itemNumDone);
-        list[itemNum - 1].setDone();
-        printDoneMessage(list, itemNum);
+        list.get(itemNum - 1).setDone();
+        printDoneMessage(itemNum);
     }
 
-    private static int processTasks(String input, int counter, Task[] list) throws IllegalTaskException{
+    private static int processTasks(String input) throws IllegalTaskException{
         if ( ( input.contains("deadline") || input.contains("event") ) && !input.contains("/")){
             throw new IllegalTaskException();
         }
         if (input.contains("todo")) {
             String description = input.substring(5);
             ToDo newTask = new ToDo(description);
-            list[counter] = newTask;
+            list.add(counter, newTask);
             counter += 1;
             printAddedTaskMessage(newTask, counter);
         } else if (input.contains("deadline")) {
@@ -89,18 +136,18 @@ public class Duke {
             }
             String date = input.substring(donePos + 4);
             Deadline newTask = new Deadline(description,date);
-            list[counter] = newTask;
+            list.add(counter, newTask);
             counter += 1;
             printAddedTaskMessage(newTask, counter);
         } else if (input.contains("event")) {
             int donePos = input.indexOf("/"); //finds pos of '/'
             String description = input.substring(6,donePos);
-            if (!input.substring(donePos + 1, donePos + 3).equals("by")) {
+            if (!input.substring(donePos + 1, donePos + 3).equals("at")) {
                 throw new IllegalTaskException();
             }
             String date = input.substring(donePos + 4);
             Event newTask = new Event(description,date);
-            list[counter] = newTask;
+            list.add(counter, newTask);
             counter += 1;
             printAddedTaskMessage(newTask, counter);
         } else {
@@ -110,18 +157,18 @@ public class Duke {
         return counter;
     }
 
-    private static void printDoneMessage(Task[] list, int itemNum) {
+    private static void printDoneMessage(int itemNum) {
         System.out.println("--------------------");
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println( itemNum + ".[" + list[itemNum - 1].getStatusIcon() + "] " + list[itemNum - 1].getDescription() );
+        System.out.println( itemNum + ".[" + list.get(itemNum - 1).getStatusIcon() + "] " + list.get(itemNum - 1).getDescription() );
         System.out.println("--------------------");
     }
 
-    private static void printListMessage(int counter, Task[] list) {
+    private static void printListMessage(int counter) {
         System.out.println("--------------------");
         System.out.println("Here are the tasks in your list:");
         for(int i = 0; i < counter; i += 1){
-            printListOfTaskSubMessage(list[i], i);
+            printListOfTaskSubMessage(list.get(i), i);
         }
         System.out.println("--------------------");
     }
