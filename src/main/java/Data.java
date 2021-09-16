d5ab54be096da3024a2054c074da02c62ea85cdd
husysg <husysg@icloud.com>
2021-09-16 14:10:26 +0800

    More OOP to Duke

---
+++ src/main/java/Data.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Data {
+    public static ArrayList<String> descriptions = new ArrayList<>();
+    public static ArrayList<Boolean> dones = new ArrayList<>();
+    public static ArrayList<String> types = new ArrayList<>();
+    public static ArrayList<String> dates = new ArrayList<>();
+}
