794100dfd0b3e4693c3a604a29e131e16604a3bc
astralum <minyue.j@gmail.com>
2021-09-17 00:48:29 +0800

    Move stored data to external file
    Add ability to add, edit and clear tasks stored in the external file

--- src/main/java/duke/TaskHandler.java
+++ src/main/java/duke/TaskHandler.java
@@ -1,39 +1,65 @@
 package duke;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
 public class TaskHandler {
 
     private int MAX_TASKS = 100;
     private String EMPTY_DESCRIPTION_MSG = "My liege, there is no description!";
 
     protected Task[] tasks;
+    private Path dataPath;
 
     public TaskHandler() {
         this.tasks = new Task[MAX_TASKS];
+        openFile();
+    }
+
+    private void openFile() {
+        try {
+            String home = System.getProperty("user.dir");
+            Path dataDirPath = Paths.get(home, "/data/");
+            Files.createDirectories(dataDirPath);
+            Path dataPath = Paths.get(home, "/data/duke.txt");
+            if (!Files.exists(dataPath)) {
+                Files.createFile(dataPath);
+            }
+            this.dataPath = dataPath;
+        } catch (IOException e) {
+            System.err.println("Open failure: " + e.getMessage());
+        }
     }
 
     public String handleTasks(String line) throws IllegalArgumentException, DukeException {
-            String lc = line.toLowerCase();
-            if (inputIsList(lc)) {
-                return listTasks();
-            } else if (inputIsDone(lc)) {
-                return doTask(lc);
-            } else if (inputIsTodo(lc)) {
-                return addTodo(line);
-            } else if (inputIsDeadline(lc)) {
-                if (!deadlineContainsBy(lc)) {
-                    return returnDeadlineNoBy();
-                } else {
-                    return addDeadline(line);
-                }
-            } else if (inputIsEvent(lc)) {
-                if (!eventContainsAt(lc)) {
-                    return returnEventNoAt();
-                } else {
-                    return addEvent(line);
-                }
+        String lc = line.toLowerCase();
+        if (inputIsList(lc)) {
+            return listTasks();
+        } else if (inputIsDone(lc)) {
+            return doTask(lc);
+        } else if (inputIsClear(lc)) {
+            return clearFileData();
+        } else if (inputIsTodo(lc)) {
+            return addTodo(line);
+        } else if (inputIsDeadline(lc)) {
+            if (!deadlineContainsBy(lc)) {
+                return returnDeadlineNoBy();
             } else {
-                throw new DukeException(returnInputInvalid());
+                return addDeadline(line);
             }
+        } else if (inputIsEvent(lc)) {
+            if (!eventContainsAt(lc)) {
+                return returnEventNoAt();
+            } else {
+                return addEvent(line);
+            }
+        } else {
+            throw new DukeException(returnInputInvalid());
+        }
     }
 
     public boolean inputIsTodo(String lc) {
@@ -70,6 +96,10 @@ public class TaskHandler {
         return lc.startsWith("done");
     }
 
+    public boolean inputIsClear(String lc) {
+        return lc.startsWith("clear");
+    }
+
     public boolean inputIsBye(String lc) {
         return lc.equals("bye");
     }
@@ -85,6 +115,7 @@ public class TaskHandler {
         String description = line.substring(5).trim();
         Todo newTodo = new Todo(description);
         tasks[Task.getNumOfTasks() - 1] = newTodo;
+        appendLinetoFileData(newTodo.toString());
         return returnAddTaskSuccess() + newTodo.toString();
     }
 
@@ -101,6 +132,7 @@ public class TaskHandler {
         }
         Deadline newDeadline = new Deadline(description, by);
         tasks[Task.getNumOfTasks() - 1] = newDeadline;
+        appendLinetoFileData(newDeadline.toString());
         return returnAddTaskSuccess() + newDeadline.toString();
     }
 
@@ -117,6 +149,7 @@ public class TaskHandler {
         }
         Event newEvent = new Event(description, at);
         tasks[Task.getNumOfTasks() - 1] = newEvent;
+        appendLinetoFileData(newEvent.toString());
         return returnAddTaskSuccess() + newEvent.toString();
     }
 
@@ -137,8 +170,10 @@ public class TaskHandler {
             String inputNumStr = lc.replace("done", "").trim();
             int inputNum = Integer.parseInt(inputNumStr);
             if (inputNum > 0 && inputNum <= Task.getNumOfTasks()) {
+                String result;
                 int id = inputNum - 1;
                 tasks[id].setDone();
+                editFileData(id, tasks[id].toString());
                 return returnTaskCompleted() + System.lineSeparator()
                         + Formatter.returnOutputStart() + tasks[id].toString();
             } else {
@@ -155,10 +190,14 @@ public class TaskHandler {
 
     public String listTasks() {
         String out = "Your magnificent tasks:";
-        for (int i = 0; i < tasks.length && tasks[i] != null; i++) {
-            //output will be doubly "indented"
-            out += System.lineSeparator() + Formatter.returnOutputStart()
-                    + tasks[i].toString();
+        try {
+            List<String> lines = Files.readAllLines(dataPath);
+            for (String line : lines) {
+                out += System.lineSeparator() + Formatter.returnOutputStart()
+                        + line;
+            }
+        } catch (IOException e) {
+            System.err.println("Read failure: " + e.getMessage());
         }
         return out;
     }
@@ -166,5 +205,35 @@ public class TaskHandler {
     public String returnInputInvalid() {
         return "I do not comprehend, my liege.";
     }
-}
 
+    private void appendLinetoFileData(String line) {
+        try {
+            List<String> lines = Files.readAllLines(dataPath);
+            lines.add(line);
+            //Path tempFile = Files.createTempFile("tmp", ".txt");
+            Files.write(dataPath, lines);
+        } catch (IOException e) {
+            System.err.println("Write failure: " + e.getMessage());
+        }
+    }
+
+    private void editFileData(int index, String line) {
+        try {
+            List<String> lines = Files.readAllLines(dataPath);
+            lines.set(index, line);
+            //Path tempFile = Files.createTempFile("tmp", ".txt");
+            Files.write(dataPath, lines);
+        } catch (IOException e) {
+            System.err.println("Write failure: " + e.getMessage());
+        }
+    }
+
+    private String clearFileData() {
+        try {
+            Files.write(dataPath, Collections.EMPTY_LIST);
+        } catch (IOException e) {
+            System.err.println("Write failure: " + e.getMessage());
+        }
+        return "A clean slate, my liege!";
+    }
+}
