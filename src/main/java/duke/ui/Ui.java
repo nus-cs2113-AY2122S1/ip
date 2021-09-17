c770c2dd1d9061270ed51de9f885b6243fa4cc79
rizemon <tan.jia.le.98@gmail.com>
2021-09-17 21:13:26 +0800

    Add description of Ui.java

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import duke.task.Task;
 
+/**
+ * The Ui class deals with printing of messages and reading of user input.
+ */
 public class Ui {
 
     private final static String LINE = "____________________________________________________________";
