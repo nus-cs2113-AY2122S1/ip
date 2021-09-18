0ef7ded23176fca4ff153bf7e3036404c5178ddd
theeugenechong <eugenechong20011@yahoo.com>
2021-09-19 02:44:40 +0800

    Add Scanner and PrintStream to Ui class for user interaction

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -2,7 +2,9 @@ package duke.ui;
 
 import duke.task.Task;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import static duke.constants.DukeConstants.*;
 import static duke.constants.DukeOutputMessages.*;
@@ -17,94 +19,101 @@ public class Ui {
     /** Format of task to be printed in list */
     private static final String DISPLAYED_TASK_FORMAT = " %1$d. %2$s";
 
+    private static final Scanner in = new Scanner(System.in);
+    private static final PrintStream out = System.out;
+
+    public static String readUserInput() {
+        return in.nextLine();
+    }
+
     public static void printHeyMessage() {
-        System.out.println("Hello from" + System.lineSeparator() + DUKE_LOGO);
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(HEY_MESSAGE);
-        System.out.println(STRAIGHT_LINE);
+        out.println("Hello from" + System.lineSeparator() + DUKE_LOGO);
+        out.println(STRAIGHT_LINE);
+        out.println(HEY_MESSAGE);
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printTaskList(ArrayList<Task> taskList) {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(HERE_IS_TASK_LIST_MESSAGE);
+        out.println(STRAIGHT_LINE);
+        out.println(HERE_IS_TASK_LIST_MESSAGE);
         for (int i = 0; i < taskList.size(); i++) {
             String formattedTask = formatTaskToBeDisplayed(i + DISPLAYED_INDEX_OFFSET, taskList.get(i));
-            System.out.println(formattedTask);
+            out.println(formattedTask);
         }
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printTaskMarkedDoneMessage(Task doneTask) {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(TASK_MARKED_DONE_MESSAGE);
-        System.out.println(INDENT + doneTask);
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
+        out.println(TASK_MARKED_DONE_MESSAGE);
+        out.println(INDENT + doneTask);
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printTaskAddedMessage(Task newTask, int taskListSize) {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(TASK_ADDED_MESSAGE);
-        System.out.println(INDENT + newTask);
-        System.out.println(WHITESPACE + "You now have " + taskListSize + " task(s) in the list.");
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
+        out.println(TASK_ADDED_MESSAGE);
+        out.println(INDENT + newTask);
+        out.println(WHITESPACE + "You now have " + taskListSize + " task(s) in the list.");
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printTaskDeletedMessage(Task deletedTask, int taskListSize) {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(TASK_DELETED_MESSAGE);
-        System.out.println(INDENT + deletedTask);
-        System.out.println(WHITESPACE + "You now have " + (taskListSize - 1) + " task(s) in the list." );
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
+        out.println(TASK_DELETED_MESSAGE);
+        out.println(INDENT + deletedTask);
+        out.println(WHITESPACE + "You now have " + (taskListSize - 1) + " task(s) in the list." );
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printEmptyListMessage() {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(TASK_LIST_EMPTY_MESSAGE);
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
+        out.println(TASK_LIST_EMPTY_MESSAGE);
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printInvalidCommandFormatMessage() {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(COMMAND_WRONG_FORMAT_MESSAGE);
-        System.out.println(ENTER_HELP);
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
+        out.println(COMMAND_WRONG_FORMAT_MESSAGE);
+        out.println(ENTER_HELP);
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printTaskNotInListMessage() {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(TASK_NOT_IN_LIST_MESSAGE);
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
+        out.println(TASK_NOT_IN_LIST_MESSAGE);
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printHelp() {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(HELP_INTRO_MESSAGE);
-        System.out.println(LIST_COMMAND_FORMAT);
-        System.out.println(DONE_COMMAND_FORMAT);
-        System.out.println(TODO_COMMAND_FORMAT);
-        System.out.println(DEADLINE_COMMAND_FORMAT);
-        System.out.println(EVENT_COMMAND_FORMAT);
-        System.out.println(DELETE_COMMAND_FORMAT);
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
+        out.println(HELP_INTRO_MESSAGE);
+        out.println(LIST_COMMAND_FORMAT);
+        out.println(DONE_COMMAND_FORMAT);
+        out.println(TODO_COMMAND_FORMAT);
+        out.println(DEADLINE_COMMAND_FORMAT);
+        out.println(EVENT_COMMAND_FORMAT);
+        out.println(DELETE_COMMAND_FORMAT);
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printFileTaskInvalidFormatMessage() {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(FILE_WRONG_FORMAT_MESSAGE);
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
+        out.println(FILE_WRONG_FORMAT_MESSAGE);
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printUnrecognizedCommandMessage() {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(UNRECOGNIZED_COMMAND_MESSAGE);
-        System.out.println(ENTER_HELP);
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
+        out.println(UNRECOGNIZED_COMMAND_MESSAGE);
+        out.println(ENTER_HELP);
+        out.println(STRAIGHT_LINE);
     }
 
     public static void printByeMessage() {
-        System.out.println(STRAIGHT_LINE);
-        System.out.println(BYE_MESSAGE);
-        System.out.println(STRAIGHT_LINE);
+        out.println(STRAIGHT_LINE);
+        out.println(BYE_MESSAGE);
+        out.println(STRAIGHT_LINE);
     }
 
     private static String formatTaskToBeDisplayed(int displayedIndex, Task task) {
