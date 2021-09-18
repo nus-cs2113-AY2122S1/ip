c55a8941da4a873cbb3ed7622736c6992fa8e156
theodorekwok <theodorekwok@gmail.com>
2021-09-18 18:58:54 +0800

    Add javadocs for task class

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -1,10 +1,19 @@
 package task;
 
+/**
+ * Represents a task in the TaskManager.
+ * It contains task basic information like name and completion status.
+ */
 public class Task {
 
     private String taskName;
     private Boolean isCompleted;
 
+    /**
+     * Creates a task and set its name and completion status.
+     *
+     * @param taskName Task name provided by user
+     */
     public Task(String taskName) {
         this.taskName = taskName;
         isCompleted = false;
