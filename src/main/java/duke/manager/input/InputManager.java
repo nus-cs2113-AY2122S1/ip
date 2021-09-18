2c88644584cb0d10095266c540de3d8d031bf68e
arvejw <jiewen.tehh@gmail.com>
2021-09-18 22:33:42 +0800

    Add JavaDoc to input package

--- src/main/java/duke/manager/input/InputManager.java
+++ src/main/java/duke/manager/input/InputManager.java
@@ -5,6 +5,15 @@ import duke.ui.UserInterface;
 
 import java.util.Scanner;
 
+/**
+ * <h1>InputManager</h1>
+ * This class manages inputs. <code>inputCommand</code> is an enum for commands which is assigned by
+ * <code>setInputCommand()</code> whenever <code>readInput()</code> is called. <code>commandArguments</code>, which
+ * represents the arguments tied to the inputted command, is also assigned a value whenever <code>readInput()</code>
+ * is called.
+ * <code>inputCommand</code> and <code>commandArguments</code> can subsequently be read through their getter methods
+ * when executing commands.
+ */
 public class InputManager {
 
     private Scanner scanner;
@@ -60,11 +69,15 @@ public class InputManager {
         return commandArguments;
     }
 
+    /**
+     * Reads in user input and parses it.
+     * <code>inputCommand</code> and <code>commandArguments</code> are then set based on the parsed input.
+     */
     public void readInput() {
         System.out.print(UserInterface.INPUT_PROMPT);
         String input = scanner.nextLine();
         Parser parsedInput = new Parser(input);
-        if (parsedInput.isMoreThanTwoWords()) {
+        if (parsedInput.isMoreThanOneWord()) {
             String firstWord = parsedInput.getFirstWord();
             setInputCommand(firstWord);
             commandArguments = parsedInput.getArguments()[1].trim();
