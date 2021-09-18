a34a5f06de8da08cc2acfe7313da3b6ad3ce77f2
xuanlc113 <xuan.lc113@gmail.com>
2021-09-18 15:22:10 +0800

    rename parser hashmap variable

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -18,16 +18,16 @@ public class Parser {
      */
     public static HashMap<String, String> parseCommand(String command) {
         String[] parts = command.split(" /");
-        HashMap<String, String> map = new HashMap<String, String>();
+        HashMap<String, String> commandArgs = new HashMap<String, String>();
         String[] commandParts = parts[0].split(" ", 2);
-        map.put("command", commandParts[0]);
-        map.put("param", commandParts.length > 1 ? commandParts[1] : "");
+        commandArgs.put("command", commandParts[0]);
+        commandArgs.put("param", commandParts.length > 1 ? commandParts[1] : "");
         for (int i = 1; i < parts.length; i++) {
             String[] argParts = parts[i].split(" ", 2);
             if (argParts.length == 2) {
-                map.put(argParts[0], argParts[1]);
+                commandArgs.put(argParts[0], argParts[1]);
             }
         }
-        return map;
+        return commandArgs;
     }
 }
