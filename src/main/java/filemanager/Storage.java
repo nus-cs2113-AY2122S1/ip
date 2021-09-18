13e5b804061ffe425249f8838100671b92c002fe
theodorekwok <theodorekwok@gmail.com>
2021-09-18 20:59:29 +0800

    Add javadocs for storage class

--- src/main/java/filemanager/Storage.java
+++ src/main/java/filemanager/Storage.java
@@ -12,18 +12,34 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Storage class manages the storage file that stores saved tasks.
+ */
 public class Storage {
 
-    public final File taskFile;
-    public final Path path;
-
+    /** Default path and file name. */
     public static final String FILE_PATH = "data/savedTasks.txt";
 
+    private final File taskFile;
+    private final Path path;
+
+    /**
+     * Creates the Storage and set the file details.
+     *
+     * @throws IOException If there is an error in the file path.
+     */
     public Storage() throws IOException {
         taskFile = new File(FILE_PATH);
         path = Paths.get(FILE_PATH);
     }
 
+    /**
+     * Loads the saved task data from the storage file into TaskManager.
+     * If the savedTasks.txt file does not exist, it creates one.
+     *
+     * @param taskManager TaskManager that stores the saved task data.
+     * @throws IOException If there is an error in the creation or reading of file.
+     */
     public void loadFileData(TaskManager taskManager) throws IOException {
         if (!Files.exists(path) || !Files.isRegularFile(path)) {
             Display.displayCreateFile();
@@ -35,6 +51,12 @@ public class Storage {
         TaskDecoder.parseFile(Files.readAllLines(path), taskManager);
     }
 
+    /**
+     * Overwrites the storage file with the latest task data from the TaskManager.
+     *
+     * @param allTasks All tasks objects managed by the TaskManager.
+     * @throws IOException If there is an error in writing to the file.
+     */
     public void updateFileData(ArrayList<Task> allTasks) throws IOException {
         List<String> encodedTaskList = TaskEncoder.encodeTaskList(allTasks);
         Files.write(path, encodedTaskList);
