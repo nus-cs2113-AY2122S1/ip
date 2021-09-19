4b69f0b9129a001e7597178d3827e83028681f6f
mudkip8 <mahvin1970@hotmail.com>
2021-09-19 20:31:40 +0800

    Added Error Messages for Exceptions

--- src/main/java/Exceptions/InsufficientParametersException.java
+++ src/main/java/Exceptions/InsufficientParametersException.java
@@ -4,4 +4,8 @@ public class InsufficientParametersException extends Exception{
     /*  This exception is thrown when the input by the user contains insufficient parameters
         For example, a deadline task without a "/by" or an event without a "/at"
      */
+    @Override
+    public String getMessage() {
+        return "There are insufficient parameters in your input. Type \"help\" for command syntax.";
+    }
 }
--- src/main/java/Exceptions/UnknownCommandException.java
+++ src/main/java/Exceptions/UnknownCommandException.java
@@ -1,5 +1,9 @@
 package Exceptions;
 
 public class UnknownCommandException extends Exception{
-    //Exception is thrown when the command received is an invalid command
+
+    @Override
+    public String getMessage() {
+        return "Unknown Command Entered. Type \"help\" for a list of available commands.";
+    }
 }
