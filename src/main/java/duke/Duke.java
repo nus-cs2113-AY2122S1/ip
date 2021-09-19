f6efa9f38a28b58635a2d582fb9ef6de8c54d7c5
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-09-19 18:08:57 +0800

    create .run()

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -22,36 +22,62 @@ public class Duke {
     public static int numberOfTasks = 0; // Task Counter
     public static ArrayList<Task> tasks = new ArrayList<>();
 
-    private Ui ui;
-
     public static int getNumberOfTasks() {
         return numberOfTasks;
     }
 
-    /*
-    public void run() {
-        public void run() {
-            ui.showWelcome();
-            boolean isExit = false;
-            while (!isExit) {
-                try {
-                    String fullCommand = ui.readCommand();
-                    ui.showLine(); // show the divider line ("_______")
-                    Command c = Parser.parse(fullCommand);
-                    c.execute(tasks, ui, storage);
-                    isExit = c.isExit();
-                } catch (DukeException e) {
-                    ui.showError(e.getMessage());
-                } finally {
-                    ui.showLine();
+    public static void run() throws IOException {
+        Ui ui = new Ui();
+        ui.showWelcome();
+
+        String userInputString;
+        Scanner userInput = new Scanner(System.in);
+        SaveTaskListToText dukeTaskText = getSaveTaskListToText();
+
+        boolean isExit = false;
+        while (!isExit) {
+            userInputString = userInput.nextLine();
+            DukeCommandHandling commandHandle = new DukeCommandHandling(userInputString);
+
+            try {
+                if (commandHandle.isBye()) {
+                    break;
+                } else if (commandHandle.isList()) {
+                    TaskList.printTaskList();
+                    continue;
+                } else if (commandHandle.isDone()) {
+                    TaskList.finishTask(userInputString, dukeTaskText);
+                    continue;
+                } else if (commandHandle.isDelete()) {
+                    TaskList.deleteTask(userInputString, dukeTaskText);
+                } else if (commandHandle.isToDo()) {
+                    TaskList.addToDo(userInputString, dukeTaskText);
+                    continue;
+                } else if (commandHandle.isDeadline()) {
+                    TaskList.addDeadline(userInputString, dukeTaskText);
+                    continue;
+                } else if (commandHandle.isEvent()) {
+                    TaskList.addEvent(userInputString, dukeTaskText);
+                    continue;
+                } else {
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
                 }
+            } catch (DukeException dukeError) {
+                dukeError.printErrorMessage();
             }
         }
-
+        userInput.close();
+        Ui.printByeText.printText();
     }
-    */
+
+    public static SaveTaskListToText getSaveTaskListToText() throws IOException {
+        SaveTaskListToText dukeTaskText = new SaveTaskListToText();
+        Duke.numberOfTasks = dukeTaskText.loadTask(Duke.tasks);
+        return dukeTaskText;
+    }
 
     public static void main(String[] args) throws IOException, DukeException {
+        // run();
         String userInputString;
         Scanner userInput = new Scanner(System.in);
 
@@ -62,8 +88,8 @@ public class Duke {
         delay.wait(500);
         Ui.printHelloText.printText();
 
-        SaveTaskListToText dukeTaskText = new SaveTaskListToText();
-        Duke.numberOfTasks = dukeTaskText.loadTask(Duke.tasks);
+        SaveTaskListToText dukeTaskText = getSaveTaskListToText();
+
 
         while (true) {
             userInputString = userInput.nextLine();
@@ -98,6 +124,6 @@ public class Duke {
 
         userInput.close();
         Ui.printByeText.printText();
-    }
 
+    }
 }
