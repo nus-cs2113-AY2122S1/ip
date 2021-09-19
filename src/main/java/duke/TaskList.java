39b5aa7692c1f7a89fdd0aff13bcb0db9cf4e637
Joshua Harsha Dass <joshua.h.dass@gmail.com>
2021-09-19 22:32:46 +0800

    add find function

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -7,14 +7,17 @@ public class TaskList {
 
     //attributes
     static ArrayList<Task> list;
+    static ArrayList<Task> searchList;
     int taskCount = 0;
 
     //constructor
     public TaskList() {
         list = new ArrayList<>();
+        searchList = new ArrayList<>();
     }
     public TaskList(ArrayList<Task> list) {
         this.list = list;
+        searchList = new ArrayList<>();
     }
 
 
@@ -24,6 +27,15 @@ public class TaskList {
         System.out.println("    Here are the tasks in your list:");
         for( int i = 0 ; i < list.size() ; i ++ ) {
             System.out.println( "    " + position  + "." + list.get(i));
+            position ++;
+        }
+    }
+
+    public static void printSearchList() {
+        int position = 1;
+        System.out.println("    Here are the matching tasks in your list:");
+        for( int i = 0 ; i < searchList.size() ; i ++ ) {
+            System.out.println( "    " + position  + "." + searchList.get(i));
             position ++;
         }
     }
@@ -71,6 +83,20 @@ public class TaskList {
         System.out.println("    Now you have " + (size - 1) + " tasks in the list.");
     }
 
+    public static void findTask(String phrase) {
+
+        for(Task task: list) {
+            if(task.description.contains(phrase)) {
+                searchList.add(task);
+            }
+        }
+
+        //print the search list
+        printSearchList();
+        searchList.clear();
+
+
+    }
 
     public static Deadlines createNewDeadline(String command) {
         String[] words = command.split(" ");
