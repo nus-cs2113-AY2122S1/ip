aef23aad47146ef86baf0efcf5e60fd0d21cd32c
mudkip8 <mahvin1970@hotmail.com>
2021-09-19 16:59:54 +0800

    Added A Storage Class

---
+++ src/main/java/Duke/Storage.java
@@ -0,0 +1,100 @@
+package Duke;
+
+import Tasks.Deadline;
+import Tasks.Event;
+import Tasks.Task;
+import Tasks.Todo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+
+    private ArrayList<Task> loadedTaskList = new ArrayList<>();
+
+    public Storage() {
+    }
+
+    /*
+        This section contains the relevant code for the loading of the stored task file
+     */
+    /**
+     * Checks for the existence of the {@code data} folder, and creates one if it does not exist
+     * @return loadedTaskList The Tasks read from the data file
+     * @throws FileNotFoundException if the {@code data} folder cannot be found
+     */
+    public ArrayList<Task> load() throws FileNotFoundException {
+        File taskFile = new File("data");
+        boolean isDir = Files.isDirectory(Path.of(taskFile.getAbsolutePath()));
+        if (!isDir) {
+            taskFile.mkdir();
+            return loadedTaskList;
+        }
+        readFile();
+        return loadedTaskList;
+    }
+
+    /**
+     * Reads the contents of the {@code tasks} file
+     * @throws FileNotFoundException if the {@code tasks} file does not exist
+     */
+    public void readFile() throws FileNotFoundException {
+        File taskFile = new File("data/tasks.txt");
+        Scanner scanner = new Scanner(taskFile);
+        if (taskFile.exists() && !taskFile.isDirectory()) {
+            while (scanner.hasNext()) {
+                processStoredTasks(scanner.nextLine());
+            }
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * Processes the data stored in the task file and stores it in loadedTaskList
+     * @param taskString
+     */
+    public void processStoredTasks(String taskString) {
+        String[] taskDetails = taskString.split("\\|", 4);
+        boolean isDone = taskDetails[1].trim().equals("X");
+        switch (taskDetails[0].trim()) {
+        case "T":
+            loadedTaskList.add(new Todo(taskDetails[2].trim(), isDone));
+            break;
+        case "D":
+            String[] dueDate = taskDetails[3].split("by:");
+            loadedTaskList.add(new Deadline(taskDetails[2].trim(), isDone, dueDate[1].trim()));
+            break;
+        case "E" :
+            String[] startDate = taskDetails[3].split("at:");
+            loadedTaskList.add(new Event(taskDetails[2].trim(), isDone, startDate[1].trim()));
+            break;
+        default:
+            break;
+        }
+    }
+    
+    /*
+        This section contains the relevant codes for the writing of the task list to the stored file
+     */
+
+    /**
+     * Saves the current tasks stored in task list to the text file.
+     * @param taskList the arraylist containing the current tasks
+     * @throws IOException if error writing to the file
+     */
+    public static void saveTasks(ArrayList<Task> taskList) throws IOException {
+        FileWriter fw = new FileWriter("data/tasks.txt");
+        for (Task t : taskList) {
+            fw.write(t.toString() + System.lineSeparator());
+        }
+        fw.close();
+    }
+
+}
