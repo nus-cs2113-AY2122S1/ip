5ffa6df70d8f6f03ae92789c9224d938efcbef37
mudkip8 <mahvin1970@hotmail.com>
2021-09-20 09:57:47 +0800

    Updated to include Find Command

--- src/main/java/Commands/HelpCommand.java
+++ src/main/java/Commands/HelpCommand.java
@@ -10,9 +10,10 @@ public class HelpCommand extends Command{
     public static final String SUCCESS_MESSAGE = "The following is the list of commands: \n" +
             "list \nDisplays the current tasks. \n" +
             "todo <task> \nAdds <task> to the task list \n" +
-            "deadline <task> /by <date>\nAdds <task> to task list with deadline <date> \n" +
-            "event <task> /at <time>\nAdds <task> to task list with time <time> \n" +
+            "deadline <task> /by <YYYY-MM-DD>T<HH:MM:SS>\nAdds <task> to task list with deadline <datetime> \n" +
+            "event <task> /at <YYYY-MM-DD>T<HH:MM:SS>\nAdds <task> to task list with time <datetime> \n" +
             "done <task index>\nMarks task number <task index> as done. <task index> should be an integer\n" +
+            "find <text to search>\nSearches for Tasks containing the given text string\n" +
             "exit\nExits the program\n";
     public HelpCommand(){
     }
