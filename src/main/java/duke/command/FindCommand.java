c9fb86a210b02f43c5941f48d4cd26a37fe93787
Leyond <leyondlee@users.noreply.github.com>
2021-09-21 04:00:43 +0800

    Fix wrong import

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -1,8 +1,8 @@
 package duke.command;
 
 import duke.DukeException;
-import duke.Storage;
-import duke.Ui;
+import duke.Util.Storage;
+import duke.Util.Ui;
 import duke.task.Task;
 import duke.task.TaskList;
 
