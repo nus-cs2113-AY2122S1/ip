7ebbe1397ca84d13c9016f4ce1fadc39face9217
mudkip8 <mahvin1970@hotmail.com>
2021-09-20 09:57:36 +0800

    Updated to support localDateTime class

--- src/main/java/Commands/DeadlineCommand.java
+++ src/main/java/Commands/DeadlineCommand.java
@@ -3,19 +3,21 @@ package Commands;
 import Tasks.TaskList;
 import Tasks.Deadline;
 
+import java.time.LocalDateTime;
+
 /**
  * Adds a deadline task to the task list
  */
 public class DeadlineCommand extends Command {
 
     public String description;
-    public String dueDate;
+    public LocalDateTime dueDate;
     public static final String SUCCESS_MESSAGE = "The following deadline task has been added\n";
 
     public DeadlineCommand(String input){
         String[] inputParts = input.split("/by");
         description = inputParts[0].trim();
-        dueDate = inputParts[1].trim();
+        dueDate = LocalDateTime.parse(inputParts[1].trim());
     }
 
     @Override
