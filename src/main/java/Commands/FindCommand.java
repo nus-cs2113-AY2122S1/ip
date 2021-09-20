36b0a4a270ef278b2db6cd7730c5203c9d867111
mudkip8 <mahvin1970@hotmail.com>
2021-09-20 11:19:55 +0800

    Updated Message

--- src/main/java/Commands/FindCommand.java
+++ src/main/java/Commands/FindCommand.java
@@ -8,7 +8,7 @@ import Tasks.TaskList;
 public class FindCommand extends Command{
 
     public static String stringToFind;
-    public static final String SUCCESS_MESSAGE = "The following task have been found:\n";
+    public static final String SUCCESS_MESSAGE = "The following tasks have been found:\n";
 
     public FindCommand(String text){
         stringToFind = text;
