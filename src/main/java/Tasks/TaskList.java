52c7ec8bd50973b2caab1dcd95d4474004ba6055
mudkip8 <mahvin1970@hotmail.com>
2021-09-20 09:58:37 +0800

    Updated text

--- src/main/java/Tasks/TaskList.java
+++ src/main/java/Tasks/TaskList.java
@@ -1,13 +1,11 @@
 package Tasks;
 
-import Tasks.Task;
-
 import java.util.ArrayList;
 
 public class TaskList {
 
     private ArrayList<Task> taskList;
-    private static final String NO_TASK_MESSAGE = "No tasks";
+    private static final String NO_TASK_MESSAGE = "No tasks\n";
 
     public TaskList(ArrayList<Task> taskList) {
         this.taskList = taskList;
