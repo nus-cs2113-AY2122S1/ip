da4fb15cb191ac20d14feb382b358e69a012ee3a
JaCh23 <jach2355@gmail.com>
2021-09-22 02:31:47 +0800

    A-Exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,8 @@
 package ip.src.main.java;
 import java.util.Objects;
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.ArrayList;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class Duke {
