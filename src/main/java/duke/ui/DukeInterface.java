eecf80159662fcec548970042e24f53297480a7d
APZH <amospzh@gmail.com>
2021-09-21 23:19:40 +0800

    Deleted unecessary methods from DukeInterface.java
    - Reduce number of printing method of duke to 1 as the message formatting and printing is now done as a already modified string created the TaskManager methods

--- src/main/java/duke/ui/DukeInterface.java
+++ src/main/java/duke/ui/DukeInterface.java
@@ -16,17 +16,11 @@ public class DukeInterface {
     private final String WELCOME_MSG = "=> Hello! I'm Duke \uD83D\uDC3B\n"
             + "=> I'm so hungry, would you mind feeding me tasks?";
 
-    private final String EXIT_MSG = "=> Come back soon, I'm still hungry \uD83D\uDE0B";
-
-    private final String HELP_MSG = "Types of Commands Available [\uD83D\uDCAC]:\n"
-            + "1. Terminate Duke            -> bye \n"
-            + "2. Print Tasklist            -> list \n"
-            + "3. Print Available Commands  -> help \n"
-            + "4. Add Todos                 -> {todo <task description>}\n"
-            + "5. Add Deadlines             -> {deadline <task description> /by <task date&time>}\n"
-            + "6. Add Events                -> {event <task description> /at <task date&time>}\n"
-            + "7. Set Task as Done          -> {done <task ID>}\n"
-            + "8. Delete Task               -> {delete <task ID>}";
+    public void printWelcomeMsg() {
+        System.out.println(LOGO);
+        printDukeName();
+        System.out.println(WELCOME_MSG);
+    }
 
     public void printUserName() {
         System.out.println("\n[You]:");
@@ -40,28 +34,10 @@ public class DukeInterface {
         System.out.print("=> ");
     }
 
-    public void printMsgWithCursor(String message) {
-        System.out.print("=> " + message + "\n");
-    }
-
-    public void printWithPadding(String message) {
-        System.out.println("   <" + message + ">");
-    }
-
-    public void printWelcomeMsg() {
-        System.out.println(LOGO);
+    public void printDukeMessage(String dukeMessage) {
         printDukeName();
-        System.out.println(WELCOME_MSG);
+        printCursor();
+        System.out.println(dukeMessage);
     }
-
-    public void printExitMsg() {
-        printDukeName();
-        System.out.println(EXIT_MSG);
-    }
-
-    public void printHelpMsg() {
-        printDukeName();
-        System.out.println(HELP_MSG);
-    }
-
+    
 }
