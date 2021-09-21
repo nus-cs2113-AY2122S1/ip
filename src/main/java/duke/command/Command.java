60f1db4d3208d667a22b5219afcf6daa2b3a32bf
AnShengLee <anshengl@yahoo.com.sg>
2021-09-21 16:52:02 +0800

    Add comments

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -1,5 +1,8 @@
 package duke.command;
 
+/**
+ * All the possible commands the user can give stored as constants.
+ */
 public enum Command {
     EXIT,
     LIST,
--- src/main/java/duke/exception/DukeBlankDescriptionsException.java
+++ src/main/java/duke/exception/DukeBlankDescriptionsException.java
@@ -1,4 +1,7 @@
 package duke.exception;
 
+/**
+ * An Exception that will be thrown if description of Task given by user is an empty or blank String.
+ */
 public class DukeBlankDescriptionsException extends Exception {
 }
