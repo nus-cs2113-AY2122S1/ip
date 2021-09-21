4fffd15588113f8c5eb7d9daa125d7bc19cd5a5d
brendanlsz <brendanlau2926@gmail.com>
2021-09-21 11:31:50 +0800

    Add separate commands for adding tasks

---
+++ src/main/java/duke/command/AddEvent.java
@@ -0,0 +1,23 @@
+package duke.command;
+
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.TaskManager;
+import duke.ui.Ui;
+
+public class AddEvent extends Command {
+    private final String description;
+    private final String timeDescription;
+
+    public AddEvent(String[] details) {
+        this.description = details[0];
+        this.timeDescription = details[1];
+    }
+
+    public void execute(TaskManager taskManager, Ui ui) {
+        Task addedTask = new Event(description, timeDescription);
+        taskManager.addTask(addedTask);
+        ui.printAddTask(addedTask, taskManager);
+    }
+}
