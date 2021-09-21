335e52c81d678948947c2108f16344304d255953
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-09-22 00:04:09 +0800

    Implement TaskDecoder to scan storage file and parse correct data into Storage

--- src/main/java/alfred/storage/TaskDecoder.java
+++ src/main/java/alfred/storage/TaskDecoder.java
@@ -1,4 +1,57 @@
 package alfred.storage;
 
+import alfred.exception.FileErrorException;
+import alfred.task.Deadline;
+import alfred.task.Event;
+import alfred.task.TaskList;
+import alfred.task.Todo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TaskDecoder {
+    private static final String SEPARATOR = ",";
+
+    public static TaskList scanTasks(File taskFile) throws FileErrorException {
+        TaskList scannedTaskList = new TaskList();
+        int taskIndex = 0;
+        try {
+            Scanner fileScanner = new Scanner(taskFile);
+            while (fileScanner.hasNext()) {
+                String inputLine = fileScanner.nextLine();
+                decodeAndLoadTask(scannedTaskList, inputLine, taskIndex);
+                taskIndex++;
+            }
+        } catch (FileNotFoundException e) {
+            throw new FileErrorException();
+        }
+        return scannedTaskList;
+    }
+
+    private static void decodeAndLoadTask(TaskList taskList, String input, int taskIndex) {
+        String[] destructuredInputs = input.split(SEPARATOR);
+        String taskType = destructuredInputs[0];
+        String taskStatus = destructuredInputs[1];
+        String taskDescription = destructuredInputs[2];
+        String taskDate;
+        switch (taskType) {
+        case "T":
+            taskList.addTask(new Todo(taskDescription));
+            break;
+        case "E":
+            taskDate = destructuredInputs[3];
+            taskList.addTask(new Event(taskDescription, taskDate));
+            break;
+        case "D":
+            taskDate = destructuredInputs[3];
+            taskList.addTask(new Deadline(taskDescription, taskDate));
+            break;
+        default:
+            return;
+        }
+        if (taskStatus.equals("true")) {
+            taskList.setTaskDoneInList(taskIndex);
+        }
+    }
 }
