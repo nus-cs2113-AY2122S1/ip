2c45b57191c64f2baedceb413a8989d2653bb062
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-09-22 00:01:23 +0800

    Implement TaskList class for operations on data structure

--- src/main/java/alfred/task/TaskList.java
+++ src/main/java/alfred/task/TaskList.java
@@ -1,4 +1,29 @@
 package alfred.task;
 
+import java.util.ArrayList;
+
 public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task removeTask(int index) {
+        Task removedTask = tasks.get(index);
+        tasks.remove(index);
+        return removedTask;
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+
+    public void setTaskDoneInList(int index) {
+        tasks.get(index).setTaskDone();
+    }
 }
