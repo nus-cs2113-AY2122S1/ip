2b7a26c3e2daa84316485c2e7475a5b1f58b7961
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-09-22 00:45:14 +0800

    Implement getTask for encoding

--- src/main/java/alfred/task/TaskList.java
+++ src/main/java/alfred/task/TaskList.java
@@ -19,6 +19,10 @@ public class TaskList {
         return removedTask;
     }
 
+    public Task getTask(int index) {
+        return tasks.get(index);
+    }
+
     public int getSize() {
         return tasks.size();
     }
