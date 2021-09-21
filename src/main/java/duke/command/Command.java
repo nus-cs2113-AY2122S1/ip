6bc87bf00cd0d35cdfab3deabfada6b8dbd9251d
APZH <amospzh@gmail.com>
2021-09-21 18:36:45 +0800

    Add abstract Command class that serves as parent class for future specific commands (E.g. AddCommand.java, ListCommand.java etc...)

---
+++ src/main/java/duke/command/Command.java
@@ -0,0 +1,17 @@
+package duke.command;
+
+import duke.task.TaskManager;
+
+public abstract class Command {
+
+    public TaskManager taskManager;
+    public String commandArguments;
+
+    public Command(TaskManager taskManager, String commandArguments) {
+        this.taskManager = taskManager;
+        this.commandArguments = commandArguments;
+    }
+
+    public abstract CommandResult executeCommand() throws Exception;
+
+}
