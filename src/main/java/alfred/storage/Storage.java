a9b6bc48af86eb05da57ba954fa8201b8c41eef9
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-09-22 00:17:22 +0800

    Implement Storage class to handle persistent file storage in chatbot app

--- src/main/java/alfred/storage/Storage.java
+++ src/main/java/alfred/storage/Storage.java
@@ -1,4 +1,41 @@
 package alfred.storage;
 
+import alfred.exception.FileErrorException;
+import alfred.task.TaskList;
+import alfred.ui.TextUi;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Storage {
+    private TaskList storage;
+    private final String DEFAULT_FILE_PATH = "data/alfred.txt";
+    private File taskFile;
+
+    public Storage() {
+        storage = new TaskList();
+    }
+
+    private boolean hasExistingFile() throws FileErrorException {
+        taskFile = new File(DEFAULT_FILE_PATH);
+        try {
+            if (!taskFile.exists()) {
+                taskFile.getParentFile().mkdirs();
+            }
+            if (taskFile.createNewFile()) {
+                TextUi.createNewFileMessage();
+                return false;
+            }
+        } catch (IOException e) {
+            throw new FileErrorException();
+        }
+        return true;
+    }
+
+    private TaskList load() throws FileErrorException {
+        if (!hasExistingFile()) {
+            return new TaskList();
+        }
+        return TaskDecoder.scanTasks(taskFile);
+    }
 }
