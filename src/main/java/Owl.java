41f8c60f2c77c96092ca1ca314569ba58e1fbea0
KZQ1999 <e0543569@u.nus.edu>
2021-09-22 12:28:01 +0800

    Added a parser class and change verifier to static instead

--- src/main/java/Owl.java
+++ src/main/java/Owl.java
@@ -3,48 +3,27 @@ import DukeUtility.OwlException;
 import DukeUtility.Ui;
 
 public class Owl {
-    public static final String INVALID_MESSAGE = "The command doesnt exist.....";
     public static final String FULL_RELATIVE_MEMORY_PATH = "data/owlmemory";
     
     private TaskList tasks;
     private FileWriter fileWriter;
     private Ui ui;
-    private Verifier verifier;
+    private Parser parser;
     
     public Owl(String filePath) {
         ui = new Ui();
-        verifier = new Verifier();
         fileWriter = new FileWriter(filePath);
         tasks = new TaskList(fileWriter.readFile());
+        parser = new Parser(tasks);
     }
     
     public void run() {
         ui.printWelcome();
         String command = ui.readCommand();
         
-        while(verifier.isNotBye(command)) {
+        while(Verifier.isNotBye(command)) {
             try {
-                String[] inputs = command.split(" ", 2);
-                int commandLength = inputs.length;
-                if(verifier.isInvalidOnePartCmd(inputs, commandLength)) {
-                    OwlException.checkException(inputs);
-                } else if (verifier.isInvalidTwoPartCmd(inputs, commandLength)) {
-                    OwlException.checkException(inputs);
-                } else if (verifier.isDone(inputs[0])) {
-                    tasks.markCompletionOfTask(tasks, inputs[1]);
-                } else if (verifier.isList(inputs[0])) {
-                    tasks.listTask(tasks);
-                } else if (verifier.isTodo(inputs[0])) {
-                    tasks.addTodo(tasks, inputs);
-                } else if (verifier.isDeadline(inputs[0])) {
-                    tasks.addDeadline(tasks, inputs);
-                } else if (verifier.isEvent(inputs[0])) {
-                    tasks.addEvent(tasks, inputs);
-                } else if (verifier.isDelete(inputs[0])) {
-                    tasks.deleteTask(tasks, inputs[1]);
-                } else {
-                    System.out.println(INVALID_MESSAGE);
-                }
+                parser.execute(command);
             } catch(OwlException owlException) {
                 owlException.printErrorMsg();
             } catch(NumberFormatException numberFormatException) {
@@ -55,6 +34,7 @@ public class Owl {
         }
         ui.printBye();
     }
+    
     
     public static void main(String[] args) {
        new Owl(FULL_RELATIVE_MEMORY_PATH).run();
--- src/main/java/Verifier.java
+++ src/main/java/Verifier.java
@@ -7,28 +7,28 @@ public class Verifier {
     public static final List<String> TWO_PART_COMMAND = Arrays.asList("todo", "done", "deadline", "event", "delete");
     
     
-    public boolean isDelete(String input) {
+    public static boolean isDelete(String input) {
         return input.equals("delete");
     }
 
 
-    public boolean isInvalidOnePartCmd(String[] inputs, int commandLength) {
+    public static boolean isInvalidOnePartCmd(String[] inputs, int commandLength) {
         return commandLength > 1 && isOnePartCmd(inputs[0]);
     }
 
-    public boolean isNotBye(String command) {
+    public static boolean isNotBye(String command) {
         return !command.equals("bye");
     }
 
-    public boolean isInvalidTwoPartCmd(String[] inputs, int commandLength) {
+    public static boolean isInvalidTwoPartCmd(String[] inputs, int commandLength) {
         return commandLength == 1 && isTwoPartCmd(inputs[0]);
     }
     
-    public boolean isOnePartCmd(String s) {
+    public static boolean isOnePartCmd(String s) {
         return ONE_PART_COMMAND.contains(s);
     }
 
-    public boolean isTwoPartCmd(String s) {
+    public static boolean isTwoPartCmd(String s) {
         return TWO_PART_COMMAND.contains(s);
     }
 
@@ -37,23 +37,23 @@ public class Verifier {
         return taskNumber <= 0 || taskNumber > taskCount;
     }
     
-    public boolean isDone(String s) {
+    public static boolean isDone(String s) {
         return s.equals("done");
     }
 
-    public boolean isEvent(String s) {
+    public static boolean isEvent(String s) {
         return s.equals("event");
     }
 
-    public boolean isDeadline(String s) {
+    public static boolean isDeadline(String s) {
         return s.equals("deadline");
     }
 
-    public boolean isTodo(String s) {
+    public static boolean isTodo(String s) {
         return s.equals("todo");
     }
 
-    public boolean isList(String s) {
+    public static boolean isList(String s) {
         return s.equals("list");
     }
     
