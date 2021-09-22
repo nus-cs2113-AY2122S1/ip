405e03fb81f5e6203bcd4886a7492571153d20e6
Kelvin Neo <2332196+kelvneo@users.noreply.github.com>
2021-09-22 21:59:11 +0800

    Add JavaDoc comments for TaskFactory

--- src/main/java/unker/command/CreateTaskCommand.java
+++ src/main/java/unker/command/CreateTaskCommand.java
@@ -9,7 +9,15 @@ public abstract class CreateTaskCommand extends Command {
     protected CreateTaskCommand(String name, String format) {
         super(name, format);
     }
-    
+
+    /**
+     * Adds a task into the Unker task manager.
+     * 
+     * @param ui The UI to print messages to.
+     * @param unker The task manager that handles all the tasks.
+     * @param task The task to add.
+     * @throws InvalidCommandException When the task is null (typically when the input is invalid).
+     */
     protected void addTask(UI ui, Unker unker, Task task) throws InvalidCommandException {
         if (task == null) {
             throw new InvalidCommandException(INVALID_FORMAT_MESSAGE, this);
--- src/main/java/unker/task/TaskFactory.java
+++ src/main/java/unker/task/TaskFactory.java
@@ -4,11 +4,29 @@ import java.util.regex.Matcher;
 import unker.util.StringUtil;
 
 public class TaskFactory {
-    
+
+    /**
+     * Create a {@link unker.task.ToDo} task based on string data.
+     * 
+     * @param data The description of the task.
+     * @return A new {@link unker.task.ToDo} task.
+     */
     public static ToDo createToDoTask(String data) {
         return new ToDo(data);
     }
-    
+
+    /**
+     * Create a {@link unker.task.Deadline} task based on string data.
+     * 
+     * The format of the string is to be:
+     * [description] /by [YYYY-MM-DD] [HH:mm]
+     * 
+     * For example:
+     * CS2113T project /by 2021-12-12 23:59
+     *
+     * @param data The description of the task, along with the deadline.
+     * @return A new {@link unker.task.Deadline} task.
+     */
     public static Deadline createDeadlineTask(String data) {
         Matcher deadlineMatcher = StringUtil.parseUserInput(Deadline.DEADLINE_DATA_PATTERN, data);
         if (deadlineMatcher != null) {
@@ -17,7 +35,19 @@ public class TaskFactory {
             return null;
         }
     }
-    
+
+    /**
+     * Create a {@link unker.task.Event} task based on string data.
+     *
+     * The format of the string is to be:
+     * [description] /at [YYYY-MM-DD] [HH:mm]-[HH:mm]
+     *
+     * For example:
+     * Google Fireside Chat /at 2021-12-12 16:00-18:45
+     *
+     * @param data The description of the task, along with the date, start time and end time.
+     * @return A new {@link unker.task.Event} task.
+     */
     public static Event createEventTask(String data) {
         Matcher eventMatcher = StringUtil.parseUserInput(Event.EVENT_DATA_PATTERN, data);
         if (eventMatcher != null) {
