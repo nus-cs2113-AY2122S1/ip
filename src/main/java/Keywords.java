4521d4b6fd54aa74d553fbc4bc53cbacae7f5f2f
EdwardZYWang <wangzy.edward@u.nus.edu>
2021-09-23 21:15:53 +0800

    add javadocs for all classes and methods created new class Keywords to store public static final variables

---
+++ src/main/java/Keywords.java
@@ -0,0 +1,15 @@
+/**
+ * This is a class containing the keywords the user's input is to be compared to
+ * in order to call the correct method
+ *
+ */
+public class Keywords {
+    public static final String INPUT_BYE = "bye";
+    public static final String INPUT_LIST = "list";
+    public static final String INPUT_DONE = "done";
+    public static final String INPUT_DELETE = "delete";
+    public static final String INPUT_TODO = "todo";
+    public static final String INPUT_DEADLINE = "deadline";
+    public static final String INPUT_EVENT = "event";
+    public static final String INPUT_FIND = "find";
+}
