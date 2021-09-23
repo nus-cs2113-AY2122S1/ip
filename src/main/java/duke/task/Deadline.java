2221fc1ff1c927fa84c03c4cea8ed0a20b36d9f7
Justin Wong <justinfidelis@gmail.com>
2021-09-23 21:03:54 +0800

    Add Increment: Level 8

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,21 +1,25 @@
 package duke.task;
 
-public class Deadline extends Task {
-    private String byDate;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String taskName, String byDate) {
+public class Deadline extends Task {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yy");
+    private LocalDate byDate;
+
+    public Deadline(String taskName, LocalDate byDate) {
         super(taskName);
         this.byDate = byDate;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " " + "(by: " + byDate + ")";
+        return "[D]" + super.toString() + " " + "(by: " + byDate.format(formatter) + ")";
     }
 
     @Override
     public String getStorageString() {
         String c = isCompleted ? "1" : "0";
-        return "D | " + c + " | " + taskName + " | " + byDate;
+        return "D | " + c + " | " + taskName + " | " + byDate.toEpochDay();
     }
 }
