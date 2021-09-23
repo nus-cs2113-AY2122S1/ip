0c1a44d47d68292564cc6bec475ad59c9f60b64b
alwinangys <alwinang99@gmail.com>
2021-09-23 10:57:58 +0800

    Add initial greeting messages

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -1,4 +1,19 @@
 package duke;
 
 public class Ui {
+
+    private static final String HORIZONTAL_LINE = "____________________________________________________________\n";
+    private static final String LOGO =  "    #    ####### #          #     #####\n"
+            + "   # #      #    #         # #   #     #\n"
+            + "  #   #     #    #        #   #  #\n"
+            + " #     #    #    #       #     #  #####\n"
+            + " #######    #    #       #######       #\n"
+            + " #     #    #    #       #     # #     #\n"
+            + " #     #    #    ####### #     #  #####\n";
+
+    private static final String WELCOME_MESSAGE = HORIZONTAL_LINE + "Hello! I'm Atlas!\n"
+            + "What can I do for you today?\n" + HORIZONTAL_LINE;
+
+
+
 }
