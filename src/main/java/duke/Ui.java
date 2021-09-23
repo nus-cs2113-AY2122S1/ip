510898e0b4494e64cef9fd161f4031bcd8da4f71
alwinangys <alwinang99@gmail.com>
2021-09-23 16:16:17 +0800

    Add UI elements for tasks

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -1,5 +1,7 @@
 package duke;
 
+import java.util.ArrayList;
+
 public class Ui {
 
     private static final String HORIZONTAL_LINE = "____________________________________________________________\n";
@@ -10,10 +12,34 @@ public class Ui {
             + " #######    #    #       #######       #\n"
             + " #     #    #    #       #     # #     #\n"
             + " #     #    #    ####### #     #  #####\n";
-
     private static final String WELCOME_MESSAGE = HORIZONTAL_LINE + "Hello! I'm Atlas!\n"
             + "What can I do for you today?\n" + HORIZONTAL_LINE;
 
+    public static void printHorizontalLine() {
+        System.out.print(HORIZONTAL_LINE);
+    }
 
+    public static void printAddedTaskMessage(Task task) {
+        System.out.print(HORIZONTAL_LINE + "Understood. I've added this task: " + System.lineSeparator()
+        + task + System.lineSeparator());
+    }
 
+    public static void printTotalNumberOfTasks(ArrayList<Task> tasks) {
+        System.out.println("Now you have " + (tasks.size()) + " tasks in the list.");
+        System.out.print(HORIZONTAL_LINE);
+    }
+
+    public static void printDoneTask(Task task) {
+        System.out.print(HORIZONTAL_LINE);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(task);
+        System.out.print(HORIZONTAL_LINE);
+    }
+
+    public static void printUndoneTask(Task task) {
+        System.out.print(HORIZONTAL_LINE);
+        System.out.println("I've undone this task for you:");
+        System.out.println(task);
+        System.out.print(HORIZONTAL_LINE);
+    }
 }
