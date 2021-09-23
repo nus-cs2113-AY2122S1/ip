f06cd83c62c19f021383379a95215af962fe3678
KaiserHuang88 <65898007+KaiserHuang88@users.noreply.github.com>
2021-09-23 15:18:27 +0800

    Updated Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,138 +1,41 @@
+
+
+
 import java.util.Scanner;
 
-
-
-
 public class Duke {
-
-    public static final int DEADLINE_LN = 8;
-    public static final int TODO_LN = 4;
-    public static final int EVENT_LN = 5;
-
-    private static boolean checkString(String[] arr, String toCheckValue) {
-        boolean result = true;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (toCheckValue.equalsIgnoreCase(arr[i])) {
-                result = false;
-                break;
-            } else if (arr[i] == null) {
-                break;
-            }
-        }
-        return result;
+    public Duke() {
     }
 
-    public static String findCommand(String line) {
-        int spaceIndex = line.indexOf(' ');
-        if (spaceIndex == -1) {
-            return line;
-        }
-        String command = line.substring(0, spaceIndex);
-        return command;
-    }
-
-    public static String findContent(String line) {
-        int spaceIndex = line.indexOf(" ");
-        if (spaceIndex == -1) {
-            return line;
-        }
-        String description = line.substring(spaceIndex);
-        return description;
-    }
-
-    public static void printList(Task[] schedule, int totalTasks) {
-        System.out.println("Here are the tasks in your list: ");
-        for (int i = 0; i < totalTasks; i += 1) {
-            System.out.print(i + 1);
-            System.out.print(". ");
-            System.out.println(schedule[i]);
-        }
-    }
-
-    public static void handleDone(Task[] schedule, String line) {
-        int number = Character.getNumericValue(line.charAt(5));
-        schedule[number - 1].markAsDone();
-    }
-
-    public static void addTodo(Task[] schedule, String content, int totalTasks) {
-        Todo t = new Todo(content);
-        schedule[totalTasks] = t;
-    }
-
-    public static void addDeadline(Task[] schedule, String content, int totalTasks) {
-        int byIndex = content.indexOf("/by");
-        String description = content.substring(0, byIndex - 1);
-        String by = content.substring(byIndex + 4);
-        Deadline d = new Deadline(description, by);
-        schedule[totalTasks] = d;
-    }
-
-    public static void gotItMessage(Task[] schedule, int totalTasks) {
-        System.out.println("Got it. I've added this task:");
-        System.out.println(schedule[totalTasks - 1]);
-        System.out.println("Now you have " + totalTasks +  " tasks in the list.");
-    }
-
-    public static void addEvent(Task[] schedule, String content, int totalTasks) {
-        int byIndex = content.indexOf("/at");
-        String description = content.substring(0, byIndex - 1);
-        String eventTime = content.substring(byIndex + 4);
-        Event e = new Event(description, eventTime);
-        schedule[totalTasks] = e;
-    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        // Level-3
-        String line;
+        Logo duke = new Logo();
+        System.out.println(duke);
         Scanner in = new Scanner(System.in);
         System.out.print("Type something: ");
-        line = in.nextLine();
+        String line = in.nextLine();
+        Manager m = new Manager();
 
-        // List of variables
-        Task[] schedule = new Task[100];
-        int totalTasks = 0;
-
-        while (true) {
-            // Exit
-            String command = findCommand(line);
-            String content = findContent(line);
-
+        while(true) {
+            String command = m.findCommand(line);
+            String content = m.findContent(line);
             if (command.equalsIgnoreCase("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                m.printBye();
                 break;
-
             } else if (command.equalsIgnoreCase("list")) {
-                printList(schedule, totalTasks);
-
+                m.printList();
             } else if (command.equalsIgnoreCase("done")) {
-                handleDone(schedule, line);
-
+                m.handleDone(line);
             } else if (command.equalsIgnoreCase("todo")) {
-                addTodo(schedule, content, totalTasks);
-                totalTasks++;
-                gotItMessage(schedule, totalTasks);
-
+                m.addTodo(content);
             } else if (command.equalsIgnoreCase("deadline")) {
-                addDeadline(schedule, content, totalTasks);
-                totalTasks++;
-                gotItMessage(schedule, totalTasks);
-
+                m.addDeadline(content);
             } else if (command.equalsIgnoreCase("event")) {
-                addEvent(schedule, content, totalTasks);
-                totalTasks++;
-                gotItMessage(schedule, totalTasks);
-
+                m.addEvent(content);
+            } else {
+                System.out.println("Oops, please reenter your command");
             }
 
             line = in.nextLine();
         }
     }
-
 }
