bdf1f22753c45114c10f40c62ddb85d88eded8f5
alwinangys <alwinang99@gmail.com>
2021-09-23 12:06:33 +0800

    Add skeleton for todo, deadline, event

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -1,6 +1,21 @@
 package duke;
 
+import java.util.ArrayList;
+
 public class TaskList {
 
+    // getting task names, deadline, duration etc should be done in Parser
 
+    public static void addTodo(String input, ArrayList<Task> tasks) {
+        String description = Parser.getTodoDescription(input);
+        tasks.add(new Todo(description));
+    }
+
+    public static void addDeadline(String input, ArrayList<Task> tasks) {
+
+    }
+
+    public static void addEvent(String input, ArrayList<Task> tasks) {
+
+    }
 }
