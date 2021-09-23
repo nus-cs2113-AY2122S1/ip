602c96a7e833d040e1db5b877f3202a3b554aca5
KaiserHuang88 <65898007+KaiserHuang88@users.noreply.github.com>
2021-09-23 15:22:03 +0800

    Added Logo

--- src/main/java/Logo.java
+++ src/main/java/Logo.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Logo {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+
+public class Logo {
+    public Logo() {
+    }
+
+    public String toString() {
+        String logo = " ____        _        \n|  _ \\ _   _| | _____ \n| | | | | | | |/ / _ \\\n| |_| | |_| |   <  __/\n|____/ \\__,_|_|\\_\\___|\n";
+        return "Hello from\n" + logo;
+    }
 }
