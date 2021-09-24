cf2ed3edaf2e5a4e0df9e28e90d6df83508e72ff
nishantrai-nus <e0560182@u.nus.edu>
2021-09-24 17:32:35 +0800

    improve JavaDocs for Command class

--- src/main/java/duke/common/Command.java
+++ src/main/java/duke/common/Command.java
@@ -20,28 +20,59 @@ public class Command {
 
     protected static final ArrayList<Task> tasks = new ArrayList<>();
 
+    /** Prints the current task list. */
     public static void executeList() {
         Ui.printList(tasks);
     }
 
+    /**
+     * Adds a todo task to the tasklist.
+     *
+     * @param description the description of the task to be added
+     * @throws IOException if an error occurred while writing to the data file
+     */
     public static void addTodo(String description) throws IOException {
         tasks.add(Task.getTaskCount(), new Todo(description));
         Ui.printAddMessage(tasks);
         Storage.saveData();
     }
 
+    /**
+     * Adds a deadline task to the tasklist. Deadlines must be entered in the format
+     * "dd/mm/yyyy hhmm" with the time being in 24-Hour format.
+     * For example: 22/12/2021 1800
+     *
+     * @param description the description of the task to be added
+     * @param by the description of the task to be added
+     * @throws IOException if an error occurred while writing to the data file
+     */
     public static void addDeadline(String description, LocalDateTime by) throws IOException {
         tasks.add(Task.getTaskCount(), new Deadline(description, by));
         Ui.printAddMessage(tasks);
         Storage.saveData();
     }
 
+    /**
+     * Adds an event task to the tasklist. Event times must be entered in the format
+     * "dd/mm/yyyy hhmm" with the time being in 24-Hour format.
+     * For example: 22/12/2021 1800
+     *
+     * @param description the description of the task to be added
+     * @param at the date and time of the task to be added
+     * @throws IOException if an error occurred while writing to the data file
+     */
     public static void addEvent(String description, LocalDateTime at) throws IOException {
         tasks.add(Task.getTaskCount(), new Event(description, at));
         Ui.printAddMessage(tasks);
         Storage.saveData();
     }
 
+    /**
+     * Marks a given task as completed.
+     *
+     * @param taskIndex the number of the task to be marked completed
+     * @throws IOException if an error occurred while writing to the data file
+     */
     public static void doneTask(int taskIndex) throws IOException {
         try {
             tasks.get(taskIndex - 1).setDone(true);
@@ -52,6 +83,12 @@ public class Command {
         }
     }
 
+    /**
+     * Deletes a given task from the tasklist.
+     *
+     * @param taskIndex the number of the task to be marked completed
+     * @throws IOException if an error occurred while writing to the data file
+     */
     public static void deleteTask(int taskIndex) throws IOException {
         try {
             Ui.printDeleteMessage(taskIndex, tasks);
@@ -62,6 +99,12 @@ public class Command {
         }
     }
 
+    /**
+     * Searches the tasklist for any tasks that contain the given keywords, and displays them
+     * to the user.
+     *
+     * @param keywords the keywords to search for in tasks
+     */
     public static void findTask(String ... keywords) {
         for (String keyword : keywords) {
             List<Task> searchResults = tasks.stream().filter(task->task.getDescription().contains(keyword)).collect(Collectors.toList());
