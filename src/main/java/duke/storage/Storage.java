ac3d3e8dbbd4f7902e771ae32d51b723b77de987
DESKTOP-TO5H2A5\Ishaa <ishaan.vyas@hotmail.com>
2021-09-25 17:44:47 +0800

    added javadoc to storage

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -4,6 +4,7 @@ import duke.tasks.Deadline;
 import duke.tasks.Event;
 import duke.tasks.Task;
 import duke.tasks.Todo;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -13,17 +14,32 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 import static duke.ui.Ui.NL;
 
+/**
+ * Loads and stores data into a data file inside a specific data directory
+ */
 public class Storage {
     private final File dataDirectory;
     private final File dataFile;
 
+    /**
+     * @param dataDirectory Directory where the data file is to be inside
+     * @param dataFile      File which contains the stored data
+     */
     public Storage(File dataDirectory, File dataFile) {
         this.dataDirectory = dataDirectory;
         this.dataFile = dataFile;
     }
 
+    /**
+     * loads all tasks found in the data file into an ArrayList<Task>
+     *
+     * @return ArrayList<Task> containing all the tasks loaded from the data file
+     * @throws FileNotFoundException when no data file exists
+     * @throws SecurityException     when data directory not found and cannot be created using mkdirs
+     */
     public ArrayList<Task> load() throws FileNotFoundException,
             SecurityException {
         if (!dataDirectory.isDirectory() && !dataDirectory.mkdirs()) {
@@ -40,13 +56,26 @@ public class Storage {
         }
         return loadedData;
     }
-    
+
+    /**
+     * takes in a task and adds it to the data file
+     *
+     * @param task          Task to be added to the data file
+     * @param numberOfTasks number of tasks in the data file after adding the task
+     * @throws IOException when I/O error occurs
+     */
     public void writeToData(Task task, int numberOfTasks) throws IOException {
         FileWriter fw = new FileWriter(dataFile, true);
-        fw.write( ((numberOfTasks > 1) ? NL : "") + task.toData());
+        fw.write(((numberOfTasks > 1) ? NL : "") + task.toData());
         fw.close();
     }
-    
+
+    /**
+     * Overwrites all the data in the data file with the tasks in the tasks ArrayList
+     *
+     * @param tasks ArrayList with all the tasks to be written into the data file
+     * @throws IOException when I/O error occurs
+     */
     public void refreshData(ArrayList<Task> tasks) throws IOException {
         FileWriter fw = new FileWriter(dataFile, false);
         for (int i = 0; i < tasks.size(); i++) {
@@ -54,7 +83,13 @@ public class Storage {
         }
         fw.close();
     }
-    
+
+    /**
+     * takes in input from the data file and extracts the data into Task objects
+     *
+     * @param input input from data file
+     * @return Task object corresponding ot the data file input, null if Task is not recognised
+     */
     private Task extractTask(String input) {
         String[] taskDetails = input.split("\\|");
         boolean isDone;
@@ -65,9 +100,9 @@ public class Storage {
         case "D":
             isDone = taskDetails[1].trim().equalsIgnoreCase("1");
             try {
-                LocalDateTime byDT = LocalDateTime.parse(taskDetails[3].trim(), 
+                LocalDateTime byDT = LocalDateTime.parse(taskDetails[3].trim(),
                         DateTimeFormatter.ofPattern("d/M/yyyy HHmm"));
-                return new Deadline(taskDetails[1].trim(),byDT, isDone);
+                return new Deadline(taskDetails[1].trim(), byDT, isDone);
             } catch (DateTimeParseException dtpe) {
                 return new Deadline(taskDetails[2].trim(), taskDetails[3].trim(), isDone);
             }
@@ -76,7 +111,7 @@ public class Storage {
             try {
                 LocalDateTime atDT = LocalDateTime.parse(taskDetails[3].trim(),
                         DateTimeFormatter.ofPattern("d/M/yyyy HHmm"));
-                return new Event(taskDetails[1].trim(),atDT, isDone);
+                return new Event(taskDetails[1].trim(), atDT, isDone);
             } catch (DateTimeParseException dtpe) {
                 return new Event(taskDetails[2].trim(), taskDetails[3].trim(), isDone);
             }
