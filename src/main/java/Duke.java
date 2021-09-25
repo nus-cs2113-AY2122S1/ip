47f5132e8e5f9943c59d25ee81cbc5614c414a92
shixi <fanshixi123@gmail.com>
2021-09-25 17:20:14 +0800

    Refactor and delete code that is now in Parser class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,16 @@
 import duke.DukeException;
+import duke.Parser;
 import duke.tasks.FileManager;
 import duke.tasks.Task;
 import duke.tasks.TaskManager;
+import duke.Ui;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
-    public static final int MAX_NUMBER_OF_TASKS = 100;
-    public static final String BY_DIVIDER = "/by";
-    public static final String AT_DIVIDER = "/at";
     public static final String LINE_SEPARATOR = "_____________________________";
     public static final String COMMAND_EXIT = "bye";
     public static final String COMMAND_LIST = "list";
@@ -26,107 +24,6 @@ public class Duke {
     public static final String DIRECTORY_PATH = "data";
 
     public static boolean canRunDuke = true;
-
-    /**
-     * Splits the input string and returns the command at the start of the string
-     *
-     * @param args the input string
-     * @return the command the user entered
-     */
-    public static String getCommand(String args) {
-        String[] words = args.split(" ");
-        String[] commandArray = Arrays.copyOf(words, 1);
-        String command = getFormattedString(Arrays.toString(commandArray));
-        return command;
-    }
-
-    /**
-     * Splits the input string and returns the item after the command
-     *
-     * @param args the input string
-     * @return the item after the command the user entered
-     */
-    public static String getItem(String args) {
-        String[] words = args.split(" ");
-        String[] itemArray = Arrays.copyOfRange(words, 1, words.length);
-        String item = getFormattedString(Arrays.toString(itemArray));
-        return item;
-    }
-
-    /**
-     * Formats the string that was converted from an array. Delete brackets and commas
-     *
-     * @param args the input string that was converted from an array
-     * @return the formatted string without brackets and commas
-     */
-    public static String getFormattedString(String args) {
-        String formattedString = args.replace(",", "")
-                .replace("]", "")
-                .replace("[", "")
-                .trim();
-        return formattedString;
-    }
-
-
-    /**
-     * Checks if the input string has an empty item after a command
-     *
-     * @param args the input string the user entered
-     * @return true if the item after the command is empty, false otherwise
-     */
-    public static boolean isEmptyItem(String args) {
-        if (getItem(args).equals("")) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * returns if there is a time entered for a deadline or event task
-     *
-     * @param args the input string the user entered
-     * @return true if there is a time entered, false otherwise
-     */
-    public static boolean canGetTime(String args) {
-        String description = getItem(args);
-        String[] time = description.substring(description.indexOf("/")).split(" ");
-        if (time.length < 2) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Checks if the item from the user input is invalid for deadline and event tasks
-     *
-     * @param args the item to be checked
-     * @return true if the item does not contain a '/by' or does not have time after /by, false otherwise
-     */
-    public static boolean isInvalidDeadline(String args) {
-        if (!getItem(args).contains(BY_DIVIDER)) {
-            return true;
-        }
-        if (!canGetTime(args)) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Checks if the item from the user input is invalid for deadline and event tasks
-     *
-     * @param args the item to be checked
-     * @return true if the item does not contain a '/at' or does not have time after /at, false otherwise
-     */
-    public static boolean isInvalidEvent(String args) {
-        if (!getItem(args).contains(AT_DIVIDER)) {
-            return true;
-        }
-        if (!canGetTime(args)) {
-            return true;
-        }
-        return false;
-    }
 
     /**
      * Reads the input and command entered by the user and execute task according to user command
@@ -150,12 +47,12 @@ public class Duke {
             taskManager.printTaskList();
             break;
         case COMMAND_DONE:
-            if (isEmptyItem(inputStr)) {
+            if (Parser.isEmptyItem(inputStr)) {
                 throw new DukeException("Oops, did you forget to enter the task to be marked as done?");
             } else if (taskManager.getNumberOfTasksUndone() == 0) {
                 throw new DukeException("Oops, there are no tasks to be marked done!");
-            } else if (taskManager.getNumberOfTasksAdded() < Integer.parseInt(getItem(inputStr))) {
-                throw new DukeException("Oops, there is no task " + Integer.parseInt(getItem(inputStr)) + "!");
+            } else if (taskManager.getNumberOfTasksAdded() < Integer.parseInt(Parser.getItem(inputStr))) {
+                throw new DukeException("Oops, there is no task " + Integer.parseInt(Parser.getItem(inputStr)) + "!");
             } else if (taskManager.isTaskDone(inputStr)) {
                 throw new DukeException("Oops, this task is already marked as done!");
             } else {
@@ -163,46 +60,46 @@ public class Duke {
                 break;
             }
         case COMMAND_TODO:
-            if (isEmptyItem(inputStr)) {
+            if (Parser.isEmptyItem(inputStr)) {
                 throw new DukeException("Oops, the description of a ToDo cannot be empty!");
             }
-            String item = getItem(inputStr);
+            String item = Parser.getItem(inputStr);
             taskManager.addToDoTaskToList(item);
             break;
         case COMMAND_DEADLINE:
-            if (isEmptyItem(inputStr)) {
+            if (Parser.isEmptyItem(inputStr)) {
                 throw new DukeException("Oops, the description of a deadline cannot be empty!");
-            } else if (isInvalidDeadline(inputStr)) {
+            } else if (Parser.isInvalidDeadline(inputStr)) {
                 throw new DukeException("Oops, the time of a deadline cannot be empty!");
             }
-            item = getItem(inputStr);
+            item = Parser.getItem(inputStr);
             taskManager.addDeadlineTaskToList(item);
             break;
         case COMMAND_EVENT:
-            if (isEmptyItem(inputStr)) {
+            if (Parser.isEmptyItem(inputStr)) {
                 throw new DukeException("Oops, the description of an event cannot be empty!");
-            } else if (isInvalidEvent(inputStr)) {
+            } else if (Parser.isInvalidEvent(inputStr)) {
                 throw new DukeException("Oops, the time of an event cannot be empty!");
             }
-            item = getItem(inputStr);
+            item = Parser.getItem(inputStr);
             taskManager.addEventTaskToList(item);
             break;
         case COMMAND_DELETE:
-            if (isEmptyItem(inputStr)) {
+            if (Parser.isEmptyItem(inputStr)) {
                 throw new DukeException("Oops, did you forget to enter the task to be deleted?");
             } else if (taskManager.getNumberOfTasksAdded() == 0) {
                 throw new DukeException("Oops, there are no tasks in the list yet!");
-            } else if (taskManager.getNumberOfTasksAdded() < Integer.parseInt(getItem(inputStr))) {
-                throw new DukeException("Oops, there is no task " + Integer.parseInt(getItem(inputStr)) + "!");
+            } else if (taskManager.getNumberOfTasksAdded() < Integer.parseInt(Parser.getItem(inputStr))) {
+                throw new DukeException("Oops, there is no task " + Integer.parseInt(Parser.getItem(inputStr)) + "!");
             } else {
                 taskManager.deleteTask(inputStr);
             }
             break;
         case COMMAND_FIND:
-            if (isEmptyItem(inputStr)) {
+            if (Parser.isEmptyItem(inputStr)) {
                 throw new DukeException("Oops, did you forget to enter a keyword?");
             }
-            String keyword = getItem(inputStr);
+            String keyword = Parser.getItem(inputStr);
             printArrayList(taskManager.findTask(keyword));
             break;
         default:
@@ -259,7 +156,7 @@ public class Duke {
 
         while(canRunDuke) {
             String inputStr = sc.nextLine();
-            String command = getCommand(inputStr);
+            String command = Parser.getCommand(inputStr);
             try {
                 executeCommand(inputStr, command, taskManager, fileManager);
             } catch (DukeException dukeException) {
