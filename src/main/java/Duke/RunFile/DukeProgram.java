20091c4b2587e13b0addfd260f11b02334bce42c
LouisLouis19 <e0661385@u.nus.edu>
2021-09-25 19:08:56 +0800

    Add JavaDoc Documentation for DukeProgram class

--- src/main/java/Duke/RunFile/DukeProgram.java
+++ src/main/java/Duke/RunFile/DukeProgram.java
@@ -5,18 +5,25 @@ import Duke.Commands.*;
 import Duke.SaveFile.DataSaver;
 import Duke.TaskTypes.Task;
 import Duke.UI.UserInterface;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-
 import static Duke.UI.DukeConstants.*;
 
+/**
+ * Starting point of Duke Program.
+ * This class encapsulates the application's
+ * main entry point.
+ */
 public class DukeProgram {
 
     public static final ArrayList<Task> taskList = new ArrayList<>();
 
+    /**
+     * The application's main entry point which takes in user input
+     * and executes the corresponding instructions until user types
+     * in "bye" to exit the program
+     */
     public static void main(String[] args) {
         UserInterface.printDukeGreet();
         DataSaver.manageLoad(taskList);
