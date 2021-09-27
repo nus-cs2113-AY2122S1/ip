93afa172db90bb7ad5c0331944126675d0eaa1d0
Ashraf Jaffar <a.ashrafjfr@gmail.com>
2021-09-27 11:15:51 +0800

    Add javadoc to methods in Jarvis class

--- src/main/java/jarvis/Jarvis.java
+++ src/main/java/jarvis/Jarvis.java
@@ -1,6 +1,6 @@
 package jarvis;
 
-import command.Command;
+import input.Input;
 import storage.Storage;
 import tasklist.Task;
 import ui.Ui;
@@ -9,18 +9,27 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Jarvis {
+
+    /**
+     * This method runs the Jarvis bot by first greeting the user. It will then create
+     * the list of task in an ArrayList followed by finding any saved task file that
+     * has been created from prior usage. Then, it will loop to take in inputs from
+     * the user and will send the inputs to the inputHandler method from Input class.
+     * If the user inputs "bye", the loop will exit and the bot will print a bye message.
+     */
     public void run() {
         Ui.printGreetMessage();
         ArrayList<Task> taskList = new ArrayList<>();
         Storage.findTaskFile(taskList);
         Scanner scanner = new Scanner(System.in);
         String userLine = scanner.nextLine().trim();
-        while(!userLine.equalsIgnoreCase(Command.COMMAND_BYE)) {
-            Command.commandHandler(userLine, taskList);
+        while(!userLine.equalsIgnoreCase(Input.COMMAND_BYE)) {
+            Input.inputHandler(userLine, taskList);
             userLine = scanner.nextLine().trim();
         }
         Ui.printByeMessage();
     }
+
 
     public static void main(String[] args) {
         Jarvis jarvis = new Jarvis();
