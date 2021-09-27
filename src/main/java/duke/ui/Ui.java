fc61ef8a4bc83361a06b47b9377f82009b9b5ee8
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-09-28 01:06:43 +0800

    Add print functions

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -1,5 +1,9 @@
 package duke.ui;
 
+import duke.task.Task;
+
+import java.util.ArrayList;
+
 public class Ui {
 
     private final static String WELCOME_GREETING = "Howdy there! I'm Fluke";
@@ -30,5 +34,43 @@ public class Ui {
      */
     public static void printLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    /**
+     * The function prints the recently added task
+     */
+    public static void printAddedTask(Task task, ArrayList<Task> tasks) {
+        printLine();
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task.getDescription());
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        printLine();
+    }
+
+    /**
+     * The function prints the recently added task
+     */
+    public static void printDeletedTask(Task task, ArrayList<Task> tasks) {
+        printLine();
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(task.getDescription());
+        System.out.println("Now you have " + (tasks.size() - 1) + " tasks in the list.");
+        printLine();
+    }
+
+    /**
+     * Prints all the tasks
+     *
+     * @param tasks the array of tasks
+     */
+    public static void printTasks(ArrayList<Task> tasks) {
+        printLine();
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i).getDescription());
+        }
+        if (tasks.size() == 0) {
+            System.out.println("Smartass, you need to add tasks before listing them !!!");
+        }
+        printLine();
     }
 }
