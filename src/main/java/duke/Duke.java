82dc711460441f07be06d16659e76b583a65e58e
alwinangys <alwinang99@gmail.com>
2021-09-27 11:14:16 +0800

    Edit formatting

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -13,11 +13,11 @@ public class Duke {
     public static void main(String[] args) {
         ArrayList<Task> tasks = new ArrayList<>();
         Storage.loadTaskFile(tasks);
-
         Ui.greetUser();
         String userInput;
         Scanner in = new Scanner(System.in);
         boolean isBye = false;
+
         while (!isBye) {
             userInput = in.nextLine();
             String command = Parser.getCommand(userInput);
