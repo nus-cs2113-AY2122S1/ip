de49f1cf807e540a6c217cc20190ef0201040046
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-09-28 12:56:06 +0800

    Update DukeException.java

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -1,5 +1,8 @@
 package duke.exception;
 
+import duke.manager.TaskManager;
+import duke.ui.Ui;
+
 public class DukeException extends Exception {
     public DukeException(String message) {
         super(message);
