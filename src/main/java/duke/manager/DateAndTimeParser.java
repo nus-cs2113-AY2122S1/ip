b8f6dfc160334c9f9bc0ab42521088b1ffa735c7
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-09-28 09:46:43 +0800

    Update DateAndTimeParser

--- src/main/java/duke/manager/DateAndTimeParser.java
+++ src/main/java/duke/manager/DateAndTimeParser.java
@@ -1,4 +1,24 @@
 package duke.manager;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class DateAndTimeParser {
+
+    public static DateTimeFormatter stringFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM dd, uuuu hh:mm a");
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm";
+
+    /**
+     * Returns the LocalDateTime object after processing
+     * the input string
+     *
+     * @param message input string containing the date and time
+     * @return LocalDateTime object "yyyy-MM-dd HH:mm"
+     */
+    static LocalDateTime processDateAndTime(String message) throws DateTimeParseException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        LocalDateTime info = LocalDateTime.parse(message.strip(), formatter);
+        return info;
+    }
 }
