5b8ef1ccf300960f7864bc39d11138ea985b25f7
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-09-28 12:56:46 +0800

    Move deadline errors to DukeException.java

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -7,4 +7,18 @@ public class DukeException extends Exception {
     public DukeException(String message) {
         super(message);
     }
+
+    public static void stringIndexDeadlineError() {
+        Ui.printLine();
+        System.out.println("YOU IDIOT !!??!! The description of a deadline cannot be empty.");
+        Ui.printLine();
+    }
+
+    public static void arrayIndexDeadlineError() {
+        Ui.printLine();
+        System.out.println("YOU IDIOT !!??!! The input format should be : ");
+        System.out.println("deadline description /by date or time");
+        Ui.printLine();
+    }
+
 }
