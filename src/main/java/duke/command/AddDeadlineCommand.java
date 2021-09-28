4512174849aec64dffc453e7207d4f40aef0960a
arcturusz <48401155+arcturusz@users.noreply.github.com>
2021-09-29 00:00:57 +0800

    Add javadocs

--- src/main/java/duke/command/AddDeadlineCommand.java
+++ src/main/java/duke/command/AddDeadlineCommand.java
@@ -4,6 +4,13 @@ import duke.tasklist.TaskList;
 import duke.ui.Ui;
 
 public class AddDeadlineCommand extends Command {
+    /**
+     * Add deadline task to task list
+     *
+     * @param taskList list of task
+     * @param userInput user input
+     */
+
     public AddDeadlineCommand(TaskList taskList, String userInput) {
         taskList.addDeadlineTask(userInput);
         Ui.printAddSuccess(taskList);
--- src/main/java/duke/command/AddToDoCommand.java
+++ src/main/java/duke/command/AddToDoCommand.java
@@ -4,6 +4,13 @@ import duke.tasklist.TaskList;
 import duke.ui.Ui;
 
 public class AddToDoCommand extends Command {
+    /**
+     * Add todo task to task list
+     *
+     * @param taskList list of task
+     * @param userInput user input
+     */
+
     public AddToDoCommand(TaskList taskList, String userInput) {
         taskList.addToDoTask(userInput);
         Ui.printAddSuccess(taskList);
