d57a24bd48cf3061b8beb3da11d414f8413ede84
arcturusz <48401155+arcturusz@users.noreply.github.com>
2021-09-28 22:15:17 +0800

    Implement find task by keyword

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -1,6 +1,9 @@
 package duke.ui;
 
+import duke.task.Task;
 import duke.tasklist.TaskList;
+
+import java.util.ArrayList;
 
 public class Ui {
     protected final String DIVIDER = "-----------------------------------------";
@@ -23,6 +26,18 @@ public class Ui {
         int lastTaskIndex = taskList.getTasks().size() - 1;
         System.out.println("Got it. I've added this task:\n  " + taskList.getTasks().get(lastTaskIndex));
         System.out.println("Now you have " + taskList.getTasks().size() + " tasks in the list");
+    }
+
+    public static void printNoMatchTask() {
+        System.out.println("No tasks found with given keyword. Please try again!");
+    }
+
+    public static void printTasks(ArrayList<Task> tasks) {
+        int currentIndex = 1;
+        for (Task task : tasks) {
+            System.out.println(currentIndex + ". " + task.toString());
+            currentIndex++;
+        }
     }
 
 }
