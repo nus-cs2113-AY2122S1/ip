287e1ecfb61fd6dcd20ad110c747b6191086bd20
BryanElmer <bryan.elmer.mulijono@gmail.com>
2021-09-29 19:10:29 +0800

    Reformat main function, add constructor and run function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,112 +5,59 @@ import duke.Storage.FileManager;
 import duke.TaskList.TaskManager;
 import duke.Ui.Parser;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Duke {
 
-    private static final String COMMAND_LIST_TASK = "list";
-    private static final String COMMAND_ADD_TODO = "todo";
-    private static final String COMMAND_ADD_DEADLINE = "deadline";
-    private static final String COMMAND_ADD_EVENT = "event";
-    private static final String COMMAND_FINISH_TASK = "done";
-    private static final String COMMAND_DELETE_TASK = "delete";
+    private FileManager fileManager;
+    private DisplayManager displayManager;
+    private TaskManager taskManager;
+    private Parser parser;
+    private Scanner in;
+
     private static final String COMMAND_EXIT = "bye";
 
-    public static void greetStart(DisplayManager displayManager) {
+    public Duke(String filePath) {
+        displayManager = new DisplayManager();
+        fileManager = new FileManager(filePath);
+        taskManager = new TaskManager();
+        parser = new Parser();
+        in = new Scanner(System.in);
+
+        try {
+            fileManager.load(taskManager, parser);
+        } catch (IOException e) {
+            displayManager.printErrorLoadingData();
+        }
+    }
+
+    public void run() {
         displayManager.printStartGreet();
-    }
-
-    public static void greetEnd(DisplayManager displayManager) {
-        displayManager.printEndGreet();
-    }
-
-    public static void processReply(TaskManager taskManager, Parser parser, String line) throws DukeException {
-        Command command = null;
-        String taskInfo;
-        String commandType = parser.extractCommand(line);
-
-        switch (commandType) {
-        case COMMAND_LIST_TASK:
-            taskManager.getAndPrintTaskList();
-            break;
-        case COMMAND_ADD_TODO:
+        String fullCommand;
+        fullCommand = in.nextLine();
+        boolean isExit = false;
+        while (!isExit) {
             try {
-                taskInfo = parser.extractTaskInfo(line);
-            } catch (IndexOutOfBoundsException e) {
-                throw new DukeException("The description of " + commandType + " cannot be empty.");
-            }
-            command = new ToDoCommand(taskManager, taskInfo);
-            break;
-        case COMMAND_ADD_DEADLINE:
-            try {
-                taskInfo = parser.extractTaskInfo(line);
-            } catch (IndexOutOfBoundsException e) {
-                throw new DukeException("The description of " + commandType + " cannot be empty.");
-            }
-            command = new DeadlineCommand(taskManager, parser, taskInfo);
-            break;
-        case COMMAND_ADD_EVENT:
-            try {
-                taskInfo = parser.extractTaskInfo(line);
-            } catch (IndexOutOfBoundsException e) {
-                throw new DukeException("The description of " + commandType + " cannot be empty.");
-            }
-            command = new EventCommand(taskManager, parser, taskInfo);
-            break;
-        case COMMAND_FINISH_TASK:
-            try {
-                taskInfo = parser.extractTaskInfo(line);
-            } catch (IndexOutOfBoundsException e) {
-                throw new DukeException("The description of " + commandType + " cannot be empty.");
-            }
-            command = new SetDoneCommand(taskManager, taskInfo);
-            break;
-        case COMMAND_DELETE_TASK:
-            try {
-                taskInfo = parser.extractTaskInfo(line);
-            } catch (IndexOutOfBoundsException e) {
-                throw new DukeException("The description of " + commandType + " cannot be empty.");
-            }
-            command = new DeleteCommand(taskManager, taskInfo);
-            break;
-        default:
-            throw new DukeException("I'm sorry, but I don't know what that means :-(");
-        }
-        if (command != null) {
-            command.execute();
-        }
-    }
-
-    public static void reply(Scanner in, TaskManager taskManager, Parser parser) {
-        String line;
-
-        line = in.nextLine();
-        while (!line.equals(COMMAND_EXIT)) {
-            try {
-                processReply(taskManager, parser, line);
+                Command command = parser.parse(taskManager, parser, fullCommand);
+                if (command != null) {
+                    command.execute();
+                }
+                if (command instanceof ExitCommand) {
+                    isExit = true;
+                    break;
+                }
             } catch (DukeException e) {
                 DisplayManager.printHorizontalSeparator();
                 System.out.println(e);
                 DisplayManager.printHorizontalSeparator();
             }
-            line = in.nextLine();
+            fullCommand = in.nextLine();
         }
-    }
-
-    public static void loadDataFile(TaskManager taskManager, Parser parser) {
-        FileManager.loadData(taskManager, parser);
+        displayManager.printEndGreet();
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        TaskManager taskManager = new TaskManager();
-        Parser parser = new Parser();
-        DisplayManager displayManager = new DisplayManager();
-
-        greetStart(displayManager);
-        loadDataFile(taskManager, parser);
-        reply(in, taskManager, parser);
-        greetEnd(displayManager);
+        new Duke("data/savedTasks.txt").run();
     }
 }
