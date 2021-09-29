d6c67769aec5e7f790d7a9673deb9e025d587925
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-09-29 21:29:44 +0800

    Add documentation DukeException.java

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -8,12 +8,20 @@ public class DukeException extends Exception {
         super(message);
     }
 
+    /**
+     * Prints an error message for wrong input format
+     * for deadline command
+     */
     public static void stringIndexDeadlineError() {
         Ui.printLine();
         System.out.println("YOU IDIOT !!??!! The description of a deadline cannot be empty.");
         Ui.printLine();
     }
 
+    /**
+     * Prints an error message for wrong input format
+     * for deadline command
+     */
     public static void arrayIndexDeadlineError() {
         Ui.printLine();
         System.out.println("YOU IDIOT !!??!! The input format should be : ");
@@ -22,7 +30,7 @@ public class DukeException extends Exception {
     }
 
     /**
-     * Error for wrong task type
+     * Prints an error for wrong task type
      */
     public static void taskError() {
         Ui.printLine();
@@ -30,6 +38,9 @@ public class DukeException extends Exception {
         Ui.printLine();
     }
 
+    /**
+     * Prints an error message for wrong date format
+     */
     public static void dateTimeParseError() {
         Ui.printLine();
         System.out.println("Please enter both date and time in the format");
@@ -37,36 +48,59 @@ public class DukeException extends Exception {
         Ui.printLine();
     }
 
+    /**
+     * Prints an error message for wrong input format
+     */
     public static void stringIndexTodoError() {
         Ui.printLine();
         System.out.println("OOPS!!! The description of a todo cannot be empty.");
         Ui.printLine();
     }
 
+    /**
+     * Prints an error message for wrong input format
+     * for done command
+     */
     public static void nullPointerDoneError() {
         Ui.printLine();
         System.out.println("OH MY GOD, can you maybe type a task that exists ?");
         Ui.printLine();
     }
 
+    /**
+     * Prints an error message for wrong input format
+     * for find command
+     */
     public static void nullPointerFindError() {
         Ui.printLine();
         System.out.println("No tasks added yet");
         Ui.printLine();
     }
 
+    /**
+     * Prints an error message for wrong input format
+     * for find command
+     */
     public static void stringIndexFindError() {
         Ui.printLine();
         System.out.println("find cannot be empty");
         Ui.printLine();
     }
 
+    /**
+     * Prints an error message for wrong input format
+     * for delete command
+     */
     public static void indexBoundDeleteError() {
         Ui.printLine();
         System.out.println("Dude there's no task at that index");
         Ui.printLine();
     }
 
+    /**
+     * Prints an error message for wrong input format
+     * for delete command
+     */
     public static void nullPointerDeleteError() {
         Ui.printLine();
         System.out.println("OH MY GOD, can you maybe type things properly ?");
@@ -74,12 +108,20 @@ public class DukeException extends Exception {
         Ui.printLine();
     }
 
+    /**
+     * Prints an error message for wrong input format
+     * for event command
+     */
     public static void stringIndexEventError() {
         Ui.printLine();
         System.out.println("YOU IDIOT !!??!! The description of an event cannot be empty.");
         Ui.printLine();
     }
 
+    /**
+     * Prints an error message for wrong input format
+     * for event command
+     */
     public static void arrayIndexEventError() {
         Ui.printLine();
         System.out.println("YOU IDIOT !!??!! The input format should be : ");
