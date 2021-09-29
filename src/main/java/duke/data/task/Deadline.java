5dd48cb36b3e5d5c2dd25f91840fa904e47388f2
Jia Yixuan <jyxhazcake@yahoo.com>
2021-09-29 17:29:49 +0800

    Add support for LocalDate in Deadline class

--- src/main/java/duke/data/task/Deadline.java
+++ src/main/java/duke/data/task/Deadline.java
@@ -1,15 +1,28 @@
 package duke.data.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     protected String by;
+    protected LocalDate byDate;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
 
+    public Deadline(String description, LocalDate byDate) {
+        super(description);
+        this.byDate = byDate;
+        this.by = byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
     public String getBy() {
         return by;
+    }
+
+    public LocalDate getByDate() {
+        return byDate;
     }
 
     @Override
