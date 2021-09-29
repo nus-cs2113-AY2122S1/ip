23f0f814981356d8f43e8eac7437a95e6b117e1c
kahhe <69589263+kahhe@users.noreply.github.com>
2021-09-29 17:45:10 +0800

    Update Parser.java

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,10 +1,5 @@
 package duke;
 
-import duke.task.Deadline;
-import duke.task.Event;
-import duke.task.Task;
-import duke.task.Todo;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Parser {
