b3bc30d2a0ff226d2760c2026337921f01ece7d8
BryanElmer <bryan.elmer.mulijono@gmail.com>
2021-09-29 20:51:44 +0800

    Fixed FindCommand class

--- src/main/java/duke/TaskList/command/FindCommand.java
+++ src/main/java/duke/TaskList/command/FindCommand.java
@@ -1,2 +1,26 @@
-package duke.TaskList.command;public class FindCommand {
+package duke.TaskList.command;
+
+import duke.TaskList.TaskManager;
+
+public class FindCommand extends Command{
+
+    protected String component;
+
+    /**
+     * Creates a FindCommand object, sets TaskManager object and command component.
+     * @param taskManager TaskManager object used to do task operations.
+     * @param component String containing keyword for searching.
+     */
+    public FindCommand(TaskManager taskManager, String component) {
+        super(taskManager);
+        this.component = component;
+    }
+
+    /**
+     * Finds and prints all the tasks that contain the keyword used.
+     */
+    public void execute() {
+        taskManager.findTask(component);
+    }
+
 }
