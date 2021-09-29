6430dfa1a78bc0457fc077a1cb8a155202d9c329
jonathanlkw <imjonlam@gmail.com>
2021-09-29 20:18:58 +0800

    Add basic date functionality to deadline class

--- src/main/java/duke/commands/DeadlineCommand.java
+++ src/main/java/duke/commands/DeadlineCommand.java
@@ -5,6 +5,11 @@ import duke.TaskList;
 import duke.Ui;
 import duke.tasks.Deadline;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class DeadlineCommand extends Command {
     public static final int END_OF_DEADLINE_INDEX = 8;
     public static final int BY_LENGTH = 3;
@@ -29,16 +34,38 @@ public class DeadlineCommand extends Command {
         int startOfByIndex = fullCommand.indexOf("/by") + BY_LENGTH;
         String description = fullCommand.substring(END_OF_DEADLINE_INDEX, endOfDescriptionIndex).trim();
         String byText = fullCommand.substring(startOfByIndex).trim();
-        String memWritableText = formatForMemory(description, byText, tasks.getTasks().size());
-        tasks.getTasks().add(new Deadline(description, byText));
+
+        String memWritableText;
+        try {
+            LocalDate byDate = LocalDate.parse(byText);
+            String byDateAsString = dateToString(byDate);
+            memWritableText = formatForMemory(description, byDateAsString, tasks.getTasks().size(), byDate);
+            tasks.getTasks().add(new Deadline(description, byDateAsString, byDate));
+        } catch (Exception e) {
+            memWritableText = formatForMemory(description, byText, tasks.getTasks().size());
+            tasks.getTasks().add(new Deadline(description, byText));
+        }
         storage.appendToMem(memWritableText);
     }
 
+    private String dateToString(LocalDate date) {
+        return Integer.toString(date.getDayOfMonth()) + " "
+                + date.getMonth().getDisplayName(TextStyle.SHORT, Locale.ENGLISH) + " "
+                + Integer.toString(date.getYear());
+    }
     private static String formatForMemory(String description, String byText, int size) {
         if (size == 0) {
             return "D~0~" + description + "~" + byText;
         } else {
             return System.lineSeparator() + "D~0~" + description + "~" + byText;
+        }
+    }
+
+    private static String formatForMemory(String description, String byText, int size, LocalDate byDate) {
+        if (size == 0) {
+            return "D~0~" + description + "~" + byText + "~" + byDate;
+        } else {
+            return System.lineSeparator() + "D~0~" + description + "~" + byText + "~" + byDate;
         }
     }
 }
--- src/main/java/duke/tasks/Deadline.java
+++ src/main/java/duke/tasks/Deadline.java
@@ -1,16 +1,26 @@
 package duke.tasks;
 
+import java.time.LocalDate;
+
 public class Deadline extends Task{
 
-    protected String by;
+    protected String byText;
+    protected LocalDate byDate;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String byText) {
         super(description);
-        this.by = by;
+        this.byText = byText;
+        this.byDate = null;
+    }
+
+    public Deadline(String description, String byText, LocalDate byDate) {
+        super(description);
+        this.byText = byText;
+        this.byDate = byDate;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + byText + ")";
     }
 }
