fcc6b8eeb0a6d6c9a98a59c9b5409caff2746b77
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-09-29 20:36:55 +0800

    Fix constants order

--- src/main/java/duke/manager/DateAndTimeParser.java
+++ src/main/java/duke/manager/DateAndTimeParser.java
@@ -6,8 +6,8 @@ import java.time.format.DateTimeParseException;
 
 public class DateAndTimeParser {
 
-    private static final String DATE_FORMAT_INPUT = "yyyy-MM-dd HH:mm";
     public static final String DATE_FORMAT_OUTPUT = "EEEE, MMMM dd, uuuu hh:mm a";
+    private static final String DATE_FORMAT_INPUT = "yyyy-MM-dd HH:mm";
     public static DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT_INPUT);
     public static DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT_OUTPUT);
 
