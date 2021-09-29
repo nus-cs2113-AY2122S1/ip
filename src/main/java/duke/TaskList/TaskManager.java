6b9da929844c5f25c16f3f1758cca9a624f94d31
BryanElmer <bryan.elmer.mulijono@gmail.com>
2021-09-30 00:47:36 +0800

    Add java docs for TaskManager class

--- src/main/java/duke/TaskList/TaskManager.java
+++ src/main/java/duke/TaskList/TaskManager.java
@@ -13,6 +13,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
+/**
+ * Class responsible for managing and storing all types of tasks.
+ */
 public class TaskManager {
 
     public static final String STATUS_DONE = "X";
@@ -27,6 +30,11 @@ public class TaskManager {
         return tasks.size();
     }
 
+    /**
+     * Creates a new 'ToDo' type task and adds it into the list of tasks.
+     * Displays a message to the user upon completion.
+     * @param taskInfo String containing the description of the task.
+     */
     public void addToDoTask(String taskInfo) {
         try {
             Task newTask = new ToDo(taskInfo);
@@ -38,6 +46,12 @@ public class TaskManager {
         }
     }
 
+    /**
+     * Creates a new 'Deadline' type task and adds it into the list of tasks.
+     * Displays a message to the user upon completion.
+     * @param parser Parser object used for parsing operations.
+     * @param taskInfo String containing the description and date time of the task.
+     */
     public void addDeadlineTask(Parser parser, String taskInfo) {
         try {
             String[] taskComponents = parser.splitTaskComponents(taskInfo);
@@ -50,6 +64,12 @@ public class TaskManager {
         }
     }
 
+    /**
+     * Creates a new 'Event' type task and adds it into the list of tasks.
+     * Displays a message to the user upon completion.
+     * @param parser Parser object used for parsing operations.
+     * @param taskInfo String containing the description and date time of the task.
+     */
     public void addEventTask(Parser parser, String taskInfo) {
         try {
             String[] taskComponents = parser.splitTaskComponents(taskInfo);
@@ -62,33 +82,58 @@ public class TaskManager {
         }
     }
 
-    public void addSavedToDo(String taskInfo, boolean taskIsDone) {
+    /**
+     * Creates a new 'Todo' type task and adds it into the list of tasks when loading tasks from storage file.
+     * Updates the status of task according to the information from the storage file.
+     * @param taskInfo String containing the details of the saved task.
+     * @param isTaskDone Boolean value for task completed status.
+     */
+    public void addSavedToDo(String taskInfo, boolean isTaskDone) {
         Task savedTask = new ToDo(taskInfo);
         tasks.add(savedTask);
-        if (taskIsDone) {
+        if (isTaskDone) {
             tasks.get(tasks.size() - 1).markAsDone();
         }
     }
 
-    public void addSavedDeadline(Parser parser, String taskInfo, boolean taskIsDone) {
+    /**
+     * Creates a new 'Deadline' type task and adds it into the list of tasks when loading tasks from storage file.
+     * Updates the status of task according to the information from the storage file.
+     * @param parser Parser object used for parsing operations.
+     * @param taskInfo String containing the details of the saved task.
+     * @param isTaskDone Boolean value for task completed status.
+     */
+    public void addSavedDeadline(Parser parser, String taskInfo, boolean isTaskDone) {
         String[] taskComponents = parser.splitTaskComponents(taskInfo);
         Task savedTask = new Deadline(taskComponents[INDEX_DESCRIPTION], taskComponents[INDEX_DATETIME]);
         tasks.add(savedTask);
-        if (taskIsDone) {
+        if (isTaskDone) {
             tasks.get(tasks.size() - 1).markAsDone();
         }
     }
 
-    public void addSavedEvent(Parser parser, String taskInfo, boolean taskIsDone) {
+    /**
+     * Creates a new 'Event' type task and adds it into the list of tasks when loading from storage file.
+     * Updates the status of the task according to the information from the storage file.
+     * @param parser Parser object used for parsing operations.
+     * @param taskInfo String containing the details of the saved task.
+     * @param isTaskDone Boolean value for task completed status.
+     */
+    public void addSavedEvent(Parser parser, String taskInfo, boolean isTaskDone) {
         String[] taskComponents = parser.splitTaskComponents(taskInfo);
         Task savedTask = new Event(taskComponents[INDEX_DESCRIPTION], taskComponents[INDEX_DATETIME]);
         tasks.add(savedTask);
-        if (taskIsDone) {
+        if (isTaskDone) {
             tasks.get(tasks.size() - 1).markAsDone();
         }
     }
 
-    public int[] filterOutOfRangeIndexes(int[] indexes) {
+    /**
+     * Filters out and returns the indexes of tasks that are out of range of the task list.
+     * @param indexes Array of integers containing the indexes of tasks taken in consideration.
+     * @return Array of integers containing indexes that are out of range.
+     */
+    private int[] filterOutOfRangeIndexes(int[] indexes) {
         int[] outOfRangeIndexes = new int[indexes.length];
         int count = 0;
 
@@ -105,7 +150,12 @@ public class TaskManager {
         return Arrays.copyOf(outOfRangeIndexes, count);
     }
 
-    public int[] filterValidIndexes(int[] indexes) {
+    /**
+     * Filters out and returns the indexes of tasks that are valid in the task list when setting tasks as done.
+     * @param indexes Array of integers containing the indexes of tasks taken in consideration.
+     * @return Array of integers containing the indexes of tasks that are valid to set as done.
+     */
+    private int[] filterValidIndexes(int[] indexes) {
         int[] validIndexes = new int[indexes.length];
         int count = 0;
 
@@ -122,7 +172,12 @@ public class TaskManager {
         return Arrays.copyOf(validIndexes, count);
     }
 
-    public int[] filterValidDeleteIndexes(int[] indexes) {
+    /**
+     * Filters out and returns the indexes of tasks that are valid in the task list when deleting tasks.
+     * @param indexes Array of integers containing the indexes of tasks taken in consideration.
+     * @return Array of integers containing the indexes of tasks that are valid to delete.
+     */
+    private int[] filterValidDeleteIndexes(int[] indexes) {
         int[] validIndexes = new int[indexes.length];
         int count = 0;
 
@@ -139,7 +194,12 @@ public class TaskManager {
         return Arrays.copyOf(validIndexes, count);
     }
 
-    public int[] filterDoneIndexes(int[] indexes) {
+    /**
+     * Filters out and returns the indexes of tasks that are done.
+     * @param indexes Array of integers containing the indexes of tasks taken in consideration.
+     * @return Array of integers containing the indexes of tasks that have been done.
+     */
+    private int[] filterDoneIndexes(int[] indexes) {
         int[] doneIndexes = new int[indexes.length];
         int count = 0;
 
@@ -156,6 +216,14 @@ public class TaskManager {
         return Arrays.copyOf(doneIndexes, count);
     }
 
+    /**
+     * Filters indexes of tasks from the input that are out of range, valid, and done,
+     * then marks all tasks with valid indexes as done.
+     * Displays to the users the tasks that are set as done.
+     * Displays to the users the indexes of tasks that are out of range.
+     * Displays to the users the indexes of tasks that have been done before.
+     * @param taskInfo String containing the indexes of tasks to be set as done.
+     */
     public void setAsDone(String taskInfo) {
         int[] indexes = filterIndexes(taskInfo);
         int[] outOfRangeIndexes = filterOutOfRangeIndexes(indexes);
@@ -176,6 +244,13 @@ public class TaskManager {
         DisplayManager.printSetAsDoneResult(tasks, outOfRangeIndexes, validIndexes, doneIndexes);
     }
 
+    /**
+     * Filters indexes of tasks from the input that are out of range, and valid,
+     * then deletes all the tasks with the valid indexes.
+     * Displays to the users the tasks that are successfully deleted.
+     * Displays to the users the indexes of tasks that are out of range.
+     * @param taskInfo String containing the indexes of tasks to be deleted.
+     */
     public void deleteTask(String taskInfo) {
         int[] indexes = filterIndexes(taskInfo);
         int[] outOfRangeIndexes = filterOutOfRangeIndexes(indexes);
@@ -198,6 +273,9 @@ public class TaskManager {
         DisplayManager.printDeleteTasksResult(deletedTasks, outOfRangeIndexes, tasks.size());
     }
 
+    /**
+     * Retrieves and displays all the tasks in the current task list.
+     */
     public void getAndPrintTaskList() {
         if (tasks.size() == 0) {
             DisplayManager.printErrorList();
@@ -206,7 +284,14 @@ public class TaskManager {
         }
     }
 
-    public static int[] filterIndexes(String taskInfo) {
+    /** Splits the input string into elements and filters out those that are numbers from those
+     * that are not.
+     * Displays the elements that are non-integer.
+     * Returns an array of integers containing integer indexes.
+     * @param taskInfo String from user input containing all possible indexes.
+     * @return Array of integers containing integer indexes.
+     */
+    private static int[] filterIndexes(String taskInfo) {
         String[] inputs = taskInfo.split(" ");
         int[] indexes = new int[inputs.length];
         int indexCount = 0;
