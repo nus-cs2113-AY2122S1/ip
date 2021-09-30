884f703e9e571e7b69e4957d90b6716af31ca777
ZhangErliCarl <zhangerli.carl@gamilcom>
2021-09-30 14:14:37 +0800

    Add increment: Level 2 Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,12 +10,24 @@ public class Duke {
         System.out.println("...................................................");
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         System.out.println("...................................................");
-        Scanner input = new Scanner(System.in); 
-        while(!lineIn.equals("bye")){
-            String string = input.nextLine();
+        Scanner in = new Scanner(System.in);
+        String[] Array = new String[100];
+        String string = "";
+        int listCounter = 0;
+        while(!string.equals("bye")){
+            string= in.nextLine();
             System.out.println("...................................................");
-            System.out.println(lineIn);
-            System.out.println("...................................................");
+            if(string.equals("list")){
+                for(int i = 0; i < listCounter; i++){
+                    System.out.println((i+1) + ". " + Array[i]);
+                }
+                System.out.println("...................................................");
+            } else {
+                Array[listCounter] = string;
+                System.out.println("added: "+ string);
+                listCounter++;
+                System.out.println("...................................................");
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("...................................................");
