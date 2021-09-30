ae649191c206aa02a0ce8b92f0c43cb140f86488
BryanElmer <bryan.elmer.mulijono@gmail.com>
2021-09-30 15:27:27 +0800

    Add java docs to all types of task classes

--- src/main/java/duke/TaskList/task/Deadline.java
+++ src/main/java/duke/TaskList/task/Deadline.java
@@ -2,20 +2,36 @@ package duke.TaskList.task;
 
 import duke.Ui.DisplayManager;
 
+/**
+ * Class of task with type 'Deadline'.
+ */
 public class Deadline extends Task {
 
     private String dateTime;
 
+    /**
+     * Creates a new 'Deadline' type task, sets the description and dateTime of the task.
+     * @param description String of task name.
+     * @param dateTime String of deadline of task.
+     */
     public Deadline(String description, String dateTime) {
         super(description);
         this.taskType = "D";
         this.dateTime = dateTime;
     }
 
+    /**
+     * Creates and returns the format used to display the task to the user.
+     * @return String data used for displaying to user.
+     */
     public String toString() {
         return DisplayManager.createListBox(this.taskType, this.getStatusIcon()) + " " + super.toString() + " (by: " + this.dateTime + ")";
     }
 
+    /**
+     * Creates and returns the format used in the storage file.
+     * @return String data used in storage file.
+     */
     public String toDataFormat() {
         return super.toDataFormat() + " | " + this.dateTime;
     }
--- src/main/java/duke/TaskList/task/ToDo.java
+++ src/main/java/duke/TaskList/task/ToDo.java
@@ -3,17 +3,32 @@ package duke.TaskList.task;
 import duke.TaskList.task.Task;
 import duke.Ui.DisplayManager;
 
+/**
+ * Class of task with type 'Todo'.
+ */
 public class ToDo extends Task {
 
+    /**
+     * Creates a new 'Todo' type task, sets the description of the task.
+     * @param description String of task name.
+     */
     public ToDo(String description) {
         super(description);
         this.taskType = "T";
     }
 
+    /**
+     * Creates and returns the format used to display the task to the user.
+     * @return String data used for displaying to user.
+     */
     public String toString() {
         return DisplayManager.createListBox(this.taskType, this.getStatusIcon()) + " " + super.toString();
     }
 
+    /**
+     * Creates and returns the format used in the storage file.
+     * @return String data used in storage file.
+     */
     public String toDataFormat() {
         return super.toDataFormat();
     }
