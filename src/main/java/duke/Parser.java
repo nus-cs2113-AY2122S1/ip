b5c2d33721a883ad0bc051607bb522030a16e5cb
YipWayne <yipwayyne@gmail.com>
2021-09-30 12:44:34 +0800

    Committed Level-9

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,17 +1,92 @@
+package duke;
+
 import duke.commands.*;
+import duke.tasks.*;
 
 public class Parser {
-    private static Command parse(String fullCommand){
+    public static Command parse(String fullCommand) {
         String[] inputArray = fullCommand.split(" ", 2);
         String inputCommand = inputArray[0];
         String inputAction = null;
-        if (inputArray.length > 1){
+        if (inputArray.length > 1) {
             inputAction = inputArray[1];
         }
-        switch(inputCommand){
+        switch (inputCommand) {
             case "":
-                break;
+                break; //to give something else
             case "bye":
-
+                return new ByeCommand();
+            case "list":
+                return new ListCommand();
+            case "todo":
+                if (inputAction != null) {
+                    Todo newTodo = new Todo(inputAction);
+                    return new AddCommand(newTodo);
+                }
+                System.out.println("INVALID: Empty 'todo' input");
+                break;
+            case "deadline":
+                try {
+                    String DeadlineToDo = inputAction.split("/", 2)[0];
+                    String dueDate = inputAction.split("/by", 2)[1];
+                    Deadline newDeadline = new Deadline(DeadlineToDo, dueDate);
+                    return new AddCommand(newDeadline);
+                }
+                catch (NullPointerException e){
+                    System.out.println("INVALID: Empty 'deadline' input");
+                }
+                catch (IndexOutOfBoundsException e){
+                    System.out.println("INVALID: Insufficient 'deadline' description or time");
+                }
+                break;
+            case "event":
+                try {
+                    String EventToDo = inputAction.split("/", 2)[0];
+                    String dueTime = inputAction.split("/at", 2)[1];
+                    Event newEvent = new Event(EventToDo, dueTime);
+                    return new AddCommand(newEvent);
+                }
+                catch (NullPointerException e){
+                    System.out.println("INVALID: Empty 'event' description");
+                }
+                catch (IndexOutOfBoundsException e){
+                    System.out.println("INVALID: Insufficient 'event' description or time");
+                }
+                break;
+            case "done":
+                try {
+                    return new DoneCommand(Integer.parseInt(inputAction) - 1);
+                }
+                catch (NullPointerException e){
+                    System.out.println("INVALID: Empty 'done' index");
+                }
+                catch (IndexOutOfBoundsException e){
+                    System.out.println("INVALID: Index out of bounds");
+                }
+                break;
+            case "delete":
+                try {
+                    return new DeleteCommand(Integer.parseInt(inputAction) - 1);
+                }
+                catch (NullPointerException e){
+                    System.out.println("INVALID: Empty 'delete' index");
+                }
+                catch (IndexOutOfBoundsException e){
+                    System.out.println("INVALID: Index out of bounds");
+                }
+                break;
+            case "find":
+                try {
+                    return new FindCommand(inputAction);
+                }
+                catch (NullPointerException e){
+                    System.out.println("INVALID: Empty 'find' keyword");
+                }
+                break;
+            default:
+                System.out.println("INVALID: Invalid command");
+                break;
+        }
+        return null;
     }
 }
--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -1,36 +1,75 @@
+package duke;
+import duke.tasks.Task;
 import java.util.Scanner;
 
 public class Ui {
     private static Scanner in = new Scanner(System.in);
 
-    private static void showLine(){
+    public static void showLine(){
         System.out.println("____________________________________________________________");
     }
 
-    private void showWelcome(){
+    public void showWelcome(){
+        showLine();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String greet = "Hello I'm Duke\n"
+        String greet = "Hello I'm duke.Duke\n"
                 + "What can I do for you?";
         System.out.println("Hello from\n" + logo);
         System.out.println(greet);
+        showLine();
     }
 
-    private String readCommand(){
+    public String readCommand(){
         return in.nextLine();
     }
 
-    private void showError(){
+    public void addTask(Task t, TaskList taskList){
+        System.out.println("Got it. I've added this task: ");
+        System.out.println(t);
+        System.out.println("Now you have " + taskList.size() + " tasks in the list");
+    }
+
+    public void listTask(TaskList taskList){
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 1; i <= taskList.size(); i++) {
+            Task currTask = taskList.get(i - 1);
+            System.out.println(i + ". " + currTask);
+        }
+    }
+
+    public void doneTask(int index, TaskList taskList){
+        Task currTask = taskList.get(index);
+        System.out.println((index + 1) + ". " + currTask);
+    }
+
+    public void deleteTask(int index, TaskList taskList){
+        Task currTask = taskList.get(index);
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(currTask);
+        System.out.println("Now you have " + (taskList.size()  - 1) + " tasks on the list.");
+    }
+
+    public void findTask(String keyword, TaskList taskList){
+        System.out.println("Here are the matching tasks in your list:");
+        for (int i = 1; i <= taskList.size(); i++) {
+            Task currTask = taskList.get(i - 1);
+            if (currTask.getDescription().contains(keyword)) {
+                System.out.println(i + ". " + currTask);
+            }
+        }
+    }
+
+    public void showLoadingError(){
         return;
     }
 
-    private void showBye(){
+    public void showBye(){
         String exit = "Bye. Hope to see you again soon!";
         System.out.println(exit);
     }
-
 
 }
