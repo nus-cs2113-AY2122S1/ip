44cc5f8b32f9ea682d00f2e50437a0710b33c5df
shoibloya <52075633+shoibloya@users.noreply.github.com>
2021-09-30 11:04:08 +0800

    Exception Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,46 +14,53 @@ public class Duke {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
 
-            if(line.split(" ")[0].equals("bye")) {
-                break;
-            } else if(line.split(" ")[0].equals("list")){
-                for (int i = 0; i < listSize; i++) {
-                    System.out.println(i+1 + "." + List[i]);
+            try {
+                if (line.split(" ")[0].equals("bye")) {
+                    break;
+                } else if (line.split(" ")[0].equals("list")) {
+                    for (int i = 0; i < listSize; i++) {
+                        System.out.println(i + 1 + "." + List[i]);
+                    }
+                } else if (line.split(" ")[0].equals("done")) {
+                    //When the user keys in done
+                    int taskNumber = Integer.valueOf(line.split(" ")[1]);
+                    List[taskNumber - 1].setDone("X");
+                } else if (line.split(" ")[0].equals("todo")) {
+                    //When the user adds a todo
+                    try {
+
+                        List[listSize] = new Todo(line.split("todo ")[1]);
+                    } catch (Exception e) {
+                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    }
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(List[listSize]);
+                    System.out.println("Now you have " + (listSize + 1) + " tasks in the list.");
+                    listSize++;
+                } else if (line.split(" ")[0].equals("deadline")) {
+                    //When the user adds a deadline
+                    List[listSize] = new Deadline(line.split("deadline ")[1], line.split("/by")[1]);
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(List[listSize]);
+                    System.out.println("Now you have " + (listSize + 1) + " tasks in the list.");
+                    listSize++;
+                } else if (line.split(" ")[0].equals("event")) {
+                    //When the user adds an event
+                    List[listSize] = new Event(line.split("event ")[1], line.split("/at")[1]);
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(List[listSize]);
+                    System.out.println("Now you have " + (listSize + 1) + " tasks in the list.");
+                    listSize++;
+                } else {
+                    //When a user adds in a random task
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    //throw new Exception("");
                 }
-            } else if (line.split(" ")[0].equals("done")) {
-                //When the user keys in done
-                int taskNumber = Integer.valueOf(line.split(" ")[1]);
-                List[taskNumber-1].setDone("X");
-            } else if(line.split(" ")[0].equals("todo")) {
-                //When the user adds a todo
-                List[listSize] = new Todo(line.split("todo ")[1]);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(List[listSize]);
-                System.out.println("Now you have " + (listSize + 1)  + " tasks in the list.");
-                listSize++;
-            } else if(line.split(" ")[0].equals("deadline")) {
-                //When the user adds a deadline
-                List[listSize] = new Deadline(line.split("deadline ")[1], line.split("/by")[1]);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(List[listSize]);
-                System.out.println("Now you have " + (listSize + 1)  + " tasks in the list.");
-                listSize++;
-            } else if(line.split(" ")[0].equals("event")) {
-                //When the user adds an event
-                List[listSize] = new Event(line.split("event ")[1], line.split("/at")[1]);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(List[listSize]);
-                System.out.println("Now you have " + (listSize + 1)  + " tasks in the list.");
-                listSize++;
-            }
-            else {
-                //When a user adds in a random task
-                List[listSize] = new Task(line);
-                System.out.println("added: " + line);
-                listSize++;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
         //Exiting the bot
