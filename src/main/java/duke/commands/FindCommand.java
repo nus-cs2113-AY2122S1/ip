20d61df732fa13b6975669e3e5c9e84cb6a17486
Dmitri <dmitriyam.rex1738@gmail.com>
2021-09-30 14:06:13 +0800

    Added documentation for FindCommand

--- src/main/java/duke/commands/FindCommand.java
+++ src/main/java/duke/commands/FindCommand.java
@@ -8,7 +8,13 @@ public class FindCommand extends Command{
     private TaskList tlist;
     private Ui ui;
     private String description;
-
+    /**
+     * Constructor for command when the user wishes to find tasks using a keyword.
+     *
+     * @param tlist list of Tasks to be viewed
+     * @param ui Handles interaction with the user
+     * @param description Description of tasks to be found
+     */
     public FindCommand(TaskList tlist, Ui ui,String description){
         this.tlist = tlist;
         this.ui = ui;
