70533ff7da0f9cd68ea3bdb942b86ba5bbe349dd
flerovious <85042815+flerovious@users.noreply.github.com>
2021-10-01 03:09:01 +0800

    Add JavaDoc for Task

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -4,25 +4,47 @@ public abstract class Task {
     private final String name;
     private Boolean isDone;
 
+    /**
+     * Task constructor
+     *
+     * @param name Name of task
+     */
     public Task(String name) {
         this.name = name;
         this.isDone = false;
     }
 
+    /**
+     * @return Name of task
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns an icon for task completion status
+     *
+     * @return Status icon of task
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Marks task as done
+     */
     public void markAsDone() {
         isDone = true;
     }
 
+    /**
+     * @return Type of Task
+     */
     public abstract TaskType getTaskType();
 
+    /**
+     * @return Time of Task in MMM dd yyyy format
+     */
     public abstract String getTime();
 
     @Override
