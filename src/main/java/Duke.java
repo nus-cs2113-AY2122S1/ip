8b14c36f4e6072e0f5e6506f570d43066fdda32a
ZhangErliCarl <zhangerli.carl@gamilcom>
2021-09-30 14:04:48 +0800

    Add increment: Level 1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,8 +7,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello from\n" + logo);
-        String greetings = " Hello! I'm Duke\n" + " What can I do for you?\n\n" + " Bye. Hope to see you again soon!";
-        System.out.println(greetings);
+        System.out.println("...................................................");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("...................................................");
+        Scanner input = new Scanner(System.in); 
+        while(!lineIn.equals("bye")){
+            String string = input.nextLine();
+            System.out.println("...................................................");
+            System.out.println(lineIn);
+            System.out.println("...................................................");
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("...................................................");
     }
 }
