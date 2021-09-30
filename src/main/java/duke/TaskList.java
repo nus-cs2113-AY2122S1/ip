3d7ed3bab603cfb6969935dd786a6d670c20a6d7
YipWayne <yipwayyne@gmail.com>
2021-09-30 14:54:27 +0800

    Added TaskList and Storage

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -1,6 +1,32 @@
-package duke.tasks;
+package duke;
+import duke.tasks.Task;
+
+import java.util.ArrayList;
 
 public class TaskList {
 
+    private static ArrayList<Task> taskList;
+
+    public TaskList(){
+        taskList = new ArrayList<Task>();
+    }
+
+    public int size(){
+        return taskList.size();
+    }
+
+    public void add(Task t){
+        taskList.add(t);
+    }
+
+    public void remove(int index){
+        taskList.remove(index);
+    }
+
+    public Task get(int index){
+        return taskList.get(index);
+    }
+
 
 }
+
