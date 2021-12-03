307354b09bef9587805182c476e465b703081fe6
Zhang Erli <73495226+ZhangErliCarl@users.noreply.github.com>
2021-12-03 14:17:35 +0800

    Update InvalidCommandException.java

--- src/main/java/InvalidCommandException.java
+++ src/main/java/InvalidCommandException.java
@@ -1,7 +1,8 @@
 public class InvalidCommandException extends RuntimeException{
     InvalidCommandException() {
+        String Message = "     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
         System.out.println("    ____________________________________________________________");
-        System.out.println("     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        System.out.println(Message);
         System.out.println("    ____________________________________________________________");
     }
 }
