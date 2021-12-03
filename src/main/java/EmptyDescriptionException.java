160450693f2ef5eae5a61ae3f503ed379c53f207
Zhang Erli <73495226+ZhangErliCarl@users.noreply.github.com>
2021-12-03 14:17:05 +0800

    Update EmptyDescriptionException.java

--- src/main/java/EmptyDescriptionException.java
+++ src/main/java/EmptyDescriptionException.java
@@ -1,7 +1,8 @@
 public class EmptyDescriptionException extends RuntimeException{
     EmptyDescriptionException() {
+        String Message = "     ☹ OOPS!!! The description of a todo cannot be empty.";
         System.out.println("    ____________________________________________________________");
-        System.out.println("     ☹ OOPS!!! The description of a todo cannot be empty.");
+        System.out.println(Message);
         System.out.println("    ____________________________________________________________");
     }
 }
